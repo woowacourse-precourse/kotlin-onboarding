b05a057f3eeafd319f77ac4fe8443cf0036e3bc1
PCBL <cksgodl1@naver.com>
2022-10-26 23:45:39 +0900

    feat : Problem1 기능 요구 사항 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -3,3 +3,22 @@ package onboarding
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
+
+// 기능 목록
+/**
+ * 각자리 숫자를 모두 더하거나 곱해 큰수를 구하는 기능
+ */
+fun getBiggerNum() {
+
+}
+
+/**
+ * 포비, 크롱 승패 반별 기능
+ * 포비가 이기면 1
+ * 크롱이 이기면 2
+ * 무승부는 0
+ * 예외사항은 -1
+ */
+fun validateWinner() {
+
+}
