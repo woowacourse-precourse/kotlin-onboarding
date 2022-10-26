ac93ab02ec6be7fb1376f0e07640faa4433fb347
hansh0101 <hansh0101@naver.com>
2022-10-26 18:45:32 +0900

    [FEATURE] Solve Problem2

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,39 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var hasRedundant = true
+    var result = cryptogram
+    while (hasRedundant) {
+        val removeIndexPairList = mutableListOf<Pair<Int, Int>>()
+        var lastCharacter = ' '
+        var startIndex = -1
+        var endIndex = -1
+        var flushFlag = false
+
+        result.forEachIndexed { index, c ->
+            if (lastCharacter == c) {
+                endIndex = index
+                flushFlag = true
+            } else {
+                if (flushFlag) {
+                    removeIndexPairList.add(Pair(startIndex, endIndex))
+                }
+                flushFlag = false
+                startIndex = index
+                lastCharacter = c
+            }
+        }
+        if (flushFlag) {
+            removeIndexPairList.add(Pair(startIndex, endIndex))
+        }
+
+        var removedCount = 0
+        removeIndexPairList.forEach {
+            result = result.replaceRange(it.first - removedCount, it.second - removedCount + 1, "")
+            removedCount += it.second - it.first + 1
+        }
+
+        hasRedundant = removeIndexPairList.isNotEmpty()
+    }
+    return result
 }
