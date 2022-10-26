4abd9be2c679850988e2cdff3f43437a368f3d5f
RightHennessy <myheaven_b@naver.com>
2022-10-26 22:33:10 +0900

    feat: Add return conditin when first or last page

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -8,7 +8,8 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
         return -1
     if (crong[0]+1 != crong[1])
         return -1
-
+    if (pobi[0] == 1 || crong[1] == 400)
+        return -1
 
     pobiNum = getMax(getSum(pobi[0]), getMultiply(pobi[0]), getSum(pobi[1]), getMultiply(pobi[1]))
     crongNum = getMax(getSum(crong[0]), getMultiply(crong[0]), getSum(crong[1]), getMultiply(crong[1]))
@@ -50,13 +51,4 @@ fun getMax(w:Int, x:Int, y:Int, z:Int) : Int
     val list = arrayOf<Int>(w, x, y, z)
     list.sort()
     return list[0]
-}
-
-fun main()
-{
-    val list1 = listOf(134, 2)
-    val list2 = listOf(34, 899)
-
-    solution1(list1, list2)
-    return
 }
