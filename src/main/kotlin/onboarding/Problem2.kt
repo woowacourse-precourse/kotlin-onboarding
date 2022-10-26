2d6fe299724a9a4ddab2ea473f5ef3dae3482e7b
ese111 <feung94@gmail.com>
2022-10-26 18:39:32 +0900

    feat: StringBuilder 생성

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,11 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    // StringBuilder 생성
+    val sb = StringBuilder()
+    // 스택에 문자를 push한다.
+    // 스택에 문자가 있으면 peek을 해서 push해야할 문자와 같은지 확인한다.
+    // 같으면 pop하고 push하지 않는다.
+    // 스택을 StringBulider에 넣고 String으로 형변환하여 반환한다
+    return sb.toString()
 }
