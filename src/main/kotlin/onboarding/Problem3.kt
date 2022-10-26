7fd009bae72c81098d55f3a143e76dfdeafa3c05
hansh0101 <hansh0101@naver.com>
2022-10-26 18:55:56 +0900

    [FEATURE] Solve Problem3

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var result = 0
+
+    for (i in 1..number) {
+        var digit = i
+        while (digit != 0) {
+            if ((digit % 10 != 0) && ((digit % 10) % 3 == 0)) {
+                result++
+            }
+            digit /= 10
+        }
+    }
+
+    return result
 }
