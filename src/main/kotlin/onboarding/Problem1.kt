4efb1f2de5d9b5012ca6d9b472696bb4835672cc
ese111 <feung94@gmail.com>
2022-10-26 18:17:49 +0900

    feat: 포비의 페이지와 크롱의 페이지를 String으로 바꾼다.

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,19 @@
 package onboarding
 
+import kotlin.math.max
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
+    // 포비의 페이지와 크롱의 페이지를 String으로 바꾼다.
+    // 배열을 돌면서 각 자리수를 합하는 sum()을 구현한다.
+    // 각 자리수를 곱하는 multiplication()을 구현한다.
+    // 각 자리수를 합하거나 곱한거중 큰 수를 찾는다.
+    // 결과를 반환한다.
+
+    val pobiToString = pobi.map { it.toString() }
+    val crongToString = crong.map { it.toString() }
+
+
+    return 0
 }
+
