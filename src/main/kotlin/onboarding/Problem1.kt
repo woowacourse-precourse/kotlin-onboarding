3d17e94c1505eea225c6ee13bfbb59f01ed9e63c
person00333 <person00333@gmail.com>
2022-10-26 23:32:33 +0900

    문제 1 업로드

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,48 @@
 package onboarding
 
+import kotlin.math.max
+
+fun calScore(user: List<Int>): Int{
+
+    val rightMax: Int = pageScore(user[0])
+
+    val leftMax: Int = pageScore(user[1])
+
+
+    return max(rightMax, leftMax)
+
+}
+
+fun pageScore(number: Int): Int{
+    // plus
+    var scorePlus = 0
+
+    //mul
+    var scoreMul = 1
+
+    var pageNumber: Int = number
+    while (pageNumber > 0){
+        val num: Int = pageNumber % 10
+
+        scorePlus += num
+        scoreMul *= num
+        pageNumber /= 10
+    }
+    return max(scoreMul, scorePlus)
+}
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    val pobiScore: Int = calScore(pobi)
+    val crongScore: Int = calScore(crong)
+
+    if (((pobi[0] % 2 == 1) && (pobi[0] + 1 == pobi[1])) && ((crong[0] % 2 == 1) && (crong[0] + 1 == crong[1]))){
+        if (pobiScore > crongScore) {
+            return 1
+        }else if(pobiScore < crongScore) {
+            return 2
+        }else {
+            return 0
+        }
+    }
+    return -1
 }
