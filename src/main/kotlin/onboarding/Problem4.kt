c0c705c57718a1bc6b20eeacc1bc7fa532c0acb9
ChunBaee <mljung10@naver.com>
2022-10-27 04:07:47 +0900

    4번] 기능 1) 선언 : 아스키코드를 통해 문자열을 분류 / 변환할 예정이므로, 사용될 아스키코드의 범위를 담은 변수 선언

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,12 @@
 package onboarding
 
+
+const val EMPTY_CODE = 32
+const val CAPITAL_START = 65
+const val CAPITAL_END = 90
+const val SMALL_START = 97
+const val SMALL_END = 122
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+
 }
+
