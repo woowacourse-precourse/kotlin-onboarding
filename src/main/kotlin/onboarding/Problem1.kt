9dd8f46b330a98613b90608c2e1e0e6caa12e89c
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-10-27 08:55:52 +0900

    feat:기능목록 1번 추가[문제1]

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,18 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
+    if(!(exception(pobi) && exception(crong))) return -1
+
+    return 0
+}
+
+/* 기능 1번*/
+fun exception(user: List<Int>) : Boolean {
+
+    if(user[0] % 2 != 1 || user[1] % 2 != 0) return false
+    if(user[0] + 1 != user[1]) return false
+    if(user[0] <= 1 || user[1] >= 400) return false
+
+    return true
 }
