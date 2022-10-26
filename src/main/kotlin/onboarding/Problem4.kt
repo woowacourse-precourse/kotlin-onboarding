b704a394b83f0f8af9e76cde1579f2661ce261e5
ese111 <feung94@gmail.com>
2022-10-26 22:56:10 +0900

    feat: decode() 구현 대소문자와 공백을 구분하여 StringBuilder에 append하는 함수

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,12 +1,37 @@
 package onboarding
 
+private lateinit var upper: Map<Char, Char>
+private lateinit var lower: Map<Char, Char>
+
 fun solution4(word: String): String {
     // 대문자를 Map으로 저장한다.
+    upper = upperBoard()
     // 소문자를 Map으로 저장한다.
+    lower = lowerBoard()
     // 들어온 문자열을 돌면서 대문자인지 소문자인지 판단한다.
     // map에 char를 키값으로 보내서 청개구리가 하는 말을 StringBuilder에 저장한다.
-    TODO("프로그램 구현")
+    return word.decode()
 }
+
+private fun String.decode(): String {
+    val sb = StringBuilder()
+    forEach {
+        if (it.isUpper()) {
+            sb.append(upper[it])
+            return@forEach
+        }
+        if (it.isLower()) {
+            sb.append(lower[it])
+            return@forEach
+        }
+        sb.append(' ')
+    }
+    return sb.toString()
+}
+
+private fun Char.isLower() = this in 'a'..'z'
+
+private fun Char.isUpper() = this in 'A'..'Z'
 
 private fun upperBoard(): Map<Char, Char> {
     var alphaCount = 25
@@ -27,4 +52,5 @@ private fun lowerBoard(): Map<Char, Char> {
     }
     return map
 }
+
 
