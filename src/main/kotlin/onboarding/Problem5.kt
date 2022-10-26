96ca7a0c786fa02f7ab378d9f18c398ef2c2b9c2
ese111 <feung94@gmail.com>
2022-10-26 23:24:29 +0900

    Problem5 feat: calculator() 구현 오만원권부터 일원까지 나누며 금액의 지폐 개수를 알아내는 함수

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,11 +1,24 @@
 package onboarding
 
+private val MONEY_TYPE = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+
 fun solution5(money: Int): List<Int> {
     // 답을 표시할 IntArray 생성
-    val moneyCount = intArrayOf(1, 0, 0, 0, 0, 2, 0, 3, 7)
-    // 5만원 단위부터 일원 단위까지 순서대로 나눈다
-    // 5만원으로 나눠지면 나눈값을 IntArray[0]에 넣는다.
-    // 위 작업을 1원까지 반복
-    // IntArray반환
+    val moneyCount = money.calculator()
     return moneyCount.toList()
+}
+
+private fun Int.calculator(): IntArray {
+    val tmp = IntArray(9) { 0 }
+    var money = this
+    // 5만원 단위부터 일원 단위까지 순서대로 나눈다
+    MONEY_TYPE.forEachIndexed { index, i ->
+        // 5만원으로 나눠지면 나눈값을 IntArray[0]에 넣는다.
+        if(money/i != 0) {
+            tmp[index] = money/i
+        }
+        money %= i
+    }
+    // IntArray반환
+    return tmp
 }
