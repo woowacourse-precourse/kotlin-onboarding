eab00ac9a2fe0bbcac064955718d2939ff1bf832
PCBL <cksgodl1@naver.com>
2022-10-27 00:01:08 +0900

    fix : Problem1 기능 요구사항 추가(checkException) 및 파라미터 명 수정

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,10 +1,15 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
 }
 
 // 기능 목록
+/**
+ * 예외 사항 체크
+ */
+fun checkException(pobi: List<Int>, crong: List<Int>) {
+
+}
 
 /**
  * 각자리 숫자를 모두 더하거나 곱해 큰 수를 반환하는 기능
@@ -16,7 +21,7 @@ fun compareAddAndMul(num: Int): Int {
 /**
  * 왼쪽 페이지, 오른쪽 페이지 중 큰 수를 구하는 기능
  */
-fun getBiggerPageNum(leftNum: Int, rightNum: Int): Int {
+fun getBiggerPageNum(leftPage: Int, rightPage: Int): Int {
 
 }
 
@@ -25,8 +30,7 @@ fun getBiggerPageNum(leftNum: Int, rightNum: Int): Int {
  * 포비가 이기면 1
  * 크롱이 이기면 2
  * 무승부는 0
- * 예외사항은 -1
  */
-fun validateWinner(pobiNum: Int, crongNumb: Int): Int {
+fun validateWinner(pobiNum: Int, crongNum: Int): Int {
 
 }
