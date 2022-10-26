87545fa38c553f00b2cf5ef65bb19c46dffd3794
RightHennessy <myheaven_b@naver.com>
2022-10-26 17:32:43 +0900

    feat : Add fuction-getSum

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,28 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    println(getSum(pobi[0]))
+    println(crong[0])
+    return 0
+}
+
+fun getSum(x:Int) : Int {
+    var sum:Int = 0
+    var numX:Int = x
+
+    while (numX > 0){
+        sum += numX%10
+        numX /= 10
+    }
+
+    return sum
+}
+
+fun main()
+{
+    val list1 = listOf(134, 2)
+    val list2 = listOf(34, 899)
+
+    solution1(list1, list2)
+    return
 }
