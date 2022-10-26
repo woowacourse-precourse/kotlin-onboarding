fdf386fdb2413a9ee6b197348012dcabaafa4571
ese111 <feung94@gmail.com>
2022-10-27 01:20:54 +0900

    Problem6 feat: 비교해야할 문자를 만드는 기능 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
+    // 2글자씩 포함된 원소가 있는 지 비교하는 기능
+    // 비교해야할 문자 만드는 기능
+    forms.forEach {
+        for (i in 1 until it[1].length) {
+            val str = it[1].getStr(i)
+        }
+    }
+    // 출력하는 기능
     TODO("프로그램 구현")
 }
+
+private fun String.getStr(index: Int) = "${this[index - 1]}${this[index]}"
+
+
