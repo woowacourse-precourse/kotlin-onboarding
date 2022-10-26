745fc1043c9d98f444af65207877f02aa12d160a
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-27 01:47:50 +0900

    feat(Problem1): add isPass method

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,4 +1,10 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+    TODO()
+}
+
+//예외상황 판단하는 함수들
+fun isPass(list: List<Int>): Boolean {
+    return (list[0] % 2 == 1) && (list[1] % 2 == 0) && (list[0] + 1 == list[1])
 }
