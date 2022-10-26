2e7b613bf2f5d3dbc95dab8268ee82dbc9a416db
ChunBaee <mljung10@naver.com>
2022-10-27 00:53:48 +0900

    1번] 기능 2) 예외2 : 마주보는 페이지이므로, 항상 왼쪽 페이지 수 + 1은 오른쪽 페이지 수 체크

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -10,7 +10,7 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
  *  예외사항 있는지 판별
  */
 private fun checkIsRight(list: List<Int>): Boolean {
-    return checkOddAndEven(list)
+    return checkOddAndEven(list) && checkCorrectPageNum(list)
 }
 
 /**
@@ -18,4 +18,11 @@ private fun checkIsRight(list: List<Int>): Boolean {
  */
 private fun checkOddAndEven(list: List<Int>): Boolean {
     return (list[0] % 2 == 1 && list[1] % 2 == 0)
+}
+
+/**
+ * 예외사항 2 : 마주보는 페이지이므로 항상 왼쪽 페이지 + 1 은 오른쪽 페이지가 되어야 한다.
+ */
+private fun checkCorrectPageNum(list : List<Int>) : Boolean {
+    return (list[0] + 1 == list[1])
 }
