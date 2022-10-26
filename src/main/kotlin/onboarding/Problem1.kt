ccd7eddb1b0be806339c6a19b0ebcd09e17b6b2e
tmdgh1592 <tmdgh1592@naver.com>
2022-10-27 00:17:19 +0900

    feat: 각 자릿수를 모두 더한 값을 반환하는 메서드를 구현 - getAddedDigitSum() 메서드 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -3,3 +3,9 @@ package onboarding
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
+
+fun getAddedDigitSum(number: Int): Int =
+    number.toString()
+        .fold(0) { total, digit ->
+            total + Character.getNumericValue(digit)
+        }
