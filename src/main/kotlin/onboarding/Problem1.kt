59bf9b47002f218975ffdf8a3a53490b5d73ee89
inseonyun <yuninseon@naver.com>
2022-10-26 22:29:49 +0900

    [Problem1] 예외 처리 함수 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,34 @@
 package onboarding
 
+fun checkOddEvenContinue(pobi: List<Int>, crong: List<Int>): Boolean {
+    for(i in 0 until 2) {
+        when(i) {
+            0 -> {
+                if(pobi[i] % 2 != 1 || crong[i] % 2 != 1)
+                    return false
+            }
+            1 -> {
+                if(pobi[i] != pobi[0] + 1 || crong[i] != crong[0] + 1)
+                    return false
+            }
+        }
+    }
+    return true
+}
+
+fun checkNormalPage(pobi: List<Int>, crong: List<Int>): Boolean {
+    for(i in 0 until 2) {
+        if(pobi[i] <= 1 || pobi[i] >= 400 )
+            return false
+        if(crong[i] <= 1 || crong[i] >= 400)
+            return false
+    }
+    return true
+}
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if(checkOddEvenContinue(pobi, crong) && checkNormalPage(pobi, crong)) {
+
+    }
+    return -1
 }
