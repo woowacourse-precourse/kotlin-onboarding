f2d605e897cb873d758baf680b6f8f41ce2285b2
RightHennessy <myheaven_b@naver.com>
2022-10-27 01:02:56 +0900

    feat : Solve problem2

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,18 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var flag:Int = 1
+    var str:String = cryptogram
+
+    while(flag > 0){
+        flag = 0
+        for (i in 1..str.length-1) {
+            if (str[i - 1] == str[i]) {
+                flag++
+                str = str.replaceRange(i-1..i, "  ")
+            }
+        }
+        str = str.replace(" ", "")
+    }
+    return str
 }
