72111112615817fd288c4b171da7395eb6894b80
ChunBaee <mljung10@naver.com>
2022-10-27 02:15:58 +0900

    3번] 기능 1) 체크 : 지정된 수 안에 원하는 숫자를 포함 한 수가 몇개인지 체크

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    checkContain(number, 3)
+    checkContain(number, 6)
+    checkContain(number, 9)
+}
+
+/**
+ * 원하는 숫자 (3, 6, 9)를 포함하는 숫자 갯수 반환하는 함수
+ */
+private fun checkContain(num : Int, standard : Int) : Int {
+    var count = 0
+    for(i in 1..num) {
+        if(i.toString().contains(standard.toString())) { //3이 포함된 숫자일 경우 동작
+            count ++
+        }
+    }
+    return count
 }
