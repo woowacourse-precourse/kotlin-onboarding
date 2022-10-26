1b627bbccad5c086f3a3fb0c750838af6351dbc7
ese111 <feung94@gmail.com>
2022-10-26 19:51:08 +0900

    feat: 대문자 보드 구현 대문자의 규칙이 담긴 map 생성 함수 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,30 @@
 package onboarding
 
 fun solution4(word: String): String {
+    // 대문자를 Map으로 저장한다.
+    // 소문자를 Map으로 저장한다.
+    // 들어온 문자열을 돌면서 대문자인지 소문자인지 판단한다.
+    // map에 char를 키값으로 보내서 청개구리가 하는 말을 StringBuilder에 저장한다.
     TODO("프로그램 구현")
 }
+
+private fun upperBoard(): Map<Char, Char> {
+    var alphaCount = 25
+    val map = mutableMapOf<Char, Char>()
+    for (i in 'A'..'Z') {
+        map[i] = i + alphaCount
+        alphaCount--
+    }
+    return map
+}
+
+//private fun lowerBoard(): Map<Char, Char> {
+//    var alphaCount = 25
+//    val map = mutableMapOf<Char, Char>()
+//    for (i in 'A'..'Z') {
+//        map[i] = i + alphaCount
+//        alphaCount--
+//    }
+//    return map
+//}
+
