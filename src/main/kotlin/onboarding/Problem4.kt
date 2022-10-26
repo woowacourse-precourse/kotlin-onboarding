5d441643c2aacab1d27348d9152a811cb1938b91
hansh0101 <hansh0101@naver.com>
2022-10-26 19:38:39 +0900

    [FEATURE] Solve Problem4

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,24 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var result = ""
+
+    val upperCaseSum = 'A'.code + 'Z'.code
+    val lowerCaseSum = 'a'.code + 'z'.code
+
+    word.forEach {
+        when (val code = it.code) {
+            in 'A'.code..'Z'.code -> {
+                result += (upperCaseSum - code).toChar()
+            }
+            in 'a'.code..'z'.code -> {
+                result += (lowerCaseSum - code).toChar()
+            }
+            else -> {
+                result += it
+            }
+        }
+    }
+
+    return result
 }
