c18658206fa5c088a7f6ece94ec5a39252c3d9e6
ChunBaee <mljung10@naver.com>
2022-10-27 01:47:00 +0900

    2번] 기능 1) 변환 : 문자열 간 비교를 편하게 하기 위해 문자를 문자열로 변환

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,5 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var mList = cryptogram.split("").filter { it != "" } //String을 개별 문자별로 비교하기 편하도록 List로 변환하며, 맨 앞 뒤 항목들 역시 공백의 배열로 추가되므로, filter 사용하여 제거했다.
 }
