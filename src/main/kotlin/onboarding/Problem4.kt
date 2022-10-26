529a5b4272ec4926dbc763e12d7e36cdaa472ddb
RightHennessy <myheaven_b@naver.com>
2022-10-27 01:42:04 +0900

    feat : Add function isAlphabet

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var str:String = word
+
+    for (i in 0..str.length-1){
+        if (isAlphabet(str[i]))
+            str.replaceRange(i..i, (str[i]-1).toString())
+    }
+
+    return str
+}
+
+fun isAlphabet(x: Char): Boolean {
+    if (x>= 'a' && x<= 'z')
+        return true
+    if (x>= 'A' && x<= 'Z')
+        return true
+    return false
 }
