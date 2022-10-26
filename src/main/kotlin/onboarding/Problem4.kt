18e96317cbaa4b29177ea5d46f80f173b65e5bd9
RightHennessy <myheaven_b@naver.com>
2022-10-27 02:04:13 +0900

    fix : Remove function isAlphabet, and add to if-else in solution function

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -4,17 +4,18 @@ fun solution4(word: String): String {
     var str:String = word
 
     for (i in 0..str.length-1){
-        if (isAlphabet(str[i]))
-            str.replaceRange(i..i, (str[i]-1).toString())
+        if (str[i] >= 'a' && str[i] <= 'm'){
+            str = str.replaceRange(i..i,('z' - (str[i] - 'a')).toString())
+        }
+        else if (str[i] >= 'n' && str[i] <= 'z'){
+            str = str.replaceRange(i..i,('a' + ('z' - str[i])).toString())
+        }
+        if (str[i] >= 'A' && str[i] <= 'M'){
+            str = str.replaceRange(i..i,('Z' - (str[i] - 'A')).toString())
+        }
+        else if (str[i] >= 'N' && str[i] <= 'Z'){
+            str = str.replaceRange(i..i,('A' + ('Z' - str[i])).toString())
+        }
     }
-
     return str
-}
-
-fun isAlphabet(x: Char): Boolean {
-    if (x>= 'a' && x<= 'z')
-        return true
-    if (x>= 'A' && x<= 'Z')
-        return true
-    return false
 }
