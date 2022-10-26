4062064acdfc14a10eebcf25d403d863f93d11ff
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-10-26 18:55:08 +0900

    Problem5 1차 구현(dp version)

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,22 @@
 package onboarding
 
+import java.lang.Math.min
+
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val coinToUse=arrayOf(1, 10, 50, 100, 500, 1000, 5000, 10000, 50000)
+    val coinCnt=Array(money+1){Array(9){0}}
+    val cache=Array(money+1){Int.MAX_VALUE}
+    cache[0]=0
+
+    coinToUse.forEachIndexed { coinIndex, curCoin ->
+        for(paidMoney in curCoin..money){
+            if(cache[paidMoney]>cache[paidMoney-curCoin]+1){
+                cache[paidMoney]=cache[paidMoney-curCoin]+1
+                coinCnt[paidMoney]=coinCnt[paidMoney-curCoin].copyOf()
+                coinCnt[paidMoney][coinIndex]++
+            }
+        }
+    }
+
+    return coinCnt[money].reversed().toList()
 }
