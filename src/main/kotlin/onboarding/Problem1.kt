3b21a95dfa48f791180608f7c54293368756d5fa
tmdgh1592 <tmdgh1592@naver.com>
2022-10-27 01:09:32 +0900

    feat: 두 정수를 비교해서 큰 값을 반환하는 메서드 구현 - compareNumber() 메서드 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -23,3 +23,13 @@ fun validatePage(leftNumber: Int, rightNumber: Int) =
 
 fun getBiggerNumber(number1: Int, number2: Int): Int =
     max(number1, number2)
+
+fun compareNumber(number1: Int, number2: Int): Int {
+    val diff = number1 - number2
+    when {
+        diff > 0 -> return 1
+        diff < 0 -> return 2
+        diff == 0 -> return 0
+    }
+    return -1
+}
