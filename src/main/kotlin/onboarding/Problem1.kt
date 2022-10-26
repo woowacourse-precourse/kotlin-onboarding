387657c01f95b87b45eabecd418537fae3fa1b75
ChunBaee <mljung10@naver.com>
2022-10-27 00:52:46 +0900

    1번] 기능 1) 예외1 : 왼쪽 수는 항상 홀수 & 오른쭉 수는 항상 짝수 체크

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,21 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if (!checkIsRight(pobi) || !checkIsRight(crong)) { //예외사항 체크 (둘 중 하나라도 false 이면 예외사항이므로 -1 리턴)
+        return -1
+    }
+}
+
+/**
+ *  예외사항 있는지 판별
+ */
+private fun checkIsRight(list: List<Int>): Boolean {
+    return checkOddAndEven(list)
+}
+
+/**
+ * 예외사항 1 : 왼쪽 수는 항상 홀수, 오른쪽 수는 항상 짝수여야 한다.
+ */
+private fun checkOddAndEven(list: List<Int>): Boolean {
+    return (list[0] % 2 == 1 && list[1] % 2 == 0)
 }
