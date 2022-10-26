57b828f87eef82bbe48380c9636a4ce5043ca122
RightHennessy <myheaven_b@naver.com>
2022-10-26 23:00:00 +0900

    refactor : Delete function getMax, Change max fuction

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,7 @@
 package onboarding
 
+import kotlin.math.max
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     val pobiNum:Int
     val crongNum:Int
@@ -11,8 +13,8 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     if (pobi[0] <= 1 || crong[1] >= 400)
         return -1
 
-    pobiNum = getMax(getSum(pobi[0]), getMultiply(pobi[0]), getSum(pobi[1]), getMultiply(pobi[1]))
-    crongNum = getMax(getSum(crong[0]), getMultiply(crong[0]), getSum(crong[1]), getMultiply(crong[1]))
+    pobiNum = max(max(getSum(pobi[0]), getMultiply(pobi[0])), max(getSum(pobi[1]), getMultiply(pobi[1])))
+    crongNum = max(max(getSum(crong[0]), getMultiply(crong[0])), max(getSum(crong[1]), getMultiply(crong[1])))
 
     if (pobiNum == crongNum)
         return 0
@@ -44,11 +46,4 @@ fun getMultiply(x:Int) : Int{
     }
 
     return multiply
-}
-
-fun getMax(w:Int, x:Int, y:Int, z:Int) : Int
-{
-    val list = arrayOf<Int>(w, x, y, z)
-    list.sort()
-    return list[0]
 }
