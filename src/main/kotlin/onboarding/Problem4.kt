cfc29ef31d42971bd338ba98ff3fd118d382b128
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-10-26 22:33:41 +0900

    20221026 우테코 프리코스(코틀린) 문제 4번 추가 수정

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,6 +1,9 @@
 package onboarding
 
 fun solution4(word: String): String {
+    /**
+     * 1. 빠른 문자 변환을 위한 Map 선언
+     * */
     val wordMap = mapOf<String, String>(
         "A" to "Z", "B" to "Y", "C" to "X", "D" to "W", "E" to "V", "F" to "U", "G" to "T", "H" to "S", "I" to "R",
         "J" to "Q", "K" to "P", "L" to "O", "M" to "N", "N" to "M", "O" to "L", "P" to "K", "Q" to "J", "R" to "I",
@@ -9,12 +12,16 @@ fun solution4(word: String): String {
         "j" to "q", "k" to "p", "l" to "o", "m" to "n", "n" to "m", "o" to "l", "p" to "k", "q" to "j", "r" to "i",
         "s" to "h", "t" to "g", "u" to "f", "v" to "e", "w" to "d", "x" to "c", "y" to "b", "z" to "a",
     )
+
+    /**
+     * 2. answer를 StringBuilder로 선언하여 index 단위로 접근 및 수정
+     * */
     var answer = StringBuilder()
     for (char in word){
-        if (wordMap.get(char.toString()).isNullOrEmpty()){
+        if (wordMap[char.toString()].isNullOrEmpty()){
             answer.append(char)
         }else{
-            answer.append(wordMap.get(char.toString()))
+            answer.append(wordMap[char.toString()])
         }
     }
     return answer.toString()
