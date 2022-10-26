ec20eee99d07354c80da5220defa28fccd1d5240
RightHennessy <myheaven_b@naver.com>
2022-10-27 01:18:44 +0900

    feat : Add function countClap

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,21 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var cnt:Int = 0
+
+    for (i in 1..number){
+        cnt += countClap(i)
+    }
+}
+
+fun countClap(x: Int): Int {
+    var numX:Int = x
+    var cnt:Int = 0
+
+    while(numX > 0){
+        if (numX%10 == 3 || numX%10 == 6 || numX%10 == 9)
+            return 1
+        numX /= 10
+    }
+    return 0
 }
