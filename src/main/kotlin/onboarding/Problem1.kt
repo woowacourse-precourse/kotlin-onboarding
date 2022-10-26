15d812fa986556e1edc8dcfd6c40074f32b2af84
yeongjin <dudwls901@naver.com>
2022-10-26 15:58:07 +0900

    [Feat] validation check 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,37 @@
 package onboarding
 
+import kotlin.math.abs
+
+/* 기능 목록
+* validation check 구현
+* getScore 구현
+* 점수 비교 구현
+* */
+
+fun isCorrectInput(pobi: List<Int>, crong: List<Int>): Boolean {
+    return checkListSize(pobi, crong) &&
+        checkInRange(pobi, crong) &&
+        checkPageSequential(pobi, crong)
+}
+
+fun checkListSize(pobi: List<Int>, crong: List<Int>): Boolean {
+    return pobi.size == 2 && crong.size == 2
+}
+
+fun checkInRange(pobi: List<Int>, crong: List<Int>): Boolean {
+    for(i in pobi.indices){
+        if(pobi[i] !in 3 .. 398 || crong[i] !in 3 .. 398) return false
+    }
+    return true
+}
+
+fun checkPageSequential(pobi: List<Int>, crong: List<Int>): Boolean {
+    return abs(pobi[0] - pobi[1]) == 1 && abs(crong[0] - crong[1]) == 1
+}
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if (!isCorrectInput(pobi, crong)) {
+        return -1
+    }
+    return 0
 }
