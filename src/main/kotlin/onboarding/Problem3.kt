caa718e29f7146baab598ed2372b850ace7f1bf1
ese111 <feung94@gmail.com>
2022-10-26 19:24:40 +0900

    feat: isThreeSixNine() 함수 구현 3, 6, 9가 있으면 count++ 하는 함수

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,26 @@
 package onboarding
 
+private val threeSixNine = listOf("3", "6", "9")
+
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var count = 0
+    // number 만큼 반복문을 돌린다.
+    repeat(number) { i ->
+        if (isThreeSixNine(i + 1)) {
+            count++
+        }
+    }
+    // 1부터 수를 세며 3이 들어가면 count를 올린다.
+    // count를 반환한다.
+    return 0
 }
+
+private fun isThreeSixNine(number: Int): Boolean {
+    number.toString().forEach {
+        if(it == '3' || it == '6' || it == '9') {
+            return true
+        }
+    }
+    return false
+}
+
