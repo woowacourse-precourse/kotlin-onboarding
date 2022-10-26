bf3ee7ae1f717f9672689d217b8cacb6bb95e882
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-10-26 16:04:47 +0900

    Problem 1에 대한 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,41 @@
 package onboarding
 
+import java.lang.Math.max
+
+fun solution1CheckException(inputList: List<Int>): Boolean {
+    if(inputList.contains(1)||inputList.contains(400)) return true
+    if(inputList[1]-inputList[0]!=1) return true
+    return false
+}
+
+fun solution1Solve(inputList: List<Int>): Int {
+    var addScore = 0
+    var multipleScore = 1
+
+    inputList.forEach { page ->
+        var (tmpAddScore, tmpMultipleScore)=arrayOf(0, 1)
+        page.toString().forEach { eachNum ->
+            tmpAddScore += Character.getNumericValue(eachNum)
+            tmpMultipleScore *= Character.getNumericValue(eachNum)
+        }
+
+        addScore= addScore.coerceAtLeast(tmpAddScore)
+        multipleScore= tmpAddScore.coerceAtLeast(tmpMultipleScore)
+    }
+
+    return addScore.coerceAtLeast(multipleScore)
+}
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if(solution1CheckException(pobi)||solution1CheckException(crong))
+        return -1
+
+    var (pobiScore, crongScore) = arrayOf(0, 0);
+
+    pobiScore = solution1Solve(pobi)
+    crongScore = solution1Solve(crong)
+
+    return if (pobiScore > crongScore) 1
+    else if (pobiScore < crongScore) 2
+    else 0
 }
