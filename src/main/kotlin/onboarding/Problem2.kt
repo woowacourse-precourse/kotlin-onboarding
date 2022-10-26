93aeb884ba27a1414e645723e21c4db02100d09a
ese111 <feung94@gmail.com>
2022-10-26 19:09:20 +0900

    feat: decode() 구현 stack을 이용해서 중복된 문자 제거하는 함수

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,11 +1,42 @@
 package onboarding
 
+import java.util.Stack
+
 fun solution2(cryptogram: String): String {
-    // StringBuilder 생성
+
     val sb = StringBuilder()
     // 스택에 문자를 push한다.
-    // 스택에 문자가 있으면 peek을 해서 push해야할 문자와 같은지 확인한다.
-    // 같으면 pop하고 push하지 않는다.
+    val decode = decode(cryptogram)
+
     // 스택을 StringBulider에 넣고 String으로 형변환하여 반환한다
+    decode.forEach {
+        sb.append(it)
+    }
+
     return sb.toString()
 }
+
+private fun decode(cryptogram: String): Stack<Char> {
+    val stack = Stack<Char>()
+
+    cryptogram.forEach {
+        if(stack.isEmpty()) {
+            stack.push(it)
+        } else {
+            // 스택에 문자가 있으면 peek을 해서 push해야할 문자와 같은지 확인한다.
+            if(stack.containsLast(it)) {
+                stack.pop()
+                return@forEach
+            }
+            // 같으면 pop하고 push하지 않는다.
+            stack.push(it)
+        }
+    }
+
+    return stack
+}
+
+private fun Stack<Char>.containsLast(char: Char): Boolean {
+    return peek() == char
+}
+
