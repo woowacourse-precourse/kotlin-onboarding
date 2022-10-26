bbe6c03018bcf7d8e9d5589d392981e99b27d23a
hansh0101 <hansh0101@naver.com>
2022-10-27 00:09:02 +0900

    [FEATURE] Solve Problem6

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,33 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val result = mutableListOf<String>()
+    val crewInfoMap = mutableMapOf<String, String>()
+    val nicknameSet = mutableSetOf<String>()
+
+    forms.forEach { crewInfo ->
+        crewInfoMap.keys.forEach { nickname ->
+            if (checkDuplicated(crewInfo[1], nickname)) {
+                nicknameSet.add(nickname)
+                nicknameSet.add(crewInfo[1])
+            }
+        }
+        crewInfoMap[crewInfo[1]] = crewInfo[0]
+    }
+
+    result.run {
+        addAll(nicknameSet.mapNotNull { nickname -> crewInfoMap[nickname] })
+        sort()
+    }
+
+    return result.toList()
+}
+
+private fun checkDuplicated(nickname1: String, nickname2: String): Boolean {
+    for (i in 0 until nickname1.length - 1) {
+        if (nickname2.contains(nickname1.substring(i, i + 2))) {
+            return true
+        }
+    }
+    return false
 }
