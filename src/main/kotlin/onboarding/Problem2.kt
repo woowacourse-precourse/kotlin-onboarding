3e7ed64093b17145734ba29d84750ee649876997
username <ieejo716@naver.com>
2022-10-27 22:54:37 +0900

    [FEAT] : 문제2

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,24 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var value = cryptogram.toCharArray()
+    while (true) {
+        val set = mutableSetOf<Int>()
+        for (idx in 0 until value.size - 1) {
+            if (value[idx] == value[idx + 1]) {
+                set.add(idx)
+                set.add(idx + 1)
+            }
+        }
+        if (set.isEmpty()) break
+        for (item in set) {
+            value[item] = '+'
+        }
+        value = String(value).replace("+", "").toCharArray()
+    }
+    return String(value)
+}
+
+fun main() {
+    println(solution2("zyelleyz"))
 }
