7fbbe3a2c1dbb03392e6941f7c2ffeefb5a183aa
RightHennessy <myheaven_b@naver.com>
2022-10-27 17:28:56 +0900

    feat : Add change money to bills

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,6 +1,18 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    val bill:List<Int> = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
-    var result:List<Int>
+    val bills:List<Int> = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    var result = mutableListOf<Int>()
+    var _money:Int = money
+
+    for (bill in bills){
+        result.add(_money/bill)
+        _money %= bill
+    }
+    return result
 }
+
+fun main(){
+    println(solution5(50237))
+}
+
