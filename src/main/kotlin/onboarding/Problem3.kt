e54530c97060f910c5cadeb1f08f3b5418cf41b8
wpwo98 <62274357+wpwo98@users.noreply.github.com>
2022-10-27 22:01:09 +0900

    Update Problem3.kt

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -2,4 +2,14 @@ package onboarding
 
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
+    var cnt: Int = 0
+
+    for(i in 0..number){
+        var j = i.toString().count { c -> c == '3' }
+        j += i.toString().count { c -> c == '6' }
+        j += i.toString().count { c -> c == '9' }
+        cnt += j
+
+    }
+    return cnt
 }
