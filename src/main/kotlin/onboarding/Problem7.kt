09d95ab2ab3f3b2d000a54658a6507dc4d16a34e
ese111 <feung94@gmail.com>
2022-10-27 20:00:19 +0900

    Problem7 feat: 사용자와 연결된 친구 찾기 함수 구현 사용자와 연결된 친구는 +5점 하기 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,21 +1,33 @@
 package onboarding
 
+private lateinit var friendsMap: HashMap<String, Int>
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
     // friend map을 만든다.
-    val friendsMap = getFriendsScore(friends)
+    friendsMap = getFriendsScore(friends)
     // friend에 user가 있는지 확인한다. 있으면 map에서 value를 +5
+    findFriends(user, friends)
     // visitors에 있는 친구를 map에서 +1
     // map을 value값으로 정렬하고 같으면 이름으로 정렬한다.
     // map키를 5까지 출력한다.
     TODO("프로그램 구현")
 }
 
-private fun getFriendsScore(friends: List<List<String>>): Map<String, Int> {
-    val map = mutableMapOf<String, Int>()
+fun findFriends(user: String, friends: List<List<String>>) {
+    friends.forEach { friend ->
+        if(user in friend) {
+            friendsMap[friend[0]] = friendsMap[friend[0]]?.plus(5) ?: 0
+            friendsMap[friend[1]] = friendsMap[friend[0]]?.plus(5) ?: 0
+        }
+    }
+}
+
+private fun getFriendsScore(friends: List<List<String>>): HashMap<String, Int> {
+    val map = HashMap<String, Int>()
     friends.forEach { friend ->
         friend.forEach { name ->
             map.put(name, 0)
@@ -23,3 +35,4 @@ private fun getFriendsScore(friends: List<List<String>>): Map<String, Int> {
     }
     return map
 }
+
