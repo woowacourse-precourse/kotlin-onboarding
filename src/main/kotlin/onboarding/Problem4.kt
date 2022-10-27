77acbef93a14baa81d21c66c02b1aa390f51a8f3
Kim Jaeyong <jy3479@naver.com>
2022-10-27 13:30:45 +0900

    soulution4 implement

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,9 @@
 package onboarding
 
-fun solution4(word: String): String {
-    TODO("프로그램 구현")
-}
+fun solution4(word: String): String = word.map {
+    when(it) {
+        in 'a'..'z' -> ('z'.code + 'a'.code - it.code).toChar().toString()
+        in 'A'..'Z' -> ('Z'.code + 'A'.code - it.code).toChar().toString()
+        else -> it.toString()
+    }
+}.reduce { acc, s -> acc + s }
