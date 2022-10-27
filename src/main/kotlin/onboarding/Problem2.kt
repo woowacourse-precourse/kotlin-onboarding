a4eb12251115d2b37f161854cdf8dbf74d698a60
dltkd1395 <dltkd13956@naver.com>
2022-10-27 21:44:45 +0900

    feat : [PROBLEM2] 암호문 해독 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -3,3 +3,15 @@ package onboarding
 fun solution2(cryptogram: String): String {
     TODO("프로그램 구현")
 }
+
+fun decodeCryptogram(index : Int, cryptogram: String, stack : Stack<Char>) : Stack<Char> {
+    if (index == cryptogram.length) {
+        return checkDuplicatedText(stack)
+    }
+    val char = cryptogram[index]
+
+    if (char != stack.last()) {
+        checkDuplicatedText(stack)
+    }
+    return decodeCryptogram(index + 1, cryptogram, stack.also { stack.push(char) })
+}
