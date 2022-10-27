2ab2df83b17585a7fbf1c18ec8d3e8af6e790f0c
wpwo98 <62274357+wpwo98@users.noreply.github.com>
2022-10-27 22:04:12 +0900

    Updatae Problem1.kt

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -2,9 +2,9 @@ package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
+    // pobi 수 구하기
     var pobi_mul: Int
     var pobi_num: Array<Int> = arrayOf(0,0,0,0)
-
     pobi_num[0] = ( (pobi[0] / 100) + (pobi[0]/10)%10 + (pobi[0] % 10) )
     pobi_num[1] = ( (pobi[1] / 100) + (pobi[1]/10)%10 + (pobi[1] % 10) )
     if (pobi[0]/100 == 0){
@@ -19,12 +19,12 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
         pobi_mul = (pobi[1] / 100) * (pobi[1]/10)%10 * (pobi[1] % 10)
     }
     pobi_num[3] = pobi_mul
+    // pobi 가장 큰 수
     var result_pobi = pobi_num.maxOrNull()
 
-
+    // crong 수 구하기
     var crong_mul: Int
     var crong_num: Array<Int> = arrayOf(0,0,0,0)
-
     crong_num[0] = ( (crong[0] / 100) + (crong[0]/10)%10 + (crong[0] % 10) )
     crong_num[1] = ( (crong[1] / 100) + (crong[1]/10)%10 + (crong[1] % 10) )
     if (crong[0]/100 == 0){
@@ -39,9 +39,11 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
         crong_mul = (crong[1] / 100) * (crong[1]/10)%10 * (crong[1] % 10)
     }
     crong_num[3] = crong_mul
+    // crong 가장 큰 수
     var result_crong = crong_num.maxOrNull()
-    var result : Int
 
+    // 결과 계산
+    var result : Int
     if(result_pobi!! > result_crong!!) result = 1
     else if(result_pobi == result_crong) result = 0
     else if(result_pobi < result_crong) result = 2
