c0cfd69aaabb595a62fe70c6b5212a0ce895f34f
Jaeheon Jeong <drdd1120@naver.com>
2022-10-28 02:03:57 +0900

    p5 최종코드

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -6,9 +6,12 @@ package onboarding
 */
 
 fun solution5(money: Int): List<Int> {
+
+    // 1. 화폐 단위 배열 생성, 단위 별로 개수를 담을 배열 생성
     val unit = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
     val cost = Array(9){ 0 }
 
+    // 2. while 문을 돌려 화폐 단위 배열 순서대로 계산
     var n = money
     var idx = 0
     while(n > 0){
