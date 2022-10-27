9037da3750df2b0e678ebb5506c859703161dd27
RightHennessy <myheaven_b@naver.com>
2022-10-27 20:37:24 +0900

    feat : Add result in result list

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,13 +1,17 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    var result:List<String> = listOf()
-    var check:Int
+    var result = mutableListOf<String>()
+    var check:Boolean
 
     for (i in 0..forms.size-2){
-        for (j in i+1..forms.size-1)
+        for (j in i+1..forms.size-1) {
             check = checkOverlap(forms[i][1], forms[j][1])
+            if (check && !(forms[i][0] in result))
+                result.add(forms[i][0])
+        }
     }
 
     return forms[1]
 }
+
