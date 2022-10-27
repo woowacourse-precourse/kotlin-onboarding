57146d35faf3febf8a3e90c45211ea200cdf5d0d
dltkd1395 <dltkd13956@naver.com>
2022-10-27 21:47:20 +0900

    feat : [PROBLEM2]  문자열 중복 여부 확인 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -15,3 +15,11 @@ fun decodeCryptogram(index : Int, cryptogram: String, stack : Stack<Char>) : Sta
     }
     return decodeCryptogram(index + 1, cryptogram, stack.also { stack.push(char) })
 }
+
+fun checkDuplicatedText(stack : Stack<Char>) : Stack<Char> {
+    val lastItem = stack.pop()
+    if (stack.isNotEmpty() && lastItem == stack.last()) {
+        return deleteDuplicatedText(stack, lastItem)
+    }
+    return stack.also { stack.push(lastItem) }
+}
