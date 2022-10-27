7b7133729676b485beb7896e154189e5d7ba3897
CHOSEONGROK <csr8367@hanmail.net>
2022-10-27 16:28:57 +0900

    Add solution4

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    return buildString {
+        for (ch in word) append(ch.greenFrog())
+    }
+}
+
+private fun Char.greenFrog(): Char {
+    return if (isUpperCase()) {
+        ('Z'.code - (code - 'A'.code)).toChar()
+    } else if (isLowerCase()) {
+        ('z'.code - (code - 'a'.code)).toChar()
+    } else {
+        this
+    }
 }
