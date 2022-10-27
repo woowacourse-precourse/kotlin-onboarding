a4c5b4791f808a756c233719d88547fe6853be8f
ese111 <feung94@gmail.com>
2022-10-27 19:43:39 +0900

    Problem7 feat: 친구 점수 판 생성 함수 구현 친구 점수판을 만드는 함수 생성

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -6,7 +6,7 @@ fun solution7(
     visitors: List<String>
 ): List<String> {
     // friend map을 만든다.
-    val friendsMap = mutableMapOf<String, Int>()
+    val friendsMap = getFriendsScore(friends)
     // friend에 user가 있는지 확인한다. 있으면 map에서 value를 +5
     // visitors에 있는 친구를 map에서 +1
     // map을 value값으로 정렬하고 같으면 이름으로 정렬한다.
@@ -14,3 +14,12 @@ fun solution7(
     TODO("프로그램 구현")
 }
 
+private fun getFriendsScore(friends: List<List<String>>): Map<String, Int> {
+    val map = mutableMapOf<String, Int>()
+    friends.forEach { friend ->
+        friend.forEach { name ->
+            map.put(name, 0)
+        }
+    }
+    return map
+}
