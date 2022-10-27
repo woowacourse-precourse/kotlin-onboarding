7f22bf5da6333401f663e0fd5c7f0a452cdec34b
ese111 <feung94@gmail.com>
2022-10-27 20:15:49 +0900

    Problem7 feat: 방문자 점수 플러스하는 함수 구현 방문자 목록을 돌면서 점수판에 +1을 해준다

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -12,16 +12,25 @@ fun solution7(
     // friend에 user가 있는지 확인한다. 있으면 map에서 value를 +5
     findFriends(user, friends)
     // visitors에 있는 친구를 map에서 +1
+    visitFriends(visitors)
     // map을 value값으로 정렬하고 같으면 이름으로 정렬한다.
     // map키를 5까지 출력한다.
     TODO("프로그램 구현")
 }
 
+fun visitFriends(visitors: List<String>) {
+    visitors.forEach {
+        friendsMap[it] = friendsMap[it]?.plus(1) ?: 0
+    }
+}
+
 fun findFriends(user: String, friends: List<List<String>>) {
     friends.forEach { friend ->
-        if(user in friend) {
-            friendsMap[friend[0]] = friendsMap[friend[0]]?.plus(5) ?: 0
+        if(user == friend[0]) {
             friendsMap[friend[1]] = friendsMap[friend[0]]?.plus(5) ?: 0
+        }
+        if(user == friend[1]) {
+            friendsMap[friend[0]] = friendsMap[friend[0]]?.plus(5) ?: 0
         }
     }
 }
@@ -35,4 +44,16 @@ private fun getFriendsScore(friends: List<List<String>>): HashMap<String, Int> {
     }
     return map
 }
-
+fun main() {
+    val user = "mrko"
+    val friends = listOf(
+        listOf("donut", "andole"),
+        listOf("donut", "jun"),
+        listOf("donut", "mrko"),
+        listOf("shakevan", "andole"),
+        listOf("shakevan", "jun"),
+        listOf("shakevan", "mrko")
+    )
+    val visitors = listOf("bedi", "bedi", "donut", "bedi", "shakevan")
+    solution7(user, friends, visitors)
+}
