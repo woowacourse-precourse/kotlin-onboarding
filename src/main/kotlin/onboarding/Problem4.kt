dc6de7c33a47617b3d3f0e83ac796f70a6d1b9a9
dltkd1395 <dltkd13956@naver.com>
2022-10-27 23:19:10 +0900

    feat : [PROBLEM4] 워드 변환 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,3 +3,13 @@ package onboarding
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
+
+fun convertWord(index : Int, word: String, result : String) : String {
+    if (index == word.length) {
+        return result
+    }
+    if (!word[index].isLetter()) {
+        return convertWord(index + 1, word, result + word[index])
+    }
+    return convertWord(index + 1, word, result + getReversedCharacter(word[index]))
+}
