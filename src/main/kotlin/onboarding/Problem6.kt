afc7dcedc968d180322bc852456e8ec0864a3878
RightHennessy <myheaven_b@naver.com>
2022-10-27 20:26:39 +0900

    feat : Add check overlap in solution

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,13 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    var result:List<String> = listOf()
+    var check:Int
+
+    for (i in 0..forms.size-2){
+        for (j in i+1..forms.size-1)
+            check = checkOverlap(forms[i][1], forms[j][1])
+    }
+
+    return forms[1]
 }
