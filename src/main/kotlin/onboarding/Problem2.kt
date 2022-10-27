bf087ee306d22ea774a804b6c8ad1c82e985a603
sojk401 <sojk401@gmail.com>
2022-10-27 23:07:50 +0900

    Problem2. 기능 목록 작성

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -2,4 +2,11 @@ package onboarding
 
 fun solution2(cryptogram: String): String {
     TODO("프로그램 구현")
+    /*
+    [기능 목록]
+    1. 연속으로 중복된 문자 제거
+        a) 중복이 시작된 문자와 중복된 마지막 문자의 인덱스 찾기
+        b) 중복된 인덱스 범위 제거
+    2. 연속으로 중복된 문자가 없을 때까지 반복
+     */
 }
