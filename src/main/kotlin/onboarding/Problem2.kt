96a7584fd5de5efc9aa87f3c3f1127c42797e760
tmdgh1592 <tmdgh1592@naver.com>
2022-10-27 10:33:57 +0900

    feat: Stack<Char> 자료형을 String 으로 변환하는 메서드 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+import java.util.*
+
 fun solution2(cryptogram: String): String {
     TODO("프로그램 구현")
+}
+
+private fun Stack<Char>.convertString(): String {
+    val emptyString = ""
+
+    return this.fold(emptyString) { string, ch ->
+        string.plus(ch)
+    }
 }
