082ae5a245d8614981ba4bd6ca592e6c0bbe6a7f
dltkd1395 <dltkd13956@naver.com>
2022-10-27 22:08:31 +0900

    feat : [PROBLEM3] 369게임 시작 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -3,3 +3,10 @@ package onboarding
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
+
+fun startGame(start : Int, end: Int, count: Int) : Int {
+    if (start == end + 1) {
+        return count
+    }
+    return startGame(start + 1, end, clapCounter(start, count))
+}
