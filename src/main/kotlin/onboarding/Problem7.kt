615a4a1e3f3f3c6e2489734d461de87440239e05
RightHennessy <myheaven_b@naver.com>
2022-10-27 22:20:38 +0900

    feat : Add function checkNonFriends in given list friends

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -6,10 +6,11 @@ fun solution7(
     visitors: List<String>
 ): List<String> {
     var friendList = listOf<String>()
-    var non_friendList = mutableMapOf<Int, String>()
+    var non_friendList = mapOf<String, Int>()
 
     friendList = checkFriends(user, friends)
-    return friendList
+    non_friendList = checkNonFriends(user, friends, friendList, visitors)
+
 }
 
 fun checkFriends(user: String, friends: List<List<String>>) : List<String> {
@@ -24,5 +25,31 @@ fun checkFriends(user: String, friends: List<List<String>>) : List<String> {
     return list
 }
 
+fun checkNonFriends(user: String,
+                    friends: List<List<String>>,
+                    friendList: List<String>,
+                    visitors: List<String>
+): Map<String, Int>{
+    var list = mutableMapOf<String, Int>()
 
+    for (friend in friends){
+        if (friend[0]==user || friend[1]==user)
+            continue
+        if (friend[0] in friendList && friend[1] in friendList)
+            continue
+        if (friend[0] in friendList){
+            if (list.containsKey(friend[1]))
+                list.put(friend[1], list.get(friend[1])!! + 10)
+            else
+                list.put(friend[1], 10)
+        }
+        if (friend[1] in friendList){
+            if (list.containsKey(friend[0]))
+                list.put(friend[0], list.get(friend[0])!! + 10)
+            else
+                list.put(friend[0], 10)
+        }
+    }
+    return list
+}
 
