6e702240717399c7c739b036b0c278b1c0786d42
EmilyCh0 <seoyeon3089@naver.com>
2022-10-27 23:13:47 +0900

    feat(problem2): 중복 문자 제거 기능 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,19 @@
 package onboarding
 
+fun eraseRepeatedChar(cryptogram: String): String {
+    var res = ""
+    val len = cryptogram.length
+    if (len < 2) return cryptogram
+    if (cryptogram[0] != cryptogram[1]) res += cryptogram[0]
+    for (i in 1 until len - 1) {
+        if (cryptogram[i] == cryptogram[i + 1]) continue
+        if (cryptogram[i] == cryptogram[i - 1]) continue
+        res += cryptogram[i]
+    }
+    if (cryptogram[len - 2] != cryptogram[len - 1]) res += cryptogram[len - 1]
+    return res
+}
+
 fun solution2(cryptogram: String): String {
     TODO("프로그램 구현")
 }
