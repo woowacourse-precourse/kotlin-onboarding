733ac78b1993cd762fdd01149367fddee818b60b
Jaeheon Jeong <drdd1120@naver.com>
2022-10-28 00:41:17 +0900

    p1 요구사항 작성

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,12 @@
 package onboarding
 
+/*
+1. 예외사항 체크
+2. 예외 사항 만족 시 왼쪽, 오른쪽 페이지 확인 후 최대값 만듦
+3. 예외 사항 만족 실패 시 return -1
+4. 2번에서 만든 pobi와 crong의 최대값을 비교
+ */
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
