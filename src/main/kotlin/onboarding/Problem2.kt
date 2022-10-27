bb557d56c6d1608b005b7da07fed75823a585b03
yeongjin <dudwls901@naver.com>
2022-10-27 10:36:15 +0900

    [Feat] 해독 로직 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -5,21 +5,26 @@ package onboarding
 * 해독 로직 구현
 * */
 
-fun String.removeDuplication(): String{
+fun String.removeDuplication(): String {
     val sb = StringBuilder(this)
     var idx = 0
-    while(idx<sb.length){
+    while (idx < sb.length) {
         var len = 1
-        while(idx+len < sb.length && sb[idx] == sb[idx+len]){
+        while (idx + len < sb.length && sb[idx] == sb[idx + len]) {
             len++
         }
-        if(len>1) sb.deleteRange(idx,idx+len)
-        else  idx++
+        if (len > 1) sb.deleteRange(idx, idx + len)
+        else idx++
     }
     return sb.toString()
 }
 
 fun solution2(cryptogram: String): String {
-    cryptogram.removeDuplication()
-    return ""
+    var crypto = cryptogram
+    while (crypto.isNotBlank()) {
+        val temp = crypto.removeDuplication()
+        if (temp.length == crypto.length) break
+        crypto = temp
+    }
+    return crypto
 }
