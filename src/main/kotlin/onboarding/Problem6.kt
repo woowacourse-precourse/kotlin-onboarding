a4c093fcb2738b338c5d19ac4029c7d1725b7223
RightHennessy <myheaven_b@naver.com>
2022-10-27 20:53:00 +0900

    feat : Add function checkOverlap

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,34 +1,31 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    var result = mutableListOf<String>()
+    var result = mutableSetOf<String>()
     var check:Boolean
 
     for (i in 0..forms.size-2){
         for (j in i+1..forms.size-1) {
             check = checkOverlap(forms[i][1], forms[j][1])
             if (check) {
-                if (!(forms[i][0] in result))
+                if (forms[i][0] in result == false) {
                     result.add(forms[i][0])
-                if (!(forms[j][0] in result))
+                }
+                if (forms[j][0] in result == false)
                     result.add(forms[j][0])
             }
         }
     }
 
-    return forms[1]
+    return result.toList().sorted()
 }
 
 fun checkOverlap(name1:String, name2:String) : Boolean{
-
-}
-
-fun main(){
-    val forms = listOf(
-            listOf("jm@email.com", "제이엠"),
-            listOf("jason@email.com", "제이슨"),
-            listOf("woniee@email.com", "워니"),
-            listOf("mj@email.com", "엠제이"),
-            listOf("nowm@email.com", "이제엠"))
-    solution6(forms)
+    for (i in 0..name1.length-2){
+        for (j in 0..name2.length-2){
+            if (name1[i]==name2[j] && name1[i+1]==name2[j+1])
+                return true
+        }
+    }
+    return false
 }
