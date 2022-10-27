c446e708662c8e890127a7d101d11eb1320d519d
Jaeheon Jeong <drdd1120@naver.com>
2022-10-28 00:51:44 +0900

    p1 4.2번에서 만든 pobi와 crong의 최대값을 비교

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -13,11 +13,13 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     check(crong[0], crong[1])
 
     // 2. 예외 사항 만족 시 왼쪽, 오른쪽 페이지 확인 후 최대값 만듦
-    return if(check(pobi[0], pobi[1]) && check(crong[0], crong[1])){
+    return if (check(pobi[0], pobi[1]) && check(crong[0], crong[1])) {
         var pobiRes = make(pobi[0]).coerceAtLeast(make(pobi[1]))
         var crongRes = make(crong[0]).coerceAtLeast(make(crong[1]))
 
-    //3. 예외 사항 만족 실패 시 return -1
+        // 4. 2번에서 만든 pobi와 crong의 최대값을 비교
+        compare(pobiRes, crongRes)
+        //3. 예외 사항 만족 실패 시 return -1
     } else {
         -1
     }
@@ -33,15 +35,15 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 // 뒷페이지가 홀수인 경우
 // 1, 2 페이지를 펼치거나
 // 399 400 페이지를 펼친 경우
-fun check(first : Int, second : Int) : Boolean {
-    if(first % 2 == 0 || second % 2 == 1 || first + 1 != second || first <= 1 || second >= 400){
+fun check(first: Int, second: Int): Boolean {
+    if (first % 2 == 0 || second % 2 == 1 || first + 1 != second || first <= 1 || second >= 400) {
         return false
     }
     return true
 }
 
 // 2. 예외 사항 만족 시 왼쪽, 오른쪽 페이지 확인 후 최대값 만듦
-fun make(pages : Int) : Int {
+fun make(pages: Int): Int {
     val page = pages.toString()
     var token = page.chunked(1)
     var sum = 0
@@ -55,4 +57,15 @@ fun make(pages : Int) : Int {
 
     tot = sum.coerceAtLeast(mul)
     return tot
+}
+
+// 4. 2번에서 만든 pobi와 crong의 최대값을 비교
+fun compare(pobiRes: Int, crongRes: Int): Int {
+    return if (pobiRes > crongRes) {
+        1
+    } else if (pobiRes < crongRes) {
+        2
+    } else {
+        0
+    }
 }
