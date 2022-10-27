c10a3551269bd67a784c63f26ec4ccab13ad0470
username <ieejo716@naver.com>
2022-10-28 00:13:53 +0900

    [FEAT] : 문제5

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,23 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val moneyList = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val result = mutableListOf(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    var value = money
+    for (idx in moneyList.indices) {
+        while (true) {
+            if (value - moneyList[idx] >= 0) {
+                value -= moneyList[idx]
+                ++result[idx]
+                continue
+            }
+            break
+        }
+    }
+    return result
+}
+
+fun main() {
+    //오마넌 , 마넌, 오처넌, 처넌, 오백원, 백원, 오십원, 십원
+    println(solution5(15000))
 }
