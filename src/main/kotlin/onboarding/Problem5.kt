e032919429d68bbde592bf6efc5253cd0341c184
Kim Jaeyong <jy3479@naver.com>
2022-10-27 15:12:03 +0900

    solution5 implement

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,8 @@
 package onboarding
 
-fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+fun solution5(money: Int): List<Int> = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1).mapIndexed { index, i ->
+    if (index == 0) money / i
+    else if (index == 8) (money % 10)
+    else if (index % 2 == 0) (money % (i * 2)) / i
+    else (money % (i * 5)) / i
 }
