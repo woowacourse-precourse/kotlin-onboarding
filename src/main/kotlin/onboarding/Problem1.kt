32725bc56767c426893e1bc270b71b8a31952009
CHOSEONGROK <csr8367@hanmail.net>
2022-10-27 23:02:00 +0900

    Style: 코드 스타일 수정

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -6,32 +6,28 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     if (!checkValidation(pobi, crong))
         return -1
 
-    val pobiPoint = pobi.getPoints()
-    val crongPoint = crong.getPoints()
-
-    return if (pobiPoint > crongPoint) 1
-    else if (pobiPoint < crongPoint) 2
-    else 0
+    return compare(getScore(pobi), getScore(crong))
 }
 
 private fun checkValidation(vararg pages: List<Int>): Boolean {
     return pages.all { page ->
-        (page.size == 2) &&
-                (page[0] in 1..400) &&
-                (page[1] in 1..400) &&
-                (page[0] % 2 == 1) &&
-                (page[1] % 2 == 0) &&
-                (page[1] - page[0] == 1)
+        (page.size == 2)
+                && (page[0] in 1..400)
+                && (page[1] in 1..400)
+                && (page[0] % 2 == 1)
+                && (page[1] % 2 == 0)
+                && (page[1] - page[0] == 1)
     }
 }
 
-private fun List<Int>.getPoints(): Int {
-    return maxOf {
-        val page = it.toString()
+private fun getScore(pages: List<Int>): Int {
+    return pages.maxOf { page ->
 
-        max(
-            page.fold(0) { acc, n -> acc + n.digitToInt() },
-            page.fold(1) { acc, n -> acc * n.digitToInt() },
-        )
+        max(page.toString().fold(0) { acc, n -> acc + n.digitToInt() },
+            page.toString().fold(1) { acc, n -> acc * n.digitToInt() })
     }
+}
+
+private fun compare(score1: Int, score2: Int): Int {
+    return if (score1 > score2) 1 else if (score1 < score2) 2 else 0
 }
