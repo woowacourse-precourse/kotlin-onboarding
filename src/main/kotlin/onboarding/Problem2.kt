b0fc344c46b182d7e63bbdccf3dfec16eda80f5b
inseonyun <yuninseon@naver.com>
2022-10-27 15:18:33 +0900

    [Problem2] 연속되는 반복문자 제거 함수 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -11,6 +11,31 @@ fun checkContinuousString(cryptogram: String): Boolean {
     return false
 }
 
+fun getRemoveContinuousString(cryptogram: String): String {
+    var tmp = cryptogram
+
+    var idx = 0
+    while(idx < tmp.length) {
+        var cnt = 0
+        for(j in idx + 1 until tmp.length) {
+            if(tmp[idx] == tmp[j]) {
+                cnt+=1
+            }else {
+                if(cnt != 0) {
+                    val substr = tmp.substring(j - cnt - 1, j)
+                    tmp = tmp.replace(substr, "")
+                    idx = j - cnt - 1
+                }
+                break
+            }
+        }
+        if(cnt == 0)
+            idx += 1
+    }
+    return tmp
+}
+
+
 fun solution2(cryptogram: String): String {
     var result = cryptogram
 
