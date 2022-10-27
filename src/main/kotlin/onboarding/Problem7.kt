41bd2089a703519e2347d45b3f7a9c8a575d9891
RightHennessy <myheaven_b@naver.com>
2022-10-27 21:49:05 +0900

    feat : Add function checkFriends

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,6 +5,24 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    var friendList = mutableListOf<String>()
+    var friendList = listOf<String>()
     var non_friendList = mutableMapOf<Int, String>()
+
+    friendList = checkFriends(user, friends)
+    return friendList
 }
+
+fun checkFriends(user: String, friends: List<List<String>>) : List<String> {
+    var list = mutableListOf<String>()
+
+    for (friend in friends){
+        if (friend[0] == user && !(friend[1] in list))
+            list.add(friend[1])
+        if (friend[1] == user && !(friend[0] in list))
+            list.add(friend[0])
+    }
+    return list
+}
+
+
+
