8e270d7f3dcc100b06ff84c3d8fc46a8723e799f
Jaeheon Jeong <drdd1120@naver.com>
2022-10-28 01:48:34 +0900

    p4 요구사항 작성

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,11 @@
 package onboarding
 
+/*
+1. 소문자와 대문자에 대해 반대로 변환하는 map 작성
+2. 문자열을 문자로 쪼갬
+3. for문을 순회하며 각 알파벳별로 map에서 찾아 변환하고 새로운 문자열 만들기
+*/
+
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
