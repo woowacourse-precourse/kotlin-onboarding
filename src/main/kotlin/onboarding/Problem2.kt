92998b7c2398d5f0f45fc896a2c914fe3bde33d6
Joonhoo <mnc5@naver.com>
2022-10-27 22:32:53 +0900

    feat(Problem 2): 암호를 요구사항에 맞는 해독 기능 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,36 @@
 package onboarding
 
+import java.util.Stack
+
+/**
+ * 기능 요구사항
+ * - 연속하는 중복 문자들을 삭제
+ * 풀이 방법
+ * - 연속되는 짝을 찾기위해 Stack 의 마지막 문자와 현재 들어올 문자를 비교
+ * - 1. 같으면 pop()
+ * - 2. 다르면 push()
+ * 예외사항 생각
+ * - 1. Stack 이 비어있을때 exception 확인
+ */
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    // 암호해독
+    return decodeCryptogram(cryptogram)
 }
+
+private fun decodeCryptogram(cryptogram: String): String {
+    val stack = Stack<Char>()
+    for (char in cryptogram) {
+        if (stack.isNotEmpty()) {
+            if (stack.peek() == char) {
+                stack.pop()
+            } else {
+                stack.push(char)
+            }
+        } else {
+            stack.push(char)
+        }
+    }
+    // Stack 의 내부 그대로 문자열로 리턴
+    return stack.joinToString("") { it.toString() }
+}
+
