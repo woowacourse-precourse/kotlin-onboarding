155202b4d2d6ebdd307f8092de9bfab4361c217f
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-10-27 18:59:11 +0900

    포비의 페이지 최댓값 (1-2,1-3)

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,58 @@
 package onboarding
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+val pobi_win = 1
+val crong_win = 2
+val draw = 0
+val exception_point = -1
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
+
+  var pobi_max_sum = 0
+  var pobi_max_mul = 1
+  var pobi_sum = 0
+  var pobi_mul = 1
+  var pobi_max = 0
+
+  for(i in 0 until 2){
+     pobi_sum = (pobi[i] / 100) + (pobi[i] % 100 / 10) + (pobi[i] % 100 % 10)
+     if(pobi_sum > pobi_max_sum){
+        pobi_max_sum = pobi_sum
+     }
+
+     if( pobi[i] >= 100){
+        pobi_mul = (pobi[i] / 100) * (pobi[i] % 100 / 10) * (pobi[i] % 100 % 10)
+     }
+     if( pobi[i] < 100){
+        pobi_mul = (pobi[i] / 10) * (pobi[i] % 10)
+     }
+
+     if(pobi_mul > pobi_max_mul){
+        pobi_max_mul = pobi_mul
+     }
+  }
+
+   if(pobi_max_mul > pobi_max_sum){
+      pobi_max = pobi_max_mul
+   }
+   if (pobi_max_sum >= pobi_max_mul){
+      pobi_max = pobi_max_sum
+   }
+
+
+
+
+
+
+
+
+
+  return exception_point
 }
+
+
+
--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,9 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+
+
+
+
+    return "1"
 }
