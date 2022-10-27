bdb87776adcf5ef064c95c1bc14f059de5b85342
EmilyCh0 <seoyeon3089@naver.com>
2022-10-27 20:43:15 +0900

    feat(problem1): 예외사항인지 확인하는 기능 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,10 @@
 package onboarding
 
+fun isException(pages: List<Int>): Boolean{
+    if(pages[0]+1!=pages[1]) return true
+    return false
+}
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
