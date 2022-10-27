20dff2352aff5bfe74479584bb311173423a5011
dltkd1395 <dltkd13956@naver.com>
2022-10-27 12:21:10 +0900

    feat : [PROBLEM1] 페이지 각 자리 수 더하기 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -3,3 +3,10 @@ package onboarding
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
+
+fun addPageNumber(pageNumber : Int) : Int {
+    if (pageNumber < 10) {
+        return pageNumber
+    }
+    return (pageNumber % 10) + addPageNumber(pageNumber / 10)
+}
