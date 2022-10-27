46a5f3bd9a649b80514e45ec1091f288e0bb7c2b
ese111 <feung94@gmail.com>
2022-10-27 19:37:38 +0900

    Problem7 feat: 친구 점수 판 생성 map을 이용한 친구의 점수판 생성

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,12 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
+    // friend map을 만든다.
+    val friendsMap = mutableMapOf<String, Int>()
+    // friend에 user가 있는지 확인한다. 있으면 map에서 value를 +5
+    // visitors에 있는 친구를 map에서 +1
+    // map을 value값으로 정렬하고 같으면 이름으로 정렬한다.
+    // map키를 5까지 출력한다.
     TODO("프로그램 구현")
 }
+
