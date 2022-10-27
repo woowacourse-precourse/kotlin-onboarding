75c19a75fe0d10fea13a6b8b4eb0749ca194eb0d
sojk401 <sojk401@gmail.com>
2022-10-28 01:41:29 +0900

    Problem5. 1. (액수,변환되는 개수)를 쌍으로 갖고, 순서를 가지는 map 생성 및 초기화

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,7 +1,6 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
     /*
     [기능 목록]
     1. 오만 원권부터 일원 동전까지 각 몇 개로 반환되는지 저장
@@ -9,4 +8,10 @@ fun solution5(money: Int): List<Int> {
         a) 몫은 변환되는 개수
         b) 나머지는 다시 변환활 money
      */
+    val amount = listOf<Int>(50000,10000,5000,1000,500,100,50,10,1)
+    var map = LinkedHashMap<Int,Int>()
+
+    for (i in amount){
+        map.put(i, 0)
+    }
 }
