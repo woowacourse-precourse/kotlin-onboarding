57392b36204e9719db1bf1ce61abbc709c91d378
wonseok <dnjstjr245@gmail.com>
2022-10-27 16:59:24 +0900

    :sparkles: Problem5 solved

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,17 @@
 package onboarding
 
+/**
+ * @author onseok
+ */
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val coins = intArrayOf(
+        50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1
+    )
+    var moneyRemained = money
+    val result = IntArray(coins.size)
+    for (i in coins.indices) {
+        result[i] = moneyRemained / coins[i]
+        moneyRemained %= coins[i]
+    }
+    return result.toList()
 }
