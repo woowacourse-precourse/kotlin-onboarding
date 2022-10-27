d6058402030abb1e32987a2d6a368a6b4dbdbab5
leah <leah@woowahan.com>
2022-10-28 01:44:12 +0900

    feat: setup precourse onboarding project

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,47 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
-}
+    if(pobi[1] - pobi[0] != 1)
+        return -1;
+    if(crong[1] - crong[0] != 1)
+        return -1;
+    if(pobi[0]+pobi[1] <= pobi[0] * pobi[1]){
+        if(crong[0]+crong[1]<crong[0]*crong[1]){
+            if(crong[0]*crong[1] < pobi[0]*pobi[1])
+                return 1;
+            else if(crong[0]*crong[1] > pobi[0]*pobi)
+                return 2;
+            else
+                return 0;
+        }
+        else{
+                if(crong[0]+crong[1] < pobi[0]*pobi[1])
+                    return 1;
+                else if(crong[0]+crong[1] > pobi[0]*pobi[1] )
+                    return 2;
+                else
+                    return 0;
+            }
+
+        }
+    else{
+        if(crong[0]+crong[1]<crong[0]*crong[1]){
+            if(crong[0]*crong[1] < pobi[0]+pobi[1])
+                return 1;
+            else if(crong[0]*crong[1] > pobi[0]+pobi)
+                return 2;
+            else
+                return 0;
+            }
+        else{
+            if(crong[0]+crong[1] < pobi[0]+pobi[1])
+                return 1;
+            else if(crong[0]+crong[1] > pobi[0]+pobi[1] )
+                return 2;
+            else
+                return 0;
+            }
+
+        }
+
+    }
