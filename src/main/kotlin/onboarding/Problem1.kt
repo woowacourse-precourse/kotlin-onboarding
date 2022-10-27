69ee497810c926ea2385c0ab132f117a21f69ff9
Min-Jae-Bae <qoalswo123@icloud.com>
2022-10-27 11:24:35 +0900

    1번 문제 테스트: [1, 2]성공, [3]실패

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,65 @@
 package onboarding
 
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
+    // TODO: 예외 상황 조건 만들기
+    if (pobi[1] - pobi[0] != 1 && crong[1] - crong[0] != 1) return -1
+
+
+
+    val pobiPagePointList = listOf(
+        getSumPagePlaceValue(pobi[0]),
+        getSumPagePlaceValue(pobi[1]),
+        getMultiplePagePlaceValue(pobi[0]),
+        getMultiplePagePlaceValue(pobi[1])
+    )
+
+    val ascendPobiPagePointList = pobiPagePointList.sorted()
+    val maxPobiPagePoint = ascendPobiPagePointList[3]
+
+
+    val crongPagePointList = listOf(
+        getSumPagePlaceValue(crong[0]),
+        getSumPagePlaceValue(crong[1]),
+        getMultiplePagePlaceValue(crong[0]),
+        getMultiplePagePlaceValue(crong[1])
+    )
+    val ascendCrongPagePointList = crongPagePointList.sorted()
+    val maxCrongPagePoint = ascendCrongPagePointList[3]
+
+
+    if (maxPobiPagePoint == maxCrongPagePoint) return 0
+    if (maxPobiPagePoint > maxCrongPagePoint) return 1
+    if (maxPobiPagePoint < maxCrongPagePoint) return 2
+
+
+    return -1
 }
+
+
+fun getSumPagePlaceValue(inputPage: Int): Int {
+    var pagePoint = 1
+    var input = inputPage
+    while (input >= 1) {
+        pagePoint *= input % 10
+        input /= 10
+    }
+    return pagePoint
+}
+
+fun getMultiplePagePlaceValue(inputPage: Int): Int {
+    var pagePoint = 1
+    var input = inputPage
+    while (input >= 1) {
+        pagePoint *= input % 10
+        input /= 10
+    }
+    return pagePoint
+}
+
+
+
+
+
+
