d5aeb1f72a6930a2383b60e58ca33c6abb41139f
tmdgh1592 <tmdgh1592@naver.com>
2022-10-27 10:39:26 +0900

    feat: 연속하는 중복 문자를 제거하는 메서드 구현 removeDuplicate() Extension 메서드 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -6,6 +6,18 @@ fun solution2(cryptogram: String): String {
     TODO("프로그램 구현")
 }
 
+private fun String.removeDuplicate(): String {
+    val stackIndex = 1
+    val notDuplicatedStack = Stack<Char>().apply {
+        this.push(this[0])
+    }
+
+    this.substring(stackIndex).forEach { ch ->
+        notDuplicatedStack.filterDuplicatedLastOrAppendWith(ch)
+    }
+    return notDuplicatedStack.convertString()
+}
+
 private fun Stack<Char>.filterDuplicatedLastOrAppendWith(ch: Char) {
     val top = peek()
 
