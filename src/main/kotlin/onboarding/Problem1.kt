b265a57bffb40c26ccee54513f0df4addc9774d0
Joonhoo <mnc5@naver.com>
2022-10-27 21:12:04 +0900

    feat(Problem 1): 문제의 요구사항 및 예외사항 기능 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,19 @@
 package onboarding
 
+/**
+ * 기능 요구 사항
+ * 1. 예외사항
+ *      - 시작혹은 마지막으로 펼치지 않는다.
+ *      - 왼쪽은 홀수, 오른쪽은 짝수어야한다.
+ *      - 페이지 이므로 연속적이어야 한다.
+ * 2. 가장 큰 수 구하기
+ * 3. 점수를 비교하여 게임의 승자 결정
+ */
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
+}
+
+private fun checkInputException(input: List<Int>): Boolean {
+    return input[0] in 3..397 && input[1] in 4.. 398 &&
+            input[1] - input[0] == 1 && input[0] % 2 == 1 && input[1] % 2 == 0
 }
