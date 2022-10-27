292bb477a88e937d32802887153c3d2580568d29
yeongjin <dudwls901@naver.com>
2022-10-27 10:56:09 +0900

    [Feat] get369Count 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,14 @@
 package onboarding
 
+/* 기능 목록
+* get369Count 구현
+* N이하 입력에 대해 3,6,9 개수 저장
+* 누적 합 구현
+* */
+
+fun Int.get369Count() = this.toString().count { it == '3' || it == '6' || it == '9' }
+
+
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    return 0
 }
