d1150d4543e98450ca87d50352571e3dd6d59f82
CHOSEONGROK <csr8367@hanmail.net>
2022-10-27 16:27:55 +0900

    Add solution2

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,40 @@
 package onboarding
 
+import java.util.*
+
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    val stack = ArrayDeque<Char>()
+    val list = LinkedList(cryptogram.toList().drop(1))
+    val it = list.listIterator()
+
+    stack.push(cryptogram[0])
+
+    while (it.hasNext()) {
+        val curr = it.next()
+
+        if (curr == stack.peek()) {
+            do {
+                it.remove()
+            } while (it.hasNext() && stack.peek() == it.next())
+
+            stack.pop()
+
+            if (it.hasPrevious())
+                it.previous()
+        } else {
+            stack.push(curr)
+            it.remove()
+        }
+
+    }
+
+    return stack.reversed().joinToString("")
+}
+
+private fun <T> ArrayDeque<T>.print(tab: String = "") {
+    println("${tab}stack=${reversed()}")
+}
+
+private fun <T> LinkedList<T>.print(tab: String = "") {
+    println("${tab}list =$this")
 }
