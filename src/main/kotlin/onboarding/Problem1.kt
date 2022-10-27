45e4cfc2c2ff84e324a424638fa78a462cc744fb
Gloria <ippnsj@gmail.com>
2022-10-27 17:30:30 +0900

    feat(Problem1/Exception): 예외 상황에 대한 예외처리

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -6,31 +6,31 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     val pobiRangeVerifier = RangeVerifier(pobi[0])
     val crongRangeVerifier = RangeVerifier(crong[0])
     if(!pobiRangeVerifier.verify() || !crongRangeVerifier.verify()) {
-        TODO("예외처리")
+        return Exception().printError()
     }
 
     val pobiOddVerifier = OddVerifier(pobi[0])
     val crongOddVerifier = OddVerifier(crong[0])
     if(!pobiOddVerifier.verify() || !crongOddVerifier.verify()) {
-        TODO("예외처리")
+        return Exception().printError()
     }
 
     val pobiNextPageVerifier = NextPageVerifier(pobi[0], pobi[1])
     val crongNextPageVerifier = NextPageVerifier(crong[0], crong[1])
     if(!pobiNextPageVerifier.verify() || !crongNextPageVerifier.verify()) {
-        TODO("예외처리")
+        return Exception().printError()
     }
 
     val pobiFirstPageVerifier = FirstPageVerifier(pobi[0])
     val crongFirstPageVerifier = FirstPageVerifier(crong[0])
     if(pobiFirstPageVerifier.verify() || crongFirstPageVerifier.verify()) {
-        TODO("예외처리")
+        return Exception().printError()
     }
 
     val pobiLastPageVerifier = LastPageVerifier(pobi[1])
     val crongLastPageVerifier = LastPageVerifier(crong[1])
     if(pobiLastPageVerifier.verify() || crongLastPageVerifier.verify()) {
-        TODO("예외처리")
+        return Exception().printError()
     }
 
     return -1
---
+++ src/main/kotlin/problem1/Exception.kt
@@ -0,0 +1,7 @@
+package problem1
+
+class Exception {
+    fun printError() : Int {
+        return -1
+    }
+}
