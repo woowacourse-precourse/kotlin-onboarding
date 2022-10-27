5f8c5fefa008a131140266e6a503f811b5158d4b
EmilyCh0 <seoyeon3089@naver.com>
2022-10-27 20:52:41 +0900

    feat(problem1): 각 자리 숫자 더한 값과 곱한 값 중 큰 값 구하는 기능 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,8 +1,23 @@
 package onboarding
 
+import kotlin.math.max
+
 fun isException(pages: List<Int>): Boolean{
     if(pages[0]+1!=pages[1]) return true
     return false
+}
+
+fun findMaxValue(num: Int): Int{
+    var plusRes = 0
+    var multiRes = 1
+    var n = num
+    while (n>0){
+        val tmp = n%10
+        plusRes+=tmp
+        multiRes*=tmp
+        n/=10
+    }
+    return max(plusRes, multiRes)
 }
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
