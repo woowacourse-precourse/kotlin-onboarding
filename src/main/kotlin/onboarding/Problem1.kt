546aed56958e0b26b07da3892e9386feaacf48df
wpwo98 <62274357+wpwo98@users.noreply.github.com>
2022-10-27 19:54:17 +0900

    미션1

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -2,4 +2,50 @@ package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
+    var pobi_mul: Int
+    var pobi_num: Array<Int> = arrayOf(0,0,0,0)
+
+    pobi_num[0] = ( (pobi[0] / 100) + (pobi[0]/10)%10 + (pobi[0] % 10) )
+    pobi_num[1] = ( (pobi[1] / 100) + (pobi[1]/10)%10 + (pobi[1] % 10) )
+    if (pobi[0]/100 == 0){
+        pobi_mul = (pobi[0]/10)%10 * (pobi[0] % 10)
+    }else{
+        pobi_mul = (pobi[0] / 100) * (pobi[0]/10)%10 * (pobi[0] % 10)
+    }
+    pobi_num[2] = pobi_mul
+    if (pobi[1]/100 == 0){
+        pobi_mul = (pobi[1]/10)%10 * (pobi[1] % 10)
+    }else{
+        pobi_mul = (pobi[1] / 100) * (pobi[1]/10)%10 * (pobi[1] % 10)
+    }
+    pobi_num[3] = pobi_mul
+    var result_pobi = pobi_num.maxOrNull()
+
+
+    var crong_mul: Int
+    var crong_num: Array<Int> = arrayOf(0,0,0,0)
+
+    crong_num[0] = ( (crong[0] / 100) + (crong[0]/10)%10 + (crong[0] % 10) )
+    crong_num[1] = ( (crong[1] / 100) + (crong[1]/10)%10 + (crong[1] % 10) )
+    if (crong[0]/100 == 0){
+        crong_mul = (crong[0]/10)%10 * (crong[0] % 10)
+    }else{
+        crong_mul = (crong[0] / 100) * (crong[0]/10)%10 * (crong[0] % 10)
+    }
+    crong_num[2] = crong_mul
+    if (crong[1]/100 == 0){
+        crong_mul = (crong[1]/10)%10 * (crong[1] % 10)
+    }else{
+        crong_mul = (crong[1] / 100) * (crong[1]/10)%10 * (crong[1] % 10)
+    }
+    crong_num[3] = crong_mul
+    var result_crong = crong_num.maxOrNull()
+    var result : Int
+
+    if(result_pobi!! > result_crong!!) result = 1
+    else if(result_pobi == result_crong) result = 0
+    else if(result_pobi < result_crong) result = 2
+    else result = -1
+
+    return result;
 }
