18e117d6856e171ef9411ef17cd00592e248c9cd
sojk401 <sojk401@gmail.com>
2022-10-28 00:18:21 +0900

    Problem4. 기능 목록 작성

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -2,4 +2,11 @@ package onboarding
 
 fun solution4(word: String): String {
     TODO("프로그램 구현")
+    /*
+    [기능 목록]
+    1.문자를 변환하여 문자열에 추가하기
+        a) 알파벳 여부 확인
+        b) 대소문자 판별
+        c) 문자 변환
+     */
 }
