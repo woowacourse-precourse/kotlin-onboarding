3f279ac18e8b093e57e1e5a6e3fa7b8af39854ff
wpwo98 <62274357+wpwo98@users.noreply.github.com>
2022-10-27 22:01:13 +0900

    Update Problem4.kt

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -2,4 +2,18 @@ package onboarding
 
 fun solution4(word: String): String {
     TODO("프로그램 구현")
+    var result = ""
+    for (ch in word) {
+        if(ch.isUpperCase()) {
+            result += ('Z'.code - (ch.code - 'A'.code)).toChar()
+        }
+        else if(ch.isLowerCase()) {
+            result += ('z'.code - (ch.code - 'a'.code)).toChar()
+        }
+        else {
+            result += " "
+        }
+    }
+    //print(result)
+    return result
 }
