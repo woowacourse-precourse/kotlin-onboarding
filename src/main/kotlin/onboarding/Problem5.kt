5fbaccf5270a659c28dc14aa2674b8fce984ecba
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-10-27 23:13:52 +0900

    feat:기능목록 1번 기능 추가[문제5]

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -3,3 +3,9 @@ package onboarding
 fun solution5(money: Int): List<Int> {
     TODO("프로그램 구현")
 }
+
+fun exception5(money: Int) {
+    if(money < 1 || money > 10000000) {
+        throw IllegalArgumentException("money는 1이상 10000000이하인 자연수여야 합니다.")
+    }
+}
