8beb828ebc7957339a1fa3f5456e3e2c3a87a93d
lgy15 <lgy159@naver.com>
2022-10-27 16:50:30 +0900

    6번 리팩토링

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,6 +1,6 @@
 package onboarding
 
-fun findIdx(num: Int): Int {
+fun findIdx(num: Int): Int { // 최대로 뺄 수 있는 값의 idx를 return
     val moneyRange = arrayListOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
     for (idx in 0 until moneyRange.size){
         if (num - moneyRange[idx] >= 0) return idx
