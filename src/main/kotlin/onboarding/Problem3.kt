1798296474c4fe5e6c5ee6045b84c1a277c3eff4
tmdgh1592 <tmdgh1592@naver.com>
2022-10-27 18:55:48 +0900

    feat: 주어진 범위의 숫자들에 포함된 3, 6, 9의 총 개수 반환 메서드 구현 - countThreeSixNineInRange() 메서드 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -4,6 +4,15 @@ fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
 
+fun countThreeSixNineInRange(start: Int = 1, end: Int): Int {
+    val initialTotal = 0
+
+    return (start..end).fold(initialTotal) { total, number ->
+        val numberString = number.toString()
+        total + countThreeSixNineOf(numberString)
+    }
+}
+
 fun countThreeSixNineOf(numberString: String): Int {
     val initialCount = 0
     val numberChars = listOf('3', '6', '9')
