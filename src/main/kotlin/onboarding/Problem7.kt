b10357aead204b26debc88baf042f3fcd45bdc37
CHOSEONGROK <csr8367@hanmail.net>
2022-10-27 16:40:10 +0900

    Add solution7

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,45 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+
+    val myFriends = hashSetOf<String>()
+    val recommendMap = hashMapOf<String, Int>()
+
+    // 1. 친구 목록 추가
+    for (friend in friends) {
+        when (user) {
+            friend[0] -> myFriends.add(friend[1])
+            friend[1] -> myFriends.add(friend[0])
+        }
+    }
+
+    // 2. 추천 맵에 간접 친구 점수 추가
+    for (friend in friends) {
+        val indirect = when {
+            friend[0] in myFriends && user != friend[1] -> friend[1]
+            friend[1] in myFriends && user != friend[0] -> friend[0]
+            else -> continue
+        }
+
+        recommendMap[indirect] = recommendMap.getOrDefault(indirect, 0) + 10
+    }
+
+    // 3. 추천 맵에 친구가 아닌 방문자 점수 추가
+    for (visitor in visitors) {
+        if (visitor in myFriends)
+            continue
+
+        recommendMap[visitor] = recommendMap.getOrDefault(visitor, 0) + 1
+    }
+
+    return recommendMap.asSequence()
+        .sortedWith { e1, e2 ->
+            val a = e2.value.compareTo(e1.value)
+            val b = e1.key.compareTo(e2.key)
+
+            if (a != 0) a else b
+        }
+        .map { it.key }
+        .take(5)
+        .toList()
 }
