237f6dc385eaa519579f2e2fb1eb2185fa69738f
tmdgh1592 <tmdgh1592@naver.com>
2022-10-27 18:41:30 +0900

    feat: 숫자 문자열을 입력받았을 때 포함된 3, 6, 9의 개수 반환 메서드 구현 - countOfThreeSixNine() 메서드 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -4,6 +4,15 @@ fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
 
+fun String.countOfThreeSixNine(): Int {
+    val initialCount = 0
+    val numberChars = listOf('3', '6', '9')
+
+    return numberChars.fold(initialCount) { count, numberChar ->
+        count + this.countOf(numberChar)
+    }
+}
+
 fun String.countOf(ch: Char): Int =
     this.count { _ch ->
         ch == _ch
