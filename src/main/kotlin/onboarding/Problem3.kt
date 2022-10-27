0ba97b6bb98cd5829d0c4802b16b2c2344be0309
yeongjin <dudwls901@naver.com>
2022-10-27 10:56:54 +0900

    [Feat] N이하 입력에 대해 3,6,9 개수 저장

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -8,7 +8,15 @@ package onboarding
 
 fun Int.get369Count() = this.toString().count { it == '3' || it == '6' || it == '9' }
 
+fun getResultArray(n: Int): IntArray {
+    val arr = IntArray(n + 1)
+    for (i in 1..n) {
+        arr[i] = i.get369Count()
+    }
+    return arr
+}
 
 fun solution3(number: Int): Int {
+    val resultArr = getResultArray(number)
     return 0
 }
