b75e6e11a62762fea44f60399f8ba88234271585
lgy15 <lgy159@naver.com>
2022-10-27 15:51:51 +0900

    4번 리팩토링

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,22 +1,19 @@
 package onboarding
 
-fun solution4(word: String): String {
+fun alphaChange(alpha: Char): Char {
     val lower_alphabet = arrayListOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
     val upper_alphabets  = arrayListOf<Char>() // 초기화
-    for (alpha in lower_alphabet) upper_alphabets.add(alpha.uppercaseChar()) // 대문자로 변경뒤 추가
+    for (alphabet in lower_alphabet) upper_alphabets.add(alphabet.uppercaseChar()) // 대문자로 변경뒤 추가
     val re_lower = lower_alphabet.reversed() // 소문자 뒤집기
     val re_upper = upper_alphabets.reversed() // 대문자 뒤집기
-    var result : String = ""
+    if (alpha == ' ') return ' '
+    if (alpha.isUpperCase()) return re_upper[upper_alphabets.indexOf(alpha)]
+    if (alpha.isLowerCase()) return re_lower[lower_alphabet.indexOf(alpha)]
+    return alpha
+}
+fun solution4(word: String): String {
 
-    for (char in word) { // result에 변환 된 문자 더하기
-        result += if (char == ' ') { // 띄어쓰기인 경우 띄어쓰기 추가
-            ' '
-        }
-        else if (char.isUpperCase()) { // 대문자인 경우 그 대문자의 index를 reverse된 index에 집어넣으면 변환 완료
-            re_upper[upper_alphabets.indexOf(char)]
-        } else {
-            re_lower[lower_alphabet.indexOf(char)]
-        }
-    }
+    var result : String = ""
+    for (char in word) result += alphaChange(char)
     return result
 }
