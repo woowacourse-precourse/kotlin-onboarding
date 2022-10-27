714d8699bf26baa4d1fe924667f10b95e0532bc5
sojk401 <sojk401@gmail.com>
2022-10-28 01:38:51 +0900

    Problem5. 기능 목록 작성

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -2,4 +2,11 @@ package onboarding
 
 fun solution5(money: Int): List<Int> {
     TODO("프로그램 구현")
+    /*
+    [기능 목록]
+    1. 오만 원권부터 일원 동전까지 각 몇 개로 반환되는지 저장
+    2. money를 돈의 액수로 나눈 몫과 나머지 구하기
+        a) 몫은 변환되는 개수
+        b) 나머지는 다시 변환활 money
+     */
 }
