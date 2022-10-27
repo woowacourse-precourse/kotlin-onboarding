a2cef9abd22a79d716b200c03309663472eb10ff
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-10-27 18:47:55 +0900

    Problem2 정규식을 이용한 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,18 @@
 package onboarding
 
+import java.util.regex.Pattern
+
+fun checkSuccessiveChar(word:String): Boolean {
+    return Pattern.matches("\\w{2,}", word)
+}
+
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    var resultEmailList=ArrayList<String>()
+    for(eachForm in forms){
+        val eachEmail=eachForm[0]
+        if(!checkSuccessiveChar(eachEmail))
+            resultEmailList.add(eachEmail)
+    }
+    return resultEmailList.sorted()
 }
