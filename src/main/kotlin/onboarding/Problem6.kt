37437d128a0296dc9d560b49648af5d0247adbfb
wonseok <dnjstjr245@gmail.com>
2022-10-27 20:48:57 +0900

    :sparkles: Problem6 solved

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,34 @@
 package onboarding
 
+/**
+ * @author onseok
+ */
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val ans = mutableSetOf<String>()
+    for (i in 0 until forms.size - 1) {
+        for (j in i + 1 until forms.size) {
+            if (forms[i].last().isDuplicatedWith(forms[j].last())) {
+                ans.add(forms[i].first())
+            }
+        }
+    }
+    return ans.toList().sorted()
+}
+
+fun String.isDuplicatedWith(other: String): Boolean {
+    if (this.length == 1 ||
+        other.length == 1
+    ) return false
+    val candidate = mutableSetOf<String>()
+    for (i in 0 until other.length - 1) {
+        for (j in 1 until other.length - i) {
+            candidate.add(other.substring(i, i + j))
+        }
+    }
+    candidate.forEach {
+        if (this.contains(it)) {
+            return true
+        }
+    }
+    return false
 }
