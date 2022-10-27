9e493268c3e349ab02b8f8d63b93551e912931c1
sojk401 <sojk401@gmail.com>
2022-10-28 00:35:52 +0900

    Problem4. 1-a. 문자의 알파벳 여부 확인

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,7 +1,6 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
     /*
     [기능 목록]
     1.문자를 변환하여 문자열에 추가하기
@@ -9,4 +8,20 @@ fun solution4(word: String): String {
         b) 대소문자 판별
         c) 문자 변환
      */
+    val alphabet = mutableListOf<String>()
+    var result: String = ""
+
+    for (i in 'a'..'z'){
+        alphabet.add(i.toString())
+    }
+
+    for (s in word){
+        if(s.isLetter()){
+            println("${s}는 알파벳")
+        }
+        else {
+            println("알파벳이 아님")
+        }
+    }
+    return "Hello World"
 }
