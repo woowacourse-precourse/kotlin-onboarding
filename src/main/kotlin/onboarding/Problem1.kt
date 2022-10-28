95d937e72120cf95c275aff1ee8124aa4239c76a
Doyeop Kim <dygames@naver.com>
2022-10-29 06:07:06 +0900

    FEATURE: 각 자리 숫자를 더하거나 곱함

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,26 @@
 package onboarding
 
+import java.lang.Integer.max
+
+/*
+기능 목록
+- 각 자리 숫자를 더하거나 곱함
+- 점수를 결정함
+- 점수를 비교함
+*/
+
+fun processDigit(num: Int, operator: Int): Int {
+    var n = num
+    var r = n % 10
+    while (true) {
+        n /= 10
+        if (n == 0) break
+        if (operator == 0) r += n % 10
+        else if (operator == 1) r *= n % 10
+    }
+    return r
+}
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
 }
