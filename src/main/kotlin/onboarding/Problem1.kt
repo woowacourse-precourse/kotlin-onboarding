26ffd80d2c94a247c2d2953eac19b72b8b825b00
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-29 04:11:56 +0900

    기능별 목록 수정 및 예외사항 확인 함수 수정

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,61 +1,26 @@
-@file:Suppress("UNREACHABLE_CODE", "DEPRECATED_IDENTITY_EQUALS")
-
 package onboarding
-
-import java.util.*
-
-fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
-    var result: Int = 0
-
-    if(exception(pobi)==1 && exception(crong) ==1){
-        return -1
-    }//1.입력값이 예외사항인지 확인한다.
-
-
-}
-//입력값이 예외사항인지 확인한다.
-fun exception(member: List<Int>): Int {
-    var outcome: Int  = 0
-    if(member[0] !== (member[1]+1)){
-        outcome = 0
-    }
-    else if(member[0]==1 || member[1]==400){
-        outcome = 0
-    }
-    else{
-        outcome = 1
-    }
-    return outcome
-}
-
-//포비와 크롱값을 계산한다.
-fun Calculation(number: List<Int>): Int {
-    var leftoutcome1: Int = 0
-    var rightoutcome1: Int = 0
-    var leftoutcome2: Int = 0
-    var rightoutcome2: Int = 0
-    var finally:Int = 0
-    var outcome= arrayListOf<Int>()
-    leftoutcome1 = (number[0]/100) + ((number[0]/10)-((number[0]/100)*100)) + (number[0]-((number[0]/100)*100) - ((number[0]/10)*10))
-    rightoutcome1 = (number[1]/100) + ((number[1]/10)-((number[1]/100)*100)) + (number[1]-((number[1]/100)*100) - ((number[1]/10)*10))
-
-    leftoutcome2 = (number[0]/100) * ((number[0]/10)-((number[0]/100)*100)) * (number[0]-((number[0]/100)*100) - ((number[0]/10)*10))
-    rightoutcome2 = (number[1]/100) * ((number[1]/10)-((number[1]/100)*100)) * (number[1]-((number[1]/100)*100) - ((number[1]/10)*10))
-
-    outcome.add(leftoutcome1)
-    outcome.add(rightoutcome1)
-    outcome.add(leftoutcome2)
-    outcome.add(rightoutcome2)
-
-    finally = Collections.max(outcome)
-    return finally
-
-
-}
 
 //기능별 목록
 //1. 입력값이 예외사항인지 확인한다.
-//2. 포비값을 계산한다.
-//3. 크롱값을 계산한다.
-//4. 결과를 리턴한다.
+//2. 포비와 크롱 값을 게산한다.
+//4. 계산한 값을 기반으로 결과를 리턴한다.
+
+fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+    var pobiresult:Int = 0
+    var crongresult:Int = 0
+    //입력값이 예외사항인지 확인한다
+    if(exception(pobi) || exception(crong)){
+        return -1
+    }
+    return 0
+}
+//입력값이 예외사항인지 확인하는 함수
+fun exception(member: List<Int>): Boolean{
+    if(member[0]+1 == (member[1])){ //펼친 페이지가 연속 숫자인지 확인
+        return false
+    }
+    else if(member[0]==1 || member[1]==400){    //펼친 페이지가 첫장과 마지막장인지 확인
+        return false
+    }
+    return true
+}
