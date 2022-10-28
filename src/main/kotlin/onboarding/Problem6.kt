2fcf5634fb55fbff12536e7f6c9314a19ee2e050
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-29 01:10:30 +0900

    feat(Problem6): add find duplicate nickname code

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,19 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val result: HashSet<String> = hashSetOf()
+    for (i in forms.indices) {
+        val name = forms[i][1]
+        for (j in 0..name.length - 2) {
+            val tmp = name.substring(j, j + 2)
+            for (k in forms.indices) {
+                if (forms[k][1].contains(tmp)) {
+                    if (forms[k][0] != forms[i][0]) {
+                        result.add(forms[k][0])
+                    }
+                }
+            }
+        }
+    }
+    return result.toList().sorted()
 }
