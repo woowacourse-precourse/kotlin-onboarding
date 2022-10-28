3ead610d62e4924b9b839da65ad4cba3f9122374
tmdgh1592 <tmdgh1592@naver.com>
2022-10-28 10:21:49 +0900

    feat: 소문자의 반대 소문자를 가지는 맵 반환 메서드 구현  - createReversedLowerAlphaMap() 메서드 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -18,3 +18,18 @@ private fun createReversedUpperAlphaMap(): Map<Char, Char> {
 
     return reversedUpperAlphaMap
 }
+
+private fun createReversedLowerAlphaMap(): Map<Char, Char> {
+    val reversedUpperAlphaMap = mutableMapOf<Char, Char>()
+    val upperA = 97
+    val upperZ = 122
+    val alphaStart = 0
+    val alphaEnd = 13
+
+    for (i in alphaStart until alphaEnd) {
+        reversedUpperAlphaMap[Char(upperA + i)] = Char(upperZ - i)
+        reversedUpperAlphaMap[Char(upperZ - i)] = Char(upperA + i)
+    }
+
+    return reversedUpperAlphaMap
+}
