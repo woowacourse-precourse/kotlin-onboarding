b2dfa0d3c813d72735950b2bbffafd047ebda32b
Jaeheon Jeong <drdd1120@naver.com>
2022-10-29 01:08:30 +0900

    p7 요구사항 작성

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,5 +1,12 @@
 package onboarding
 
+/*
+1. friends 배열을 바탕으로 관계를 hashmap으로 만들어준다.
+2. 함께 아는 친구의 수에 따라 점수를 계산한다.
+3. 타임 라인에 방문한 횟수에 따라 점수를 계산한다.
+4. 점수를 내림차순 정렬하고 최대 5명을 리턴한다.
+ */
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
