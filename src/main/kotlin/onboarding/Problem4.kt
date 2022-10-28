07c3dff5ea77d71309bec6d0d0e8858e1a644ab9
jjiiin <wjdwlin4051@gmail.com>
2022-10-28 23:53:34 +0900

    feat: Problem4 알파벳 바꾸는 반복문 및 조건문 구현
    Problem4.kt 문자열을 처음부터 끝까지 탐색해서 해당 문자가 대문자인지 소문자인지 공백인지 확인하고 아스키 코드를 계산해서 result에 추가한다

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,27 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    val wordToCharList: List<Char> = word.toList()
+    var result = ""
+
+    for ( i in 0..wordToCharList.size - 1){
+        var frogDictionaryResult: Int
+
+        if(wordToCharList[i].isUpperCase()){
+
+            frogDictionaryResult = 90 - (wordToCharList[i].code - 65)
+            result += frogDictionaryResult.toChar()
+
+        }else if(wordToCharList[i].isLowerCase()) {
+
+            frogDictionaryResult = 122 - (wordToCharList[i].code - 97)
+            result += frogDictionaryResult.toChar()
+
+        }else{
+
+            result += " "
+        }
+    }
+
+    return result
 }
