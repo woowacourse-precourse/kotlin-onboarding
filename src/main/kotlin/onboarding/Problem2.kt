8f29f0c1483065e5630229a46b74b82328a6c427
JunJangE <wnswkd486@gmail.com>
2022-10-28 16:40:07 +0900

    Feat: Problem2 1번 기능 추가

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    return removeString(cryptogram)
+}
+
+// 연속되는 중복 문자들을 삭제하는 기능
+fun removeString(str: String) : String{
+    var removeStr = str
+
+    // 똑같은 문자가 2개 이상 나오면 빈 문자열로 변환
+    removeStr.forEach {
+        removeStr = str.replace("$it{2,}".toRegex(), "")
+    }
+
+    return removeStr
 }
