fd2c18cee5a6518b36808da1810a244a75bae29e
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-10-29 05:13:27 +0900

    Refactor: else 구문 수정, 공간복잡도 단순화, 가독성 향상 in Problem1.kt
    기존 else 구문 모두 삭제
    2차원 배열을 이용해 모든 숫자를 저장하던 것을 1차원 배열에 결과만 저장하도록 수정
    주석 수정 및 전반적인 구조 단순화로 가독성 향상

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,15 +1,14 @@
 package onboarding
 
-import java.lang.Math.*
+import java.lang.Math.max
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 
     /**
      * 기능목록
      * 1. 예외처리
-     * 2. pobi, crong List 내 값들을 지역 변수에 저장
-     * 3. pobi 좌우, crong 좌우 숫자 연산 후 비교
-     * 4. pobi, crong 비교 후 결과 리턴
+     * 2. pobi 좌우, crong 좌우 중 큰 숫자를 각각의 최대값 변수에 할당
+     * 3. pobi, crong 비교 후 결과 리턴
      * */
 
     /** 1. 예외처리 */
@@ -32,89 +31,50 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
         return -1
     }
 
-
-    var temp = 0
-    var cnt = 0
-    var pobiNumLen = 0
     var pobiSum = 0
     var pobiMul = 1
+    var pobiCalculatedNumList = IntArray(2)
     var pobiMax = 0
-    var crongNumLen = 0
+
     var crongSum = 0
     var crongMul = 1
+    var crongCalculatedNumList = IntArray(2)
     var crongMax = 0
 
-    /**
-     * 0. 자릿 수 확인
-     * */
-    temp = pobi.get(1)
-    while (temp > 0) {
-        temp /= 10
-        cnt += 1
-    }
-    pobiNumLen = cnt
+    var temp: Int = 0
 
-    temp = crong.get(1)
-    while (temp > 0) {
-        temp /= 10
-        cnt += 1
-    }
-    crongNumLen = cnt
-
-    /**
-     * 1. 숫자 자릿수를 나눠 저장하는 배열
-     * */
-    var pobiNumArr = Array(2){IntArray(pobiNumLen)}
-
-    /**
-     * 2. 나눈 자릿 수를 저장한 뒤 더하기 및 곱하기 진행 후 최대값 저장
-     * */
-    for (i in 0..1){
+    /** 2. pobi 좌우, crong 좌우 중 큰 숫자를 각각의 최대값 변수에 할당 */
+    for (i in 0..1) {
         temp = pobi[i]
-        cnt = 0
         while (temp > 0) {
-            pobiNumArr[i][cnt] = temp % 10
-            pobiSum += pobiNumArr[i][cnt]
-            pobiMul *= pobiNumArr[i][cnt]
+            pobiSum += temp % 10
+            pobiMul *= temp % 10
             temp /= 10
-            cnt += 1
         }
-        pobiMax = max(pobiSum, pobiMul)
+        pobiCalculatedNumList[i] = max(pobiSum, pobiMul)
     }
+    pobiMax = max(pobiCalculatedNumList[0], pobiCalculatedNumList[1])
 
-    /**
-     * 3. 1과 동일
-     * */
-    var crongNumArr = Array(2){IntArray(crongNumLen)}
-
-    /**
-     * 4. 2와 동일
-     * */
     for (i in 0..1){
         temp = crong[i]
-        cnt = 0
         while (temp > 0) {
-            crongNumArr[i][cnt] = temp % 10
-            crongSum += crongNumArr[i][cnt]
-            crongMul *= crongNumArr[i][cnt]
+            crongSum += temp % 10
+            crongMul *= temp % 10
             temp /= 10
-            cnt += 1
         }
-        crongMax = max(crongSum, crongMul)
+        crongCalculatedNumList[i] = max(crongSum, crongMul)
     }
+    crongMax = max(crongCalculatedNumList[0], crongCalculatedNumList[1])
 
-    /**
-     * 5. 각 최대값을 비교하여 결과값 리턴
-     * */
+    /** 3. pobi, crong 비교 후 결과 리턴 */
     if (pobiMax > crongMax){
         return 1
     }
-    else if (pobiMax < crongMax){
+    if (pobiMax < crongMax){
         return 2
     }
-    else if (pobiMax == crongMax){
+    if (pobiMax == crongMax){
         return 0
-    }else{
-        return -1
     }
+    return -1
 }
