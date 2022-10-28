1bd1b7c7c66773ec43d07334183a6b9b4b11341b
Gloria <ippnsj@gmail.com>
2022-10-28 23:48:59 +0900

    feat(Problem3/Game369): 각 숫자에 포함된 3, 6, 9 개수 세기

--- src/main/kotlin/problem3/Game369.kt
+++ src/main/kotlin/problem3/Game369.kt
@@ -1,9 +1,13 @@
 package problem3
 
 class Game369 {
+    private fun count369(num:String) : Int {
+        return num.count { it == '3' || it == '6' || it == '9' }
+    }
+
     fun count369(start:Int, end:Int) {
         for(i in start..end) {
-            i.toString()
+            count369(i.toString())
         }
     }
 }
