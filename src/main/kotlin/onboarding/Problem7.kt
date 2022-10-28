88f30a9d97cd2575f87361881c8eb924f6e0ddff
Jaeheon Jeong <drdd1120@naver.com>
2022-10-29 01:11:25 +0900

    p7 2. 함께 아는 친구의 수에 따라 점수를 계산한다.

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,5 +1,9 @@
 package onboarding
 
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
 /*
 1. friends 배열을 바탕으로 관계를 hashmap으로 만들어준다.
 2. 함께 아는 친구의 수에 따라 점수를 계산한다.
@@ -13,6 +17,8 @@ fun solution7(
     visitors: List<String>
 ): List<String> {
     val hm = makeRelations(friends)
+
+    var score: MutableMap<String, Int> = calcFreindPoint(hm, user)
 }
 
 private fun makeRelations(friends: List<List<String>>): HashMap<String, ArrayList<String>> {
@@ -26,4 +32,21 @@ private fun makeRelations(friends: List<List<String>>): HashMap<String, ArrayLis
         hm[friends[i][1]] = list2
     }
     return hm
+}
+
+private fun calcFreindPoint(hm: HashMap<String, ArrayList<String>>, user: String): MutableMap<String, Int> {
+    var score: MutableMap<String, Int> = TreeMap()
+
+    for (key in hm.keys) {
+        if (key == user) {
+            continue
+        } else {
+            for (i in 0 until hm.get(user)!!.size) {
+                if (hm[key]!!.contains(hm[user]!![i])) {
+                    score[key] = score.getOrDefault(key, 0) + 10
+                }
+            }
+        }
+    }
+    return score
 }
