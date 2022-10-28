10be384f37be8d3ddeb1189f4d1c36b46cbed9d0
ChunBaee <mljung10@naver.com>
2022-10-29 03:12:09 +0900

    5번] 기능 4) 반환 : 계산값의 몫들, 즉 몇 번 바꿀수 있는지 를 담은 배열을 반환

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -6,12 +6,16 @@ fun solution5(money: Int): List<Int> {
     var mMoney = money // 파라미터로 들어오는 변수는 val 이므로, 루프마다 값을 변경해주기 위해 var로 재선언했다.
     for(i in priceList) {
         val mList = checkHowMuch(mMoney, i)
-        resultList.add(resultList[0])
-        mMoney = mList[1]
+        resultList.add(mList[0]) //위의 mList의 0번째에는 몫의 값, 즉 몇 번 바꿀 수 있는지가 들어간다. 그러므로 결과값을 리턴해줄 List에 추가한다.
+        mMoney = mList[1] // mList의 1번째에는 나머지 값, 즉 남은 금액이 들어가므로, 이를 활용해 다시 한번 루프를 돌리면 남은 금액에 그 다음번 필터가 몇 번 돌아갈지가 구해진다.
     }
+    return resultList
 }
 
+/**
+ * 현재 금액을 몇 번 바꿀 수 있는지, 바꾼 후 금액은 얼마인지 계산해주는 함수
+ */
 private fun checkHowMuch(price : Int, priceList : Int) : List<Int> {
-    return listOf(price / priceList, price % priceList)
+    return listOf(price / priceList, price % priceList) // 몫과 나머지 모두를 반환해줘야 하므로 List<Int> 형 반환을 선택했다.
 }
 
