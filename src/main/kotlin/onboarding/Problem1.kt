c42d4704fceccb338cd9add6d90c622b4b7f838c
JunJangE <wnswkd486@gmail.com>
2022-10-28 16:27:30 +0900

    Feat: Problem1 2번 기능 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -7,6 +7,11 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     if (!exceptionCheck(pobi[0], pobi[1]) || !exceptionCheck(crong[0], crong[1])){
         return -1
     }
+
+    // 포비와 크롱의 최대 점수
+    val pobiNum = pobi.toMaxNum()
+    val crongNum = crong.toMaxNum()
+
     return 0
 }
 
@@ -17,4 +22,17 @@ fun exceptionCheck(startPageNum : Int, endPageNum : Int) : Boolean{
             && endPageNum - startPageNum == 1
             && startPageNum in 1..400
             &&endPageNum in 1 ..400
+}
+
+
+// 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구하는 기능
+fun List<Int>.toMaxNum() : Int{
+
+    return maxOf {
+        val target = it.toString()
+        max(
+            target.fold(0){ acc, i -> acc + i.digitToInt()},
+            target.fold(1){ acc, i -> acc * i.digitToInt()}
+        )
+    }
 }
