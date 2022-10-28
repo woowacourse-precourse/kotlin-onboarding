8ad3805d2ccadc817a826da97fb2ee2f68a99181
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-10-29 04:15:29 +0900

    Problem2 전역변수를 지역변수로 수정

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,18 +1,19 @@
 package onboarding
 
-val successiveCharRegex="""a{2,}|b{2,}|c{2,}|d{2,}|e{2,}|f{2,}|g{2,}|h{2,}|i{2,}|j{2,}|k{2,}|l{2,}|m{2,}|n{2,}|o{2,}|p{2,}|q{2,}|r{2,}|s{2,}|t{2,}|u{2,}|v{2,}|w{2,}|x{2,}|y{2,}|z{2,}""".toRegex()
-fun solution2checkSuccessiveChar(word:String): Boolean {
+
+fun solution2checkSuccessiveChar(word:String, successiveCharRegex:Regex): Boolean {
     return successiveCharRegex.containsMatchIn(word)
 }
 
-fun solution2replaceSuccessiveChar(word:String): String{
+fun solution2replaceSuccessiveChar(word:String, successiveCharRegex:Regex): String{
     return word.replace(successiveCharRegex, "")
 }
 
 fun solution2(cryptogram: String): String {
     var resultString=cryptogram
-    while(solution2checkSuccessiveChar(resultString)){
-        resultString=solution2replaceSuccessiveChar(resultString)
+    val successiveCharRegex="""a{2,}|b{2,}|c{2,}|d{2,}|e{2,}|f{2,}|g{2,}|h{2,}|i{2,}|j{2,}|k{2,}|l{2,}|m{2,}|n{2,}|o{2,}|p{2,}|q{2,}|r{2,}|s{2,}|t{2,}|u{2,}|v{2,}|w{2,}|x{2,}|y{2,}|z{2,}""".toRegex()
+    while(solution2checkSuccessiveChar(resultString, successiveCharRegex)){
+        resultString=solution2replaceSuccessiveChar(resultString, successiveCharRegex)
     }
 
    return resultString
