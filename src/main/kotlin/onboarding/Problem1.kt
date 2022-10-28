946a55c2c390f9dc0116f03e57825eb14a26d7e3
YuGyeong98 <kuk3097@naver.com>
2022-10-29 00:46:45 +0900

    feature(Problem1): 페이지 번호의 각 자릿수 더하기 함수 생성

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,26 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+  val pobiLeftPage = pobi[0]
+  val pobiRightPage = pobi[1]
+  val pobiLeftAdd = addEachPageNum(pobiLeftPage)
+  val pobiRightAdd = addEachPageNum(pobiRightPage)
+
+  val crongLeftPage = crong[0]
+  val crongRightPage = crong[1]
+  val crongLeftAdd = addEachPageNum(crongLeftPage)
+  val crongRightAdd = addEachPageNum(crongRightPage)
+
+  return 0
+}
+
+fun addEachPageNum(page: Int): Int {
+  var page = page
+  var pageSum = 0
+
+  while (page > 0) {
+    pageSum += page % 10
+    page /= 10
+  }
+  return pageSum
 }
