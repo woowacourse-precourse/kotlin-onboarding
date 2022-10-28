6cc1bf44c955ef11c8ed56597ce51f1377efe083
dltkd1395 <dltkd13956@naver.com>
2022-10-28 14:10:40 +0900

    feat : [PROBLEM5] 돈 단위 개수 변환 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,18 @@
 package onboarding
 
+val moneyList = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+
 fun solution5(money: Int): List<Int> {
     TODO("프로그램 구현")
+}
+
+fun calculateMoney(index : Int, money : Int, result : MutableList<Int>) : List<Int> {
+    if (money == 0) {
+        return result
+    }
+    if (money / moneyList[index] != 0) {
+        result[index] = (money / moneyList[index])
+        return calculateMoney(index + 1, money % moneyList[index], result)
+    }
+    return  calculateMoney(index + 1, money, result)
 }
