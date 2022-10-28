d2179cad3ff8528225aa9d5a909977aaa74e34fe
inseonyun <yuninseon@naver.com>
2022-10-28 15:33:36 +0900

    [Problem5] 화폐 단위 배열 생성 및 화폐 개수 리턴 함수 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,13 @@
 package onboarding
 
+val currency = arrayListOf<Int>(50000, 10000, 5000, 1000, 500, 100, 10, 1)
+
+fun getCountMoney(idx: Int, money: Int):Int {
+    return money / currency[idx]
+}
+
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var result = mutableListOf<Int>()
+
+    return result
 }
