3e87f0dba1aadf901927e476b5288acfad106cec
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-10-29 03:53:50 +0900

    Problem1 각 페이지를 더하는 함수와 곱하는 함수의 분리

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,40 +1,55 @@
 package onboarding
 
+import java.lang.Math.max
+
 fun solution1CheckException(inputList: List<Int>): Boolean {
     //문제에서 정의한 예외를 다음과 같은 상황이라고 해석했음
-    if(inputList.contains(1)||inputList.contains(400)) return true
-    if(inputList[1]-inputList[0]!=1) return true
+    if (inputList.contains(1) || inputList.contains(400)) return true
+    if (inputList[1] - inputList[0] != 1) return true
     return false
 }
 
-fun solution1Solve(inputList: List<Int>): Int {
-    var addScore = 0
+fun solution1calcMultipleScore(inputList: List<Int>): Int {
     var multipleScore = 1
 
     inputList.forEach { page ->
-        var (tmpAddScore, tmpMultipleScore)=arrayOf(0, 1)
+        var tmpMultipleScore = 1
         page.toString().forEach { eachNum ->
-            tmpAddScore += Character.getNumericValue(eachNum)
             tmpMultipleScore *= Character.getNumericValue(eachNum)
         }
-
-        addScore= addScore.coerceAtLeast(tmpAddScore)
-        multipleScore= tmpAddScore.coerceAtLeast(tmpMultipleScore)
+        multipleScore = multipleScore.coerceAtLeast(tmpMultipleScore)
     }
 
-    return addScore.coerceAtLeast(multipleScore)
+    return multipleScore
 }
 
-fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    if(solution1CheckException(pobi)||solution1CheckException(crong))
-        return -1
+fun solution1calcAddScore(inputList: List<Int>): Int {
+    var addScore = 0
 
-    var (pobiScore, crongScore) = arrayOf(0, 0);
+    inputList.forEach { page ->
+        var tmpAddScore = 0
+        page.toString().forEach { eachNum ->
+            tmpAddScore += Character.getNumericValue(eachNum)
+        }
+        addScore = addScore.coerceAtLeast(tmpAddScore)
+    }
 
-    pobiScore = solution1Solve(pobi)
-    crongScore = solution1Solve(crong)
+    return addScore
+}
 
+fun solution1whoWin(pobiScore:Int, crongScore:Int):Int{
     return if (pobiScore > crongScore) 1
     else if (pobiScore < crongScore) 2
     else 0
+}
+
+fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+    if (solution1CheckException(pobi) || solution1CheckException(crong))
+        return -1
+    var (pobiScore, crongScore) = arrayOf(0, 0);
+
+    pobiScore = solution1calcMultipleScore(pobi).coerceAtLeast(solution1calcAddScore(pobi))
+    crongScore = solution1calcMultipleScore(crong).coerceAtLeast(solution1calcAddScore(crong))
+
+    return solution1whoWin(pobiScore, crongScore)
 }
