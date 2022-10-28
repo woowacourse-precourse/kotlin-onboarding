36308c39bd8a08e6d67b86e6131d846cf3661298
dohun_ <silver2679@gmail.com>
2022-10-28 20:39:43 +0900

    Feat Problem1: find the sum, multiply of crong's page and save max value

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -19,8 +19,11 @@ fun exceptions(pobi: List<Int>, crong: List<Int>) {
 }
 fun findMaxNumber(pobi: List<Int>, crong: List<Int>): Int {
     var pobiMaxValue : Int = 0
+    var crongMaxValue : Int = 0
     var pobiSum : Int = 0
+    var crongSum : Int = 0
     var pobiMul : Int = 0
+    var crongMul : Int = 0
     var temp : Int = 0
     var MaxValue : Int = 0
 
@@ -46,6 +49,29 @@ fun findMaxNumber(pobi: List<Int>, crong: List<Int>): Int {
         if(pobiMaxValue < MaxValue){
             pobiMaxValue = MaxValue
         }
+        //곱한 수와 더한 수 비교 후 저장
+        //crong 점수 구하기
+        crongSum = crong[i].toString().map{ it.toInt() - 48 }.sum()
+        //페이지 합
+        temp = crong[i]
+        if(temp >= 100){
+            crongMul = (temp/100) * (temp%100/10) * (temp%10)
+        } else if (temp < 10){
+            crongMul = temp
+        } else{
+            crongMul = (temp/10) * (temp%10)
+        }
+        //페이지 곱
+        if(crongSum > crongMul){
+            MaxValue = crongSum
+        } else{
+            MaxValue = crongMul
+        }
+
+        if(crongMaxValue < MaxValue){
+            crongMaxValue = MaxValue
+        }
+        //곱한 수와 더한 수 비교 후 저장
     }
     return 0
 }
