82e9b45adf75ad381c846d44e02eca6f522cc0ef
jjiiin <wjdwlin4051@gmail.com>
2022-10-28 23:22:41 +0900

    feat: Problem2 해독한 문자열 갱신하는 반복문 구현
    Problem2.kt solution2 cryptogramResult가 갱신되지 않을 때까지 decodeString을 호출하는 반복문 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,37 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var cryptogramResult = cryptogram
+    var decodeResult=decodeString(cryptogram)
+
+    while(true){
+        if (cryptogramResult != decodeResult){
+            cryptogramResult = decodeResult
+        }else{
+            break
+        }
+        decodeResult = decodeString(decodeResult)
+    }
+    return decodeResult
+
 }
+fun decodeString(cryptogram:String) : String{
+    var cryptogramResult = cryptogram
+    var count=0
+
+    for (i in 0..cryptogram.length - 2 step (1)){
+        if (cryptogram[i] == cryptogram[i+1]){
+            ++count
+        }
+
+        if(count>0){
+
+            val subString= cryptogram.substring(i until i+count+1)
+            cryptogramResult=cryptogramResult.replace(subString,"")
+            break
+        }
+    }
+
+    return cryptogramResult
+}
+
