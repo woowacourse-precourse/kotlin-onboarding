036b3f3cfdfc55176873b0a1c365e79f2d5529b2
EmilyCh0 <seoyeon3089@naver.com>
2022-10-28 19:41:56 +0900

    feat(problem5): 오만원 부터 일원까지 순차적으로 result 값 구하는 기능 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,24 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val result = IntArray(9)
+    var m = money
+    result[0] = m/50000
+    m%=50000
+    result[1] = m/10000
+    m%=10000
+    result[2] = m/5000
+    m%=5000
+    result[3] = m/1000
+    m%=1000
+    result[4] = m/500
+    m%=500
+    result[5] = m/100
+    m%=100
+    result[6] = m/50
+    m%=50
+    result[7] = m/10
+    m%=10
+    result[8] = m
+    return result.toList()
 }
