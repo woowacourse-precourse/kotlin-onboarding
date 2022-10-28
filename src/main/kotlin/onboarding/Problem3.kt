3bb532926a8b38324d84c8149ec69e9a5de926fd
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-10-28 10:51:35 +0900

    solve : Problem3

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,22 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var result = 0
+
+    for (i in 1 .. number) {
+        var temp = i
+
+        while (temp != 0) {
+            if (temp % 10 == 3 || temp % 10 == 6 || temp % 10 == 9) {
+                result++
+            }
+            temp /= 10
+        }
+    }
+    return result
+}
+
+fun main() {
+    println(solution3(13))
+    println(solution3(33))
 }
