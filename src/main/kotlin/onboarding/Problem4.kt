3a6c049efe05e7f76b3161cbe05b7c1290de84d7
yulim <yulim13sky@naver.com>
2022-10-28 16:28:42 +0900

    [FEAT] Problem4 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,29 @@
 package onboarding
 
+const val DIFF_UPPER_LOWER_LETTER = 32
+const val MAPPING_STEP = 25
+
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var answer = ""
+    val alphaList = listOf<Char>('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+    val mapping: MutableMap<Char, Char> = mutableMapOf()
+
+    makeMap(alphaList, mapping)
+
+    word.forEach {
+        if (it.isLetter())
+            answer += mapping[it]
+        else if (!it.isLetter())
+            answer += it
+    }
+
+    return answer
+}
+
+// mapping 시키기
+private fun makeMap(alphaList: List<Char>, mapping: MutableMap<Char, Char>){
+    for (i in alphaList.indices) {
+        mapping[alphaList[i]] = alphaList[MAPPING_STEP - i]
+        mapping[alphaList[i] + DIFF_UPPER_LOWER_LETTER] = alphaList[MAPPING_STEP - i] + DIFF_UPPER_LOWER_LETTER
+    }
 }
