217904f61701523c04932121a25a727b5d1e4d92
JunJangE <wnswkd486@gmail.com>
2022-10-28 16:54:50 +0900

    Feat: Problem2 2번 기능 추가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -7,7 +7,10 @@ fun solution3(number: Int): Int {
 
     return 0
 
+    val answer = oneToNumSum(number)
+    return stringCount(answer)
 }
+
 // 1부터 number까지 숫자를 문자열로 더하는 기능
 fun oneToNumSum(num : Int) : String{
     var cnt : String = ""
@@ -16,4 +19,8 @@ fun oneToNumSum(num : Int) : String{
     }
     return cnt
 
+}
+
+fun stringCount(str: String) : Int{
+    return str.count { c -> (c == '3' || c == '6' || c == '9')}
 }
