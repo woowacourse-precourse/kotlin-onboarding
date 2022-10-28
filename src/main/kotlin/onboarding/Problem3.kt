f7277d59a6968980b52aa343b1c474b2dff117fe
dohun_ <silver2679@gmail.com>
2022-10-28 22:06:33 +0900

    Feat Problem3: create fun game

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,17 @@
 package onboarding
 
+fun game(number: Int): Int{
+    var current : Int = number
+    var count : Int = 0
+    for(i in 0..number){
+        current = i
+        if(current % 10 == 3 || current % 10 == 6 || current % 10 == 9){
+            count += 1
+        }
+    }
+    return count
+}
+
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    return game(number)
 }
