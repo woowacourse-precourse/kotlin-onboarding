d621260f023e5930bd98d41ae5a19ec164955118
jjiiin <wjdwlin4051@gmail.com>
2022-10-28 23:05:16 +0900

    feat: Problem1 페이지 계산 후 최댓값 반환 함수 구현
    Problem1.kt 포비와 크롱의 페이지 계산 후 페이지 계산 결과값중 최댓값 반환하는 함수 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,9 +1,39 @@
 package onboarding
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1)
         return -1
 
+    val pobiMax:Int = calculateMax(pobi)
+    val crongMax:Int = calculateMax(crong)
+
+
     return 0
+
+}
+fun calculateMax(pageList:List<Int>): Int{
+
+    val resultList:ArrayList<Int> = arrayListOf()
+    for(page in pageList){
+
+        var pagePlus = 0
+        var pageMultiply = 1
+        var page_temp = page
+
+        while (page_temp != 0){
+
+            pagePlus += page_temp  % 10
+            pageMultiply *= page_temp % 10
+
+            page_temp /= 10
+        }
+        resultList.add(pagePlus)
+        resultList.add(pageMultiply)
+
+    }
+    return Collections.max(resultList)
 
 }
