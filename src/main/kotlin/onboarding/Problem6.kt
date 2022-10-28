5088ef2308bdbf358d9232075f990f9c85a02f4e
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-10-28 23:38:48 +0900

    feat:기능목록 1번 기능 추가[문제6]

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,3 +3,31 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
+fun exception6(forms: List<List<String>>) {
+    if(forms.size < 1 || forms.size > 10000) {
+        throw IllegalArgumentException("크루는 1명이상 10000명 이하여야 합니다.")
+    }
+}
+
+fun exception_name(nickName : String) {
+
+    if(nickName.length < 1 || nickName.length >= 20) {
+        throw IllegalArgumentException("닉네임은 1자 이상 20자 미만이어야 합니다.")
+    }
+
+    if(!nickName.matches("^[가-]*$".toRegex())) {
+        throw IllegalArgumentException("닉네임은 한글만 가능합니다.")
+    }
+}
+
+fun exception_email(email : String) {
+
+    if(email.length < 11 || email.length >= 20) {
+        throw IllegalArgumentException("이메일은 11자 이상 20자 미만이어야 합니다.")
+    }
+
+    if(!email.matches("^([a-zA-Z0-9._%-]+@email.com)*$".toRegex())) {
+        throw IllegalArgumentException("이메일의 도메인은 @email.com이어야 합니다.")
+    }
+}
--- src/test/kotlin/onboarding/ApplicationTest.kt
+++ src/test/kotlin/onboarding/ApplicationTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest {
     @Nested
