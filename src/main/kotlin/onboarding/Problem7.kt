7cf2419a82fbfd40b9b7ac89a38ddb2dd11a2658
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-29 01:11:19 +0900

    feat(Problem7): add find friend function

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -7,6 +7,21 @@ fun countScore( user: String, friends: List<List<String>>, visitors: List<String
     countVisitScore(visitors, score, usersFriend)
 }
 
+//user 의 친구 찾기
+fun findFriend(user: String, friends: List<List<String>>): List<String> {
+    val usersFriend = mutableListOf<String>()
+    for (f in friends) {
+        if (f.contains(user)) {
+            if (f.indexOf(user) == 0) {
+                usersFriend.add(f[1])
+            } else {
+                usersFriend.add(f[0])
+            }
+        }
+    }
+    return usersFriend
+}
+
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
