a12827bed1f18b7610115ac3782f5885ef9bdfa6
Jaeheon Jeong <drdd1120@naver.com>
2022-10-29 01:10:10 +0900

    p7 1. friends 배열을 바탕으로 관계를 hashmap으로 만들어준다.

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -12,5 +12,18 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val hm = makeRelations(friends)
+}
+
+private fun makeRelations(friends: List<List<String>>): HashMap<String, ArrayList<String>> {
+    val hm = HashMap<String, ArrayList<String>>()
+    for (i in friends.indices) {
+        val list1 = hm.getOrDefault(friends[i][0], arrayListOf())
+        val list2 = hm.getOrDefault(friends[i][1], arrayListOf())
+        list1.add(friends[i][1])
+        list2.add(friends[i][0])
+        hm[friends[i][0]] = list1
+        hm[friends[i][1]] = list2
+    }
+    return hm
 }
