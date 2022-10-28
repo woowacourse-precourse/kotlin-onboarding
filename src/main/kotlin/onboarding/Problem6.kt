cac814b85a10bae577f6458b0c02159b447574af
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-10-28 22:30:14 +0900

    Problem6의 연속 문자패턴 확인을 위한 map 수정

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -2,26 +2,37 @@ package onboarding
 
 import java.util.*
 import java.util.regex.Pattern
+import java.util.stream.Collectors
 import kotlin.collections.HashMap
 
+fun solution6CalcPatternCount(forms : List<List<String>>) : HashMap<String, Int>{
+    val nicknamePatternCnt = HashMap<String, Int>()
 
-fun solution6(forms: List<List<String>>): List<String> {
-    val nicknameSequence = HashMap<Char, MutableSet<Char>>()
     forms.forEach { eachForm ->
-        var beforeChar = ' '
-        eachForm[1].forEach { curChar ->
-            if (beforeChar == ' ')
-                beforeChar = curChar
-            else {
-                if (nicknameSequence[beforeChar] == null)
-                    nicknameSequence[beforeChar] = mutableSetOf()
-
-                nicknameSequence[beforeChar]!!.add(curChar)
-                beforeChar=curChar
+        val nickname = eachForm[1]
+        var pattern = ""
+        nickname.forEach { eachChar ->
+            pattern += eachChar
+            if (pattern.length == 2) {
+                if (nicknamePatternCnt.containsKey(pattern))
+                    nicknamePatternCnt[pattern] = nicknamePatternCnt[pattern]!! + 1
+                else
+                    nicknamePatternCnt[pattern] = 1
+                pattern = ""
+                pattern += eachChar
             }
         }
     }
 
+    return nicknamePatternCnt
+}
 
-    return listOf(" ")
+
+
+fun solution6(forms: List<List<String>>): List<String> {
+    val nicknamePatternCount = solution6CalcPatternCount(forms)
+    val alertEmailList=ArrayList<String>()
+
+
+    return alertEmailList.toList().sorted()
 }
