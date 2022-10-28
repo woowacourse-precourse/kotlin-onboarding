faae5a865f8a478e20ff38af444fe0a09f9e2afa
Joonhoo <mnc5@naver.com>
2022-10-28 16:06:10 +0900

    feat(Problem 4): 문제 요구사항 추가

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+/*
+ * 기능 요구 사항
+ * 1. 알파벳 대문자 -> 대문자, 소문자 -> 소문자
+ * 2. 청개구리 사전 -> 모든 문자는 반대로 ex) A- > Z
+ * 문제 풀이
+ * 1. HashMap 으로 청개구리 사전을 대문자와 소문자를 따로 저장
+ * 2. 해당하는 문자를 청개구리 사전에 맞게 변환한다
+ *      - StringBuilder 를 사용하여 문자열을 만든다
+ */
+
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
