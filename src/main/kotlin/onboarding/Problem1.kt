d8b3c13f11c4620caf84c8e135d7f82d0f1fe736
s9hn <tnghqn@naver.com>
2022-10-29 03:54:00 +0900

    [FEAT(미션1)] 각 자릿수의 합을 연산하는 메소드 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,28 @@
 package onboarding
 
+import kotlin.math.max
+
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
+
+fun digitPlus(list: List<Int>): Int {
+    var leftPage = list[0]
+    var rightPage = list[1]
+    var plusLeftResult = 0
+    var plusRightResult = 0
+
+    while (leftPage != 0) {
+        plusLeftResult += leftPage % 10
+        leftPage /= 10
+    }
+
+    while (rightPage != 0) {
+        plusRightResult += rightPage % 10
+        rightPage /= 10
+    }
+
+    return max(plusLeftResult, plusRightResult)
+}
+
