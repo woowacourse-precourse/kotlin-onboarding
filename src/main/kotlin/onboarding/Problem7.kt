2ca8c8ff9d95e1b2ebe3604286f75816f67fbeb4
JunJangE <wnswkd486@gmail.com>
2022-10-28 20:24:15 +0900

    Feat: Problem7 1번 기능 추가

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,17 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+
+    val userFriends = mutableListOf<String>()
+
+    // user와 친구인 사용자를 찾는 기능
+    friends.forEach {
+        when (user) {
+            it[0] -> userFriends.add(it[1])
+            it[1] -> userFriends.add(it[0])
+        }
+    }
+
+    return listOf()
+
 }
