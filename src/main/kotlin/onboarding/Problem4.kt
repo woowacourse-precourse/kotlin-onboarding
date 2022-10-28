35757a791add1e33e8a254b60bc181e33c07e4fc
EmilyCh0 <seoyeon3089@naver.com>
2022-10-28 13:31:19 +0900

    feat(problem4): 각 알파벳과 변환된 알파벳 대응시킨 map 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,13 @@
 package onboarding
 
 fun solution4(word: String): String {
+    val upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val lowerCase = "abcdefghijklmnopqrstuvwxyz"
+    val alphabetMap = mutableMapOf<Char, Char>()
+    for (i in 0 until 26) {
+        alphabetMap[upperCase[i]] = upperCase[25 - i]
+        alphabetMap[lowerCase[i]] = lowerCase[25 - i]
+    }
+
     TODO("프로그램 구현")
 }
