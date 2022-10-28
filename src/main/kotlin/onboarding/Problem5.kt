6ee3abe83d2db13e2ec06010f366860d59871b95
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-29 01:09:02 +0900

    feat(Problem5): add convert money function

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -2,4 +2,16 @@ package onboarding
 
 fun solution5(money: Int): List<Int> {
     TODO("프로그램 구현")
+fun convertMoney(x: Int, moneyList: List<Int>): List<Int> {
+
+    val result = MutableList<Int>(9) { 0 }
+    var target = x
+
+    for ((index, money) in moneyList.withIndex()) {
+        if (target == 0) break
+        val mok = target / money
+        result[index] = mok
+        target -= mok * money
+    }
+    return result
 }
