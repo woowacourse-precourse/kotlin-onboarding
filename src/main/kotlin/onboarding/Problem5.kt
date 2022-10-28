0c9b0b7642876612f1db85ef47cdc6766c8fcfe1
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-10-28 14:30:25 +0900

    solve : Problem5

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,21 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val monetaryUnit = arrayListOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    var change = arrayListOf(0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+    var m = money
+
+    for (i in 0 until monetaryUnit.size) {
+        change[i] =  m / monetaryUnit[i]
+        m %= monetaryUnit[i]
+    }
+
+    return change
+}
+
+fun main() {
+    println(solution5(50237))
+    println(solution5(15000))
+
 }
