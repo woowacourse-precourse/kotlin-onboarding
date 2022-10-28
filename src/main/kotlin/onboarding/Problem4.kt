de3c9449d5e03684a48dae09b6dc81433846b16f
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-29 01:08:14 +0900

    feat(Problem4): add change Char function

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,3 +3,16 @@ package onboarding
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
+
+fun change(x: Char): Char {
+    val intX = x.code
+    return if ((intX >= 65) && (intX <= 90)) {
+        val gap = intX - 65
+        (90 - gap).toChar()
+    } else if ((intX >= 97) && (intX <= 122)) {
+        val gap = intX - 97
+        (122 - gap).toChar()
+    } else {
+        x
+    }
+}
