0e0698061711b92478771793b174fbded064a9d8
Gloria <ippnsj@gmail.com>
2022-10-28 16:38:45 +0900

    feat(Problem2/RangeVerifier): 암호문의 길이가 1 이상 1000 이하인지 확인

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+import problem1.RangeVerifier
+import java.lang.RuntimeException
+
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    val minLength = 1
+    val maxLength = 1000
+    val rangeVerifier = RangeVerifier(cryptogram.length, minLength, maxLength)
+    if(!rangeVerifier.verify()) {
+        throw RuntimeException("Input length is not between range $minLength and $maxLength")
+    }
+
+    return ""
 }
