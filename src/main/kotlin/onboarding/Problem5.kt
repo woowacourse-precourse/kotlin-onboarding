9527f426aa9f20e390d56639f10cce1623ff81b4
dltkd1395 <dltkd13956@naver.com>
2022-10-28 14:11:28 +0900

    feat : [PROBLEM5] 결과값 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -3,7 +3,8 @@ package onboarding
 val moneyList = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val result = mutableListOf(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    return  calculateMoney(0, money, result)
 }
 
 fun calculateMoney(index : Int, money : Int, result : MutableList<Int>) : List<Int> {
