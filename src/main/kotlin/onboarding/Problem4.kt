5c2d7b5ce6b37adf73ff8a7dbdc89b5249b31b94
zqwz <zqwz@naver.com>
2022-10-28 22:50:01 +0900

    [4번 문제] 소문자 청개구리 사전을 만드는 기능, 대문자 청개구리 사전을 만드는 기능, 알파벳인지 확인하여 반환하는 기능 추가

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,43 @@
 package onboarding
 
+import java.lang.StringBuilder
+
+const val OTHER = 0
+const val LOWER = 1
+const val UPPER = 2
+fun createLowerDictionary() : Map<Char, Char>{
+    val lowerDictionary = mutableMapOf<Char, Char>()
+    for(i in 0 .. 25){
+        lowerDictionary['a' + i] = ('z' - i)
+    }
+    return lowerDictionary
+}
+fun createUpperDictionary() : Map<Char,Char>{
+    val upperDictionary = mutableMapOf<Char, Char>()
+    for(i in 0 .. 25){
+        upperDictionary['A' + i] = ('Z' - i)
+    }
+    return upperDictionary
+}
+fun checkAlphabet(alphabet : Char) : Int{
+    when {
+        alphabet.isLowerCase() -> return LOWER
+        alphabet.isUpperCase() -> return UPPER
+    }
+    return OTHER
+}
+
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    val lowerDictionary = createLowerDictionary()
+    val upperDictionary = createUpperDictionary()
+    val result = StringBuilder("")
+
+    for(alphabet in word){
+        when(checkAlphabet(alphabet)){
+            LOWER -> result.append(lowerDictionary[alphabet].toString())
+            UPPER -> result.append(upperDictionary[alphabet].toString())
+            OTHER -> result.append(" ")
+        }
+    }
+    return result.toString()
 }
