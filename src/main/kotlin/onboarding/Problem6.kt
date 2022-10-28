71b803b7250da05bf1ab594a40fff7cbd0085b87
EmilyCh0 <seoyeon3089@naver.com>
2022-10-28 23:03:57 +0900

    feat(problem6): 모든 두 글자의 개수 구하는 기능 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val twoLetterCnt = mutableMapOf<String, Int>()
+    for (form in forms) {
+        val nickName = form[1]
+        val set = mutableSetOf<String>()
+        for (i in 0 until nickName.length - 1) {
+            set.add(nickName.substring(i, i + 2))
+        }
+        set.forEach { twoLetter ->
+            if (twoLetterCnt.contains(twoLetter)) twoLetterCnt[twoLetter] = twoLetterCnt[twoLetter]?.plus(1)!!
+            else twoLetterCnt[twoLetter] = 1
+        }
+    }
+    TODO()
 }
