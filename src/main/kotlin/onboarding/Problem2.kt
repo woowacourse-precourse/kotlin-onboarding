6aa03ad738a2a1b1762b225ae2d564a6aa7e1773
ChoHJin <cjc9739@naver.com>
2022-10-29 03:26:01 +0900

    Problem3 solved

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,21 +1,12 @@
 /*
-문제 2 :
-    암호문을 좋아하는 괴짜 개발자 브라운이 이번에는 중복 문자를 이용한 새로운 암호를 만들었다. 예를 들어 "browoanoommnaon"이라는 암호문은 다음과 같은 순서로 해독할 수 있다.
-    "browoanoommnaon"
-    "browoannaon"
-    "browoaaon"
-    "browoon"
-    "brown"
-    임의의 문자열 cryptogram이 매개변수로 주어질 때, 연속하는 중복 문자들을 삭제한 결과를 return 하도록 solution 함수를 완성하라.
-
-제한사항
-    cryptogram은 길이가 1 이상 1000 이하인 문자열이다.
-    cryptogram은 알파벳 소문자로만 이루어져 있다.
-* */
+기능 목록 :
+1. 중복 제거
+1-1. 파라미터로 받은 문자열의 문자들을 검사해 i번째 문자와 i+1번째 문자가 같다면 두 문자 모두 제거한다.
+1-2. 1-1과정을 i와 i+1 문자가 같지 않을 때까지 반복한다.
+*/
 
 package onboarding
 
 fun solution2(cryptogram: String): String {
-//    TODO("프로그램 구현")
-
+    TODO("프로그램 구현")
 }
