619b1d4afccca6b4ed7742cf779569cafca21341
zqwz <zqwz@naver.com>
2022-10-28 21:31:08 +0900

    [1번 문제] 정적 변수 추가, 페이지 순서가 연속적인지 체크하는 기능, 리스트의 크기가 2인지 확인하는 기능 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -2,7 +2,11 @@ package onboarding
 
 import java.lang.Integer.max
 
-fun getMaxNumber(number : Int) : Int{
+const val EXECPTION = -1
+const val POBI_WIN = 1
+const val CRONG_WIN = 2
+const val DRAW = 0
+fun maxPageNumber(number : Int) : Int{
     var plus = 0
     var multiple = 1
     var temp = number
@@ -15,27 +19,30 @@ fun getMaxNumber(number : Int) : Int{
     }
     return max(plus, multiple)
 }
+fun continuous(pages : List<Int>) : Boolean{
+    return pages[1] - pages[0] == 0
+}
+fun checkLength(pages : List<Int>) : Boolean{
+    return pages.size == 2
+}
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    var pobiLeft : Int = pobi[0]
-    var pobiRight : Int = pobi[1]
-    var pobiResult : Int?
+    val pobiLeft : Int = pobi[0]
+    val pobiRight : Int = pobi[1]
+    val crongLeft : Int = crong[0]
+    val crongRight : Int = crong[1]
 
-    var crongLeft : Int = crong[0]
-    var crongRight : Int = crong[1]
-    var crongResult : Int?
-
-    if(pobiRight - 1 != pobiLeft || crongRight - 1 != crongLeft)
-        return -1
-
-    pobiResult = max(getMaxNumber(pobiLeft), getMaxNumber(pobiRight))
-    crongResult = max(getMaxNumber(crongLeft), getMaxNumber(crongRight))
-
-    if(crongResult > pobiResult){
-        return 2
-    }
-    if(pobiResult > crongResult){
-        return 1
+    if(!continuous(pobi) || !continuous(crong) ||  !checkLength(pobi) || !checkLength(crong)){
+        return EXECPTION
     }
 
-    return 0
+    val pobiResult = max(maxPageNumber(pobiLeft), maxPageNumber(pobiRight))
+    val crongResult = max(maxPageNumber(crongLeft), maxPageNumber(crongRight))
+
+    return when {
+        crongResult > pobiResult -> CRONG_WIN
+        crongResult < pobiResult -> POBI_WIN
+        else -> DRAW
+    }
+
 }
