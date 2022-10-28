a53fb68fc6cac5e8e1a09ada37bd1bbb1b59711e
tmdgh1592 <tmdgh1592@naver.com>
2022-10-28 11:21:39 +0900

    feat: Problem5 solution5() 메서드 로직 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,7 +1,7 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    return getMinimizedCoinList(money)
 }
 
 private fun getMinimizedCoinList(money: Int): List<Int> {
