d45ca242fd83b5eaddfffd03bdc0fcc7d26eb908
dltkd1395 <dltkd13956@naver.com>
2022-10-28 21:46:34 +0900

    feat : [PROBLEM6] 닉네임에서 연속적으로 만들 수 있는 글자 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -2,6 +2,7 @@ package onboarding
 
 val nicknameList = arrayListOf<String>()
 val form = mutableMapOf<String, String>()
+val letterList = arrayListOf<String>()
 
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
@@ -16,5 +17,15 @@ fun separateNickname(index : Int, forms: List<List<String>>) : Map<String, Strin
     form.put(nickname, email)
     nicknameList.add(nickname)
     return separateNickname(index + 1, forms)
+}
+
+fun splitLetters(index : Int) {
+    if (index == nicknameList.size) {
+        return
+    }
+    val nickname = nicknameList[index]
+
+    getDulicatedLetters(0, nickname)
+    return splitLetters(index + 1)
 }
 
