0696fbfd56e1be922ced3d9387803d2d3c636071
zqwz <zqwz@naver.com>
2022-10-28 21:04:45 +0900

    [2번 문제] 중복된 문자가 있는지 판별하는 함수, 중복된 문자를 제거 후 반환해주는 함수 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,38 @@
 package onboarding
 
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+fun checkDuplicate(word: String, start: Int): Int {
+    var nextIndex = start + 1
+    var result = -1
+
+    while (nextIndex < word.length && word[start] == word[nextIndex]){
+        result = nextIndex++
+    }
+
+    return result
+}
+
+fun deleteDuplicate(word: String, start: Int, end: Int): String {
+    return word.substring(0, start) + word.substring(end + 1, word.length)
+}
+
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var index = 0
+    var word = cryptogram
+    var endIndex: Int
+
+    while (index < word.length) {
+        endIndex = checkDuplicate(word, index)
+        if (endIndex == -1) {
+            index++
+        } else {
+            word = deleteDuplicate(word, index, endIndex)
+            index = 0
+        }
+    }
+
+    return word
 }
