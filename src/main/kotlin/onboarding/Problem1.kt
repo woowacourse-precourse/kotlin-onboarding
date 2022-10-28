b4af4f23a2d87ac9df5fc876ba7db23e3a6f474b
dohun_ <silver2679@gmail.com>
2022-10-28 20:37:10 +0900

    Feat Problem1: create fun findMaxNumber, find the sum, multiply of pobi's page and save max value

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -9,21 +9,50 @@ fun exceptions(pobi: List<Int>, crong: List<Int>) {
     if(pobi[0] + 1 != pobi[1] || crong[0] + 1 != crong[1]) {
         isException = -1
         //연속된 숫자가 아니기 때문에 예외 사항
-    }
-    else if(pobimax!! >= 400 || pobimin!! <= 1 || crongmax!! >= 400 || crongmin!! <= 1){
+    } else if(pobimax!! >= 400 || pobimin!! <= 1 || crongmax!! >= 400 || crongmin!! <= 1){
         isException = -1
         //1보다 작거나 400보다 크면 예외 사항
-    }
-    else if(pobi[1] % 2 != 0 || crong[1] % 2 != 0){
+    } else if(pobi[1] % 2 != 0 || crong[1] % 2 != 0){
         isException =  -1
         //오른쪽 페이지가 짝수가 아니면 예외 사항
     }
 }
+fun findMaxNumber(pobi: List<Int>, crong: List<Int>): Int {
+    var pobiMaxValue : Int = 0
+    var pobiSum : Int = 0
+    var pobiMul : Int = 0
+    var temp : Int = 0
+    var MaxValue : Int = 0
 
+    for(i in 0..1){//왼쪽페이지 구하고 오른쪽페이지 계산
+        //pobi 점수 구하기
+        pobiSum = pobi[i].toString().map{ it.toInt() - 48 }.sum()
+        //페이지 합
+        temp = pobi[i]
+        if(temp >= 100){
+            pobiMul = (temp/100) * (temp%100/10) * (temp%10)
+        } else if (temp < 10){
+            pobiMul = temp
+        } else{
+            pobiMul = (temp/10) * (temp%10)
+        }
+        //페이지 곱
+        if(pobiSum > pobiMul){
+            MaxValue = pobiSum
+        } else{
+            MaxValue = pobiMul
+        }
+
+        if(pobiMaxValue < MaxValue){
+            pobiMaxValue = MaxValue
+        }
+    }
+    return 0
+}
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     exceptions(pobi, crong)
     if(isException == -1) {
         return -1
     }
-    return 0
+    return findMaxNumber(pobi, crong)
 }
