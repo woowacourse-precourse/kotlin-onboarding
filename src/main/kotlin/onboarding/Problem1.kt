e1b9ebbb13b0040561d5dd614272eb0802d08b18
healthykim <bsbs8645@snu.ac.kr>
2022-10-29 00:20:03 +0900

    feat(Problem1): Add function for making score from a page number

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,7 @@
 package onboarding
 
+import kotlin.math.max
+
 /*------Custom Variables------*/
 val FIRST_PAGE = 0
 val LAST_PAGE = 400
@@ -15,6 +17,21 @@ fun isValidPages(page: List<Int>) : Boolean {
     if(page[0] % 2 == 0) return false
     if(page[0] == FIRST_PAGE || page[1] == LAST_PAGE) return false
     return true
+}
+
+/**
+ * Returns a biggest Number which can be made from [pageNumber]
+ */
+fun makeBigNumber(pageNumber: Int) : Int {
+    var pageSum: Int = 0
+    var pageMultiply: Int = 1
+    var remainder: Int = pageNumber
+    while(remainder != 0) {
+        pageSum += pageNumber % 10
+        pageMultiply *= pageNumber % 10
+        remainder /= 10
+    }
+    return max(pageSum, pageMultiply)
 }
 
 /*----------------------------*/
