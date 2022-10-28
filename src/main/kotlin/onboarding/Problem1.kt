fae994663c8f91c4c3fb686271a5aa9d604f1620
s9hn <tnghqn@naver.com>
2022-10-29 03:56:11 +0900

    [FEAT(미션1)] 각 자릿수의 곱을 연산하는 메소드 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -23,6 +23,24 @@ fun digitPlus(list: List<Int>): Int {
         rightPage /= 10
     }
 
-    return max(plusLeftResult, plusRightResult)
+    return max(plusLeftResult, plusRightResult) // kotlin max 문법찾기
 }
 
+fun digitMultiple(list: List<Int>): Int {
+    var leftPage = list[0]
+    var rightPage = list[1]
+    var plusLeftResult = 1
+    var plusRightResult = 1
+
+    while (leftPage != 0) {
+        plusLeftResult *= leftPage % 10
+        leftPage /= 10
+    }
+
+    while (rightPage != 0) {
+        plusRightResult *= rightPage % 10
+        rightPage /= 10
+    }
+
+    return max(plusLeftResult, plusRightResult)
+}
