b78eac2a3662eaf7616d6c24678e5e56719e476f
zqwz <zqwz@naver.com>
2022-10-28 23:24:05 +0900

    [5번 문제] 액수별 개수를 구하고 반환하는 기능 추가

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val won = listOf(50000,10000,5000,1000,500,100,50,10,1)
+    val result : ArrayList<Int> = arrayListOf()
+    var temp = money
+
+    for(i in won){
+        result.add(temp % i)
+        temp /= i
+    }
+
+    return result
 }
