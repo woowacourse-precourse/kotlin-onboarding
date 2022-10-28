1809cecafa79fd8ebfa24229e8e3ae187df6b7fc
yeongjin <dudwls901@naver.com>
2022-10-28 21:34:41 +0900

    [Feat] 문자 reverse 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,4 +1,15 @@
 package onboarding
+/*
+* 기능 목록
+* 각 문자 reverse 구현
+* 문자열 전체 reverse 구현
+* */
+
+fun Char.reversed(): Char {
+    val standardChar = if (this.isUpperCase()) 'A' else 'a'
+    return if (this.isLetter()) Char(25 - (this - standardChar) + standardChar.code)
+    else this
+}
 
 fun solution4(word: String): String {
     TODO("프로그램 구현")
