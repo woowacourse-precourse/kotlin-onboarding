d7a87860ae9a8e70f1a6692761ea9147663429a3
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-29 04:48:30 +0900

    문자열 쪼개서 리스트로 변환

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+//1.문자열 쪼개서 리스트 변환
+//2.연속으로 같은 문자가 있는지 확인
+//3.같은 문자가 있을 시 삭제
+//4.같은 문자가 없을 때 까지 반복
 fun solution2(cryptogram: String): String {
+
+
+    var list:MutableList<String> = mutableListOf()
+    list = cryptogram.split("").toMutableList()
+    //문자열 쪼개서 리스트로 변환
+
     TODO("프로그램 구현")
 }
