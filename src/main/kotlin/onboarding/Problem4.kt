841401ef390f40445c7d44af9f6e8aab9890d8cf
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-10-28 11:23:27 +0900

    solve : Problem4

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,34 @@
 package onboarding
 
+import kotlin.math.abs
+
+
+/**
+    대문자
+    65 + 90 = 155
+    소문자
+    97 + 122 = 219
+    공백 = 32
+ */
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var result = ""
+
+    for (i in word.indices) {
+        val c = word[i]
+        // 소문자
+        if (c == ' ')
+            result += " "
+        else if (c.code >= 97) {
+            result += abs(219 - word[i].code).toChar()
+        }else {
+            result += abs(155 - word[i].code).toChar()
+        }
+    }
+
+    return result
 }
+
+fun main() {
+    println(solution4("I love you"))
+}
+
