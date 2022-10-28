414bc0f2821bfce06d567b7b746b2d96bd9ce44f
JunJangE <wnswkd486@gmail.com>
2022-10-28 16:25:59 +0900

    Feat: Problem1 1번 기능 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,20 @@
 package onboarding
 
+import kotlin.math.max
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
+    // 예외 처리
+    if (!exceptionCheck(pobi[0], pobi[1]) || !exceptionCheck(crong[0], crong[1])){
+        return -1
+    }
+    return 0
+}
+
+// 페이지 예외 처리 기능
+fun exceptionCheck(startPageNum : Int, endPageNum : Int) : Boolean{
+    // 왼쪽 페이지가 홀수, 오른쪽 페이지가 짝수인지, 양쪽 페이지 차이가 1인지, 페이지 쪽수가 1부터 400안에 있는지
+    return startPageNum % 2 != 0
+            && endPageNum - startPageNum == 1
+            && startPageNum in 1..400
+            &&endPageNum in 1 ..400
 }
