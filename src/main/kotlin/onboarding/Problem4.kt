e2aa4789448c40a53692242485ea9ed5b5ab05fb
qhfhdls <79788971+qhfhdls@users.noreply.github.com>
2022-10-28 21:03:16 +0900

    Update Problem4.kt
    20221028_2102 problem.tmp

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,20 @@
 package onboarding
 
+// 알파벳 변환 함수
+fun change_alphabet(c: Char): Char{
+    var num = c.code
+    if(num == 32)
+        return 32.toChar()
+    else if(num < 93)
+        return((155-num).toChar())
+    else
+        return((219-num).toChar())
+}
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var size = word.length
+    var f_word: String = ""
+    for(i in 0..size-1)
+        f_word += change_alphabet(word[i])
+    return f_word
+
 }
