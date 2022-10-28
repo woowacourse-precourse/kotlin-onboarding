347004a47f207d27357668cd44353045fd384145
no1msh <no1msh1217@gmail.com>
2022-10-28 09:37:13 +0900

    [Problem1][feat] 2번, 3번 기능 구현
    2번 기능 : 포비나 크롱이 펼친 페이지 두개에 각각 1번 메소드를 실행시키고 max를 통해 그중 큰 값을 변수에 저장한다.
    3번 기능 : pobiScore 와 crongScore를 비교하여 승자를 리턴 [pobi 승 : 1 , crong 승 : 2 , 무승부 : 0 , 예외 상황 -1]

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -7,8 +7,22 @@ import kotlin.math.*
 // 2. 왼쪽 페이지와 오른쪽 페이지 번호 따로 1번 메소드를 실행시켜 나온 값을 비교하고 각각 pobiScore , crongScore 변수에 저장.
 // 3. pobiScore 와 crongScore를 비교하여 승자를 리턴 [pobi 승 : 1 , crong 승 : 2 , 무승부 : 0 , 예외 상황 -1]
 // 4. 예외처리 구현
+// 4-1. 왼쪽 페이지는 홀수 , 오른쪽 페이지는 짝수가 나와야 한다.
+// 4-2. 왼쪽 페이지와 오른쪽 페이지는 1페이지가 차이나야 한다.
+// 4-3. 페이지는 1부터 시작하여 400을 넘지 않아야 한다.
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    // 기능 목록 2번 구현
+    val pobiScore = max(calculatePageNumber(pobi[0]), calculatePageNumber(pobi[1]))
+    val crongScore = max(calculatePageNumber(crong[0]), calculatePageNumber(crong[1]))
+
+    return if (pobiScore > crongScore) {
+        1
+    } else if (pobiScore == crongScore) {
+        0
+    } else {
+        2
+    }
 }
 
 // 기능목록 1번 구현
@@ -17,8 +31,7 @@ fun calculatePageNumber(pageNumber: Int): Int {
     var resultAdd = 0
     var resultMultiply = 1
 
-    for (i: Char in pageNumberString) {
-        resultAdd += Character.getNumericValue(i)
+    for (i: Char in pageNumberString) { resultAdd += Character.getNumericValue(i)
         resultMultiply *= Character.getNumericValue(i)
     }
 
