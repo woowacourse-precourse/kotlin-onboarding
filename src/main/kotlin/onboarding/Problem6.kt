ac99a60ecec8e8fecc7b61fcbf60e4906621f2f7
dltkd1395 <dltkd13956@naver.com>
2022-10-28 21:34:26 +0900

    feat : [PROBLEM6] 닉네임 분리 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,20 @@
 package onboarding
 
+val nicknameList = arrayListOf<String>()
+val form = mutableMapOf<String, String>()
+
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
+fun separateNickname(index : Int, forms: List<List<String>>) : Map<String, String> {
+    if (index == forms.size) {
+        return form
+    }
+    val (email, nickname) = forms[index]
+
+    form.put(nickname, email)
+    nicknameList.add(nickname)
+    return separateNickname(index + 1, forms)
+}
+
