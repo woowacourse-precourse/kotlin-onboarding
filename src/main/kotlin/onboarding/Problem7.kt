bb765e95e52cd6726890265c4d8cea0193c42ce4
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-29 01:10:56 +0900

    feat(Problem7): add count score function

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,6 +1,12 @@
 package onboarding
 
 fun solution7(
+//추천 점수 계산
+fun countScore( user: String, friends: List<List<String>>, visitors: List<String>, usersFriend: List<String>, score: HashMap<String, Int>) {
+    countFriendScore(user, usersFriend, friends, score)
+    countVisitScore(visitors, score, usersFriend)
+}
+
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
