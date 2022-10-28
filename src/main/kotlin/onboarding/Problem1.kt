212a72797c92a299d94126be51fe33da45237bce
bluecat0823 <star4_you@naver.com>
2022-10-28 23:33:02 +0900

    미션1 : 예외사항 체크 및 값 반환

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,22 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
+    if ((pobi[0]%2 != 1) or (pobi[1]%2 != 0)) {
+        return -1
+    } else if ((crong[0]%2 != 1) or (crong[1]%2 != 0)) {
+        return -1
+    } else if (pobi[0]+1 != pobi[1]) {
+        return -1
+    } else if (crong[0]+1 != crong[1]) {
+        return -1
+    } else if ((pobi[0]<=1) or (pobi[1]>=400)) {
+        return -1
+    } else if ((crong[0]<=1) or (crong[1]>=400)) {
+        return -1
+    }
+
+
+
+    return 1
 }
