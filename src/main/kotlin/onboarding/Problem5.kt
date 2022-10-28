1b366e6bd5549a37607f04884a445ec7b7230968
ChunBaee <mljung10@naver.com>
2022-10-29 03:01:56 +0900

    5번] 기능 1) 밑그림 : 특정 금액으로 필터링을 진행하므로, 필터링을 위한 금액 목록 전역변수 추가 (변경)

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,10 +1,8 @@
 package onboarding
 
+private val priceList = mutableListOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
 fun solution5(money: Int): List<Int> {
+    val resultList = mutableListOf<Int>()
 
 }
 
-fun returnCurrentFilter(currentLoop : Int) : Int {
-    val priceList = mutableListOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
-    return priceList[currentLoop]
-}
