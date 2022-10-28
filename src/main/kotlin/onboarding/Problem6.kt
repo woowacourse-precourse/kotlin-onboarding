e5ed2960bdf07274ae1292d1e25144f447af5d5e
yulim <yulim13sky@naver.com>
2022-10-29 01:34:22 +0900

    [FEAT] Problem6 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,18 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val result = mutableSetOf<String>()
+
+    for (indexList in forms.indices) {
+        val name = forms[indexList][1]
+        val email = forms[indexList][0]
+        for (nameIndex in 0 until name.length - 1) {
+            for (index in forms.indices) {
+                if (forms[index][1].contains(name.substring(nameIndex, nameIndex + 2)) && indexList != index) {
+                    result.add(email)
+                }
+            }
+        }
+    }
+    return result.toMutableList().sorted()
 }
