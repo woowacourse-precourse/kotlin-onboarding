d491af893e00b488d03a45307e2aa3347eacfcfe
JunJangE <wnswkd486@gmail.com>
2022-10-28 16:50:38 +0900

    Feat: Problem3 1번 기능 추가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,19 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var answer : String = ""
+
+    answer = oneToNumSum(number)
+
+    return 0
+
+}
+// 1부터 number까지 숫자를 문자열로 더하는 기능
+fun oneToNumSum(num : Int) : String{
+    var cnt : String = ""
+    for (i in 1 .. num){
+        cnt += i.toString()
+    }
+    return cnt
+
 }
