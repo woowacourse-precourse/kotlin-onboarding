8e68eafe5e316a033a6008ca28af43c602e89aa7
ChunBaee <mljung10@naver.com>
2022-10-29 03:05:10 +0900

    5번] 기능 2) 체크 : 특정 금액으로 필터링을 진행 후 몫은 몇 번 바꿀 수 있는지, 나머지는 현재 금액으로 바꿀 수 없는 금액이므로 재반환

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -3,6 +3,13 @@ package onboarding
 private val priceList = mutableListOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
 fun solution5(money: Int): List<Int> {
     val resultList = mutableListOf<Int>()
+    var mMoney = money // 파라미터로 들어오는 변수는 val 이므로, 루프마다 값을 변경해주기 위해 var로 재선언했다.
+    for(i in priceList) {
+        checkHowMuch(money, i)
+    }
+}
 
+private fun checkHowMuch(price : Int, priceList : Int) : List<Int> {
+    return listOf(price / priceList, price % priceList)
 }
 
