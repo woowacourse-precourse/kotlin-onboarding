0361969c4a31d4bee06efe8a1288c823761fb267
ChunBaee <mljung10@naver.com>
2022-10-29 03:07:20 +0900

    5번] 기능 3) 구현 : 계산해주는 함수가 몫과 나머지 모두를 리턴해야 하므로 List 변수를 하나 더 만들어준 후 루프에 활용했다.

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -5,7 +5,9 @@ fun solution5(money: Int): List<Int> {
     val resultList = mutableListOf<Int>()
     var mMoney = money // 파라미터로 들어오는 변수는 val 이므로, 루프마다 값을 변경해주기 위해 var로 재선언했다.
     for(i in priceList) {
-        checkHowMuch(money, i)
+        val mList = checkHowMuch(mMoney, i)
+        resultList.add(resultList[0])
+        mMoney = mList[1]
     }
 }
 
