1f51720111a80555647a8df80663746e242b00a2
tmdgh1592 <tmdgh1592@naver.com>
2022-10-28 11:19:34 +0900

    feat: 주어진 금액을 가능한 큰 단위의 화폐로 나누어 단위별 화폐 개수를 최소화한 리스트를 반환하는 메서드 구현 - getMinimizedCoinList() 메서드 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -3,3 +3,16 @@ package onboarding
 fun solution5(money: Int): List<Int> {
     TODO("프로그램 구현")
 }
+
+private fun getMinimizedCoinList(money: Int): List<Int> {
+    var currentMoney = money
+    val coinTypeList = listOf(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1)
+    val minimizedCoinList = mutableListOf<Int>()
+
+    coinTypeList.forEach { coinType ->
+        minimizedCoinList.add(currentMoney / coinType)
+        currentMoney %= coinType
+    }
+
+    return minimizedCoinList
+}
