9fb0f392eacbabc16d19c7ebc95657553ebfe04b
JunJangE <wnswkd486@gmail.com>
2022-10-28 18:38:48 +0900

    Refactor: Problem2 1번 기능 리팩토링

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,17 +1,13 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    return removeString(cryptogram)
-}
 
-// 연속되는 중복 문자들을 삭제하는 기능
-fun removeString(str: String) : String{
-    var removeStr = str
+    var answer = cryptogram
 
-    // 똑같은 문자가 2개 이상 나오면 빈 문자열로 변환
-    removeStr.forEach {
-        removeStr = str.replace("$it{2,}".toRegex(), "")
+    // 연속되는 중복 문자들을 삭제하는 기능 
+    answer.forEach {
+        answer = answer.replace("$it{2,}".toRegex(), "")
     }
 
-    return removeStr
+    return answer
 }
