9561304d06358531bd04ee031bb095401494081c
jjiiin <wjdwlin4051@gmail.com>
2022-10-29 00:07:51 +0900

    feat: Problem5 계좌에 있는 돈 현금 인출하는 코드 구현
    Problem5.kt 제일 큰 수의 현금부터 계산해서 결과값에 넣고 계좌에 있는 돈은 계속 줄어드는 코드 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,15 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val result = ArrayList<Int>()
+    var moneyInAccount=money
+    val cashList :ArrayList <Int> = arrayListOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+
+    for (cash in cashList){
+
+        result.add(moneyInAccount/cash)
+        moneyInAccount -= cash * (moneyInAccount/cash)
+
+    }
+    return result
 }
