7540a0cf8908a26cc67de2552832e62cafa90270
EmilyCh0 <seoyeon3089@naver.com>
2022-10-28 11:27:41 +0900

    feat(problem3): 숫자 하나의 369 개수 구하는 기능

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+fun count369(num: Int): Int {
+    var tmp = num
+    var cnt = 0
+    while (tmp > 0) {
+        if (tmp % 10 == 3 || tmp % 10 == 6 || tmp % 10 == 9) cnt++
+        tmp /= 10
+    }
+    return cnt
+}
+
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
