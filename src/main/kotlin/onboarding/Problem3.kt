230db5cea9174b5ea7e1173c059651454c5a7847
Gloria <ippnsj@gmail.com>
2022-10-28 22:39:14 +0900

    feat(Problem3/RangeVerifier): 입력값이 1 이상 10000 이하인지 확인

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+import problem1.RangeVerifier
+import java.lang.RuntimeException
+
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    val minRange = 1
+    val maxRange = 10000
+    val rangeVerifier = RangeVerifier(number, minRange, maxRange)
+    if(!rangeVerifier.verify()) {
+        throw RuntimeException("Input length is not between range $minRange and $maxRange")
+    }
+
+    return 0
 }
