1b55aa5dfb2e2046f6e060b80f512fbc2de28b67
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-10-29 04:12:55 +0900

    Problem5 dp로 푸는 과정을 함수로 분리

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,13 +1,7 @@
 package onboarding
 
-import java.lang.Math.min
-
-fun solution5(money: Int): List<Int> {
-    val coinToUse=arrayOf(1, 10, 50, 100, 500, 1000, 5000, 10000, 50000)
-    val coinCnt=Array(money+1){Array(9){0}}
-    val cache=Array(money+1){Int.MAX_VALUE}
+fun solution5dynamicProgramming(coinToUse:Array<Int>, coinCnt:Array<Array<Int>>, cache:Array<Int>, money:Int){
     cache[0]=0
-
     coinToUse.forEachIndexed { coinIndex, curCoin ->
         for(paidMoney in curCoin..money){
             if(cache[paidMoney]>cache[paidMoney-curCoin]+1){
@@ -17,6 +11,14 @@ fun solution5(money: Int): List<Int> {
             }
         }
     }
+}
+
+fun solution5(money: Int): List<Int> {
+    val coinToUse=arrayOf(1, 10, 50, 100, 500, 1000, 5000, 10000, 50000)
+    val coinCnt=Array(money+1){Array(9){0}}
+    val cache=Array(money+1){Int.MAX_VALUE}
+
+    solution5dynamicProgramming(coinToUse, coinCnt, cache, money)
 
     return coinCnt[money].reversed().toList()
 }
