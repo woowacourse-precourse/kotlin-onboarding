c700fc2ffcde5c21a2a98c7237190fc016b39d93
healthykim <bsbs8645@snu.ac.kr>
2022-10-28 23:59:10 +0900

    feat(Problem1): Add function for page validity check

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,25 @@
 package onboarding
 
+/*------Custom Variables------*/
+val FIRST_PAGE = 0
+val LAST_PAGE = 400
+/*----------------------------*/
+
+/*------Custom Functions------*/
+/**
+ * Returns whether the [page] is valid.
+ */
+fun isValidPages(page: List<Int>) : Boolean {
+    if(page.size != 2) return false
+    if(page[0] - page[1] != 1) return false
+    if(page[0] % 2 == 0) return false
+    if(page[0] == FIRST_PAGE || page[1] == LAST_PAGE) return false
+    return true
+}
+
+/*----------------------------*/
+
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
