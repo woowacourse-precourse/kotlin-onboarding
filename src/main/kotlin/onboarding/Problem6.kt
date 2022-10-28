8f4b101f28d68dbfdf76721d9e34b550ee95bdd2
Jaeheon Jeong <drdd1120@naver.com>
2022-10-28 22:38:26 +0900

    p6 요구사항 작성

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,12 @@
 package onboarding
-
+/*
+1. 이름에서 2글자로 조합할 수 있는 연속된 문자열들을 사전으로 만든다
+사전은 hashmap으로 문자열, 등장횟수로 구성되어 있다.
+제이제이와 같이 같은 문자열이 반복해서 등장할 경우 등장횟수를 1로 한다
+2. 등장횟수가 2 이상인 것들만 사전에서 가져와 forms에 있는 이름들과 비교한다
+3. 조건에 맞는 것들을 answer에 담아준 후 sort한다.
+*/
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
