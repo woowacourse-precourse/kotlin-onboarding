cec88ecdd03df4d7bcfad328ad79420f6c16481c
ChunBaee <mljung10@naver.com>
2022-10-28 22:52:13 +0900

    5번] 기능 1) 밑그림 : 특정 금액으로 필터링을 진행하므로, 원하는 시기에 특정 필터링을 진행할 수 있도록 필터 함수 추가

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -7,7 +7,7 @@ const val CAPITAL_END = 90
 const val SMALL_START = 97
 const val SMALL_END = 122
 fun solution4(word: String): String {
-    var mList = word.split("").filter { it != "" } // 문자를 각 문자들을 체크하기 용이하도록 "" 기준, 즉 한 글자마다 문자열 배열로 변환한다. 이렇게 하면 맨 앞과 뒤에 공백을 가지는 빈 배열칸이 생성되므로, filter를 통해 없애준다.
+    val mList = word.split("").filter { it != "" } // 문자를 각 문자들을 체크하기 용이하도록 "" 기준, 즉 한 글자마다 문자열 배열로 변환한다. 이렇게 하면 맨 앞과 뒤에 공백을 가지는 빈 배열칸이 생성되므로, filter를 통해 없애준다.
     return checkItsLetter(mList)
 }
 
@@ -22,7 +22,7 @@ private fun convertToChar(word: String): Char {
  * 각 문자별로 공백인지, 소문자인지, 대문자인지 Validate 해주는 함수
  */
 private fun checkItsLetter(list: List<String>) : String{
-    var concat = mutableListOf<Char>() // 변환 후 새롭게 변경되는 문자들이 담겨질 배열
+    val concat = mutableListOf<Char>() // 변환 후 새롭게 변경되는 문자들이 담겨질 배열
     for (i in list) {
         if (convertToChar(i).code == EMPTY_CODE) {
             concat.add(convertEmptyLetter(convertToChar(i).code))
