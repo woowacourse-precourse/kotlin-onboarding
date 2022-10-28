9a2191f772b2dd4177d402c5155704266e519193
inseonyun <yuninseon@naver.com>
2022-10-28 16:53:30 +0900

    [Problem6] 모든 유저 닉네임 하나로 담는 set 함수 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,20 @@
 package onboarding
 
+fun setUserNickname(forms: List<List<String>>): String {
+    var result = ""
+
+    for(i in forms.indices - 1) {
+        result += forms[i][1] + "_"
+    }
+    result += forms[forms.size - 1][1]
+
+    return result
+}
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    var result = mutableListOf<String>()
+
+    val row = setUserNickname(forms)
+
+    return result.toList()
 }
