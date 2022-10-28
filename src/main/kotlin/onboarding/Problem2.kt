7710a9de545b8287e9c1b89f4a16ba0a8b979272
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-10-28 18:01:46 +0900

    중복문자가 나올 때와 아닐 경우를 나누어 스택에 푸시

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -9,5 +9,29 @@ fun solution2(cryptogram: String): String {
     var st = Stack<Char>()
     var answer = ""
 
+    for(i in 0 until cryptogram.length) {
+
+        if(st.isEmpty()){
+            st.push(cryptogram[i])
+        }
+
+        if (st.isNotEmpty() && i >= 1) {
+            if (st.peek() == cryptogram[i]) {
+                //println(st.peek() + " " + cryptogram[i].toString())
+                st.pop()
+                continue
+            }
+
+            if (st.peek() != cryptogram[i]) {
+                st.push(cryptogram[i])
+                //print(cryptogram[i])
+            }
+        }
+
+
+    }
+
+
+    
 
 }
