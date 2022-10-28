a904a6b42d68e28870c46a25afbd9c904ee7bdff
yulim <yulim13sky@naver.com>
2022-10-29 00:21:12 +0900

    [FEAT] Problem2 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,38 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var parameter = cryptogram
+    var answer = parameter[0].toString()
+    var continuousCnt = 1
+    var totalCnt = 0
+    var index = 1
+
+    while (true) {
+        if (parameter[index] == parameter[index - 1]) {
+            continuousCnt++ // 몇 개 중복인지
+            totalCnt++ // 몇 번 중복인지
+        } else if (parameter[index] != parameter[index - 1] && continuousCnt != 1) {
+            answer = answer.substring(0, answer.length - continuousCnt)
+            continuousCnt = 1
+        }
+        answer += parameter[index]
+
+        if (index == parameter.length - 1) {
+            // 더 이상 연속된 값이 없는 경우 반복문 빠져나가기
+            if (totalCnt == 0) break
+            // 전부 다 연속된 값인 경우 ""로 반복문 빠져나가기
+            if (continuousCnt == parameter.length) {
+                answer = ""
+                break
+            }
+            // 전부 다 돈 경우 reset 시키고 다시 반복
+            index = 1
+            continuousCnt = 1
+            parameter = answer
+            totalCnt = 0
+            answer = parameter[0].toString()
+        } else index++
+    }
+
+    return answer
 }
