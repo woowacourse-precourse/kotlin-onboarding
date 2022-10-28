49551c283956e049fed60fa47e97d0f05805f30f
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-10-28 10:24:18 +0900

    solve : Problem2

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,40 @@
 package onboarding
 
+
+/**
+ *  루프를 돌 때, 현재 숫자와 그 다음 숫자가
+ */
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var result = cryptogram
+    var temp = ""
+    var flag = false
+
+    while (result.isNotEmpty()) {
+        val idx = result.length-1
+        var changed = false
+        temp = ""
+        for (i in result.indices) {
+            if (flag) {
+                flag = false
+                continue
+            }
+            if (i < idx && result[i] == result [i+1]) {
+                flag = true
+                changed = true
+            } else {
+                temp += result[i]
+                flag = false
+            }
+        }
+        result = temp
+
+        if (!changed) break
+    }
+
+    return result
+}
+
+fun main() {
+    println(solution2("browoanoommnaon"))
+    solution2("zyelleyz")
 }
