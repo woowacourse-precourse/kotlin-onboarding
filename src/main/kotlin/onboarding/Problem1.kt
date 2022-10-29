a2b790c70fac0888e5f7794245cb5b0f9e1e224b
sujin-kk <sjoo0002@gmail.com>
2022-10-30 02:36:38 +0900

    [FEAT] 주요 값 상수화

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,17 @@
 package onboarding
 
+import java.lang.Integer.max
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
 }
+
+
+
+
+const val START_PAGE_NUM = 1
+const val LAST_PAGE_NUM = 400
+const val POBI_WIN_RESULT = 1
+const val CRONG_WIN_RESULT = 2
+const val TIE_RESULT = 0
+const val EXCEPTION_RESULT = -1
