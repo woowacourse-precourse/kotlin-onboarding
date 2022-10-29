8ba92bc7167088d89723a7c3b323f977564df4c7
sujin-kk <sjoo0002@gmail.com>
2022-10-30 08:05:37 +0900

    [CHORE] 화폐 금액 상수화

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -3,3 +3,14 @@ package onboarding
 fun solution5(money: Int): List<Int> {
     TODO("프로그램 구현")
 }
+
+const val FIFTY_THOUS = 50000
+const val TEN_THOUS = 10000
+const val FIVE_THOUS = 5000
+const val ONE_THOUS = 1000
+const val FINE_HUND = 500
+const val ONE_HUND = 100
+const val FIFTY = 50
+const val TEN = 10
+const val ONE = 1
+
