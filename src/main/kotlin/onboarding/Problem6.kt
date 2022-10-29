2f0f2aea0beda89b0c0bb16d409b5bdb9bd8489e
yeongjin <dudwls901@naver.com>
2022-10-29 11:37:07 +0900

    [Feat] 연속 문자열 이메일 셋 저장

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,4 +1,25 @@
 package onboarding
+/* 기능 목록
+* hashmap을 이용하여 연속 문자열마다 이메일셋 저장
+* 제한 닉네임 추출 및 정렬
+* */
+fun getWordMap(forms: List<List<String>>): Map<String,Set<String>>{
+    val wordMap = mutableMapOf<String,MutableSet<String>>()
+    for((email, name) in forms){
+        for(s in name.indices){
+            val word = StringBuilder()
+            for(i in s until name.length){
+                word.append(name[i])
+                if(word.length>=2){
+                    val wordStr = word.toString()
+                    if(wordMap[wordStr]==null) wordMap[wordStr] = mutableSetOf()
+                    wordMap[wordStr]?.add(email)
+                }
+            }
+        }
+    }
+    return wordMap
+}
 
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
