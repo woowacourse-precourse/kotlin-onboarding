11fc46c0444ccd4955b560b99608f753c6a0bc10
zqwz <zqwz@naver.com>
2022-10-29 16:51:34 +0900

    [6번 문제] 닉네임의 길이가 부합하는지 확인하는 기능

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -2,7 +2,9 @@ package onboarding
 fun emailLength(email: String): Boolean {
     return email.length in 11 until 20
 }
-
+fun nickNameLength(nickname: String): Boolean {
+    return nickname.length in 1 until 20
+}
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
