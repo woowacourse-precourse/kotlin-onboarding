e4afa82fe375d8f6f5cfc3262d4f73d1003e7273
hansh0101 <hansh0101@naver.com>
2022-10-29 15:29:34 +0900

    [REFACTOR] Refactor Problem1

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,21 +1,22 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    if (hasExceptions(pobi, crong)) {
+    if (checkExceptions(pobi, crong)) {
         return -1
     }
 
-    val pobiScore = calculateMaxPoints(pobi)
-    val crongScore = calculateMaxPoints(crong)
+    val pobiScore = calculateMaxPoint(pobi[0], pobi[1])
+    val crongScore = calculateMaxPoint(crong[0], crong[1])
 
     return when {
-        pobiScore > crongScore -> 1
         pobiScore == crongScore -> 0
-        else -> 2
+        pobiScore > crongScore -> 1
+        pobiScore < crongScore -> 2
+        else -> -1
     }
 }
 
-private fun hasExceptions(pobi: List<Int>, crong: List<Int>): Boolean {
+private fun checkExceptions(pobi: List<Int>, crong: List<Int>): Boolean {
     return !(checkValidateRange(pobi[0]) &&
             checkValidateRange(pobi[1]) &&
             checkValidateRange(crong[0]) &&
@@ -30,32 +31,31 @@ private fun checkValidateRange(page: Int): Boolean =
 private fun checkValidatePage(leftPage: Int, rightPage: Int): Boolean =
     leftPage + 1 == rightPage && leftPage % 2 == 1 && rightPage % 2 == 0
 
-private fun calculateMaxPoints(list: List<Int>): Int {
-    var left = list[0]
-    var right = list[1]
 
-    val resultList: MutableList<Int> = mutableListOf<Int>()
+private fun calculatePagePoint(page: Int): Int {
+    var additionResult = 0
+    var multiplicationResult = 1
 
-    var leftPlusResult = 0
-    var leftMultiplyResult = 1
-    while (left != 0) {
-        leftPlusResult += left % 10
-        leftMultiplyResult *= left % 10
-        left /= 10
+    var remainDigits = page
+    while (remainDigits != 0) {
+        additionResult += remainDigits % 10
+        multiplicationResult *= remainDigits % 10
+        remainDigits /= 10
     }
-    resultList.add(leftPlusResult)
-    resultList.add(leftMultiplyResult)
 
-    var rightPlusResult = 0
-    var rightMultiplyResult = 1
-    while (right != 0) {
-        rightPlusResult += right % 10
-        rightMultiplyResult *= right % 10
-        right /= 10
+    return if (additionResult >= multiplicationResult) {
+        additionResult
+    } else {
+        multiplicationResult
     }
-    resultList.add(rightPlusResult)
-    resultList.add(rightMultiplyResult)
+}
 
-    resultList.sortDescending()
-    return resultList[0]
+private fun calculateMaxPoint(leftPage: Int, rightPage: Int): Int {
+    val leftPageScore = calculatePagePoint(leftPage)
+    val rightPageScore = calculatePagePoint(rightPage)
+    return if (leftPageScore >= rightPageScore) {
+        leftPageScore
+    } else {
+        rightPageScore
+    }
 }
