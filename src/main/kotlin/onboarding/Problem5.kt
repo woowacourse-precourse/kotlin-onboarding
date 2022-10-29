ce3e3a98bc21ec1a96c67e57bbdebe218eaa8a31
Doyeop Kim <dygames@naver.com>
2022-10-29 20:28:36 +0900

    FEATURE: 화폐 단위에 맞게 출금

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -6,6 +6,13 @@ package onboarding
 - 출금된 단위에 맞게 배열에 저장
 */
 
+fun withdrawMoney(money: Int, unit: Int): Int {
+    return if (money >= unit)
+        money % unit
+    else
+        -1
+}
+
 fun solution5(money: Int): List<Int> {
     TODO("프로그램 구현")
 }
