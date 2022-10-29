d348dd02be4ed3c88ca075daaab8cb85a566c512
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-10-29 13:43:03 +0900

    Refactor: else 구문 수정, 주석 수정 in Problem2.kt
    while문 내 조건문 진행 후 left, right를 초기화한 뒤 바로 증가되는 문제 때문에 else를 사용했었는데 continue 추가하여 else를 삭제할 수 있었음
    기능목록에 맞게 주석 수정

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -10,7 +10,7 @@ fun solution2(cryptogram: String): String {
      * */
 
     /**
-     * 1. 문자열의 위치를 가리킬 포인터
+     * 1. 문자열의 위치를 가리킬 포인터 2개 선언
      * */
     var left = 0
     var right = 1
@@ -18,19 +18,19 @@ fun solution2(cryptogram: String): String {
     var answer = cryptogram
 
     /**
-     * 2. 두 포인터가 가르키는 문자가 같은 문자일 경우 substring으로 잘라냄
-     *    만약 다를 경우 포인터를 한 칸씩 우측으로 이동
-     *    right가 자르고 남은 문자열 길이보다 작을 경우까지만 반복
+     * 2. 두 포인터을 우측으로 한 칸씩 옮김
+     *    두 포인터가 가리키는 문자가 같을 경우 substring으로 같은 부분을 잘라내고 포인터를 최초 위치로 초기화
+     *    같은 문자가 없어질 때까지 반복
      */
     while (right < answer.length){
         if (answer[left] == answer[right]){
             answer = answer.substring(0, left) + answer.substring(right + 1, answer.length)
             left = 0
             right = 1
-        }else{
-            left += 1
-            right += 1
+            continue
         }
+        left += 1
+        right += 1
     }
     return answer
 }
