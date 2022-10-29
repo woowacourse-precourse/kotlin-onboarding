e467cc03b4ec34f4282bf4ea5376841db00a99ca
Doyeop Kim <dygames@naver.com>
2022-10-29 15:10:21 +0900

    DOCS: 2번 문제 기능 목록 정의

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,16 @@
 package onboarding
 
+/*
+기능 목록
+- 중복된 문자열 탐색
+    - 이분 탐색
+    - 중복된 문자열의 시작 인덱스와 길이 반환
+- 중복된 문자열 삭제
+    - 탐색 결과를 통해 문자열 삭제와 재탐색 지시
+    - 처리 완료된 문자열 반환
+*/
+
+
 fun solution2(cryptogram: String): String {
     TODO("프로그램 구현")
 }
