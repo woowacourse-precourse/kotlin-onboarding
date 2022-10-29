a537d97a21d5cbd3495af0888de23f2420d7a853
cyjadela <adelacho@naver.com>
2022-10-29 19:37:10 +0900

    feat: [문제1] getPageMax 함수 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,26 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    var pobiMax: Int = 0
+    var crongMax: Int = 0
+
+    fun getPageMax(page: Int): Int {
+        var plusSum: Int = 0
+        var mutlipleSum: Int = 1
+        var page: Int = page
+
+        while (page != 0) {
+            plusSum = plusSum + (page % 10)
+            mutlipleSum = mutlipleSum * (page % 10)
+            page /= 10
+        }
+
+        if (plusSum < mutlipleSum) {
+            return mutlipleSum
+        }
+        else
+            return plusSum
+    }
+
+    return 0
 }
