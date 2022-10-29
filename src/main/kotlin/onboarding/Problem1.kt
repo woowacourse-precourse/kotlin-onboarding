dd52d049e8006c4780acbbbb41184da2baa06c3b
윤동현 <donghyunyoon81@gmail.com>
2022-10-30 01:42:57 +0900

    Feat:Get pobi's score 왼쪽과 오른쪽 페이지의 자릿수를 더하거나 곱하여 더 높은 수를 얻기위해서 각 자릿수를 인트 타입의 목록으로 만듭니다. List의 sum() 함수는 자릿수를 더한 값을 구하는 데 사용하고, for 문과 복합 대입 연산자를 사용하여 자릿수를 곱한 값을 구했습니다.  for 문을 통해 더 높은 값을 얻기 위해서 왼쪽과 오른쪽 중에 높은 값을 목록에 추가하였습니다. 목록에서 최대값을 추출하여 포빈의 점수를 얻습니다.

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,29 @@
 package onboarding
 
-fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+fun solution1(pobi: List<Int>,crong:List<Int>): Int {
+    val pobiMaxList =mutableListOf<Int>()
+    val crongMaxList = mutableListOf<Int>()
+    var pobiMax = 0
+    var crongMax = 0
+    for(i in pobi.indices) {
+        var pobiPageIntList = pobi[i].toString().map{it.digitToInt()}
+        var pobiTimes = 1
+        for(i in pobiPageIntList) {
+            pobiTimes *= i
+        }
+        pobiMaxList.add(if(pobiPageIntList.sum() >= pobiTimes) pobiPageIntList.sum() else pobiTimes)
+    }
+    pobiMax = pobiMaxList.maxOf { it }
+   return 0
+
+}
+fun main () {
+    val a = listOf<Int>(97,98)
+    val b = listOf<Int>(197, 198)
+    val c = listOf<Int>(131, 132)
+    val d = listOf<Int>(211, 212)
+    val e = listOf<Int>(99, 102)
+    val f = listOf<Int>(211, 212)
+    print(solution1(c,d))
+
 }
