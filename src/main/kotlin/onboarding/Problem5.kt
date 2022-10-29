dd73b4190dc8da899cf1df8b00027e02da0f5fc9
YuGyeong98 <kuk3097@naver.com>
2022-10-29 19:26:54 +0900

    feature(Problem5): 각 화폐로 나눈 나머지와 몫을 사용하여 리스트에 저장

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,27 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+  val withdrawalList = List(9) { 0 }.toMutableList()
+
+  checkMoney(money)
+
+  withdrawalList[0] = money / 50000
+  withdrawalList[1] = money % 50000 / 10000
+  withdrawalList[2] = money % 50000 % 10000 / 5000
+  withdrawalList[3] = money % 50000 % 10000 % 5000 / 1000
+  withdrawalList[4] = money % 50000 % 10000 % 5000 % 1000 / 500
+  withdrawalList[5] = money % 50000 % 10000 % 5000 % 1000 % 500 / 100
+  withdrawalList[6] = money % 50000 % 10000 % 5000 % 1000 % 500 % 100 / 50
+  withdrawalList[7] = money % 50000 % 10000 % 5000 % 1000 % 500 % 100 % 50 / 10
+  withdrawalList[8] = money % 50000 % 10000 % 5000 % 1000 % 500 % 100 % 50 % 10 / 1
+
+  return withdrawalList
+}
+
+fun checkMoney(money: Int) {
+  if (money !in 1..1000000) {
+    throw IllegalArgumentException(
+      "money는 1 이상 1,000,000 이하인 자연수여야 합니다."
+    )
+  }
 }
