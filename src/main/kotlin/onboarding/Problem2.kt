e006b770ca5b9de013f7d4db6f9b6a6b1a768011
Yerin <myheaven_b@naver.com>
2022-10-29 19:47:28 +0900

    refactor : add function getSolve

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,7 +1,7 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    var result:String = cryptogram
+    var result: String = cryptogram
 
     while(isCryptogram(result)){
         result = getSolve(result)
@@ -18,4 +18,16 @@ fun isCryptogram(str: String): Boolean {
     }
 
     return false
+}
+
+fun getSolve(_str: String): String {
+    var str: String = _str
+
+    for (i in 1..str.length-1) {
+        if (str[i - 1] == str[i]) {
+            str = str.replaceRange(i-1..i, "  ")
+        }
+    }
+
+    return str.replace("  ", "")
 }
