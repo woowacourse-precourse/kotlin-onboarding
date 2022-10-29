5b6b44150499386e8cc54bc0aa88d482e023a69b
healthykim <bsbs8645@snu.ac.kr>
2022-10-29 14:07:24 +0900

    feat(Problem5): Implement solution5 function

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,8 +1,16 @@
 package onboarding
 
+val SCALES = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+
 fun getNumberOfScale(remainder: Int, scale:Int): Int {
     return remainder/scale
 }
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var answer = mutableListOf<Int>()
+    var remainder = money
+    for(scale in SCALES) {
+        answer.add(getNumberOfScale(remainder, scale))
+        remainder %= scale
+    }
+    return answer
 }
