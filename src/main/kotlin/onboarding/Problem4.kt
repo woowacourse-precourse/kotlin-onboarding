612864cf67a7de0737870eda1e84fae6a65ecada
hansh0101 <hansh0101@naver.com>
2022-10-29 18:16:34 +0900

    [REFACTOR] Refactor Problem4

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,22 +3,26 @@ package onboarding
 fun solution4(word: String): String {
     var result = ""
 
-    val upperCaseSum = 'A'.code + 'Z'.code
-    val lowerCaseSum = 'a'.code + 'z'.code
-
     word.forEach {
-        when (val code = it.code) {
-            in 'A'.code..'Z'.code -> {
-                result += (upperCaseSum - code).toChar()
-            }
-            in 'a'.code..'z'.code -> {
-                result += (lowerCaseSum - code).toChar()
-            }
-            else -> {
-                result += it
-            }
-        }
+        result += translateCharacter(it)
     }
 
     return result
+}
+
+private fun translateCharacter(character: Char): Char {
+    val upperCaseSum = 'A'.code + 'Z'.code
+    val lowerCaseSum = 'a'.code + 'z'.code
+
+    return when (val code = character.code) {
+        in 'A'.code..'Z'.code -> {
+            (upperCaseSum - code).toChar()
+        }
+        in 'a'.code..'z'.code -> {
+            (lowerCaseSum - code).toChar()
+        }
+        else -> {
+            character
+        }
+    }
 }
