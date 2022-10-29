9ce03a7612536d6905951a03656afad96a430717
no1msh <no1msh1217@gmail.com>
2022-10-30 02:03:05 +0900

    [Problem4][style][feat] 기능 목록 주석 수정 , 1번 기능 구현
    기능 목록 1번
    - 대문자인지, 소문자인지 혹은 공백인지, 전부 아닌지 구분하는 메소드 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,9 +1,31 @@
 package onboarding
 
 // 기능 목록
-// 1. 엄마의 말을 변환 청개구리 말로 변환하는 메소드 구현
-// 2. 엄마의 말을 본격적으로 변환하는 구문 구현
-// 3. 이 때 공백은 건너뛰기
+// 1. 대문자인지, 소문자인지 혹은 공백인지, 전부 아닌지 구분하는 메소드 구현
+// 2. 엄마의 말을 변환 청개구리 말로 변환하는 메소드 구현
+// 3. 엄마의 말을 본격적으로 변환하는 구문 구현
+
+const val NOT_ALPHABET_SPACE = 0
+const val SPACE = 1
+const val LOWERCASE = 2
+const val UPPERCASE = 3
+
 fun solution4(word: String): String {
     TODO("프로그램 구현")
+}
+// 기능 목록 1번
+fun checkChar(target : Char) : Int {
+    if (target == ' '){
+        return SPACE
+    }
+
+    if (target in 'a'..'z'){
+        return LOWERCASE
+    }
+
+    if (target in 'A'..'Z'){
+        return UPPERCASE
+    }
+
+    return NOT_ALPHABET_SPACE
 }
