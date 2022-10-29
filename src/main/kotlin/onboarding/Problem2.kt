80edad364cbe0d8baee5260b2c3d2ab2bfa3a83f
sujin-kk <sjoo0002@gmail.com>
2022-10-30 04:51:29 +0900

    [FEAT] 중복을 가지지 않을때까지 암호 해독 기능 추가

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -5,8 +5,19 @@ fun main(args: Array<String>) {
 }
 
 fun solution2(cryptogram: String): String {
+
 }
 
+
+fun decryption(cryptogram: String): String {
+    var targetStr = cryptogram
+
+    do {
+        targetStr = targetStr.toCharArray().distinct().joinToString("")
+    } while (hasDuplicates(cryptogram = cryptogram))
+
+    return targetStr
+}
 
 fun hasDuplicates(cryptogram: String): Boolean {
     if (cryptogram.length != cryptogram.toCharArray().distinct().size) return true
