1dfe6c75f4b73600dee290e114cbd26803828734
cyjadela <adelacho@naver.com>
2022-10-29 21:22:34 +0900

    refactor: [문제5] moneys List 추가, 반복문 사용

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -2,31 +2,24 @@ package onboarding
 
 fun solution5(money: Int): List<Int> {
     var bills = mutableListOf<Int>()
+    var moneys = mutableListOf(
+        50000,
+        10000,
+        5000,
+        1000,
+        500,
+        100,
+        50,
+        10
+    )
     var leftMoney: Int = money
+    var i = 0
 
-    bills.add(leftMoney/50000)
-    leftMoney -= bills[0] * 50000
-
-    bills.add(leftMoney/10000)
-    leftMoney -= bills[1] * 10000
-
-    bills.add(leftMoney/5000)
-    leftMoney -= bills[2] * 5000
-
-    bills.add(leftMoney/1000)
-    leftMoney -= bills[3] * 10000
-
-    bills.add(leftMoney/500)
-    leftMoney -= bills[4] * 500
-
-    bills.add(leftMoney/100)
-    leftMoney -= bills[5] * 100
-
-    bills.add(leftMoney/50)
-    leftMoney -= bills[6] * 50
-
-    bills.add(leftMoney/10)
-    leftMoney -= bills[7] * 10
+    while (i < 8) {
+        bills.add(leftMoney / moneys[i])
+        leftMoney -= bills[i] * moneys[i]
+        i++
+    }
 
     bills.add(leftMoney)
 
