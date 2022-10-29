fda9d1f50cb65e9f88fedba302b15e66ec669d8f
sujin-kk <sjoo0002@gmail.com>
2022-10-30 07:36:57 +0900

    [FEAT] 청개구리 사전 생성

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,18 +1,30 @@
 package onboarding
 
+fun main(args: Array<String>) {
+    createDictionary()
+}
+
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
 
-fun changeWord(word: String) {
-    val dictionary = listOf('a' to 'z', )
+//fun changeWord(word: String): String {
+//
+//}
+
+fun createDictionary(): List<Pair<Char, Char>> {
+    val dictionary = mutableListOf<Pair<Char, Char>>()
+
+    repeat(NUMBER_OF_ALPHA) {
+        dictionary.add((START_UPPER_ALPHA_CODE + it).toChar() to (END_UPPER_ALPHA_CODE - it).toChar())
+        dictionary.add((START_LOWER_ALPHA_CODE + it).toChar() to (END_LOWER_ALPHA_CODE - it).toChar())
+    }
+    println(dictionary)
+    return dictionary
 }
 
-fun isAlpha(ch: Char): Boolean {
-    if (ch.code in START_ASCII..END_ASCII) return true
-    return false
-}
-
-const val START_ASCII = 65
-const val END_ASCII = 122
+const val START_UPPER_ALPHA_CODE = 65
+const val END_UPPER_ALPHA_CODE = 90
+const val START_LOWER_ALPHA_CODE = 97
+const val END_LOWER_ALPHA_CODE = 122
 const val NUMBER_OF_ALPHA = 26
