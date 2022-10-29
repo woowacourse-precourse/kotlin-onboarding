45463ecef34725f0bcdb75534dcb35c73c392750
re4rk <37167652+re4rk@users.noreply.github.com>
2022-10-29 18:26:08 +0900

    FEAT : solution1 함수 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,46 @@
 package onboarding
 
+import java.lang.Integer.max
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if (checkValidation(pobi).not() ||
+        checkValidation(crong).not()
+    ) {
+        return -1
+    }
+
+    return when (getPageScore(pobi).compareTo(getPageScore(crong))) {
+        // pobi win
+        1 -> 1
+        // crong win
+        -1 -> 2
+        // draw
+        else -> 0
+    }
+}
+
+private fun checkValidation(pages: List<Int>): Boolean {
+    if (pages.size != 2 ||
+        pages[0] < 1 ||
+        pages[1] > 400 ||
+        pages[0] + 1 != pages[1] ||
+        pages[1] % 2 == 1
+    ) {
+        return false
+    }
+    return true
+}
+
+private fun getPageScore(pages: List<Int>): Int {
+    var pageSum = 0
+    var pageMul = 1
+    pages.forEach() { page ->
+        page.toString()
+            .map { it.digitToInt() }
+            .forEach {
+                pageSum += it
+                pageMul *= it
+            }
+    }
+    return max(pageMul, pageSum)
 }
