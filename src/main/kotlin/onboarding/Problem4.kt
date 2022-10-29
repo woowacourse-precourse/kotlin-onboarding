d02dbc3f063bffc45e8beb9613dbd57d498fd28f
healthykim <bsbs8645@snu.ac.kr>
2022-10-29 13:41:02 +0900

    feat(Problem4): Implement solution4 function
    There was no need to make another function to do translation.
    Translating mother's words into frog's words is a function of solution4!

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,23 @@
 package onboarding
+import java.util.Dictionary
+val FROG_DICT: Map<String, String> = mapOf(
+    "A" to "Z", "B" to "Y", "C" to "X", "D" to "W", "E" to "V", "F" to "U", "G" to "T", "H" to "S", "I" to "R", "J" to "Q",
+    "K" to "P","L" to "O","M" to "N","N" to "M","O" to "L","P" to "K","Q" to "J","R" to "I","S" to "H","T" to "G","U" to "F",
+    "V" to "E", "W" to "D", "X" to "C", "Y" to "B", "Z" to "A")
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var answer: String = ""
+    for(character in word) {
+        if(!character.isLetter()) {
+            answer += character
+            continue
+        }
+
+        var frogChar = FROG_DICT[character.uppercase()]
+        if (frogChar != null) {
+            if(character.isUpperCase()) answer += frogChar
+            else answer += frogChar.lowercase()
+        }
+    }
+    return answer
 }
