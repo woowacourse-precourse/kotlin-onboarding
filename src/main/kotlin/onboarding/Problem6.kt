aa8a98ba546ef58d5ff2f7eb6db30f8e51150e61
yeongjin <dudwls901@naver.com>
2022-10-29 11:56:37 +0900

    [Feat] Validation Check

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,18 +1,34 @@
 package onboarding
 /* 기능 목록
 * hashmap을 이용하여 연속 문자열마다 이메일셋 저장
-* 제한 닉네임 추출 및 정렬
+* 제한 닉네임의 이메일 추출 및 정렬
+* validation check
 * */
-fun getWordMap(forms: List<List<String>>): Map<String,Set<String>>{
-    val wordMap = mutableMapOf<String,MutableSet<String>>()
-    for((email, name) in forms){
-        for(s in name.indices){
+
+const val DOMAIN = "email.com"
+
+fun isCorrectForm(email: String, name: String): Boolean =
+    email.contains(DOMAIN) &&
+        email.length in 11 until 20 &&
+        name.find { it.isAlpha() } == null &&
+        name.length in 1 until 20
+
+fun Char.isAlpha(): Boolean =
+    this.code in 'a'.code..'z'.code ||
+        this.code in 'A'.code..'Z'.code
+
+
+fun getWordMap(forms: List<List<String>>): Map<String, Set<String>> {
+    val wordMap = mutableMapOf<String, MutableSet<String>>()
+    for ((email, name) in forms) {
+        if (!isCorrectForm(email, name)) continue
+        for (s in name.indices) {
             val word = StringBuilder()
-            for(i in s until name.length){
+            for (i in s until name.length) {
                 word.append(name[i])
-                if(word.length>=2){
+                if (word.length >= 2) {
                     val wordStr = word.toString()
-                    if(wordMap[wordStr]==null) wordMap[wordStr] = mutableSetOf()
+                    if (wordMap[wordStr] == null) wordMap[wordStr] = mutableSetOf()
                     wordMap[wordStr]?.add(email)
                 }
             }
@@ -21,10 +37,10 @@ fun getWordMap(forms: List<List<String>>): Map<String,Set<String>>{
     return wordMap
 }
 
-fun getResultList(wordMap: Map<String,Set<String>>): List<String>{
+fun getResultList(wordMap: Map<String, Set<String>>): List<String> {
     val resultSet = mutableSetOf<String>()
     wordMap.forEach { (_, value) ->
-        if(value.size>=2){
+        if (value.size >= 2) {
             resultSet.addAll(value)
         }
     }
