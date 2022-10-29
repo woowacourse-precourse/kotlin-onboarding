e4c2d16de48ff52ea298bdfbc59651d2df2431ee
YuGyeong98 <kuk3097@naver.com>
2022-10-29 17:19:00 +0900

    feature(Problem4): word의 알파벳을 반대로 변환하여 저장 및 예외 처리

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,38 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+  val upperAlphabets = ('A'..'Z').toMutableList()
+  val lowerAlphabets = ('a'..'z').toMutableList()
+  val reversedUpperAlphabets = upperAlphabets.reversed()
+  val reversedLowerAlphabets = lowerAlphabets.reversed()
+  var reversedWord = ""
+
+  checkWordLength(word)
+
+  for (i in word.indices) {
+    if (word[i].isUpperCase()) {
+      for (j in upperAlphabets.indices) {
+        if (word[i] == upperAlphabets[j]) {
+          reversedWord += reversedUpperAlphabets[j]
+        }
+      }
+    } else if (word[i].isLowerCase()) {
+      for (j in lowerAlphabets.indices) {
+        if (word[i] == lowerAlphabets[j]) {
+          reversedWord += reversedLowerAlphabets[j]
+        }
+      }
+    } else {
+      reversedWord += word[i]
+    }
+  }
+  return reversedWord
+}
+
+fun checkWordLength(word: String) {
+  if (word.length !in 1..1000) {
+    throw IllegalArgumentException(
+      "word는 길이가 1 이상 1,000 이하인 문자열이어야 합니다."
+    )
+  }
 }
