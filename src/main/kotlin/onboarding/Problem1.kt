234080b01c187d304c576c66ebbab63fc109edbe
Suheon Song <ssheon0812@naver.com>
2022-10-30 00:29:15 +0900

    feat: [Problem1] getScore 함수 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,44 @@
 package onboarding
 
+import kotlin.math.max
+
+
+fun getScroe(pageList: List<Int>): Int {
+    val leftPage = pageList[0]
+    val rightPage = pageList[1]
+
+    val biggestNumInLeftPage = max(addDigit(leftPage), multiplyDigit(leftPage))
+    val biggestNumInRightPage = max(addDigit(rightPage), multiplyDigit(rightPage))
+
+    return max(biggestNumInLeftPage, biggestNumInRightPage)
+}
+
+fun addDigit(num: Int): Int {
+    var tempNum = num
+    var result = 0
+    while (tempNum > 0) {
+        result += (tempNum % 10)
+        tempNum /= 10
+    }
+
+    return result
+}
+
+
+fun multiplyDigit(num: Int): Int {
+    var tempNum = num
+    var result = 1
+    while (tempNum > 0) {
+        result *= (tempNum % 10)
+        tempNum /= 10
+    }
+
+    return result
+}
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    val pobiScore = getScroe(pobi)
+    val crongScore = getScroe(crong)
+
+    return 0
 }
