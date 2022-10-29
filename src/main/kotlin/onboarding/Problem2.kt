c686bcb572c74e41dd2ae9e5d3f62da67b5ed5f1
YuGyeong98 <kuk3097@naver.com>
2022-10-29 14:52:27 +0900

    feature(Problem2): 연속되는 중복 문자를 삭제하는 함수 생성

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,21 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+  val str = StringBuilder(cryptogram)
+  val length = cryptogram.length
+  return decode(str, length)
+}
+
+fun decode(str: StringBuilder, length: Int): String {
+  var len = length
+  for (i in 0 until len) {
+    if (i + 1 < str.length) {
+      if (str[i] == str[i + 1]) {
+        str.delete(i, i + 2)
+        len -= 2
+        decode(str, len)
+      }
+    }
+  }
+  return str.toString()
 }
