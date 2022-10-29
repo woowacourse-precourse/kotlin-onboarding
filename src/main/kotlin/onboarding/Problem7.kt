0375d9bd2a76caffb1862b9ca154553f1f86991b
dltkd1395 <dltkd13956@naver.com>
2022-10-29 17:45:54 +0900

    feat : [PROBLEM7] 각 유저에 해당하는 친구 분리 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,19 @@
 package onboarding
 
+val friendList = mutableMapOf<String, MutableSet<String>>()
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
     TODO("프로그램 구현")
+}
+
+fun findUserFriend(index: Int, userFriendList: MutableSet<String>) {
+    if (index == userFriendList.size) {
+        return
+    }
+    checkUserFriends(0, friendList.keys, userFriendList.elementAt(index))
+    return findUserFriend(index + 1, userFriendList)
 }
