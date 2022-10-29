05c1eda90f4f70843d676e207227d6c1fd6dd645
tmdgh1592 <tmdgh1592@naver.com>
2022-10-29 13:54:01 +0900

    feat: 각 사용자들의 친구 리스트를 Value로 가지는 맵 반환 메서드 구현 - createFriendsMap() 메서드 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -8,6 +8,20 @@ fun solution7(
     TODO("프로그램 구현")
 }
 
+private fun createFriendsMap(friendRelations: List<List<String>>): Map<String, List<String>> {
+    val friendsMap = mutableMapOf<String, MutableList<String>>()
+
+    friendRelations.forEach { relation ->
+        val friendA = relation[0]
+        val friendB = relation[1]
+
+        friendsMap[friendA] = addFriend(friendsMap[friendA], friendB)
+        friendsMap[friendB] = addFriend(friendsMap[friendB], friendA)
+    }
+
+    return friendsMap
+}
+
 private fun addFriend(friends: MutableList<String>?, newFriend: String): MutableList<String> {
     return (friends ?: mutableListOf()).apply {
         add(newFriend)
