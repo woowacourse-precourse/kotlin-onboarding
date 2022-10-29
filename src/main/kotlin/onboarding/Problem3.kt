407d14b41f794fce5e0c5688fb8e018d00a3b04c
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-30 02:19:53 +0900

    숫자 당 박수 카운트 함수

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -8,7 +8,19 @@ fun solution3(number: Int): Int {
 
     for(i:Int in 1 .. number){
         var numberlist = i.toString().map{it.digitToInt()}  //주어진 숫자까지 자리수를 쪼개 리스트로 바꾼다.
-
+        clap += CountClap(numberlist)
     }
     return clap
+}
+
+//숫자 당 박수 카운트 함수
+fun CountClap(numberlist: List<Int>):Int{
+    var answer:Int = 0
+
+    for(i in numberlist) {
+        if ( i==3 || i==6 || i==9) {    //리스트 원소가 3, 6, 9 일때 1카운트
+            answer += 1
+        }
+    }
+    return answer
 }
