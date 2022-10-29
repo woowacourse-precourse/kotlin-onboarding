fed1facb9bc362109fbbc188ef979e3c2619ea0c
kyounggudev <homin222@naver.com>
2022-10-30 00:53:19 +0900

    문제1 - 요구사항 분석 추가

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -19,14 +19,3 @@ fun solution6(forms: List<List<String>>): List<String> {
 fun checkOverlap(nickname : String, forms : List<List<String>>){
 
 }
-// 수정필요
-fun main(){
-    val forms = listOf(
-        listOf("jm@email.com", "제이엠"),
-        listOf("jason@email.com", "제이슨"),
-        listOf("woniee@email.com", "워니"),
-        listOf("mj@email.com", "엠제이"),
-        listOf("nowm@email.com", "이제엠")
-    )
-    println(solution6(forms))
-}
