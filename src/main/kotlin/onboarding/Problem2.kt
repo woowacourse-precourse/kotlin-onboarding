90c1abb5d7fd9645a932e9486460e1f934b49b53
no1msh <no1msh1217@gmail.com>
2022-10-29 22:38:55 +0900

    [Problem2][style][feat][fix] 기능 목록 수정 , 3번 기능 구현, 2번 기능 수정
    기능 목록 3번
    - cryptogram을 해독하는 구문 구현
    
    기능 목록 2번
    - 중복되는 문자를 가져오는 substring의 범위 수정

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -3,14 +3,23 @@ package onboarding
 // 기능 목록
 // 1. 해독 해야하는지 확인 하는 메소드 구현
 // 2. 중복되는 문자를 지우는 메소드 구현
-// 3. 예외처리 구현
-// 3-1 cryptogram의 길이가 1이라면 바로 리턴
+// 3. cryptogram을 해독하는 구문 구현
+// 4. 예외처리 구현
+// 4-1 cryptogram의 길이가 1이라면 바로 리턴
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var targetOfDecode = cryptogram
+
+    while (shouldDecode(targetOfDecode)) {
+        targetOfDecode = removeDuplication(targetOfDecode)
+    }
+
+    return targetOfDecode
 }
+
 
 // 기능 목록 1번
 fun shouldDecode(cryptogram: String): Boolean {
+    // 기능 목록 4번
     if (cryptogram.length <= 1) {
         return false
     }
@@ -25,12 +34,13 @@ fun shouldDecode(cryptogram: String): Boolean {
 
     return false
 }
+
 // 기능 목록 2번
 fun removeDuplication(cryptogram: String): String {
     for (i in 1 until cryptogram.length) {
-        if (cryptogram[i-1] == cryptogram[i]) {
-            val removeString = cryptogram.substring(i-1 , i)
-            return cryptogram.replace(removeString , "")
+        if (cryptogram[i - 1] == cryptogram[i]) {
+            val removeString = cryptogram.substring(i - 1, i + 1)
+            return cryptogram.replace(removeString, "")
         }
     }
 
