4c4471500b122fa4a4c4459fa6a96d556084e339
EmilyCh0 <seoyeon3089@naver.com>
2022-10-29 10:28:30 +0900

    feat(problem7): 각 사용자별 친구 목록

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,13 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
+    val friendList = mutableMapOf<String, ArrayList<String>>()
+    for (f in friends) {
+        if (!friendList.contains(f[0])) friendList[f[0]] = arrayListOf(f[1])
+        else friendList[f[0]]?.add(f[1])
+        if (!friendList.contains(f[1])) friendList[f[1]] = arrayListOf(f[0])
+        else friendList[f[1]]?.add(f[0])
+    }
+    val userFriendList = friendList[user]
     TODO("프로그램 구현")
 }
