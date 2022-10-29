6756a425c00ebe410112d98c5eb71b5a7361159f
ChoHJin <cjc9739@naver.com>
2022-10-30 05:18:13 +0900

    Problem3 solved

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -2,17 +2,16 @@
 기능 목록
 1. 카운트
 1-1. 1에서 number만큼 for문으로 숫자를 검사한다.
-1-2. i의 자릿수를 분리하여 각각 10으로 나눈 나머지가 3, 6, 9라면 cnt를 증가시킨다.
+1-2. i의 자릿수를 분리하여 각각 10으로 나눈 나머지가 3, 6, 9 중에 해당된다면 cnt를 증가시킨다.
 1-3. for문이 종료되면 cnt값을 리턴한다.
 */
 
 package onboarding
 
-fun solution3(number: Int): Int {
-//    TODO("프로그램 구현")
+fun cntClap(num : Int) : Int {
     var cnt = 0
 
-    for(i in 1..number) {
+    for(i in 1..num) {
         var tmp = i
         while(tmp != 0) {
             if(tmp % 10 == 3 || tmp % 10 == 6 || tmp % 10 == 9) {
@@ -21,5 +20,11 @@ fun solution3(number: Int): Int {
             tmp /= 10
         }
     }
+
     return cnt
+}
+fun solution3(number: Int): Int {
+//    TODO("프로그램 구현")
+
+    return cntClap(number)
 }
