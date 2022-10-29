cbd37442e368b751e28285c2701ddaf57959ec76
DormunDoorlock <skrudgh2001@naver.com>
2022-10-30 08:59:24 +0900

    feat: Problem3.kt 각 숫자 자릿수로 나누는 기능, counting 값 반환 기능 추가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,23 @@
 package onboarding
-
+// 3,6,9 있는지 확인
+fun checkNum(number: Int):Int{
+    return 1
+}
+// 숫자 자릿수로 나누는 기능
+fun shareNum(number: Int):Int{
+    var input:Int = number
+    var result=0
+    while(input !=0){
+        result += checkNum(input % 10)
+        input /= 10
+    }
+    return result
+}
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    // counting 값 반환
+    var result =0
+    for(i in 1..number){
+        result += shareNum(i)
+    }
+    return result
 }
