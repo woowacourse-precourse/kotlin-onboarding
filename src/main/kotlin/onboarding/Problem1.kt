8afa83e22118d96edd4b66e00357cb542f9075a8
윤동현 <donghyunyoon81@gmail.com>
2022-10-30 02:37:07 +0900

    Feat: 조건에 올바른 값 출력 페이지가 펼쳐질 경우 오른쪽 페이지가 왼쪽 페이지보다 1높은 규칙을 벗어나는 페이지가 있을 경우 -1 출력 포비의 점수가 크롱보다 높을시 1 출력 크롱의 점수가 포비보다 높을시 2 출력 둘의 값이 같을 경우에 0출력

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -14,8 +14,19 @@ fun solution1(pobi: List<Int>,crong:List<Int>): Int {
         pobiMaxList.add(if(pobiPageIntList.sum() >= pobiTimes) pobiPageIntList.sum() else pobiTimes)
     }
     pobiMax = pobiMaxList.maxOf { it }
-   return 0
-
+    for(i in crong.indices) {
+        var crongPageIntList = crong[i].toString().map{ it.digitToInt() }
+        var crongTimes = 1
+        for(i in crongPageIntList) { crongTimes *= i }
+        crongMaxList.add(if (crongPageIntList.sum() >= crongTimes) crongPageIntList.sum() else crongTimes)
+    }
+    crongMax = crongMaxList.maxOf { it }
+    return when {
+        pobi[1]-pobi[0] !=1 && crong[1]-pobi[0] !=1 -> -1
+        pobiMax > crongMax -> 1
+        pobiMax < crongMax -> 2
+        else  -> 0
+    }
 }
 fun main () {
     val a = listOf<Int>(97,98)
