728d54bcb02b835946453587405d19b0e19edc8b
hansh0101 <hansh0101@naver.com>
2022-10-29 16:26:53 +0900

    [REFACTOR] Refactor Problem2

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,39 +1,52 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    var hasRedundant = true
-    var result = cryptogram
-    while (hasRedundant) {
-        val removeIndexPairList = mutableListOf<Pair<Int, Int>>()
-        var lastCharacter = ' '
-        var startIndex = -1
-        var endIndex = -1
-        var flushFlag = false
-
-        result.forEachIndexed { index, c ->
-            if (lastCharacter == c) {
-                endIndex = index
-                flushFlag = true
-            } else {
-                if (flushFlag) {
-                    removeIndexPairList.add(Pair(startIndex, endIndex))
-                }
-                flushFlag = false
-                startIndex = index
-                lastCharacter = c
-            }
-        }
-        if (flushFlag) {
-            removeIndexPairList.add(Pair(startIndex, endIndex))
-        }
-
-        var removedCount = 0
-        removeIndexPairList.forEach {
-            result = result.replaceRange(it.first - removedCount, it.second - removedCount + 1, "")
-            removedCount += it.second - it.first + 1
-        }
-
-        hasRedundant = removeIndexPairList.isNotEmpty()
+    var isDuplicated = true
+    var resultString = cryptogram
+    while (isDuplicated) {
+        val duplicatedRanges = getDuplicatedRanges(resultString)
+        val duplicationsRemovedResult = getDuplicationsRemovedResult(resultString, duplicatedRanges)
+        isDuplicated = resultString != duplicationsRemovedResult
+        resultString = duplicationsRemovedResult
     }
-    return result
+    return resultString
+}
+
+private fun getDuplicatedRanges(string: String): List<List<Int>> {
+    val duplicatedRanges = mutableListOf<List<Int>>()
+    var lastCharacter = ' '
+    var startIndex = -1
+    var endIndex = -1
+    var isFlushed = true
+
+    string.forEachIndexed { index, c ->
+        if (lastCharacter == c) {
+            endIndex = index + 1
+            isFlushed = false
+            return@forEachIndexed
+        }
+        if (!isFlushed) {
+            duplicatedRanges.add(listOf(startIndex, endIndex))
+            isFlushed = true
+        }
+        startIndex = index
+        lastCharacter = c
+    }
+    if (!isFlushed) {
+        duplicatedRanges.add(listOf(startIndex, endIndex))
+    }
+
+    return duplicatedRanges
+}
+
+private fun getDuplicationsRemovedResult(resultString: String, duplicatedRanges: List<List<Int>>): String {
+    var duplicationsRemovedResult = resultString
+    var removedCount = 0
+    duplicatedRanges.forEach { duplicatedRange ->
+        val startIndex = duplicatedRange[0] - removedCount
+        val endIndex = duplicatedRange[1] - removedCount
+        duplicationsRemovedResult = duplicationsRemovedResult.replaceRange(startIndex, endIndex, "")
+        removedCount += endIndex - startIndex
+    }
+    return duplicationsRemovedResult
 }
