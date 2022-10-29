37a11c74775cb612b28f6c4eb0baf886a8879227
username <ieejo716@naver.com>
2022-10-29 23:33:17 +0900

    [FEAT] : 문제7

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,56 +1,65 @@
 package onboarding
 
+import java.util.*
+
+
+//user와 직접적인 칭구들 배열 생성 -> user[ friend1 : List<> , friend2 : List<>  ]
+//해당 칭구들의 직접적인 친구들 리스트 생성
+//resultList 만들어 칭구들의 점수를 표기 Map<String,Int> 입장 조건 : key값이 있으면 점수 추가 없으면 새로 추가
+//visitor는 이미 user의 실제 칭구면 continue, 아니면 위와 동일하게 동작
+
 fun solution7(
     user: String, friends: List<List<String>>, visitors: List<String>
 ): List<String> {
-    //user와 직접적인 칭구들 배열 생성 -> user[ friend1 : List<> , friend2 : List<>  ]
-    //해당 칭구들의 직접적인 친구들 리스트 생성
-    //resultList 만들어 칭구들의 점수를 표기 Map<String,Int> 입장 조건 : key값이 있으면 점수 추가 없으면 새로 추가
-    //visitor는 이미 user의 실제 칭구면 continue, 아니면 위와 동일하게 동작
     val userDirectFriendList = mutableListOf<String>()
     val resultList = mutableMapOf<String, Int>()
 
+    //user와 직접 친구 고르기
     friends.forEach { list ->
-        if (list.contains(user)) {
-            userDirectFriendList.add(list[0])
-        }
+        if (list.contains(user)) userDirectFriendList.add(list[0])
     }
 
+    //유저와 함께 아는 친구 점수 부여
     userDirectFriendList.forEach { directFriend ->
-        friends.map {
-            if (it[0] == directFriend && resultList.containsKey(it[1])) {
-                resultList[it[1]] = resultList.getValue(it[1]) + 10
-            } else {
-                resultList.put(it[1], 10)
-            }
+        friends.forEach {
+            if (it[0] == directFriend && resultList.containsKey(it[1])) resultList[it[1]] =
+                resultList.getValue(it[1]) + 10
+            else resultList[it[1]] = 10
         }
     }
 
+    //방문한 사람 점수 부여
     visitors.forEach {
-        if (resultList.containsKey(it)) {
-            resultList[it] = resultList.getValue(it) + 1
-        } else if (!userDirectFriendList.contains(it)) {
-            resultList[it] = 1
-        }
+        if (resultList.containsKey(it)) resultList[it] = resultList.getValue(it) + 1
+        else if (!userDirectFriendList.contains(it)) resultList[it] = 1
+
     }
-
+    //이용자 제거
     resultList.remove(user)
-    resultList.toList().sortedWith(compareBy { it.second })
+    //점수 높은순으로 정렬 근데 알파벳
 
-    return resultList.keys.toList()
+    val keySortedList =
+        resultList.toSortedMap().toList().sortedWith(compareBy<Pair<String, Int>> { it.second }.reversed())
+
+    val answer = keySortedList.toMap().keys.toList()
+
+    return if (answer.size > 5)
+        answer.subList(0, 5)
+    else
+        answer
 }
 
 fun main() {
     val user = "mrko"
     val friends = listOf(
-        listOf("donut", "andole"),
         listOf("donut", "jun"),
+        listOf("donut", "andole"),
         listOf("donut", "mrko"),
         listOf("shakevan", "andole"),
         listOf("shakevan", "jun"),
         listOf("shakevan", "mrko")
     )
-    val visitors = listOf("bedi", "bedi", "donut", "bedi", "shakevan")
+    val visitors = listOf("bedi", "bedi", "donut", "bedi", "shakevan", "koko", "haha", "kuku")
     val list = solution7(
         user, friends, visitors
     )
