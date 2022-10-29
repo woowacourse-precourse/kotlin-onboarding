6b95985419e83db8c0564da3c450d60a62c2c3f4
kjyook <106497893+kjyook@users.noreply.github.com>
2022-10-30 02:37:35 +0900

    문제 1 solution함수에서 등호가 불필요하게 있던 부분 수정 + 코드 최적화 + 실수 수정

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -5,8 +5,6 @@ fun calculation(a: List<Int>): Int {
     val leftStirng = a[0].toString()    //왼쪽과 오른쪽 페이지의 숫자를 stirng으로 바꿔서 저장해준다
     val rightStirng = a[1].toString()
 
-    var tempResult = 0
-
     if(a[0]+1==a[1] && a[0]%2 == 1){       //왼쪽 페이지 번호는 홀수이고 오른쪽 페이지보다 1 작아야 한다
 
         var tempSum1 = 0
@@ -17,10 +15,10 @@ fun calculation(a: List<Int>): Int {
             tempSum1 += elem - '0'
             tempMul1 *= elem - '0'
         }
-        if(tempSum1>=tempMul1)      //가장 큰 수를 resultLeft 에 저장
-            resultLeft=tempSum1
+        if(tempSum1 >= tempMul1)      //가장 큰 수를 resultLeft 에 저장
+            resultLeft = tempSum1
         else
-            resultLeft=tempMul1
+            resultLeft = tempMul1
 
         var tempSum2 = 0
         var tempMul2 = 1
@@ -30,31 +28,29 @@ fun calculation(a: List<Int>): Int {
             tempSum2 += elem - '0'
             tempMul2 *= elem - '0'
         }
-        if(tempSum1>=tempMul1)      //가장 큰 수를 resultRight 에 저장
-            resultRight=tempSum2
+        if(tempSum2 >= tempMul2)      //가장 큰 수를 resultRight 에 저장
+            resultRight = tempSum2
         else
-            resultRight=tempMul2
+            resultRight = tempMul2
 
         if(resultLeft >= resultRight)   //resultLeft 와 resultRight 를 비교하여 더 큰 값을 tempResult에 저장한다
-            tempResult = resultLeft
+            return resultLeft
         else
-            tempResult = resultRight
+            return resultRight
     }
     else        //왼쪽 페이지 번호가 홀수가 아니거나 오른쪽 페이지 번호보다 1 작은게 아니라면
-        return -1   //-1을 반환하고
-
-    return tempResult   //아니라면 tempResult를 반환한다
+        return -1   //-1을 반환한다
 }
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     var pobiNum = calculation(pobi)
     var crongNum = calculation(crong)
 
-    if(pobiNum==1 || crongNum==1)
+    if(pobiNum== -1 || crongNum== -1)
         return -1
-    else if(pobiNum>=crongNum)
+    else if(pobiNum > crongNum)
         return 1
-    else if(pobiNum<=crongNum)
+    else if(pobiNum < crongNum)
         return 2
     else
         return 0
