f5fadaf92b41b2ad4a630784315f506f063feb58
YuGyeong98 <kuk3097@naver.com>
2022-10-29 16:09:18 +0900

    feature(Problem3): number에 3, 6, 9가 포함되면 박수 횟수 증가 및 예외 처리

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,26 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+  var numberOfClaps = 0
+
+  checkNumber(number)
+
+  for (i in 1..number) {
+    var current = i
+    while (current != 0) {
+      if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
+        numberOfClaps += 1
+      }
+      current /= 10
+    }
+  }
+  return numberOfClaps
+}
+
+fun checkNumber(number: Int) {
+  if (number !in 1..10000) {
+    throw IllegalArgumentException(
+      "number는 1 이상 10,000 이하인 자연수여야 합니다."
+    )
+  }
 }
