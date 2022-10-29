bcbd25a466bf2a590c97feb99c38b2a14f067611
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-10-30 02:31:53 +0900

    Problem4 중괄호 생략된 곳들 추가해주기

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -21,10 +21,12 @@ fun solution4(word: String): String {
         val appendChar = mappingRecord[upperWord[index].code - 'A'.code]
 
         //확인하면서 input에서 주어진 문자가 소문자인 경우는 다시 소문자로 바꾸기
-        resultString += if (word[index].isUpperCase())
+        resultString += if (word[index].isUpperCase()){
             appendChar
-        else
+        }
+        else{
             appendChar.lowercaseChar()
+        }
     }
 
     return resultString
