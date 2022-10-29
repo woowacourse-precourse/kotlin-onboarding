d28c5e98e63e85d27b7c6d6df5b870bf5e4c3cba
cyjadela <adelacho@naver.com>
2022-10-29 20:00:39 +0900

    feat: [문제3] 각자릿수가 3,6,9일 때 count 증가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var count = 0
+
+    for (i in 0..number) {
+        var i: Int = i
+
+        while (i != 0) {
+            when (i % 10) {
+                3, 6, 9 -> {
+                    count++
+                }
+            }
+            i /= 10
+        }
+    }
+
+    return count
 }
