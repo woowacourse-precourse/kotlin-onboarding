082cbf8b5e42f8e24d90366c92c5ac807ca4a572
ChunBaee <mljung10@naver.com>
2022-10-29 22:18:31 +0900

    7번] 기능 1) 밑그림 : friends, visitors 에 존재하는 모든 유저 닉네임 가져오기

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,13 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+
+    returnAllUser(friends, visitors)
+}
+
+/**
+ * 2차원 배열을 평탄화시키고, 방문자 배열과 합친 후 중복을 제거하는 함수 (배열들에 존재하는 모든 유저명 추려내기)
+ */
+private fun returnAllUser(friends: List<List<String>>, visitors: List<String>): List<String> {
+    return (friends.flatten() + visitors).distinct()
 }
