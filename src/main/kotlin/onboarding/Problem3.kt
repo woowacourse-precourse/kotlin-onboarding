39dd16b1e9d646e49c618a9c402df5352df84818
healthykim <bsbs8645@snu.ac.kr>
2022-10-29 13:11:16 +0900

    feat(Problem2): Add function for getting a number of claps
    Added getClap function in order to get a number of claps generated from one number

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+fun getClap(number: Int): Int {
+    var clapCnt = 0
+    var remain = number
+    while(remain != 0) {
+        if(remain % 10 == 3 || remain % 10 == 6 || remain % 10 == 9) clapCnt += 1
+        remain /= 10
+    }
+    return clapCnt
+}
+
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
