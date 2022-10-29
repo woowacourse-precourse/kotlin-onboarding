fae7b3b3a12f8374d90503f1d9163061921ad557
sujin-kk <sjoo0002@gmail.com>
2022-10-30 06:12:44 +0900

    [FEAT] 3, 6, 9 가 들어가는 숫자 찾기

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,19 @@
 package onboarding
 
-fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+fun main(args: Array<String>) {
+    println(solution3(33))
 }
+
+fun solution3(number: Int): Int {
+
+}
+
+fun isTargetNumber(num: Int): Boolean {
+    val targetNumbers = intArrayOf(3, 6, 9)
+
+    targetNumbers.forEach { targetNum ->
+        if (num.toString().contains(targetNum.toString())) return true
+    }
+    return false
+}
+
