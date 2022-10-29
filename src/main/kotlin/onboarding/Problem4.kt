e3fbddee4efdb0adc822e2a01b1b87d3831bffeb
cyjadela <adelacho@naver.com>
2022-10-29 20:08:38 +0900

    feat: [문제4] 소문자 변환

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -6,6 +6,12 @@ fun solution4(word: String): String {
     word.forEach {
         if (it.code in 65..90)
             result += (155 - it.code).toChar()
+
+        else if (it.code in 97..122)
+            result += (219 - it.code).toChar()
+
+        else
+            result += it
     }
 
     return result
