5287c0bcadff4aa49f7041639bd1d52dee78689c
bluecat0823 <star4_you@naver.com>
2022-10-30 01:47:44 +0900

    미션3 : 1부터 주어진 숫자까지 3, 6, 9가 들어가는 횟수 카운트 및 반환

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,21 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+
+    var claps = 0
+
+    for (i in 1..number)
+    {
+        var next = i
+
+        while (next>=1){
+            if ((next%10==3) or (next%10==6) or (next%10==9)){
+                claps += 1
+            }
+            next /= 10
+        }
+
+    }
+
+    return claps
 }
