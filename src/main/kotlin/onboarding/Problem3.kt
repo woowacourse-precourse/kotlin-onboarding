07fda4b67e722697674f43de93903c839879807f
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-10-29 13:47:35 +0900

    Refactor: 기능목록 주석 추가, 변수명 변경 in Problem3.kt
    기능목록 주석 추가
    리턴 변수명이 cnt였는데 가독성이 떨어지는 것 같아서 answer로 변수명 변경

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -2,18 +2,24 @@ package onboarding
 
 fun solution3(number: Int): Int {
     /**
+     * 기능목록
+     * 1. 주어진 number 크기의 str 배열을 생성
+     * 2. 배열 내 각 원소의 3, 6, 9 갯수만큼 answer에 더함
+     * */
+
+    /**
      * 1. 주어진 number 크기의 str 배열을 생성
      * */
     val arrayOfNum = Array<String>(number){i -> (i + 1).toString()}
-    var cnt = 0
+    var answer = 0
 
     /**
      * 2. 배열 내 각 원소마다 3, 6, 9 갯수만큼 cnt에 더함
      * */
     for (i in arrayOfNum){
-        cnt += i.count { x -> x == '3' }
-        cnt += i.count { x -> x == '6' }
-        cnt += i.count { x -> x == '9' }
+        answer += i.count { x -> x == '3' }
+        answer += i.count { x -> x == '6' }
+        answer += i.count { x -> x == '9' }
     }
-    return cnt
+    return answer
 }
