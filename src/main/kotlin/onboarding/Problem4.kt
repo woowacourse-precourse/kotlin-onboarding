8c0a903a1b0ec99a85e69e94da5e6ebc8a17f7ff
no1msh <no1msh1217@gmail.com>
2022-10-30 02:50:14 +0900

    [Problem4][fix][feat][style] 2번 기능 수정 , 3번 기능 구현, 기능 목록 주석 수정, 상수 접근제어자 수정
    기능 목록 2번
    - 올바른 값을 리턴하지 못하던 현상을 수정
    
    기능 목록 3번
    - 엄마의 말을 변환하고 StringBuilder로 추합한뒤 형변환을 하고 반환하는 구문 구현
    
    typeCode 상수 접근제어자 변경
    - private로 제한

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,16 +3,28 @@ package onboarding
 // 기능 목록
 // 1. 대문자인지, 소문자인지 혹은 공백인지, 전부 아닌지 구분하는 메소드 구현
 // 2. 엄마의 말을 변환 청개구리 말로 변환하는 메소드 구현
-// 3. 엄마의 말을 본격적으로 변환하는 구문 구현
+// 3. 엄마의 말을 변환하고 StringBuilder로 추합한뒤 형변환을 하고 반환하는 구문 구현
 
-const val NOT_ALPHABET_SPACE = 0
-const val SPACE = 1
-const val LOWERCASE = 2
-const val UPPERCASE = 3
+// typeCode
+private const val NOT_ALPHABET_SPACE = 0
+private const val SPACE = 1
+private const val LOWERCASE = 2
+private const val UPPERCASE = 3
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    // 기능 목록 3번
+    val resultStringBuilder = StringBuilder()
+
+    for (i in word.indices){
+        val typeCode = checkChar(word[i])
+        val translatedChar = translateFrogLanguage(word[i] , typeCode)
+
+        resultStringBuilder.append(translatedChar)
+    }
+
+    return resultStringBuilder.toString()
 }
+
 
 // 기능 목록 1번
 fun checkChar(target: Char): Int {
@@ -38,11 +50,11 @@ fun translateFrogLanguage(targetChar: Char, typeCode: Int): Char {
     }
 
     if (typeCode == LOWERCASE) {
-        return targetChar + 25 - (targetChar - 'a')
+        return 'z' - (targetChar - 'a')
     }
 
     if (typeCode == UPPERCASE) {
-        return targetChar + 25 - (targetChar - 'A')
+        return 'Z' - (targetChar - 'A')
     }
 
     return targetChar
