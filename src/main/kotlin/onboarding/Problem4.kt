593e16e7daec98de86634ecc6e503a3a3fa5529e
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-10-29 17:09:30 +0900

    Feat : 문제4 리스트에 문자가 대문자일 경우 반대 대문자 저장

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -4,7 +4,15 @@ fun solution4(word: String): String {
 
     var list = ArrayList<String>()
 
-    
+    for(i in 0 until word.length) {
+
+        if (word[i].isUpperCase()) {
+            var a = word[i] - 'A'
+            list.add(('A' + 25 - a).toString())
+            continue
+        }
+    }
+
 
 }
 
