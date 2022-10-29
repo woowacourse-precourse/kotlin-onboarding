27d81465b58efb24eca9a67f933e877a10a5ee5d
Doyeop Kim <dygames@naver.com>
2022-10-29 19:11:49 +0900

    FEATURE: 각 자리 숫자가 3의 배수인지 판별하고 개수 반환

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -6,6 +6,17 @@ package onboarding
 - 손뼉 횟수 결정
 */
 
+fun processDigit(num: Int): Int {
+    var n = num
+    var r = 0
+    while (n != 0) {
+        if ((n % 10) % 3 == 0)
+            r++
+        n /= 10
+    }
+    return r
+}
+
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
