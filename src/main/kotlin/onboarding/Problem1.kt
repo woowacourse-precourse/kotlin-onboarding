7c10b8fcd0ae5547a71f9f57682c1da41cbf975b
DormunDoorlock <skrudgh2001@naver.com>
2022-10-29 22:12:08 +0900

    feat: Problem1.kt 책 페이지가 정상인지 확인하는 기능 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,26 @@
 package onboarding
 
+// Page가 정상인지 확인. 1. 왼쪽 페이지와 오른쪽 페이지가 1차이가 나는지 확인 2.왼쪽 오른쪽이 1~400 범위 인지 3.왼쪽 페이지번호가 홀수인지 확인
+fun checkPage(pages: List<Int>): Boolean{
+    var left:Int = pages[0]
+    var right:Int = pages[1]
+    println(left)
+    // 1. 왼쪽 페이지와 오른쪽 페이지가 1차이가 나는지 확인
+    if(left+1 != right)
+        return false
+    // 2. 왼쪽 페이지 번호가 1~400 인지 확인
+    if(left<=0 || left >=400)
+        return false
+    // 3. 왼쪽 페이지번호가 홀수인지 확인
+    if(left % 2 == 0)
+        return false
+    return true
+}
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
+    // pobi, crong의 책 페이지가 정상인지 확인하는 기능
+    if(!checkPage(pobi) || !checkPage(crong)) {
+        return -1
+    }
+
 }
