a2066286bc8c9d3c4cf2c4f08adfb69601334de8
zqwz <zqwz@naver.com>
2022-10-29 16:52:24 +0900

    [6번 문제] 이메일 형식에 부합하는지 확인하는 기능

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,10 +1,24 @@
 package onboarding
+
+
+const val DOMAIN = "@email.com"
+
 fun emailLength(email: String): Boolean {
     return email.length in 11 until 20
 }
+
 fun nickNameLength(nickname: String): Boolean {
     return nickname.length in 1 until 20
 }
+
+fun correctLength(email: String, nickname: String): Boolean {
+    return emailLength(email) && nickNameLength(nickname)
+}
+
+fun correctDomain(email: String): Boolean {
+    return email.contains(DOMAIN)
+}
+
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
