f142b2aa8ba650e46124220d656817c058c50fc1
kyoung_mo <kumong97@knu.ac.kr>
2022-10-29 19:38:20 +0900

    5단계

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,19 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+
+    val answer = mutableListOf<Int>(8)
+
+    answer[0] = money/50000
+    answer[1] = money%50000/10000
+    answer[2] = money%10000/5000
+    answer[3] = money%5000/1000
+    answer[4] = money%1000/500
+    answer[5] = money%500/100
+    answer[6] = money%100/50
+    answer[7] = money%50/10
+    answer[8] = money%10
+
+    return answer
+
 }
