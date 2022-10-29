e84b743ef1a74a6e88204b285c802f7e65ecab04
tmdgh1592 <tmdgh1592@naver.com>
2022-10-29 12:20:39 +0900

    feat: 주어진 길이보다 크거나 같으면 email 집합을 주어진 집합에 모두 추가하는 메서드 구현 - putAllIfLongerThan() SortedSet<String> Extension 메서드 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,13 @@
 package onboarding
 
+import java.util.SortedSet
+
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
+}
+
+private fun SortedSet<String>.putAllIfLongerThan(length: Int, emails: Collection<String>) {
+    if (emails.size >= length) {
+        this.addAll(emails)
+    }
 }
