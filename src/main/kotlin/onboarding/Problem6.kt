f19a83fe3528908ea199f61034f2f0c5a74e4a6c
YuGyeong98 <kuk3097@naver.com>
2022-10-29 22:10:17 +0900

    feature(Problem6): 예외 처리

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -4,6 +4,8 @@ fun solution6(forms: List<List<String>>): List<String> {
   var emails = mutableListOf<String>()
   val hashMap = HashMap<String, String>()
 
+  checkForms(forms)
+
   for (i in forms.indices) {
     val email = forms[i][0]
     val nickname = forms[i][1]
@@ -23,4 +25,42 @@ fun solution6(forms: List<List<String>>): List<String> {
   emails = emails.distinct().toMutableList()
 
   return emails
+}
+
+fun checkForms(forms: List<List<String>>) {
+  if (forms.size !in 1..10000) {
+    throw IllegalArgumentException(
+      "크루는 1명 이상 10,000명 이하여야 합니다."
+    )
+  }
+  for (i in forms.indices) {
+    if (forms[i][0].length !in 11..19) {
+      throw IllegalArgumentException(
+        "이메일의 전체 길이는 11자 이상 20자 미만이어야 합니다."
+      )
+    }
+  }
+  for (i in forms.indices) {
+    if (forms[i][0].split("@")[1] != "email.com") {
+      throw IllegalArgumentException(
+        "이메일은 email.com 도메인만 가능합니다."
+      )
+    }
+  }
+  for (i in forms.indices) {
+    val regex = "^[ㄱ-ㅎ가-힣]*\$".toRegex()
+    if (!forms[i][1].matches(regex)) {
+      throw IllegalArgumentException(
+        "닉네임은 한글만 가능합니다."
+      )
+    }
+  }
+  for (i in forms.indices) {
+    if (forms[i][1].length !in 1..19) {
+      throw IllegalArgumentException(
+        "닉네임의 전체 길이는 1자 이상 20자 미만이어야 합니다."
+      )
+    }
+  }
+
 }
