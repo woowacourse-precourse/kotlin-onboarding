b54b9a615ab2295f3c2cbb4bfbfb11b4f295da55
YuGyeong98 <kuk3097@naver.com>
2022-10-29 22:04:22 +0900

    feature(Problem6): 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 리스트

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,26 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+  var emails = mutableListOf<String>()
+  val hashMap = HashMap<String, String>()
+
+  for (i in forms.indices) {
+    val email = forms[i][0]
+    val nickname = forms[i][1]
+    for (j in 0..nickname.length - 2) {
+      val key = nickname.substring(j, j + 2)
+      if (hashMap.containsKey(key)) {
+        val emailFromKey = hashMap[key]
+        if (emailFromKey != email) {
+          emails.add(emailFromKey.toString())
+          emails.add(email)
+        }
+      }
+      hashMap[key] = email
+    }
+  }
+  emails = emails.sorted().toMutableList()
+  emails = emails.distinct().toMutableList()
+
+  return emails
 }
