f77ff0ce2d4e898e9ed273f8f72d484260e1ed85
cyjadela <adelacho@naver.com>
2022-10-29 20:19:37 +0900

    feat: [문제5] bills에 개수 구현 후 저장

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,34 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var bills = mutableListOf<Int>()
+    var leftMoney: Int = money
+
+    bills.add(leftMoney/50000)
+    leftMoney -= bills[0] * 50000
+
+    bills.add(leftMoney/10000)
+    leftMoney -= bills[1] * 10000
+
+    bills.add(leftMoney/5000)
+    leftMoney -= bills[2] * 5000
+
+    bills.add(leftMoney/1000)
+    leftMoney -= bills[3] * 10000
+
+    bills.add(leftMoney/500)
+    leftMoney -= bills[4] * 500
+
+    bills.add(leftMoney/100)
+    leftMoney -= bills[5] * 100
+
+    bills.add(leftMoney/50)
+    leftMoney -= bills[6] * 50
+
+    bills.add(leftMoney/10)
+    leftMoney -= bills[7] * 10
+
+    bills.add(leftMoney)
+
+    return bills
 }
