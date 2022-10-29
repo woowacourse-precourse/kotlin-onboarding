f5d5f553be48bbd03935ebd667b3d397cc4759e3
bmsk <vv99911@gmail.com>
2022-10-29 16:58:49 +0900

    [FEAT] #3 - Problem3.kt

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,49 @@
 package onboarding
 
+var numbers: Array<Int> = Array(10001) { -1 }
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    numbers[1] = 0
+    numbers[3] = 1
+    numbers[6] = 2
+    numbers[9] = 3
+
+    return calClapCount(number)
+}
+
+fun countTSN(number: Int) : Int {
+    var tempNum = number
+    var count = 0
+
+    while(tempNum != 0) {
+        val n = tempNum % 10
+        if(n == 3 || n == 6 || n == 9)
+            count++
+        tempNum /= 10
+    }
+    return count
+}
+fun findNearNum(number : Int) : Int {
+    if(number == 1) return 1
+
+    var nowNum = number - 1
+    while(nowNum > 1) {
+        if(countTSN(nowNum) != 0) break;
+        nowNum--
+    }
+
+    return nowNum
+}
+
+fun calClapCount(number : Int) : Int {
+    if(numbers[number] != -1)
+        return numbers[number]
+
+
+    val nearNum = findNearNum(number)
+    if(numbers[nearNum] == -1)
+        calClapCount(nearNum)
+
+    numbers[number] = numbers[nearNum] + countTSN(number)
+
+    return numbers[number]
 }
