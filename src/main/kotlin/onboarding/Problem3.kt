8ca05bcdcd07a52c2f54af67f8d262a1dd53dcaf
no1msh <no1msh1217@gmail.com>
2022-10-29 23:08:32 +0900

    [Problem3][style][feat] 주석 수정, 2번 기능 구현
    기능 목록 2번
    - 1부터 number까지 반복하며 1번 메소드를 호출하고 return 값에 따라 result값을 1씩 증가시킨다.

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -4,9 +4,17 @@ package onboarding
 // 1. 박수를 몇번 쳐야하는지 체크하는 메소드 구현
 // 2. 1부터 number까지 반복하며 1번 메소드를 호출하고 return 값에 따라 result값을 1씩 증가시킨다.
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    // 기능 목록 2번
+    var result = 0
+
+    for (i in 1..number) {
+        result += countClap(i)
+    }
+
+    return result
 }
 
+// 기능 목록 1번
 fun countClap(targetNumber: Int): Int {
     val targetNumberString = targetNumber.toString()
     var count = 0
