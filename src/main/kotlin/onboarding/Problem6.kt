03d87d5ba60c0e4e1af25337e8def8e0217749a5
ChunBaee <mljung10@naver.com>
2022-10-29 18:02:06 +0900

    6번] 기능 1) 예외 : 이메일의 형식(email.com)을 포함하는지, 길이는 11자 이상 20자 미만인지 판단 후 조건에 맞지 않는 값들 제거

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,20 @@
 package onboarding
 
+import java.util.regex.Pattern
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val mCrewList = forms.toMutableList()
+    for(i in mCrewList.indices) {
+        if(!checkException(mCrewList[i])) {
+            mCrewList.removeAt(i)
+        }
+    }
 }
+private fun checkException(crew: List<String>) : Boolean {
+    return checkEmailRegex(crew[0])
+}
+
+private fun checkEmailRegex(email : String) : Boolean {
+    return email.contains("email.com") && email.length in 11 until 20
+}
+
