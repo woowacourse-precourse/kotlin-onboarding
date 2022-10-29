7d56d9bcd4c649e31ce2d898304cb133e06d7899
swjw1110 <swjw1110@naver.com>
2022-10-30 01:02:29 +0900

    P6 문제 해결

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,31 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val res = mutableSetOf<String>()
+    val nameSplit = mutableMapOf<String, MutableSet<Pair<String, String>>>()
+    forms.forEach {
+        splitName(it[1]).forEach { splited ->
+            if (nameSplit[splited].isNullOrEmpty()) {
+                nameSplit[splited] = mutableSetOf(Pair(it[0], it[1]))
+            } else {
+                nameSplit[splited]?.add(Pair(it[0], it[1]))
+            }
+        }
+    }
+
+    nameSplit.forEach {
+        if (it.value.size > 1) {
+            res.addAll(it.value.map { it.first })
+        }
+    }
+
+    return res.toList().sorted()
+}
+
+fun splitName(name: String): Set<String>{
+    val res = mutableSetOf<String>()
+    for (i in 0 until name.length - 1){
+        res.add("${name[i]}${name[i + 1]}")
+    }
+    return res
 }
