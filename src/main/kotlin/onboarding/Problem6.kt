020e277116600eba6a40d6c3fe04f3cda45a6164
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-10-29 23:38:26 +0900

    Feat: Add 중복 결과 저장 및 정렬 후 반환(기능 3번, 4번), Fix 기능 2번 in Problem6.kt
    기능 3번, 4번 추가
    result를 동적할당 배열로 만들어 중복이 발생한 이메일을 할당
    .toSet()으로 중복 삭제후 정렬하여 리턴
    기능 2번 배열 index가 이름이 아닌 메일을 확인하도록 되어 있어서 수정
    문자열 중복 확인으로 count를 사용했었는데 count는 문자 하나만 확인하는 메소드여서 contains로 교체
    val 권장 부분 수정

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -9,13 +9,12 @@ fun solution6(forms: List<List<String>>): List<String> {
      *    동일한 두 글자가 있을 경우 지원자 index에 대응하는 0배열 index의 값을 1증가
      *    이미 0배열 index의 값이 0보다 클 경우 continue로 스킵
      *    지원자 비교가 끝났을 경우 다음 지원자로 넘어감
-     * 3. 0배열 index의 값이 양수 인 경우에 해당하는 지원자의 이메일의 @ 앞부분만 잘라 임시 배열에 저장
-     * 4. 임시 배열 중복 제거 후 오름차순 정렬
-     * 5. result 배열에 임시배열 값을 할당하면서 @email.com 부분을 뒤에 붙인 뒤 리턴
+     * 3. 0배열 index의 값이 양수 인 경우 result 배열에 저장
+     * 4. result 배열 중복 제거 후 오름차순 정렬 후 리턴
      * */
 
     /** 1. 지원자 수와 동일한 크기의 0으로 된 배열 선언 및 글자를 가리킬 포인터 2개 선언 */
-    var arrayOfRepeat: Array<Int> = Array(forms.size) { 0 }
+    val arrayOfRepeat: Array<Int> = Array(forms.size) { 0 }
     var left = 0
     var right = 1
 
@@ -29,13 +28,14 @@ fun solution6(forms: List<List<String>>): List<String> {
      *    지원자 비교가 끝났을 경우 다음 지원자로 넘어감
      * */
     for (i in 0 until (forms.size - 1)) {
-        while (right <= forms[i][0].length ) {
-            stringForCompare = forms[i][0][left].toString() + forms[i][1][right].toString()
+        while (right < forms[i][1].length) {
+            stringForCompare = forms[i][1][left].toString() + forms[i][1][right].toString()
             for (j in (i + 1) until forms.size) {
                 if (arrayOfRepeat[j] > 0) {
                     continue
                 }
-                if ((forms[j][0].count {x -> x.equals(stringForCompare)}) > 0) {
+                if (forms[j][1].contains(stringForCompare)) {
+                    arrayOfRepeat[i] += 1
                     arrayOfRepeat[j] += 1
                 }
             }
@@ -46,5 +46,17 @@ fun solution6(forms: List<List<String>>): List<String> {
         right = 1
     }
 
-    /** 3. 0배열 index의 값이 양수 인 경우에 해당하는 지원자의 이메일의 @ 앞부분만 잘라 임시 배열에 저장 */
+    /** 3. 0배열 index의 값이 양수 인 경우 result 배열에 저장 */
+    val result: ArrayList<String> = ArrayList()
+
+    for (i in arrayOfRepeat.indices) {
+        if (arrayOfRepeat[i] > 0) {
+            result.add(forms[i][0])
+        }
+    }
+
+    /** 4. result 배열 중복 제거 후 오름차순 정렬 후 리턴 */
+    val setOfResult = result.toSet()
+
+    return setOfResult.sorted().toList()
 }
