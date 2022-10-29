1d0aba29d60841b0c254d85576f6c37511598bc1
no1msh <no1msh1217@gmail.com>
2022-10-29 21:14:32 +0900

    [Problem2][style] 기능 목록 주석 작성

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,10 @@
 package onboarding
 
+// 기능 목록
+// 1. 해독 해야하는지 확인 하는 메소드 구현
+// 2. 중복되는 문자를 지우는 메소드 구현
+// 3. 예외처리 구현
+// 3-1 cryptogram의 길이가 1이라면 바로 리턴
 fun solution2(cryptogram: String): String {
     TODO("프로그램 구현")
 }
