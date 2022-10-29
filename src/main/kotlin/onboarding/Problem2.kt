8609c18c65139206836d9e2fdb1f55165625e869
HIBER97 <hibar9710@gmail.com>
2022-10-29 17:27:47 +0900

    2022. 10. 29. PM 5:26 --------------------------------------- test2 while문에서 break되어 나오는것 수정 replace 는 안먹힘

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,38 +1,51 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
+
     var Result = cryptogram
+    var Removed = true
 
-    var Start_idx = -1
-    var End_idx = -1
-    var Flag = false
-    var Last_Char = ' '
-    var Remove = 0
+    while (Removed){
 
-    while (true){
-        val remove_text = mutableListOf<Pair<Int, Int>>()
+        val remove_Text = mutableListOf<Pair<Int, Int>>()
+
+        var Start_idx = -1
+        var End_idx = -1
+        var Flag = false
+        var Cnt = 0
+
+        var Last_Text = ' '
+
+        //큐 생성
         Result.forEachIndexed { idx, ch ->
-            if (Last_Char == ch) {
+            if (Last_Text == ch) {
                 End_idx = idx
                 Flag = true
-            } else if (Flag) {
-                remove_text.add(Pair(Start_idx, End_idx))
+            } else {
+                if (Flag)
+                    remove_Text.add(Pair(Start_idx, End_idx))
                 Flag = false
                 Start_idx = idx
-                Last_Char = ch
+                Last_Text = ch
             }
         }
-        if (Flag) {
-            remove_text.add(Pair(Start_idx, End_idx))
+
+        if (Flag)
+            remove_Text.add(Pair(Start_idx, End_idx))
+
+        remove_Text.forEach{
+            //일치하는 텍스트 삭제 시작idx에서 끝 idx까지
+//            for (i in it.first - Cnt..it.second - Cnt +1)
+//                Result = Result.replace("") -> replace가 안먹힘
+
+            //replaceRange 사용
+            Result = Result.replaceRange(it.first - Cnt, it.second - Cnt +1,"")
+            Cnt += it.second - it.first +1
         }
 
-        remove_text.forEach{
-            Result = Result.replaceRange(it.first - Remove, it.second - Remove +1,"")
-            Result += it.second -it.second +1
-        }
+        //큐가 비었는지 확인
+        Removed = remove_Text.isNotEmpty()
 
-       if (remove_text.isEmpty())
-           break
     }
 
     return Result
