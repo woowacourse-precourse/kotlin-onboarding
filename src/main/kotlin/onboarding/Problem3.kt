fa1a304f230472c7be9b265fad60326ca4eb1eee
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-30 02:19:09 +0900

    주어진 숫자까지 자리수를 쪼개 리스트로 바꾼다.

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,14 @@
 package onboarding
 
+//기능별 목록
+//1. 1부터 주어진 숫자까지 자리수를 쪼개 리스트로 바꾼다.
+//2. 리스트들에 3 6 9가 포함 되면 그 수 만큼 박수를 카운팅 한다.
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var clap:Int = 0
+
+    for(i:Int in 1 .. number){
+        var numberlist = i.toString().map{it.digitToInt()}  //주어진 숫자까지 자리수를 쪼개 리스트로 바꾼다.
+
+    }
+    return clap
 }
