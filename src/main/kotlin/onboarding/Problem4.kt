cd5583a453a4a5e540a9b1b5201df39e31ec1227
sujin-kk <sjoo0002@gmail.com>
2022-10-30 07:03:25 +0900

    [FEAT] alphabet인지 검사

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,3 +3,16 @@ package onboarding
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
+
+fun changeWord(word: String) {
+    val dictionary = listOf('a' to 'z', )
+}
+
+fun isAlpha(ch: Char): Boolean {
+    if (ch.code in START_ASCII..END_ASCII) return true
+    return false
+}
+
+const val START_ASCII = 65
+const val END_ASCII = 122
+const val NUMBER_OF_ALPHA = 26
