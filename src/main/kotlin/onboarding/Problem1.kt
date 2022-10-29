8da1e369400261a2b6ed8f9f9e4803915ebb7631
kyounggudev <homin222@naver.com>
2022-10-30 01:54:57 +0900

    문제1 - match 함수 추가 및 리팩토링

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,51 +1,51 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    if(!checkException(pobi)||!checkException(crong)){
+    if (!checkException(pobi) || !checkException(crong)) {
         return -1
     }
     val pobi_num = calculate(pobi)
     val crong_num = calculate(crong)
-    return when(pobi_num-crong_num){
-        in 1..300 -> 1
-        0 -> 0
-        else ->2
-    }
+    return match(pobi_num, crong_num)
 }
 
-//예외 입력값 검토
-fun checkException(list : List<Int>): Boolean {
-    //왼쪽이 홀수, //오른쪽이 왼쪽 바로 다음 페이지//왼쪽은 3 이상 397이하
-    return when((list[0]%2==1)&&
-            (list[1]==list[0]+1)&&
-            (list[0]>=3)&&
-            (list[0]<=397)){
+fun checkException(list: List<Int>): Boolean {
+    return when ((list[0] % 2 == 1) &&
+            (list[1] == list[0] + 1) &&
+            (list[0] >= 3) &&
+            (list[0] <= 397)) {
         true -> true
         false -> false
     }
 }
 
-//calculate2에서 구한 왼쪽, 오른쪽 값 비교하기
-fun calculate(list : List<Int>):Int{
+fun calculate(list: List<Int>): Int {
     val left = calculate2(list[0])
     val right = calculate2(list[1])
-    return when(left>right){
+    return when (left > right) {
         true -> left
         false -> right
     }
 }
 
-// 쪽 자릿수의 합, 곱을 비교하여 큰 값 구하기
-fun calculate2(par_num : Int) : Int{
+fun calculate2(par_num: Int): Int {
     var num = par_num
-    var sumAndProduct = mutableListOf<Int>(0,1)
-    do{
+    var sumAndProduct = mutableListOf<Int>(0, 1)
+    do {
         sumAndProduct[0] += num % 10
-        sumAndProduct[1] *=num%10
+        sumAndProduct[1] *= num % 10
         num /= 10
-    }while(num>0)
-    return when(sumAndProduct[0] > sumAndProduct[1]) {
+    } while (num > 0)
+    return when (sumAndProduct[0] > sumAndProduct[1]) {
         true -> sumAndProduct[0]
         false -> sumAndProduct[1]
+    }
+}
+
+fun match(pobi_num: Int, crong_num: Int): Int {
+    return when (pobi_num - crong_num) {
+        in 1..300 -> 1
+        0 -> 0
+        else -> 2
     }
 }
