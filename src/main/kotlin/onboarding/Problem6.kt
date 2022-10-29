8c0783bf388edb0e8b8f15b646473a8dcadf76d7
tmdgh1592 <tmdgh1592@naver.com>
2022-10-29 12:23:59 +0900

    feat: 주어진 닉네임의 부분 문자열을 Key로 가지고, 부분 문자열을 포함하는 유저 이메일 집합을 Value로 가지는 Map 반환 메서드 구현 - createSubNames() 메서드 구현 - constructSubNames() Extension 메서드 구현 - putSubNamesToEmail() Extension 메서드 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -2,8 +2,48 @@ package onboarding
 
 import java.util.SortedSet
 
+private const val subNameMinimumLength = 2
+
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
+}
+
+private fun createSubNames(forms: List<List<String>>): Map<String, MutableSet<String>> {
+    val emailsOfSubNames = mutableMapOf<String, MutableSet<String>>()
+
+    forms.forEach { userInfo ->
+        val email = userInfo[0]
+        val name = userInfo[1]
+
+        emailsOfSubNames.constructSubNames(email, name)
+    }
+
+    return emailsOfSubNames
+}
+
+private fun MutableMap<String, MutableSet<String>>.constructSubNames(email: String, name: String) {
+    val nameLength = name.length
+
+    if (nameLength < 2) {
+        return
+    }
+
+    for (subNameLength in subNameMinimumLength..nameLength) {
+        this.putSubNamesToEmail(name, email, subNameLength)
+    }
+}
+
+private fun MutableMap<String, MutableSet<String>>.putSubNamesToEmail(name: String, email: String, subNameLength: Int) {
+    val start = 0
+    val end = (name.length) - subNameLength
+
+    for (index in start..end) {
+        val subWord = name.slice(index until index + subNameLength)
+        val newEmailSet = this[subWord] ?: mutableSetOf(email)
+
+        newEmailSet.add(email)
+        this[subWord] = newEmailSet
+    }
 }
 
 private fun SortedSet<String>.putAllIfLongerThan(length: Int, emails: Collection<String>) {
