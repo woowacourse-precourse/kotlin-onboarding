ea391b4cbd3606a1931db7f43293e70025315ab2
Min-Jae-Bae <qoalswo123@icloud.com>
2022-10-29 23:20:07 +0900

    5번 문제 테스트[성공]

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,24 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+
+    val moneyList = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val moneyCountList = mutableListOf<Int>()
+    var restMoney = money
+
+    moneyList.map { it }.forEach {
+        val moneyCount = restMoney / it
+
+        if (moneyCount != 0) {
+            moneyCountList.add(moneyCount)
+            restMoney -= moneyCount * it
+        }
+
+        if (moneyCount == 0) {
+            moneyCountList.add(0)
+        }
+    }
+
+    return moneyCountList
+
 }
