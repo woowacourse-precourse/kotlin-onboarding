4cf015c3c40d0c45b861c04df2751bdb990346de
no1msh <no1msh1217@gmail.com>
2022-10-29 22:50:27 +0900

    [Problem3][style] 기능 목록 주석 작성
    1. 박수를 쳐야하는지 체크하는 메소드 구현
    2. 1부터 number까지 반복하며 1번 메소드를 호출하고 return 값에 따라 result값을 1씩 증가시킨다.

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,8 @@
 package onboarding
 
+// 기능 목록
+// 1. 박수를 쳐야하는지 체크하는 메소드 구현
+// 2. 1부터 number까지 반복하며 1번 메소드를 호출하고 return 값에 따라 result값을 1씩 증가시킨다.
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
