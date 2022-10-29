886bad1d5f76b00bb2ff0995fc7020500cdcd265
Min-Jae-Bae <qoalswo123@icloud.com>
2022-10-29 21:18:54 +0900

    4번 문제 테스트[성공]

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,11 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    return word.toCharArray().map {
+        when (it) {
+            in 'A'..'Z' -> ('A'.code + 'Z'.code- it.code).toChar()
+            in 'a'..'z' -> ('a'.code + 'z'.code - it.code).toChar()
+            else -> it
+        }
+    }.joinToString("")
 }
