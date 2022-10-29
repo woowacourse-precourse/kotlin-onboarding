d06f83e29c468628b7906cdb34c040c507655067
zqwz <zqwz@naver.com>
2022-10-29 16:51:01 +0900

    [6번 문제] 이메일의 길이가 부합하는지 확인하는 기능

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,4 +1,7 @@
 package onboarding
+fun emailLength(email: String): Boolean {
+    return email.length in 11 until 20
+}
 
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
