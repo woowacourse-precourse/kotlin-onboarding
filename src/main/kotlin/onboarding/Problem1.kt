09115d85f71afa6d2a955335fd61bce3687ff734
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-10-29 21:47:23 +0900

    :sparkles: 페이지의 예외상황 값 체크

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    return 0
+}
+
+fun checkStraightNum(list : List<Int>) : Boolean{
+    return list[0]+1==list[1]
+}
+
+fun checkOverNumPage(list : List<Int>) : Boolean{
+    return !(list[0]>400 || list[1]>400 || list[0]<1 || list[1]<1)
+}
+
+fun checkInputValue(list1 : List<Int>, list2 : List<Int>) : Boolean{
+    return checkStraightNum(list1) && checkOverNumPage(list1) && checkStraightNum(list2) && checkStraightNum(list2)
 }
