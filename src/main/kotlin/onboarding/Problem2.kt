8d75f92c5b007a0cde6e7fb3321ad72f26e21ae2
cyjadela <adelacho@naver.com>
2022-10-29 19:50:36 +0900

    feat: [문제2] removeDuplication 함수 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,23 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var result: String = ""
+
+    fun removeDuplication(originString: String): String {
+        var resultString: String = ""
+
+        resultString.plus(originString[0]).also { resultString = it }
+
+        for (i in 1..originString.length - 2) {
+            if ((originString[i - 1] != originString[i]) && (originString[i] != originString[i + 1])) {
+                resultString.plus(originString[i]).also { resultString = it }
+            }
+        }
+
+        resultString.plus(originString[originString.length - 1]).also { resultString = it }
+
+        return resultString
+    }
+
+    return result
 }
