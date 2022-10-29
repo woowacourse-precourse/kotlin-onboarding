6c6fd1e306c9daf460f48cf6bd89bb9a7f9d9424
tmdgh1592 <tmdgh1592@naver.com>
2022-10-29 14:03:34 +0900

    refactor: addFriend() 메서드에서 User 자신의 경우에는 친구 목록에 추가하지 않도록 수정

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -8,22 +8,24 @@ fun solution7(
     TODO("프로그램 구현")
 }
 
-private fun createFriendsMap(friendRelations: List<List<String>>): Map<String, List<String>> {
+private fun createFriendsMap(friendRelations: List<List<String>>, user: String): Map<String, List<String>> {
     val friendsMap = mutableMapOf<String, MutableList<String>>()
 
     friendRelations.forEach { relation ->
         val friendA = relation[0]
         val friendB = relation[1]
 
-        friendsMap[friendA] = addFriend(friendsMap[friendA], friendB)
-        friendsMap[friendB] = addFriend(friendsMap[friendB], friendA)
+        friendsMap[friendA] = addFriend(friendsMap[friendA], user, friendB)
+        friendsMap[friendB] = addFriend(friendsMap[friendB], user, friendA)
     }
 
     return friendsMap
 }
 
-private fun addFriend(friends: MutableList<String>?, newFriend: String): MutableList<String> {
+private fun addFriend(friends: MutableList<String>?, user: String, newFriend: String): MutableList<String> {
     return (friends ?: mutableListOf()).apply {
-        add(newFriend)
+        if (user != newFriend) {
+            add(newFriend)
+        }
     }
 }
