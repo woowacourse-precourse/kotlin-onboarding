e4df5c20103602693fe96dfa4a2ad07c89213d16
DormunDoorlock <skrudgh2001@naver.com>
2022-10-30 13:06:14 +0900

    feat: Problem4.kt 변환된 문자열 반환 기능 추가

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,13 @@
 package onboarding
-
+fun reverseAlphabet(num: Int):Int{
+    return 97
+}
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var result=""
+    for(w in word){
+        val data = w.code
+        // 문자열 변환
+        result += reverseAlphabet(data).toChar()
+    }
+    return result
 }
