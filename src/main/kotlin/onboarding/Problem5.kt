71bd0228fdbac055b1f7d7cbfc750d6854d7878e
Yerin <myheaven_b@naver.com>
2022-10-31 00:20:40 +0900

    refactor: apply coding convention

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -2,11 +2,11 @@ package onboarding
 
 fun solution5(money: Int): List<Int> {
     val bills: List<Int> = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
-    var result = mutableListOf<Int>()
+    var result:MutableList<Int> = mutableListOf()
     var balance: Int = money
 
     for (bill in bills){
-        result.add(balance/bill)
+        result.add(balance / bill)
         balance %= bill
     }
     return result
