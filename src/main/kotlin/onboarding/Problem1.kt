d0db29fe54b4564233747f6d09819726ee705303
hyunji1203 <hj27720@gmail.com>
2022-10-30 15:37:52 +0900

    문제1 - 예외상황 처리

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,22 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    //pobi case
+    //예외처리1 : 페이지 범위를 벗어난 경우
+    for (i in pobi){
+        if ( i <= 2 || i >= 399){
+            return -1
+        }
+    }
+    //예외처리2 : 연이은 페이지 번호가 아닌 경우
+    if (pobi[1]-pobi[0] != 1){
+        return -1
+    }
+    //예외처리3 : 리스트의 크기가 2가 아닌 경우
+    if (pobi.size != 2){
+        return -1
+    }
+
+
+
 }
