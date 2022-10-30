13195f41848749566817830ed7c49abed1ab0760
Suheon Song <ssheon0812@naver.com>
2022-10-30 23:25:11 +0900

    feat: [Problem5] convert 함수 추가

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,17 @@
 package onboarding
 
+fun convert(money: Int): List<Int> {
+    val changeArray = arrayOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val result = mutableListOf<Int>()
+
+    var tempMoney = money
+    for (change in changeArray) {
+        result.add(tempMoney / change)
+        tempMoney -= (change * (tempMoney / change))
+    }
+    return result
+}
+
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    return convert(money)
 }
