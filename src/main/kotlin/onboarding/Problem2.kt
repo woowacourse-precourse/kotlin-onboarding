efd4aaa69a895f34363a847aaa3c7c91dc0a4864
s9hn <tnghqn@naver.com>
2022-10-31 05:02:07 +0900

    [REFACTOR(미션2)] 함수분리 및 정상 작동 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,30 +1,41 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
+    return checkDuplication(cryptogram)
+}
 
+fun checkDuplication(cryptogram: String): String {
+    var word = cryptogram
     var resultCrypto = ""
-    var i = 0
     var checkDuplication = false
 
 
 
-    while (i < cryptogram.length - 1) {
+    for (j in 0 until cryptogram.length / 2) {
+        word += " "
+        for (i in 0 until word.length - 1) {
 
-        if (cryptogram[i] != cryptogram[i + 1]) {
-            if (checkDuplication) {
-                i++
-                checkDuplication = false
+            if (word[i] != word[i + 1]) {
+                if (checkDuplication) {
+
+                    checkDuplication = false
+                } else {
+
+                    resultCrypto += word[i]
+                }
             } else {
-                resultCrypto += cryptogram[i]
-                i++
-            }
 
-        } else {
-            checkDuplication = true
-            i++
+                checkDuplication = true
+            }
         }
 
+
+        word = resultCrypto
+        checkDuplication = false
+        resultCrypto = ""
+
     }
-    return resultCrypto
-    println(resultCrypto)
+
+    return word
+
 }
