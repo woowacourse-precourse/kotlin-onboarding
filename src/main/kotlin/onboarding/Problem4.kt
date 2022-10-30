c955fc6b0c53903f6fd174c77b7b82ac08f30831
ChoHJin <cjc9739@naver.com>
2022-10-31 04:26:09 +0900

    Problem4 solved

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -2,7 +2,7 @@
 * 기능 목록
 * 1. 알파벳 변환
 * 1-1. 문자열의 문자를 하나씩 검사하여 해당 문자가 대문자인지 소문자인지 확인한다.
-* 1-2. 문자가 대문자라면, 대문자 범위를 합한 변수인 upperAlpha 를 char값으로 바꿔준 후 해당되는 문자를 빼주고 그 값을 char값으로 바꿔준다. (소문자도 같음)
+* 1-2. 문자가 대문자라면, Z - (해당문자 - 'A') 를 해준다. (소문자도 동일한 방식으로 진행)
 * 1-3. 위의 과정이 끝나면 변환이 끝난 문자를 빈 문자열인 tmpString 에 붙여준다.
 * 1-4. 알파벳이 아닌 문자라면 그냥 문자열에 붙여준다.
 * */
@@ -15,15 +15,12 @@ fun convertAlpha(str : String) : String {
     var upperAlphaRange = 'A' .. 'Z'
     var lowerAlphaRange = 'a' .. 'z'
 
-    var upperAlpha = 155    //65 + 90
-    var lowerAlpha = 219    //97 + 122
-
     for(c in str) {
         if(c in upperAlphaRange) {
-            tmpString += (upperAlpha.toChar() - c).toChar()
+            tmpString += 'Z' - (c - 'A')
         }
         else if(c in lowerAlphaRange) {
-            tmpString += (lowerAlpha.toChar() - c).toChar()
+            tmpString += 'z' - (c - 'a')
         }
         else tmpString += c
     }
