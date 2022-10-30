c6a1306ddca99294cd6f32f138387c42d8474cd9
jiingom <vvankatam0@gmail.com>
2022-10-30 22:26:48 +0900

    feat: 문제 6번 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -8,7 +8,22 @@ fun solution6(forms: List<List<String>>): List<String> {
 
     index = getBaseCrew(forms)
 
+    for (i in 0 until forms[index][1].length-1) {
+        val check = forms[index][1].substring(i, i+2)
+        result.addAll(searchDuplicatedCrew(forms, check))
+    }
+
     return result.distinct().sorted().toList()
+}
+
+fun searchDuplicatedCrew(forms: List<List<String>>, checkString: String): List<String> {
+    val duplicatedCrew = mutableListOf<String>()
+    for (element in forms) {
+        if (element[1].contains(checkString)) {
+            duplicatedCrew.add(element[0])
+        }
+    }
+    return duplicatedCrew
 }
 
 fun getBaseCrew(forms: List<List<String>>): Int {
