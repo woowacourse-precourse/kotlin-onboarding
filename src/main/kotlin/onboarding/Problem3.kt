fa3d04b26985e67be9da2f6858443b86ac3f588c
최우석 <chws0508@naver.com>
2022-10-30 22:21:29 +0900

    problem3 클린코드로 수정

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,20 +1,20 @@
 package onboarding
+
 /*
 기능항목 정리
 1. 각 자릿수를 구하는 기능
 2. 각 자릿수마다 3,6,9가 있는지 확인하는 기능
  */
 fun solution3(number: Int): Int {
-    var answer:Int=0
-    for(i in 1..number){ //1부터 매개변수 숫자까지 루프를 돌려줍니다.
-        var temp:Int=i
-        while(temp!=0){
-            when(temp%10){ //각 자리수마다 3,6,9가 있는지 확인합니다
-                3,6,9->answer++ //3,6,9 중 숫자가 있다면 answer값을 1 증가시킵니다.
-                else->{}
-            }
-            temp /= 10
+    var answer = 0
+    for (i in 1..number) {
+        var presentNumber: Int = i
+        while (presentNumber != 0) {
+            when (presentNumber % 10) {3, 6, 9 -> answer++}
+            presentNumber /= 10
         }
     }
     return answer
 }
+
+
