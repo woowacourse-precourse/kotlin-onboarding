0659406ea538a0d301cae7231ae3d22da9d6416d
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-31 02:26:25 +0900

    changemoney함수 리스트 추가 및 간소화

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -7,46 +7,17 @@ fun solution5(money: Int): List<Int> {
 
     return changemoney(money)
 }
-
+//주어진 돈을 변환하는 함수
 fun changemoney(money: Int): MutableList<Int>{
     val solutionList:MutableList<Int> = mutableListOf(0,0,0,0,0,0,0,0,0)   //변환한 돈을 저장한 리스트를 만든다.
+    val unit:List<Int> = listOf(50000,10000,5000,1000,500,100,50,10,1)  //단위 리스트
     var change:Int= money
 
-    if(money>=50000){   //주어진 돈을 주어진 화폐의 큰 단위부터 변환한다.
-        solutionList[0]=money/50000
-        change = money-(50000*solutionList[0])
+    for(i:Int in unit.indices){
+        if(change>=unit[i]){
+            solutionList[i]=change/unit[i]
+            change -= (unit[i] * solutionList[i])
+        }
     }
-    if(change>=10000){
-        solutionList[1]=change/10000
-        change -= (10000 * solutionList[1])
-    }
-    if(change>=5000){
-        solutionList[2]=change/5000
-        change -= (5000 * solutionList[2])
-    }
-    if(change>=1000){
-        solutionList[3]=change/1000
-        change -= (1000 * solutionList[3])
-    }
-    if(change>=500){
-        solutionList[4]=change/500
-        change -= (500 * solutionList[4])
-    }
-    if(change>=100){
-        solutionList[5]=change/100
-        change -= (100 * solutionList[5])
-    }
-    if(change>=50){
-        solutionList[6]=change/50
-        change -= (50 * solutionList[6])
-    }
-    if(change>=10){
-        solutionList[7]=change/10
-        change -= (10 * solutionList[7])
-    }
-    if(change>=1){
-        solutionList[8]=change
-    }
-
     return solutionList
 }
