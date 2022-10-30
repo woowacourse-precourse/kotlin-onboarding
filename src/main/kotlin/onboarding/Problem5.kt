89ed79c868936d79d9573e670da3b629c940ba5c
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-10-30 15:49:20 +0900

    문제 5 : Feat: 생성한 함수들을 사용하여 result 배열 출력

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,6 +1,6 @@
 package onboarding
 
-val unit = arrayListOf<Int>(50000,10000,5000,1000,500,50,10,1)
+val unit = arrayListOf<Int>(50000,10000,5000,1000,500,100,50,10,1)
 
 fun solution5(money: Int): List<Int> {
 
@@ -8,7 +8,15 @@ fun solution5(money: Int): List<Int> {
 
     var now_money = money
 
+    for(i in 0 until unit.size){
+        result.add(i, pay_money(i, now_money))
+        now_money = change_money(i , now_money)
+    }
+
+    return result
+
 }
+
 fun pay_money(i : Int , money: Int) : Int{
     return money / unit[i]
 }
