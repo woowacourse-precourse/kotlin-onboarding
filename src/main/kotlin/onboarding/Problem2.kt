4585d7136ab41d11a6c0109cff95e71784d30433
Hyuna Kim <akxmcse@gmail.com>
2022-10-30 21:38:19 +0900

    feat: problem2 연속 중복 문자 제거하는 메인 함수 구현
    >> test case 2번 failed

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,24 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    return removeReduplication(cryptogram)
+}
+
+private fun removeReduplication(string: String): String {
+    var index = 0
+    var string= string
+
+    while (index < string.length) {
+        var compareIndex = 0
+        while (compareIndex < string.length) {
+            if (string[compareIndex] == string[compareIndex+1]) {
+                string = string.removeRange(IntRange(compareIndex, compareIndex+1))
+                break
+            } else {
+                compareIndex++
+            }
+        }
+        index++
+    }
+    return string
 }
