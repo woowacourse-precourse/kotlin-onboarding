425c2c75ced2f6c4eba5a9d83ebb59d6af4a8700
kyounggudev <homin222@naver.com>
2022-10-30 22:50:32 +0900

    문제6 - 리팩토링

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,21 +1,22 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    var result_email : MutableSet<String> = mutableSetOf()
-    for(nickname in forms){
-        for(i in 0 until nickname[1].length-1) {
-            var res = forms.filter { it[1].contains("${nickname[1][i]}" + "${nickname[1][i+1]}") }
-            if(res.size>1){
-              for(i in 0 until res.size){
-                  result_email.add(res[i][0])
-              }
-            }
-        }
-    }
-    var result : List<String> = result_email.toList()
+    val resultEmail = checkOverlap(forms)
+    val result = resultEmail.toList()
     return result.sorted()
 }
 
-fun checkOverlap(nickname : String, forms : List<List<String>>){
-
+fun checkOverlap(forms: List<List<String>>): MutableSet<String> {
+    val resultEmail: MutableSet<String> = mutableSetOf()
+    for (nickname in forms) {
+        for (i in 0 until nickname[1].length - 1) {
+            val filtered = forms.filter { it[1].contains("${nickname[1][i]}" + "${nickname[1][i + 1]}") }
+            if (filtered.size > 1) {
+                for (element in filtered) {
+                    resultEmail.add(element[0])
+                }
+            }
+        }
+    }
+    return resultEmail
 }
