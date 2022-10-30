6b148354952644de437a3497d87f752808bb1a19
jiingom <vvankatam0@gmail.com>
2022-10-30 22:25:11 +0900

    feat(Problem6): 문자열 비교할 기준 크루 찾기 기능

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,23 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    //TODO("프로그램 구현")
+
+    val result = mutableListOf<String>()
+    var index = 0
+
+    index = getBaseCrew(forms)
+
+    return result.distinct().sorted().toList()
+}
+
+fun getBaseCrew(forms: List<List<String>>): Int {
+    var index = 0
+    for (i in forms.indices) {
+        if (forms[i][1].length >= 2) {
+            index = i
+            break
+        }
+    }
+    return index
 }
