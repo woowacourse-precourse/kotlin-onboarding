5e9906805ddc950ce677f49541d0b372d818fee4
s9hn <tnghqn@naver.com>
2022-10-30 15:54:26 +0900

    [FEAT(미션2)] 문자열 중복검사

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,32 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var count = 0
+    var temp = 8888
+    var result = 0
+
+    while (temp != 0){
+        result
+    }
+
+
+    for (i in 1..10000) {
+        val a = i / 10
+        val b = i % 10
+
+
+        if ((a == 3) or (a == 6) or (a == 9)) {
+            count++
+            if ((b == 3) or (b == 6) or (b == 9)) {
+                count++
+                continue
+            }
+        } else if ((b == 3) or (b == 6) or (b == 9)) {
+            count++
+        }
+
+    }
+
+
+   return count
 }
