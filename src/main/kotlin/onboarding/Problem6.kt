ac0918eac5a2b4e71bb4ffffea7f017c5edd7f58
sujin-kk <sjoo0002@gmail.com>
2022-10-31 00:12:08 +0900

    [FEAT] 이메일 형식 부합 확인

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,3 +3,22 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
+fun isWrongEmail(email: String): Boolean {
+    val (_, domain) = email.split("@")
+
+    return when {
+        !EMAIL_PATTERN.toRegex().matches(email) -> true
+        email.length !in EMAIL_MIN_LEN..EMAIL_MAX_LEN -> true
+        domain != EMAIL_DOMAIN -> true
+        else -> false
+    }
+}
+
+const val EMAIL_DOMAIN = "email.com"
+const val EMAIL_PATTERN = """^[a-zA-Z]+@[a-zA-Z]+\\.[a-z]+${'$'}"""
+const val EMAIL_MIN_LEN = 11
+const val EMAIL_MAX_LEN = 19
+const val NICKNAME_PATTERN = """/[가-힣]+$/"""
+const val NICKNAME_MIN_LEN = 1
+const val NICKNAME_MAX_LEN = 19
