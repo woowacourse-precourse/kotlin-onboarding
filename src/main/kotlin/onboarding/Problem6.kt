3e7e575f76f32d902539dbb41ab09f0c25ce26db
jjiiin <wjdwlin4051@gmail.com>
2022-10-30 15:57:18 +0900

    feat: Problem6 중복된 닉네임의 이메일 목록 작성
    Problem6.kt 해당 닉네임이 다른 닉네임과 연속적으로 중복될 경우 해당 닉네임의 이메일을 목록에 추가한다.

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,44 @@
 package onboarding
 
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val crewNicknameList = ArrayList<String>()
+    val crewNicknameEmailHashMap : HashMap<String, String> = hashMapOf()
+    val emailNotificationList = ArrayList<String>()
+
+    for( (i, _ ) in forms.withIndex() ){
+
+        crewNicknameEmailHashMap.put(forms[i][1] , forms[i][0])
+        crewNicknameList.add(forms[i][1])
+    }
+    
+    for ( i in 0..forms.size-1){
+        val notificationNickname = compareNickname(forms[i][1], crewNicknameList)
+        
+            if( notificationNickname != null ) {
+                crewNicknameEmailHashMap
+                    .get(notificationNickname)
+                    ?.let { emailNotificationList.add(it) }
+            }
+    }
+    
+    return emailNotificationList.sorted()
 }
+
+fun compareNickname(nickname:String,crewNicknameList: ArrayList<String>) : String? {
+
+    for(i in 0..nickname.length-2) {
+        val substringWord = nickname.substring(i, i + 2)
+
+        for (crewNickname in crewNicknameList) {
+
+            if( nickname != crewNickname && crewNickname.contains(substringWord)){
+                return nickname
+            }
+        }
+    }
+    return null
+}
+
