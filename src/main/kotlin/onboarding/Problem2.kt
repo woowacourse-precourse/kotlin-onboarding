80f9295825817e4af32118134dcd8f6a647dd16b
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-10-30 20:48:35 +0900

    Problem2  함수명 수정

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,11 +1,11 @@
 package onboarding
 
 
-fun solution2checkSuccessiveChar(word:String, successiveCharRegex:Regex): Boolean {
+fun checkSuccessiveChar(word:String, successiveCharRegex:Regex): Boolean {
     return successiveCharRegex.containsMatchIn(word)
 }
 
-fun solution2replaceSuccessiveChar(word:String, successiveCharRegex:Regex): String{
+fun replaceSuccessiveChar(word:String, successiveCharRegex:Regex): String{
     return word.replace(successiveCharRegex, "")
 }
 
@@ -13,8 +13,8 @@ fun solution2(cryptogram: String): String {
     var resultString=cryptogram
     //연속해서 같은 문자(소문자 알파벳)이 포함되는 경우를 확인하기 위한 정규식
     val successiveCharRegex="""a{2,}|b{2,}|c{2,}|d{2,}|e{2,}|f{2,}|g{2,}|h{2,}|i{2,}|j{2,}|k{2,}|l{2,}|m{2,}|n{2,}|o{2,}|p{2,}|q{2,}|r{2,}|s{2,}|t{2,}|u{2,}|v{2,}|w{2,}|x{2,}|y{2,}|z{2,}""".toRegex()
-    while(solution2checkSuccessiveChar(resultString, successiveCharRegex)){
-        resultString=solution2replaceSuccessiveChar(resultString, successiveCharRegex)
+    while(checkSuccessiveChar(resultString, successiveCharRegex)){
+        resultString=replaceSuccessiveChar(resultString, successiveCharRegex)
     }
 
    return resultString
