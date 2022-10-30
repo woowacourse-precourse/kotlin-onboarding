d01ae0f629e2925a3232b1fa38e146d0184d2f25
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-10-30 18:20:57 +0900

    :sparkles: 돈 분류 로직 추가

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,19 @@
 package onboarding
 
+private var currentMoney = 0
+private var result = mutableListOf<Int>()
+private var moneyUnits = listOf<Int>(50000,10000,5000,1000,500,100,50,10,1)
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    currentMoney = money
+    filterMoney()
+    return result
+}
+private fun filterMoney(){
+    for(i in 0..8){
+        result.add(currentMoney/moneyUnits[i])
+        getCurrentMoney(i)
+    }
+}
+private fun getCurrentMoney(index : Int){
+
 }
