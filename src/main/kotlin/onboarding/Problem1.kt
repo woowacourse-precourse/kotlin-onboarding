f42e1bc24eec7d9c8c25969573dafd65e956f421
Hyuna Kim <akxmcse@gmail.com>
2022-10-30 16:41:30 +0900

    fix: problem1 예외사항 체크 함수 오류 수정

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -20,18 +20,18 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 
 private fun checkNoException(pobi: List<Int>, crong: List<Int>): Boolean {
     return (checkPageRange(pobi[0], pobi[1])
-            || checkPageRange(crong[0], crong[1])
-            || checkValidPage(pobi[0], pobi[1])
-            || checkValidPage(crong[0], crong[1])
-            || checkPairPage(pobi[0], pobi[1])
-            || checkPairPage(crong[0], crong[1]))
+            && checkPageRange(crong[0], crong[1])
+            && checkValidPage(pobi[0], pobi[1])
+            && checkValidPage(crong[0], crong[1])
+            && checkPairPage(pobi[0], pobi[1])
+            && checkPairPage(crong[0], crong[1]))
 }
 
 private fun checkPageRange(leftPage: Int, rightPage: Int) = (leftPage > 1 && rightPage < 400)
 
-private fun checkValidPage(leftPage: Int, rightPage: Int) = (rightPage - leftPage == 1)
+private fun checkValidPage(leftPage: Int, rightPage: Int) = (leftPage%2 == 1 && rightPage%2 == 0)
 
-private fun checkPairPage(leftPage: Int, rightPage: Int) = (leftPage%2 == 1 && rightPage%2 == 0)
+private fun checkPairPage(leftPage: Int, rightPage: Int) = (rightPage - leftPage == 1)
 
 private fun calculatePoint(page: List<Int>): Int {
     var calculatedNumList: MutableList<Int> = mutableListOf()
