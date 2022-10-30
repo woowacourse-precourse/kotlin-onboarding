7b24111b1a086e84893ecd00513fe009af31e706
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-10-31 00:01:19 +0900

    [추가] 두 글자가 겹치는지 확인하는 Boolean 함수

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,3 +3,16 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
+fun duplication(nickname1: String, nickname2: String): Boolean {
+    for (i in 0 until nickname1.length - 1) {
+        for (j in 0 until nickname2.length - 1) {
+            if (nickname1[i] == nickname2[j]) {
+                if (nickname1[i + 1] == nickname2[j + 1]) {
+                    return true
+                }
+            }
+        }
+    }
+    return false
+}
