fdcc523eb02482f823eeffbfaa3b8b7d8eb3f8ab
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-10-30 20:48:53 +0900

    Problem3 함수명 수정

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,6 +1,6 @@
 package onboarding
 
-fun solution3calcClapCount(number: Int): Int {
+fun calcClapCount(number: Int): Int {
     return number.toString().count { c -> c == '3' } +
             number.toString().count { c -> c == '6' } +
             number.toString().count { c -> c == '9' }
@@ -9,7 +9,7 @@ fun solution3calcClapCount(number: Int): Int {
 fun solution3(number: Int): Int {
     var totalClapCount = 0
     for (eachNum in 1..number) {
-        totalClapCount += solution3calcClapCount(eachNum)
+        totalClapCount += calcClapCount(eachNum)
     }
 
     return totalClapCount
