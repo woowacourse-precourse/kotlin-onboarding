1baa094cc28dab96348ed5404e8642504fa4b397
Doyeop Kim <dygames@naver.com>
2022-10-30 19:58:34 +0900

    FEATURE: 두 글자 이상 중복되는 문자열 판별

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -6,6 +6,26 @@ package onboarding
 - 중복되는 닉네임이 있는지 판별
 */
 
+fun findDuplicatedSubstring(string1: String, string2: String): Boolean {
+    var isCharEqual = false
+
+    for (i in string1.indices) {
+        var tempIdx = i
+        isCharEqual = false
+        for (j in string2.indices) {
+            if (tempIdx < string1.length && string1[tempIdx] == string2[j]) {
+                if (isCharEqual)
+                    return true
+                isCharEqual = true
+                tempIdx++
+            } else {
+                isCharEqual = false
+            }
+        }
+    }
+    return false
+}
+
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
