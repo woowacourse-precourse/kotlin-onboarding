e1b1d644f14e8e685e305c0c780b8fb231be6eb6
boogieWoogie <fantasy7772@konkuk.ac.kr>
2022-10-30 19:13:33 +0900

    Problem7 friend의 이름을 int값으로 mapping하는 함수 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,4 +1,22 @@
 package onboarding
+fun solution7mappingToInt(friends:List<List<String>>, visitors:List<String>) : HashMap<String, Int>{
+    val friendsId=HashMap<String, Int>()
+    friends.forEach { friendRelation ->
+        val (friendA, friendB)=arrayOf(friendRelation[0], friendRelation[1])
+        var id=0
+
+        if(!friendsId.containsKey(friendA)){
+            friendsId[friendA]=id++
+        }
+
+        if(!friendsId.containsKey(friendB)){
+            friendsId[friendB]=id++
+        }
+    }
+
+    return friendsId
+}
+
 
 fun solution7(
     user: String,
