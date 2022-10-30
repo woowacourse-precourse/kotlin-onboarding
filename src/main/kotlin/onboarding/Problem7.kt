3dda41c991a1d1b1f81048dfe9568692a6ac1adc
jjiiin <wjdwlin4051@gmail.com>
2022-10-30 16:21:53 +0900

    feat: Problem7 사용자 아이디 user의 친구 목록 추출 함수 구현
    Problem7.kt 사용자 아이디 user와 이미 친구인 아이디를 목록으로 만드는 함수 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,34 @@
 package onboarding
 
+import java.lang.Math.abs
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val userFriendList : ArrayList<String> = userFriendExtract(user,friends)
+    var friendsWithScore : HashMap<String, Int> = hashMapOf()
+
+
+
+    return friendsWithScore.toList().sortedWith(compareBy({-it.second}, {it.first})).toMap().keys.toList()
 }
+
+fun userFriendExtract(user: String, friends: List<List<String>>): ArrayList<String>{
+    val userFriendList = ArrayList<String>()
+    //user의 친구목록 만들기
+    for ((i, _) in friends.withIndex()) {
+        if (friends[i].contains(user)) {
+            if (friends[i].indexOf(user) == 1) {
+                userFriendList.add(friends[i][0])
+            } else {
+                userFriendList.add(friends[i][1])
+            }
+        }
+    }
+    return userFriendList
+}
+
+
+
