49670eb2391c1290d1c0d4be29a5bb692d74d999
dohun_ <silver2679@gmail.com>
2022-10-31 03:43:30 +0900

    Feat Problem5: moneyType 선언, PROBLEM5.md 기능 구현 목록 추가

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,6 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val moneyType = arrayListOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1) //돈을 큰수부터 나누기 위해 내림차순으로 저장
+
 }
