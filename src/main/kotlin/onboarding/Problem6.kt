d3671f6b93a0baa4a316c7188b23d032aa3ea662
pingu244 <69796976+pingu244@users.noreply.github.com>
2022-10-30 23:52:03 +0900

    Chore: [6번 문제] 코드 순서 변경

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -5,10 +5,11 @@ fun solution6(forms: List<List<String>>): List<String> {
     val answer = arrayListOf<String>()
 
     for(member in forms.indices) {
-        for (alphabet in 0 until forms[member][1].length-1){
-            for (compare in member+1 until forms.size){
-                val nickname = forms[member][1]
-                val email = forms[member][0]
+        val nickname = forms[member][1]
+        val email = forms[member][0]
+
+        for (alphabet in 0 until forms[member][1].length-1) {
+            for (compare in member+1 until forms.size) {
                 val compareNickname = forms[compare][1]
                 val compareEmail = forms[compare][0]
 
