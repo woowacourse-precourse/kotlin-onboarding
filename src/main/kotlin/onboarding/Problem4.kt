5dfa6e202ad57ca894d451c59fe57624cc626013
s9hn <tnghqn@naver.com>
2022-10-31 05:41:50 +0900

    [FEAT(미션4)] 소문자 리스트 구현 && 리드미 해당 기능체크

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+
+}
+
+fun mappingLowerCase(): MutableMap<Char, Char> {
+    var temp = 0
+    val lowerCase = mutableMapOf<Char, Char>()
+    for (i in 'a'..'z') {
+        temp++
+        val reverseLowerCase = (123 - temp).toChar()
+        lowerCase[i] = reverseLowerCase
+
+    }
+    return lowerCase
 }
