ba516cf4118fcfb99e40121cb1f013e2dc99a6d0
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-10-30 22:26:22 +0900

    Fix: Modify 기능 5번 in Problem7.kt
    테스트 케이스 실행 시 완전 다른 값이 나와서 디버그로 데이터 흐름 추적으로 다음과 같은 문제들을 확인 후 수정
    1. idScoreMap에 user의 점수가 들어가 있음
     - 기능목록 3번의 36번째 줄에 'and !coFriend.contains(user)'를 추가하여 걸러냄
    2. idScoreMap에 이미 친구인 id가 들어있음
     - 기능목록 4번에서 이미 친구인 id를 예외처리 하지 않아서 발생
       visitorsWithoutFriends 라는 MutableList를 만들어서 visitors 전체를 할당
       listOfFriends 배열 내 값이 있을 경우 remove로 삭제하고 기존 기능 진행
    3. 정렬되어 리턴된 값이 테스트와 다름
     - sort 부분을 단순 오름차순 정렬만하고 있어서 내림차순+int 기준으로 정렬한 다음 String으로 정렬하도록 수정
       또한 기존에는 정렬된 값을 바로 리턴하게 했으나 문제에서 최대 5개 결과를 리턴하도록 명시하고 있어 리턴되는 배열 크기에 제약이 필요하여 MutableList인 result를 만들고 초기값을 넣지 않음
       다음 for문을 만들어 정렬된 값을 result에 하나 씩 담는데 횟수가 5이상일 경우 break하도록 함
       그리고 결과를 리턴

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -15,8 +15,8 @@ fun solution7(
      * */
 
     /** 1. 아이디와 점수를 담을 MutableMap 선언, user의 친구들을 담을 ArrayList 선언 */
-    var idScoreMap: MutableMap<String, Int> = mutableMapOf()
-    var listOfFriends: ArrayList<String> = ArrayList()
+    val idScoreMap: MutableMap<String, Int> = mutableMapOf()
+    val listOfFriends: ArrayList<String> = ArrayList()
 
     /** 2. friends에서 user 친구들을 ArrayList에 할당(for) */
     for (friend in friends) {
@@ -33,7 +33,7 @@ fun solution7(
     /** 3. ArrayList의 요소들과 friends 요소들을 체크하여 MutableMap에 담고 10점 추가(for) */
     for (friend in listOfFriends) {
         for (coFriend in friends) {
-            if (coFriend.contains(friend)) {
+            if (coFriend.contains(friend) and !coFriend.contains(user)) {
                 if (coFriend[0] == friend) {
                     idScoreMap[coFriend[1]] = idScoreMap[coFriend[1]]?.plus(10) ?: 10
                 }
@@ -45,13 +45,27 @@ fun solution7(
     }
 
     /** 4. visitors 목록을 MutableMap에 할당하고 횟수 당 1점 추가 */
-    for (visitor in visitors) {
+    val visitorsWithoutFriends = visitors.toMutableList()
+
+    for (friend in listOfFriends) {
+        visitorsWithoutFriends.remove(friend)
+    }
+
+    for (visitor in visitorsWithoutFriends) {
         idScoreMap[visitor] = idScoreMap[visitor]?.plus(1) ?: 1
     }
 
     /** 5. MutableMap의 Value를 기준으로 정렬 후 리턴 */
     val idScoreList = idScoreMap.toList()
-    val sortedIdScoreList = idScoreList.sortedBy { it.second }
+    val sortedIdScoreList = idScoreList.sortedWith(compareByDescending<Pair<String, Int>> { it.second }.thenBy { it.first })
+    val result:MutableList<String> = mutableListOf()
 
-    return listOf(sortedIdScoreList[0].first, sortedIdScoreList[1].first, sortedIdScoreList[2].first)
+    for (i in sortedIdScoreList.indices) {
+        if (i >= 5) {
+            break
+        }
+        result.add(sortedIdScoreList[i].first)
+    }
+
+    return result.toList()
 }
