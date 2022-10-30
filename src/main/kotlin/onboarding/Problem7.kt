f0f9186e1307272470840bd538b03f144b760d0e
yulim <yulim13sky@naver.com>
2022-10-31 00:16:35 +0900

    [REFACTOR] Problem7 함수로 변경

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -9,20 +9,50 @@ fun solution7(
     val relation = mutableMapOf<String, MutableSet<String>>()
     val score = mutableMapOf<String, Int>()
 
+    makeRelationMap(friends, relation)
+    makeScoreMapByFriendsList(relation, user, score)
+    makeScoreMapByVisitorsList(visitors, relation, user, score)
+    exceptUserAndFriendsOfUser(score, relation, user)
+
+    return score.toList()
+        .sortedBy { it.first }
+        .sortedByDescending { it.second }
+        .filter { it.second > 0 }
+        .map { it.first }
+        .let {
+            if (it.size > 5) it.subList(0, 5)
+            else it
+        }
+}
+
+private fun makeRelationMap(friends: List<List<String>>, relation: MutableMap<String, MutableSet<String>>) {
     for (i in friends) {
         if (relation.containsKey(i[0])) relation[i[0]]!!.add(i[1])
         else relation[i[0]] = mutableSetOf(i[1])
         if (relation.containsKey(i[1])) relation[i[1]]!!.add(i[0])
         else relation[i[1]] = mutableSetOf(i[0])
     }
+}
 
+private fun makeScoreMapByFriendsList(
+    relation: MutableMap<String, MutableSet<String>>,
+    user: String,
+    score: MutableMap<String, Int>
+) {
     relation[user]?.forEach { userFriend ->
         relation[userFriend]?.forEach { friend ->
             if (score.containsKey(friend)) score[friend] = score[friend]!!.plus(10)
             else score[friend] = 10
         }
     }
+}
 
+private fun makeScoreMapByVisitorsList(
+    visitors: List<String>,
+    relation: MutableMap<String, MutableSet<String>>,
+    user: String,
+    score: MutableMap<String, Int>
+) {
     for (i in visitors) {
         if (!relation.containsKey(user) || relation[user]?.contains(i) == false) {
             if (score.containsKey(i)) {
@@ -30,16 +60,16 @@ fun solution7(
             } else score[i] = 1
         }
     }
+}
 
+private fun exceptUserAndFriendsOfUser(
+    score: MutableMap<String, Int>,
+    relation: MutableMap<String, MutableSet<String>>,
+    user: String
+) {
     score.forEach { (key, _) ->
         if (relation[user]?.contains(key) == true || key == user) {
             score[key] = 0
         }
     }
-
-    return score.toList().sortedBy { it.first }.sortedByDescending { it.second }.filter { it.second > 0 }
-        .map { it.first }.let {
-            if (it.size > 5) it.subList(0, 5)
-            else it
-        }
 }
