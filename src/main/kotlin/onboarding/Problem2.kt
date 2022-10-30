e360d2b52c77d32c4ee04a13e55f997bcce302ba
kyounggudev <homin222@naver.com>
2022-10-30 22:17:28 +0900

    문제2 - 리팩토링

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,19 +1,18 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    val decode = decoding(cryptogram)
-    return decode
+    return decoding(cryptogram)
 }
 
-fun decoding(cryptogram: String) : String{
-    var decode=""
+fun decoding(cryptogram: String): String {
+    var decode = ""
     var encode = cryptogram
-    do{
+    do {
         val alphabet = encode[0]
-        if(encode.count{c -> c == alphabet }%2==1){
+        if (encode.count { c -> c == alphabet } % 2 == 1) {
             decode = decode.plus(alphabet)
         }
-        encode = encode.replace(alphabet.toString(),"")
-    }while(encode.isNotEmpty())
+        encode = encode.replace(alphabet.toString(), "")
+    } while (encode.isNotEmpty())
     return decode
 }
