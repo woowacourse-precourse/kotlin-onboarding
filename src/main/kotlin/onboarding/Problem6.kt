f3865d8078eb3b06f4a4fe5d210a9f72d46bd1dd
Doyeop Kim <dygames@naver.com>
2022-10-30 20:24:18 +0900

    FEATURE: 중복되는 닉네임이 있는지 판별

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -26,6 +26,24 @@ fun findDuplicatedSubstring(string1: String, string2: String): Boolean {
     return false
 }
 
+fun findDuplicatedNickname(forms: List<List<String>>): List<String> {
+    val r = mutableListOf<String>()
+    for (i in forms.indices) {
+        var isDuplicated = false
+        for (j in 0 until forms.size - 1) {
+            if (i != j && findDuplicatedSubstring(forms[i][1], forms[j][1])) {
+                if (!r.contains(forms[j][0]))
+                    r.add(forms[j][0])
+                isDuplicated = true
+            }
+        }
+        if (isDuplicated && !r.contains(forms[i][0]))
+            r.add(forms[i][0])
+    }
+    r.sort()
+    return r
+}
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    return findDuplicatedNickname(forms)
 }
