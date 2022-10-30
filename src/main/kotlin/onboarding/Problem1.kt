be4eb68e63e336957a4ffe9732186628dc9c21ce
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-10-30 15:14:06 +0900

    :art: 문제 1번 함수 private로 변경

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -4,14 +4,20 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     return 0
 }
 
-fun checkStraightNum(list : List<Int>) : Boolean{
+private fun checkStraightNum(list : List<Int>) : Boolean{
     return list[0]+1==list[1]
 }
 
-fun checkOverNumPage(list : List<Int>) : Boolean{
+private fun checkOverNumPage(list : List<Int>) : Boolean{
     return !(list[0]>400 || list[1]>400 || list[0]<1 || list[1]<1)
 }
 
-fun checkInputValue(list1 : List<Int>, list2 : List<Int>) : Boolean{
+private fun checkInputValue(list1 : List<Int>, list2 : List<Int>) : Boolean{
     return checkStraightNum(list1) && checkOverNumPage(list1) && checkStraightNum(list2) && checkStraightNum(list2)
+}
+
+private fun calculateEachDigitSum(value : Int) : Int{
+    return value.toString().map {
+        it.code - '0'.code
+    }.sum()
 }
