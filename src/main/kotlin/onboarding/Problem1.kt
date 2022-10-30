fa3b99ab1eec1ed7d65fa87b62d0bd74b76d8b0f
Sujin Kim <sujinkim919@gmail.com>
2022-10-30 21:00:48 +0900

    feat: 상수값 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,17 @@
 package onboarding
 
+class Const {
+    companion object {
+        const val WIN_POBI = 1
+        const val WIN_CRONG = 2
+        const val DRAW = 0
+        const val EXCEPTION = -1
+
+        const val MIN_PAGE = 1
+        const val MAX_PAGE = 400
+    }
+}
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     var scoreOfPobi = 0
     var scoreOfCrong = 0
@@ -9,11 +21,11 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 
 fun getWinner(poby:Int, crong:Int) : Int {
     if(poby > crong) {
-        return 1;
+        return Const.WIN_POBI;
     } else if(poby < crong) {
-        return 2;
+        return Const.WIN_CRONG;
     }
     else {
-        return 0;
+        return Const.DRAW;
     }
 }
