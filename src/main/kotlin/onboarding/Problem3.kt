ae89e2a0f8e8500d10056c13da55af6126dfa766
sonac.kim <104493771+kimsonac@users.noreply.github.com>
2022-10-30 19:07:43 +0900

    Solve problem 3, 5

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,25 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var count = 0
+    var cur = 0
+    var tmp = 0
+
+    for(i in 1 until number + 1){
+        cur = i
+        tmp = count
+
+        while(cur != 0){
+
+            var num = cur % 10
+
+            if(num == 3 || num == 6 || num == 9){
+                count++
+            }
+            cur /= 10
+        }
+    }
+
+    return count
+
 }
