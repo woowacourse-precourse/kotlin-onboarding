7b80480912de6531ccc768ab2c2b17b689578fef
hyunji1203 <hj27720@gmail.com>
2022-10-30 19:37:08 +0900

    문제6 - 이메일 중복 방지를 위한 mutableSet 선언

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,8 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    //이메일 중복 기록 방지를 위한 mutableSet 선언
+    var emailSet = mutableSetOf<String>()
+
+
 }
