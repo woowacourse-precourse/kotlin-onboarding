04bbb336fffe1d327776b1b8ec7e455d72603cf1
changhwan <chang5hustle@gmail.com>
2022-10-31 01:11:44 +0900

    [Problem 1] (요구사항 4) 최대값 비교 결과값 도출 로직 작성

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -7,31 +7,41 @@ const val INPUT_EXCEPTION = -1
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     if (isInputException(pobi, crong)) return INPUT_EXCEPTION
+
+    val pobiPoint = calculateMaxPoint(pobi)
+    val crongPoint = calculateMaxPoint(crong)
+
+    return when {
+        pobiPoint > crongPoint -> POBI_WIN
+        pobiPoint < crongPoint -> CRONG_WIN
+        pobiPoint == crongPoint -> DRAW
+        else -> throw IllegalStateException("의도하지 않은 코드")
+    }
 }
 
 private fun isInputException(pobi: List<Int>, crong: List<Int>): Boolean {
-    val tempList = listOf<List<Int>>(pobi, crong)
+    val tempList = listOf(pobi, crong)
 
     tempList.forEach {
-        if (isPageRange(it)) return true
-        if (isContinuedNumber(it)) return true
-        if (isLeftPageOddNumber(it)) return true
-        if (isValidPage(it)) return true
+        if (isNotInPageRange(it)) return true
+        if (isNotContinuedNumber(it)) return true
+        if (isNotLeftPageOddNumber(it)) return true
+        if (isStartOrEndPage(it)) return true
     }
 
     return false
 }
 
-private fun isPageRange(pages: List<Int>): Boolean {
+private fun isNotInPageRange(pages: List<Int>): Boolean {
     for (i: Int in pages) if (i !in 1..400) return true
     return false
 }
 
-private fun isContinuedNumber(pages: List<Int>): Boolean = pages[0] - pages[1] != 1
+private fun isNotContinuedNumber(pages: List<Int>): Boolean = pages[1] - pages[0] != 1
 
-private fun isLeftPageOddNumber(pages: List<Int>): Boolean = pages[0] % 2 != 1
+private fun isNotLeftPageOddNumber(pages: List<Int>): Boolean = pages[0] % 2 != 1
 
-private fun isValidPage(pages: List<Int>): Boolean = pages[0] == 1 || pages[1] == 400
+private fun isStartOrEndPage(pages: List<Int>): Boolean = pages[0] == 1 || pages[1] == 400
 
 private fun calculateMaxPoint(pages: List<Int>): Int = maxOf(
     calculatePlusPoint(pages[0]),
@@ -39,7 +49,6 @@ private fun calculateMaxPoint(pages: List<Int>): Int = maxOf(
     calculateMultiplyPoint(pages[0]),
     calculateMultiplyPoint(pages[1])
 )
-
 
 private fun calculatePlusPoint(page: Int): Int {
     var maxPoint = 0
@@ -64,4 +73,3 @@ private fun calculateMultiplyPoint(page: Int): Int {
 
     return maxPoint
 }
-
