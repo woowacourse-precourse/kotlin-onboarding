cc5e6a116f6d95eafbffda9a266dd3abab2a4633
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-10-30 22:18:55 +0900

    [추가] 입력된 문자열에서 중복을 체크하며 중복일 경우 중복 문자 삭제

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,35 @@
 package onboarding
 
+import java.util.*
+
 fun solution2(cryptogram: String): String {
     TODO("프로그램 구현")
+}
+
+fun deleteDuplication(cryptogram: String): String {
+    var st = Stack<Char>()
+    var check = false
+    st.push(cryptogram[0])
+    var preChar = cryptogram[0]
+    for (i in 1 until cryptogram.length) {
+        if (preChar == cryptogram[i]) {
+            preChar = cryptogram[i]
+            check = true
+        }
+        if (preChar == cryptogram[i] && i == (cryptogram.length - 1)) {
+            st.pop()
+        }
+        if (preChar != cryptogram[i]) {
+            preChar = cryptogram[i]
+            if (!check) {
+                st.push(cryptogram[i])
+            }
+            if (check) {
+                st.pop()
+                st.push(preChar)
+                check = false
+            }
+        }
+    }
+    TODO()
 }
