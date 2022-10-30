738abb21cca764f72565fc50ed276ca7802929ac
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-31 01:08:59 +0900

    문자<->리스트 변환

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+//기능별 목록
+//1.주어진 문자를 리스트로 변환
+//2.대문자 일 때 아스키 코드값을 이용한 변경
+//3.소문자 일 때 아스키 코드값을 이용한 변경
+//4.리스트를 문자로 변환
+
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var WordList:MutableList<Char> = word.toMutableList()   //문자를 리스트로 변환
+
+    
+    return WordList.joinToString("")    //리스트를 문자로 변환
+
 }
