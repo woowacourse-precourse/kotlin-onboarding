686008c01d22fefc3a2c963eb6a0588095741900
Yerin <myheaven_b@naver.com>
2022-10-31 02:30:55 +0900

    refactor: apply coding convention

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -2,7 +2,7 @@ package onboarding
 
 fun solution2(cryptogram: String): String {
     var result: String = cryptogram
-    var regex: Regex = "((\\w)\\2{1,})".toRegex()
+    var regex: Regex = "(\\w)\\1+".toRegex()
 
     while(regex.findAll(result).count() > 0){
         result = result.replace(regex, "")
--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,19 +1,17 @@
 package onboarding
 
+import java.util.SortedMap
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    var friendList: List<String>
-    var non_friendList: Map<String, Int>
-    var result = mutableListOf<String>()
     var i:Int = 0
-
-    friendList = checkFriends(user, friends)
-    non_friendList = checkNonFriends(user, friends, friendList, visitors)
-
-    var sortedMap = non_friendList.toList().sortedByDescending { it.second }
+    var friendsList: List<String> = checkFriends(user, friends)
+    var nonFriendsList: Map<String, Int> = checkNonFriends(user, friends, friendsList, visitors)
+    var sortedMap: List<Pair<String, Int>> = nonFriendsList.toList().sortedByDescending { it.second }
+    var result: MutableList<String> = mutableListOf()
 
     while (i < 5 && i < sortedMap.size){
         if (sortedMap[i].second == 0)
@@ -41,7 +39,7 @@ fun checkNonFriends(user: String,
                     friendList: List<String>,
                     visitors: List<String>
 ): Map<String, Int> {
-    var list = mutableMapOf<String, Int>()
+    var list: MutableMap<String, Int> = mutableMapOf()
 
     for (friend in friends) {
         if (friend[0] == user || friend[1] == user)
@@ -50,15 +48,15 @@ fun checkNonFriends(user: String,
             continue
         if (friend[0] in friendList) {
             if (list.containsKey(friend[1]))
-                list.put(friend[1], list.get(friend[1])!! + 10)
+                list[friend[1]] = list[friend[1]]!! + 10
             else
-                list.put(friend[1], 10)
+                list[friend[1]] = 10
         }
         if (friend[1] in friendList) {
             if (list.containsKey(friend[0]))
-                list.put(friend[0], list.get(friend[0])!! + 10)
+                list[friend[0]] = list[friend[0]]!! + 10
             else
-                list.put(friend[0], 10)
+                list[friend[0]] = 10
         }
     }
 
@@ -66,9 +64,9 @@ fun checkNonFriends(user: String,
         if (visitor in friendList)
             continue
         if (list.containsKey(visitor))
-            list.put(visitor, list.get(visitor)!! + 1)
+            list[visitor] = list[visitor]!! + 1
         else
-            list.put(visitor, 1)
+            list[visitor] = 1
     }
     return list
 }
