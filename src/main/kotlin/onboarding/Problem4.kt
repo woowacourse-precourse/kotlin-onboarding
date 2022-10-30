ae3fd59bfd49963d81378ff0e3502af2780d7d5d
WoeunJung <leena09123@naver.com>
2022-10-31 01:31:07 +0900

    solve: prob4 - treeFrog's word

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,11 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var result = ""
+    for (i in word){
+        result += if (i.isUpperCase()) 'Z' - (i-'A')
+                  else if (i.isLowerCase()) 'z' - (i-'a')
+                  else i
+    }
+    return result
 }
