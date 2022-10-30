d53f58194d92faf6052640c3b44fc49f62d69e3d
Doyeop Kim <dygames@naver.com>
2022-10-30 21:58:09 +0900

    DOCS: 7번 문제 기능 목록 정의

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,5 +1,14 @@
 package onboarding
 
+/*
+기능 목록
+- 사용자 친구 목록 구하기
+- 친구 추천 점수 결정
+- 추천 친구 목록 출력
+*/
+var usersFriends = mutableListOf<String>()
+var friendsRecommendScore = mutableMapOf<String, Int>()
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
