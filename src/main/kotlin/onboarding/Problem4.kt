aa6e344e09f4a0a8032ec704b4ce2b904cdc6a50
jiingom <vvankatam0@gmail.com>
2022-10-30 21:02:31 +0900

    feat: 문제 4번 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,24 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+
+    var alphabet = listOf<Char>('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'M', 'O', 'P'
+        ,'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+    alphabet = alphabet.reversed()
+
+    val s = "I love you"
+    var ans = ""
+    for (i in s.indices) {
+        if (s[i].isUpperCase()) {
+            ans += ('A' + ('Z' - s[i]))
+        } else if (s[i].isLowerCase()) {
+            ans += ('a' + ('z' - s[i]))
+        } else if (s[i] == ' ') {
+            ans += (' ')
+        } else {
+            ans += (s[i])
+        }
+    }
+
+    return ans
 }
