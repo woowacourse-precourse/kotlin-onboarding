5c9d4b9348cb5c25ec74cab02f8fd387725198aa
yulim <yulim13sky@naver.com>
2022-10-30 23:59:48 +0900

    [REFACTOR] Problem6 변수 추가

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -7,8 +7,9 @@ fun solution6(forms: List<List<String>>): List<String> {
         val name = forms[indexList][1]
         val email = forms[indexList][0]
         for (nameIndex in 0 until name.length - 1) {
+            val twoLetters = name.substring(nameIndex, nameIndex + 2)
             for (index in forms.indices) {
-                if (forms[index][1].contains(name.substring(nameIndex, nameIndex + 2)) && indexList != index) {
+                if (forms[index][1].contains(twoLetters) && indexList != index) {
                     result.add(email)
                 }
             }
