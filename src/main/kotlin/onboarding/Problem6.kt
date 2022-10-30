b4abc98562bf7224e4b8fa03f97a03bd25951370
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-10-30 18:37:34 +0900

    :sparkles: 닉네임을 통한 이메일 필터 기능 추가

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -4,7 +4,7 @@ private var result = mutableListOf<String>()
 private var content = HashMap<String, MutableSet<String>>()
 fun solution6(forms: List<List<String>>): List<String> {
     repeatAllNickName(forms)
-//    repeatAllNickNameForFilter(forms)
+    repeatAllNickNameForFilter(forms)
     result = result.distinct().toMutableList()
     result.sort()
     return result
@@ -24,5 +24,19 @@ private fun addSplitNickName(forms : List<List<String>>, i : Int, nickNameIndex:
     }
     else {
         content.put(forms[i][1].substring(nickNameIndex, nickNameIndex+2), mutableSetOf(forms[i][0]))
+    }
+}
+private fun repeatAllNickNameForFilter(forms : List<List<String>>){
+    for (i in 0 until forms.size) {
+        filterEmail(forms,i)
+    }
+}
+private fun filterEmail(forms : List<List<String>>,i : Int){
+    var checkPoint = content.filter {
+        forms[i][1].contains(it.key)
+        it.value.size>1
+    }.values
+    for(j in checkPoint.toList()){
+        result.addAll(j)
     }
 }
