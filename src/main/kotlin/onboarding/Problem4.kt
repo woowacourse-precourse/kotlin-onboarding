62bb5eaf4af9b6ab5b53013329225265932a8903
최우석 <chws0508@naver.com>
2022-10-30 22:24:46 +0900

    problem4 클린코드로 수정

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,15 +1,18 @@
 package onboarding
+
 /*
 기능항목 정리
 1. 소문자, 대문자, 알파벳이 아닌 문자들을 구분하는 기능
+1. 소문자, 대문자, 그리고 공백을 구분하는 기능
 2. 알파벳을 규칙대로 치환하는 기능
  */
 fun solution4(word: String): String {
     return word.map {
-        when(it){
-            in 'a'..'z'-> 'a'+('z'-it)//소문자인 경우
-            in 'A'..'Z'-> 'A'+('Z'-it)//대문자인 경우
-            else -> it// 알파벳이 아닌 경우
+        when (it) {
+            in 'a'..'z' -> 'a' + ('z' - it)
+            in 'A'..'Z' -> 'A' + ('Z' - it)
+            else -> it
         }
     }.joinToString("")
 }
+
