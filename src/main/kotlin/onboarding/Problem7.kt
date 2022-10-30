900c175c7772593a4577440ee5e26ec5e60f6160
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-10-30 16:03:38 +0900

    feat:기능목록 2번 기능 추가[문제7]

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,11 +1,16 @@
 package onboarding
 
+import java.util.*
+import kotlin.collections.HashSet
+import kotlin.collections.LinkedHashMap
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
     TODO("프로그램 구현")
+    val friend = HashSet<String>()
 }
 
 fun exception7(user : String, friends: List<List<String>>, visitors: List<String>) {
@@ -31,4 +36,50 @@ fun exception_id(id : String) {
     if(!id.matches("^[a-z]*S".toRegex())) {
         throw IllegalArgumentException("아이디는 소문자여야 합니다.")
     }
+}
+
+fun connectEdge(friends: List<List<String>>) : LinkedHashMap<String, LinkedList<String>> {
+
+    val graph = LinkedHashMap<String, LinkedList<String>>()
+
+    friends.forEach {
+        graph.run {
+            set(it[0], LinkedList())
+            set(it[1], LinkedList())
+        }
+    }
+
+    friends.forEach {
+        graph.get(it[0])?.add(it[1])
+        graph.get(it[1])?.add(it[0])
+    }
+    return graph
+}
+
+fun get_friend(user : String ,friends: List<List<String>>) : Set<String> {
+
+    val graph = connectEdge(friends)
+    println(graph.toString())
+
+    return graph.get(user)!!.toSet()
+}
+
+fun get_nearFriend(user : String , friends: List<List<String>>) : Set<String> {
+
+    val near_friend = HashSet<String>()
+
+    val graph = connectEdge(friends)
+    val friendList = get_friend(user, friends)
+
+    friendList.forEach {
+        val list = graph.get(it)
+
+        list!!.forEach {
+
+        if(!it.equals(user))
+            near_friend.add(it)
+        }
+    }
+
+    return near_friend
 }
