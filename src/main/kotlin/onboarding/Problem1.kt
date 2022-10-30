254c293ed0a4641c9d56fb3b9c1af33804da8956
Hyuna Kim <akxmcse@gmail.com>
2022-10-30 16:21:40 +0900

    feat: problem1 최대 숫자 구하는 함수 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,7 @@
 package onboarding
 
+import kotlin.math.max
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     if(!checkNoException(pobi, crong)) {
         return -1
@@ -21,3 +23,33 @@ private fun checkPageRange(leftPage: Int, rightPage: Int) = (leftPage > 1 && rig
 private fun checkValidPage(leftPage: Int, rightPage: Int) = (rightPage - leftPage == 1)
 
 private fun checkPairPage(leftPage: Int, rightPage: Int) = (leftPage%2 == 1 && rightPage%2 == 0)
+
+private fun calculatePoint(page: List<Int>): Int {
+    var calculatedNumList: MutableList<Int> = mutableListOf()
+
+    calculatedNumList.add(calculateMaxNum(page[0]))
+    calculatedNumList.add(calculateMaxNum(page[1]))
+    calculatedNumList.sortedDescending()
+
+    return calculatedNumList[0]
+}
+
+private fun calculateMaxNum(page: Int): Int{
+    var page = page
+    var plusPage = 0
+    var multiplyPage= 1
+    var maxNum = 0
+    while(page > 0) {
+        plusPage += page%10
+        multiplyPage *= page%10
+        page /= 10
+    }
+
+    maxNum = if(plusPage < multiplyPage) {
+        multiplyPage
+    } else {
+        plusPage
+    }
+
+    return maxNum
+}
