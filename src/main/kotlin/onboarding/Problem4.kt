5788e456a17f136cd4900f2e5689362e0e8273fd
Suheon Song <ssheon0812@naver.com>
2022-10-30 21:23:27 +0900

    feat: [Problem4] convert 함수 추가

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,22 @@
 package onboarding
 
+fun convert(character: Char): Char {
+    // 소문자일 때
+    if ('a' <= character && character <= 'z') {
+        return 'a' + (25 - (character - 'a')) % 26
+    }
+    // 대문자일 때
+    else if ('A' <= character && character <= 'Z'){
+        return 'A' + (25 - (character - 'A')) % 26
+    } else {
+        return character
+    }
+}
+
+fun translate(word: String) {
+
+}
+
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
