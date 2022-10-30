a71f930994989837458f50f2dc9af5edf941e949
Hyuna Kim <akxmcse@gmail.com>
2022-10-31 02:25:53 +0900

    feat: problem6 중복된 문자를 확인하는 함수 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,3 +3,10 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
+private fun checkReduplication(nickname1: String, nickname2: String): Boolean {
+    for(i in 0..nickname1.length-2){
+        if(nickname2.contains(nickname1.substring(IntRange(i, i+1)))) return true
+    }
+    return false
+}
