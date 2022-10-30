8866b69c485e0a335e6ed9438c21de594671758d
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-10-30 22:38:49 +0900

    [추가] 숫자에 3, 6, 9가 들어가는 횟수 구하는 기능

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -3,3 +3,14 @@ package onboarding
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
+
+fun count(num: Int): Int {
+    var input = num
+    var cnt = 0
+    while (input != 0) {
+        if (input % 10 == 3 || input % 10 == 6 || input % 10 == 9)
+            cnt++
+        input /= 10
+    }
+    return cnt
+}
