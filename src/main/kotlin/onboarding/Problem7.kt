affa8e71fdeca815edc2cc3d3b93d776ab290b5e
jiingom <vvankatam0@gmail.com>
2022-10-31 00:00:35 +0900

    feat(Problem7): 각 사용자별 친구목록 작성 기능 추가

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -6,7 +6,9 @@ fun solution7(
     visitors: List<String>
 ): List<String> {
     var map = HashMap<String, Int>()
+
     map = setUniqueNumber(friends)
+    var graph: Array<MutableList<String>> = addFriends(friends, user, map)
 
     TODO("프로그램 구현")
 }
@@ -25,4 +27,22 @@ fun setUniqueNumber(friends: List<List<String>>): HashMap<String, Int> {
         }
     }
     return map
+}
+
+fun addFriends(friends: List<List<String>>, user: String, map: HashMap<String, Int>): Array<MutableList<String>> {
+    var graph = Array(friends.size){ mutableListOf<String>() }
+    for (i in friends.indices) {
+        val (a, b) = friends[i].map { it.toString() }
+        if (a == user) {
+            graph[map[a]!!].add(b)
+        }
+        else if (b == user) {
+            graph[map[b]!!].add(a)
+        }
+        else {
+            graph[map[a]!!].add(b)
+            graph[map[b]!!].add(a)
+        }
+    }
+    return graph
 }
