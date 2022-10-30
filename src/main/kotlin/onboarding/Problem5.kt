bb0ad4a1fcc65a2fd57e1527d54fba06c3426204
hyunji1203 <hj27720@gmail.com>
2022-10-30 16:03:34 +0900

    문제5 - 제한사항 위반 사례 처리

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,12 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var moneyList = mutableListOf(0,0,0,0,0,0,0,0,0)
+    var money = money
+
+    //제한사항 위반 사례 처리
+    if (money < 1 || money > 1000000){
+        return moneyList
+    }
+
 }
