60d9444c157695da7bd2e484299e577457a101a8
re4rk <37167652+re4rk@users.noreply.github.com>
2022-10-30 09:21:15 +0900

    FEAT : solution5 함수 추가

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,12 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val units = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    var changes = money
+
+    return units.map {
+        val count = changes / it
+        changes %= it
+        count
+    }.toList()
 }
