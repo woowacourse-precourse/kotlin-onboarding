709045c43c5330b4ef3ace4e9dc5ce7e4af0cb35
s9hn <tnghqn@naver.com>
2022-10-30 16:51:27 +0900

    [REFACTOR(미션3)] 각 숫자 3,6,9 검사
    - 세 자리, 네 자리수까지 가능하게 구현
    - 테스크 케이스 추가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,22 +1,21 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-
+    return loopCheck(number)
 }
 
-fun loopCheck(number: Int) {
+fun loopCheck(number: Int): Int {
 
     var count = 0
-
     for (i in 1..number) {
         var tempNumber = i
 
         while (tempNumber > 0) {
-            if ((i % 10 == 3) or (i % 10 == 6) or (i % 10 == 9)) {
+            if ((tempNumber % 10 == 3) or (tempNumber % 10 == 6) or (tempNumber % 10 == 9)) {
                 count++
             }
             tempNumber /= 10
         }
     }
-
+    return count
 }
