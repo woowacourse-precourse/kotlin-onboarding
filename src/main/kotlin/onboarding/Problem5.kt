61702f73eb4b38be4601c2e13059a42d9a3b7b59
no1msh <no1msh1217@gmail.com>
2022-10-30 15:27:46 +0900

    [Problem5][feat] 2번 기능 구현
    기능 목록 2번
    - 1번 리스트를 반복하며 각 화폐로 나누는 메소드 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -3,10 +3,24 @@ package onboarding
 // 기능 목록
 // 1. 사용할 화폐의 종류를 나열한 리스트 생성
 // 2. 1번 리스트를 반복하며 각 화폐로 나누는 메소드 구현
-// 3. 2번 메소드로 반환된 값을 리스트로 만들어 반환하는 구문 구현
+// 3. 2번 메소드로 반환된 값을 반환하는 구문 구현
 
 // 기능 목록 1번
-private val moneyList = listOf<Int>(50000,10000,5000,1000,500,100,50,10,1)
+private val moneyList = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
 fun solution5(money: Int): List<Int> {
     TODO("프로그램 구현")
+}
+
+// 기능 목록 2번
+fun exchangeMoney(money: Int): List<Int> {
+    var tempMoney = money
+    val result = mutableListOf<Int>(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    var index = 0
+
+    for (currency in moneyList) {
+        result[index] = tempMoney / currency
+        tempMoney -= currency * result[index++]
+    }
+
+    return result.toList()
 }
