7d8d952218804c83fbe9f01eb7ab4859dd521576
Suheon Song <ssheon0812@naver.com>
2022-10-30 20:55:02 +0900

    feat: [Problem3] 3, 6, 9의 개수를 카운트하는 함수 추가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,22 @@
 package onboarding
 
+
+fun count369(number: Int): Int {
+    var count = 0
+    val numberToString = number.toString()
+
+    for (digit in numberToString) {
+        if (digit == '3' || digit == '6' || digit == '9') {
+            count++
+        }
+    }
+    return count
+}
+
+fun countNumberOfClaps() {
+
+}
+
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
