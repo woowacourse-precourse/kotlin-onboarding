3acd7ea41129a215dad04368452662a15be75afb
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-31 02:09:16 +0900

    리스트 형변환

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -9,7 +9,7 @@ fun solution5(money: Int): List<Int> {
 }
 
 fun changemoney(money: Int): MutableList<Int>{
-    var solutionList:MutableList<Int> = mutableListOf(0,0,0,0,0,0,0,0,0)   //변환한 돈을 저장한 리스트를 만든다.
+    val solutionList:MutableList<Int> = mutableListOf(0,0,0,0,0,0,0,0,0)   //변환한 돈을 저장한 리스트를 만든다.
     var change:Int= money
 
     if(money>=50000){   //주어진 돈을 주어진 화폐의 큰 단위부터 변환한다.
