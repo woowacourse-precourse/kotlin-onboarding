994084b3a52fee6b70cca301377afccac41a56c2
s9hn <tnghqn@naver.com>
2022-10-31 05:52:11 +0900

    [REFACTOR(미션4)] fun solution, while -> for문 변경

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -5,21 +5,19 @@ fun solution4(word: String): String {
     val upperCaseMap = mappingUpperCase()
 
     var sonFrog = ""
-    var i = 0
 
 
-
-    while (i < word.length) {
+    for (i in word.indices) {
 
         if (word[i] == ' ') {
             sonFrog += ' '
-            i++
+
         } else if (word[i] in 'a'..'z') {
             sonFrog += lowerCaseMap.getValue(word[i]).toString()
-            i++
+
         } else if (word[i] in 'A'..'Z') {
             sonFrog += upperCaseMap.getValue(word[i]).toString()
-            i++
+
         }
     }
     return sonFrog
