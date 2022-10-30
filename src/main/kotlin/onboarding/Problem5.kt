19e00df3419b9064d1a420d85a49face5ff587e5
82104 <tir2986@naver.com>
2022-10-30 17:52:17 +0900

    feat: 각 화폐 단위로 검사

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,7 +1,24 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val units = listOf(
+        50000, 10000, 5000,
+        1000, 500, 100,
+        50, 10, 1
+    )
+
+    val counts = mutableListOf<Int>()
+
+    var remain = money
+    for (unit in units) {
+        if (remain < 0) break
+        val cnt = availableCnt(remain, unit)
+        counts.add(cnt)
+
+        remain -= cnt * unit
+    }
+
+    return counts
 }
 
 private fun availableCnt(money: Int, unit: Int): Int {
