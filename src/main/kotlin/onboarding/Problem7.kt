55eb26f74347c9404554932af672e970a694008a
jaemin-Yoo <iamjm29@gmail.com>
2022-10-30 15:28:43 +0900

    feat: 친구 추천 알고리즘 - Problem7

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,57 @@
 package onboarding
 
+import java.lang.Integer.min
+import java.util.*
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val friendsMap = mutableMapOf<String, MutableList<String>>()
+    val users = mutableMapOf<String, Int>()
+    friends.forEach { friend ->
+        val (a, b) = friend
+        friendsMap[a]?.add(b) ?: run {
+            friendsMap[a] = mutableListOf(b)
+        }
+        friendsMap[b]?.add(a) ?: run {
+            friendsMap[b] = mutableListOf(a)
+        }
+        users[a] = 0
+        users[b] = 0
+    }
+
+    visitors.forEach {
+        if (users.containsKey(it)) {
+            users[it] = users[it]!! + 1
+        } else {
+            users[it] = 0
+        }
+    }
+
+    friendsMap[user]?.forEach { friend ->
+        users[friend] = 0
+        friendsMap[friend]?.forEach {
+            users[it] = users[it]!! + 10
+        }
+    }
+
+    val result = users.filter { (k, v) ->
+        v > 0 && k != user
+    }.toList().sortedByDescending {
+        it.second
+    }.map { (k, _) ->
+        k
+    }
+    return result.subList(0, min(5, result.size))
+}
+
+fun main() {
+    val users = sortedMapOf<Char, Int>(Collections.reverseOrder())
+    users['c'] = 2
+    users['b'] = 1
+    users['a'] = 3
+
+    println(users.toString())
 }
