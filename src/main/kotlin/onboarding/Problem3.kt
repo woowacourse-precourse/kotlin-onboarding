9cb2cda30d94b6d5d00708f1b62b8330e16b65b2
82104 <tir2986@naver.com>
2022-10-30 17:14:42 +0900

    feat: 각 자릿수 중 조건에 맞는 수 카운트 후 합

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,7 +1,11 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    val conditionSet = setOf(3, 6, 9)
+
+    return (1..number).fold(0) { acc, i ->
+        acc + getDigits(i).count { it in  conditionSet}
+    }
 }
 
 private fun getDigits(num: Int): List<Int> {
