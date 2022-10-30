e20899b8fa37dc8f5c8731d6ba2d9399d9ca4100
zqwz <zqwz@naver.com>
2022-10-30 16:15:00 +0900

    [7번 문제] 친구 관계 정보 리스트를 만드는 기능

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,14 +1,13 @@
 package onboarding
 
-
 fun solution5(money: Int): List<Int> {
     val won = listOf(50000,10000,5000,1000,500,100,50,10,1)
     val result : ArrayList<Int> = arrayListOf()
     var temp = money
 
     for(i in won){
-        result.add(temp % i)
-        temp /= i
+        result.add(temp / i)
+        temp %= i
     }
 
     return result
--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,28 @@
 package onboarding
+fun getFollower(friends: List<List<String>>) : MutableMap<String, MutableSet<String>>{
+    val follower = mutableMapOf<String, MutableSet<String>>()
+
+    for (element in friends) {
+        val name = element[0]
+        val friend = element[1]
+
+        if (follower[name] == null)
+            follower[name] = mutableSetOf()
+        if (follower[friend] == null)
+            follower[friend] = mutableSetOf()
+
+        follower[friend]!!.add(name)
+        follower[name]!!.add(friend)
+    }
+    return follower
+}
 
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val follower = getFollower(friends)
+
+    return listOf()
 }
