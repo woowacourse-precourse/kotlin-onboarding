e44386ab284898758c9f8d70fb2881ae4ddbdc76
sujin-kk <sjoo0002@gmail.com>
2022-10-31 02:17:26 +0900

    [FEAT] 2개 이상의 중복 문자열 찾아내기

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,17 +1,17 @@
 package onboarding
 
-fun main(args: Array<String>) {
-    val forms = listOf(
-        listOf("jm@email.com", "제이엠"),
-        listOf("jason@email.com", "제이슨"),
-        listOf("woniee@email.com", "워니"),
-        listOf("mj@email.com", "엠제이"),
-        listOf("nowm@email.com", "이제엠"),
-        listOf("jm@email.com", "dhfㄱ"),
-        listOf("jmemail.com", "제이"),
-    )
-    println(solution6(forms))
-}
+//fun main(args: Array<String>) {
+//    val forms = listOf(
+//        listOf("jm@email.com", "제이엠"),
+//        listOf("jason@email.com", "제이슨"),
+//        listOf("woniee@email.com", "워니"),
+//        listOf("mj@email.com", "엠제이"),
+//        listOf("nowm@email.com", "이제엠"),
+//        listOf("jm@email.com", "dhfㄱ"),
+//        listOf("jmemail.com", "제이"),
+//    )
+//    println(solution6(forms))
+//}
 
 fun solution6(forms: List<List<String>>): List<String> {
     val rightForms = forms.filter { (email, nickname) ->
@@ -22,21 +22,47 @@ fun solution6(forms: List<List<String>>): List<String> {
 }
 
 fun checkDuplication(forms: List<List<String>>): List<String> {
-    val resultEmails = mutableListOf<String>()
+    val resultEmails = mutableSetOf<String>()
 
-    return resultEmails.toList()
+    forms.forEachIndexed { fidx, (targetEmail, targetNickname) ->
+
+        for (i in fidx until forms.size) {
+            val (compareEmail, compareNickname) = forms[i]
+
+            if (targetNickname == compareNickname) continue
+
+            if (isDuplicated(targetNickname = targetNickname, compareNickname = compareNickname)) {
+                resultEmails.add(targetEmail)
+                resultEmails.add(compareEmail)
+            }
+        }
+    }
+
+    return resultEmails.toList().sorted()
 }
 
-fun isRightEmail(email: String): Boolean {
-    if (EMAIL_PATTERN.toRegex().matches(email)) {
-        val emailStruct = email.split("@")
+fun isDuplicated(targetNickname: String, compareNickname: String): Boolean {
+    var pattern = ""
 
-        if (email.length in EMAIL_MIN_LEN until EMAIL_MAX_LEN && emailStruct[1] == EMAIL_DOMAIN) {
+    targetNickname.forEach { targetChar ->
+        pattern += targetChar
+        println("target:$targetNickname, compare:$compareNickname, pattern:$pattern")
+
+        if (pattern !in compareNickname) {
+            pattern = ""
+            pattern += targetChar
+        }
+
+        if(pattern.length == 2) {
             return true
         }
     }
     return false
 }
+
+fun isRightEmail(email: String): Boolean =
+    EMAIL_PATTERN.toRegex().matches(email) && email.length in EMAIL_MIN_LEN until EMAIL_MAX_LEN && EMAIL_DOMAIN in email
+
 
 fun isRightNickname(nickname: String): Boolean =
     nickname.length in NICKNAME_MIN_LEN until NICKNAME_MAX_LEN && NICKNAME_PATTERN.toRegex().matches(nickname)
