097c664f867c1126f52d88b8b323ed0d8c4cd967
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-10-30 17:50:29 +0900

    :sparkles: 문제 3 number까지 반복 로직 추가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,14 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var count=0
+    var index=0
+    while(true){
+        if(number<index){
+            break
+        }
+        index++
+    }
+    return count
 }
+
