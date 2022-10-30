f3c021443cb83e40a05ccd4db0a4f334092c534a
Min-Jae-Bae <qoalswo123@icloud.com>
2022-10-30 23:13:35 +0900

    6번째 테스트[성공]

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,32 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+
+    val emails: HashSet<String> = HashSet()
+    val formsMap: HashMap<String, String> = HashMap()
+
+
+    for (i in forms.indices) {
+        val nickName: String = forms[i][1]
+
+        for (k in 0 until nickName.length - 1) {
+            val nickNameSliceKey: String = nickName.substring(k, k + 2)
+
+            if (formsMap.containsKey(nickNameSliceKey)) {
+                val email = formsMap[nickNameSliceKey]!!
+
+                if (email != forms[i][0]) {
+                    emails.add(email)
+                    emails.add(forms[i][0])
+                }
+            }
+            formsMap[nickNameSliceKey] = forms[i][0]
+        }
+    }
+    val result: List<String> = emails
+        .sorted()
+        .toList()
+
+    return result
 }
+
