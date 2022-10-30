bd69b4f41eb0af2b08b07e2eb4203edb4b4715d9
yeongjin <dudwls901@naver.com>
2022-10-30 11:31:02 +0900

    [Feat] 친구 관계 저장 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,35 @@
 package onboarding
+/* 기능 목록
+* 친구 관계 저장
+* 사용자의 친구들과 친구인 사람들의 점수 계산
+* 방문자 점수 계산
+* 정렬 및 출력
+* */
+
+
+
+fun getFriendshipMap(friends: List<List<String>>): MutableMap<String,MutableSet<String>> {
+    val friendshipMap = mutableMapOf<String,MutableSet<String>>()
+    friends.forEach {
+        val (f1,f2) = it
+        if(friendshipMap[f1] == null){
+            friendshipMap[f1] = mutableSetOf()
+        }
+        if(friendshipMap[f2] == null){
+            friendshipMap[f2] = mutableSetOf()
+        }
+        friendshipMap[f1]!!.add(f2)
+        friendshipMap[f2]!!.add(f1)
+    }
+    return friendshipMap
+}
 
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
+    val friendshipMap = getFriendshipMap(friends)
     TODO("프로그램 구현")
 }
+
