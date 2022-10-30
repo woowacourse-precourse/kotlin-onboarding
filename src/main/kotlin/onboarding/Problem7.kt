ad1d994a17e52b938a6dbb55e373c9c56eef46f6
jiingom <vvankatam0@gmail.com>
2022-10-31 00:23:20 +0900

    feat(Problem7): 사용자 타임 라인 방문 기록 점수 업데이트 기능 추가

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,9 +5,8 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    var map = HashMap<String, Int>()
 
-    map = setUniqueNumber(friends)
+    var map: HashMap<String, Int> = setUniqueNumber(friends)
     var graph: Array<MutableList<String>> = addFriends(friends, user, map)
     var userFriends = HashMap<String, Int>()
 
@@ -20,7 +19,20 @@ fun solution7(
         }
     }
 
+    updateVisitUser(visitors, userFriends)
+
     TODO("프로그램 구현")
+}
+
+fun updateVisitUser(visitors: List<String>, result: HashMap<String, Int>): HashMap<String, Int> {
+    for (i in visitors.indices) {
+        if (result.containsKey(visitors[i])) {
+            result[visitors[i]] = result[visitors[i]]!! + 1
+        } else {
+            result[visitors[i]] = 1
+        }
+    }
+    return result
 }
 
 fun setUniqueNumber(friends: List<List<String>>): HashMap<String, Int> {
