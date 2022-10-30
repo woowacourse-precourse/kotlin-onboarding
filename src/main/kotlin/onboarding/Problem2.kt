2c6a24dc578b96057acc59925d5fc8d85310dc3c
DormunDoorlock <skrudgh2001@naver.com>
2022-10-30 17:54:43 +0900

    Revert "feat: Problem2.kt 중복 문자 위치 찾는 기능 추가"
    This reverts commit c4938c85f343b150ae70e442da1a78679eaf2d55.

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,6 +1,6 @@
 package onboarding
 // 중복 문자 위치 찾는 기능
-fun findIndex(data : MutableList<String>): MutableSet<Int>{
+fun findIndex(data : String): MutableSet<Int>{
     val letters = data.zipWithNext()
     val index = mutableSetOf<Int>()
     for(i in letters.indices){
@@ -11,23 +11,12 @@ fun findIndex(data : MutableList<String>): MutableSet<Int>{
     }
     return index
 }
-// 중복 부분 삭제
-fun deleteString(data : MutableList<String> , index : MutableSet<Int>): MutableList<String>{
-    for (i in index.reversed()){
-        data.removeAt(i)
-    }
-    return data
-}
 fun solution2(cryptogram: String): String {
-    var data = cryptogram.chunked(1).toMutableList()
-    // 중복문자 있을 때가지 반복
+    val data = cryptogram
     while(true){
-        if (data.size<=1)
+        if (data.length<=1)
             break
-        val index = findIndex(data)
-        if (index.size == 0)
-            break
-        data = deleteString(data,index)
+        var index = findIndex(data)
     }
-    return data.joinToString("")
+    return ""
 }
