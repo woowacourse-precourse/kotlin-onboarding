edd135bfe691017fdce5728847ac52c1c96d368d
yelm <21yrshin@naver.com>
2022-10-30 23:23:04 +0900

    4 commit edited

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -5,9 +5,14 @@ fun solution4(word: String): String {
     val alphabet1 = CharRange('a','z').toMutableList()
 
     val res = word.forEach ({ ch ->
-        if (ch == " "){
-
-        } else if {
+        val str = ch.toInt()
+        if (str >=32 || str <= 57){
+            str = str - 32
+            str = 25 - str
+            str += 32
+            val charval = str.toChar()
+            print("$charval")
+        } else if (){
 
         }
     })
