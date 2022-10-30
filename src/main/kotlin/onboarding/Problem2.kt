541107933765dad48e64054f16d900dccda281f9
DormunDoorlock <skrudgh2001@naver.com>
2022-10-30 17:34:34 +0900

    feat: Problem2.kt 중복 문자 위치 찾는 기능 추가

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,22 @@
 package onboarding
-
+// 중복 문자 위치 찾는 기능
+fun findIndex(data : String): MutableSet<Int>{
+    val letters = data.zipWithNext()
+    val index = mutableSetOf<Int>()
+    for(i in letters.indices){
+        if (letters[i].first == letters[i].second) {
+            index.add(i)
+            index.add(i+1)
+        }
+    }
+    return index
+}
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    val data = cryptogram
+    while(true){
+        if (data.length<=1)
+            break
+        var index = findIndex(data)
+    }
+    return ""
 }
