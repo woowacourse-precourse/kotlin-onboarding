2f09298c141c32c59ebed6ee47ec9de093bc8ec3
Hyuna Kim <akxmcse@gmail.com>
2022-10-30 15:47:09 +0900

    feat: problem1 예외사항 체크 함수 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,23 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if(!checkNoException(pobi, crong)) {
+        return -1
+    }
+    return 0
 }
+
+private fun checkNoException(pobi: List<Int>, crong: List<Int>): Boolean {
+    return (checkPageRange(pobi[0], pobi[1])
+            || checkPageRange(crong[0], crong[1])
+            || checkValidPage(pobi[0], pobi[1])
+            || checkValidPage(crong[0], crong[1])
+            || checkPairPage(pobi[0], pobi[1])
+            || checkPairPage(crong[0], crong[1]))
+}
+
+private fun checkPageRange(leftPage: Int, rightPage: Int) = (leftPage > 1 && rightPage < 400)
+
+private fun checkValidPage(leftPage: Int, rightPage: Int) = (rightPage - leftPage == 1)
+
+private fun checkPairPage(leftPage: Int, rightPage: Int) = (leftPage%2 == 1 && rightPage%2 == 0)
