2f270ee9adc77225625895e69db2ac149e765bf9
hyunji1203 <hj27720@gmail.com>
2022-10-30 15:58:54 +0900

    문제3 - 3,6,9 들어갔을 때 손뼉 치기

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -7,4 +7,16 @@ fun solution3(number: Int): Int {
         return 0
     }
 
+    //3,6,9 들어갔을 때 손뼉 치기
+    for (i in 1..number){
+        var numString = i.toString().split("").toMutableList()
+        numString.removeAt(numString.size-1)
+        numString.removeAt(0)
+
+        for (i in (0 until numString.size)){
+            if (numString[i] == "3" || numString[i] == "6" || numString[i] == "9")
+                result += 1
+        }
+    }
+    return result
 }
