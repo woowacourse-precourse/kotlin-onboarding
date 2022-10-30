5d0fa5d95e9138cd02ffffb09fba02da0e457b0b
82104 <tir2986@naver.com>
2022-10-30 17:08:45 +0900

    feat: 각 자릿수 분리

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -3,3 +3,15 @@ package onboarding
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
+
+private fun getDigits(num: Int): List<Int> {
+    val digits = mutableListOf<Int>()
+
+    var digit = num
+    while (digit > 0) {
+        digits.add(digit % 10)
+        digit /= 10
+    }
+
+    return digits
+}
