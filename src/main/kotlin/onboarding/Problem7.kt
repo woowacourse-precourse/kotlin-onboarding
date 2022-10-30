c86e42ccbe6c0b6c31a2765504c02f3a85c03e23
YuGyeong98 <kuk3097@naver.com>
2022-10-30 14:55:17 +0900

    feature(Problem7): friends 리스트에서 idA, idB로 나누고, 새로운 리스트에 추가

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,21 @@
 package onboarding
 
 fun solution7(
-    user: String,
-    friends: List<List<String>>,
-    visitors: List<String>
+  user: String,
+  friends: List<List<String>>,
+  visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+  val recommendFriends: List<String> = listOf("")
+  val idMap = mutableMapOf<String, MutableList<String>>()
+  val scoreMap = mutableMapOf<String, Int>()
+
+  friends.forEach { friend ->
+    val idA = friend[0]
+    val idB = friend[1]
+
+    if (idMap.containsKey(idA)) idMap[idA]?.add(idB) else idMap[idA] = mutableListOf(idB)
+    if (idMap.containsKey(idB)) idMap[idB]?.add(idA) else idMap[idB] = mutableListOf(idA)
+  }
+
+  return recommendFriends
 }
