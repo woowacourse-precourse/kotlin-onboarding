155df0a7d6ee242325e365a7f8da58294e18c433
JEGHTNER <jeheon0717@naver.com>
2022-10-30 14:09:14 +0900

    Problem 3 Solve

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -10,7 +10,7 @@ fun solution2(cryptogram: String): String
     new_string.add(cryptogram[0])
     while(i < cryptogram.length) {
         if(cryptogram[i] != new_string.peek())
-            new_string.add(cryptogram[i])
+            new_string.push(cryptogram[i])
         else if(cryptogram[i] == new_string.peek())
             new_string.pop()
         //println("new_string : " + new_string)
@@ -36,4 +36,5 @@ fun main()
     solution2(cryptogram1)
     solution2(cryptogram2)
 }
+
 */
