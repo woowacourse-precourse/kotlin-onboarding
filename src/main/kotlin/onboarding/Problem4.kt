74f22819c6ecac22f66152bfb016f61db8183a60
yelm <21yrshin@naver.com>
2022-10-30 23:03:18 +0900

    4 commit

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -4,4 +4,13 @@ fun solution4(word: String): String {
     val alphabet0 = CharRange('A','Z').toMutableList()
     val alphabet1 = CharRange('a','z').toMutableList()
 
+    val res = word.forEach ({ ch ->
+        if (ch == " "){
+
+        } else if {
+
+        }
+    })
+
+    return res
 }
