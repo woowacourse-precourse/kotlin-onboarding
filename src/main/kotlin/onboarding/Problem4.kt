6f7b0cafbac114e8a44243bd380e43840490e67f
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-10-30 18:08:05 +0900

    :sparkles: 각문자 소문자/대문자/그외 분류

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,21 @@
 package onboarding
 
+/* TODO 기능 목록
+*   - 알파벳 소문자 <-> 대문자 변경
+*   - 문자열 역순으로 변경 */
+
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var result = ""
+    for(i in word){
+        if(i.isUpperCase()){
+
+        }
+        else if(i.isLowerCase()){
+
+        }
+        else if(!i.isLowerCase() && !i.isUpperCase()){
+
+        }
+    }
+    return result
 }
