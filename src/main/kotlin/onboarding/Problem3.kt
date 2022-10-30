5e2ef7d75cc576543de60cec6135d218c57ded90
KimDongHyun0907 <ehdgus8397@naver.com>
2022-10-30 23:18:35 +0900

    commit files

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var cnt=0
+    for(i in 1..number){
+        var num2=i
+        while(true){
+            var tmp=num2%10
+            if(tmp==3 || tmp==6 || tmp==9){
+                cnt+=1
+            }
+            num2=num2/10
+            if(num2==0){
+                break
+            }
+        }
+    }
+    return cnt
 }
+
