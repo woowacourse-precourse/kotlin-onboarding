3d0c185065b3efd08b5642e40dd64f74d88e5ea4
vrexpert <vrexpert2302@gmail.com>
2022-10-30 23:34:07 +0900

    Solving problem 7

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,46 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+        val knowEachList = findFriends(user,friends)
+        val knowEachSet = knowEachList.toSet()
+    return listOf("")
+}
+fun	findFriends(user:String ,friends: List<List<String>>,):MutableList<String>{
+
+    var friendsML = friends.toMutableList()
+    var findedFriends = mutableListOf<String>(user)
+    var friendsSet = mutableListOf<String>()
+
+    for(i in 0..1){
+        var count = findedFriends.size
+
+        for(j in 0 until count){
+
+            val person = findedFriends.removeAt(0)
+            var k = 0
+            while(k < friendsML.size){
+
+                if(friendsML[k][0] == person)
+                {
+                    findedFriends.add(friendsML[k][1])
+                    friendsML.removeAt(k)
+                    k--
+                }
+                else if(friendsML[k][1] == person)
+                {
+                    findedFriends.add(friendsML[k][0])
+                    friendsML.removeAt(k)
+                    k--
+                }
+                k++
+            }
+
+        }
+
+        if (i==0){
+            friendsSet = findedFriends
+        }
+    }
+    findedFriends.add(friendsSet.toString())
+    return findedFriends
 }
