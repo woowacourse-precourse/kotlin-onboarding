399b573dba33a336871191c483c8971a7096ab96
WoeunJung <leena09123@naver.com>
2022-10-31 01:45:50 +0900

    solve: prob5 - withdraw money

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,16 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var rest = money
+    var unitList  = mutableListOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    var moneyList = mutableListOf<Int>(0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+    for (index in 0..8){
+        println(index)
+        while (rest >= unitList[index]){
+            rest -= unitList[index]
+            moneyList[index] += 1
+        }
+    }
+    return moneyList
 }
