a389b8e18b68a2b0b5ad20272dbd7e3d092154e4
person00333 <person00333@gmail.com>
2022-10-31 01:36:57 +0900

    문제 2 업로드

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,30 @@
 package onboarding
 
+import java.util.Stack
+
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+//    TODO("프로그램 구현")
+    val stack = Stack<Char>()
+
+
+    for (i in 0 .. cryptogram.length-1 step(1)){
+        if (stack.empty()){
+            stack.push(cryptogram[i])
+        }
+        else{
+            if (stack.peek() == cryptogram[i]){
+                stack.pop()
+            }
+            else{
+                stack.push(cryptogram[i])
+            }
+        }
+
+    }
+    var result: String
+    result = ""
+    while (!stack.empty()){
+        result = stack.pop() + result
+    }
+    return result
 }
