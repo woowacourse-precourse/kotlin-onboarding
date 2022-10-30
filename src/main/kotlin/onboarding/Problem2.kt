5f4283009170ca5e2143ab4a44aa0e9d2ddd780e
s9hn <tnghqn@naver.com>
2022-10-30 15:52:56 +0900

    [FEAT(미션2)] 문자열 중복검사

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,30 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+
+    var resultCrypto = ""
+    var i = 0
+    var checkDuplication = false
+
+
+
+    while (i < cryptogram.length - 1) {
+
+        if (cryptogram[i] != cryptogram[i + 1]) {
+            if (checkDuplication) {
+                i++
+                checkDuplication = false
+            } else {
+                resultCrypto += cryptogram[i]
+                i++
+            }
+
+        } else {
+            checkDuplication = true
+            i++
+        }
+
+    }
+    return resultCrypto
+    println(resultCrypto)
 }
