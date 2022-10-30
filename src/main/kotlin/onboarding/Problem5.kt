12fcc90043517b5cdfde6a5faefac8dd52573c51
DormunDoorlock <skrudgh2001@naver.com>
2022-10-30 15:14:15 +0900

    feat : Problem5.kt 큰 금액의 화폐부터 필요한 갯수 세는 기능

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,13 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val result = arrayListOf<Int>()
+    var data = money
+    val currencyKind = arrayOf(50000,10000,5000,1000,500,100,50,10,1)
+    // 최소 화폐 개수를 위해 최대 가치 화폐 개수부터 구함.
+    for (i in 0 .. 8){
+        result.add( data / currencyKind[i] )
+        data %= currencyKind[i]
+    }
+    return result
 }
