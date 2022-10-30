a9752ecfa9dbb31850967fb35b4e067f7d283096
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-10-30 15:39:37 +0900

    문제 5 : Feat: 추가 현재 돈에서 최대 돈으로 나누는 함수

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -4,6 +4,11 @@ val unit = arrayListOf<Int>(50000,10000,5000,1000,500,50,10,1)
 
 fun solution5(money: Int): List<Int> {
 
-    
+    var result = ArrayList<Int>()
 
+    var now_money = money
+
+}
+fun pay_money(i : Int , money: Int) : Int{
+    return money / unit[i]
 }
