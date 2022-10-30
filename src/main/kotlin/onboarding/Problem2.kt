06ef909a04d5024a9c393b61e81ade35e855728b
WoeunJung <leena09123@naver.com>
2022-10-31 01:29:30 +0900

    solve: prob2 - brown's cryptogram

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,24 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var result = cryptogram
+    while (true){
+        val removedStr = removeRepeat(result)
+        if (removedStr == result) break
+        result = removedStr
+    }
+    return result
+}
+
+fun removeRepeat(cryp: String): String{
+    var removedStr = ""
+    var previousStr = ' '
+    for (char in cryp) {
+        if (previousStr != char){
+            previousStr = char
+            removedStr += char
+        }
+        else removedStr = removedStr.dropLast(1)
+    }
+    return removedStr
 }
