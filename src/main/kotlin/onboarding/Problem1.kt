9fabcc0e9f8896e51a4d63f1a86b6b102749974f
82104 <tir2986@naver.com>
2022-10-30 17:03:57 +0900

    feat: Problem1 구조 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,36 @@
 package onboarding
 
+private const val ERROR         = -1
+private const val DRAW          = 0
+private const val POBI_WIN      = 1
+private const val CRONG_WIN     = 2
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if ((validate(pobi) && validate(crong)) == false) {
+        return ERROR
+    }
+
+    val pobiScore = score(pobi)
+    val crongScore = score(crong)
+    val diff = pobiScore - crongScore
+
+    val result = if (diff > 0) {
+        POBI_WIN
+    } else if (diff < 0) {
+        CRONG_WIN
+    } else {
+        DRAW
+    }
+
+    return result
 }
+
+private fun validate(pages: List<Int>): Boolean {
+    TODO("유효성 검사")
+}
+
+private fun score(pages: List<Int>): Int {
+    TODO("점수계산")
+}
+
+
