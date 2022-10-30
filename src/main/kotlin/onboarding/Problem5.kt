929937bef1f9fd6c681270e8c793f39ae53bd499
yulim <yulim13sky@naver.com>
2022-10-30 17:43:26 +0900

    [REFACTOR] Problem5 toList() 삭제, list 자료형 생략

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -3,7 +3,7 @@ package onboarding
 fun solution5(money: Int): List<Int> {
     val answer = mutableListOf<Int>()
     var changeMoney = money
-    val unitMoney = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val unitMoney = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
     for (i in unitMoney.indices) {
         if (changeMoney >= unitMoney[i]) {
             answer.add(changeMoney / unitMoney[i])
@@ -12,5 +12,5 @@ fun solution5(money: Int): List<Int> {
             answer.add(0)
         }
     }
-    return answer.toList()
+    return answer
 }
