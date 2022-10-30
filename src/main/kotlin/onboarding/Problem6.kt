6e7b49784a3d39c1759716fef9efac9522daa2d2
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-31 04:17:30 +0900

    닉네임 추출 함수

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,24 @@
 package onboarding
 
+//기능별 목록
+//1. 닉네임 추출
+//2. 닉네임 중복될 수 있는 단어 추출
+//3. 중복되는 단어 추출
+//4. 단어가 포함되어 있는 사용자 이메일 추출
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val nicknamelist = onlynickname(forms)  //닉네임 추출
+
+    return nicknamelist
 }
+
+//닉네임 추출 함수
+fun onlynickname(forms: List<List<String>>): List<String>{
+    val nicknamelist:MutableList<String> = mutableListOf()
+    for(i:Int in forms.indices){
+        nicknamelist.add(forms[i][1])
+    }
+    println(nicknamelist)
+    return nicknamelist
+}
+
