7e3165ea902ce28e9ea19a32fcfab762835a25a5
dlgocks1 <cksgodl1@naver.com>
2022-10-31 01:48:49 +0900

    feat: (Problem3) 3, 6, 9 카운트 함수 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -8,5 +8,13 @@ fun solution3(number: Int): Int {
  * 파라미터로 온 숫자에 3,6,9의 숫자가 몇개 포함되어있는지 반환한다.
  */
 fun count369InNum(num: Int): Int {
-
+    val numList = listOf(3, 6, 9)
+    val cnt = 0
+    while (num > 0) {
+        if (num % 10 in numList) {
+            cnt += 1
+        }
+        num /= 10
+    }
+    return cnt
 }
