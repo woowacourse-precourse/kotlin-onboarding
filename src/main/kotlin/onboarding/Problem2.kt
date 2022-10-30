a0314348bfb42a11510b2eec8fd52257ccf7d185
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-10-30 17:19:43 +0900

    :sparkles: 문제2 문자열 전체 탐색 로직 추가

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,11 +1,57 @@
 package onboarding
 
 /* TODO 기능 목록   */
+private var result = ""
+private var restartFlag = false
+private var removeIndex = 0
+private var index=0
 fun solution2(cryptogram: String): String {
-    var result = cryptogram
+    result = cryptogram
     if(cryptogram.length==1){
         return cryptogram
     }
-
+    else{
+        index=0
+        restartFlag =false
+        findOverlapString()
+    }
     return result
+}
+
+private fun findOverlapString(){
+    while(true){
+        if(result.length-1<=index){
+            if(restartFlag){
+                repeatFirstChar()
+            }
+            else{
+                break
+            }
+        }
+        else{
+            if(result[index]==result[index+1]){
+                getRemoveIndex()
+                removeOverlapString()
+                restart()
+            }
+            else{
+                index++
+            }
+        }
+    }
+}
+
+private fun getRemoveIndex(){
+
+}
+
+private fun removeOverlapString(){
+
+}
+
+private fun restart(){
+}
+
+private fun repeatFirstChar(){
+
 }
