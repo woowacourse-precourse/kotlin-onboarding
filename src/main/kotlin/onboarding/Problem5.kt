3fc18d505bb3744f9cf86d0bfa4340e3e9bef83d
kyounggudev <homin222@naver.com>
2022-10-30 22:14:55 +0900

    문제5 - 리팩토링

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -2,16 +2,15 @@ package onboarding
 
 fun solution5(money: Int): List<Int> {
     val change = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
-    var result = exchange(money, change)
-    return result
+    return exchange(money, change)
 }
 
-fun exchange(money : Int, change : List<Int>) : List<Int>{
-    var money = money
-    var result : MutableList<Int> = mutableListOf()
-    for(i in change){
-        result.add(money/i)
-        money %= i
+fun exchange(money: Int, change: List<Int>): List<Int> {
+    var restMoney = money
+    val result: MutableList<Int> = mutableListOf()
+    for (i in change) {
+        result.add(restMoney / i)
+        restMoney %= i
     }
     return result
 }
