54225b23a1e092c622152f6e8a42a1eef38bb3f9
82104 <tir2986@naver.com>
2022-10-30 17:32:24 +0900

    feat: 알파벳 반대로 추가

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,3 +3,13 @@ package onboarding
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
+
+private fun Char.reverseAlpha(): Char {
+    if (!this.isLetter()) return this
+
+    return if (this.isUpperCase()) {
+        'Z' - (this - 'A')
+    } else {
+        'z' - (this - 'a')
+    }
+}
