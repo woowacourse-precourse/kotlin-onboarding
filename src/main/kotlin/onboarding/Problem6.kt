601ba05f3170f1518bd8e4f842d4b69a7a1b3211
Suheon Song <ssheon0812@naver.com>
2022-10-31 01:41:38 +0900

    feat: [Problem6] 중복된 닉네임이 있는지 확인하는 함수 추가

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,7 +1,26 @@
 package onboarding
 
+import java.util.TreeSet
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    var result = TreeSet<String>()
+    val twoLetterMap = mutableMapOf<String, Set<String>>()
+    val emailList = mutableListOf<String>()
+
+    for (form in forms) {
+        val email = form[0]
+        val nickName = form[1]
+        twoLetterMap[email] = convertNickNameToTwoLetterSet(nickName)
+        emailList.add(email)
+    }
+
+    for (email in emailList) {
+        if (checkDuplicateNickname(email, twoLetterMap)) {
+            result.add(email)
+        }
+    }
+
+    return result.toList()
 }
 
 fun convertNickNameToTwoLetterSet(nickNmae: String): Set<String> {
@@ -12,6 +31,20 @@ fun convertNickNameToTwoLetterSet(nickNmae: String): Set<String> {
     return twoLetterSet
 }
 
-
+fun checkDuplicateNickname(email: String, twoLetterMap: Map<String, Set<String>>): Boolean {
+    for (form in twoLetterMap) {
+        if (form.component1() == email) {
+            continue
+        } else {
+            for (twoLetter in twoLetterMap[email]!!) {
+                // 두 글자씩 비교했을 때 중복이 될 경우
+                if (form.component2().contains(twoLetter)) {
+                    return true
+                }
+            }
+        }
+    }
+    return false
+}
 
 
