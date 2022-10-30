c830e814ed60a2808bf6e7e8262a3e204d034463
82104 <tir2986@naver.com>
2022-10-30 18:59:28 +0900

    feat: Problem7 구조

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,29 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+
+    // 추천 받을 유저의 친구 목록
+
+    // 친구들의 친구 목록 - 유저의 친구 목록 -> Set
+
+    // 리스트 저장
+    // +10
+
+    // 방문자 목록 - 유저의 친구 목록
+
+    // +1
+
+    // 정렬
+
+    TODO()
+}
+
+private class User(val name: String, var score: Int = 0)
+
+private fun getUserFriends(user: String, friends: List<List<String>>): Set<String> {
+    TODO()
+}
+
+private fun getSharedFriends(friends1: List<List<String>>, friends2: List<List<String>>): List<String> {
+    TODO()
 }
