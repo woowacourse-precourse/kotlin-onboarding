7c4b56bd615747ff035940003a2caa593d4ed2dd
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-10-30 18:09:15 +0900

    :sparkles: 대문자<->소문자 역순으로 변경

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -8,13 +8,15 @@ fun solution4(word: String): String {
     var result = ""
     for(i in word){
         if(i.isUpperCase()){
-
+            result=result.plus((27-((i
+                .uppercaseChar().code)-64)+64)
+                .toChar().toString())
         }
         else if(i.isLowerCase()){
-
+            result=result.plus((27-((i.lowercaseChar().code)-96)+96).toChar().toString())
         }
         else if(!i.isLowerCase() && !i.isUpperCase()){
-
+            result=result.plus(i)
         }
     }
     return result
