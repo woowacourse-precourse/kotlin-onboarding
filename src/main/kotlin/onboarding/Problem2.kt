b408208a7e2f2413f083c1f4f2152430f26e7e0b
Yerin <myheaven_b@naver.com>
2022-10-30 17:14:30 +0900

    refactor: use regex for replacing cryptogram

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -2,32 +2,11 @@ package onboarding
 
 fun solution2(cryptogram: String): String {
     var result: String = cryptogram
+    var regex: Regex = "((\\w)\\2{1,})".toRegex()
 
-    while(isCryptogram(result)){
-        result = getSolve(result)
+    while(regex.findAll(result).count() > 0){
+        result = result.replace(regex, "")
     }
 
     return result
-}
-
-fun isCryptogram(str: String): Boolean {
-    for (i in 1..str.length-1) {
-        if (str[i - 1] == str[i]) {
-            return true
-        }
-    }
-
-    return false
-}
-
-fun getSolve(_str: String): String {
-    var str: String = _str
-
-    for (i in 1..str.length-1) {
-        if (str[i - 1] == str[i]) {
-            str = str.replaceRange(i-1..i, "  ")
-        }
-    }
-
-    return str.replace("  ", "")
 }
