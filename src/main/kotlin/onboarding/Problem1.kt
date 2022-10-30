1de228d511fe27342694504d51526694d224276d
WoeunJung <leena09123@naver.com>
2022-10-31 01:19:05 +0900

    Revert "Revert "solve: prob1 - PageGame of Pobi and Crong""
    This reverts commit d464791dc024b97d44bec93209fe8f7ab47c9957.

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,45 @@
 package onboarding
+import java.lang.Integer.max
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    val pobiMax = getMaxPageByList(pobi) // pobi's max page
+    val crongMax = getMaxPageByList(crong) // crong's max page
+
+    // return value
+    var result = 0 // undecided match
+    if (pobiMax > crongMax) result = 1 // pobi win
+    if (pobiMax < crongMax) result = 2 // crong win
+    if (!(isValid(pobi) && isValid(crong))) result = -1 // exception case
+
+    return result
+}
+fun getMaxPageByList(mylist: List<Int>): Int{
+    // max(maxLeftPage, maxRightPage)
+    return max(getMaxPage(mylist[0]), getMaxPage(mylist[1]))
+}
+
+fun getMaxPage(pageNum: Int = 0): Int{
+    // add all digits
+    var plusNum = 0
+    var page = pageNum
+    while (page > 0){
+        plusNum += page%10
+        page /= 10
+    }
+
+    // multiply all digits
+    var multiNum = 1
+    page = pageNum
+    while (page > 0) {
+        multiNum *= page%10
+        page /= 10
+    }
+    // get max of plus and multiply of digits
+    return max(plusNum, multiNum)
+}
+
+fun isValid(pageList: List<Int>): Boolean{
+    // check if the page is valid
+    val diff = pageList[1] - pageList[0] // should be 1
+    return (diff == 1)
 }
