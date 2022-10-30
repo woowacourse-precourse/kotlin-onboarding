1d9b1e40a67db74f970b326d0e0929fbd22f2dfe
changhwan <chang5hustle@gmail.com>
2022-10-30 19:34:16 +0900

    [Problem1] (요구사항 1) 결과 값 상수 지정

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,12 @@
 package onboarding
 
+const val POBI_WIN = 1
+const val CRONG_WIN = 2
+const val DRAW = 0
+const val INPUT_EXCEPTION = -1
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    
 }
+
+
