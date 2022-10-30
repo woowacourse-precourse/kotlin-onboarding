91e37446399684e303808790fe125a90feb4c5be
jiingom <vvankatam0@gmail.com>
2022-10-30 23:49:22 +0900

    feat(Problem7): 각 사용자 별 고유번호 붙여주기 기능

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,24 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
+    var map = HashMap<String, Int>()
+    map = setUniqueNumber(friends)
+
     TODO("프로그램 구현")
+}
+
+fun setUniqueNumber(friends: List<List<String>>): HashMap<String, Int> {
+    var index = 0
+    var map = HashMap<String, Int>()
+
+    for (i in friends.indices) {
+        val (a, b) = friends[i].map { it.toString() }
+        if (!map.containsKey(a)) {
+            map[a] = index++
+        }
+        if (!map.containsKey(b)) {
+            map[b] = index++
+        }
+    }
+    return map
 }
