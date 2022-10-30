308f3ae51cadeea105c38e04089082c9dbf38c9f
zqwz <zqwz@naver.com>
2022-10-30 18:03:26 +0900

    [7번 문제] 용어 변경

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -2,14 +2,14 @@ package onboarding
 
 import java.lang.Integer.max
 
-const val EXCPTION_PAGE_SIZE = 2
-const val EXECPTION = -1
-const val CONTINOUS = 1
+const val EXCEPTION_PAGE_SIZE = 2
+const val EXCEPTION = -1
+const val CONTINUOUS = 1
 
 const val POBI_WIN = 1
 const val CRONG_WIN = 2
 const val DRAW = 0
-fun maxPageNumber(number : Int) : Int{
+fun getMaxPageNumber(number : Int) : Int{
     var plus = 0
     var multiple = 1
     var temp = number
@@ -22,27 +22,24 @@ fun maxPageNumber(number : Int) : Int{
     }
     return max(plus, multiple)
 }
-fun continuous(pages : List<Int>) : Boolean{
-    return pages[1] - pages[0] == CONTINOUS
+fun isContinuous(pages : List<Int>) : Boolean{
+    return pages[1] - pages[0] == CONTINUOUS
 }
-fun checkLength(pages : List<Int>) : Boolean{
-    return pages.size == EXCPTION_PAGE_SIZE
+fun checkSize(pages : List<Int>) : Boolean{
+    return pages.size == EXCEPTION_PAGE_SIZE
 }
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    if(!continuous(pobi) || !continuous(crong))
-        return EXECPTION
-
-    if(!checkLength(pobi) || !checkLength(crong))
-        return EXECPTION
+    if(!isContinuous(pobi) || !isContinuous(crong) || !checkSize(pobi) || !checkSize(crong))
+        return EXCEPTION
 
     val pobiLeftPage : Int = pobi[0]
     val pobiRightPage : Int = pobi[1]
     val crongLeftPage : Int = crong[0]
     val crongRightPage : Int = crong[1]
 
-    val pobiResult = max(maxPageNumber(pobiLeftPage), maxPageNumber(pobiRightPage))
-    val crongResult = max(maxPageNumber(crongLeftPage), maxPageNumber(crongRightPage))
+    val pobiResult = max(getMaxPageNumber(pobiLeftPage), getMaxPageNumber(pobiRightPage))
+    val crongResult = max(getMaxPageNumber(crongLeftPage), getMaxPageNumber(crongRightPage))
 
     return when {
         crongResult > pobiResult -> CRONG_WIN
