1da3b836af10c0609fe6bace87d64488266576f8
changhwan <chang5hustle@gmail.com>
2022-10-31 02:33:40 +0900

    [Problem 2] (요구사항 1) 1단계 중복문자 삭제 로직 작성

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,19 @@
 package onboarding
 
-fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+fun solution2(cryptogram: String):String {
+
+    var processedCryptogram = cryptogram
+
+
+    for (i:Int in processedCryptogram.indices){
+        var duplicateCount = 0
+        for (j:Char in processedCryptogram.substring(i+1)){
+            if(processedCryptogram[i] == j){
+                duplicateCount += 1
+            }else break
+        }
+        if (duplicateCount > 0){
+            processedCryptogram.removeRange(i,i+duplicateCount+1)
+        }
+    }
 }
