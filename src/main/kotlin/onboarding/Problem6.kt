270d08aec0d87aa27134e8e4ccb70f943f19d30c
Suheon Song <ssheon0812@naver.com>
2022-10-30 23:49:45 +0900

    feat: [Problem6] convertNickNameToTwoLetterSet 함수 추가

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,3 +3,15 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
+fun convertNickNameToTwoLetterSet(nickNmae: String): Set<String> {
+    val twoLetterSet = hashSetOf<String>()
+    for (i in 0 until nickNmae.length - 1) {
+        twoLetterSet.add(nickNmae.substring(i, i + 2))
+    }
+    return twoLetterSet
+}
+
+
+
+
