4f4350220ddb53b444a5b376c10f7797718a8270
kyounggudev <homin222@naver.com>
2022-10-30 21:57:32 +0900

    문제4 - decodeWord() 추가

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,17 +1,21 @@
- package onboarding
+package onboarding
 
 fun solution4(word: String): String {
+    return decodeWord(word)
+}
+
+fun decodeWord(word: String): String {
     var changeWord = ""
-    for( alphabet in word){
+    for (alphabet in word) {
         changeWord = changeWord.plus(change(alphabet))
     }
     return changeWord
 }
 
-fun change(alphabet : Char) :Char{
-    var ChangeAlphabet = when(alphabet.code){
-        in 65..90 -> 155-alphabet.code
-        in 97..122 -> 219-alphabet.code
+fun change(alphabet: Char): Char {
+    var ChangeAlphabet = when (alphabet.code) {
+        in 65..90 -> 155 - alphabet.code
+        in 97..122 -> 219 - alphabet.code
         else -> alphabet.code
     }
     return ChangeAlphabet.toChar()
