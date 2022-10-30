5717704c9bb38920636a4429dcd3e35f878a5093
joh9911 <joh9911@naver.com>
2022-10-31 06:55:23 +0900

    update

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -2,84 +2,85 @@ package onboarding
 
 import kotlin.random.Random
 
-fun main(){ // 최대한 기능을 함수 단위로 쪼개도록 시도했음
-    val pobi = arrayListOf<Int>()
-    val crong = arrayListOf<Int>()
-    pageSelect(pobi)
-    pageSelect(crong)
-    println(pobi)
-    println(crong)
-    println(solution1(pobi, crong))
+fun main(){
+
 }
 
-fun pageSelect(list: ArrayList<Int>){ // 포비와 크롱이 임의로 페이지를 피는 함수이다.
-    var leftPageNum = 0
-    var rightPageNum = 0
-    val random = Random.nextInt(3,398)
 
-    if (random % 2 == 1){
-        leftPageNum = random
-        rightPageNum = random + 1
+fun exceptionCheck(pobi: List<Int>, crong: List<Int>): Boolean{ //예외 체크를 해주는 함수이다. 예외에 해당되면 false를 리턴한다
+    if (pobi[0] % 2 == 0 || crong[0] % 2 == 0){ // 왼쪽 페이지 번호가 짝수일 때
+        return false
     }
-    else{
-        leftPageNum = random - 1
-        rightPageNum = random
+    else if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1){ // 왼쪽 오른쪽 페이지 번호가 1이 넘게 차이가 날 때
+        return false
     }
-    list.add(leftPageNum)
-    list.add(rightPageNum)
+    else if (pobi[0] >= pobi[1] || crong[0] >= crong[1]){ // 왼쪽 페이지 번호가 오른쪽 번호보다 크거나 같을 때
+        return false
+    }
+    else return !(pobi.size > 2 || crong.size > 2) // 베열 길이가 2보다 클 때
+    // 이 각각을 제외하고 true를 리턴한다.
 }
 
 fun plusCalculation(num: Int): Int{ // 더하기 함수
     var num = num
     var answer = 0
-    if (num < 10){
+    if (num < 10){ // 1의 자리 숫자이면 답은 숫자 값이 된다.
         answer = num
     }
     else{
-        while(num > 9){
-            answer += num%10
-            num /= 10
+        while(num >= 10){ // 숫자가 10의 자리 일 때 까지 반복문을 돌려
+            answer += num%10 // 10으로 나눈 나머지 값들을 모두 더하고
+            num /= 10 //  그때 마다 숫자를 10으로 나눈 값으로 설정한다.
         }
     }
-    return answer + num
+    return answer + num // 마지막 자리 숫자를 마지막으로 더한다.
 }
 
 fun productCalculation(num: Int): Int{ // 곱하기 함수
     var num = num
     var answer = 1
-    if (num < 10){
+    if (num < 10){// 1의 자리 숫자이면 답은 숫자 값이 된다.
         answer = num
     }
     else{
-        while(num >= 10){
-            answer *= num%10
-            num /= 10
+        while(num >= 10){ // 숫자가 10의 자리 일 때 까지 반복문을 돌려
+            answer *= num%10 // 10으로 나눈 나머지 값들을 모두 곱하고
+            num /= 10 //  그때 마다 숫자를 10으로 나눈 값으로 설정한다.
         }
     }
-    return answer * num
+    return answer * num // 마지막 자리 숫자를 마지막으로 곱한다.
 }
 
 fun comparison(num1: Int, num2: Int, num3: Int, num4: Int): Int{ // 4개의 숫자를 비교하는 함수
     val array = listOf(num1, num2, num3, num4)
-    var answer = array.maxOrNull()
+    var answer = array.maxOrNull() // 가장 큰 숫자를 answer에 저장한다
     return answer!!
 }
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    val pobiMaxValue = comparison(plusCalculation(pobi[0]), productCalculation(pobi[0]), plusCalculation(pobi[1]),
-        productCalculation(pobi[1]))
-    val crongMaxValue = comparison(plusCalculation(crong[0]), productCalculation(crong[0]), plusCalculation(crong[1]),
-        productCalculation(crong[1]))
-    if (pobiMaxValue > crongMaxValue){
-        return 1
-    }
-    else if (pobiMaxValue < crongMaxValue){
-        return 2
-    }
-    else if (pobiMaxValue == crongMaxValue){
-        return 0
+    var result = 0
+
+    if (!exceptionCheck(pobi, crong)){ //  만약 예외 체크에 걸렸다면 result 는 -1
+        result = -1
     }
     else{
-        return -1
+        val pobiMaxValue = comparison(plusCalculation(pobi[0]), productCalculation(pobi[0]), plusCalculation(pobi[1]),
+            productCalculation(pobi[1]))
+        val crongMaxValue = comparison(plusCalculation(crong[0]), productCalculation(crong[0]), plusCalculation(crong[1]),
+            productCalculation(crong[1]))
+        if (pobiMaxValue > crongMaxValue){ // 포비가 이기면 1
+            result = 1
+        }
+        else if (pobiMaxValue < crongMaxValue){ // 크롱이 이기면 2
+            result = 2
+        }
+        else if (pobiMaxValue == crongMaxValue){ // 무승부면 0을 설정
+            result = 0
+        }
     }
+
+    return result
 }
+
+
+
--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,43 @@
 package onboarding
-
-fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+fun main(){ //와 스트링도 배열로 쓸 수 있구나
+    val string = "browoanoommnaon"
+    solution2(string)
 }
+fun solution2(cryptogram: String): String {
+    var tag = 0 // 연속으로 중복된 2개의 문자 다음과의 비교를 피하기 위한 체크용 변수이다.
+    var checkDuplication = 0 //중복된 문자열이 존재하는지 확인을 위한 변수
+    var cryptogram = cryptogram
+    cryptogram = cryptogram.lowercase() // 소문자로 변환
+    var extraString = "" // 중복된 문자를 제외한 나머지를 저장하는 변수이다.
+
+    while(true) { // 무한 반복문을 돌려
+        for (index in 1 until cryptogram.length) { // 문자열 개수 만큼의 for문에서
+            if (cryptogram[index] == cryptogram[index - 1]) { //이전문자와 현재 문자가 같다면\
+                checkDuplication = 1 // 중복 변수 1로 설정
+                tag = 1 // 태그를 1로 설정하고 continue
+                continue
+            } else { //아니라면 이전 문자를 extraString에 추가
+                if (tag == 1) { // 이 때 tag가 1이였다면 다시 0으로 설정 후 continue
+                    tag = 0
+                    continue
+                }
+                extraString += cryptogram[index - 1]
+            }
+        }
+
+        if(extraString == ""){ // for문 이후 extraString이 빈 문자열 상태가 되면 break
+            cryptogram = extraString
+            break
+        }
+        else{ //마지막 문자열 까지 추가를 해준 후 cryptogram은 extraString 값으로 바꾸고, extraString은 초기화
+            extraString += cryptogram[cryptogram.length-1]
+            if (checkDuplication == 0) break //만약 중복된 문자열이 없으면 break
+            cryptogram = extraString
+            extraString = ""
+            checkDuplication = 0 // 다시 0으로 초기화
+        }
+        println(cryptogram)
+    }
+    return cryptogram
+}
+
