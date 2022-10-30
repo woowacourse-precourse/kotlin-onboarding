634d49ee61aadc75baf1e977887f855f29260bc7
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-31 02:48:49 +0900

    wordlist 변수 수정 및 변환 간소화

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -7,20 +7,20 @@ package onboarding
 //4.리스트를 문자로 변환
 
 fun solution4(word: String): String {
-    var WordList:MutableList<Char> = word.toMutableList()   //문자를 리스트로 변환
-    WordList=frogDictionary(WordList)
-    return WordList.joinToString("")    //리스트를 문자로 변환
+    var wordlist:MutableList<Char> = word.toMutableList()   //문자를 리스트로 변환
+    wordlist=frogDictionary(wordlist)
+    return wordlist.joinToString("")    //리스트를 문자로 변환
 
 }
 //아스키 코드값을 이용한 문자 변환
-fun frogDictionary(WordList:MutableList<Char>): MutableList<Char> {
-    for(i:Int in 0 until WordList.size){
-        if(WordList[i].code in 65..96){  //대문자 일 때 아스키 코드값을 이용한 변경
-            WordList[i]=(WordList[i].code+((90-(WordList[i].code-65))-WordList[i].code)).toChar()
+fun frogDictionary(wordlist:MutableList<Char>): MutableList<Char> {
+    for(i:Int in 0 until wordlist.size){
+        if(wordlist[i].code in 65..96){  //대문자 일 때 아스키 코드값을 이용한 변경
+            wordlist[i]=(155-wordlist[i].code).toChar()
         }
-        else if(WordList[i].code in 97..122){ //소문자 일 때 아스키 코드값을 이용한 변경
-            WordList[i]=(WordList[i].code+((122-(WordList[i].code-97))-WordList[i].code)).toChar()
+        else if(wordlist[i].code in 97..122){ //소문자 일 때 아스키 코드값을 이용한 변경
+            wordlist[i]=(219-wordlist[i].code).toChar()
         }
     }
-    return WordList
+    return wordlist
 }
