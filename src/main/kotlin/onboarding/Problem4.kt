0ba3ebdcf4ec97117873436548d1db366f6043be
DormunDoorlock <skrudgh2001@naver.com>
2022-10-30 13:11:24 +0900

    feat: Problem4.kt 문자열 내 문자 변환 기능 추가

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,6 +1,13 @@
 package onboarding
 fun reverseAlphabet(num: Int):Int{
-    return 97
+    // 알파벳 소문자의 경우
+    if(num in 97..122)
+        return num + ( 25 - 2 * (num - 97) )
+    // 알파벳 대문자의 경우
+    if(num in 65 .. 90)
+        return num + ( 25 - 2 * (num - 65) )
+    // 알파벳 그 외 문자
+    return num
 }
 fun solution4(word: String): String {
     var result=""
