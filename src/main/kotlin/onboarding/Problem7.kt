676c0c787fa05a50f8109b2b0e0bb49887177f58
kyounggudev <homin222@naver.com>
2022-10-30 23:06:01 +0900

    문제7 - 요구사항 분석 추가

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,9 +5,9 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    val Myfriend = makeMyFriend(user,friends)
-    addFriend(user,friends,Myfriend)
-    addVisitor(visitors,Myfriend)
+    val myFriend = makeMyFriend(user,friends)
+    addFriend(user,friends,myFriend)
+    addVisitor(visitors,myFriend)
 
     val result = sortAsc(ScoreCalculate.Scores)
 
@@ -17,19 +17,19 @@ fun solution7(
 object ScoreCalculate{
     var Scores = mutableListOf<Score>()
 
-    fun Calculate(name : String , score : Int){
+    fun calculate(name : String, score : Int){
         when(Scores.any{it.name==name}){
             true -> addOld(name, score)
             false -> addNew(name, score)
         }
     }
 
-    fun addNew(name :String, score : Int){
+    private fun addNew(name :String, score : Int){
         val new = Score(name, score)
         Scores.add(new)
     }
 
-    fun addOld(name : String, score : Int){
+    private fun addOld(name : String, score : Int){
         for(i in Scores){
             if(i.name==name){
                 i.score+=score
@@ -42,39 +42,39 @@ object ScoreCalculate{
 data class Score(val name:String, var score:Int)
 
 fun makeMyFriend(user : String ,friends : List<List<String>>): List<String>{
-    val Myfriend : MutableList<String> = mutableListOf()
+    val myFriend : MutableList<String> = mutableListOf()
     for(relation in friends){
         if(relation.contains(user)){
-            Myfriend.add(when(relation[0]==user){
+            myFriend.add(when(relation[0]==user){
                 true -> relation[1]
                 false -> relation[0]
             })
         }
     }
-    return Myfriend
+    return myFriend
 }
 
-fun addFriend( user: String ,
-               friends: List<List<String>>,
-               Myfriend :List<String>){
+fun addFriend(user: String,
+              friends: List<List<String>>,
+              myFriend :List<String>){
     for(i in friends){
         if(!i.contains(user) &&
-            (Myfriend.any{ it == i[0] } xor Myfriend.any{it == i[1]} )){ //둘 다 친구인 경우는 제외
-            when(Myfriend.any{ it == i[0] }){
-                true -> ScoreCalculate.Calculate(i[1],10)
-                false ->ScoreCalculate.Calculate(i[0],10)
+            (myFriend.any{ it == i[0] } xor myFriend.any{it == i[1]} )){ //둘 다 친구인 경우는 제외
+            when(myFriend.any{ it == i[0] }){
+                true -> ScoreCalculate.calculate(i[1],10)
+                false ->ScoreCalculate.calculate(i[0],10)
             }
         }
     }
 }
 
-fun addVisitor(visitors : List<String>, Myfriend: List<String>){
+fun addVisitor(visitors : List<String>, myFriend: List<String>){
     val visitorList  = visitors.toMutableList()
-    for(i in Myfriend){
+    for(i in myFriend){
         visitorList.remove(i)
     }
     for(i in visitorList){
-        ScoreCalculate.Calculate(i,1)
+        ScoreCalculate.calculate(i,1)
     }
 }
 
