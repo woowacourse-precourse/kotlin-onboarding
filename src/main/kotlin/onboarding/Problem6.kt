04cc8f65137da47e4705e48ec6d25d232ff7e16f
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-10-30 18:36:45 +0900

    :sparkles: 모든 닉네임을 2글자씩 끊어서 저장

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,28 @@
 package onboarding
 
+private var result = mutableListOf<String>()
+private var content = HashMap<String, MutableSet<String>>()
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    repeatAllNickName(forms)
+//    repeatAllNickNameForFilter(forms)
+    result = result.distinct().toMutableList()
+    result.sort()
+    return result
+}
+private fun repeatAllNickName(forms : List<List<String>>){
+    for (i in 0 until forms.size) {
+        for (nickNameIndex in 0 until forms[i][1].length - 1) {
+            addSplitNickName(forms,i,nickNameIndex)
+        }
+    }
+}
+private fun addSplitNickName(forms : List<List<String>>, i : Int, nickNameIndex:Int){
+    if(content.containsKey(forms[i][1].substring(nickNameIndex,nickNameIndex+2))){
+        var currentSet = content.getValue(forms[i][1].substring(nickNameIndex,nickNameIndex+2))
+        currentSet.add(forms[i][0])
+        content.put(forms[i][1].substring(nickNameIndex,nickNameIndex+2),currentSet)
+    }
+    else {
+        content.put(forms[i][1].substring(nickNameIndex, nickNameIndex+2), mutableSetOf(forms[i][0]))
+    }
 }
