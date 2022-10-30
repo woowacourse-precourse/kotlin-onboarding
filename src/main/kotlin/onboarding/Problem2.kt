4b7812d8210d52944ee01165980fad883915072b
Sujin Kim <sujinkim919@gmail.com>
2022-10-30 23:13:37 +0900

    feat: 연속된 중복 문자를 제거하는 기능 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,19 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    return removeDuplicates(cryptogram)
+}
+
+fun removeDuplicates(str: String): String {
+    var decodedString = str
+    var i = 1
+    while(i < decodedString.length) {
+        if(decodedString[i-1] == decodedString[i]) {
+            decodedString = decodedString.replaceRange(i-1..i, "")
+        } else {
+            i++
+        }
+    }
+
+    return decodedString
 }
