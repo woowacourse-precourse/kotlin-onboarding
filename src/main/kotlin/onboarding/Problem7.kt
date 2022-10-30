fd5e15f1a2e5dab6fc1614731bb9c86bec733782
sujin-kk <sjoo0002@gmail.com>
2022-10-31 04:23:22 +0900

    [FEAT] 친구 관계 그래프 생성

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,7 +1,17 @@
 package onboarding
 
 fun main(args: Array<String>) {
-
+    val user = "mrko"
+    val friends = listOf(
+        listOf("donut", "andole"),
+        listOf("donut", "jun"),
+        listOf("donut", "mrko"),
+        listOf("shakevan", "andole"),
+        listOf("shakevan", "jun"),
+        listOf("shakevan", "mrko")
+    )
+    val visitors = listOf("bedi", "bedi", "donut", "bedi", "shakevan")
+    println(solution7(user, friends, visitors))
 }
 
 fun solution7(
@@ -9,7 +19,23 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val friendRelation = createFriendRelation(friends = friends)
+}
+
+fun createFriendRelation(friends: List<List<String>>): HashMap<String, MutableSet<String>> {
+    val friendRelation = hashMapOf<String, MutableSet<String>>()
+
+    friends.forEach { (friendA, friendB) ->
+        friendRelation[friendA]?.add(friendB) ?: run {
+            friendRelation[friendA] = mutableSetOf(friendB)
+        }
+
+        friendRelation[friendB]?.add(friendA) ?: run {
+            friendRelation[friendB] = mutableSetOf(friendA)
+        }
+    }
+
+    return friendRelation
 }
 
 const val MAX_RECOMMEND_NUM = 5
