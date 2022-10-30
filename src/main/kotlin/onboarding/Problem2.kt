4e64a1aa1138d3696c9eb4414643f5d6acf435d0
pingu244 <69796976+pingu244@users.noreply.github.com>
2022-10-30 23:49:16 +0900

    Feat: [2번 문제] 2개 이상의 중복 문자들 삭제 기능 추가

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -5,12 +5,17 @@ fun solution2(cryptogram: String): String {
 
     do {
         var loopCheck = false
-        for (i in 1 until word.length)
-            if (word[i] == word[i-1]) {
-                word = word.removeRange(i-1, i+1)
+        for (left in 0 until word.length-1){
+            var right = left + 1
+            while ((right != word.length) && (word[left] == word[right]))
+                right++
+
+            if (right != left+1){
+                word = word.removeRange(left, right)
                 loopCheck = true
                 break
             }
+        }
     } while (loopCheck)
 
     return word
