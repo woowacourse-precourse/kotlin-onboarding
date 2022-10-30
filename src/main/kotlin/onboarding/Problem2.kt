afcb914ac99a18af779c9b0f808a60452cc5407f
Suheon Song <ssheon0812@naver.com>
2022-10-30 20:29:19 +0900

    feat: [Problem2] 중복된 연속 문자열을 제거하는 함수 추가

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,32 @@
 package onboarding
 
+fun removeConsecutiveDuplicateCharacters(cryptogram: String): String {
+    var decodedString = ""
+    var isConsecutive = false
+
+    for (i in 0 until cryptogram.length) {
+        if (i == cryptogram.length - 1) {
+            // 현재 검사하고 있는 문자가 마지막 문자이고 그 전의 문자와 연속적으로 반복되지 않고 있다면 해독한 문자열에 추가한다.
+            if (!isConsecutive) {
+                decodedString += cryptogram[i]
+            }
+        } else {
+            if (cryptogram[i] == cryptogram[i + 1]) {
+                isConsecutive = true
+            } else {
+                // 현재 검사하고 있는 문자가 다음 문자와 다르고 그 전의 문자와 연속적으로 반복되지 않고 있다면 해독한 문자열에 추가한다.
+                if (!isConsecutive) {
+                    decodedString += cryptogram[i]
+                }
+                isConsecutive = false
+            }
+        }
+    }
+
+    if (cryptogram.length != decodedString.length) return removeConsecutiveDuplicateCharacters(decodedString)
+    return decodedString
+}
+
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    return removeConsecutiveDuplicateCharacters(cryptogram)
 }
