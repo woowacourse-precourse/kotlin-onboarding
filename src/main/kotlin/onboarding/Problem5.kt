4f7473df0a29b40d6003fcef44ff8960b42b2b2e
sujin-kk <sjoo0002@gmail.com>
2022-10-30 23:25:53 +0900

    [FEAT] 동전 반환 dp로 풀기

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,16 +1,58 @@
 package onboarding
 
-fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+import java.lang.Integer.min
+
+fun main(args: Array<String>) {
+    println(solution5(money = 50237))
 }
 
-const val FIFTY_THOUS = 50000
-const val TEN_THOUS = 10000
-const val FIVE_THOUS = 5000
-const val ONE_THOUS = 1000
-const val FINE_HUND = 500
-const val ONE_HUND = 100
-const val FIFTY = 50
-const val TEN = 10
-const val ONE = 1
+fun solution5(money: Int): List<Int> {
+    val coins = intArrayOf(
+        ONE,
+        TEN,
+        FIFTY,
+        ONE_HUND,
+        FIVE_HUND,
+        ONE_THOUS,
+        FIVE_THOUS,
+        TEN_THOUS,
+        FIFTY_THOUS
+    )
+    val coinCnt = Array(size = money + 1) { IntArray(size = coins.size) { 0 } } // 화폐 개수
 
+    return calculateCoinCnt(money = money, coins = coins, coinCnt = coinCnt)
+}
+
+// 반환 할 동전 개수를 계산하는 함수
+fun calculateCoinCnt(
+    money: Int,
+    coins: IntArray,
+    coinCnt: Array<IntArray>
+): List<Int> {
+    /* dp[i] -> {i}원을 거슬러 주는데 사용된 최소 화폐의 개수
+    dp[j] = min(dp[j], dp[j - coin[i]] + 1) */
+    val dp = IntArray(size = money + 1) { Int.MAX_VALUE }
+    dp[0] = 0
+
+    coins.forEachIndexed { cidx, coin ->
+        for (curMoney in coin .. money) {
+            if (dp[curMoney] > dp[curMoney - coin] + 1) {
+                dp[curMoney] = dp[curMoney - coin] + 1
+                coinCnt[curMoney] = coinCnt[curMoney - coin].copyOf()
+                coinCnt[curMoney][cidx]++
+            }
+        }
+    }
+
+    return coinCnt[money].reversed().toList()
+}
+
+const val ONE = 1
+const val TEN = 10
+const val FIFTY = 50
+const val ONE_HUND = 100
+const val FIVE_HUND = 500
+const val ONE_THOUS = 1000
+const val FIVE_THOUS = 5000
+const val TEN_THOUS = 10000
+const val FIFTY_THOUS = 50000
