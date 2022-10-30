34d8e71bece2ea378308075fc622a8f345efc45d
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-31 02:08:17 +0900

    변환한 돈을 저장한 리스트를 만든다.

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,16 @@
 package onboarding
 
+//기능별 목록
+//1.변환한 돈을 저장한 리스트를 만든다.
+//2.주어진 돈을 주어진 화폐의 큰 단위부터 변환한다.
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+
+    return changemoney(money)
+}
+
+fun changemoney(money: Int): MutableList<Int>{
+    var solutionList:MutableList<Int> = mutableListOf(0,0,0,0,0,0,0,0,0)   //변환한 돈을 저장한 리스트를 만든다.
+    var change:Int= money
+
+    return solutionList
 }
