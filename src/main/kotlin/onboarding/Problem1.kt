a37edf1fe6fb728f3d81a2312d193c0c586ceeb9
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-10-30 15:25:40 +0900

    [추가] 각 자리의 수를 더한값과 곱한값 비교하여 큰 수 반환하는 함수 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -7,3 +7,15 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 fun max(a: Int, b: Int): Int {
     return if (a >= b) a else b
 }
+
+fun bigNumber(num: Int): Int {
+    var input = num
+    var pSum = 0
+    var mSum = 1
+    while (input != 0) {
+        pSum += input % 10
+        mSum *= input % 10
+        input /= 10
+    }
+    return max(pSum, mSum)
+}
