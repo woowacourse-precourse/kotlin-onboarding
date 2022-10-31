4f5ca53649a8aedc005a41a8cafdd185346a3efd
yulim <yulim13sky@naver.com>
2022-10-31 21:21:54 +0900

    [REFACTOR] Problem5 for문 indices 없애기

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -5,9 +5,9 @@ fun solution5(money: Int): List<Int> {
     var currentMoney = money
     val unitMoney = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
 
-    for (i in unitMoney.indices) {
-        answer.add(currentMoney / unitMoney[i])
-        currentMoney %= unitMoney[i]
+    for (unit in unitMoney) {
+        answer.add(currentMoney / unit)
+        currentMoney %= unit
     }
 
     return answer
