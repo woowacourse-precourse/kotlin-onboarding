74416655d922708b107d6da4e035da1639ac2bf1
SeungHyeon Shim <tladhehd@naver.com>
2022-10-31 17:23:51 +0900

    Problem1_commit_2

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,45 +1,75 @@
 package onboarding
 
-fun digit_sum(num: Int): Int { // 각 자리수 합 return
-    var sum = 0
-    var n = num
-    while (n > 0) {
-        sum += n % 10
-        n /= 10
-    }
-    return sum
-}
-
-fun digit_multiple(num: Int): Int { // 각 자리수 곱 return
-    var multiple = 1
-    var n = num
-    while (n > 0) {
-        multiple *= n % 10
-        n /= 10
-    }
-    return multiple
-}
-
-fun max(a: Int, b: Int): Int { // a와 b중 큰 값 return
-    if (a > b) return a
-    return b
-}
-
-fun exception(lst: List<Int>): Boolean { // 예외 사항, 문제 발생 시 true return
-    if (lst.size != 2) return true // 입력된 배열의 크기가 2가 아닌 경우
-    if (lst.first() % 2 == 0 || lst.last() % 2 == 1) return true // 왼쪽 페이지가 짝수거나 오른쪽 페이지가 홀수인 경우
-    if (lst.first() + 1 != lst.last()) return true // 왼쪽 페이지와 오른쪽 페이지의 순서가 다르거나 차이가 1이 아닌 경우
-    if (lst.first() < 1 || lst.last() > 400) return true // 페이지가 1보다 작거나, 400보다 큰 경우와 시작면과 마지막 면인 경우
-    // 시작면과 마지막면을 책의 표지라 생각하
-    return false
-}
+import kotlin.math.max
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+    var p_score = 0
+    var c_score = 0
+    var left = 0
+    var right = 0
+    var plus = 0
+    var multiple = 1
+    var answer = 0
+    var flag = true
 
-    if (exception(pobi) || exception(crong)) return -1
+    /*예외사항 검증하는 곳
+    * 예외사항
+    * 1. 페이지 차이 1 초과 나는경우(테케)
+    * 2. 왼쪽 페이지가 더 큰 경우(아래 조건으로 성립)
+    */
+    if (pobi[1]-pobi[0]!=1 || crong[1]-crong[0]!=1){
+        flag = false
+    }
+    //포비 점수 구하기
+    for (j in 0 until pobi.size) {
+        // 값 초기화 시켜준 곳
+        plus = 0
+        multiple = 1
+        // 왼쪽, 오른쪽 페이지 각 자리 수 모두 더하거나 모두 곱해 수 값 비교
+        for (i in 0 until (pobi[j]).toString().length) {
+            plus += (pobi[j].toString()[i]).digitToInt()
+            multiple *= (pobi[j].toString()[i]).digitToInt()
+        }
+        if (j == 0) {
+            left = max(plus, multiple)
+        } else if (j == 1) {
+            right = max(plus, multiple)
+        }
 
-    val pobi_score = max(max(digit_sum(pobi.first()), digit_multiple(pobi.first())), max(digit_sum(pobi.last()), digit_multiple(pobi.last())))
-    val crong_score = max(max(digit_sum(crong.first()), digit_multiple(crong.first())), max(digit_sum(crong.last()), digit_multiple(crong.last())))
+    }
+    p_score = max(left, right)
+    //크롱 점수 구하기
+    for (j in 0 until crong.size) {
+        // 값 초기화 시켜준 곳
+        plus = 0
+        multiple = 1
+        // 왼쪽, 오른쪽 페이지 각 자리 수 모두 더하거나 모두 곱해 수 값 비교
+        for (i in 0 until (crong[j]).toString().length) {
+            plus += (crong[j].toString()[i]).digitToInt()
+            multiple *= (crong[j].toString()[i]).digitToInt()
+        }
+        if (j == 0) {
+            left = max(plus, multiple)
+        } else if (j == 1) {
+            right = max(plus, multiple)
+        }
 
-    return if(pobi_score>crong_score) 1 else if(pobi_score<crong_score) 2 else 0
+    }
+    c_score = max(left, right)
+    // 포비 점수와 크롱 점수 비교
+    /*
+    * 포비가 이긴다면 1, 크롱이 이긴다면 2
+    * 무승부는 0, 예외는 -1 return
+    * */
+    if (flag==false) { // 예외사항
+        answer = -1
+    } else if (p_score > c_score) {
+        answer = 1
+    } else if (p_score < c_score) {
+        answer = 2
+    } else if (p_score == c_score) {
+        answer = 0
+    }
+
+    return answer
 }
