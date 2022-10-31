1996d87d9e9d41849a90092598a588aee2b6e2d7
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-31 22:07:47 +0900

    fix(Problem6): if 조건문 추가하여 에러 수정

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -6,11 +6,10 @@ fun solution6(forms: List<List<String>>): List<String> {
         val targetName = forms[i][1]
         for (j in 0..targetName.length - 2) {
             val targetString = targetName.substring(j, j + 2)
-            for (k in i until forms.size) {
-                val nameToCheck = forms[k][1]
-                if (nameToCheck.contains(targetString)) {
-                    val emailToCheck = forms[k][0]
-                    result.add(emailToCheck)
+            for (k in forms.indices) {
+                if (forms[k][1].contains(targetString)) {
+                    if(forms[k][0] != forms[i][0])
+                        result.add(forms[k][0])
                 }
             }
         }
