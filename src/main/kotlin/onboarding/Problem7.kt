34d378e4244de94c812c43fee134cb5bdce00641
윤동현 <donghyunyoon81@gmail.com>
2022-11-01 02:48:38 +0900

    Feat:추천 친구 알고리즘 구현 현 친구를 추천받지 않기 위해서 현 친구 list도 for문으로 만들어줍니다. 추천 이름과 점수를 얻기 위해서 친구의 친구 list  타임 라인 방문자 list를 for문으로 만들어줍니다. 친구의 친구 중에 본인도 포함되어 있으니 중복되지 않게 만듭니다. 사람마다 점수를 주기 위해서 groupingby와 eachCount를 사용해서 각 사람마다 개수를 key와 value형식으로 만들어줍니다. for 문과 조건문을 사용해서 사람들의 추천점수를 key와 value값으로 할당한 map을 만듭니다. 점수가 가장 높은 순으로 정렬하기 위해서 value값을 기준으로 정렬합니다. 최대 5명만 추천하기 때문에 정렬한 뒤 5번째 이후는 목록에서 제외합니다. 친구 추천 목록에 list 형식으로 이름만 필요하므로 key값들만 담고 list로 형식을 변환해서 return합니다.

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,34 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val friendOfFriendList = mutableListOf<String>()
+    val friendsNameList = mutableListOf<String>()
+    val visitorNamesList = mutableListOf<String>()
+    val everyoneList = mutableSetOf<String>()
+    var nameAndScore = mutableMapOf<String,Int>()
+    for (i in friends) {
+        if (i[1] != user) {
+            friendOfFriendList.add(i[1])
+            friendsNameList.add(i[0])
+            everyoneList.add(i[1])
+        }
+    }
+    for (i in visitors) {
+        if (i != user && friendsNameList.indexOf(i) == -1) {
+            visitorNamesList.add(i)
+            everyoneList.add(i)
+        }
+    }
+    var friendOfFriendCountMap = friendOfFriendList.groupingBy {it}.eachCount()
+    var visitorNamesCountMap = visitorNamesList.groupingBy {it}.eachCount()
+    for(i in everyoneList) {
+        if(friendOfFriendCountMap.containsKey(i)) { nameAndScore[i] = friendOfFriendCountMap[i]!!.times(10) }
+    }
+    for(i in visitorNamesCountMap.keys) {
+        if(nameAndScore.containsKey(i)) nameAndScore[i]=nameAndScore[i]!!.plus(visitorNamesCountMap[i]!!)
+        else nameAndScore[i] = visitorNamesCountMap[i]!!
+    }
+    nameAndScore = if(nameAndScore.size>5)  nameAndScore.toSortedMap().toList().sortedByDescending { it.second }.subList(0,5).toMap() as MutableMap
+    else nameAndScore.toSortedMap().toList().sortedByDescending { it.second }.toMap() as MutableMap
+    return nameAndScore.keys.toList()
 }
