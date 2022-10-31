0ea397155bd635bd8018868689e3c5d0db068ec0
권용민 <briandr@naver.com>
2022-11-01 04:20:37 +0900

    [Problem1] 예외사항 검사  - 연속된 페이지인지 검사  - 페이지 범위 검사

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -6,5 +6,19 @@ private const val CRONG_WIN = 2
 private const val DRAW = 0
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if (!isException(pobi)) return ERROR
+    if (!isException(crong)) return ERROR
 }
+
+private fun isException(pages: List<Int>): Boolean {
+    return (isContinuous(pages) and isInRange(pages))
+}
+
+private fun isContinuous(pages: List<Int>): Boolean {
+    return (pages[1] - pages[0] == 1)
+}
+
+private fun isInRange(pages: List<Int>): Boolean {
+    return (pages[0] in 3..398) and (pages[1] in 3..398)
+}
+
