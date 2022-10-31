3236211fe86c259c3aff61070f76c707d64a78a7
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-31 21:13:20 +0900

    refactor(Problem2): 함수명 및 코드 리팩토링
    else 예약어 제거, 변수명 변경, 함수명 변경했습니다.

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -4,29 +4,23 @@ fun solution2(cryptogram: String): String {
     var text = cryptogram
     while (true) {
         val removedText = removeDuplicate(text)
-        if (text == removedText) {  // 더 이상 중복 문자 없음
+        if (text == removedText) {
             return text
-        } else {
-            text = removedText
         }
+        text = removedText
     }
 }
 
 fun removeDuplicate(text: String): String {
-    val length = text.length
-    return if (length < 2) {
-        text
-    } else {
-        var new = ""
-        if (text[0] != text[1]) new += text[0]  //첫번째 문자 따로 처리
-
-        for (i in 1 until length - 1) {
-            if (text[i] != text[i + 1] && text[i] != text[i - 1]) {
-                new += text[i].toString()
-            }
-        }
-
-        if (text[length - 1] != text[length - 2]) new += text[length - 1] //마지막 문자 따로 처리
-        new
+    if (text.length < 2) {
+        return text
     }
+    var newText = ""
+    for (i in text.indices) {
+        if ((text[i] != text.getOrNull(i-1)) && (text[i] != text.getOrNull(i + 1))) {
+            newText += text[i].toString()
+        }
+    }
+
+    return newText
 }
