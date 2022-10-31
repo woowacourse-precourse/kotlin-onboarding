c80e9efd41d6eaa8c8e56c038dc75b050938b1e7
YuGyeong98 <kuk3097@naver.com>
2022-10-31 22:59:05 +0900

    style(Problem2): 코드 포맷팅
    인덴트 수정, 함수 이름 변경

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,21 +1,23 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-  val str = StringBuilder(cryptogram)
-  val length = cryptogram.length
-  return decode(str, length)
+    val str = StringBuilder(cryptogram)
+    val length = cryptogram.length
+
+    return decodedCryptogram(str, length)
 }
 
-fun decode(str: StringBuilder, length: Int): String {
-  var len = length
-  for (i in 0 until len) {
-    if (i + 1 < str.length) {
-      if (str[i] == str[i + 1]) {
-        str.delete(i, i + 2)
-        len -= 2
-        decode(str, len)
-      }
+fun decodedCryptogram(str: StringBuilder, length: Int): String {
+    var len = length
+
+    for (i in 0 until len) {
+        if (i + 1 < str.length) {
+            if (str[i] == str[i + 1]) {
+                str.delete(i, i + 2)
+                len -= 2
+                decodedCryptogram(str, len)
+            }
+        }
     }
-  }
-  return str.toString()
+    return str.toString()
 }
