c2fc4659ffe9bd3045a83d0f5009a85ce51f1876
junsu1023 <12jangdokg55@gmail.com>
2022-10-31 14:35:03 +0900

    Problem4 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,22 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    val sb = StringBuilder()
+    var ascii: Int
+
+    word.toCharArray().forEach {
+        when (it) {
+            in 'a' .. 'z' -> {
+                ascii = 'z'.code - it.code + 'a'.code
+                sb.append(ascii.toChar())
+            }
+            in 'A' .. 'Z' -> {
+                ascii = 'Z'.code - it.code + 'A'.code
+                sb.append(ascii.toChar())
+            }
+            else -> sb.append(it)
+        }
+    }
+
+    return sb.toString()
 }
