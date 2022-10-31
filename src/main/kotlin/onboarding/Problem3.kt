f4aa426ab577bc3a5fbef86281e0efefebc542b8
lh99j <grand2181@gmail.com>
2022-10-31 23:49:43 +0900

    [lh99j] Solve Problem3

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,39 @@
 package onboarding
 
+import kotlin.math.sign
+
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var answer : Int = 0
+
+    for(i in 1 .. number){
+        val temp : Int= i
+        answer += isMatchNum(temp)
+    }
+
+    var a = 'A'
+    var b = 'Z'
+
+    println(a.code)
+    println(b.code)
+    return answer
+}
+
+fun isMatchNum(num : Int) : Int{
+    var count : Int = 0
+    var tempNum = num
+    var array : ArrayList<Int> = ArrayList<Int>()
+
+    while(tempNum != 0){
+        array.add(tempNum % 10)
+
+        tempNum /= 10
+    }
+
+    for(i in 0 until array.size){
+        if(array[i] == 3 || array[i] == 6 || array[i] == 9)
+            count++
+    }
+
+    return count
+
 }
