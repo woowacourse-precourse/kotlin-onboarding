c76b2cc6d98842e2bc050ae0b1eb40df2d06cd6b
healthykim <bsbs8645@snu.ac.kr>
2022-10-31 20:31:14 +0900

    feat(Problem6): Add function for counting frequency of substring
    Added countFrequency function in order to count frequency of every substring that can be generated from each nickname.
    Length parameter is also used for reusability.

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -17,6 +17,29 @@ fun makeSubstring(length: Int, string: String) : List<String> {
     }
 }
 
+/**
+ * Returns the map of frequency.
+ * The key is substrings of length [length] from nicknames in [forms].
+ * The value is frequency of substring key.
+ */
+fun countFrequency(forms: List<List<String>>, length: Int) : Map<String, Int> {
+    val result = mutableMapOf<String, Int>()
+    for(form in forms) {
+        if(form.size != 2) continue
+        val nickName: String = form[1]
+        val subStrings = makeSubstring(length, nickName)
+        for(subString in subStrings) {
+            if(result[subString] == null) {
+                result[subString] = 1
+            }
+            else {
+                result[subString] = result[subString]!! + 1
+            }
+        }
+    }
+    return result
+}
+
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
