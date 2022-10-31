a01c3f38ec3b59765ca3fa81a7c91477c31c6e1c
yulim <yulim13sky@naver.com>
2022-11-01 02:31:38 +0900

    [COMMENT] Problem6 기능 목록 추가

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,13 @@
 package onboarding
 
+/**
+기능 목록
+- 닉네임이 두 글자 이상 중복되는지 확인
+- 중복되는 경우 Set에 넣기
+- 오름차순 정렬
+- checkNickName: 닉네임이 두 글자 이상 중복되는 것이 있으면 true 반환
+ */
+
 fun solution6(forms: List<List<String>>): List<String> {
     val result = mutableSetOf<String>()
 
