e5e784291f9c2fdf2e0d7b3b417e1cd9ea497cee
권용민 <briandr@naver.com>
2022-11-01 04:21:58 +0900

    [Problem1] 포비와 크롱 각각의 가장 큰 수를 계산  - 자릿수의 합 구하기  - 자릿수의 곱 구하기

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,7 @@
 package onboarding
 
+import kotlin.math.max
+
 private const val ERROR = -1
 private const val POBI_WIN = 1
 private const val CRONG_WIN = 2
@@ -8,6 +10,9 @@ private const val DRAW = 0
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     if (!isException(pobi)) return ERROR
     if (!isException(crong)) return ERROR
+
+    val pobiMax = calculatePageMax(pobi)
+    val crongMax = calculatePageMax(crong)
 }
 
 private fun isException(pages: List<Int>): Boolean {
@@ -22,3 +27,28 @@ private fun isInRange(pages: List<Int>): Boolean {
     return (pages[0] in 3..398) and (pages[1] in 3..398)
 }
 
+private fun calculatePageMax(pages: List<Int>): Int {
+    val leftPage = max(plusDigit(pages[0]), multipleDigit(pages[0]))
+    val rightPage = max(plusDigit(pages[1]), multipleDigit(pages[1]))
+    return max(leftPage, rightPage)
+}
+
+private fun plusDigit(x: Int): Int {
+    var input = x
+    var result = 0
+    while (input != 0) {
+        result += input % 10
+        input /= 10
+    }
+    return result
+}
+
+private fun multipleDigit(x: Int): Int {
+    var input = x
+    var result = 1
+    while (input != 0) {
+        result *= input % 10
+        input /= 10
+    }
+    return result
+}
