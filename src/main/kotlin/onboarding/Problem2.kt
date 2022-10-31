03c206899ccad98ffcc149df53e806cd2ddd45e9
jaemin-Yoo <iamjm29@gmail.com>
2022-10-31 23:51:21 +0900

    fix: 2개 이상 중복 시 모두 제거

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -4,12 +4,20 @@ import java.util.Stack
 
 fun solution2(cryptogram: String): String {
     val stack = Stack<Char>()
+    var seqChar: Char? = null
     cryptogram.forEach { c ->
-        if (stack.isNotEmpty() && stack.peek() == c) {
+        if (c == seqChar) {
+            return@forEach
+        }
+
+        seqChar = if (stack.isNotEmpty() && stack.peek() == c) {
             stack.pop()
+            c
         } else {
             stack.add(c)
+            null
         }
     }
+
     return stack.joinToString("")
 }
