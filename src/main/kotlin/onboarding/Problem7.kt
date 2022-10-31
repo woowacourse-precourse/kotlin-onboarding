6fcfc5b8fbf3bd1f4af6d5a5933bc0eae8126d12
jaemin-Yoo <iamjm29@gmail.com>
2022-10-31 23:51:42 +0900

    refactor: 각 기능에 따른 함수 분리

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,7 +1,6 @@
 package onboarding
 
 import java.lang.Integer.min
-import java.util.*
 
 fun solution7(
     user: String,
@@ -10,6 +9,19 @@ fun solution7(
 ): List<String> {
     val friendsMap = mutableMapOf<String, MutableList<String>>()
     val users = mutableMapOf<String, Int>()
+
+    setFriendsMap(friends, friendsMap, users)
+    setVisitorScore(visitors, users)
+    setFriendsScore(friendsMap, users, user)
+
+    return getRecommendedUsers(users, user)
+}
+
+fun setFriendsMap(
+    friends: List<List<String>>,
+    friendsMap: MutableMap<String, MutableList<String>>,
+    users: MutableMap<String, Int>
+) {
     friends.forEach { friend ->
         val (a, b) = friend
         friendsMap[a]?.add(b) ?: run {
@@ -21,7 +33,9 @@ fun solution7(
         users[a] = 0
         users[b] = 0
     }
+}
 
+fun setVisitorScore(visitors: List<String>, users: MutableMap<String, Int>) {
     visitors.forEach {
         if (users.containsKey(it)) {
             users[it] = users[it]!! + 1
@@ -29,29 +43,28 @@ fun solution7(
             users[it] = 0
         }
     }
+}
 
+fun setFriendsScore(
+    friendsMap: MutableMap<String, MutableList<String>>,
+    users: MutableMap<String, Int>,
+    user: String
+) {
     friendsMap[user]?.forEach { friend ->
         users[friend] = 0
         friendsMap[friend]?.forEach {
             users[it] = users[it]!! + 10
         }
     }
+}
 
-    val result = users.filter { (k, v) ->
+fun getRecommendedUsers(users: MutableMap<String, Int>, user: String): List<String> {
+    val recommendedUsers = users.filter { (k, v) ->
         v > 0 && k != user
     }.toList().sortedByDescending {
         it.second
     }.map { (k, _) ->
         k
     }
-    return result.subList(0, min(5, result.size))
-}
-
-fun main() {
-    val users = sortedMapOf<Char, Int>(Collections.reverseOrder())
-    users['c'] = 2
-    users['b'] = 1
-    users['a'] = 3
-
-    println(users.toString())
+    return recommendedUsers.subList(0, min(5, recommendedUsers.size))
 }
