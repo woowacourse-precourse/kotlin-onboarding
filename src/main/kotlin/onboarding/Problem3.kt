437f6fb22f5c812e3b4dd6dd9a1ea8298a7d6bfc
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-31 21:01:22 +0900

    주석 수정 및 코드 정리

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -8,11 +8,11 @@ fun solution3(number: Int): Int {
 }
 
 //1부터 주어진 숫자까지 자리수를 쪼개 리스트로 바꾸고 countclap 실행하는 함수
-fun castlist(number:Int):Int{
-    var numberlist:List<Int> = listOf()
+fun castlist(number: Int): Int {
+    var numberlist: List<Int> = listOf()
     var clap = 0
-    for(i:Int in 1 .. number){
-        numberlist = i.toString().map{it.digitToInt()}  //주어진 숫자까지 자리수를 쪼개 리스트로 바꾼다.
+    for (i: Int in 1..number) {
+        numberlist = i.toString().map { it.digitToInt() }
         clap += countclap(numberlist)
     }
     return clap
@@ -20,11 +20,11 @@ fun castlist(number:Int):Int{
 
 
 //숫자 당 박수 카운트 함수
-fun countclap(numberlist: List<Int>):Int{
+fun countclap(numberlist: List<Int>): Int {
     var answer = 0
 
-    for(i in numberlist) {
-        if ( i==3 || i==6 || i==9) {    //리스트 원소가 3, 6, 9 일때 1카운트
+    for (i in numberlist) {
+        if (i == 3 || i == 6 || i == 9) {    //리스트 원소가 3, 6, 9 일때 1카운트
             answer += 1
         }
     }
