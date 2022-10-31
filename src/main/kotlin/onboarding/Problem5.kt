5e7aa3bbfefded44af2988e54887d40010d5bf07
dev-worthwhile <dev.lboram@gmail.com>
2022-10-31 22:54:57 +0900

    feat: (기능1-3) 잔액을 화폐 단위로 나누어서, 각 화폐 단위 별로 필요한 수량 확인

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -7,5 +7,20 @@ package onboarding
  * (3) 기능1-2를 반복하여 남은 단위도 몇 개가 필요한 지 확인한다.
  * **/
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var balance = money
+    val unitList = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10) //화폐단위
+    var result = mutableListOf<Int>() //각 단위 별로 필요한 수량 목록
+
+    for(i in 0 .. unitList.size-1){
+        if(i==unitList.size-1){
+            result.add(balance/unitList[i]) //10원 동전 수
+            result.add(balance%unitList[i]) //1원 동전 수
+            break
+        }
+
+        val unitQuantity = balance/unitList[i]
+        result.add(unitQuantity) //수량 목록에 추가
+        balance %= unitList[i] //잔액 갱신
+    }
+    return result
 }
