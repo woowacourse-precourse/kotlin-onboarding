2af9d233944b66ee11d3f67bcdedc66ce6585d8d
changhwan <chang5hustle@gmail.com>
2022-10-31 13:56:40 +0900

    [Problem 5] (요구사항 2) 화폐단위 별 값 연산 로직 작성

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,7 +1,8 @@
 package onboarding
 
-fun solution5(money: Int) {
+fun solution5(money: Int): List<Int> {
     val conversionToIndexMap = getConversionToIndexMap()
+    return calcUnitCount(money, conversionToIndexMap)
 }
 
 private fun getConversionToIndexMap(): MutableMap<Int, Int> {
@@ -12,4 +13,16 @@ private fun getConversionToIndexMap(): MutableMap<Int, Int> {
         conversionToIndexMap[moneyUnitList[i]] = i
 
     return conversionToIndexMap
+}
+
+private fun calcUnitCount(money: Int, conversionToIndexMap: MutableMap<Int, Int>): List<Int> {
+    val unitCount = mutableListOf<Int>()
+    var processedMoney = money
+
+    conversionToIndexMap.forEach {
+        unitCount.add(processedMoney / it.key)
+        processedMoney %= it.key
+    }
+
+    return unitCount
 }
