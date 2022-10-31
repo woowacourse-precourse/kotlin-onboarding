63df92b40533b806f82ae49ee8f8e572bf468765
82104 <tir2986@naver.com>
2022-10-31 20:13:43 +0900

    test: problem2 테스트 케이스 추가

--- src/test/kotlin/onboarding/ApplicationTest.kt
+++ src/test/kotlin/onboarding/ApplicationTest.kt
@@ -47,6 +47,27 @@ class ApplicationTest {
             val result = ""
             assertThat(solution2(cryptogram)).isEqualTo(result)
         }
+
+        @Test
+        fun case3() {
+            val cryptogram = "zyellleyz"
+            val result = ""
+            assertThat(solution2(cryptogram)).isEqualTo(result)
+        }
+
+        @Test
+        fun case4() {
+            val cryptogram = "zyalllelyz"
+            val result = "zyaelyz"
+            assertThat(solution2(cryptogram)).isEqualTo(result)
+        }
+
+        @Test
+        fun case5() {
+            val cryptogram = "aabba"
+            val result = "a"
+            assertThat(solution2(cryptogram)).isEqualTo(result)
+        }
     }
 
     @Nested
