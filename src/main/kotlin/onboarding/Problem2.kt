c399e88f375ec1efcc0387491f5ab458527cb3bd
Jaehun.Kim <rlawogns0411@naver.com>
2022-11-01 03:23:00 +0900

    feat : problem1 to 7

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,35 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var inputString = cryptogram
+    var resultString = deleteRepeat(inputString)
+    while(inputString!=resultString && resultString!=""){
+        inputString=resultString
+        resultString= deleteRepeat(inputString)
+    }
+    return resultString
+}
+fun deleteRepeat(s:String): String {
+    var inputString=s
+    var length = inputString.length
+    if(length==1){
+        return inputString
+    }
+    var result=""
+    if(inputString[0]!=inputString[1]){
+        result+=inputString[0]
+    }
+    for(i in 1..length-2){
+        result+= repeatString(inputString,i)
+    }
+    if(inputString[length-1]!=inputString[length-2]){
+        result+=inputString[length-1]
+    }
+    return result
+}
+fun repeatString(s:String, idx:Int): String {
+    if(s[idx]!=s[idx-1] && s[idx]!=s[idx+1]){
+        return s[idx].toString()
+    }
+    return ""
 }
--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,32 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var result =""
+    for(i in word){
+        result+=changeChar(i)
+    }
+    return result
+}
+fun isAlpha(c:Char): Boolean {
+    var ascii= c.toInt()
+    if((ascii>=65 && ascii<=90) ||(ascii>=97 && ascii<=122)){
+        return true
+    }
+    return false
+}
+
+fun changeChar(c: Char):Char{
+    if(!isAlpha(c)){
+        return c
+    }
+    if(c.isUpperCase()){
+        return changeUpper(c)
+    }
+    return changeLower(c)
+}
+fun changeUpper(c : Char): Char {
+    return (155-c.toInt()).toChar()
+}
+fun changeLower(c : Char): Char {
+    return (219-c.toInt()).toChar()
 }
