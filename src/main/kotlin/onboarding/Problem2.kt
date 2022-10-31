99f23c446a3ecb1b2632cae7f56c79fe1b80d960
Min-Jae-Bae <qoalswo123@icloud.com>
2022-10-31 19:30:10 +0900

    2번째 문제 테스트[성공]

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,32 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+
+    var newCryptogram = cryptogram
+    var isChanged = true
+
+    while (isChanged) {
+        isChanged = false
+        val newCryptogramCharArray: CharArray = newCryptogram.toCharArray()
+        var result = ' '
+
+        for (i in newCryptogramCharArray.indices) {
+
+            if (newCryptogramCharArray[i] == result) {
+                newCryptogramCharArray[i] = '-'
+                newCryptogramCharArray[i - 1] = '-'
+                isChanged = true
+            }
+
+            if (newCryptogramCharArray[i] != result) result = newCryptogramCharArray[i]
+        }
+
+        newCryptogram = String(newCryptogramCharArray)
+            .replace("-", "")
+
+    }
+    return newCryptogram
+
 }
+
+
