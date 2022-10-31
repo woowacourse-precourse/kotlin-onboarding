6e2e18c380b7190255fcaf02255f10e769073227
yulim <yulim13sky@naver.com>
2022-10-31 21:29:14 +0900

    [COMMENT] Problem5 기능 목록 추가

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,11 @@
 package onboarding
 
+/**
+기능 목록
+- 현재 돈을 단위로 나눈 몫을 리스트에 담기
+- 현재 돈을 나눈 나머지로 초기화
+ */
+
 fun solution5(money: Int): List<Int> {
     val answer = mutableListOf<Int>()
     var currentMoney = money
