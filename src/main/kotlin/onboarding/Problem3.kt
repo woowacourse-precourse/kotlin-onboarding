a5ff12b532782538a90d0dc037a57d626487d79f
jklom0326 <jklom0326@gmail.com>
2022-10-31 20:50:09 +0900

    Problem3 기능 목록 및 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,4 +1,9 @@
 package onboarding
+/*
+    기능 목록
+    제시된 숫자를 모두 String 타입으로 이어 붙인다음 filter 함수로 3, 6, 9의 개수를 반환하는 함수
+ */
+
 
 fun solution3(number: Int): Int  = (1..number)
     .joinToString("") { it.toString() }
