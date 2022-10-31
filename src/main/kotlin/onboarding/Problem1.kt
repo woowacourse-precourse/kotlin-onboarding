77ad4ebe4c0b6aca17466f0a6a47eaa9fd232753
dev-worthwhile <dev.lboram@gmail.com>
2022-10-31 13:24:04 +0900

    feat: (기능4) 포비와 크롱의 점수를 비교하여 결과 확인

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -4,8 +4,8 @@ package onboarding
  * 기능 목록
  * (1) 페이지 수를 잘못 기입한 예외 상황 검증 -> 예외 발생시 종료
  * (2) 포비와 크롱이 선택한 4개의 페이지 번호를 자릿수 별로 나누어 더하고, 곱한다
- * (3) 각 사용자가 가진 숫자 목록 중 가장 큰 숫자를 선택한다
- * (4) 포비의 숫자와 크롱의 숫자를 비교하여 결과 산출
+ * (3) 각 사용자가 가진 숫자 목록 중 가장 큰 숫자를 찾아 점수로 선택
+ * (4) 포비의 숫자와 크롱의 점수를 비교하여 결과 산출
  * **/
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     if(pobi[0]+1!=pobi[1] || crong[0]+1!=crong[1]){ //예외사항 검증
@@ -23,7 +23,13 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     pobiNumberList.sortDescending() //포비 숫자 - 내림차순 정렬
     crongNumberList.sortDescending() //크롱 숫자 - 내림차순 정렬
 
-    return 0
+    var result = 0 //결과 (무승부)
+    if(pobiNumberList[0]>crongNumberList[0]){ //포비가 이기는 경우
+        result = 1
+    }else if(pobiNumberList[0]<crongNumberList[0]){ //크롱이 이기는 경우
+        result = 2
+    }
+    return result
 }
 private fun getCalculatedNumbers(pageNumber: Int) :List<Int>{
     var target = pageNumber
