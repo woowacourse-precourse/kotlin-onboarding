75e6f23bc89024c2d249667e3462672d74b93048
ParkSeungMin <qazwsxedc1226@gmail.com>
2022-10-31 14:47:28 +0900

    문제5번 구현 완료 v5

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,18 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val list=MutableList<Int>(9, {i->0}) //지폐와 동전의 갯수를 담을 리스트 생성
+    val unitList:List<Int> = listOf(50000,10000,5000,1000,500,100,50,10,1) //각 화폐의 단위가 저장된 리스트
+
+    if(money<=0){  //0보다 작은 입력이 들어오면 빈 리스트 반환
+        return list
+    }
+    var answer= money
+
+    for(i in 0 until 9){ //9번 반복한다
+        list[i]=answer/unitList[i] //큰 단위부터 나누어서 몫을 리스트에 저장
+        answer=answer%unitList[i]   // 나눈 나머지를 answer에 저장
+    }
+
+    return list
 }
