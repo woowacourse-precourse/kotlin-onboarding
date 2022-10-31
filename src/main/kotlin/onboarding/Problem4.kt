d5fb1882421c0896e08c34bb5d0417d440d1f81b
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-10-31 10:38:41 +0900

    4s 룩업테이블 함수

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,3 +3,15 @@ package onboarding
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
+fun Lut(): HashMap<Char, Char> { //룩업테이블을 만들어주는 함수
+    var lukupTable = HashMap<Char, Char>()
+    for (i in 0..25)//룩업테이블 활용
+    {
+        lukupTable.put('\u0061' + i, '\u007A' - i)  //a = z 부터 z = a 까지 값 저장
+    }
+    for (i in 0..25)//룩업테이블 활용
+    {
+        lukupTable.put('\u0041' + i, '\u005A' - i) //A = Z 부터 Z ==A 까지 값 저장
+    }
+    return lukupTable
+}
