6dde58570065e927fc036028e0074912489add91
s9hn <tnghqn@naver.com>
2022-11-01 03:25:52 +0900

    [REFACTOR(미션6)] 코드 정리 및 기능 구현 수정
    1. 해시맵(포문) 제거
    2. mutableSet 생성
    3. 이름은 같으나, 이메일이 다를 경우 분기처리 추가

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,33 +1,24 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
+    val resultSet = mutableSetOf<String>()
 
-    val hash = hashMapOf<String, String>()
-    var name = listOf<String>()
-    val email = mutableListOf<String>()
+    for (j in forms.indices) { // j = 0
 
-    for (i in forms.indices) {
+        val nickName = forms[j][1] // 제이슨
+        for (k in 0 until nickName.length - 1) { // k = 0
+            val string = nickName.substring(k, k + 2)
 
-        name += listOf(forms[i][1])
-        hash[forms[i][1]] = forms[i][0]
+            for (l in forms.indices) { // l = 0 , 1부터 4
 
-    }
+                if ((nickName != forms[l][1]) and forms[l][1].contains(string)) { // 원피수.contains(원피)
+                    resultSet.add(forms[l][0])
 
-    for (j in name.indices) {
 
-        for (k in 0 until name[j].length - 1) {
-
-            for (l in name.indices) {
-
-                if ((name[l] != name[j]) and name[l].contains(name[j].substring(k, k + 2))) {
-                    email += hash[name[l]] ?: ""
-
-                    continue
                 }
-
             }
         }
     }
 
-    return email.distinct().sorted()
+    return resultSet.sorted()
 }
