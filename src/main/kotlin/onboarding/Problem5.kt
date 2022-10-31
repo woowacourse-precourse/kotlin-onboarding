379ee954a3b7a2a1f19217fbd461ea074a720466
changhwan <chang5hustle@gmail.com>
2022-10-31 12:41:22 +0900

    [Problem 5] (요구사항 1) 화폐 단위 별 Index 변환 map 작성

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,15 @@
 package onboarding
 
-fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+fun solution5(money: Int) {
+    val conversionToIndexMap = getConversionToIndexMap()
+}
+
+private fun getConversionToIndexMap(): MutableMap<Int, Int> {
+    val conversionToIndexMap = mutableMapOf<Int, Int>()
+    val moneyUnitList = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+
+    for (i in moneyUnitList.indices)
+        conversionToIndexMap[moneyUnitList[i]] = i
+
+    return conversionToIndexMap
 }
