5dddd0c96f5a2d109a575b3144ebbc96c170e546
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-11-01 01:28:54 +0900

    6s 같은글자 연속적으로 포함인지 확인하는 함수

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -5,6 +5,16 @@ import java.util.regex.Pattern
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+fun isCompareWard(name1: String, name2: String): Boolean {
+    var result: Boolean = false
+    for (i in 0..name1.length - 2) {
+        var compareWard: String = name1.substring(i, i + 2)
+        if (name2.contains(compareWard)) {
+            result = true
+        }
+    }
+    return result
+}
 fun isKoreanNickname(nickname : String):Boolean{
     val nicknamePattern = "^[가-힣]*$"
     val pattern = Pattern.compile(nicknamePattern)
