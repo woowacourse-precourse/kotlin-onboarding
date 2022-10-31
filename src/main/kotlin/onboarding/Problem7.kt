b772b82e710415287dcbe1615b0c99cd93953477
Gloria <ippnsj@gmail.com>
2022-11-01 08:55:43 +0900

    feat(Problem7): 사용자 아이디가 올바른지 확인하는 기능 사용자 아이디 길이가 1 이상 30 이하인지 확인 사용자 아이디가 알파벳으로만 이루어져 있는지 확인 사용자 아이디가 소문자로만 이루어져 있는지 확인

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,27 @@
 package onboarding
 
+import problem2.AlphabetVerifier
+import problem2.LowerCaseVerifier
+import problem2.StringLengthVerifier
+
+private fun validateUser(user:String) {
+    val userValidator = InputValidator(
+        listOf(
+            StringLengthVerifier(1, 30),
+            AlphabetVerifier(),
+            LowerCaseVerifier()
+        )
+    )
+
+    userValidator.validate(user)
+}
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    validateUser(user)
+    
+    return listOf()
 }
