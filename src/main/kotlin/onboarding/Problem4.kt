a3a02ede1c3d9d83dbe2c886d9f50763c327bc1b
phs <gustjd0808@pusan.ac.kr>
2022-11-01 02:59:22 +0900

    Problem4 기능 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,23 @@
 package onboarding
 
+fun changeUpperCase(alphabet: Char): Char =
+    ('Z'.code - alphabet.code + 'A'.code).toChar()
+
+
+fun changeLowerCase(alphabet: Char): Char =
+    ('z'.code - alphabet.code + 'a'.code).toChar()
+
+
+fun changeCharacter(character: Char): Char = when (character) {
+    in 'A'..'Z' -> changeUpperCase(character)
+    in 'a'..'z' -> changeLowerCase(character)
+    else -> character
+}
+
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var res = ""
+    for (c in word) {
+        res += changeCharacter(c)
+    }
+    return res
 }
