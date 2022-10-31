432ccb2d07d83457134eba8d56d09cfb37de2e56
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-10-31 23:45:54 +0900

    feat: (문제4) a~z,A~Z와 z~a,Z~A 리스트 만들기 기능 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+import java.lang.StringBuilder
+
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+}
+
+fun convertWord(word: String): StringBuilder{
+    val azList: List<Char> =
+        listOf('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+    val zaList: List<Char> =
+        listOf('z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a',
+        'Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K','J','I','H','G','F','E','D','C','B','A')
 }
