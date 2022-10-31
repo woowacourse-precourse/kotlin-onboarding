159b052757e240c40c4311c5e491508a6c60a3cf
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-01 02:11:47 +0900

    feat(problem6): Add get users two letter name set method

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,27 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val twoLetterNameSetList = setTwoLetterNameSetList(forms)
+    return emptyList()
+}
+
+// 각 유저마다 2글자씩 분리한 Set 생성
+fun getTwoLetterNameSet(userNameList: List<String>): MutableSet<String> {
+    val twoLetterNameSet = mutableSetOf<String>()
+    val userNickname = userNameList[1]
+    for (alphabetIdx in 0 until userNickname.lastIndex) {
+        val twoLetterName = "${userNickname[alphabetIdx]}${userNickname[alphabetIdx + 1]}"
+        twoLetterNameSet.add(twoLetterName)
+    }
+    return twoLetterNameSet
+}
+
+fun setTwoLetterNameSetList(forms: List<List<String>>): MutableList<MutableSet<String>> {
+    val twoLetterNameSetList = mutableListOf<MutableSet<String>>()
+    for (user in forms) {
+        val twoLetterNameSet = getTwoLetterNameSet(user)
+        twoLetterNameSetList.add(twoLetterNameSet)
+    }
+
+    return twoLetterNameSetList
 }
