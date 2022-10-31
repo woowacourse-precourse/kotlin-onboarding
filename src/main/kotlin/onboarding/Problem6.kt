c21b207d817b62f1aa2f66c6fa4e55949dafff8e
jklom0326 <jklom0326@gmail.com>
2022-10-31 21:05:50 +0900

    [Problem6] 기능 목록 및 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,4 +1,11 @@
 package onboarding
+/*
+    기능 목록
+    1. 이메일 유효성 검사하는 함수 isValidEmail
+    2. 닉네임 유효성 검사하는 함수 isValidNickName
+    3. 닉네임 중복 체크하는 함수 checkNickName
+
+ */
 
 fun solution6(forms: List<List<String>>): List<String> {
     var result = forms.filter { it -> isValidEmail(it[0]) && isValidNickName(it[1]) }
@@ -6,26 +13,6 @@ fun solution6(forms: List<List<String>>): List<String> {
     forms.filter { it -> isValidEmail(it[0]) && isValidNickName(it[1]) }
     // 닉네임 중복 체크
     checkNickName(result).sortedBy { it.toString() }
-    println(checkNickName(result).sortedBy { it })
     // 이메일 정렬
     return checkNickName(result).sortedBy { it }
-}
-
-fun isValidEmail(email: String): Boolean = "[A-Za-z]*(@email.com)$".toRegex().matches(email)
-fun isValidNickName(nickName: String): Boolean = "[가-힣]{1,19}".toRegex().matches(nickName)
-
-fun checkNickName(forms: List<List<String>>): List<String> {
-    val resultList = mutableSetOf<String>()
-    for (i in forms) {
-        var window = i[1].split("").windowed(2).map { it.joinToString("") }.filter { it.length == 2 }
-        for (j in forms) {
-            if (i == j) continue
-            window.forEach {
-                if (j[1].contains(it)) {
-                    resultList += j[0]
-                }
-            }
-        }
-    }
-    return resultList.toList()
 }
