98cdbab7f235739266badb12888e8ee965321041
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-10-31 21:53:18 +0900

    22.10.31 problem1 기능 목록 작성 및 기능 1번 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if (isCorrect(pobi) && isCorrect(crong)) {
+
+    }
+    else print("값이 올바르지 않습니다. 다시 입력해주세요.")
+    return 0;
+}
+
+fun isCorrect(list: List<Int>): Boolean {
+    val pageLeft = list[0]
+    val pageRight = list[1]
+    val condition1 = list.size == 2
+    val condition2 = (pageLeft in 1..400 && pageRight in 1..400)
+    val condition3 = (pageLeft % 2 == 1) && (pageRight % 2 == 0)
+    val condition4 = pageRight - pageLeft == 1
+
+    return condition1 && condition2 && condition3 && condition4
 }
