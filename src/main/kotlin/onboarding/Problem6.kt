9a773737b35a7ab56c0b58249755a9dc24d04948
no1msh <no1msh1217@gmail.com>
2022-10-31 16:22:20 +0900

    [Problem6][style] 기능 목록 주석 작성
    1. 사용할 HashSet과 MutableMap 선언
    2. 닉네임을 하나 받아 2글자씩 순회하면서 해시맵에 저장하는 메소드 구현
    3. 중복된 닉네임을 가진 이메일들을 추합하여 정렬하여 리턴

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,9 @@
 package onboarding
 
+// 기능 목록
+// 1. 사용할 HashSet과 MutableMap 선언
+// 2. 닉네임을 하나 받아 2글자씩 순회하면서 해시맵에 저장하는 메소드 구현
+// 3. 중복된 닉네임을 가진 이메일들을 추합하여 정렬하여 리턴
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
