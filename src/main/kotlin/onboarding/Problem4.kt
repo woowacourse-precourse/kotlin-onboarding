21dc4180f9133424aea93aae43e595509e738735
jklom0326 <jklom0326@gmail.com>
2022-10-31 20:56:12 +0900

    Problem4 reverseUpper, reverseLower 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -12,3 +12,23 @@ fun solution4(word: String): String {
     result = reverseLower(result)
     return result
 }
+
+fun reverseUpper(word: String): String {
+    val reverseUpper = word.map { it.code }.toMutableList()
+    for (i in word.indices) {
+        if (reverseUpper[i].toChar() in 'A'..'Z') {
+            reverseUpper[i] = ('A'.code + 'Z'.code) - reverseUpper[i]
+        }
+    }
+    return reverseUpper.map { it.toChar() }.joinToString("")
+}
+
+fun reverseLower(word: String): String {
+    val reverseLower = word.map { it.code }.toMutableList()
+    for (i in word.indices) {
+        if (reverseLower[i].toChar() in 'a'..'z') {
+            reverseLower[i] = ('a'.code + 'z'.code) - reverseLower[i]
+        }
+    }
+    return reverseLower.map { it.toChar() }.joinToString("")
+}
