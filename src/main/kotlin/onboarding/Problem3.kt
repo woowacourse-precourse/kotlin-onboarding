4eccf99c4a5300f8ee1e6d54a4b0af5e9379fd1c
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-01 02:08:34 +0900

    feat(problem3): Add return clab count method

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -5,7 +5,7 @@ import java.util.*
 fun solution3(number: Int): Int {
     val clabCountList: MutableList<Int> = ArrayList(Collections.nCopies(10003, -1))
     setSingleDigitCountList(clabCountList)
-    return 0
+    return getSumClabCount(number, clabCountList)
 }
 
 fun setSingleDigitCountList(clabCountList: MutableList<Int>) {
@@ -13,4 +13,25 @@ fun setSingleDigitCountList(clabCountList: MutableList<Int>) {
         if (number % 3 == 0) clabCountList[number] = 1
         else clabCountList[number] = 0
     }
+}
+
+fun setCurrentNumberClabCount(currentNumber: Int, clabCountList: MutableList<Int>) {
+    val unitDigit = currentNumber % 10
+
+    clabCountList[currentNumber] = clabCountList[currentNumber / 10]
+    if (unitDigit != 0 && (unitDigit % 3 == 0)) {
+        clabCountList[currentNumber]++
+    }
+}
+
+fun getSumClabCount(lastNumber: Int, clabCountList: MutableList<Int>): Int {
+    var numberClabCount = 0
+
+    for (currentNumber in 1..lastNumber) {
+        if (currentNumber >= 10)
+            setCurrentNumberClabCount(currentNumber, clabCountList)
+
+        numberClabCount += clabCountList[currentNumber]
+    }
+    return numberClabCount
 }
