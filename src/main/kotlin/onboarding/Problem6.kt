51104a24e27a6ba1d31c14a7ce49291f61e292a7
healthykim <bsbs8645@snu.ac.kr>
2022-10-31 20:10:14 +0900

    feat(Problem6): Add function for making list of substrings
    Added makeSubstring function.
    This function returns the list of substrings with [length] in [string]
    [length] parameter is for code reusability.

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,22 @@
 package onboarding
 
+/**
+ * Returns the list of substrings with [length] in [string]
+ */
+fun makeSubstring(length: Int, string: String) : List<String> {
+    var result: MutableList<String> = mutableListOf()
+    if(string.length == length) {
+        result.add(string)
+        return result
+    }
+    else {
+        for (index in 0..string.length-length) {
+            result.add(string.substring(index, index+length))
+        }
+        return result
+    }
+}
+
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
