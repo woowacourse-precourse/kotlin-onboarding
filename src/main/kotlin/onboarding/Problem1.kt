77ecc91e0a78c6d54e255f20e6e6b79e640b9799
dlgocks1 <cksgodl1@naver.com>
2022-11-01 02:32:03 +0900

    modify: (Problem1) 주석 및 변수정 가독성을 위해 수정

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,8 +1,8 @@
 package onboarding
 
-const val POBI_WIN = 1
-const val CRONG_WIN = 2
-const val DRAW = 0
+private const val POBIWIN = 1
+private const val CRONGWIN = 2
+private const val DRAW = 0
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     if (checkException(pobi, crong)) {
         return -1
@@ -23,17 +23,15 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
  * 예외 사항 체크
  * 좌우 페이지가 1차이가 나지 않으면 true를 반환한다.
  */
-fun checkException(pobi: List<Int>, crong: List<Int>): Boolean {
+private fun checkException(pobi: List<Int>, crong: List<Int>): Boolean {
     if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1) {
         return true
     }
     return false
 }
 
-/**
- * 각자리 숫자를 모두 더하거나 곱해 큰 수를 반환하는 기능
- */
-fun compareAddAndMul(num: Int): Int {
+/** 각자리 숫자를 모두 더하거나 곱한수 중 큰 수를 반환하는 함수 */
+private fun compareAddAndMul(num: Int): Int {
     var userNum = num
     var addNum = 0
     var mulNum = 1
@@ -45,10 +43,8 @@ fun compareAddAndMul(num: Int): Int {
     return if (addNum > mulNum) addNum else mulNum
 }
 
-/**
- * 왼쪽 페이지, 오른쪽 페이지 중 큰 수를 구하는 기능
- */
-fun getBiggerPageNum(leftPage: Int, rightPage: Int): Int {
+/** 왼쪽 페이지, 오른쪽 페이지 중 compareAddAndMul을 수행해 큰 수를 반환한다. */
+private fun getBiggerPageNum(leftPage: Int, rightPage: Int): Int {
     val leftNum = compareAddAndMul(leftPage)
     val rightNum = compareAddAndMul(rightPage)
     return if (leftNum > rightNum) leftNum else rightNum
@@ -60,5 +56,5 @@ fun getBiggerPageNum(leftPage: Int, rightPage: Int): Int {
  * 크롱이 이기면 2
  * 무승부는 0
  */
-fun validateWinner(pobiNum: Int, crongNum: Int): Int =
-    if (pobiNum == crongNum) DRAW else if (pobiNum > crongNum) POBI_WIN else CRONG_WIN
+private fun validateWinner(pobiNum: Int, crongNum: Int): Int =
+    if (pobiNum == crongNum) DRAW else if (pobiNum > crongNum) POBIWIN else CRONGWIN
