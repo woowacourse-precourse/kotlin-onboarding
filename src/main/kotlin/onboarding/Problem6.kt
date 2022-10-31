e38535aba845b74b4df1a0725ff689eec3ad41e9
cyjadela <adelacho@naver.com>
2022-10-31 20:56:10 +0900

    feat: [문제6] duplicateString 추출

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -7,6 +7,13 @@ fun solution6(forms: List<List<String>>): List<String> {
         val email = value[0]
         val name = value[1]
 
+        for (j in 0..name.length - 3) {
+            val duplicateString = if (name.length != 2) {
+                name.substring(j, j + 2)
+            } else {
+                name
+            }
+        }
     }
 
     return result.sorted()
