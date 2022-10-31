7bde0f58bff0245025c4fe7305d2587645e0e695
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-10-31 10:45:49 +0900

    4s 문자를 변환하는 함수

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,6 +3,7 @@ package onboarding
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
+
 fun Lut(): HashMap<Char, Char> { //룩업테이블을 만들어주는 함수
     var lukupTable = HashMap<Char, Char>()
     for (i in 0..25)//룩업테이블 활용
@@ -14,4 +15,18 @@ fun Lut(): HashMap<Char, Char> { //룩업테이블을 만들어주는 함수
         lukupTable.put('\u0041' + i, '\u005A' - i) //A = Z 부터 Z ==A 까지 값 저장
     }
     return lukupTable
+}
+
+fun changeAlphabet(word : String ,lukupTable :HashMap<Char,Char>): String { //문자에서 룩업테이블의 값이 있는지 확인해서 변환해주는 함수
+    var result = ""
+    for (i in word.indices) { //문자열 길이만큼 반복
+        var char: Char = word[i]
+        if (char in '\u0061'..'\u007A' || char in '\u0041'..'\u005A') { //만약 알파벳이면 룩업테이블을 활용해서 변환
+            result += (lukupTable.get(char)).toString()
+        }
+        if (lukupTable.get(char) == null) {//만약 알파벳이 아니면 그대로 저장
+            result += word[i].toString()
+        }
+    }
+    return result
 }
