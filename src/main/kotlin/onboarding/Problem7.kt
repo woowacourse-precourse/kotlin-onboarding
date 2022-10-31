bf7f7c8466b6f67144d86730d614814e60f06a3a
bluecat0823 <star4_you@naver.com>
2022-11-01 01:58:56 +0900

    미션7 : 이미 친구인 사람 제외

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -6,16 +6,16 @@ fun solution7(
     visitors: List<String>
 ): List<String> {
 
-    var recommend = HashMap<String,Int>()
+    var recommend = mutableMapOf<String,Int>()
     var visited = mutableListOf<String>()
-    var score = mutableListOf<Int>()
+    var count = mutableListOf<Int>()
     var person = mutableListOf<String>()
     var alreadyFriend = mutableListOf<String>()
 
     for (i in visitors.indices){
         visited.add(visitors[i])
         person.add(visitors[i])
-        score.add(1)
+        count.add(1)
     }
 
     for (group in friends){
@@ -30,26 +30,27 @@ fun solution7(
         if (group[0] in alreadyFriend){
             if (group[1] != user){
                 person.add(group[1])
-                score.add(10)
+                count.add(10)
             }
         } else if(group[1] in alreadyFriend){
             if (group[0] != user){
                 person.add(group[0])
-                score.add(10)
+                count.add(10)
             }
         }
     }
 
+    for (j in person.indices){
+        if (person[j] !in recommend.keys){
+            recommend[person[j]] = count[j]
+        } else if (person[j] in recommend.keys){
+            recommend[person[j]] = recommend[person[j]]!! + count[j]
+        }
+    }
+
+    for (person in alreadyFriend){
+        recommend.remove(person)
+    }
 
 
-
-
-    /*
-     - 친구들의 총점을 구한다.
-     - 총점이 가장 높은 5명을 찾는다.
-     - 점수가 0점인 경우는 삭제한다.
-     - 추천할 친구 중 점수가 같은 경우가 있으면 이름순으로 정렬한다.
-     */
-
-    return person
 }
