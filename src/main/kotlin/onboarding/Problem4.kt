e4012a876f155a5f2c89b30fe64718cec3e0bf15
jjiiin <wjdwlin4051@gmail.com>
2022-10-31 22:09:23 +0900

    refactor: Problem4 else 제거 및 영문자 외 다른 문자는 해독하지 않는 코드 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -4,7 +4,7 @@ fun solution4(word: String): String {
     val wordToCharList: List<Char> = word.toList()
     var wordReverseResult = ""
 
-    for ( letter in wordToCharList){
+    for ( letter in wordToCharList ){
         wordReverseResult += frogDictionary(letter)
     }
     return wordReverseResult
@@ -15,10 +15,11 @@ fun frogDictionary(letter:Char):Char{
     if(letter.isUpperCase()){
         return (90 - (letter.code - 65)).toChar()
 
-    }else if(letter.isLowerCase()) {
+    }
+    if(letter.isLowerCase()) {
         return (122 - (letter.code - 97)).toChar()
 
-    }else{
-        return 32.toChar()
     }
+    return letter
+
 }
