ee80a6b8ae62a487b3c90e3311eddae4332d7933
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-10-31 22:16:11 +0900

    22.10.31 problem1 기능 2번 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -2,6 +2,11 @@ package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     if (isCorrect(pobi) && isCorrect(crong)) {
+        val pSumOfPageLeft = addNumberOfDigit(pobi[0])
+        val pSumOfPageRight = addNumberOfDigit(pobi[1])
+        val cSumOfPageLeft = addNumberOfDigit(crong[0])
+        val cSumOfPageRight = addNumberOfDigit(crong[1])
+
 
     }
     else print("값이 올바르지 않습니다. 다시 입력해주세요.")
@@ -17,4 +22,14 @@ fun isCorrect(list: List<Int>): Boolean {
     val condition4 = pageRight - pageLeft == 1
 
     return condition1 && condition2 && condition3 && condition4
+}
+
+fun addNumberOfDigit(num: Int): Int {
+    var number = num
+    var sum = 0
+    while (number != 0) {
+        sum += (number % 10)
+        number /= 10
+    }
+    return sum
 }
