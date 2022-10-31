ab945b0c514a3c1a8da87948a83acafa94f09afe
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-01 02:14:34 +0900

    feat(problem7): Add get input user's friend set method

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,23 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    return emptyList()
+}
+
+fun checkUserFriend(relationship: List<String>, userName: String): String? {
+    if (relationship[0] == userName && relationship[1] == userName) return null
+
+    return when (userName) {
+        relationship[0] -> relationship[1]
+        relationship[1] -> relationship[0]
+        else -> null
+    }
+}
+
+fun getUserFriends(name: String, allFriendsList: List<List<String>>): Set<String> {
+    val userFriends = mutableSetOf<String>()
+    for (relationship in allFriendsList) {
+        checkUserFriend(relationship, name)?.let { friendName -> userFriends.add(friendName) }
+    }
+    return userFriends
 }
