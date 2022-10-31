5b74515a1482e087ad17b8eabce1d8ea9fd6ada5
Gloria <ippnsj@gmail.com>
2022-10-31 17:49:49 +0900

    feat(Problem5): 입력값이 올바른지 확인하는 기능 돈의 액수가 1 이상 1000000 이하인지 확인

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+import problem1.RangeVerifier
+import java.lang.RuntimeException
+
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val minRange = 1
+    val maxRange = 1000000
+    val rangeVerifier = RangeVerifier(money, minRange, maxRange)
+    if(!rangeVerifier.verify()) {
+        throw RuntimeException("Input is not between range $minRange and $maxRange")
+    }
+
+    return listOf()
 }
