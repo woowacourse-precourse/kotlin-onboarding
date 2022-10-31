27c0276ea36ce4ba600dea2812d1701dff91c7a4
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-11-01 01:48:20 +0900

    7s 유저친구찾는함수

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,22 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    var result = mutableListOf<String>()
+    var score: HashMap<String, Int> //점수 해쉬맵
+    var myFriends = mutableListOf<String>() // 나의 친구들
+
+    //친구 찾기
+    fun findMyFriends(friends: List<List<String>>): MutableList<String> {
+        var MyFriends = mutableListOf<String>()
+        for (i in 0..friends.size - 1) {
+            if (user.equals(friends[i][0])) {
+                MyFriends.add(friends[i][1])
+            }
+            if (user.equals(friends[i][1])) {
+                MyFriends.add(friends[i][0])
+            }
+        }
+        return MyFriends
+    }
+    return result
 }
