8b3ea208cca9dad4c009cb01e91bf6b03d69f720
dlgocks1 <cksgodl1@naver.com>
2022-10-31 11:33:45 +0900

    feat: (Problem6) 기능 목록 정의

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,3 +3,43 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
+
+/**
+ * @를 기준으로 도메인과 이메일로 나누어 반환하는 함수
+ * first요소에 이메일, second요소에 도메인을 반환한다.
+ */
+fun divideEmail(email: String): Pair<String, String> {
+
+}
+
+/**
+ * 문자열에서 파라미터로 온 문자열을 포함하는지 체크한다.
+ * 2글자 이상의 파라미터 문자열 및 기본 문자열만 체크한다.
+ */
+fun String.contains(findStr: String): Boolean {
+
+}
+
+/**
+ * 이메일의 도메인이 "email.com"인지 체크하는 함수
+ */
+fun String.isEmailDomain(): Boolean {
+
+}
+
+/**
+ * 이메일 리스트를 이메일에 해당하는 부분의 문자열 기준으로
+ * 오름차순으로 정렬하여 반환한다.
+ */
+fun sortEmailByAesc(emailList: List<String>): List<String> {
+
+}
+
+/**
+ * 리스트에 중복된 이메일이 있는지 체크 및 제거
+ */
+fun emailDuplicateCheck(emailList: List<String>): List<String> {
+
+}
+
