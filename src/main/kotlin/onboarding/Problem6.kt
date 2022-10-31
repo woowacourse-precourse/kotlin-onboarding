2d7ca325cdc4f8236ea690962c39bbb47123f5c7
agfalcon <agfalcon12@gmail.com>
2022-11-01 03:15:34 +0900

    2022.11.01 mission all clear

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,8 +1,35 @@
 package onboarding
 
-fun solution6(forms: List<List<String>>): List<String> {
-    var result = mutableListOf<String>()
-    var list = MutableList<Boolean>(forms.count(), {it->false})
+import java.util.stream.Collectors
 
-    result.sort()
+fun solution6(forms: List<List<String>>): List<String> {
+    var answer = mutableListOf<String>()
+    val emails: HashSet<String> = HashSet()
+    val hashMap: MutableMap<String, String> = HashMap()
+
+    var name: String
+    var email: String
+    var keyword: String
+
+    for (i in 0 until forms.size) {
+        name = forms[i][1]
+        for (j in 0 until name.length - 1) {
+            keyword = name.substring(j, j + 2)
+            if (hashMap.containsKey(keyword)) {
+                email = hashMap[keyword]!!
+                if (email != forms[i][0]) {
+                    emails.add(email)
+                    emails.add(forms[i][0])
+                }
+            }
+            hashMap[keyword] = forms[i][0]
+        }
+    }
+    for(i in emails){
+        answer.add(i)
+    }
+
+    val list = answer.toList()
+    return list
+
 }
