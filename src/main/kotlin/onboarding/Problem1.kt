41f2222dd11364e69ed01c7f399e88b3d16db6e2
hyungmin <utopia96@hanmail.net>
2022-10-31 22:16:33 +0900

    feat: 첫번째 문제 solution 작성 및 검증

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,37 @@
 package onboarding
 
+import java.lang.Integer.max
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    val resOfPobi = getMax(pobi)
+    val resOfCrong = getMax(crong)
+    return if (resOfPobi > resOfCrong) {
+        1
+    } else if (resOfPobi == resOfPobi) {
+        0
+    } else {
+        2
+    }
+}
+
+fun getMax(values: List<Int>): Int {
+    var max = -1
+    for (value in values) {
+        max = max(max, calculate(value))
+    }
+    return max
+}
+
+fun calculate(number: Int): Int {
+    return if (number > 100) {
+        val sum = number / 100 + number / 10 + number % 10
+        val multi = number / 100 * number / 10 - number % 10
+        max(sum, multi)
+    } else if (number > 10) {
+        val sum = number / 10 + number % 10
+        val multi = number / 10 - number % 10
+        max(sum, multi)
+    } else {
+        number
+    }
 }
