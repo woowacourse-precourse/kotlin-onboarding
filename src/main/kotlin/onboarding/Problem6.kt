59ddd91a03b511ea725a667ccff81fa7bc8c308c
StrayCat <lumineko@naver.com>
2022-10-31 23:49:37 +0900

    Solve Problem 6

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,41 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+
+    var m : MutableMap<String, String> = mutableMapOf()
+    var emails : MutableSet<String> = mutableSetOf()
+
+    val len = forms.size
+
+    for(i in 0 until len){
+        val nameLength = forms[i][1].length
+        for(j in 0 until nameLength-1){
+            val subStr = forms[i][1].substring(j .. j+1)
+            if(m.containsKey(subStr)){
+                emails.add(m[subStr]!!)
+                emails.add(forms[i][0])
+            }else {
+                m[subStr] = forms[i][0]
+            }
+        }
+
+
+    }
+
+
+    return emails.toMutableList().sortedBy { it }
+}
+
+
+fun main() {
+    val a: MutableList<List<String>> = mutableListOf()
+    a.add(listOf("jm@email.com", "제이엠"))
+    a.add(listOf("jason@email.com", "제이슨"))
+    a.add(listOf("woniee@email.com", "워니"))
+    a.add(listOf("mj@email.com", "엠제이"))
+    a.add(listOf("nowm@email.com", "이제엠"))
+
+
+    println(solution6(a))
+
 }
