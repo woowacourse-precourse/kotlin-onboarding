d50851dcdcefcbb09ce05cf4191ba3c9b050ed9f
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-01 02:07:54 +0900

    feat(problem3): Add set clab count list at single digit

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,16 @@
 package onboarding
 
+import java.util.*
+
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    val clabCountList: MutableList<Int> = ArrayList(Collections.nCopies(10003, -1))
+    setSingleDigitCountList(clabCountList)
+    return 0
+}
+
+fun setSingleDigitCountList(clabCountList: MutableList<Int>) {
+    for (number in 1 until 10) {
+        if (number % 3 == 0) clabCountList[number] = 1
+        else clabCountList[number] = 0
+    }
 }
