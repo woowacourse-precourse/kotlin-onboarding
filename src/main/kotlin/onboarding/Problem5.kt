5c2f9ec16bf5890b91266277cd01d5b212dc0b27
YuGyeong98 <kuk3097@naver.com>
2022-10-31 23:21:45 +0900

    style(Problem5): 코드 포맷팅
    인덴트 수정, 변수 이름 변경, 예외 처리 주석 추가 및 private으로 변경

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,28 +1,29 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-  val withdrawalList = List(9) { 0 }.toMutableList()
-  val currencyList = mutableListOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
-  var idx = 0
-  var _money = money
+    val withdrawalList = List(9) { 0 }.toMutableList()
+    val currencyList = mutableListOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    var idx = 0
+    var moneyInAccount = money
 
-  checkMoney(money)
+    checkMoney(money)
 
-  currencyList.forEach { currency ->
-    while (_money / currency > 0) {
-      withdrawalList[idx] += 1
-      _money -= currency
+    currencyList.forEach { currency ->
+        while (moneyInAccount / currency > 0) {
+            withdrawalList[idx] += 1
+            moneyInAccount -= currency
+        }
+        idx += 1
     }
-    idx += 1
-  }
 
-  return withdrawalList
+    return withdrawalList
 }
 
-fun checkMoney(money: Int) {
-  if (money !in 1..1000000) {
-    throw IllegalArgumentException(
-      "money는 1 이상 1,000,000 이하인 자연수여야 합니다."
-    )
-  }
+/** 예외 처리 */
+private fun checkMoney(money: Int) {
+    if (money !in 1..1000000) {
+        throw IllegalArgumentException(
+            "money는 1 이상 1,000,000 이하인 자연수여야 합니다."
+        )
+    }
 }
