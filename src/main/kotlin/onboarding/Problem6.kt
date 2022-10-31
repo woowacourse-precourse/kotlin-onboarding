97d80581c36e73a025655393fa2dcadd6ed9884b
dlwgnud <wngud980411@naver.com>
2022-11-01 01:09:11 +0900

    feat: 6번 문제

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val answer = mutableSetOf<String>()
+
+    forms.forEach { form ->
+        forms.forEach{
+            if(form[1] != it[1]){
+                var a = ""
+                for(i in 0..form[1].length-2){
+                    a = form[1][i].toString().plus(form[1][i+1])
+                    if(it[1].contains(a))
+                        answer.add(it[0])
+                }
+            }
+        }
+    }
+
+    return answer.toList().sorted()
 }
