8859dfac8c92d052fb33b769ac2cf0f37a8ea3a4
Gloria <ippnsj@gmail.com>
2022-10-31 19:20:14 +0900

    feat(Problem5): 모든 화폐 단위로 출금하는 기능

--- src/main/kotlin/problem5/Bank.kt
+++ src/main/kotlin/problem5/Bank.kt
@@ -1,9 +1,19 @@
 package problem5
 
 class Bank(private var account:Int) {
+    private var count = 0
     private val moneyManager = mutableListOf<Int>()
+    private val unit = listOf(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1)
 
-    private fun withdrawByAllUnit() {}
+    private fun noticeMoneyManager() {}
+
+    private fun withdrawByAllUnit() {
+        for(monetaryUnit in unit) {
+            count = account / monetaryUnit
+            account %= monetaryUnit
+            noticeMoneyManager()
+        }
+    }
 
     fun withdraw() : List<Int> {
         withdrawByAllUnit()
