a6b367635df10eecff0159e5d5e054bee276f798
DormunDoorlock <skrudgh2001@naver.com>
2022-10-31 16:56:00 +0900

    feat: Problem6.kt 2번 기능 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,13 +1,12 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    val result = mutableListOf<String>()
+    var result = mutableListOf<String>()
     val onlyOne = mutableListOf<Pair<Char,Char>>()
-    val twoMore = mutableListOf<Pair<Char,Char>>()
+    var twoMore = mutableListOf<Pair<Char,Char>>()
     // 1번. 각 닉네임 두 글자씩 pair로 나누고, 조건에 따라 처음 나온 pair, 두 번 이상 나온 pair로 두 개의 리스트에 나눠서 넣기
     for(i in forms.indices){
         val nickName = forms[i][1]
-
         val pairs = nickName.zipWithNext()
         for(pair in pairs){
             if(!onlyOne.contains(pair))
@@ -17,8 +16,20 @@ fun solution6(forms: List<List<String>>): List<String> {
         }
     }
     // 중복제거
-    twoMore.distinct()
+    twoMore= twoMore.distinct() as MutableList<Pair<Char, Char>>
 
-
-    return result
+    // 2번. 각 닉네임에서 두번 이상 나온 pair를 가지고 있는 list에 있는 pair가 있는 경우 result에 이메일 넣는 기능
+    for(i in forms.indices){
+        val email = forms[i][0]
+        val nickName = forms[i][1]
+        val pairs = nickName.zipWithNext()
+        for (pair in pairs){
+            if(twoMore.contains(pair)){
+                result.add(email)
+                break
+            }
+        }
+    }
+    result = result.distinct() as MutableList<String>
+    return result.sorted()
 }
