7f4efc3383d5a4b9a8e23b20d8050c80dccf6d3a
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-11-01 00:28:53 +0900

    문제 1: fix : 페이지 별 덧셈 곱셈을 한 번에 하는 함수 생성

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,93 +1,26 @@
 package onboarding
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.util.*
-import kotlin.io.path.createTempDirectory
-
-var win = 1
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 
+  var win = -1
+  var pobi_max = 0
+  var crong_max =0
 
+  //예외처리
   if((pobi[pobi.size-1] - pobi[pobi.size-2] != 1)  or (crong[crong.size-1] - crong[crong.size-2] != 1) ){
       return win
   }
-
   if((pobi.size > 2) or (crong.size > 2)){
       return win
   }
-
   if(pobi.contains(1) or pobi.contains(400)){
       return win
   }
-
   if(crong.contains(1) or crong.contains(400)){
         return win
   }
 
-  var pobi_max_sum = 0
-  var pobi_max_mul = 1
-  var pobi_sum = 0
-  var pobi_mul = 1
-  var pobi_max = 0
-
-    for(i in 0 until 2){
-        pobi_sum = (pobi[i] / 100) + (pobi[i] % 100 / 10) + (pobi[i] % 100 % 10)
-        if(pobi_sum > pobi_max_sum){
-            pobi_max_sum = pobi_sum
-        }
-
-        if( pobi[i] >= 100){
-            pobi_mul = (pobi[i] / 100) * (pobi[i] % 100 / 10) * (pobi[i] % 100 % 10)
-        }
-        if( pobi[i] < 100){
-            pobi_mul = (pobi[i] / 10) * (pobi[i] % 10)
-        }
-
-        if(pobi_mul > pobi_max_mul){
-            pobi_max_mul = pobi_mul
-        }
-    }
-
-   if(pobi_max_mul > pobi_max_sum){
-      pobi_max = pobi_max_mul
-   }
-   if (pobi_max_sum >= pobi_max_mul){
-      pobi_max = pobi_max_sum
-   }
-
-
-    var crong_max_sum = 0
-    var crong_max_mul = 1
-    var crong_sum = 0
-    var crong_mul = 1
-    var crong_max = 0
-
-    for(i in 0 until 2){
-        crong_sum = (crong[i] / 100) + (crong[i] % 100 / 10) + (crong[i] % 100 % 10)
-        if(crong_sum > crong_max_sum){
-            crong_max_sum = crong_sum
-        }
-
-        if( crong[i] >= 100){
-            crong_mul = (crong[i] / 100) * (crong[i] % 100 / 10) * (crong[i] % 100 % 10)
-        }
-        if( crong[i] < 100){
-            crong_mul = (crong[i] / 10) * (crong[i] % 10)
-        }
-
-        if(crong_mul > crong_max_mul){
-            crong_max_mul = crong_mul
-        }
-    }
-
-    if(crong_max_mul > crong_max_sum){
-        crong_max = crong_max_mul
-    }
-    if (crong_max_sum >= crong_max_mul){
-        crong_max = crong_max_sum
-    }
 
 
     if(pobi_max > crong_max){
@@ -104,6 +37,29 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 
     return win
 }
+//각 페이지 자릿수 합과 곱 계산
+fun cal_page(num : Int) : List<Int>{
+
+    var hun = num / 100
+    var ten = (num % 100) / 10
+    var one = (num % 100) % 10
+
+    var sum = hun + ten + one
+    var mul = 1
+
+    if(hun >= 1){
+        mul = hun * ten * one
+    }
+    if(hun == 0){
+        mul = (num / 10) * (num % 10)
+    }
+
+    return listOf(sum, mul)
+}
+
+
+
+
 
 
 
