da1835f5d260a9873722794163831c348cc29bcf
YuGyeong98 <kuk3097@naver.com>
2022-10-31 22:53:12 +0900

    style(Problem1): 코드 포맷팅
    인덴트 수정, 변수 함수 이름 변경, curly brace 추가, 단일표현식 return 제거

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,59 +1,64 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-  if (checkPage(pobi, crong)) return -1
+    if (checkPage(pobi, crong)) {
+        return -1
+    }
 
-  val pobiResult = getResult(pobi)
-  val crongResult = getResult(crong)
+    val pobiResult = getResult(pobi)
+    val crongResult = getResult(crong)
 
-  return when {
-    pobiResult > crongResult -> 1
-    pobiResult < crongResult -> 2
-    else -> 0
-  }
+    return when {
+        pobiResult > crongResult -> 1
+        pobiResult < crongResult -> 2
+        else -> 0
+    }
 }
 
 fun getResult(person: List<Int>): Int {
-  val leftPage = person[0]
-  val rightPage = person[1]
+    val leftPage = person[0]
+    val rightPage = person[1]
 
-  val leftPageSum = 0
-  val leftPageMultiply = 1
+    val leftPageSum = 0
+    val leftPageMultiply = 1
 
-  val rightPageSum = 0
-  val rightPageMultiply = 1
+    val rightPageSum = 0
+    val rightPageMultiply = 1
 
-  val leftPageMax = calculatePage(leftPage, leftPageSum, leftPageMultiply)
-  val rightPageMax = calculatePage(rightPage, rightPageSum, rightPageMultiply)
+    val leftPageMax = calculatedPage(leftPage, leftPageSum, leftPageMultiply)
+    val rightPageMax = calculatedPage(rightPage, rightPageSum, rightPageMultiply)
 
-  return getMaxNum(leftPageMax, rightPageMax)
+    return getMaxNum(leftPageMax, rightPageMax)
 }
 
-fun calculatePage(_page: Int, _sum: Int, _multiply: Int): Int {
-  var page = _page
-  var sum = _sum
-  var multiply = _multiply
+fun calculatedPage(pg: Int, s: Int, mul: Int): Int {
+    var page = pg
+    var sum = s
+    var multiply = mul
 
-  while (page > 0) {
-    sum += page % 10
-    multiply *= page % 10
-    page /= 10
-  }
-  return getMaxNum(sum, multiply)
+    while (page > 0) {
+        sum += page % 10
+        multiply *= page % 10
+        page /= 10
+    }
+    return getMaxNum(sum, multiply)
 }
 
 fun getMaxNum(num1: Int, num2: Int): Int {
-  return if (num1 > num2) num1 else num2
+    return if (num1 > num2) {
+        num1
+    } else {
+        num2
+    }
 }
 
-/**
- * 예외 처리
- */
-fun checkPage(pobi: List<Int>, crong: List<Int>): Boolean {
-  return !(exceptionPage(pobi[0], pobi[1]) && exceptionPage(crong[0], crong[1]))
-}
+/** 예외 처리 */
+private fun checkPage(pobi: List<Int>, crong: List<Int>): Boolean =
+    !(checkExceptionPage(pobi[0], pobi[1]) && checkExceptionPage(crong[0], crong[1]))
 
-fun exceptionPage(leftPage: Int, rightPage: Int): Boolean {
-  return (leftPage in 1..400 && rightPage in 1..400) && (leftPage + 1 == rightPage) && (leftPage % 2 == 1) && (rightPage % 2 == 0)
+private fun checkExceptionPage(leftPage: Int, rightPage: Int): Boolean {
+    return (leftPage in 1..400) && (rightPage in 1..400) &&
+            (leftPage + 1 == rightPage) &&
+            (leftPage % 2 == 1) && (rightPage % 2 == 0)
 }
 
