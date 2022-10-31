33d5845add106e30e96ee787bb825c6f6f9d5a81
권용민 <briandr@naver.com>
2022-11-01 04:19:38 +0900

    [Problem1] 함수의 결과인 -1,0,1,2를 알아보기 쉽게 상수화

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,10 @@
 package onboarding
 
+private const val ERROR = -1
+private const val POBI_WIN = 1
+private const val CRONG_WIN = 2
+private const val DRAW = 0
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
