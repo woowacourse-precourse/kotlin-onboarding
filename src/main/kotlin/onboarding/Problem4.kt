356ed79a45f0911f057edb23579250aed5c056b7
dlwgnud <wngud980411@naver.com>
2022-11-01 01:08:17 +0900

    feat: 4번 문제

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,32 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    val upperCase = mutableListOf<Char>()
+    for (char in 'A'..'Z') {
+        upperCase.add(char)
+    }
+    val upperCaseReverse = mutableListOf<Char>()
+    for (char in 'Z' downTo 'A') {
+        upperCaseReverse.add(char)
+    }
+    val lowerCase = mutableListOf<Char>()
+    for (char in 'a'..'z') {
+        lowerCase.add(char)
+    }
+    val lowerCaseReverse = mutableListOf<Char>()
+    for (char in 'z' downTo 'a') {
+        lowerCaseReverse.add(char)
+    }
+
+    var answer = ""
+    word.forEach { char ->
+        answer += if(char.isUpperCase()){
+            upperCaseReverse[upperCase.indexOf(char)]
+        } else if(char.isLowerCase()){
+            lowerCaseReverse[lowerCase.indexOf(char)]
+        } else
+            char
+    }
+
+    return answer
 }
