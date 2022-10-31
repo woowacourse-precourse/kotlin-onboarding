7ad7b50f199d07d9e898ffdbb2688cbe1174273c
yelm <21yrshin@naver.com>
2022-10-31 13:49:49 +0900

    problem 4 test finished

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -4,18 +4,27 @@ fun solution4(word: String): String {
     val alphabet0 = CharRange('A','Z').toMutableList()
     val alphabet1 = CharRange('a','z').toMutableList()
 
-    val res = word.forEach ({ ch ->
-        val str = ch.toInt()
-        if (str >=32 || str <= 57){
-            str = str - 32
-            str = 25 - str
-            str += 32
+    val rev0 = alphabet0.reversed()
+    val rev1 = alphabet1.reversed()
+
+    val map0: Map<Char, Char> = alphabet0.zip(rev0).toMap()
+    val map1: Map<Char, Char> = alphabet1.zip(rev1).toMap()
+
+    var retval = ""
+
+    val res = word.forEach { ch ->
+        var str: Int = ch.code;
+        if (str in 65..90) {
+            var charval = str.toChar()
+            retval += map0.get(charval)!!
+        } else if (str in 97..122) {
             val charval = str.toChar()
-            print("$charval")
-        } else if (){
-
+            retval += map1.get(charval)!!
+        } else{
+            retval += str.toChar()
         }
-    })
+    }
 
-    return res
+    return retval;
+
 }
