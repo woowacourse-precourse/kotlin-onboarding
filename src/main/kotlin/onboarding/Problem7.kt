4d73ee19b9aa5fead4c59af757b92004ab3d9c38
inseonyun <yuninseon@naver.com>
2022-10-31 19:49:13 +0900

    [Problem7] user 친구 리스트 생성 함수 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,5 +1,20 @@
 package onboarding
 
+fun setUserFriends(infoFriends: List<List<String>>, user: String): List<String> {
+    var result = mutableListOf<String>()
+
+    for(i in infoFriends.indices) {
+        if (infoFriends[i][0] == user) {
+            result.add(infoFriends[i][1])
+            continue
+        }
+        if(infoFriends[i][1] == user)
+            result.add(infoFriends[i][0])
+    }
+
+    return result.toList()
+}
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
