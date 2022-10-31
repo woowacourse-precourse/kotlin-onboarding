9cb7c7d5d159726b3d8f42284ce246456b4ee2d1
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-10-31 22:50:21 +0900

    feat: (문제3) 3, 6, 9 개수 세기 기능 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,24 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    return count369(number)
+}
+
+fun count369(number: Int):Int{
+    var sum = 0
+    for(i in 1..number){
+        val numberString = "$i"
+        sum += sum369(numberString)
+    }
+    return sum
+}
+
+fun sum369(numberString: String): Int{
+    var sum = 0
+    for(j in numberString.indices){
+        if(numberString[j] == '3' || numberString[j] == '6' || numberString[j] == '9'){
+            sum += 1
+        }
+    }
+    return sum
 }
