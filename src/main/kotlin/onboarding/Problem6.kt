29cc0d728320d89148818ec622a74450b88682d3
phs <gustjd0808@pusan.ac.kr>
2022-11-01 03:25:48 +0900

    Problem6 기능 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,27 @@
 package onboarding
 
+private fun addTwoLengthWords(
+    form: List<String>,
+    map: HashMap<String, ArrayList<Int>>,
+    index: Int
+) {
+    for (j in 1 until form[1].length) {
+        val word = "${form[1][j - 1]}${form[1][j]}"
+        if (map[word] == null) map[word] = arrayListOf(index)
+        else map[word]!!.add(index)
+    }
+}
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val twoLengthWordMap = hashMapOf<String, ArrayList<Int>>()
+
+    for ((index, form) in forms.withIndex()) {
+        addTwoLengthWords(form, twoLengthWordMap, index)
+    }
+
+    return twoLengthWordMap.filter { mapEntry -> mapEntry.value.size >= 2 }
+        .flatMap { mapEntry -> mapEntry.value }
+        .distinct()
+        .map { duplicatedIndex -> forms[duplicatedIndex][0] }
+        .toList().sorted()
 }
