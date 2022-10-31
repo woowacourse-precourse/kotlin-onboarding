1a6578cbf5df7348cd9d5c4446ed06e246e8b998
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-31 22:24:57 +0900

    refactor(Problem6): if문 리팩토링

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -6,10 +6,10 @@ fun solution6(forms: List<List<String>>): List<String> {
         val targetName = forms[i][1]
         for (j in 0..targetName.length - 2) {
             val targetString = targetName.substring(j, j + 2)
-            for (k in forms.indices) {
+            for (k in i+1 until forms.size) {
                 if (forms[k][1].contains(targetString)) {
-                    if(forms[k][0] != forms[i][0])
-                        result.add(forms[k][0])
+                    result.add(forms[i][0])
+                    result.add(forms[k][0])
                 }
             }
         }
