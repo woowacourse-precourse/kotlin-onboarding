715ad7060da7837822df7f49708f107d1908a50e
no1msh <no1msh1217@gmail.com>
2022-11-01 02:23:09 +0900

    [Problem7][feat] 2번 기능 구현
    2. 친구를 매개변수로 받아 추천 점수를 계산하는 메소드 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,5 +1,7 @@
 package onboarding
 
+import java.util.Collections
+
 // 기능 목록
 // 1. 맵 자료구조를 활용하여 친구목록을 정리하는 메소드 구현
 // 2. 친구를 매개변수로 받아 추천 점수를 계산하는 메소드 구현
@@ -42,4 +44,30 @@ fun makeFriendsMap(friends: List<List<String>>) {
             friendsMap[friend2] = friend1
         }
     }
+}
+
+// 기능 목록 2번
+fun calculateRecommendScore(user: String, checkForName: String, visitors: List<String>): Int {
+    var score = 0
+    // 친구는 아닌데 방문만 한경우
+    if (!friendsMap.containsKey(checkForName)) {
+        score = Collections.frequency(visitors, checkForName)
+        return score
+    }
+
+    // 이미 친구인 경우
+    if (friendsMap[user]!!.contains(checkForName)) {
+        return 0
+    }
+
+    // 친구는 아니고 추천 점수를 계산해야하는 경우 , 내친구 목록과 타겟 친구 목록을 서로 비교 같은게 있으면
+    score += Collections.frequency(visitors, checkForName)
+    val userFriends = friendsMap[user]!!.split(".")
+    val checkForNameFriends = friendsMap[checkForName]!!.split(".")
+
+    val intersectFriends = userFriends.toSet().intersect(checkForNameFriends.toSet())
+    score += intersectFriends.size * 10
+
+    return score
+
 }
