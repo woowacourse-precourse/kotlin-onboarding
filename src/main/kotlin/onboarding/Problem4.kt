08e450a2f26a7c39a86b129aefd6acd789417e0f
YuGyeong98 <kuk3097@naver.com>
2022-10-31 23:11:20 +0900

    style(Problem4): 코드 포맷팅
    인덴트 수정, curly brace 추가, 예외 처리 주석 추가 및 private으로 변경

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,32 +1,39 @@
 package onboarding
 
 fun solution4(word: String): String {
-  val upperAlphabets = ('A'..'Z').toMutableList()
-  val lowerAlphabets = ('a'..'z').toMutableList()
-  val reversedUpperAlphabets = upperAlphabets.reversed()
-  val reversedLowerAlphabets = lowerAlphabets.reversed()
-  var reversedWord = ""
+    val upperAlphabets = ('A'..'Z').toMutableList()
+    val lowerAlphabets = ('a'..'z').toMutableList()
+    val reversedUpperAlphabets = upperAlphabets.reversed()
+    val reversedLowerAlphabets = lowerAlphabets.reversed()
+    var reversedWord = ""
 
-  checkWordLength(word)
+    checkWordLength(word)
 
-  word.indices.forEach { i ->
-    if (word[i].isUpperCase()) {
-      upperAlphabets.indices.forEach { j ->
-        if (word[i] == upperAlphabets[j]) reversedWord += reversedUpperAlphabets[j]
-      }
-    } else if (word[i].isLowerCase()) {
-      lowerAlphabets.indices.forEach { j ->
-        if (word[i] == lowerAlphabets[j]) reversedWord += reversedLowerAlphabets[j]
-      }
-    } else reversedWord += word[i]
-  }
-  return reversedWord
+    word.indices.forEach { i ->
+        if (word[i].isUpperCase()) {
+            upperAlphabets.indices.forEach { j ->
+                if (word[i] == upperAlphabets[j]) {
+                    reversedWord += reversedUpperAlphabets[j]
+                }
+            }
+        } else if (word[i].isLowerCase()) {
+            lowerAlphabets.indices.forEach { j ->
+                if (word[i] == lowerAlphabets[j]) {
+                    reversedWord += reversedLowerAlphabets[j]
+                }
+            }
+        } else {
+            reversedWord += word[i]
+        }
+    }
+    return reversedWord
 }
 
-fun checkWordLength(word: String) {
-  if (word.length !in 1..1000) {
-    throw IllegalArgumentException(
-      "word는 길이가 1 이상 1,000 이하인 문자열이어야 합니다."
-    )
-  }
+/** 예외 처리 */
+private fun checkWordLength(word: String) {
+    if (word.length !in 1..1000) {
+        throw IllegalArgumentException(
+            "word는 길이가 1 이상 1,000 이하인 문자열이어야 합니다."
+        )
+    }
 }
