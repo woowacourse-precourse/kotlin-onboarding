8f471538e0a3b5ba825e975d9252d7232f4a1faf
dlgocks1 <cksgodl1@naver.com>
2022-10-31 10:57:14 +0900

    fix: (Problem5) Solution코드 오타 수정 및 for문 레인지변경

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -2,10 +2,10 @@ package onboarding
 
 fun solution5(money: Int): List<Int> {
     // 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전
-    val unitList = intArrayOf(50000, 10000, 5000, 100, 500, 100, 50, 10, 1)
+    val unitList = intArrayOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
     val answer = mutableListOf(0, 0, 0, 0, 0, 0, 0, 0, 0)
     var leftMoney = money
-    for (i in 0..answer.size) {
+    for (i in 0 until answer.size) {
         answer[i] += changesCount(leftMoney, unitList[i])
         leftMoney = changesMoney(leftMoney, unitList[i])
     }
