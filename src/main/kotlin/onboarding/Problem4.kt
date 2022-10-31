9fc5c816a838a847266f4157a8028ec9eb9accff
SeungHyeon Shim <tladhehd@naver.com>
2022-10-31 16:55:43 +0900

    Problem4_commit_2

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,36 +1,19 @@
 package onboarding
 
-/*
-어느 연못에 엄마 말씀을 좀처럼 듣지 않는 청개구리가 살고 있었다.
-청개구리는 엄마가 하는 말은 무엇이든 반대로 말하였다.
-엄마 말씀 word가 매개변수로 주어질 때,
-아래 청개구리 사전을 참고해 반대로 변환하여 return 하도록 solution 함수를 완성하라.
-*/
+fun alphaChange(alpha: Char): Char { // 변환된 문자를 return
+    val lower_alphabet = arrayListOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+    val upper_alphabets  = arrayListOf<Char>() // 초기화
+    for (alphabet in lower_alphabet) upper_alphabets.add(alphabet.uppercaseChar()) // 대문자로 변경뒤 추가
+    val re_lower = lower_alphabet.reversed() // 소문자 뒤집기
+    val re_upper = upper_alphabets.reversed() // 대문자 뒤집기
 
-// word 1 .. 1000
-// A 65 Z 90 a 97 z 122
-
+    if (alpha == ' ') return ' '
+    if (alpha.isUpperCase()) return re_upper[upper_alphabets.indexOf(alpha)] // alpha에 해당하는 알파벳 위치로 변환한다.
+    if (alpha.isLowerCase()) return re_lower[lower_alphabet.indexOf(alpha)]
+    return alpha
+}
 fun solution4(word: String): String {
-    var result = ""
-    var ascii = 0
-    var letter = ' '
-    for (i in 0 until word.length) {
-        ascii = word[i].toInt()
-        // 대문자일때
-        if (ascii in 65..90) {
-            letter = (90 - (ascii - 65)).toChar()
-            result+=letter
-        }
-        // 소문자일때
-        else if(ascii in 97 .. 122){
-            letter = (122 - (ascii - 97)).toChar()
-            result+=letter
-        }
-        // 스페이스일때 스페이스만 추가
-        if (ascii == 32) {
-            result+=' '
-        }
-    }
-    //result값에도 쌍따옴표 추가
-    return "\"${result}\""
+    var result : String = ""
+    for (char in word) result += alphaChange(char)
+    return result
 }
