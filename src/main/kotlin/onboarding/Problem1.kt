036b99076443013eb4bc0d8c38061a23bd154ec2
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-10-31 19:20:37 +0900

    [Problem1] feat: 페이지를 받아 각 자릿수 합을 반환하는 함수 작성

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,8 +1,19 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+
     if(!checkInputException(pobi) || !checkInputException(crong))
         return -1
+
+    val pobisNumbers: ArrayList<Int> = arrayListOf()
+    val crongsNumbers: ArrayList<Int> = arrayListOf()
+
+    pobisNumbers.add(sumPageNum(pobi[0]))
+    pobisNumbers.add(sumPageNum(pobi[1]))
+
+    crongsNumbers.add(sumPageNum(crong[0]))
+    crongsNumbers.add(sumPageNum(crong[1]))
+
 
     return 0
 }
@@ -18,8 +29,15 @@ fun checkInputException(pageList: List<Int>): Boolean{
 }
 
 fun sumPageNum(pageNum: Int): Int{
-    // TODO: 2022-10-31
-    return 0
+    var sum = 0
+    var currNum = pageNum
+
+    while(currNum > 0){
+        sum += currNum % 10
+        currNum /= 10
+    }
+
+    return sum
 }
 
 fun multiplyPageNum(pageNum: Int): Int{
