f7e150392c7b90b206072ac25f3987ab486669a9
Hyemin Song <hyemdooly@gmail.com>
2022-10-31 15:15:33 +0900

    Problem5,6,7 solve

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,16 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    // 돈의 액수 money가 매개변수로 주어질 때, 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전 각 몇 개로 변환되는지 금액이 큰 순서대로 리스트/배열에 담아 return 하도록 solution 함수를 완성하라.
+
+    var result = mutableListOf<Int>()
+    val unit = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    var current = money
+
+    for (idx: Int in unit.indices) {
+        result.add(current / unit[idx])
+        current = money % unit[idx]
+    }
+
+    return result
 }
--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,68 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val friendsRelation = mutableMapOf<String, MutableSet<String>>()
+    val points = mutableMapOf<String, Int>()
+    val result = mutableListOf<String>()
+
+    for (idx: Int in friends.indices) { // (이름, 친구명단) map 생성
+        if (!friendsRelation.containsKey(friends[idx][0])) {
+            friendsRelation[friends[idx][0]] = mutableSetOf()
+        }
+        if (!friendsRelation.containsKey(friends[idx][1])) {
+            friendsRelation[friends[idx][1]] = mutableSetOf()
+        }
+        friendsRelation[friends[idx][0]]!!.add(friends[idx][1])
+        friendsRelation[friends[idx][1]]!!.add(friends[idx][0])
+
+    }
+    // 함께 아는 친구의 수의 점수 계산
+    if (friendsRelation.containsKey(user)) {
+        val userFriends = friendsRelation[user]!!.toList()
+        for (idx: Int in userFriends.indices) {
+            val fFriends = friendsRelation[userFriends[idx]]!!.toList()
+            for (idx2: Int in fFriends.indices) {
+                if (fFriends[idx2] == user) {
+                    continue
+                }
+                if (!points.containsKey(fFriends[idx2])) {
+                    points[fFriends[idx2]] = 0
+                }
+                points[fFriends[idx2]] = points[fFriends[idx2]]!! + 10
+
+            }
+        }
+    }
+    // 방문 점수 계산
+    for (idx: Int in visitors.indices) {
+        if (friendsRelation.containsKey(user) && friendsRelation[user]!!.contains(visitors[idx])) { // 자기자신 제외
+            continue
+        }
+        if(!points.containsKey(visitors[idx])) {
+            points[visitors[idx]] = 0
+        }
+        points[visitors[idx]] = points[visitors[idx]]!! + 1
+
+    }
+
+    val candidates = points.toList().sortedByDescending { it.second }
+    for (i: Int in candidates.indices) {
+        result.add(candidates[i].first)
+        if (result.size > 5) {
+            break
+        }
+    }
+    return result
+
 }
+
+//fun main() {
+//    val friends = listOf(listOf("donut", "andole"),
+//            listOf("donut", "jun"),
+//            listOf("donut", "mrko"),
+//            listOf("shakevan", "andole"),
+//            listOf("shakevan", "jun"),
+//            listOf("shakevan", "mrko"))
+//    val visitors = listOf("bedi", "bedi", "donut", "bedi", "shakevan")
+//    solution7("mrko", friends, visitors)
+//}
