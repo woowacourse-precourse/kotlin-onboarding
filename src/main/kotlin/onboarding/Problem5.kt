4f83a44d74c65f2818897ec7eae29751cd3210c6
mendel <mendel.urssu@gmail.com>
2022-10-31 15:01:41 +0900

    Problem5.kt 문제5 기능 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,18 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    //오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원
+    val moneyDigit= listOf<Int>(50000,10000,5000,1000,500,100,50,10,1)
+    val result= mutableListOf<Int>(0,0,0,0,0,0,0,0,0)
+    var curMoney=money
+
+    var i=0
+    while(curMoney>0){
+        val mNum=curMoney/moneyDigit[i]
+        result[i]=mNum
+        curMoney-=(mNum*moneyDigit[i])
+        i++
+    }
+
+    return result
 }
