34b728364219c0ae4e0421dd12994829fa86ef26
jklom0326 <jklom0326@gmail.com>
2022-10-31 21:06:58 +0900

    [Problem6] checkNickName 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -22,3 +22,22 @@ fun isValidEmail(email: String): Boolean = "[A-Za-z]*(@email.com)$".toRegex()
 
 fun isValidNickName(nickName: String): Boolean = "[가-힣]{1,19}".toRegex()
     .matches(nickName)
+
+fun checkNickName(forms: List<List<String>>): List<String> {
+    val resultList = mutableSetOf<String>()
+    for (i in forms) {
+        val window = i[1].split("")
+            .windowed(2)
+            .map { it.joinToString("") }
+            .filter { it.length == 2 }
+        for (j in forms) {
+            if (i == j) continue
+            window.forEach {
+                if (j[1].contains(it)) {
+                    resultList += j[0]
+                }
+            }
+        }
+    }
+    return resultList.toList()
+}
