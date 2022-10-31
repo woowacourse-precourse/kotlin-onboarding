4fe55e7d380b1cd30f6ea9f9393d66bcd7374122
leehandsub <gustjq7027@naver.com>
2022-10-31 18:09:02 +0900

    feature/숫자 더하기 곱하기 최대값 구하기

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,21 @@
 package onboarding
 
+import java.lang.Integer.max
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
+}
+
+fun calMax(now: Int): Int {
+    var plus = 0
+    var mul = 1
+    var temp = now
+    while (temp > 0) {
+        val cur = temp % 10
+
+        mul *= cur
+        plus += cur
+        temp /= 10
+    }
+    return max(mul, plus)
 }
