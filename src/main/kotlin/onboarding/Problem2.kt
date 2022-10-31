2d314425ba72d6d30bae524e75845fe4403ebca4
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-10-31 22:38:21 +0900

    [Problem2] fix: 현재 암호문이 중복이 모두 제거된 암호문인지 검사하는 함수 삭제 feat: 중복된 문자열을 제거하는 함수 작성

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,27 +1,23 @@
 package onboarding
 
-import java.util.Stack
-
 fun solution2(cryptogram: String): String {
-    return ""
+    for(i in cryptogram.indices){
+        if(!isLowerCase(cryptogram[i]))
+            throw IllegalArgumentException("invalid input found!")
+    }
+
+    var currCrypto = cryptogram
+    while (currCrypto != removeDuplication(currCrypto))
+        currCrypto = removeDuplication(currCrypto)
+
+    return currCrypto
 }
 
-fun removeDuplication(cryptogram: String): String{
-    // TODO: 2022-10-31
-    return ""
+fun removeDuplication(cryptogram: String): String {
+    val regax = Regex("(\\w)\\1+")
+    return regax.replace(cryptogram, "")
 }
 
 fun isLowerCase(char: Char): Boolean{
     return char.isLowerCase()
-}
-
-fun isCompletedCrypto(cryptogram: String): Boolean{
-    val stack:Stack<Char> = Stack()
-    stack.push(cryptogram[0])
-    for(i in 1 until cryptogram.length){
-        if(stack.peek() == cryptogram[i])
-            return false
-        stack.push(cryptogram[i])
-    }
-    return true
 }
