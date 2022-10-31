5385ccccb226b2d3c6aef10693a650ca5d93d465
jjiiin <wjdwlin4051@gmail.com>
2022-10-31 22:06:37 +0900

    refactor: Problem1 1메소드 당 1기능 클린코드 원칙 적용

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -4,42 +4,55 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1)
+
+    if ((pobi[1] - pobi[0] != 1) || (crong[1] - crong[0] != 1)) {
         return -1
+    }
 
     val pobiMax:Int = calculateMax(pobi)
     val crongMax:Int = calculateMax(crong)
 
     if (pobiMax > crongMax){
         return 1
-    }else if(pobiMax < crongMax){
-        return 2
-    }else{
-        return 0
     }
+    if (pobiMax < crongMax){
+        return 2
+    }
+    return 0
 
 }
 
 fun calculateMax(pageList:List<Int>): Int{
-
     val resultList:ArrayList<Int> = arrayListOf()
+
     for(page in pageList){
-
-        var pagePlus = 0
-        var pageMultiply = 1
-        var page_temp = page
-
-        while (page_temp != 0){
-
-            pagePlus += page_temp  % 10
-            pageMultiply *= page_temp % 10
-
-            page_temp /= 10
-        }
-        resultList.add(pagePlus)
-        resultList.add(pageMultiply)
-
+        resultList.add(calculatePlus(page))
+        resultList.add(calculateMultiply(page))
     }
     return Collections.max(resultList)
 
+}
+
+fun calculatePlus(page:Int) : Int{
+    var calculatePage=page
+    var pageResult = 0
+    while (calculatePage != 0) {
+
+        pageResult += calculatePage % 10
+        calculatePage /= 10
+    }
+    return pageResult
+
+}
+
+fun calculateMultiply(page:Int) : Int {
+    var calculatePage=page
+    var pageResult = 1
+
+    while (calculatePage != 0) {
+        pageResult += calculatePage % 10
+        calculatePage /= 10
+    }
+
+    return pageResult
 }
