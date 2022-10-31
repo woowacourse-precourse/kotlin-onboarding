9f7a4b7f767c4fd8e2682a907235b5a6ca0f1881
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-10-31 17:29:18 +0900

    문제 6 : Feat : 추가 추출한 이름을 첫 글자 부터 2글자씩 잘라 map에 넣고 다음 자른 부분들과 비교 후 중복이면 출력할 set에 추가

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -14,6 +14,28 @@ fun solution6(forms: List<List<String>>): List<String> {
             break
         }
 
-        
+        for(j in 0 until name.length-1){
+
+            var key = name.substring(j , j+2)
+
+            if(email_and_nick.containsKey(key)){
+
+                var email = email_and_nick.get(key)
+
+                if(!email.equals(forms[i][0])){
+                    email?.let { email_set.add(it) }
+                    email_set.add(forms[i][0])
+                }
+            }
+
+
+
+            email_and_nick.put(key, forms[i][0])
+        }
+
+
+
+    }
+
 
 }
