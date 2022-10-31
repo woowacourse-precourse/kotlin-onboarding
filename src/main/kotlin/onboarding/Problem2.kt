998fd5a9048033b8bf5fb1ac4de4ef7027831b29
junsu1023 <12jangdokg55@gmail.com>
2022-10-31 14:34:52 +0900

    Problem2 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,34 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var password = cryptogram
+
+    while(true) {
+        if(password.isEmpty()) return password
+
+        var check = true
+        val pwSplit = password.toCharArray()
+
+        var nextPassword = pwSplit[0].toString()
+        var prevWord = pwSplit[0]
+        var alreadyDelete = false
+
+        for(i in 1 until pwSplit.size) {
+            if(pwSplit[i] == prevWord) {
+                if(!alreadyDelete) {
+                    alreadyDelete = true
+                    nextPassword = nextPassword.substring(0, nextPassword.length-1)
+                    check = false
+                }
+            }
+            else {
+                alreadyDelete = false
+                prevWord = pwSplit[i]
+                nextPassword += pwSplit[i]
+            }
+        }
+
+        if(check) return password
+        else password = nextPassword
+    }
 }
