cd42990387766ec3be294a70c5dd759bc04be7bd
dlgocks1 <cksgodl1@naver.com>
2022-10-31 10:46:03 +0900

    feat: (Problem5) 돈 반환 기능 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -7,11 +7,11 @@ fun solution5(money: Int): List<Int> {
 /**
  * 돈의 단위와 금액을 입력받아 변환값을 리턴한다.
  */
-
+fun changesCount(money: Int, unit: Int): Int return money / sunit
 
 /**
  * 돈의 단위와 금액을 입력받아 남은 금액을 리턴한다.
  */
-
+fun changesMoney(money: Int, unit: Int): Int return money % sunit
 
 
