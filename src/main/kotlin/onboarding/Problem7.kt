70e360d4679d09d22b3c9be62ce59a154a09dff8
StrayCat <lumineko@naver.com>
2022-11-01 01:25:02 +0900

    Solve Problem 7

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,73 @@
 package onboarding
 
+import java.util.stream.Collectors.toList
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+
+    val scoreMap = mutableMapOf<String, Int>()
+    val friendMap = mutableMapOf<String, MutableList<String>>()
+
+    friends.map{
+        if(friendMap[it[0]] == null){
+            friendMap[it[0]] = mutableListOf()
+        }
+        friendMap[it[0]]!!.add(it[1])
+
+        if(friendMap[it[1]] == null){
+            friendMap[it[1]] = mutableListOf()
+        }
+        friendMap[it[1]]!!.add(it[0])
+
+    }
+
+    friendMap[user]?.map{ f ->
+        friendMap[f]?.map{
+            if(!scoreMap.containsKey(it)){
+                scoreMap[it] = 0
+            }
+            scoreMap[it] = scoreMap[it]!! + 10
+        }
+
+    }
+
+
+    visitors.map{
+        if(!scoreMap.containsKey(it)){
+            scoreMap[it] = 0
+        }
+        scoreMap[it] = scoreMap[it]!! + 1
+    }
+
+    scoreMap[user] = -100
+    friendMap[user]?.map{
+        scoreMap[it] = -100
+    }
+
+    val comparator : Comparator<Pair<String, Int>> = compareByDescending<Pair<String, Int>> {it.second}.thenBy { it.first }
+
+
+    return scoreMap.filter{ it.value > 0}.toList().sortedWith(comparator).map{it.first}
+}
+
+
+
+fun main() {
+    val user = "mrko"
+
+    val a: MutableList<List<String>> = mutableListOf()
+    a.add(listOf("donut", "andole"))
+    a.add(listOf("donut", "jun"))
+    a.add(listOf("donut", "mrko"))
+    a.add(listOf("shakevan", "andole"))
+    a.add(listOf("shakevan", "jun"))
+    a.add(listOf("shakevan", "mrko"))
+
+    val b = listOf<String>("bedi", "bedi", "donut", "bedi", "shakevan")
+
+    println(solution7(user, a, b))
+
 }
