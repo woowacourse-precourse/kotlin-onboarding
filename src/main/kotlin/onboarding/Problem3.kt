d289d224ba1ee40e27c72391b28bc71a612378b2
YuGyeong98 <kuk3097@naver.com>
2022-10-31 23:04:16 +0900

    style(Problem3): 코드 포맷팅
    인덴트 수정, 예외 처리 주석 추가 및 private으로 변경

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,26 +1,27 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-  var numberOfClaps = 0
+    var numberOfClaps = 0
 
-  checkNumber(number)
+    checkNumber(number)
 
-  for (i in 1..number) {
-    var current = i
-    while (current != 0) {
-      if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
-        numberOfClaps += 1
-      }
-      current /= 10
+    for (i in 1..number) {
+        var current = i
+        while (current != 0) {
+            if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
+                numberOfClaps += 1
+            }
+            current /= 10
+        }
     }
-  }
-  return numberOfClaps
+    return numberOfClaps
 }
 
-fun checkNumber(number: Int) {
-  if (number !in 1..10000) {
-    throw IllegalArgumentException(
-      "number는 1 이상 10,000 이하인 자연수여야 합니다."
-    )
-  }
+/** 예외 처리 */
+private fun checkNumber(number: Int) {
+    if (number !in 1..10000) {
+        throw IllegalArgumentException(
+            "number는 1 이상 10,000 이하인 자연수여야 합니다."
+        )
+    }
 }
