2e0a3acd46c68ff51d45a745aed9ff2a177cab48
WoeunJung <leena09123@naver.com>
2022-10-31 10:56:30 +0900

    solve: prob6 - similar mickname limit

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,33 @@
 package onboarding
 
+import kotlin.math.max
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    var result = mutableListOf<String>()
+    var flag = false
+    forms.forEachIndexed { index1, list1 ->
+        forms.forEachIndexed { index2, list2 ->
+            if (index1 != index2){
+                val lcs = LCSubstring(list1[1], list2[1])
+                if (lcs > 1) flag = true
+            }
+        }
+        if (flag) result.add(list1[0])
+        flag = false
+    }
+    return result.distinct().sorted()
+}
+
+fun LCSubstring(str1: String, str2: String): Int{
+    var cnt = 0
+    for (i in 0 until (str1.length)){
+        for (j in 0 until (str2.length)){
+            var k = 0
+            while ((i+k) < str1.length && (j+k) < str2.length
+                && str1[i+k] == str2[j+k])
+                k += 1
+            cnt = max(cnt, k)
+        }
+    }
+    return cnt
 }
