365dcdf8ae6936c29344c7b6ef901323abecaf91
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-11-01 03:57:41 +0900

    feat: (문제6) 이메일과 닉네임의 형식과 길이 검사 기능 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,29 @@
 package onboarding
 
+import org.mockito.internal.matchers.Null
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+
+    val regularForms = checkEmailAndNickname(forms)
+
+}
+
+fun checkEmailAndNickname(forms: List<List<String>>): List<List<String>>{
+    val regularForms = mutableListOf<List<String>>()
+    for(i in forms.indices){
+        if(forms[i][0].length in 11 until 20 && forms[i][0].contains("@email.com")
+            && forms[i][0].length in 1 until 20 && isKorean(forms[i][1])){
+            regularForms.add(forms[i])
+        }
+    }
+    return regularForms
+}
+
+fun isKorean(nickname: String): Boolean {
+    for(i in nickname.indices){
+        val char = nickname.codePointAt(i)
+        if (char < 0xAC00 || char > 0xD800)
+            return false
+    }
+    return true
 }
