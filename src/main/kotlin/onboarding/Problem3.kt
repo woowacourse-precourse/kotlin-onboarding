2dc52ec346f4dc23925e89a37a995320e4d7d760
phs <gustjd0808@pusan.ac.kr>
2022-11-01 02:52:29 +0900

    Problem3 기능 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,32 @@
 package onboarding
 
+enum class KeyNumberOfGame(val number: Int){
+    First(3), Second(6), Third(9)
+}
+
+class NumberOf369Game(private val number: Int){
+    private fun is369(number: Int) =
+        number == KeyNumberOfGame.First.number || number == KeyNumberOfGame.Second.number
+                || number == KeyNumberOfGame.Third.number
+
+    fun countNumberContains369(): Int {
+        var result = 0
+        var mutableNumber = number
+        while (mutableNumber != 0) {
+            val remainder = mutableNumber % 10
+
+            if (is369(remainder)) result++
+
+            mutableNumber /= 10
+        }
+        return result
+    }
+}
+
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var result = 0
+    for (i in 3..number) {
+        result += NumberOf369Game(i).countNumberContains369()
+    }
+    return result
 }
