f0469ebbfdf796b6d52a1ce475638c64a063673c
jjw0071 <jjw0071@naver.com>
2022-10-31 17:34:30 +0900

    problem 5 done

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,18 @@
 package onboarding
 
+fun cal(money : Int, index : Int,result : Array<Int>, paper: Array<Int>): Int {
+    result[index] = money/paper[index]
+    val ret = money - (result[index]*paper[index])
+    return ret
+}
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var a = Array<Int>(9,{0})
+    var b = arrayOf(50000,10000,5000,1000,500,100,50,10,1)
+
+    var cur = money
+    for(i : Int in 0..8){
+        cur = cal(cur,i,a,b)
+    }
+    var c =a.toList()
+    return c
 }
