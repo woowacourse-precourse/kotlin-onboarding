196ea151acadbdcc02b839fde544ff6f20a3bdc8
DormunDoorlock <skrudgh2001@naver.com>
2022-10-31 16:38:05 +0900

    feat: Problem6.kt 1번 기능 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,25 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val result = mutableListOf<String>()
+    val onlyOne = mutableListOf<Pair<Char,Char>>()
+    val twoMore = mutableListOf<Pair<Char,Char>>()
+    //
+    for(i in forms.indices){
+        val nickName = forms[i][1]
+
+        val pairs = nickName.zipWithNext()
+        for(pair in pairs){
+            if(!onlyOne.contains(pair))
+                onlyOne.add(pair)
+            else if(onlyOne.contains(pair))
+                twoMore.add(pair)
+        }
+    }
+    // 중복제거
+    twoMore.distinct()
+    println(twoMore)
+
+
+    return result
 }
