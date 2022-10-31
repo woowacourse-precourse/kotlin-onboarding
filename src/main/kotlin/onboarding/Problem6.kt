89948f49701e6950b4cb7e421fecdad3420ff9cb
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-11-01 01:27:32 +0900

    6s 한글닉넴인확인함수

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,16 @@
 package onboarding
 
+import java.util.regex.Pattern
+
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
+}
+fun isKoreanNickname(nickname : String):Boolean{
+    val nicknamePattern = "^[가-힣]*$"
+    val pattern = Pattern.compile(nicknamePattern)
+    val matcher = pattern.matcher(nickname)
+    if(matcher.find()){
+        return true
+    }
+    return false
 }
