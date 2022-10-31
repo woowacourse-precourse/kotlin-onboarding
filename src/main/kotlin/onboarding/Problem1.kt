2742ce82dda1972169038a407a0ee4a7dfd0c4d8
SEO-J17 <qw4643987@naver.com>
2022-10-31 19:20:22 +0900

    프리코스 1주차 온보딩 : [문제1] 문제 풀이

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,39 @@
 package onboarding
 
+import kotlin.math.max
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1) {
+        return -1
+    }
+
+    val pobiMaxScore = calcScore(pobi)
+    val crongMaxScore = calcScore(crong)
+
+    return if (crongMaxScore > pobiMaxScore) {
+        2
+    } else if (crongMaxScore < pobiMaxScore) {
+        1
+    } else if (crongMaxScore == pobiMaxScore) {
+        0
+    } else {
+        -1
+    }
+
+}
+
+private fun calcScore(list: List<Int>): Int {
+    var maxScore = 0
+    list.forEach { page ->
+        var sum = 0
+        var multi = 1
+        page.toString().map {
+            with(it.code - 48) {
+                sum += this
+                multi *= this
+            }
+        }
+        maxScore = max(maxScore, max(sum, multi))
+    }
+    return maxScore
 }
