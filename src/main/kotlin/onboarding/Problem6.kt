cdbd0af76885fae11271e0df9bc8557dff58dc84
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-10-31 17:26:11 +0900

    문제 6 : Feat : 추가 입력값들 중에 이름만 추출 , 이름이 2글자 미만일 경우는 break

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -6,6 +6,14 @@ fun solution6(forms: List<List<String>>): List<String> {
     var email_set = HashSet<String>()
     var email_and_nick = HashMap<String,String>()
 
-    
+    for(i in 0 until forms.size){
+
+        var name = forms[i][1]
+
+        if(name.length < 2){
+            break
+        }
+
+        
 
 }
