b04898e759c3b85a39b4a7b36ca029d538526563
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-31 21:25:52 +0900

    refactor(Problem5): 코드 리팩토링
    변수명 변경, convertMoney 함수를 따로 작성하지 않고 solution5 내에서 해결하도록 수정

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,20 +1,15 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    val moneyList = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
-    return convertMoney(money, moneyList)
-}
+    val moneyList = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val result = MutableList(9) { 0 }
+    var target = money
 
-fun convertMoney(x: Int, moneyList: List<Int>): List<Int> {
-
-    val result = MutableList<Int>(9) { 0 }
-    var target = x
-
-    for ((index, money) in moneyList.withIndex()) {
+    for ((index, value) in moneyList.withIndex()) {
         if (target == 0) break
-        val mok = target / money
-        result[index] = mok
-        target -= mok * money
+        val num = target / value
+        result[index] = num
+        target -= num * value
     }
     return result
 }
