d1c23cd165a640368a65da8ad14d5cb2f28b4917
krrong <khk9664@naver.com>
2022-10-31 18:18:47 +0900

    중복된 문자열 확인 함수 생성

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,20 @@
 package onboarding
 
+// 1. 중복된 문자열이 존재하는지 확인하는 함수 생성
+// 2. 중복된 문자열을 제거하는 함수 생성
+// 3. 중복된 문자열이 존재하지 않을 때까지 중복된 문자열 제거
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    return ""
+}
+
+/**
+ * 중복된 문자열이 존재하는지 확인
+ */
+fun checkRepeated(cryptogram: String): Boolean{
+    for(i in 0 until (cryptogram.length - 1)){
+        if(cryptogram[i] == cryptogram[i+1]){
+            return false
+        }
+    }
+    return true
 }
