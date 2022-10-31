917bb5e559bec4b05d19d9d3abcae1162974039a
changhwan <chang5hustle@gmail.com>
2022-10-31 11:52:13 +0900

    [Problem 4] (요구사항 1) 변환테이블 생성 함수 작성

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,19 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    val conversionMap = getConversionMap()
+}
+
+private fun getConversionMap(): MutableMap<Char, Char> {
+    val alphabet = "ABCDEFGHIJKLMNOPQRXTUVWXYZ"
+    val upperAlphabetList = alphabet.toList()
+    val reverseUpperAlphabetList = upperAlphabetList.reversed()
+    val lowerAlphabetList = alphabet.lowercase().toList()
+    val reverseLowerAlphabetList = lowerAlphabetList.reversed()
+    val conversionTable = mutableMapOf<Char, Char>()
+
+    for (i in upperAlphabetList.indices) conversionTable[upperAlphabetList[i]] = reverseUpperAlphabetList[i]
+    for (i in lowerAlphabetList.indices) conversionTable[lowerAlphabetList[i]] = reverseLowerAlphabetList[i]
+
+    return conversionTable
 }
