59cf8f56be652972e9911a503e9e6a3b68aa7a13
changhwan <chang5hustle@gmail.com>
2022-10-31 14:02:11 +0900

    [Problem 5] (리펙토링 1) Index 변환 map 제거

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,27 +1,17 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    val conversionToIndexMap = getConversionToIndexMap()
-    return calcUnitCount(money, conversionToIndexMap)
-}
-
-private fun getConversionToIndexMap(): MutableMap<Int, Int> {
-    val conversionToIndexMap = mutableMapOf<Int, Int>()
     val moneyUnitList = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
-
-    for (i in moneyUnitList.indices)
-        conversionToIndexMap[moneyUnitList[i]] = i
-
-    return conversionToIndexMap
+    return calcUnitCount(money, moneyUnitList)
 }
 
-private fun calcUnitCount(money: Int, conversionToIndexMap: MutableMap<Int, Int>): List<Int> {
+private fun calcUnitCount(money: Int, moneyUnitList: List<Int>): List<Int> {
     val unitCount = mutableListOf<Int>()
     var processedMoney = money
 
-    conversionToIndexMap.forEach {
-        unitCount.add(processedMoney / it.key)
-        processedMoney %= it.key
+    moneyUnitList.forEach {
+        unitCount.add(processedMoney / it)
+        processedMoney %= it
     }
 
     return unitCount
