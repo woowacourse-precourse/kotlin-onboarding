3bd8164496b3c936d53d0cb6ed85873d65a64e95
dlwgnud <wngud980411@naver.com>
2022-11-01 01:07:49 +0900

    feat: 2번 문제

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,33 @@
 package onboarding
 
+import java.util.*
+
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    val answer = removeDuplication(cryptogram)
+    return toString(answer)
+}
+
+fun toString(stack: Stack<Char>): String {
+    var answer = ""
+    stack.forEach {
+        answer += it
+    }
+    return answer
+}
+
+fun removeDuplication(cryptogram: String): Stack<Char> {
+    val stack = Stack<Char>()
+
+    cryptogram.forEach {
+        if (stack.isEmpty()) stack.push(it)
+        else {
+            // 전에 문자와 같을 때 push 안함
+            if (stack.peek() == it) {
+                stack.pop()
+                return@forEach
+            }
+            stack.push(it)
+        }
+    }
+    return stack
 }
