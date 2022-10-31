c1b997e1ae1b36a3fa447134f865a4487c4fb374
lanpia <95679243+lanpia@users.noreply.github.com>
2022-10-31 20:30:41 +0900

    Problem5 solve

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,21 @@
 package onboarding
 
-fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+fun solution5(money: Int): List<Int> 
+{
+    //TODO("프로그램 구현")
+    var mon = money
+    val result = mutableListOf(0,0,0,0,0,0,0,0,0)
+
+    when {
+    mon >= 50000 -> {result[0] = mon / 50000;    mon -= 50000 * result [0]}
+    mon >= 10000 ->  result[1] = mon / 10000
+    mon >= 5000 ->  {result [2] = mon / 5000;    mon -= 5000}
+    mon >= 1000 ->   result [3] = mon / 1000
+    mon >= 500 ->   {result [4] = mon / 500 ;    mon -= 500}
+    mon >= 100 ->    result[5] = mon / 100
+    mon >= 50 ->    {result [6] = mon / 50 ;     mon -= 50}
+    mon >= 10 ->     result[7] = mon / 10 
+    mon >= 1 ->      result[8] = mon 
+    }
+    return result. toList()
 }
