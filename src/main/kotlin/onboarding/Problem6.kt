bf34066ef0f56f2f11010c388c5a3854102d7ea4
bluecat0823 <star4_you@naver.com>
2022-10-31 21:54:58 +0900

    미션6 : 2글자 이상 같은 글자가 들어있는 경우를 찾고 해당 닉네임이 가진 주소를 반환

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,31 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+
+    var overlapNames = mutableListOf<String>()
+
+    for (i in 0 until (forms.size)){
+
+        var nowName = forms[i][1]
+
+        for (j in 0 until (forms.size)){
+
+            if (j!=i){
+                for (k in 0 until (nowName.length-1)){
+
+                    if ((forms[j][1]).contains (nowName.substring(k..(k+1)))){
+
+                        overlapNames.add(forms[j][0])
+
+                    }
+
+                }
+            }
+
+        }
+
+    }
+
+
+    return (overlapNames.distinct()).sorted()
 }
