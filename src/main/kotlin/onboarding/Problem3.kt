25fe94c173e46075dae9a51de4b59b39b96b1cd9
lanpia <95679243+lanpia@users.noreply.github.com>
2022-10-31 20:29:01 +0900

    Problem3 solve

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,14 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    //TODO("프로그램 구현")
+    var ret = 0
+    for (i in 1..number)
+    {
+        var num_s = i.toString()
+        for (k in 0..(num_s.length-1))
+            if (num_s[k].digitToInt() % 3 == 0 && num_s[k].digitToInt() != 0)
+                ret++
+    }
+    println(ret)
 }
