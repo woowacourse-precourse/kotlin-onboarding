4b67f205c9786b1b99d552c21c15fede3418215d
phs <gustjd0808@pusan.ac.kr>
2022-11-01 03:31:55 +0900

    Problem7 기능 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,36 @@
 package onboarding
 
+fun getUserFriends(
+    user: String,
+    friends: List<List<String>>
+): List<String> {
+    return friends.filter { it.contains(user) }
+        .map { it[it.indexOf(user) xor 1] }
+        .toList()
+}
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val resultMap = hashMapOf<String, Int>()
+    val userFriends = getUserFriends(user, friends)
+
+    userFriends.flatMap { userFriend -> getUserFriends(userFriend, friends) }
+        .forEach { acquaintance ->
+            if (resultMap[acquaintance] == null) resultMap[acquaintance] = 0
+            else resultMap[acquaintance]!!.plus(10)
+        }
+
+    for (visitor in visitors) {
+        if (resultMap[visitor] == null) resultMap[visitor] = 0
+        else resultMap[visitor]!!.plus(1)
+    }
+
+    return resultMap.toList().sortedBy { mapEntry -> mapEntry.second }
+        .filter { mapEntry ->
+            !userFriends.contains(mapEntry.first) && mapEntry.first != user
+        }
+        .map { mapEntry -> mapEntry.first }.take(5)
 }
