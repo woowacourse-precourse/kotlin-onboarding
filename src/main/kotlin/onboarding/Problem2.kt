4348661917e21701866f3b4437bf45c142d36ff5
junhyungss <daba44@naver.com>
2022-10-31 22:28:44 +0900

    final

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var answer = cryptogram
+
+    while (true) {
+        var res = answer
+
+        // 현재 문자열의 문자 중에서 2개이상 연속되는 수가 있으면 없앤다.
+        for (i in answer.toSet()) {
+            res = res.replace("$i{2,}".toRegex(), "")
+        }
+
+        // 만약 연속되는 수가 없으면 그 전 문자열과 현재 문자열이 똑같다. 그렇다면 종료한다.
+        // 전 문자열과 현재 문자열이 다르다면 현재 바뀐 문자열을 answer에 넣어주고 다시 루프를 돈다.
+        if (answer == res) break else answer = res
+    }
+
+    return answer
 }
--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,27 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val answer = mutableSetOf<String>()
+    val check = BooleanArray(forms.size+1)
+    // 같은 글자가 연속적으로 포함될 경우
+    for (i in forms.indices){
+        // 현재 글자의 다음 글자부터 탐색
+        for (j in i+1 until forms.size){
+            // answer 들어 있는 않은 닉네임만 탐색
+            if (!check[j]){
+                // 현재 글자를 두글자씩 조합하여 포함되는지 탐색
+                for (k in 0 until forms[i][1].length-1){
+                    val twoWord = forms[i][1][k]+forms[i][1][k+1].toString()
+                    // 현재 탐색 중인 forms에 towWord가 포함된다면 answer에 넣음
+                    if (twoWord in forms[j][1]){
+                        check[j] = true
+                        answer.add(forms[j][0])
+                        answer.add(forms[i][0])
+                        break
+                    }
+                }
+            }
+        }
+    }
+    return answer.toList().sorted()
 }
