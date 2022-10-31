e3adf52f4b2fb3818bb5ec3297898c97c9df07df
lanpia <95679243+lanpia@users.noreply.github.com>
2022-10-31 20:31:26 +0900

    Problem6 solve

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,29 @@
 package onboarding
 
-fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+fun solution6(forms: List<List<String>>): List<String> 
+{
+    //TODO("프로그램 구현")
+    var dict = mutableMapOf<String, String>()
+    var ret = mutableSetOf<String>()
+
+    forms.forEach{crew ->
+        dict.forEach { (key,value) ->
+            if (checkdup(crew[1], key))
+            {
+                ret.addAll(listOf(crew[0],value))
+            }
+        }
+        dict[crew[1]] = crew[0]
+    }
+    ret = ret.toSortedSet()
+    return ret.toList()
+}
+
+fun checkdup(str1: String, str2: String): Boolean {
+    for (i in 0 until str1.length - 1) {
+        if (str2.contains(str1.substring(i, i + 2))) {
+            return true
+        }
+    }
+    return false
 }
