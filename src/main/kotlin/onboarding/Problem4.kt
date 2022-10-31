e35b16d42a68d86371a21818b952acc9f634cbc3
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-01 02:09:40 +0900

    feat(problem4): Add decrypt character method

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,19 @@
 package onboarding
 
+import kotlin.math.roundToInt
+
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    return ""
+}
+
+fun getMiddleLetterAsciiCode(isUpperCase: Boolean): Int {
+    return if (isUpperCase)
+        (('A'.code.toDouble() + 'Z'.code.toDouble()) / 2).roundToInt()
+    else
+        (('a'.code.toDouble() + 'z'.code.toDouble()) / 2).roundToInt()
+}
+
+fun getCryptoChar(alphabetByte: Byte, isUpperCase: Boolean): Char {
+    val middleLetterAsciiCode = getMiddleLetterAsciiCode(isUpperCase)
+    return (alphabetByte + (2 * (middleLetterAsciiCode - alphabetByte) - 1)).toChar()
 }
