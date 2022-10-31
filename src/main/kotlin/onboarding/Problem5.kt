a3ffee6d8ecf89079a016751bc70f6480a1c0ffa
SEO-J17 <qw4643987@naver.com>
2022-10-31 19:20:46 +0900

    프리코스 1주차 온보딩 : [문제5] 문제 풀이

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,16 @@
 package onboarding
 
+private var realMoney = 0
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    realMoney = money
+    val list = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+
+    return mutableListOf<Int>().apply {
+        list.forEach {
+            add(calcMoney(it))
+        }
+    }
 }
+
+private fun calcMoney(cash: Int): Int =
+    (realMoney / cash).also { realMoney %= cash }
