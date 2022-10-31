23dc55e54f5b0cad72a75087167471fffd76886b
Suheon Song <ssheon0812@naver.com>
2022-11-01 00:18:07 +0900

    feat: [Problem7] 유저들의 친구 정보를 반환하는 함수 추가

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,26 @@
 package onboarding
 
+import java.util.LinkedList
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val result = listOf<String>()
+    
+    
+
+    return result
+}
+
+fun getFriendInformation(friends: List<List<String>>): Map<String, LinkedList<String>> {
+    val friendInformation  = LinkedHashMap<String, LinkedList<String>>()
+    for (element in friends) {
+        friendInformation[element[0]] = friendInformation[element[0]] ?: LinkedList<String>()
+        friendInformation[element[1]] = friendInformation[element[1]] ?: LinkedList<String>()
+        friendInformation[element[0]]?.add(element[1])
+        friendInformation[element[1]]?.add(element[0])
+    }
+    return friendInformation
 }
