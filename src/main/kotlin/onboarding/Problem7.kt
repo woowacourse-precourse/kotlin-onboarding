e995d3cae1fee375649af9b9b8bea832827bd63c
YuGyeong98 <kuk3097@naver.com>
2022-10-31 23:38:49 +0900

    style(Problem7): 코드 포맷팅
    인덴트 수정, forEach 파라미터 명시적 선언, curly brace 추가, 함수 호출 연결된 코드 구분

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,96 +1,115 @@
 package onboarding
 
 fun solution7(
-  user: String,
-  friends: List<List<String>>,
-  visitors: List<String>
+    user: String,
+    friends: List<List<String>>,
+    visitors: List<String>
 ): List<String> {
-  val recommendFriends: List<String>
-  val idMap = mutableMapOf<String, MutableList<String>>()
-  val scoreMap = mutableMapOf<String, Int>()
+    val recommendFriends: List<String>
+    val idMap = mutableMapOf<String, MutableList<String>>()
+    val scoreMap = mutableMapOf<String, Int>()
 
-  checkSNS(user, friends, visitors)
+    checkSNS(user, friends, visitors)
 
-  friends.forEach { friend ->
-    val idA = friend[0]
-    val idB = friend[1]
+    friends.forEach { friend ->
+        val idA = friend[0]
+        val idB = friend[1]
 
-    if (idMap.containsKey(idA)) idMap[idA]?.add(idB) else idMap[idA] = mutableListOf(idB)
-    if (idMap.containsKey(idB)) idMap[idB]?.add(idA) else idMap[idB] = mutableListOf(idA)
-  }
+        if (idMap.containsKey(idA)) {
+            idMap[idA]?.add(idB)
+        } else {
+            idMap[idA] = mutableListOf(idB)
+        }
 
-  idMap[user]?.forEach {
-    idMap[it]?.filter { it != user }?.forEach { friend ->
-      if (scoreMap.containsKey(friend)) scoreMap[friend] = scoreMap.getValue(friend) + 10
-      else scoreMap[friend] = 10
+        if (idMap.containsKey(idB)) {
+            idMap[idB]?.add(idA)
+        } else {
+            idMap[idB] = mutableListOf(idA)
+        }
     }
-  }
 
-  visitors.filter { idMap[user]?.contains(it) == false }.forEach { visitor ->
-    if (scoreMap[visitor] != null) scoreMap[visitor] = scoreMap.getValue(visitor) + 1
-    else scoreMap[visitor] = 1
-  }
+    idMap[user]?.forEach { id ->
+        idMap[id]?.filter { id != user }?.forEach { friend ->
+            if (scoreMap.containsKey(friend)) {
+                scoreMap[friend] = scoreMap.getValue(friend) + 10
+            } else {
+                scoreMap[friend] = 10
+            }
+        }
+    }
 
-  recommendFriends = scoreMap.toList().sortedByDescending { it.second }.map { it.first }.take(5)
+    visitors.filter { idMap[user]?.contains(it) == false }.forEach { visitor ->
+        if (scoreMap[visitor] != null) {
+            scoreMap[visitor] = scoreMap.getValue(visitor) + 1
+        } else {
+            scoreMap[visitor] = 1
+        }
+    }
 
-  return recommendFriends
+    recommendFriends = scoreMap.toList()
+        .sortedByDescending { it.second }
+        .map { it.first }
+        .take(5)
+
+    return recommendFriends
 }
 
-fun checkSNS(
-  user: String,
-  friends: List<List<String>>,
-  visitors: List<String>
+/** 예외 처리 */
+private fun checkSNS(
+    user: String,
+    friends: List<List<String>>,
+    visitors: List<String>
 ) {
-  val regex = "^[a-z]*\$".toRegex()
+    val regex = "^[a-z]*\$".toRegex()
 
-  if (user.length !in 1..30) {
-    throw IllegalArgumentException(
-      "user 아이디는 길이가 1 이상 30 이하인 문자열이어야 합니다."
-    )
-  }
-  friends.forEach { friend ->
-    if (friend[0].length !in 1..30 && friend[1].length !in 1..30) {
-      throw IllegalArgumentException(
-        "friends 리스트의 아이디는 길이가 1 이상 30 이하인 문자열이어야 합니다."
-      )
+    if (user.length !in 1..30) {
+        throw IllegalArgumentException(
+            "user 아이디는 길이가 1 이상 30 이하인 문자열이어야 합니다."
+        )
     }
-  }
-  visitors.forEach { visitor ->
-    if (visitor.length !in 1..30) {
-      throw IllegalArgumentException(
-        "visitors 리스트의 아이디는 길이가 1 이상 30 이하인 문자열이어야 합니다."
-      )
+    friends.forEach { friend ->
+        if (friend[0].length !in 1..30 && friend[1].length !in 1..30) {
+            throw IllegalArgumentException(
+                "friends 리스트의 아이디는 길이가 1 이상 30 이하인 문자열이어야 합니다."
+            )
+        }
     }
-  }
+    visitors.forEach { visitor ->
+        if (visitor.length !in 1..30) {
+            throw IllegalArgumentException(
+                "visitors 리스트의 아이디는 길이가 1 이상 30 이하인 문자열이어야 합니다."
+            )
+        }
+    }
 
-  if (friends.size !in 1..10000) {
-    throw IllegalArgumentException(
-      "friends는 길이가 1 이상 10,000 이하인 리스트/배열이어야 합니다."
-    )
-  }
-  if (visitors.size !in 0..10000) {
-    throw IllegalArgumentException(
-      "visitors는 길이가 0 이상 10,000 이하인 리스트/배열이어야 합니다."
-    )
-  }
+    if (friends.size !in 1..10000) {
+        throw IllegalArgumentException(
+            "friends는 길이가 1 이상 10,000 이하인 리스트/배열이어야 합니다."
+        )
+    }
+    if (visitors.size !in 0..10000) {
+        throw IllegalArgumentException(
+            "visitors는 길이가 0 이상 10,000 이하인 리스트/배열이어야 합니다."
+        )
+    }
 
-  if (!user.matches(regex)) {
-    throw IllegalArgumentException(
-      "user 아이디는 알파벳 소문자만 가능합니다."
-    )
-  }
-  friends.forEach { friend ->
-    if (!friend[0].matches(regex) && !friend[1].matches(regex)) {
-      throw IllegalArgumentException(
-        "friends 리스트의 아이디는 알파벳 소문자만 가능합니다."
-      )
+    if (!user.matches(regex)) {
+        throw IllegalArgumentException(
+            "user 아이디는 알파벳 소문자만 가능합니다."
+        )
     }
-  }
-  visitors.forEach { visitor ->
-    if (!visitor.matches(regex)) {
-      throw IllegalArgumentException(
-        "visitors 리스트의 아이디는 알파벳 소문자만 가능합니다."
-      )
+    friends.forEach { friend ->
+        if (!friend[0].matches(regex) && !friend[1].matches(regex)) {
+            throw IllegalArgumentException(
+                "friends 리스트의 아이디는 알파벳 소문자만 가능합니다."
+            )
+        }
     }
-  }
+    visitors.forEach { visitor ->
+        if (!visitor.matches(regex)) {
+            throw IllegalArgumentException(
+                "visitors 리스트의 아이디는 알파벳 소문자만 가능합니다."
+            )
+        }
+    }
 }
