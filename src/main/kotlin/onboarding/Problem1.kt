73e230e179560eaa8348a9adb62595fdd09fc6ca
choHarmony <jws0183@naver.com>
2022-11-01 03:50:46 +0900

    [문제 1] 기능 요구 사항 - 3

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -4,10 +4,6 @@ import kotlin.math.max
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 
-    if (pobi[1]-pobi[0] != 1 || crong[1]-crong[0] != 1 || pobi[1] == 1 || crong[1] == 1 || pobi[0] == 400 || crong[0] == 400) {
-        return -1
-    }
-
     fun gameResult(pages: List<Int>): Int {
 
         val left = pages[0].toString().chunked(1)
@@ -17,13 +13,19 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
         val right_split: Array<Int> = right.map(String::toInt).toTypedArray()
 
         val left_plus = left_split.sum()
+        val right_plus = right_split.sum()
 
         var left_multi = 1
+        var right_multi = 1
         for(i in left_split) {
             left_multi *= i
         }
+        for(j in right_split) {
+            right_multi *= j
+        }
 
         val max_left = arrayOf(left_plus, left_multi).maxOrNull()
+        val max_right = arrayOf(right_plus, right_multi).maxOrNull()
 
         return 0
     }
