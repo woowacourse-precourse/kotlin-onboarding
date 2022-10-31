517426f9e8efdd98db275d87d330594c9ad89e7c
dlwgnud <wngud980411@naver.com>
2022-11-01 01:08:04 +0900

    feat: 3번 문제

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,14 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var cnt = 0
+    for (i in 1..number) {
+        var num = i
+        while (num > 0) {
+            if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9)
+                cnt++
+            num /= 10
+        }
+    }
+    return cnt
 }
