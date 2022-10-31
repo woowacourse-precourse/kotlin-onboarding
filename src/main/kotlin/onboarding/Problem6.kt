9f2fb4f26d666a81f14fd9d385f8c20905c3b198
Gloria <ippnsj@gmail.com>
2022-11-01 03:47:25 +0900

    feat(Problem6): 주어진 리스트가 올바른지 확인하는 기능 크루가 1명 이상 10000명 이하인지 확인 각 크루가 2개의 정보를 가지고 있는지 확인

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,27 @@
 package onboarding
 
+import problem6.ListSizeVerifier
+
+private fun validateList(forms: List<List<String>>) {
+    val formsSizeValidator = InputValidator(
+        listOf(
+            ListSizeVerifier(1, 10000)
+        )
+    )
+    formsSizeValidator.validate(forms)
+
+    val formSizeValidator = InputValidator(
+        listOf(
+            ListSizeVerifier(2, 2)
+        )
+    )
+    for(form in forms) {
+        formSizeValidator.validate(form)
+    }
+}
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    validateList(forms)
+
+    return listOf()
 }
