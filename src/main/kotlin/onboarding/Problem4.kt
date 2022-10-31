3a000a9732fa7385eb1da49221e297dedaa070d2
changhwan <chang5hustle@gmail.com>
2022-10-31 12:17:39 +0900

    [Problem 4] (요구사항 2) 알파벳만 전환하는 전환 함수 작성

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -2,10 +2,11 @@ package onboarding
 
 fun solution4(word: String): String {
     val conversionMap = getConversionMap()
+    return getConversionWord(word, conversionMap)
 }
 
 private fun getConversionMap(): MutableMap<Char, Char> {
-    val alphabet = "ABCDEFGHIJKLMNOPQRXTUVWXYZ"
+    val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     val upperAlphabetList = alphabet.toList()
     val reverseUpperAlphabetList = upperAlphabetList.reversed()
     val lowerAlphabetList = alphabet.lowercase().toList()
@@ -14,6 +15,13 @@ private fun getConversionMap(): MutableMap<Char, Char> {
 
     for (i in upperAlphabetList.indices) conversionTable[upperAlphabetList[i]] = reverseUpperAlphabetList[i]
     for (i in lowerAlphabetList.indices) conversionTable[lowerAlphabetList[i]] = reverseLowerAlphabetList[i]
+    conversionTable[' '] = ' '
 
     return conversionTable
+}
+
+private fun getConversionWord(word: String, conversionMap: MutableMap<Char, Char>): String {
+    val processedWordList = mutableListOf<Char>()
+    for (i: Char in word.toList()) processedWordList.add(conversionMap[i] ?: i)
+    return processedWordList.joinToString("")
 }
