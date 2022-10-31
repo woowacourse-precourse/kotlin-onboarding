1cefc4aec1661c67222c59075666469a16f763a7
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-10-31 21:53:53 +0900

    :sparkles: user 친구 추가 기능

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,44 @@
 package onboarding
 
+private var friendsCopy = mutableListOf<MutableList<String>>()
+private var myFriends = mutableListOf<String>()
+private var recommmedPoint = HashMap<String,Int>()
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+
+    for(i in 0 until friends.size){
+        friendsCopy.add(i,friends[i].toMutableList())
+    }
+
+    addMyFriend(user,friends)
+//    setPoint(user, friends, visitors)
+
+    var resultSort = mutableListOf<Pair<String,Int>>()
+    for(i in recommmedPoint){
+        resultSort.add(Pair(i.key,i.value))
+    }
+    resultSort.sortBy {
+        it.second
+    }
+    var result = mutableListOf<String>()
+    for(i in resultSort){
+        result.add(i.first)
+    }
+    return result.toList()
 }
+private fun addMyFriend(user : String, friends : List<List<String>>){
+    for(i in friends){
+        if(i[0]==user){
+            myFriends.add(i[1])
+            friendsCopy.remove(i)
+        }
+        else if(i[1]==user){
+            myFriends.add(i[0])
+            friendsCopy.remove(i)
+        }
+    }
+}
+//
