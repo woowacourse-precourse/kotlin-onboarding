9c4fc448098a14e4b5ce8d1f7bac940e84e6f344
jjw0071 <jjw0071@naver.com>
2022-10-31 17:15:13 +0900

    problem 4 done

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var check = word.toCharArray()
+    var a : Int
+    for(i : Int in 0..check.size-1){
+        a = check[i].code
+        if(a>64 && a<91){
+            a = 155-a
+            check[i] = a.toChar()
+        }else if(a>96 && a<123)
+            a = 219-a
+        check[i] = a.toChar()
+    }
+    var answer = check.joinToString("")
+    return answer
 }
