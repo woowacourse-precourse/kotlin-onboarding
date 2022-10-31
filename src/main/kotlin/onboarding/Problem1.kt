aec7e88d6c71ce45bfdbb7837d99859b2a5b4588
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-01 01:56:59 +0900

    feat(problem1): Add Error Check

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,22 @@
 package onboarding
 
+import kotlin.math.abs
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if(isErrorExist(pobi, crong))
+        return -1
+
+    return -1
+}
+
+fun isInputError(list: List<Int>): Boolean {
+    return list[0] % 2 == 0 || list[1] % 2 != 0 || abs(list[0] - list[1]) != 1
+}
+
+fun isErrorExist(pobi: List<Int>, crong: List<Int>): Boolean {
+    if (isInputError(pobi) || isInputError(crong)) {
+        return true
+    }
+
+    return false
 }
