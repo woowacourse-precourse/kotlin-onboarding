ec994f99076577b7570038e778f3b3fa34ec68c1
Hyuna Kim <akxmcse@gmail.com>
2022-10-31 20:48:42 +0900

    feat: problem7 유저의 친구 목록을 반환하는 함수 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -7,3 +7,16 @@ fun solution7(
 ): List<String> {
     TODO("프로그램 구현")
 }
+
+private fun getUserFriendsList(userId: String, friends: List<List<String>>): MutableList<String> {
+    val userFriendsList: MutableList<String> = mutableListOf()
+
+    for (i in friends.indices) {
+        if (friends[i][0] == userId) {
+            userFriendsList.add(friends[i][1])
+        } else if (friends[i][1] == userId) {
+            userFriendsList.add(friends[i][0])
+        }
+    }
+    return userFriendsList
+}
