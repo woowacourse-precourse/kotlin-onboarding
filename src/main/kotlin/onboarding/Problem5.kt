b0369e7023f624f38bdd78084fc14e33bbe6ba58
권용민 <briandr@naver.com>
2022-11-01 04:48:37 +0900

    [Problem5] 큰 화폐 단위부터 내림차순으로 money가 몇 개 변환되는지를 계산  - 화폐 단위로 money를 나눈 몫을 결과 리스트에 추가한다.  - 나눈 나머지를 money에 대입한다.

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -2,4 +2,17 @@ package onboarding
 
 fun solution5(money: Int): List<Int> {
     val unitList = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    return convertMoney(money, unitList)
+}
+
+private fun convertMoney(money: Int, unitList: List<Int>): List<Int> {
+    var input = money
+    val resultList = mutableListOf<Int>()
+
+    unitList.forEach {
+        resultList.add(input / it)
+        input %= it
+    }
+
+    return resultList
 }
