d5d768abd1c7e6089c2bc7b8b276b696b8242570
choHarmony <jws0183@naver.com>
2022-11-01 03:39:08 +0900

    [문제1] 기능  2

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,23 @@
 package onboarding
 
+import kotlin.math.max
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
+    fun gameResult(pages: List<Int>): Int {
+        val left = pages[0].toString().chunked(1)
+        val right = pages[1].toString().chunked(1)
+
+        val left_split: Array<Int> = left.map(String::toInt).toTypedArray()
+        val right_split: Array<Int> = right.map(String::toInt).toTypedArray()
+
+        val left_plus = left_split.sum()
+        val right_plus = right_split.sum()
+        val max_plus = arrayOf(left_plus, right_plus).maxOrNull()
+
+        return TODO("Provide the return value")
+
+    }
+
+    return TODO("Provide the return value")
 }
