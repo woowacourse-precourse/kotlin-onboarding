4af4c6d59b805147d8a7c9f8d1866cac63ffec6a
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-10-31 23:36:15 +0900

    22.10.31 problem1 기능 3번 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,21 +1,26 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    if (isCorrect(pobi) && isCorrect(crong)) {
-        val pSumOfPageLeft = addNumberOfDigit(pobi[0])
-        val pSumOfPageRight = addNumberOfDigit(pobi[1])
-        val cSumOfPageLeft = addNumberOfDigit(crong[0])
-        val cSumOfPageRight = addNumberOfDigit(crong[1])
+    val pPageLeft = pobi[0]
+    val pPageRight = pobi[1]
+    val cPageLeft = crong[0]
+    val cPageRight = crong[1]
 
+    if (isCorrect(pobi, pPageLeft, pPageRight) && isCorrect(crong, cPageLeft, cPageRight)) {
+        val pSumOfPageLeft = addNumberOfDigit(pPageLeft)
+        val pSumOfPageRight = addNumberOfDigit(pPageRight)
+        val cSumOfPageLeft = addNumberOfDigit(cPageLeft)
+        val cSumOfPageRight = addNumberOfDigit(cPageRight)
 
+        val pMultipleOfPageLeft = multiplyNumberOfDigit(pPageLeft)
+        val pMultipleOfPageRight = multiplyNumberOfDigit(pPageRight)
+        val cMultipleOfPageLeft = multiplyNumberOfDigit(cPageLeft)
+        val cMultipleOfPageRight = multiplyNumberOfDigit(cPageRight)
     }
-    else print("값이 올바르지 않습니다. 다시 입력해주세요.")
-    return 0;
+    else return -1
 }
 
-fun isCorrect(list: List<Int>): Boolean {
-    val pageLeft = list[0]
-    val pageRight = list[1]
+fun isCorrect(list: List<Int>, pageLeft: Int, pageRight: Int): Boolean {
     val condition1 = list.size == 2
     val condition2 = (pageLeft in 1..400 && pageRight in 1..400)
     val condition3 = (pageLeft % 2 == 1) && (pageRight % 2 == 0)
@@ -33,3 +38,14 @@ fun addNumberOfDigit(num: Int): Int {
     }
     return sum
 }
+
+fun multiplyNumberOfDigit(num: Int): Int {
+    var number = num
+    var multiple = 1
+    while (number != 0) {
+        multiple *= (number % 10)
+        number /= 10
+    }
+    return multiple
+}
+
