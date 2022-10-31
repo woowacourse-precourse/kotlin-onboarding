679eb1e603724e208d986a45a46e51fb1fc3fca5
yelm <21yrshin@naver.com>
2022-10-31 22:58:29 +0900

    problem 1 solved and tested

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -4,16 +4,20 @@ import kotlin.math.roundToInt
 fun pointsum(pages: List<Int>): Int{
     var temp0 = pages[0]
     var sum0 = 0;
-    while (temp0 % 10 == 0){
-        sum0 += temp0 % 10
+    var check0 = temp0 % 10
+    while (temp0 != 0){
+        sum0 += check0
         temp0 = round((temp0 / 10).toDouble()).toInt()
+        check0 %= 10
     }
 
     var temp1 = pages[1]
     var sum1 = 0;
-    while (temp1 % 10 == 0) {
+    var check1 = temp1 % 10
+    while (temp1 != 0) {
         sum1 += temp1 % 10
         temp1 = round((temp1 / 10).toDouble()).toInt()
+        check1
     }
 
     return if (sum0 >= sum1){
@@ -26,15 +30,19 @@ fun pointsum(pages: List<Int>): Int{
 fun pointmult(pages: List<Int>): Int{
     var temp0 = pages[0]
     var res0 = 1
-    while (temp0 % 10 == 0){
-        res0 *= temp0 % 10
+    var check = temp0 % 10
+    while (temp0 != 0){
+        res0 *= check
         temp0 = round((temp0 / 10).toDouble()).toInt()
+        check %= 10
     }
 
     var temp1 = pages[1]
     var res1 = 1
-    while (temp1 % 10 == 0) {
-        res1 *= temp1 % 10
+
+    while (temp1 != 0) {
+        var check1 = temp1 % 10
+        res1 *= check1
         temp1 = round((temp1 / 10).toDouble()).toInt()
     }
 
@@ -52,16 +60,16 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 
     val crongPoint = if (pointsum(crong) >= pointmult(crong))
                         pointsum(crong)
-                    else pointmult(pobi)
+                    else pointmult(crong)
 
     val err = -1
     val pobiwin = 1
     val crongwin = 2
     val draw = 0
 
-    return if (pobi[0]+1 == pobi[1]) {
+    return if (pobi[0]+1 != pobi[1]) {
         err
-    }else if (crong[0]+1 == crong[1]){
+    }else if (crong[0]+1 != crong[1]){
         err
     }else{
         if (pobiPoint > crongPoint)
