bce075e4fc89133f4fe1a73868771a838f8bba74
dev-worthwhile <dev.lboram@gmail.com>
2022-10-31 15:58:21 +0900

    feat: (기능2) 아스키코드 번호를 활용하여 각 문자의 역순 알파벳 번호 찾아주고, 문자로 바꿔준다.

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,15 +1,31 @@
 package onboarding
 
+import kotlin.math.abs
+
 /**
  * 기능목록
  * (1) 문자열을 한 글자씩 분리
- * (2) 아스키코드 번호를 활용하여 각 문자의 역순 알파벳을 찾아주고, 출력한다
+ * (2) 아스키코드 번호를 활용하여 각 문자의 역순 알파벳 번호를 찾아주고, 역순 알파벳출력한다
  * (3) 알파벳을 제외한 문자는 그대로 출력
  * **/
 fun solution4(word: String): String {
     var solutionWord = "" //return 값
 
     val wordPiece = word.toList() //(1) 한 글자씩 분리
+
+    for(index  in wordPiece.indices) {
+        if (wordPiece[index].isUpperCase()) { //대문자 -> 역순알파벳찾아주기
+            val reverseOrder = abs(wordPiece[index].code - 91) + 64
+            solutionWord += reverseOrder.toChar()
+            continue
+        }
+
+        if (wordPiece[index].isLowerCase()) { //소문자 -> 역순알파벳찾아주기
+            val reverseOrder = abs(wordPiece[index].code - 123) + 96
+            solutionWord += reverseOrder.toChar()
+            continue
+        }
+    }
 
     return solutionWord
 }
