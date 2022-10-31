213705fa51604824773c02a9755d0a52805e54d9
윤동현 <donghyunyoon81@gmail.com>
2022-10-31 21:20:57 +0900

    Feat:같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 목록 구하기 for 문을 활용하여 연속적으로 포함되는 글자를 구하기 위해서 forms에 있는 모든 닉네임을 두 글자씩 잘라서 list에 담습니다. for 문을 활용하여 분리된 닉네임에서 중복되는 닉네임을 list에 담습니다. for 문을 활용하여 forms안에 닉네임중에 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자를 list에 담습니다. 중복된 닉네임을 가진 사람의 이메일 list에 중복 값을 제거하고 정렬하여 return 합니다.

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,25 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val separateNicknameList = mutableListOf<String>()
+    val duplicateNicknameList = mutableSetOf<String>()
+    val duplicateNicknameEmailList = mutableListOf<String>()
+    for (i in forms.indices) {
+        for(j in forms[i][1].indices) {
+            if (j + 1 == forms[i][1].length) break
+            separateNicknameList.add("${forms[i][1][j]}${forms[i][1][j + 1]}")
+        }
+    }
+    for(i in separateNicknameList.indices) {
+        if(separateNicknameList.indexOf(separateNicknameList[i]) !=separateNicknameList.lastIndexOf(separateNicknameList[i])) duplicateNicknameList.add(separateNicknameList[i])
+    }
+    for(i in forms.indices) {
+        for(j in duplicateNicknameList) {
+            if(forms[i][1].indexOf(j)!=-1) {
+                duplicateNicknameEmailList.add(forms[i][0])
+                break
+            }
+        }
+    }
+    return duplicateNicknameEmailList.distinct().sorted()
 }
