d3a07fd44a8ab47e7048b0ef43fd162adb248234
hyungmin <utopia96@hanmail.net>
2022-10-31 23:26:34 +0900

    feat: 네번째 문제 solution 작성 및 검증

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,25 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var res = word.toCharArray()
+    for(i in word.indices){
+        if(word[i]!=' '){
+            res[i] = reverse(res[i])
+        }
+
+    }
+    return res.concatToString()
 }
+
+fun reverse(char:Char): Char {
+    //소문자
+    return if(char.code >= 97){
+        (219-char.code).toChar()
+    }
+    //대문자
+    else{
+        (155-char.code).toChar()
+    }
+}
+
+
