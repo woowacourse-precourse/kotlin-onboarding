92b3fcd715b16e30d696a49b09a384c29c30d37d
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-31 16:48:39 +0900

    이미 친구인 목록 추출 함수

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,22 @@
 package onboarding
 
+//기능별 목록
+//1. 이미 친구인 목록 추출
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val friendslist = alredayfriends(friends,user)
+    return friendslist
+}
+//이미 친구인 목록 추출 함수
+fun alredayfriends(friends: List<List<String>>,user: String):List<String> {
+    val friendslist:MutableList<String> = mutableListOf()
+    for(i:Int in friends.indices){
+        if(friends[i][1]==user){
+            friendslist.add(friends[i][0])
+        }
+    }
+    return friendslist
 }
