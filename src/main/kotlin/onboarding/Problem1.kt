455fbdc2e3bc206329217aca2c9fcbbb02500c87
leehandsub <gustjq7027@naver.com>
2022-10-31 18:38:43 +0900

    feature/잘못된 인풋값 확인

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -3,15 +3,43 @@ package onboarding
 import java.lang.Integer.max
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+
+    if (!checkInput(pobi) || !checkInput(crong))
+        return -1
     val pobiMax = compareNum(pobi)
     val crongMax = compareNum(crong)
 
-    return if(pobiMax>crongMax)
+    return if (pobiMax > crongMax)
         1
-    else if(pobiMax<crongMax)
+    else if (pobiMax < crongMax)
         2
     else
         0
+}
+
+fun checkBound(page: Int): Boolean {
+    return page in 1..400
+}
+
+fun checkOdd(page: Int): Boolean {
+    return page % 2 == 1
+}
+
+fun checkContinue(left: Int, right: Int): Boolean {
+    return left - right == -1
+}
+
+fun checkInput(person: List<Int>): Boolean {
+    val left = person[0]
+    val right = person[1]
+
+    if (!checkBound(left) || !checkBound(right))
+        return false
+    if (!checkOdd(left) || checkOdd(right))
+        return false
+    if (!checkContinue(left, right))
+        return false
+    return true
 }
 
 fun compareNum(person: List<Int>): Int {
@@ -31,4 +59,3 @@ fun calMax(now: Int): Int {
     }
     return max(mul, plus)
 }
-
