b1b79c01d7352141b0ec7c1ffedcdbfd4defe3fa
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-10-31 17:24:13 +0900

    문제 6 : Feat : 추가 입력값을 담을 map과 출력값을 만들 set 생성

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,11 @@
 package onboarding
 
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+
+    var email_set = HashSet<String>()
+    var email_and_nick = HashMap<String,String>()
+
+    
+
 }
