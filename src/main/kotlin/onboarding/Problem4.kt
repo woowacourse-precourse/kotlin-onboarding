dee153bce80b3156cb6164c6c6bb70f295b1467e
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-31 21:24:49 +0900

    refactor(Problem4): 함수명, 변수명 변경 및 코드 리팩토링
    함수명 및 변수명 변경, else 예약어를 쓰지 않도록 코드 리팩토링

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -2,21 +2,21 @@ package onboarding
 
 fun solution4(word: String): String {
     var answer = ""
-    for (i in word) {
-        answer += change(i)
+    for (char in word) {
+        answer += convertChar(char)
     }
     return answer
 }
 
-fun change(x: Char): Char {
+fun convertChar(x: Char): Char {
     val intX = x.code
-    return if ((intX >= 65) && (intX <= 90)) {
+    if ((intX >= 65) && (intX <= 90)) {
         val gap = intX - 65
-        (90 - gap).toChar()
-    } else if ((intX >= 97) && (intX <= 122)) {
-        val gap = intX - 97
-        (122 - gap).toChar()
-    } else {
-        x
+        return (90 - gap).toChar()
     }
+    if ((intX >= 97) && (intX <= 122)) {
+        val gap = intX - 97
+        return (122 - gap).toChar()
+    }
+    return x
 }
