2f5f92cc15f6101fbd0740150a0fafc687fd3471
DormunDoorlock <skrudgh2001@naver.com>
2022-10-31 17:54:57 +0900

    feat: Problem7.kt 1번 기능 추가

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,19 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val friendRelations = mutableMapOf<String,MutableSet<String>>()
+    val answer = mutableListOf<String>()
+    // friend 관계도 생성
+    for(friend in friends){
+        val friendA=friend[0]
+        val friendB=friend[1]
+        if(!friendRelations.containsKey(friendA))
+            friendRelations[friendA]= mutableSetOf()
+        if(!friendRelations.containsKey(friendB))
+            friendRelations[friendB]= mutableSetOf()
+        friendRelations[friendA]?.add(friendB)
+        friendRelations[friendB]?.add(friendA)
+    }
+    println(friendRelations)
+    return answer
 }
