6aa1608bb3ef41eac49a344e5bfb0124a6d9455f
dlwgnud <wngud980411@naver.com>
2022-11-01 01:08:52 +0900

    feat: 5번 문제

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var money = money
+    val moneyTypes = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val answer = moneyTypes.map { it * 0 }.toMutableList()
+
+    while (money > 0) {
+        for (i in moneyTypes.indices) {
+            while (money >= moneyTypes[i]) {
+                money -= moneyTypes[i]
+                answer[i]++
+            }
+        }
+    }
+    return answer
 }
