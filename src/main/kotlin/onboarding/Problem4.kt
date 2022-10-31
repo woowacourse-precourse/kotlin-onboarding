76894e1f32a4c27a75709fc1fcb17c36a8ec5ba4
mendel <mendel.urssu@gmail.com>
2022-10-31 15:01:31 +0900

    Problem4.kt 문제4 기능 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,32 @@
 package onboarding
 
+import java.lang.StringBuilder
+
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var str = StringBuilder(word)
+    // 65~90 26개가 A~Z임.
+    // 97~122 26개가 a~z임.
+    for (i in str.toString().indices) {
+        var code = word[i].code
+        if (code in 65..90) { // 대문자이면 소문자로 변환.
+            if (str[i].code >= 78) {
+                val ch: Char = (65 + 90 - str[i].code).toChar()
+                str.replace(i, i + 1, ch.toString())
+            } else {
+                val ch: Char = (90 - str[i].code + 65).toChar()
+                str.replace(i, i + 1, ch.toString())
+            }
+        }
+        else if(code in 97..122){
+            if (str[i].code >= 109) {
+                val ch: Char = (97 + 122 - str[i].code).toChar()
+                str.replace(i, i + 1, ch.toString())
+            } else {
+                val ch: Char = (122 - str[i].code + 97).toChar()
+                str.replace(i, i + 1, ch.toString())
+            }
+        }
+    }
+
+    return str.toString()
 }
