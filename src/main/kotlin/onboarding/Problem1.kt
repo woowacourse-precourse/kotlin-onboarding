be60088db629ac2b249340d2ae0024a212bd2755
hayeon <hayeon2401@naver.com>
2022-10-31 19:20:22 +0900

    solution1 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,31 @@
 package onboarding
 
+import kotlin.math.*
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    val pobiMaxNum = findMaxNum(pobi)
+    val crongMaxNum = findMaxNum(crong)
+
+    if(pobiMaxNum > crongMaxNum) return 1
+    else if(pobiMaxNum < crongMaxNum) return 2
+    else if(pobiMaxNum == crongMaxNum) return 0
+    else return -1
+}
+
+fun findMaxNum(list: List<Int>): Int{
+    var maxNum = 0
+
+    for(i in list){
+        var plusNum = 0; var mulNum = 1; var tmp = i
+
+        while(tmp!=0){
+            plusNum += tmp % 10
+            mulNum *= tmp % 10
+            tmp /= 10
+        }
+
+        maxNum = max(max(mulNum, plusNum), maxNum)
+    }
+
+    return maxNum
 }
