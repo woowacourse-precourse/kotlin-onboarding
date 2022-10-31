3ad156ad782e5d59851d0f01733b61b5a7150fb8
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-10-31 09:57:26 +0900

    1s 페이지 분할 함수

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,22 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    var result: Int = -1//결과 값
+    fun pageDivision(page: Int): List<Int> { //페이지 분할 해주는 함수(ex Int 153 => list(1,5,3))
+        var numList = mutableListOf<Int>()
+        var page: Int = page
+        if (page > 100) {
+            numList.add(page / 100)
+            page -= (page / 100 )* 100
+        }
+        if (page > 10) {
+            numList.add(page / 10)
+            page -= (page / 10)* 10
+        }
+        if (page in 1..9) {
+            numList.add(page)
+        }
+        return numList
+    }
+    return  result //결과 반환
 }
