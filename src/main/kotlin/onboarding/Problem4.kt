88f1e8ffbb8b61b0709587932e5281425d968ceb
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-01 00:15:09 +0900

    [Problem4] feat: 유효한 입력값인지 검사하는 함수 작성

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,25 @@
 package onboarding
 
+import java.util.regex.Pattern
+
+
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    if(!checkValidInput(word))
+        throw IllegalArgumentException("invalid input found!")
+
+    return ""
 }
+fun checkValidInput(word: String): Boolean{
+    return Pattern.matches("^[a-zA-Z\\s]*\$",word )
+}
+
+fun getConvertedWord(word: String): String{
+    // TODO: 2022-11-01
+    return ""
+}
+
+fun convertChar(originalChar: Char): Char {
+    // TODO: 2022-11-01
+    return 'a'
+}
+
