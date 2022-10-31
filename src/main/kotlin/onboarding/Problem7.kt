c8f8ea65eeac78572cddaec86f1f783de6520648
jjiiin <wjdwlin4051@gmail.com>
2022-10-31 22:11:49 +0900

    refactor: Problem5 1메소드 1기능 클린코드 원칙 구현 및 indent 2이하로 줄임.

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -7,13 +7,12 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    val userFriendList : ArrayList<String> = userFriendExtract(user,friends)
-    var friendsWithScore : HashMap<String, Int>
-
-    friendsWithScore = friendsWith10Points(user,userFriendList,friends)
-
-
-    friendsWithScore = friendsWith3Points(visitors, userFriendList, friendsWithScore)
+    val userFriendList : ArrayList<String> = arrayListOf()
+    userFriendExtract(user, friends, userFriendList)
+    var friendsWithScore : HashMap<String, Int> = hashMapOf()
+    
+    friendsWithIntersects(user,userFriendList,friends, friendsWithScore)
+    friendsWithScore = friendsWith1Points(visitors, userFriendList, friendsWithScore)
 
     var friendsWithScoreList = friendsWithScore.toList()
         .sortedWith(compareBy({-it.second}, {it.first}))
@@ -24,68 +23,78 @@ fun solution7(
     if(friendsWithScoreList.size > 5){
         friendsWithScoreList = friendsWithScoreList.slice(0..4)
         return friendsWithScoreList
-
-    }else{
-
-        return friendsWithScoreList
     }
+    return friendsWithScoreList
 }
 
-fun userFriendExtract(user: String, friends: List<List<String>>): ArrayList<String>{
-    val userFriendList = ArrayList<String>()
-
+fun userFriendExtract(user: String, friends: List<List<String>>,userFriendList: ArrayList<String> ){
     for ((i, _) in friends.withIndex()) {
-        if (friends[i].contains(user)) {
-            if (friends[i].indexOf(user) == 1) {
-                userFriendList.add(friends[i][0])
-            } else {
-                userFriendList.add(friends[i][1])
-            }
-        }
+        userFriendFind(friends[i], user, userFriendList)
     }
-    return userFriendList
 }
 
-fun friendsWith10Points(user : String, userFriends : ArrayList<String>, friends:List<List<String>>): HashMap<String, Int>{
-    val friendsWithScore : HashMap<String, Int> = hashMapOf()
-
-    for ((i, row) in friends.withIndex()) {
-        for ((j, _) in row.withIndex()) {
-
-            if (userFriends.contains(friends[i][j])) {
-                if (friends[i][abs(j - 1)] != user) {
-                    if (friendsWithScore.containsKey(friends[i][abs(j - 1)])) {
-                        val score = friendsWithScore.get(friends[i][abs(j - 1)])?.plus(10)
-                        if (score != null) {
-                            friendsWithScore[friends[i][abs(j - 1)]] = score
-                        }
-                    } else {
-                        friendsWithScore.put(friends[i][abs(j - 1)], 10)
-                    }
-                }
-            }
+fun userFriendFind(friends: List<String>, user:String, userFriendList: ArrayList<String>){
+    if (friends.contains(user)){
+        if (friends.indexOf(user) == 1) {
+            userFriendList.add(friends[0])
         }
+        if (friends.indexOf(user) == 0){
+            userFriendList.add(friends[1])
+        }
+    }
+}
+
+fun friendsWithIntersects(user : String,
+                          userFriends : ArrayList<String>,
+                          friends:List<List<String>>,
+                          friendsWithScore: HashMap<String, Int>): HashMap<String, Int>{
+    for ((i,_) in friends.withIndex()) {
+        friendIsUsers(friends[i], userFriends, user, friendsWithScore)
     }
     return friendsWithScore
 }
 
-fun friendsWith3Points(visitors: List<String>,userFriendList: ArrayList<String>,friendsWithScore : HashMap<String, Int>): HashMap<String, Int>{
-    val friendsWithScoreUpdate = friendsWithScore
+fun friendIsUsers(friends:List<String>,
+                   userFriendsList:ArrayList<String>,
+                   user:String,
+                   friendsWithScore: HashMap<String, Int>){
+    for (i in 0..friends.size-1) {
+        if (userFriendsList.contains(friends[i])) {
+            val friend = friends[abs(i - 1)]
+            friendsWith10Points(friend, user, friendsWithScore)
+        }
+    }
+}
+
+fun friendsWith10Points(friend:String,
+                        user:String,
+                        friendsWithScore: HashMap<String, Int>){
+    if (friend != user) {
+        if (friendsWithScore.containsKey(friend)) {
+            val score = friendsWithScore[friend]?.plus(10) ?: 0
+            friendsWithScore[friend]?.plus(score)
+        }
+        if (!friendsWithScore.containsKey(friend)) {
+            friendsWithScore.put(friend, 10)
+        }
+    }
+}
+
+fun friendsWith1Points(visitors: List<String>,
+                       userFriendList: ArrayList<String>,
+                       friendsWithScore : HashMap<String, Int>): HashMap<String, Int>{
 
     for (visitor in visitors) {
-
         if(userFriendList.contains(visitor)) {
             continue
         }
         if (friendsWithScore.containsKey(visitor)) {
-            val score = friendsWithScore.get(visitor)?.plus(3)
-            if (score != null) {
-                friendsWithScore[visitor] = score
-            }
-        } else {
-            friendsWithScore.put(visitor, 3)
+            friendsWithScore[visitor] = friendsWithScore[visitor]?.plus(1) ?:0
+        }
+        if (!friendsWithScore.containsKey(visitor)){
+            friendsWithScore.put(visitor, 1)
         }
     }
-    return friendsWithScoreUpdate
+    return friendsWithScore
 }
 
