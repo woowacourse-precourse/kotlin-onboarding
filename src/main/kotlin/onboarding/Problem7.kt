eabf9cdaaefd5ae68190d6bc3ebd29abc74b7eaf
no1msh <no1msh1217@gmail.com>
2022-11-01 01:46:43 +0900

    [Problem7][feat] 1번 기능 구현
    1. 맵 자료구조를 활용하여 친구목록을 정리하는 메소드 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,10 +5,41 @@ package onboarding
 // 2. 친구를 매개변수로 받아 추천 점수를 계산하는 메소드 구현
 // 3. 이미 친구인 사람을 제외 하고 추천 점수의 우열을 가려 최대 5명 정답리스트에 추가
 // 4. 만약 추천 점수가 같다면 이름순으로 정렬하여 리턴
+
+private val friendsMap: MutableMap<String, String> = HashMap()
+private val recommendedFriendsSet = HashSet<String>()
+private val recommendScoreMap: MutableMap<String, Int> = HashMap()
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    makeFriendsMap(friends)
+    TODO("리턴 값 미구현")
+}
+
+// 기능 목록 1번
+fun makeFriendsMap(friends: List<List<String>>) {
+    for (friendlyRelations in friends) {
+        val friend1 = friendlyRelations[0]
+        val friend2 = friendlyRelations[1]
+
+        if (friendsMap.containsKey(friend1)) {
+            if (!friendsMap[friend1]!!.contains(friend2)) {
+                friendsMap[friend1] += ".$friend2"
+            }
+        }
+        if (!friendsMap.containsKey(friend1)) {
+            friendsMap[friend1] = friend2
+        }
+
+        if (friendsMap.containsKey(friend2)) {
+            if (!friendsMap[friend2]!!.contains(friend1)) {
+                friendsMap[friend2] += ".$friend1"
+            }
+        }
+        if (!friendsMap.containsKey(friend2)) {
+            friendsMap[friend2] = friend1
+        }
+    }
 }
