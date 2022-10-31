7dec777ed4c415b6ff8db95e01ff6ee5823fa0f5
cyjadela <adelacho@naver.com>
2022-10-31 20:53:57 +0900

    feat: [문제6] email, name 받아오기

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,5 +3,11 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     val result = mutableSetOf<String>()
 
+    forms.forEachIndexed { _, value ->
+        val email = value[0]
+        val name = value[1]
+
+    }
+
     return result.sorted()
 }
