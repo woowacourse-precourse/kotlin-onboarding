b425fc10a42c18a6c739f36eb6bced7ef98b2713
dev-worthwhile <dev.lboram@gmail.com>
2022-10-31 14:10:44 +0900

    feat: (기능2) 숫자가 3, 6, 9 중 하나와 일치하는 지 확인 후 일치한다면 손뼉 횟수 추가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -11,6 +11,9 @@ fun solution3(number: Int): Int {
         val testNumber: String = i.toString() //3,6,9 포함되어있는 지 검증할 숫자
         for (index in testNumber.indices){
             val piece = testNumber[index].digitToInt() //숫자를 자리수대로 분리
+            if (piece%3==0 && piece!=0) { //한자리 숫자가 3,6,9 중 하나와 일치하면
+                clap++ //손뼉횟수 추가
+            }
         }
     }
     return clap
