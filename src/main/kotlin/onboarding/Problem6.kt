7379608efc33aa47a7598d8b4dd643a72b32b3f4
DormunDoorlock <skrudgh2001@naver.com>
2022-10-31 16:38:39 +0900

    feat: Problem6.kt 1번 기능 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -4,7 +4,7 @@ fun solution6(forms: List<List<String>>): List<String> {
     val result = mutableListOf<String>()
     val onlyOne = mutableListOf<Pair<Char,Char>>()
     val twoMore = mutableListOf<Pair<Char,Char>>()
-    //
+    // 1번. 각 닉네임 두 글자씩 pair로 나누고, 조건에 따라 처음 나온 pair, 두 번 이상 나온 pair로 두 개의 리스트에 나눠서 넣기
     for(i in forms.indices){
         val nickName = forms[i][1]
 
