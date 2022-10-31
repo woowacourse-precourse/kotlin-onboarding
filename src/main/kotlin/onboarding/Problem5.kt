7388a6a29543f5833e13cbcda038f90d11d109e8
junsu1023 <12jangdokg55@gmail.com>
2022-10-31 14:35:10 +0900

    Problem5 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,16 @@
 package onboarding
 
+import kotlin.collections.ArrayList
+
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val list = ArrayList<Int>()
+    var curMoney = money
+
+    val price = arrayListOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    price.forEach {
+        list.add(curMoney / it)
+        curMoney %= it
+    }
+
+    return list
 }
