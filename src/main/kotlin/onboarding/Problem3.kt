1b703354fea6c81442b40cb9d6ef1d72c0c07122
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-10-31 23:25:24 +0900

    [Problem3] feat: 숫자에 포함된 3, 6, 9의 갯수를 세는 함수 작성

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,24 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    //TODO
+    return 0
+}
+
+fun getTotalClaps(number: Int): Int{
+    // TODO: 2022-10-31
+    return 0
+}
+
+fun countThreeMultiple(number: Int): Int{
+    var currNum = number
+    var count = 0
+    while (currNum > 0){
+        val remainder = currNum % 10
+        currNum /= 10
+        if(remainder%3 == 0)
+            count++
+    }
+
+    return count
 }
