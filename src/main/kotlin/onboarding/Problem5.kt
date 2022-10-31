c957e1780a23b4329116eb4e1706ea31c4ae600e
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-11-01 00:53:27 +0900

    feat: (문제5) 돈을 화폐의 금액만큼 나누기 기능 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    return calculateMoney(money)
+}
+
+fun calculateMoney(money: Int): MutableList<Int>{
+    var m = money
+    val cash = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val result = mutableListOf<Int>(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    for(i in cash.indices){
+        result[i] = m / cash[i]
+        m %= cash[i]
+
+        if(m == 0){
+            break
+        }
+    }
+    return result
 }
