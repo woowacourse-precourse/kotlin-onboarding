dfac531a7540ce8771407d90d920a2728d612d33
krrong <khk9664@naver.com>
2022-10-31 22:44:26 +0900

    list를 활용하여 문제 해결

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,15 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var remainder = money
+    var answer : MutableList<Int> = MutableList(9, {0})
+    var divider = listOf<Int>(50000,10000,5000,1000,500,100,50,10,1)
+
+    for (i in divider.indices){
+        answer[i] = remainder/divider[i]
+        remainder %= divider[i]
+    }
+
+    return answer
 }
+
