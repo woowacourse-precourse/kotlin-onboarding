6590a979be56ace9a16fad4ce688ccd4a077c134
jjw0071 <jjw0071@naver.com>
2022-10-31 12:56:25 +0900

    problem2 done

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,9 +1,37 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
-    var a = "abcd"
-    a= ""
+    //TODO("프로그램 구현")
+   // var temp :arrayOf<Char?> = cryptogram.toCharArray()
+    var char = arrayOfNulls<Char>(cryptogram.length)
+    for( i in cryptogram.indices)
+        char[i] = cryptogram[i]
+    var length : Int = char.size
+    var check : Boolean = false
+    var cLeng : Int = 0
+    var howmanytimes : Int = char.size-2
 
-    return a
+    while(!check){
+        check = true
+        for(i : Int in 0..howmanytimes){
+            if(char[i] == char[i+1]){
+                check = false
+                for(j : Int in i+2..howmanytimes+1){
+                    char[j-2] = char[j]
+                }
+                var newchar = arrayOfNulls<Char>(howmanytimes)
+                for(j : Int in 0..howmanytimes-1){
+                    newchar[j] = char[j]
+                }
+
+                char = newchar
+                howmanytimes = howmanytimes - 2
+                break
+            }
+        }
+    }
+    val answer = char.joinToString("")
+
+    return answer
 }
+
