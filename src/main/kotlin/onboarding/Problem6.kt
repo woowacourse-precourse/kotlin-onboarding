6f29be213898a5f0538f070a772cbc433a286ff4
junsu1023 <12jangdokg55@gmail.com>
2022-10-31 14:35:16 +0900

    Problem6 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,26 @@
 package onboarding
 
+import java.util.*
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val list = TreeSet<String>()
+
+    for(i in forms.indices) {
+        val curName = forms[i][1]
+
+        for(j in i + 1 until forms.size) {
+            val otherName = forms[j][1]
+
+            for(k in 2 .. curName.length) {
+                val nameSplit = curName.substring(0, k)
+
+                if(otherName.contains(nameSplit)) {
+                    list.add(forms[i][0])
+                    list.add(forms[j][0])
+                }
+            }
+        }
+    }
+
+    return list.toList()
 }
