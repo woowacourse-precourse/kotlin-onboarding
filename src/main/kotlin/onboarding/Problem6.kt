f6e2deaeefba6797a94ef7d107562649eefc65cd
s9hn <tnghqn@naver.com>
2022-11-01 03:22:21 +0900

    [FEAT(미션6)] 기능 구현 및 분기처리
    1. 리스트 담을 해쉬맵 구현
    2. 리스트의 이름, 2글자씩 파싱
    3. 파싱값과 리스트의 이름 대조 및 리스트 대입
    4. 리스트 중복제거 및 정렬

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,33 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+
+    val hash = hashMapOf<String, String>()
+    var name = listOf<String>()
+    val email = mutableListOf<String>()
+
+    for (i in forms.indices) {
+
+        name += listOf(forms[i][1])
+        hash[forms[i][1]] = forms[i][0]
+
+    }
+
+    for (j in name.indices) {
+
+        for (k in 0 until name[j].length - 1) {
+
+            for (l in name.indices) {
+
+                if ((name[l] != name[j]) and name[l].contains(name[j].substring(k, k + 2))) {
+                    email += hash[name[l]] ?: ""
+
+                    continue
+                }
+
+            }
+        }
+    }
+
+    return email.distinct().sorted()
 }
