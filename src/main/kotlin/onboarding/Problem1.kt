93050a28f0321f492646379dec2aabababdbfd26
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-10-31 18:58:47 +0900

    [Problem1] feat: input 값이 유요한지 검사하는 함수 작성

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,38 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if(!checkInputException(pobi) || !checkInputException(crong))
+        return -1
+
+    return 0
+}
+
+fun checkInputException(pageList: List<Int>): Boolean{
+    if(pageList[1] - pageList[0] != 1)
+        return false
+    if(pageList[0] < 3 || pageList[0] > 397)
+        return false
+    if(pageList[0] % 2 == 0)
+        return false
+    return true
+}
+
+fun sumPageNum(pageNum: Int): Int{
+    // TODO: 2022-10-31
+    return 0
+}
+
+fun multiplyPageNum(pageNum: Int): Int{
+    // TODO: 2022-10-31
+    return 0
+}
+
+fun getMax(numbers: List<Int>): Int{
+    // TODO: 2022-10-31
+    return 0
+}
+
+fun getWinner(pobiMax:Int, crongMax:Int): Int{
+    // TODO: 2022-10-31
+    return 0
 }
