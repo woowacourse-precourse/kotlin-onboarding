b8c33be36daf2ab7070104bee5ec671177d549c1
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-10-31 20:37:11 +0900

    1부터 주어진 숫자까지 자리수를 쪼개 리스트로 바꾸고 countclap 실행하는 함수

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -4,14 +4,20 @@ package onboarding
 //1. 1부터 주어진 숫자까지 자리수를 쪼개 리스트로 바꾼다.
 //2. 리스트들에 3 6 9가 포함 되면 그 수 만큼 박수를 카운팅 한다.
 fun solution3(number: Int): Int {
-    var clap = 0
+    return (castlist(number))
+}
 
+//1부터 주어진 숫자까지 자리수를 쪼개 리스트로 바꾸고 countclap 실행하는 함수
+fun castlist(number:Int):Int{
+    var numberlist:List<Int> = listOf()
+    var clap = 0
     for(i:Int in 1 .. number){
-        val numberlist = i.toString().map{it.digitToInt()}  //주어진 숫자까지 자리수를 쪼개 리스트로 바꾼다.
+        numberlist = i.toString().map{it.digitToInt()}  //주어진 숫자까지 자리수를 쪼개 리스트로 바꾼다.
         clap += countclap(numberlist)
     }
     return clap
 }
+
 
 //숫자 당 박수 카운트 함수
 fun countclap(numberlist: List<Int>):Int{
