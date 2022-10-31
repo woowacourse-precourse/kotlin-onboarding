95befffbf0c8d8c771a896b286e5c11d7cda09b1
yulim <yulim13sky@naver.com>
2022-11-01 01:06:07 +0900

    [REFACTOR] Problem6 checkNickName 함수 분리

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -4,16 +4,21 @@ fun solution6(forms: List<List<String>>): List<String> {
     val result = mutableSetOf<String>()
 
     for (indexList in forms.indices) {
-        val name = forms[indexList][1]
         val email = forms[indexList][0]
-        for (nameIndex in 0 until name.length - 1) {
-            val twoLetters = name.substring(nameIndex, nameIndex + 2)
-            for (index in forms.indices) {
-                if (forms[index][1].contains(twoLetters) && indexList != index) {
-                    result.add(email)
-                }
+        if (checkNickName(indexList, forms)) result.add(email)
+    }
+    return result.toList().sorted()
+}
+
+private fun checkNickName(indexList: Int, forms: List<List<String>>): Boolean {
+    val name = forms[indexList][1]
+    for (nameIndex in 0 until name.length - 1) {
+        val twoLetters = name.substring(nameIndex, nameIndex + 2)
+        for (index in forms.indices) {
+            if (forms[index][1].contains(twoLetters) && indexList != index) {
+                return true
             }
         }
     }
-    return result.toMutableList().sorted()
+    return false
 }
