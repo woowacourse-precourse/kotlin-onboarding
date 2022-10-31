d9f6950e04ea364e9ac0221c67b77d8f9d1f7f30
Gloria <ippnsj@gmail.com>
2022-11-01 02:46:54 +0900

    modify(Problem6): 리스트 길이 에러메시지 변경

--- src/main/kotlin/problem6/ListSizeVerifier.kt
+++ src/main/kotlin/problem6/ListSizeVerifier.kt
@@ -5,7 +5,7 @@ import problem1.Verifier
 class ListSizeVerifier(private val min:Int, private val max:Int) : Verifier<List<Any>>() {
     override fun verify(input:List<Any>) {
         if(input.size !in min..max) {
-            throw RuntimeException("Input length is not between range ${this.min} and ${this.max}")
+            throw RuntimeException("Input size is not between range ${this.min} and ${this.max}")
         }
     }
 }
