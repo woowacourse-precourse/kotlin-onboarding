f3a8b855c932bf120630713396d9722475142d01
jklom0326 <jklom0326@gmail.com>
2022-10-31 20:40:25 +0900

    기능 목록 및 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,52 +1,21 @@
 package onboarding
+/*
+
+    기능 목록
+    1. 양쪽 페이지를 비교해서 가장 큰 점수를 반환하는 함수 exceptions
+    3. 배열의 곱을 구하는 식이 반복되어 함수 multiply
+    2. 예외 사항에 대한 처리를 위한 함수 score
+
+*/
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     // 예외 처리
-    if (!exceptions(pobi, crong)) return -1
+    if (!exceptions(pobi) || !exceptions(crong)) return -1
+    // 점수 측정
     val pobiScore = score(pobi)
     val crongScore = score(crong)
+    // 같으면 결과값
     if (pobiScore == crongScore) return 0
-
+    // 점수를 비교
     return if (pobiScore > crongScore) 1 else 2
-}
-
-private fun score(score: List<Int>): Int {
-    val left = score[0].toString()
-        .map{ it.digitToInt() }
-    val right = score[1].toString()
-        .map { it.digitToInt() }
-    val leftSum = left.sumOf { it }
-    val rightSum = right.sumOf { it }
-    var leftMul = 1
-    var rightMul = 1
-
-    for (i in left) {
-        leftMul *= i
-    }
-    for (i in left) {
-        rightMul *= i
-    }
-
-    return (listOf(leftSum, rightSum, leftMul, rightMul).maxOf { it })
-}
-
-private fun exceptions(pobi: List<Int>, crong: List<Int>): Boolean {
-    when {
-        // 왼쪽페이지가 홀수, 오른쪽 페이지가 짝수가 아닐 때
-        pobi[0] % 2 == 0 -> return false
-        pobi[1] % 2 != 0 -> return false
-        crong[0] % 2 == 0 -> return false
-        crong[1] % 2 != 0 -> return false
-
-        // 페이지가 연속이 아닐 때
-        pobi[0] + 1 != pobi[1] -> return false
-        crong[0] + 1 != crong[1] -> return false
-
-        // 페이지가 시작면이나 마지막이 아닐 때
-        pobi[0] !in 2..399 -> return false
-        pobi[1] !in 2..399 -> return false
-        crong[0] !in 2..399 -> return false
-        crong[1] !in 2..399 -> return false
-    }
-    return true
 }
