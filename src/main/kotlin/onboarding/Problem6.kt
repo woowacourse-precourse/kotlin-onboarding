05af073f9d95634a3f262d6be4cbd9543ac356cf
권용민 <briandr@naver.com>
2022-11-01 05:28:54 +0900

    [Problem6] 전체 닉네임을 탐색하여 사전에 key, value를 등록한다.

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -2,4 +2,15 @@ package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
     val emailDictionary = mutableMapOf<String, MutableSet<String>>()
+    makeEmailDictionary(emailDictionary, forms)
+}
+
+private fun makeEmailDictionary(emailDictionary: MutableMap<String, MutableSet<String>>, forms: List<List<String>>) {
+    for (i in forms.indices) {
+        val nickName = forms[i][1]
+        for (idx in 0..nickName.length - 2) {
+            val part = nickName.slice(IntRange(idx, idx + 1))
+            emailDictionary[part]?.add(forms[i][0]) ?: emailDictionary.put(part, mutableSetOf(forms[i][0]))
+        }
+    }
 }
