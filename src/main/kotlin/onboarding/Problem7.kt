7a79a107e76dabefcf0cbbf34bc0dc8549f6f8d7
changhwan <chang5hustle@gmail.com>
2022-10-31 18:46:07 +0900

    [Problem 7] (요구사항 3) 친구 후보자 score  map 생성

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -7,6 +7,7 @@ fun solution7(
 ): List<String> {
     val peopleSet = getPeopleSet(user, friends, visitors)
     val friendCandidateSet = excludeAlreadyFriendAndUser(peopleSet, user, friends)
+    val candidateScoreMap = getCandidateScoreMap(friendCandidateSet)
 }
 
 private fun getPeopleSet(user: String, friends: List<List<String>>, visitors: List<String>): MutableSet<String> {
@@ -28,4 +29,11 @@ private fun excludeAlreadyFriendAndUser(
     for (i in friends) if (user in i) peopleSet.removeAll(i.toSet())
 
     return peopleSet
+}
+
+private fun getCandidateScoreMap(friendCandidateSet: MutableSet<String>): MutableMap<String, Int> {
+    val candidateScoreMap = mutableMapOf<String, Int>()
+    for (i in friendCandidateSet) candidateScoreMap[i] = 0
+
+    return candidateScoreMap
 }
