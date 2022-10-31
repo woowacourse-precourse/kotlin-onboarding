244778ddaf09043e2c89f58d84673e09cdc83803
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-10-31 21:28:53 +0900

    refactor(Problem6): 코드 리팩토링
    변수명 변경, 복잡성을 줄이기 위해 for문 리팩토링

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,14 +3,14 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     val result: HashSet<String> = hashSetOf()
     for (i in forms.indices) {
-        val name = forms[i][1]
-        for (j in 0..name.length - 2) {
-            val tmp = name.substring(j, j + 2)
-            for (k in forms.indices) {
-                if (forms[k][1].contains(tmp)) {
-                    if (forms[k][0] != forms[i][0]) {
-                        result.add(forms[k][0])
-                    }
+        val targetName = forms[i][1]
+        for (j in 0..targetName.length - 2) {
+            val targetString = targetName.substring(j, j + 2)
+            for (k in i until forms.size) {
+                val nameToCheck = forms[k][1]
+                if (nameToCheck.contains(targetString)) {
+                    val emailToCheck = forms[k][0]
+                    result.add(emailToCheck)
                 }
             }
         }
