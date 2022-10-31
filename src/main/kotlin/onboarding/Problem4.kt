0528c6f6d82b5aeb76e417ba345d5f38ed058928
yulim <yulim13sky@naver.com>
2022-10-31 20:11:29 +0900

    [COMMENT] Problem4 기능 목록 추가

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,12 +1,19 @@
 package onboarding
 
+/**
+기능 목록
+- 청개구리 사전 순으로 문자 변환 map 만들기 (ex. mapping['A'] = 'Z', mapping['a'] = 'z')
+- word를 돌면서 알파벳인 경우에는 answer에 변환한 문자 추가, 그 외에는 그대로 문자 추가
+- makeMap : 청개구리 사전으로 mapping 시키기
+ */
+
 const val TO_LOWER_CASE = 32
 const val MAPPING_STEP = 25
 
 fun solution4(word: String): String {
     var answer = ""
     val upperAlphabet = listOf('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-    val mapping: MutableMap<Char, Char> = mutableMapOf()
+    val mapping = mutableMapOf<Char, Char>()
 
     makeMap(upperAlphabet, mapping)
 
@@ -19,7 +26,6 @@ fun solution4(word: String): String {
     return answer
 }
 
-// mapping 시키기
 private fun makeMap(upperAlphabet: List<Char>, mapping: MutableMap<Char, Char>) {
     for (i in upperAlphabet.indices) {
         mapping[upperAlphabet[i]] = upperAlphabet[MAPPING_STEP - i]
