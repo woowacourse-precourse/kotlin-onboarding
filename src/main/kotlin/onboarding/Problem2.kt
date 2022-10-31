d9950625e8775f2dd2306bcc840101d166570f78
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-10-31 21:20:00 +0900

    [Problem2] feat: 현재 암호문이 중복이 모두 제거된 암호문인지 검사하는 함수 작성

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,7 @@
 package onboarding
 
+import java.util.Stack
+
 fun solution2(cryptogram: String): String {
     return ""
 }
@@ -13,4 +15,13 @@ fun isLowerCase(char: Char): Boolean{
     return char.isLowerCase()
 }
 
-
+fun isCompletedCrypto(cryptogram: String): Boolean{
+    val stack:Stack<Char> = Stack()
+    stack.push(cryptogram[0])
+    for(i in 1 until cryptogram.length){
+        if(stack.peek() == cryptogram[i])
+            return false
+        stack.push(cryptogram[i])
+    }
+    return true
+}
