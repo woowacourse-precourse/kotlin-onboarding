1d8b3ec505d2631cf15d658e8fbca0cfb3ebbaa5
SeungHyeon Shim <tladhehd@naver.com>
2022-10-31 14:42:52 +0900

    Problem3_commit

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,15 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var answer = 0
+    for (i in 1..number) {
+        var num: String = "" + i
+        for (j in 0 until num.length) {
+            var partStr = num.substring(j, j+1)
+            if (partStr.equals("3") || partStr.equals("6") || partStr.equals("9")) {
+                answer+=1
+            }
+        }
+    }
+    return answer
 }
