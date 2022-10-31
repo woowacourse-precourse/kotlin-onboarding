a7511e23bfb8cc4a0a255d4a01c0a7ddcf7c948e
Suheon Song <ssheon0812@naver.com>
2022-11-01 02:40:28 +0900

    style: [Problem4] 함수 위치 변경

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,12 +1,17 @@
 package onboarding
 
+
+fun solution4(word: String): String {
+    return translate(word)
+}
+
 fun convert(character: Char): Char {
     // 소문자일 때
-    if ('a' <= character && character <= 'z') {
+    if (character in 'a'..'z') {
         return 'a' + (25 - (character - 'a')) % 26
     }
     // 대문자일 때
-    else if ('A' <= character && character <= 'Z'){
+    else if (character in 'A'..'Z') {
         return 'A' + (25 - (character - 'A')) % 26
     } else {
         return character
@@ -15,8 +20,4 @@ fun convert(character: Char): Char {
 
 fun translate(word: String): String {
     return word.map { char -> convert(char) }.joinToString(separator = "")
-}
-
-fun solution4(word: String): String {
-    return translate(word)
 }
