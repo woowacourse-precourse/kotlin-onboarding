7d033ce59c66e751947fb7b27fd09750d62843be
jjiiin <wjdwlin4051@gmail.com>
2022-10-31 22:07:57 +0900

    refactor: Problem2 3개 이상 연속된 문자 제거하는 코드 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -2,36 +2,47 @@ package onboarding
 
 fun solution2(cryptogram: String): String {
     var cryptogramResult = cryptogram
-    var decodeResult=decodeString(cryptogram)
+    var decodeResult: String
+
 
     while(true){
-        if (cryptogramResult != decodeResult){
+        decodeResult = decodeString(cryptogramResult)
+
+        if (!cryptogramResult.equals(decodeResult)){
             cryptogramResult = decodeResult
-        }else{
+            continue
+        }
+        if (cryptogramResult.equals(decodeResult)){
             break
         }
-        decodeResult = decodeString(decodeResult)
-    }
-    return decodeResult
 
-}
-fun decodeString(cryptogram:String) : String{
-    var cryptogramResult = cryptogram
-    var count=0
-
-    for (i in 0..cryptogram.length - 2 step (1)){
-        if (cryptogram[i] == cryptogram[i+1]){
-            ++count
-        }
-
-        if(count>0){
-
-            val subString= cryptogram.substring(i until i+count+1)
-            cryptogramResult=cryptogramResult.replace(subString,"")
-            break
-        }
     }
 
     return cryptogramResult
 }
+
+fun decodeString(cryptogram:String) : String{
+    var cryptogramResult  = ""
+    var count = 0
+
+    for ( i in 1.. cryptogram.length-1){
+        if(cryptogram[i-1] != cryptogram[i]){
+            if (count > 0 ){
+                cryptogramResult += cryptogram.substring(i,cryptogram.length)
+                break
+            }
+            cryptogramResult += cryptogram[i-1].toString()
+        }
+        if(cryptogram[i - 1] == cryptogram[i]){
+               count++
+        }
+    }
+
+    if(count == 0){
+        return cryptogram
+    }
+
+    return cryptogramResult
+}
+
 
