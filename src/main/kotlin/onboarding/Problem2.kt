581e968f5a5862dc32d8c7b445d32fb000485158
jklom0326 <jklom0326@gmail.com>
2022-10-31 20:48:39 +0900

    Problem2 decode 함수 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -15,3 +15,15 @@ fun solution2(cryptogram: String): String {
     }
     return after
 }
+
+private fun decode(cryptogram: String): String {
+    val str = cryptogram.map { it.toString() }
+        .toMutableList()
+    for (i in 0 until str.size - 1) {
+        if (str[i] == str[i + 1]) {
+            str[i] = ""
+            str[i + 1] = ""
+        }
+    }
+    return str.joinToString("")
+}
