a4a193d3f4a32c25bd96e8bec8f3c0ae2be3843d
hyungmin <utopia96@hanmail.net>
2022-10-31 23:01:59 +0900

    feat: 두번째 문제 solution 작성 및 검증

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,30 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var flag: Boolean
+    var res = cryptogram
+    while (true) {
+        flag = false
+        for (i in 0 until res.length - 1) {
+            if (res[i] == res[i + 1]) {
+                if (i != 0) {
+                    val front = res.slice(IntRange(0, i - 1))
+                    val rear = res.slice(IntRange(i + 2, res.length - 1))
+                    flag = true
+                    res = front + rear
+                    break
+                } else if (i == 0) {
+                    val rear = res.slice(IntRange(i + 2, res.length - 1))
+                    flag = true
+                    res = rear
+                    break
+                }
+            }
+
+        }
+        if (!flag) {
+            break
+        }
+    }
+    return res
 }
