fe27eb2767d9924fe8d456d971b2fd4fe039f740
lh99j <grand2181@gmail.com>
2022-10-31 23:49:49 +0900

    [lh99j] Solve Problem4

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,23 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var str = ""
+    for(element in word){
+        str += getAscIICode(element)
+    }
+
+    return str
+}
+
+fun getAscIICode(num : Char) : Char{
+    val temp : Int = num.code
+
+    return if(num == ' ')
+        ' '
+    else if(temp in 65..90)
+        (155 - temp).toChar()
+    else if(temp in 97 .. 122)
+        (219 - temp).toChar()
+    else
+        num
 }
