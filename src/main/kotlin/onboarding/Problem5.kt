9b024821ea2da5bdb5a1232ba299aec22164390a
jjiiin <wjdwlin4051@gmail.com>
2022-10-31 22:10:06 +0900

    chore: Problem5 코틀린 컨벤션 지키기

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -2,11 +2,10 @@ package onboarding
 
 fun solution5(money: Int): List<Int> {
     val cashCountList = ArrayList<Int>()
-    var moneyInAccount=money
+    var moneyInAccount = money
     val cashList :ArrayList <Int> = arrayListOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
 
     for (cash in cashList){
-
         cashCountList.add(moneyInAccount/cash)
         moneyInAccount -= cash * (moneyInAccount/cash)
 
