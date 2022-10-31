8c60065f5de3d12a7f047d34810d36f4594a82f8
권용민 <briandr@naver.com>
2022-11-01 04:26:53 +0900

    [Problem4] 위에서 만든 사전으로 입력된 문자열 변환  - 알파벳 외의 문자는 변환하지 않는다.  - 대문자일 경우 사전으로 변환한다.  - 소문자일 경우 대문자로 변환 후 사전으로 변환하고 다시 소문자로 변환한다.

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,10 +3,23 @@ package onboarding
 fun solution4(word: String): String {
     val frogDictionary = mutableMapOf<Char, Char>()
     makeDictionary(frogDictionary)
+    return convertWord(word, frogDictionary)
 }
 
 private fun makeDictionary(frogDictionary: MutableMap<Char, Char>) {
     for (i in 0..25) {
         frogDictionary[(65 + i).toChar()] = (90 - i).toChar()
     }
+}
+
+private fun convertWord(word: String, frogDictionary: MutableMap<Char, Char>): String {
+    var result: String = ""
+    for (c in word) {
+        result += when (c) {
+            in 'A'..'Z' -> frogDictionary[c]
+            in 'a'..'z' -> frogDictionary[c.uppercaseChar()]?.lowercaseChar()
+            else -> c
+        }
+    }
+    return result
 }
