046359d845da1f032ed581dac14c6ba7cc2ec9da
hyungmin <ramkid91@naver.com>
2022-11-01 17:53:44 +0900

    feat: 일곱번째 문제 해결 - Map 을 이용하여 이름, 점수를 관리함 - 먼저 각 이름에 대해 vistors, friends를 순회하면서 점수를 처리하고, 이미 친구관계인 경우 map에서 remove함 - 로직의 적용 순서는 추후 수정 고려

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,53 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val recommendFriendScore = mutableMapOf<String,Int>()
+    for(visitor in visitors){
+        if(recommendFriendScore.containsKey(visitor)){
+            recommendFriendScore[visitor] = recommendFriendScore[visitor]!! + 1
+        }
+        else{
+            recommendFriendScore[visitor] = 1
+        }
+    }
+    for(friend in friends){
+        var realFriend = ""
+
+        //user와 친구관계
+        if(friend[0]==user){
+            realFriend = friend[1]
+        }
+        else if(friend[1]==user){
+            realFriend = friend[0]
+        }
+        println(realFriend)
+        if(recommendFriendScore.containsKey(realFriend)){
+            recommendFriendScore.remove(realFriend)
+        }
+        for(recommendFriend in friends){
+            //user의 realFriend의 friend type1
+            if(recommendFriend[0]==realFriend && recommendFriend[1]!=user){
+                //이미 map에 등록되어 있으면 score만 ++
+                if(recommendFriendScore.containsKey(recommendFriend[1])){
+                    recommendFriendScore[recommendFriend[1]] = recommendFriendScore[recommendFriend[1]]!! + 10
+                }
+                else {
+                    recommendFriendScore.put(recommendFriend[1], 10)
+                }
+            }
+            else if(recommendFriend[0]!=user && recommendFriend[1]==realFriend){
+                if(recommendFriendScore.containsKey(recommendFriend[0])){
+                    recommendFriendScore[recommendFriend[0]] = recommendFriendScore[recommendFriend[0]]!! + 10
+                }
+                else {
+                    recommendFriendScore[recommendFriend[0]] = 10
+                }
+            }
+        }
+    }
+
+    val res = recommendFriendScore.toList().sortedWith(compareByDescending<Pair<String, Int>> { it.second }.thenComparing(
+        compareBy { it.first })).toMap()
+    return res.keys.toList()
+
 }
