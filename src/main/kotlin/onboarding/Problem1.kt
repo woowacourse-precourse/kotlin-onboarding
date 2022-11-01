c0ffc82907856da84641f2ca7add859cb3921739
lsakee <lsls4868@gmail.com>
2022-11-01 10:54:45 +0900

    Feat : findpageMaxCalculation() 메서드 추가 -숫자를 문자열로 변환하여 저장하기 위하여 List에 담긴 숫자를 문자열로 저장하는 기능 -calculatePage() 메서드를 이용하여 user의 page계산 최대값을 저장

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -7,14 +7,27 @@ import java.util.*
 //숫자를 문자로 전환하여 분리
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    var result: Int = 0
-    return result
-}
 
-fun calculatePage(StringConversion: String): Int {
-    var PlusNum = 0
-    var MultiplyNum = 1
-    var maxValue: Int
+    var result: Int =0
+
+    return result
+
+}
+fun findpageMaxCalculation(userOpenPage: List<Int>):List<Int>{
+
+    val userPageMaxValuelist = mutableListOf<Int>()
+
+    for (i in 0..userOpenPage.size - 1) {
+        //숫자를 문자열로 변경하여 수를 분리하기 위하여 문자열로 저장
+        val StringConversion: String = userOpenPage[i].toString()
+        userPageMaxValuelist.add(calculatePage(StringConversion))
+    }
+    return userPageMaxValuelist
+}
+fun calculatePage(StringConversion:String):Int{
+    var PlusNum=0
+    var MultiplyNum=1
+    var maxValue:Int
     for (j in 0..StringConversion.length - 1) {
         //문자열로 분리하여 양쪽페이지의 최대 값을 저장 ex) 2 2 7
         val stringNumber = StringConversion.substring(j until j + 1).toInt()
