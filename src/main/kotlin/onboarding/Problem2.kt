958d15ad538562018efd6ca5a98836af84d22e12
JinJoa <mitejoa@gmail.com>
2022-11-01 19:58:34 +0900

    문제2 -

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,24 @@
 package onboarding
 
+fun main(){
+    solution2("browoanoommnaon")
+}
+
+
+//암호문. 연속되는 중복 알파벳 제거
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var decryption :String = cryptogram
+    // 중복 글자 없을때까지 무한 반복
+    while (true){
+        for (i in 0..decryption.length){
+            if(decryption[i] == decryption[i+1])
+                println(decryption[i])
+                //중복된값 삭제
+                println(decryption)
+
+        }
+    //    if() { //중복글자 없음
+            return decryption //해독문 리턴
+    //    }
+    }
 }
