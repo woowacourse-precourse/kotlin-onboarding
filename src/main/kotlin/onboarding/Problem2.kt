4dd74c2b70ccab2ff54a2f95e60c93ce3ae6ad09
lh99j <grand2181@gmail.com>
2022-11-01 15:47:51 +0900

    Modify Problem 2

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -7,19 +7,24 @@ fun solution2(cryptogram: String): String {
     var strStack : ArrayDeque<Char> = ArrayDeque<Char>()
     var answer : String= ""
 
+
     strStack.addLast(str[0])
 
+    var lastValue : Char =' '
     for(i in 1 until str.length){
-        val temp = str[i]
+        val currentValue = str[i]
 
-        if(strStack.isNotEmpty() && strStack.peekLast() != temp)
-            strStack.addLast(temp)
+        if(strStack.isNotEmpty() && strStack.peekLast() != currentValue && currentValue != lastValue)
+            strStack.addLast(currentValue)
 
-        else if(strStack.isNotEmpty() && strStack.peekLast() == temp)
+        else if(strStack.isNotEmpty() && (strStack.peekLast() == currentValue || strStack.peekLast() == lastValue))
             strStack.removeLast()
 
-        else if(strStack.isEmpty())
-            strStack.addLast(temp)
+        else if(strStack.isEmpty() && currentValue != lastValue )
+            strStack.addLast(currentValue)
+
+        lastValue = str[i]
+
 
     }
 
