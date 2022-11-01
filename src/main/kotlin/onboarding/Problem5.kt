aba7da27be41ea9fa8dcdde666fc6ac673ad9199
JinJoa <mitejoa@gmail.com>
2022-11-01 21:06:03 +0900

    문제5 - 필요한 list, 변수 만들기

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,15 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    //화폐단위 리스트
+    //만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전
+    val unit_list : List<Int> = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    //단위당 각 개수
+    var cnt_unit = 0
+    //남은돈
+    var res_mony = money
+    //반환할 값
+    var return_list = mutableListOf<Int>()
+
+    return return_list
 }
