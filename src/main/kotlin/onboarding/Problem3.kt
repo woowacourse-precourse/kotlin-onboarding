58b7fb2d893074fca84bdda26ae345a04a57af51
권용민 <briandr@naver.com>
2022-11-01 15:29:45 +0900

    [Problem3] 숫자의 자릿수를 분리

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -3,3 +3,13 @@ package onboarding
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
+
+private fun separateDigit(number: Int): List<Int> {
+    var input = number
+    val digitList = mutableListOf<Int>()
+    while (input != 0) {
+        digitList.add(input % 10)
+        input /= 10
+    }
+    return digitList
+}
