79082ad5403f195bd312cd0eb828d11e2c2549df
Yuhee Kim <dbgml4509@naver.com>
2022-11-01 21:04:31 +0900

    feat : 청개구리 사전 참고해 엄마 말씀 변환

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,35 @@
 package onboarding
 
+//기능 목록
+/*
+    1. 엄마 말씀 word의 알파벳을 청개구리 사전을 참고해 변환
+    2. 알파벳 대문자의 경우 대문자로, 소문자의 경우 소문자로 변환
+ */
+//예외 사항
+/*
+    1. word가 알파벳 외의 문자인 경우
+ */
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    //프로그램 구현
+    return replaceWithDic(word)
+}
+
+fun replaceWithDic(word: String): String{
+//    val result = ""
+    val builder = StringBuilder()
+
+    for(i in word){
+        //1. 엄마 말씀 word의 알파벳을 청개구리 사전을 참고해 변환
+        if(i.isUpperCase()){ //2. 대문자의 경우 대문자로 변환
+            builder.append((155 - i.toInt()).toChar())
+        }
+        if(i.isLowerCase()){ //2. 소문자의 경우 소문자로 변환
+            builder.append((219 - i.toInt()).toChar())
+        }
+        if(i.isWhitespace()){
+            print(i)
+            builder.append(" ")
+        }
+    }
+    return builder.toString()
 }
