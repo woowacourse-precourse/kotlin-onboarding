96ac66292b760387f2a3029af6d22f386c2e33d5
권용민 <briandr@naver.com>
2022-11-01 16:08:43 +0900

    [Problem2] 중복된 문자 제거  - 문자열을 첫번째 인덱스부터 마지막 인덱스까지 검사하여 중복되지 않은 문자만 저장한다.

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -3,3 +3,19 @@ package onboarding
 fun solution2(cryptogram: String): String {
     TODO("프로그램 구현")
 }
+
+private fun checkRepetition(secretCode: String) {
+    val stack = mutableListOf<Char>()
+    var previousChar: Char = ' '
+    var isFinished: Boolean = true
+
+    for (currentChar in secretCode) {
+        if (previousChar != currentChar) {
+            stack.add(currentChar)
+            previousChar = currentChar
+            continue
+        }
+        if (previousChar == stack.lastOrNull()) stack.removeLast()
+        isFinished = false
+    }
+}
