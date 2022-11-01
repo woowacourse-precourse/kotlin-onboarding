d7067adcbf454c92eee9ed2e040ebfe0ba8b1b3b
healthykim <bsbs8645@snu.ac.kr>
2022-11-01 16:48:20 +0900

    feat(Problem7): Add function for making a graph of friends
    Added function to make a graph of friends.
    Map structure is used to find the friends of user quickly.

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,5 +1,24 @@
 package onboarding
 
+import java.util.*
+import kotlin.collections.HashSet
+
+/**
+ * Returns a graph of [friends].
+ */
+fun makeFriendGraph(friends: List<List<String>>): Map<String, Set<String>> {
+    val result = mutableMapOf<String, HashSet<String>>()
+    for(relation in friends) {
+        val friendA = relation[0]
+        val friendB = relation[1]
+        if(result[friendA] == null) result[friendA] = hashSetOf(friendB)
+        else result[friendA]?.add(friendB)
+        if(result[friendB] == null) result[friendB] = hashSetOf(friendA)
+        else result[friendB]?.add(friendA)
+    }
+    return result
+}
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
