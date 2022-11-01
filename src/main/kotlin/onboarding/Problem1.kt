dd04d893f50ec2ca6507a9db8476df9a6d3f0e29
lkeonwoo94 <lkeonw0o@naver.com>
2022-11-01 20:08:22 +0900

    Prob1 - Add Functional Requirements List

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -3,3 +3,29 @@ package onboarding
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
+
+/**
+ * 1. Returns max(add, multiply) [left page]
+ */
+
+/**
+ * 2. Returns max(add, multiply) [right page]
+ */
+
+ /**
+  * 3. Returns max(add, multiply) [both page]
+  */
+
+/**
+ * 4. Returns winner [player]
+ */
+
+ /**
+  * @Restriction
+  * 1. pobi, crong: except first,last page [1, 400]
+  * 2. pobi, crong: length = 2
+  * 3. pobi, crong: [left, right] page
+  * 4. left : odd number
+  * 5. right : even number
+  * 6. right = left + 1
+  */
