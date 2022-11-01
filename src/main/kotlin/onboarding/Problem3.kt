c6029b329a33b9bcd0e404d475cdead3200cff29
lkeonwoo94 <lkeonw0o@naver.com>
2022-11-01 21:32:06 +0900

    solve Prob3

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,9 +1,13 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var ret = 0
+    for (i in 1..number)
+    {
+        var num_s = i.toString()
+        for (k in 0..(num_s.length-1))
+            if (num_s[k].digitToInt() % 3 == 0 && num_s[k].digitToInt() != 0)
+                ret++
+    }
+    return ret
 }
-
-/* numberarray Int to String [number]*/
-
-/* Calc mod 3 , each number in numberarray */
