b6a31a9612a5d06be37f8083c006a5d32fb1f803
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-01 19:02:56 +0900

    [Problem7] feat: 유효한 입력값인지 검사하는 함수 작성

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,48 @@
 package onboarding
 
-fun solution7(
-    user: String,
-    friends: List<List<String>>,
-    visitors: List<String>
-): List<String> {
-    TODO("프로그램 구현")
+import java.util.regex.Pattern
+
+fun solution7(user: String, friends: List<List<String>>, visitors: List<String>): List<String> {
+    isValidInput(user, friends, visitors)
+
+    // TODO: 2022-11-01  
+}
+
+fun isValidInput(user: String, friends: List<List<String>>, visitors: List<String>){
+    //user 검사
+    if(user.length > 30 || user.isEmpty())
+        throw IllegalArgumentException("user name length must be less than 30!")
+    if(!Pattern.matches("^[a-z]*\$", user))
+        throw IllegalArgumentException("user name must be in English!")
+    //friends 검사
+    for(i in friends.indices){
+        if(friends[i][0].length > 30 || friends[i][0].isEmpty() ||
+                friends[i][1].length > 30 || friends[i][1].isEmpty())
+            throw IllegalArgumentException("friends name must be less than 30!")
+        if(!Pattern.matches("^[a-z]*\$", friends[i][0]) || !Pattern.matches("^[a-zA-Z]*\$", friends[i][1]))
+            throw IllegalArgumentException("friends name must be in English!")
+    }
+    //visitors 검사
+    for(i in visitors.indices){
+        if(visitors[i].length > 30)
+            throw IllegalArgumentException("visitors name must be less than 30!")
+        if(!Pattern.matches("^[a-z]*\$", visitors[i]))
+            throw IllegalArgumentException("visitors name must be in English!")
+    }
+}
+
+fun getUsersFriendsSet(user: String, friends: List<List<String>>): Set<String>{
+    // TODO: 2022-11-01  
+}
+
+fun makeRecommendMap(user: String, usersFriendsSet: Set<String>, friends: List<List<String>>): MutableMap<String, Int>{
+    // TODO: 2022-11-01  
+}
+
+fun updateRecommendMapbyVisitors(visitors: List<String>, recommendMap: MutableMap<String, Int>, usersFriendsSet: Set<String> ): MutableMap<String, Int>{
+    // TODO: 2022-11-01  
+}
+
+fun getResultList(recommendMap: MutableMap<String, Int>): List<String>{
+    // TODO: 2022-11-01  
 }
