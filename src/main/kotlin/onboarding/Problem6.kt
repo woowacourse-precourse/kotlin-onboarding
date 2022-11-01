66c3113c02eee72371461348dc1a012dbe92e7d4
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-01 13:25:03 +0900

    [Problem6] feat: 유효한 입력값인지 검사하는 함수 작성

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,34 @@
 package onboarding
 
+import java.util.regex.Pattern
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    isValidInput(forms)
+    // TODO: 2022-11-01  
+}
+fun isValidInput(forms: List<List<String>>){
+    for(i in forms.indices){
+        if(forms[i][0].length in 20..10)
+            throw IllegalArgumentException("email length is out of range!")
+        if(forms[i][1].length in 20..0 )
+            throw IllegalArgumentException("nickname length is out of range!")
+        if(!Pattern.matches("^[ㄱ-ㅎ가-힣]*\$", forms[i][1]))
+            throw IllegalArgumentException("nickname must be in Korean")
+    }
+}
+
+fun getDuplicatedList(forms: List<List<String>>):List<String>{
+    // TODO: 2022-11-01  
+}
+
+fun compareTwoNickname(nickName1: String, nickName2: String): Boolean{
+    // TODO: 2022-11-01  
+}
+
+fun addToDuplicatedEmailList(duplicatedList: ArrayList<String>, email: String): ArrayList<String>{
+    // TODO: 2022-11-01  
+}
+
+fun isEmailDuplicated(duplicatedList: ArrayList<String>, email: String): Boolean{
+    // TODO: 2022-11-01  
 }
