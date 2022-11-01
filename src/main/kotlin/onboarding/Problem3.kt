5f2f7a48b542f6959f66c431a76a92e1b371f437
lee989898 <@@qwer1352>
2022-11-01 22:43:28 +0900

    [refactor](Problem3) 3,6,9 개수 만큼 세는 로직 함수화

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -9,16 +9,22 @@ fun solution3(number: Int): Int {
     val filterWith369 =
         (0..number).filter { it % 10 == 3 || it % 10 == 6 || it % 10 == 9 || it / 10 == 3 || it / 10 == 6 || it / 10 == 9 }
 
+    countNumberOf369 = count369(filterWith369, countNumberOf369)
+    return countNumberOf369
+}
+
+private fun count369(filterWith369: List<Int>, countNumberOf369: Int): Int {
+    var countNumberOf3691 = countNumberOf369
     for (element in filterWith369) {
         var element369 = element
         while (element369 > 0) {
             if (element369 % 10 == 3 || element369 % 10 == 6 || element369 % 10 == 9) {
-                countNumberOf369++
+                countNumberOf3691++
             }
             element369 /= 10
         }
     }
-    return countNumberOf369
+    return countNumberOf3691
 }
 
 fun isAppropriateNumber(number: Int) = when (number) {
