9eb6ee942ce6ea2dfa0f710a580c3e4607010014
Jaehun-Jo <chojh922@gmail.com>
2022-11-01 18:26:34 +0900

    Problem2-기능 구현 완료

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -2,8 +2,7 @@ package onboarding
 
 fun solution2(cryptogram: String): String {
     checkThrowException(cryptogram)
-
-    return ""
+    return solveCryptogram(cryptogram)
 }
 
 private fun checkThrowException(string: String) {
@@ -19,4 +18,27 @@ private fun throwLowercaseException(string: String) {
     for (i in string.indices) {
         if (!string[i].isLowerCase()) throw error("문자열이 알파벳 소문자로만 이루어져 있어야 합니다!")
     }
+}
+
+private fun solveCryptogram(input: String): String {
+    val result = input.toMutableList()
+    var prevLength = 0
+
+    while (prevLength != result.size) {
+        prevLength = result.size
+        val deletionList = MutableList(result.size) { false }
+
+        for (i in result.size - 1 downTo 1) {
+            if (result[i] == result[i - 1]) {
+                deletionList[i] = true
+                deletionList[i - 1] = true
+            }
+        }
+
+        for (j in result.size - 1 downTo 0) {
+            if (deletionList[j]) result.removeAt(j)
+        }
+    }
+
+    return result.joinToString("")
 }
