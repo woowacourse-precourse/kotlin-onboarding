6b656f773ad5060f039ae46c80daee7c3981e972
kjyook <106497893+kjyook@users.noreply.github.com>
2022-11-01 19:53:15 +0900

    문제 6번 정렬 기능 + answer에 중복되어 입력되는것 수정

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,42 +1,60 @@
 package onboarding
 
 fun checkName(elem: String, temp: String):Int{
+    /*
     var a=0
     var b=0
+
     for(e in elem){
         var count = 0
 
         for(t in temp){
             if(e==t){
-                if(elem[a+1]==temp[b+1])
+                if(temp.length != b + 1 && elem.length != a + 1 && elem[a+1]==temp[b+1])
                     return 1
             }
-        b++
+            b++
         }
         a++
+    }
+     */
+
+    for( a in elem.indices){
+        for( b in temp.indices){
+            if ( elem[a] == temp[b] && a < elem.length - 1 && b < temp.length - 1){
+                if(elem[a+1]==temp[b+1]){
+                    return 1
+                }
+            }
+        }
     }
     return 0
 }
 fun solution6(forms: List<List<String>>): List<String> {
     var answer = mutableListOf<String>()
-    var temp = arrayListOf("","","","","")
+    var tempName = arrayListOf("","","","","")
+    var tempEmail = arrayListOf("","","","","")
 
     var count = 0
-    var idx = 0
     var check = 0
+
     for(elem in forms){
         for(a in 0 until count ) {   //하나씩 중복검사
-            if (checkName(elem[1], temp[a]) == 1) {
-                answer.add(elem[0])         //중복이니까 answer에 이메일 입력해주기
+            if (checkName(elem[1], tempName[a]) == 1) {
                 if(check == 0){
-                    answer.add(temp[a])
+                    answer.add(tempEmail[a])
                     check = 1
                 }
+                answer.add(elem[0])         //중복이니까 answer에 이메일 입력해주기
+                break
             }
         }
 
-        temp[count] = elem[1]
+        tempEmail[count] = elem[0]
+        tempName[count] = elem[1]
         count++
     }
-    return answer
+
+    val temp = answer.sorted()
+    return temp
 }
