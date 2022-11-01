4825e4a7a7a6301d280ad4f8509527fed7da87d2
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-01 18:47:43 +0900

    22.11.01 problem4 기능목록 및 기능 1번 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,19 @@
 package onboarding
 
-fun solution4(word: String): String {
-    TODO("프로그램 구현")
+fun solution4(cryptogram: String): String {
+    var result = ""
+    val maxSumOfLowerCase = 'a'.toInt() + 'z'.toInt()
+    val maxSumOfUpperCase = 'A'.toInt() + 'Z'.toInt()
+
+    if (isCorrect4(cryptogram)) {
+
+    }
+    else return "wrong cryptogram"
 }
+
+fun isCorrect4(cryptogram: String): Boolean {
+    val condition1 = cryptogram.length in 1..1000
+    return condition1
+}
+
+
