bce60a24058bc94501952f8df9a7149d6b8bbf76
2taezeat <xorudfl772@naver.com>
2022-11-01 10:11:24 +0900

    feat : 0. 예외사항 처리 함수

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,8 +1,26 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+
+    if ( isException(pobi) || isException(crong) ) {
+        return -1
+    }
+
+
 
 }
 
 
+fun isException(numberList : List<Int>) : Boolean {
+    if ( numberList[1] - numberList[0] != 1) {
+        return true
+    }
+    if (numberList[0] < 1 || numberList[1] > 400) {
+        return true
+    }
+    if( numberList[0] % 2 != 1 || numberList[1] % 2 != 0 ) {
+        return true
+    }
+
+    return false
+}
