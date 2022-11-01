05f8763551ce3b2cec0fad97896bdc64f5f2afab
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-01 20:41:11 +0900

    두 문자열이 비슷한지 검사하는 isSimilar 메소드 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,3 +3,16 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
+/* 두 문자열 사이에 연속되는 문자 있는지 확인하는 메소드 */
+fun isSimilar(str1: String, str2: String) : Boolean{
+    for(i in 0..str1.length-2){
+        for(j in 0..str2.length-2){
+            if(str1[i] != str2[j]) continue
+            if(str1[i+1] == str2[j+1]) return true
+        }
+    }
+    return false
+}
+
+
