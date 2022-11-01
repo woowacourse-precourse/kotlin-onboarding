77cc064f4ecb3d5071154139ccc7cfc96f4bcd8b
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-01 20:43:25 +0900

    주석 추가 및 Problem6 완성

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,10 +1,21 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    var dup = arrayListOf<String>() /* 비슷한 닉네임을 가진 이메일들을 저장하는 list */
+    for(i in 0..forms.size-2){
+        for(j in i+1 until forms.size){
+            var left = forms[i]
+            var right = forms[j]
+            if(!isSimilar(left[1], right[1])) continue
+            dup.add(left[0])
+            dup.add(right[0])
+        }
+    }
+    var result = dup.distinct()
+    return result.sorted()
 }
 
-/* 두 문자열 사이에 연속되는 문자 있는지 확인하는 메소드 */
+/* 두 닉네임 사이에 연속되는 문자 있는지 확인하는 메소드 */
 fun isSimilar(str1: String, str2: String) : Boolean{
     for(i in 0..str1.length-2){
         for(j in 0..str2.length-2){
