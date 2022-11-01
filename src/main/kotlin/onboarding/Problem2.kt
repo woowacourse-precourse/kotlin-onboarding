0e06dbec99ccc0bac226a26020ddd4742a54bc6b
sjh970603 <sjh970603@naver.com>
2022-11-01 15:09:48 +0900

    problem 2번 slice 활용했지만 실패

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,32 @@
 package onboarding
 
+
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    // TODO("프로그램 구현")
+    var count=1
+    var str = cryptogram
+
+
+    while(count != 0)
+    {
+        count=0
+        for (i in 0 until str.length)
+        {
+
+            if (i+1 < str.length && str[i] == str[i + 1]) {
+//                str.replace(str[i],null,false)
+                //var target = str.slice(0..i)
+                //var target = str.substring(i)
+                //str = target[0].plus(target[1].toString())
+//                str.replace(str[i],null,false)
+
+                // slice나 replace 이용해서 해봤지만 실패함...
+
+
+
+            }
+        }
+    }
+    return str
+
 }
