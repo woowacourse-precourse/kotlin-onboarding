ad3961ade364b18b2a4f0a99ab58f1b94a291acf
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-01 19:02:18 +0900

    화폐별 필요한 개수를 리턴하는 getCount 메소드 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,16 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var num = money
+    val unit = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    var count = arrayListOf<Int>(0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+    return count
 }
+
+fun getCount(num: Int, unit: Int): Int{
+    var cnt: Int = num / unit
+    return cnt
+}
+
+
