7ec51a3471fc7768e0ddd089771a6c84b59ef7ae
phs <gustjd0808@pusan.ac.kr>
2022-11-01 15:35:24 +0900

    Problem6 기능 목록 및 구현 수정

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,17 @@
 package onboarding
 
+fun isValidNickname(nickname: String): Boolean{
+    return nickname.matches("^[가-힣]{1,19}".toRegex())
+}
+
+fun isValidEmail(email: String): Boolean{
+    return email.matches("""^[\w+-_.]{1,9}@email[.]com$""".toRegex())
+}
+
+fun isValidForm(form: List<String>): Boolean{
+    return isValidEmail(form[0]) && isValidNickname(form[1])
+}
+
 private fun addTwoLengthWords(
     form: List<String>,
     map: HashMap<String, ArrayList<Int>>,
@@ -15,13 +27,14 @@ private fun addTwoLengthWords(
 fun solution6(forms: List<List<String>>): List<String> {
     val twoLengthWordMap = hashMapOf<String, ArrayList<Int>>()
 
-    for ((index, form) in forms.withIndex()) {
-        addTwoLengthWords(form, twoLengthWordMap, index)
-    }
+    forms
+        .filter { form -> isValidForm(form) }
+        .forEachIndexed { index, form ->
+            addTwoLengthWords(form, twoLengthWordMap, index) }
 
     return twoLengthWordMap.filter { mapEntry -> mapEntry.value.size >= 2 }
         .flatMap { mapEntry -> mapEntry.value }
         .distinct()
-        .map { duplicatedIndex -> forms[duplicatedIndex][0] }
+        .map { duplicatedNicknameIndex -> forms[duplicatedNicknameIndex][0] }
         .toList().sorted()
 }
