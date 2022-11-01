197d492eca97e44e181f3df6dd677d399a0adc30
sjh970603 <sjh970603@naver.com>
2022-11-01 15:11:41 +0900

    problem 3번 클리어

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,22 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+//    TODO("프로그램 구현")
+
+    var num = number
+    var count = 0
+
+    for(i in 1..num)
+    {
+        if ( i%10 == 3 || i%10 == 6 || i%10 == 9 )
+            count+=1
+
+        if ( i/10 == 3 || i/10 == 6 || i/10 == 9)
+            count+=1
+    }
+
+    // 나머지 값이 3,6,9일때 count 1증가 , 나눈 몫이 3,6,9일때 count 1 증가
+
+
+    return count
 }
