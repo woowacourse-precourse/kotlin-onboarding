af7ef040257920a0e7ce960167f4c42779414661
changhwan <chang5hustle@gmail.com>
2022-11-01 15:03:27 +0900

    [Problem 4] (리펙토링 2) 리스트로 변환했던 로직들 문자열내에서 해결하는 수정

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -7,21 +7,21 @@ fun solution4(word: String): String {
 
 private fun getConversionMap(): MutableMap<Char, Char> {
     val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    val upperAlphabetList = alphabet.toList()
-    val reverseUpperAlphabetList = upperAlphabetList.reversed()
-    val lowerAlphabetList = alphabet.lowercase().toList()
-    val reverseLowerAlphabetList = lowerAlphabetList.reversed()
+    val upperAlphabet = alphabet
+    val reverseUpperAlphabet = upperAlphabet.reversed()
+    val lowerAlphabet = alphabet.lowercase()
+    val reverseLowerAlphabet = lowerAlphabet.reversed()
     val conversionMap = mutableMapOf<Char, Char>()
 
-    for (i in upperAlphabetList.indices) conversionMap[upperAlphabetList[i]] = reverseUpperAlphabetList[i]
-    for (i in lowerAlphabetList.indices) conversionMap[lowerAlphabetList[i]] = reverseLowerAlphabetList[i]
+    for (i in upperAlphabet.indices) conversionMap[upperAlphabet[i]] = reverseUpperAlphabet[i]
+    for (i in lowerAlphabet.indices) conversionMap[lowerAlphabet[i]] = reverseLowerAlphabet[i]
     conversionMap[' '] = ' '
 
     return conversionMap
 }
 
 private fun getConversionWord(word: String, conversionMap: MutableMap<Char, Char>): String {
-    val processedWordList = mutableListOf<Char>()
-    for (i: Char in word.toList()) processedWordList.add(conversionMap[i] ?: i)
-    return processedWordList.joinToString("")
+    var processedWord = ""
+    for (i: Char in word) processedWord += conversionMap[i] ?: i
+    return processedWord
 }
