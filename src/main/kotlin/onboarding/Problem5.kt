8ffa5ba5e1f27d8cea7f07f44389098089bf7706
Jaehun-Jo <chojh922@gmail.com>
2022-11-01 19:05:48 +0900

    Problem5-기능 구현 완료

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -2,7 +2,7 @@ package onboarding
 
 fun solution5(money: Int): List<Int> {
     checkThrowException(money)
-    return listOf()
+    return calculateCurrency(money)
 }
 
 private fun checkThrowException(money: Int) {
@@ -11,4 +11,17 @@ private fun checkThrowException(money: Int) {
 
 private fun throwNumberRangeException(number: Int) {
     if (number > 1_000_000 || number < 1) throw error("money는 1 이상 1,000,000 이하인 자연수여야 합니다!")
+}
+
+private fun calculateCurrency(money: Int): List<Int> {
+    val currencies = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val result = mutableListOf<Int>()
+    var remain = money
+
+    currencies.forEach{ currency ->
+        result.add(remain / currency)
+        remain %= currency
+    }
+
+    return result
 }
