5e3f40d3884169fd4b90377d800f3ccbde939ca6
parkji1on <103587652+parkji1on@users.noreply.github.com>
2022-11-01 22:44:37 +0900

    Update Problem3.kt

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var count:Int = 0
+    var clap:Int
+
+    for(i in (1..number)){
+        clap = i
+        //각 숫자의 자리수 별로 3,6,9의 숫자를 검사하여 count를 올려준다
+        while(clap!= 0){
+            if(((clap%10)%3 ==0) && (clap%10!= 0)) count++
+            clap /= 10 
+        }
+    }
+
+    return count
 }
