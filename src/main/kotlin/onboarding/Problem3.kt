75cf3146311ff6b18d68aea46215509991ac9d19
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-01 17:48:11 +0900

    22.11.01 problem3 기능 2,3번 구현 및 테스트 완료

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -6,7 +6,15 @@ fun solution3(number: Int): Int {
     var nineCount = 0
 
     if (isCorrect(number)) {
+        for (i in 1..number) {
+            for (j in "$i") {
+                if ('3' == j) threeCount++
+                if ('6' == j) sixCount++
+                if ('9' == j) nineCount++
+            }
 
+        }
+        return threeCount + sixCount + nineCount
     } else return -1
 }
 
