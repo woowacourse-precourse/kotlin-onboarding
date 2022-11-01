1f8eed62fde8439f976c7723abbf501e58d09d62
lsakee <lsls4868@gmail.com>
2022-11-01 12:03:53 +0900

    Feat : getUserFriend()메소드 기능 추가 -user의 친구를 찾는 기능

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,32 @@
 package onboarding
 
+import java.util.*
+import kotlin.collections.HashMap
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    //user친구
+    val userFriends = getUserFriend(user, friends)
+    //아는 친구의친구 지인
+    var acquaintance = mutableListOf<String>()
+    //방문한 사람들
+    var visitorFriend = mutableListOf<String>()
+    //추천 친구 점수
+    val recommendedScore = HashMap<String, Int>()
+
+
+//    return sortHashMap(recommendedScore)
+}
+
+fun getUserFriend(user: String, friends: List<List<String>>) : List<String>{
+    val friendList = mutableListOf<String>()
+    for (i in friends){
+        if (user == i[1]) friendList.add(i[0])
+        if (user == i[0]) friendList.add(i[1])
+    }
+    return friendList
+
 }
