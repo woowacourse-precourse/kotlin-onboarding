dd53c39a769b4ab45b1dd3189b673dc818df1d7c
changhwan <chang5hustle@gmail.com>
2022-11-01 16:43:00 +0900

    [Problem 2] (리펙토링 1) 1단계 중복문자 삭제로직 함수분리

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,28 +1,34 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    var processedCryptogram = cryptogram.toMutableList()
+    var processedCryptogram = cryptogram
     var cryptographyState = true
 
     while (cryptographyState) {
-        cryptographyState = false
-        var duplicateRangeSet = mutableSetOf<Int>()
-        for (i: Int in processedCryptogram.indices) {
-            var duplicateCount = 0
-            for (j: Char in processedCryptogram.subList(i + 1, processedCryptogram.size)) {
-                if (processedCryptogram[i] == j) {
-                    duplicateCount += 1
-                } else break
-            }
-            if (duplicateCount > 0) {
-                duplicateRangeSet.addAll(i..i + duplicateCount)
-                cryptographyState = true
-            }
-        }
-        for (i in duplicateRangeSet.reversed()) {
-            processedCryptogram.removeAt(i)
-        }
+        val stepConclusion = decodeCryptogramStep(processedCryptogram)
+        processedCryptogram = stepConclusion.first
+        cryptographyState = stepConclusion.second
     }
 
-    return processedCryptogram.joinToString("")
+    return processedCryptogram
+}
+
+private fun decodeCryptogramStep(cryptogram: String): Pair<String, Boolean> {
+    var cryptographyState = false
+    var duplicateRangeSet = mutableSetOf<Int>()
+    for (i: Int in cryptogram.indices) {
+        var duplicateCount = 0
+        for (j: Char in cryptogram.substring(i + 1, cryptogram.length)) {
+            if (cryptogram[i] == j) duplicateCount += 1 else break
+        }
+        if (duplicateCount > 0) {
+            duplicateRangeSet.addAll(i..i + duplicateCount)
+            cryptographyState = true
+        }
+    }
+    val cryptogramList = cryptogram.toMutableList()
+    for (i in duplicateRangeSet.reversed()) {
+        cryptogramList.removeAt(i)
+    }
+    return Pair(cryptogramList.joinToString(""), cryptographyState)
 }
