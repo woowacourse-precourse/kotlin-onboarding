301100821bd2e3ef5c65481614c1dc33d475e14a
bmlsj <gaga2353@gmail.com>
2022-11-01 21:18:24 +0900

    Problem4 기능 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,31 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+
+    val ans = ArrayList<Int>()
+    val ansString = StringBuilder()
+
+    word.forEach {
+
+        val ascii_num = it.code
+        var tmp = 0
+
+        if (97 <= ascii_num && ascii_num <= 122){
+            tmp = ascii_num + (25-2*(ascii_num-97))
+            ans.add(tmp)
+        } else if (65 <= ascii_num && ascii_num <= 90){
+
+            tmp = ascii_num + (25-2*(ascii_num-65))
+            ans.add(tmp)
+        } else{
+            ans.add(ascii_num)
+        }
+    }
+
+    for (i in ans){
+        ansString.append(i.toChar())
+    }
+
+    println(ansString)
+    return ansString.toString()
 }
