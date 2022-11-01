18cd9ad36089dcf54ed294271c7bade3f4838338
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-01 17:52:22 +0900

    주석 추가 및 Problem3 완성

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,13 +1,22 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var cur: Int = 1
+    var ans: Int = 0
+
+    /* 1부터 number까지 돌면서 필요한 박수의 수를 ans에 더한다 */
+    while(cur <= number){
+        ans += clap(cur)
+        cur++
+    }
+    return ans
 }
 
+/* 해당 숫자에 필요한 박수를 리턴하는 메소드 */
 fun clap(num: Int): Int {
     var num = num
     var ans: Int = 0
-    var temp: Int = 0
+    var temp: Int
 
     while(num > 0){
         temp = num % 10
@@ -17,6 +26,7 @@ fun clap(num: Int): Int {
     return ans
 }
 
+/* 3, 6, 9 인지를 판별하는 메소드 */
 fun is369(num: Int): Int{
     if(num % 3 == 0 && num != 0){
         return 1
