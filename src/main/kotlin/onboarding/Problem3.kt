b705f8406674e72072921b630ab53e87f167a221
최재훈 <be_clever@naver.com>
2022-11-01 18:27:52 +0900

    feat(Problem3): 각 숫자에 포함된 3, 6, 9의 갯수를 반환하는 기능 작성

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,21 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+
+}
+
+fun getNumOf369(_number: Int): Int {
+    val number = _number.toString()
+    val compareList = arrayOf(3, 6, 9)
+
+    var result = 0
+    for(c in number) {
+        for(num in compareList) {
+            if(c - '0' == num) {
+                result++
+            }
+        }
+    }
+
+    return result
 }
