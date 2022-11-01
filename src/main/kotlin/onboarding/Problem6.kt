1cecec6084c3c5e134fab03f2f4b1320c9bcf13a
joh9911 <joh9911@naver.com>
2022-11-01 19:56:43 +0900

    problem6: 완성

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -2,10 +2,16 @@ package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
     val forms = forms
+
     var twoWordsArray = ArrayList<String>() //두 개의 단어 단위로 저장하는 배열
     var countArray = ArrayList<Int>() // 두 개의 단어를 포함하는 개수를 저장하는 배열
     var lengthArray = ArrayList<Int>() // 각 닉네임의 길이를 저장한 배열
     var result = ArrayList<String>() // 결과 배열
+    saveByTwoWords(forms, twoWordsArray)
+    saveCountAndLength(forms, twoWordsArray, countArray, lengthArray)
+    selectEmailOfDuplicatedNickname(countArray, lengthArray, result, forms)
+
+    return result
 }
 /** 전달 받은 배열에서 각 닉네임을 두 글자씩 쪼개어 배열에 저장하는 반복문 */
 fun saveByTwoWords(forms: List<List<String>>, twoWordsArray: ArrayList<String>){
