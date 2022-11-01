10be89911d211b3d2d0b5951a5bc74741eb83de0
Yuhee Kim <dbgml4509@naver.com>
2022-11-01 19:22:20 +0900

    feat : number까지 3,6,9 포함 개수를 세는 기능 추가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -5,7 +5,7 @@ package onboarding
     1. 숫자 중 3,6,9 를 포함하는지 확인한다.
     2. 포함한다면, 3,6,9를 몇 개 포함하는지 확인한다.
     3. 2. 에서 확인한 개수를 누적하여 더한다.
-    3. 1부터 number까지 1,2. 를 동일하게 수행한다.
+    4. 1부터 number까지 1,2. 를 동일하게 수행한다.
  */
 //예외 사항
 /*
@@ -13,5 +13,22 @@ package onboarding
  */
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    //프로그램 구현
+    return countGame(number)
+}
+
+fun countGame(number: Int): Int{
+
+    var count = 0
+    //4. 1부터 number까지 1,2. 를 동일하게 수행한다.
+    for(i in 1 until number){
+        val str = number.toString()
+
+        for(chr in i){ //1. 숫자 중 3,6,9를 포함하는지 확인
+            if(chr == '3') count += 1 //2,3. 3,6,9를 몇 개 포함하는지 count로 누적 계산한다.
+            else if(chr == '6') count += 1
+            else if(chr == '9') count += 1
+        }
+    }
+    return count
 }
