f0d4e04fa28e95f6de41304bdb3473eb4330e5d3
noion0511 <noion0511@gmail.com>
2022-11-01 23:52:15 +0900

    [problem 7][feat] 친구 리스트 만드는 기능

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,20 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    return listOf()
 }
+
+fun getFriendList(user: String, friends: List<List<String>>): HashSet<String> {
+    val friendList = hashSetOf<String>()
+
+    for (friend in friends) {
+        when (user) {
+            friend[0] -> friendList.add(friend[1])
+            friend[1] -> friendList.add(friend[0])
+        }
+    }
+
+    return friendList
+}
+
+
