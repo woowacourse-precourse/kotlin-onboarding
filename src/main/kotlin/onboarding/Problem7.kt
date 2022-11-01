02b5488e519144783c67d4cab7deb2f8394981d7
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-11-01 17:01:44 +0900

    [추가] user와 친구관계인 사용자 목록

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -7,3 +7,14 @@ fun solution7(
 ): List<String> {
     TODO("프로그램 구현")
 }
+
+fun friendList(user: String, friends: List<List<String>>): MutableSet<String> {
+    val friend = mutableSetOf<String>()
+    friends.forEach {
+        when (user) {
+            it[0] -> friend.add(it[1])
+            it[1] -> friend.add(it[0])
+        }
+    }
+    return friend
+}
