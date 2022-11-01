893cd6a1f77d0b98de9c594adebc8e1136230b6c
3ehdgusdl <3ehdgusdl@naver.com>
2022-11-01 23:51:18 +0900

    문제 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,61 @@
 package onboarding
 
+import java.lang.Math.abs
+import java.lang.Math.max
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    var answer = 0
+
+    if (abs(pobi[0] - pobi[1]) != 1) {
+        answer = -1
+    } else if (abs(crong[0] - crong[1]) != 1) {
+        answer = -1
+    } else {
+        val pobiConvert = pobi.map {
+            var sumRes = 0
+            var mulRes = 1
+
+            it.toString().forEach {
+                sumRes += it.digitToInt()
+                mulRes *= it.digitToInt()
+            }
+
+            return@map max(sumRes, mulRes)
+        }
+
+        val crongConvert = crong.map {
+            var sumRes = 0
+            var mulRes = 1
+            it.toString().forEach {
+                sumRes += it.digitToInt()
+                mulRes *= it.digitToInt()
+            }
+            return@map max(sumRes, mulRes)
+        }
+
+        var pobiScore = 0
+        var crongScore = 0
+
+        if (pobiConvert[0] > crongConvert[0]) {
+            pobiScore++
+        } else if (pobiConvert[0] < crongConvert[0]) {
+            crongScore++
+        }
+
+        if (pobiConvert[1] > crongConvert[1]) {
+            pobiScore++
+        } else if (pobiConvert[1] < crongConvert[1]) {
+            crongScore++
+        }
+
+        if (pobiScore > crongScore) {
+            answer = 1
+        } else if (pobiScore < crongScore) {
+            answer = 2
+        } else {
+            answer = 0
+        }
+    }
+
+    return answer
 }
--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,14 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var N = money
+    var list = listOf<Int>(50000,10000,5000,1000,500,100,50,10,1)
+    var result = mutableListOf<Int>()
+
+    for (i in list){
+        result.add(N/i)
+        N %= i
+    }
+    println(result)
+    return result
 }
