429db4136c245e6750fbc27327f32fa7a8a8c71a
noion0511 <noion0511@gmail.com>
2022-11-01 23:52:39 +0900

    [problem 7][feat] 친구 점수를 계산 하는 기능

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -21,4 +21,27 @@ fun getFriendList(user: String, friends: List<List<String>>): HashSet<String> {
     return friendList
 }
 
+fun countFriendScore(user: String, friends: List<List<String>>,
+                     visitors: List<String>): HashMap<String, Int> {
+    val countScoreMap = hashMapOf<String, Int>()
+    val friendList = getFriendList(user, friends)
 
+    for (friend in friends) {
+        val relation = when {
+            friend[0] in friendList && user != friend[1] -> friend[1]
+            friend[1] in friendList && user != friend[0] -> friend[0]
+            else -> continue
+        }
+
+        countScoreMap[relation] = countScoreMap.getOrDefault(relation, 0) + 10
+    }
+
+    for (visitor in visitors) {
+        if (visitor in friendList)
+            continue
+
+        countScoreMap[visitor] = countScoreMap.getOrDefault(visitor, 0) + 1
+    }
+
+    return countScoreMap
+}
