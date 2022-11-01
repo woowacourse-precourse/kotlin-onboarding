32fa2c6a35b83dcb5ad3d9f8479347c2b66851c7
lee989898 <@@qwer1352>
2022-11-01 12:55:07 +0900

    [feat](Problem3) 3 혹은 6 혹은 9를 하나라도 포함하는 요소만 필터링 해서 담는 변수 추가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -6,6 +6,8 @@ fun solution3(number: Int): Int {
     }
 
     var countNumberOf369 = 0
+    val filterWith369 = (0..number).filter { it % 10 == 3 || it % 10 == 6 || it % 10 == 9 || it / 10 == 3 || it / 10 == 6 || it / 10 == 9 }
+
     return -1
 }
 
