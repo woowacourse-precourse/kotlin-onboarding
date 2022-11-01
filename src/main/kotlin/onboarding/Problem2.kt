e57cc9183eed0ccb40614d6532ffe5f88e4e31a2
Yuhee Kim <dbgml4509@naver.com>
2022-11-01 18:53:04 +0900

    fix : 알파벳 아닌 경우 예외 사항 수정

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,6 @@
 package onboarding
 
+
 //기능 목록
 /*
     1. 처음 들어온 cryptogram에 연속된 중복 문자가 있는지 확인
@@ -30,7 +31,7 @@ fun solution2(cryptogram: String): String {
 
     //3. 2.에서 제거했음에도 연속 중복 문자가 있다면 제거, 4. 중복 문자가 없을 때까지 제거
     for(alphabet in cryptogram){ //1. 처음 들어온 cryptogram에 연속된 중복 문자가 있는지 확인
-        if(builder.last() == alphabet) builder.deleteCharAt(builder.length -1) //2. 1.에서 연속 중복 문자가 있다면 제거
+        if(builder.isNotEmpty() && builder.last() == alphabet) builder.deleteCharAt(builder.length -1) //2. 1.에서 연속 중복 문자가 있다면 제거
         else builder.append(alphabet)
     }
     return builder.toString()
@@ -38,23 +39,21 @@ fun solution2(cryptogram: String): String {
 
 //예외 사항  1. cryptogram 중 알파벳 대문자가 포함된 경우
 fun lowerToUpper(cryptogram: String): String{
-    val lower = cryptogram.toLowerCase()
+    val lower = cryptogram.lowercase()
     return lower
 }
 
 //예외 사항 2. cryptogram이 알파벳이 아닌 경우
 fun isAlphabets(cryptogram: String): Boolean{
     for (chr in cryptogram){
-        if(chr !in 'A'..'Z' && c !in 'a'..'z') return false
+        if(chr !in 'A'..'Z' && chr !in 'a'..'z') return false
     }
     return true
 }
 
 //예외 사항 2. cryptogram이 알파벳이 아닌 경우, 다른 문자로 대체
 fun charToAlphabet(cryptogram: String): String{
-    val replacementChar = "abcdefg"
-    val replace = cryptogram.map{
-        if(it !in !in 'A'..'Z' && it !in 'a'..'z') replacementChar else it
-    }.joinToString(separator = "")
-    return replace
+    val rep = Regex("[^A-Za-z0-9 ]")
+    val result = rep.replace(cryptogram, "")
+    return result
 }
