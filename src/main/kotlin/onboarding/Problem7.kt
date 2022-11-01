fa9ed4a03d1b68c023e7f117709b33ebdbae5b9b
hyunji1203 <hj27720@gmail.com>
2022-11-01 14:48:37 +0900

    문제7 - 사용자와 친구 관계인 친구 리스트 만들기

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,17 @@
 package onboarding
 
-fun solution7(
-    user: String,
-    friends: List<List<String>>,
-    visitors: List<String>
-): List<String> {
-    TODO("프로그램 구현")
+fun solution7(user: String, friends: List<List<String>>, visitors: List<String>): List<String> {
+    var userFriends = mutableSetOf<String>()
+    var recommend = mutableMapOf<String, Int>()
+
+    //사용자와 친구 관계를 맺고 있는 친구 리스트 만들기
+    for (i in friends){
+        if (i.contains(user)){
+            for (j in (0..1)){
+                userFriends.add(i[j])
+            }
+        }
+    }
+    userFriends.remove(user)
+
 }
