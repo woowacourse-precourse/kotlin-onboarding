21cb862119566413d69b93e0bddec63293743a0b
Jaehun-Jo <chojh922@gmail.com>
2022-11-01 18:32:49 +0900

    Problem3-제한사항 예외 처리

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,14 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    checkThrowException(number)
+    return 0
+}
+
+private fun checkThrowException(number: Int) {
+    throwNumberRangeException(number)
+}
+
+private fun throwNumberRangeException(number: Int) {
+    if (number > 10000 || number < 1) throw error("number는 1 이상 10,000 이하인 자연수여야 합니다!")
 }
