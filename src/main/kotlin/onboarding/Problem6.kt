279fb207ba6a471c1056acb1137c30f2c2d4e1ae
2taezeat <xorudfl772@naver.com>
2022-11-01 14:48:18 +0900

    feat : 0.유저들의 닉네임을 2글자씩 잘라서, List에 담는 함수 작성

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,3 +3,15 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
+
+fun splitTwoLength(userName : String): ArrayList<String> {
+    val splitResult = arrayListOf<String>()
+    for (i in 0 until userName.lastIndex) {
+        splitResult.add(userName.substring(i,i+2))
+    }
+
+    return splitResult
+}
+
+
