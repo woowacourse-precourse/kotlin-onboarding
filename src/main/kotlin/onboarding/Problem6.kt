64d2a5d4fff735e471eb0423dc2442cb9e07aeb0
changhwan <chang5hustle@gmail.com>
2022-11-01 12:51:32 +0900

    [Problem 6] (요구사항 1) 이메일 길이제한 에 맞지않는 신청 제거함수 작성

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,3 +3,9 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
+private fun getValidLengthEmailForms(forms: List<List<String>>): List<List<String>> {
+    val processedForms = forms.toMutableList()
+    for (i in forms) if (i[0].length !in 11..19) processedForms.remove(i)
+    return processedForms
+}
