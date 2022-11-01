5fa330e1c46d1dea63775eba2fc12246b438c181
lh99j <grand2181@gmail.com>
2022-11-01 13:16:53 +0900

    Solve Problem 2

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,31 @@
 package onboarding
 
+import java.util.ArrayDeque
+
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var str : String= cryptogram
+    var strStack : ArrayDeque<Char> = ArrayDeque<Char>()
+    var answer : String= ""
+
+    strStack.addLast(str[0])
+
+    for(i in 1 until str.length){
+        val temp = str[i]
+
+        if(strStack.isNotEmpty() && strStack.peekLast() != temp)
+            strStack.addLast(temp)
+
+        else if(strStack.isNotEmpty() && strStack.peekLast() == temp)
+            strStack.removeLast()
+
+        else if(strStack.isEmpty())
+            strStack.addLast(temp)
+
+    }
+
+    for(i in 0 until strStack.size)
+        answer += strStack.pollFirst()
+
+
+    return answer
 }
