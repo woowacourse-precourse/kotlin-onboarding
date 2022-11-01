b0020178cb9bc808383a17050d790e02c68b4e76
lsakee <lsls4868@gmail.com>
2022-11-01 12:00:48 +0900

    Feat : List를 탐색을 위한 조건물 설정

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,16 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    var result = mutableListOf<String>()
+    for (i in 0..forms.size - 1) {
+        for (j in 0..forms[i][1].length - 1) {
+            // forms의 인덱스를 초과하지 않기 위해설정
+            if (j + 1 > forms[i][1].length - 1) {
+                break
+            }
+        }
+    }
+    //중복값 제거 후 정렬
+    return result.distinct().sorted()
 }
+
