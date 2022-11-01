7fa26dbe9b62027584e471edcd0e9a9d7a9467f7
s9hn <tnghqn@naver.com>
2022-11-01 18:41:08 +0900

    [REFACTOR(미션3)] 코드 정리 및 리드미 기능목록 수정
    1. 주석 추가
    2. 변수명 및 함수명 재설정
    3. 나머지 검사하는 if문 코드 간소화
    4. 리드미 기능목록 재수정

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,21 +1,22 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    return loopCheck(number)
+    return check369InNum(number)
 }
 
-fun loopCheck(number: Int): Int {
+fun check369InNum(number: Int): Int { // 1..입력값까지, 숫자 3,6,9 포함 시, 카운트+1, 리턴
+    var countClap = 0
 
-    var count = 0
     for (i in 1..number) {
-        var tempNumber = i
+        var currentNum = i
 
-        while (tempNumber > 0) {
-            if ((tempNumber % 10 == 3) or (tempNumber % 10 == 6) or (tempNumber % 10 == 9)) {
-                count++
+        while (currentNum > 0) {
+            val unitsCurrentNum = currentNum % 10
+            if (((unitsCurrentNum) != 0) and ((unitsCurrentNum) % 3 == 0)) {
+                countClap++
             }
-            tempNumber /= 10
+            currentNum /= 10
         }
     }
-    return count
+    return countClap
 }
