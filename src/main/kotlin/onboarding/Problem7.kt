9988f5f3b913a5322f755dc6b16b9defd6e21c46
lsakee <lsls4868@gmail.com>
2022-11-01 12:05:05 +0900

    Feat : addVisitorFriend()메소드 기능 추가 -visitor를 List에 추가하는 기능

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -16,9 +16,12 @@ fun solution7(
     var visitorFriend = mutableListOf<String>()
     //추천 친구 점수
     val recommendedScore = HashMap<String, Int>()
+    //방문한 친구 목록 추가 함수
+    visitorFriend= addVisitorFriend(user,visitorFriend,visitors)
+    //지인친구 목록 추가 함수
 
 
-//    return sortHashMap(recommendedScore)
+    return sortHashMap(recommendedScore)
 }
 
 fun getUserFriend(user: String, friends: List<List<String>>) : List<String>{
@@ -29,4 +32,13 @@ fun getUserFriend(user: String, friends: List<List<String>>) : List<String>{
     }
     return friendList
 
+}
+fun addVisitorFriend(user: String,visitorFriend: MutableList<String>, visitors: List<String>):MutableList<String>{
+    for (i in 0..visitors.size - 1) {
+        //방문자 목록추가
+        visitorFriend.add(visitors[i])
+        //자신은 방문자 목록에서 제외
+        visitorFriend.remove(user)
+    }
+    return visitorFriend
 }
