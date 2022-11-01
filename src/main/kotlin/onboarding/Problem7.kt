60c1aa47d9130e852860b5241511d600b19e11ea
lanpia <95679243+lanpia@users.noreply.github.com>
2022-11-01 23:25:15 +0900

    Problem7 solve

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -4,6 +4,46 @@ fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
-): List<String> {
+): List<String> 
+{
     //TODO("프로그램 구현")
+    val userFriend = hashSetOf<String>()
+    val friendScore = hashMapOf<String, Int>()
+
+    friends.forEach() 
+    {
+        when (user) {
+            it[0] -> userFriend.add(it[1])
+            it[1] -> userFriend.add(it[0])
+        }
+    }
+
+    friends.forEach {
+        val stranger = when 
+        {
+            (it[0] in userFriend) && (user != it[1]) -> it[1]
+            (it[1] in userFriend) && (user != it[0]) -> it[0]
+            else -> return@forEach
+        }
+        friendScore[stranger] = friendScore.getOrDefault(stranger, 0) + 10
+    }
+
+    visitors
+        .filterNot { it in userFriend }
+        .forEach() { friendScore[it] = friendScore.getOrDefault(it + 1, 1) + 1 }
+
+    return friendScore.asSequence()
+        .sortedWith { p1, p2 ->
+            val scoreSame = p2.value.compareTo(p1.value)
+            val keySame = p1.key.compareTo(p2.key)
+
+            if (scoreSame != 0) 
+                scoreSame
+            else 
+                keySame
+            
+        }
+        .map { it.key }
+        .take(5)
+        .toList()
 }
