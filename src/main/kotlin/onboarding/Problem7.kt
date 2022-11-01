75afae8172ce0cf4ebc17495ac6d3f1009a53f6a
dev-worthwhile <dev.lboram@gmail.com>
2022-11-01 17:36:20 +0900

    docs: 기능 목록 추가

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,5 +1,14 @@
 package onboarding
 
+/**
+ * 기능목록
+ * (1) user 의 친구 목록 확인
+ * (2) friends 목록에서 user 친구와 친구를 맺고있는 사람에게 10 점 부여
+ * (3) user 타임 라인 방문했던 visitor 에게 1점 부여
+ * (4) 점수 목록에서 (기능1)에서 확인한 친구들은 제외
+ * (5) 점수 목록 이름 순 정렬 후 점수 순 정렬
+ * (6) 점수가 가장 높은 순으로 최대 5명 선정
+ * **/
 fun solution7(
     user: String,
     friends: List<List<String>>,
