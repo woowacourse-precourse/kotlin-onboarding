858040a88cbcef6d6d92e0b0bb61023ed26542f1
leehandsub <gustjq7027@naver.com>
2022-11-01 23:10:29 +0900

    feature/아는 친구 점수 부여

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,30 +1,36 @@
 package onboarding
 
-import org.assertj.core.api.Assertions
-
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
 
-    val relation = mutableMapOf<String,MutableSet<String>>()
+    val relation = mutableMapOf<String, MutableSet<String>>()
+    val snsScore = mutableMapOf<String, Int>()
     val ans = listOf<String>()
 
-    for(data in friends){
-        if(relation[data[0]]==null)
-            relation[data[0]]= mutableSetOf()
-        else {
+    for (data in friends) {
+        if (relation[data[0]] == null) {
+            snsScore[data[0]] = 0
+            relation[data[0]] = mutableSetOf()
+        } else {
             relation[data[0]]?.add(data[1])
-
         }
-        if(relation[data[1]]==null)
-            relation[data[1]]= mutableSetOf()
-        else {
+        if (relation[data[1]] == null) {
+            relation[data[1]] = mutableSetOf()
+            snsScore[data[1]] = 0
+        } else {
             relation[data[1]]?.add(data[0])
 
         }
         relation[data[1]]?.add(data[0])
+    }
+
+    relation[user]?.forEach {
+        relation[it]?.forEach { friend ->
+            snsScore[friend] = snsScore[friend]!! + 10
+        }
     }
 
     
@@ -33,7 +39,7 @@ fun solution7(
 }
 
 
-fun main(){
+fun main() {
     val user = "mrko"
     val friends = listOf(
         listOf("donut", "andole"),
