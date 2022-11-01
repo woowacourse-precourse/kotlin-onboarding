c8d3fdb385292e906bfcc7a83b5d6c794fb5ceed
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-11-01 19:37:06 +0900

    docs : 문제 3 기능목록 , refactor : 주석 처리 / 함수 간소화

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -4,7 +4,16 @@ package onboarding
 
 fun solution3(number: Int): Int {
 
-    var answer = 0
+
+   return count_369(number)
+
+
+
+
+}
+//3 6 9가 들어간 숫자를 계산하는 함수
+fun count_369(number: Int) : Int{
+    var count = 0
 
     for(i in 1..number){
 
@@ -13,13 +22,11 @@ fun solution3(number: Int): Int {
         while( current != 0){
 
             if(  (current % 10 == 3 || current % 10 == 6 || current % 10 == 9) ){
-                answer++
+                count++
             }
             current /= 10
         }
     }
 
-    return answer
-
-
+    return count
 }
