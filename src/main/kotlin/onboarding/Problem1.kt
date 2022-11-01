eb695750126d48c674dcc56d442306e8bd1621fe
lee989898 <@@qwer1352>
2022-11-01 12:29:15 +0900

    [feat](Problem1) 잘못된 값이 들어왔을때 -1을 리턴하는 함수 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,22 @@
 package onboarding
 
+import kotlin.math.abs
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    val exception = -1
+
+    if (!isAppropriateList(pobi, crong)) {
+        return exception
+    }
+    return -1
+}
+
+fun isAppropriateList(pobi: List<Int>, crong: List<Int>): Boolean {
+    return when {
+        !(pobi[0] in 1..399 && pobi[1] in 1..399) -> false
+        !(crong[0] in 1..399 && crong[1] in 1..399) -> false
+        abs(pobi[0] - pobi[1]) > 1 || abs(crong[0] - crong[1]) > 1 -> false
+        pobi[0] > pobi[1] || crong[0] > crong[1] -> false
+        else -> true
+    }
 }
