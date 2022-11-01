d4395d34c6b82afa5a2bea804cfc7abe480b04a4
Yuhee Kim <dbgml4509@naver.com>
2022-11-01 21:44:30 +0900

    feat : money를 한 자리씩 비교하며 변환한 리스트 반환하는 기능 생성

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -8,7 +8,7 @@ package onboarding
  */
 fun solution5(money: Int): List<Int> {
     //프로그램 구현
-
+    return getCaches(money)
 }
 
 fun getCaches(money: Int): List<Int>{
@@ -16,5 +16,13 @@ fun getCaches(money: Int): List<Int>{
     //1. 각 화폐 가치만큼의 수들을 가진 리스트 생성
     val cacheList = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
 
+    val changedMoney = MutableList<Int>(cacheList.size){ 0 }
+    //2. money를 한 자리씩 비교하면서 1.에서 생성한 리스트를 참고해 변환
+    for(i in 0 until cacheList.size){
+        changedMoney[i] = money / cacheList[i]
+        money %= cacheList[i]
+    }
+
+    return changedMoney
 
 }
