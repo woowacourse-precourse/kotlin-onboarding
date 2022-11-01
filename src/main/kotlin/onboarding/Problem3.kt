bb40c6a42ce1135f1a932226e70b6dd6c69d9913
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-11-01 12:12:09 +0900

    refactor : P3문제 Iterable 확장함수 reduce, String 확장함수 count 를 이용하여, 코드 간략하게 리팩토링 작업

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,17 +1,11 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    var count = 0
+    val list = arrayListOf(3, 6, 9)
 
-    for (i in 1 .. number) {
-        var curNum = i
-
-        while (curNum != 0) {
-            if (curNum % 10 == 3 || curNum % 10 == 6 || curNum % 10 == 9) {
-                count++
-            }
-            curNum /= 10
+    return (0 ..  number).reduce() { acc, number ->
+        acc + number.toString().count {
+            it.digitToInt() in list
         }
     }
-    return count
 }
