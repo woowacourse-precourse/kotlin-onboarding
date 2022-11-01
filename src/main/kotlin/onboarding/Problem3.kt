696ef36be5c2263bb01a7d089f14cf4bbb4097d8
Yuhee Kim <dbgml4509@naver.com>
2022-11-01 19:52:52 +0900

    fix : solution3 확인 범위 수정

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -2,16 +2,22 @@ package onboarding
 
 fun solution3(number: Int): Int {
     //프로그램 구현
-    var count = 0
-    //4. 1부터 number까지 1,2. 를 동일하게 수행한다.
-    for(i in 1 until number){
-        val str = number.toString()
+    return countGame(number)
+}
 
-        for(chr in i){ //1. 숫자 중 3,6,9를 포함하는지 확인
-            if(chr == '3') count += 1 //2,3. 3,6,9를 몇 개 포함하는지 count로 누적 계산한다.
-            else if(chr == '6') count += 1
-            else if(chr == '9') count += 1
+fun countGame(number: Int): Int{
+    var count = 0
+
+    //1. 3,6,9 들어가는지 확인하기 위한 리스트 작성
+    val checkNumList = listOf('3','6','9')
+
+    //4. 1부터 number까지 1,2. 를 동일하게 수행한다.
+    for(i in 1 until number+1){
+        var stringList = i.toString().toList()
+        count += stringList.count{
+            it in checkNumList //2. 포함한다면, 3,6,9를 몇 개 포함하는지 확인한다. 3. 2. 에서 확인한 개수를 누적하여 더한다.
         }
     }
+
     return count
 }
