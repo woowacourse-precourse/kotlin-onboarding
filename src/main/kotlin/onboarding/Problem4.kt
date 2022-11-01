f3718c92ea0aa38bb3607506365108d373c4f72a
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-11-01 19:42:51 +0900

    docs : 문제 4 기능목록 , refactor : 주석 처리 / 함수 간소화

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -4,6 +4,23 @@ fun solution4(word: String): String {
 
     var list = ArrayList<String>()
 
+    reverse_string(word, list)
+
+    var result =""
+
+    list.forEach {
+        result += it
+    }
+
+    return result
+
+
+
+}
+// 문자를 사전순서로 반대로 계산하여 배열에 저장하는 함수
+fun reverse_string(word: String, list: ArrayList<String>){
+
+
     for(i in 0 until word.length) {
 
         if (word[i].isUpperCase()) {
@@ -22,18 +39,6 @@ fun solution4(word: String): String {
 
     }
 
-
-    var result =""
-
-    list.forEach {
-        result += it
-    }
-
-    return result
-
-
-
 }
-
 
 
