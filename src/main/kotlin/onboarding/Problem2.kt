3af4293a775ee1297b44b02b08bbeeca44420dd2
JinJoa <mitejoa@gmail.com>
2022-11-01 20:21:11 +0900

    문제2 - 연속하는 중복 문자 1개 삭제
    문자열 -> mutableList

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -7,18 +7,19 @@ fun main(){
 
 //암호문. 연속되는 중복 알파벳 제거
 fun solution2(cryptogram: String): String {
-    var decryption :String = cryptogram
-    // 중복 글자 없을때까지 무한 반복
-    while (true){
-        for (i in 0..decryption.length){
-            if(decryption[i] == decryption[i+1])
-                println(decryption[i])
-                //중복된값 삭제
-                println(decryption)
-
-        }
-    //    if() { //중복글자 없음
-            return decryption //해독문 리턴
-    //    }
+    //var decryption :String = cryptogram
+    var list = mutableListOf<Char>()
+    for (i in 0..cryptogram.length-1){
+        list.add(cryptogram[i].toChar())
     }
+    println(list.size)
+    for(i in 1..list.size){
+        if(list[i] == list[i-1]){
+            list.removeAt(i)
+            break
+        }
+    }
+    println(list.size)
+    return "---"
+
 }
