683d0d5234a3c2f415018dda8c7ab8d3d0ca37f8
Jisu Moon <barcode_no_1@naver.com>
2022-11-01 18:07:42 +0900

    페이지번호 곱셈 최고값 산출 기능

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -4,19 +4,19 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
         TODO("프로그램 구현")
 }
 
-/* 덧셈최고값 산출함수 */
-fun sumMax(value: List<Int>): Int {
-    var leftPageCalMax = 0
-    var rightPageCalMax = 0
+/* 곱셈최고값 산출함수 */
+fun multiplyMax(value: List<Int>): Int {
+    var leftPageCalMax = 1
+    var rightPageCalMax = 1
     val arr1 = value[0].toString().chunked(1).toTypedArray()
     for (s in arr1) {
-        leftPageCalMax += s.toInt()
+        leftPageCalMax *= s.toInt()
     }
     val arr2 = value[1].toString().chunked(1).toTypedArray()
     for (s in arr2) {
-        rightPageCalMax += s.toInt()
+        rightPageCalMax *= s.toInt()
     }
-    //print("$value\n왼쪽페이지합:$leftPageCalMax 오른쪽페이지합:$rightPageCalMax ")
-    //print("최고합:${Math.max(leftPageCalMax, rightPageCalMax)}\n")
+    //print("왼쪽페이지곱:$leftPageCalMax 오른쪽페이지곱:$rightPageCalMax ")
+    //print("최고곱:${Math.max(leftPageCalMax, rightPageCalMax)}\n")
     return Math.max(leftPageCalMax, rightPageCalMax)
 }
