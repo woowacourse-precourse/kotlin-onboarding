150914f885fb868299b9b83f3eb2bbde7feb3121
Jaehun-Jo <chojh922@gmail.com>
2022-11-01 16:44:31 +0900

    Problem2-제한사항 예외 처리

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,22 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    checkThrowException(cryptogram)
+
+    return ""
+}
+
+private fun checkThrowException(string: String) {
+    throwStringLengthException(string)
+    throwLowercaseException(string)
+}
+
+private fun throwStringLengthException(string: String) {
+    if (string.isEmpty() || string.length > 1_000) throw error("문자열 길이가 1 이상 1000 이하여야 합니다!")
+}
+
+private fun throwLowercaseException(string: String) {
+    for (i in string.indices) {
+        if (!string[i].isLowerCase()) throw error("문자열이 알파벳 소문자로만 이루어져 있어야 합니다!")
+    }
 }
