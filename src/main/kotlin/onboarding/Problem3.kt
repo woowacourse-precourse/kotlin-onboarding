405d3031e49cbb6b5095d980faa8b5f880689472
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-01 17:47:20 +0900

    22.11.01 problem3 기능목록 및 기능 1번 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,16 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var threeCount = 0
+    var sixCount = 0
+    var nineCount = 0
+
+    if (isCorrect(number)) {
+
+    } else return -1
+}
+
+fun isCorrect(number: Int): Boolean {
+    val condition1 = number in 1..10000
+    return condition1
 }
