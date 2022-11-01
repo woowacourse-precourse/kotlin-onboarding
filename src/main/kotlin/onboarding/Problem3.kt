f17d459c807d71620bb114d6d3da8c17c117f416
JinJoa <mitejoa@gmail.com>
2022-11-01 19:28:14 +0900

    문제3 -clap_cnt 증가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,25 +1,19 @@
 package onboarding
 
-fun main(){
-    solution3(33)
-}
-
+//369게임
 fun solution3(number: Int): Int {
-    //369게임
-    var clap_cnt :Int = 0 //박수 카운터
 
-    for (i in 1..number) {
+    var clap_cnt :Int = 0 //박수 카운터
+    for (i in 1..number){
         //숫자 문자열로 분리
         val num_to_str = i.toString()
         //자리수마다 3,6,9 확인
-        for (j in 0..num_to_str.length - 1) {
+        for (j in 0..num_to_str.length-1){
             var n = num_to_str[j]
-            print(num_to_str[j])
             if( n == '3' || n == '6' || n == '9'){
-                print("짝")
+                clap_cnt += 1
             }
         }
-        println()
     }
-        return clap_cnt
+    return clap_cnt
 }
