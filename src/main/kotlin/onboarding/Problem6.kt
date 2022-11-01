2c4123791bde30dad995b7daa25c179ddc84c637
Jihee.Han <42907876+jihee-dev@users.noreply.github.com>
2022-11-01 23:24:52 +0900

    feat: solve problem 6

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,31 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val result = mutableSetOf<String>()
+
+    forms.forEachIndexed { index, info ->
+        val address = info[0]
+        val name = info[1]
+
+        if (name.length == 1) return@forEachIndexed
+
+        for (startChar in name.indices - 2) {
+            val token = if (name.length != 2) name.substring(startChar, startChar + 2) else name
+
+            var isDuplicated = false
+            for (i in index + 1 until forms.size) {
+                if (i < forms.size) {
+                    if (forms[i][1].contains(token)) {
+                        isDuplicated = true
+                        result.add(forms[i][0])
+                        break
+                    }
+                }
+            }
+
+            if (isDuplicated) result.add(address)
+        }
+    }
+
+    return result.sorted()
 }
