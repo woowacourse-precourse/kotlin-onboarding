4bd41f05c09a4d4e7d8d33712505e752849e1ad6
YeonwooLee <fox_93@naver.com>
2022-11-01 23:00:48 +0900

    problem2 solve

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,40 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+
+    return cryp(cryptogram)
+}
+
+fun cryp(word:String):String{
+    var change=false
+    var result = ""
+    var i=0
+    var s = ""
+    var count=0
+    while(i<=word.length-1){
+
+        s = word.substring(i,i+1)
+//        println(s)
+        var j = i+1
+        count = 0
+        while(j<word.length && word.substring(j,j+1).equals(s)){
+//            println(count)
+            j++
+            count++
+            change=true
+        }
+        i+=count
+        if(count==0){
+            result+=s
+//            println(result)
+        }
+        i++
+    }
+//    if(count==0) result += s
+    if(change) return cryp(result)
+    return result
+}
+
+fun main(){
+    println(cryp("cbabbcabba"))
 }
