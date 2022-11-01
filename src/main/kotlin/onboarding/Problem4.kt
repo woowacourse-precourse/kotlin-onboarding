c08be01ba9e3cad7beb042bd244db85b1bda255a
s9hn <tnghqn@naver.com>
2022-11-01 19:08:50 +0900

    [REFACTOR(미션3)] 코드 정리 및 리드미 기능목록 수정
    1. 주석 추가
    2. 변수명 및 함수명 재설정
    3. 함수분리
    4. 리드미 기능목록 재수정

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,48 +1,48 @@
 package onboarding
 
 fun solution4(word: String): String {
-    val lowerCaseMap = mappingLowerCase()
-    val upperCaseMap = mappingUpperCase()
+    return translate(word)
+}
 
+fun translate(word: String): String { // 문자열 검사 및 치환
+
+    val momFrog = word
     var sonFrog = ""
+    for (i in momFrog.indices) {
 
-
-    for (i in word.indices) {
-
-        if (word[i] == ' ') {
+        if (momFrog[i] == ' ') {
             sonFrog += ' '
 
-        } else if (word[i] in 'a'..'z') {
-            sonFrog += lowerCaseMap.getValue(word[i]).toString()
+        } else if (momFrog[i] in 'a'..'z') {
+            sonFrog += mappingLowerCase().getValue(momFrog[i]).toString()
 
-        } else if (word[i] in 'A'..'Z') {
-            sonFrog += upperCaseMap.getValue(word[i]).toString()
+        } else if (momFrog[i] in 'A'..'Z') {
+            sonFrog += mappingUpperCase().getValue(momFrog[i]).toString()
 
         }
     }
+
     return sonFrog
 }
 
-fun mappingLowerCase(): MutableMap<Char, Char> {
+fun mappingLowerCase(): MutableMap<Char, Char> { // 소문자 Map 리스트 구현
     var temp = 0
     val lowerCase = mutableMapOf<Char, Char>()
     for (i in 'a'..'z') {
         temp++
         val reverseLowerCase = (123 - temp).toChar()
         lowerCase[i] = reverseLowerCase
-
     }
     return lowerCase
 }
 
-fun mappingUpperCase(): MutableMap<Char, Char> {
+fun mappingUpperCase(): MutableMap<Char, Char> { // 대문자 Map 리스트 구현
     var temp = 0
     val upperCase = mutableMapOf<Char, Char>()
     for (i in 'A'..'Z') {
         temp++
         val reverseUpperCase = (91 - temp).toChar()
         upperCase[i] = reverseUpperCase
-
     }
     return upperCase
 }
