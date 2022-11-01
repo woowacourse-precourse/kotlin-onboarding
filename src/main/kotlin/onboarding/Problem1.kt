5af2d40b493871a14717bf13e2aac9dac73a8cb4
lsakee <lsls4868@gmail.com>
2022-11-01 10:58:31 +0900

    Feat : main solution1 구현 -findpageMaxCalculation() 함수로 얻은 user들의 페이지의 값을 비교 -올바른 페이지 확인

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -8,12 +8,26 @@ import java.util.*
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 
-    var result: Int =0
-
+    var result: Int
+    //책 페이지 수를 계산(더했을 경우,곱할 경우)Max값을 리스트에 저장
+    val comparePobiList = findpageMaxCalculation(pobi)
+    val compareCrongList = findpageMaxCalculation(crong)
+    //포비와 크롱의 맥스 값을 비교
+    if (Collections.max(comparePobiList) > Collections.max(compareCrongList)) {
+        result = 1
+    } else if (Collections.max(comparePobiList) == Collections.max(compareCrongList)) {
+        result = 0
+    } else {
+        result = 2
+    }
+    //페이지수가 1장이상차이나는 경우는 오류로 선정
+    if (pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1)
+        result = -1
     return result
 
 }
-fun findpageMaxCalculation(userOpenPage: List<Int>):List<Int>{
+
+fun findpageMaxCalculation(userOpenPage: List<Int>): List<Int> {
 
     val userPageMaxValuelist = mutableListOf<Int>()
 
@@ -24,10 +38,11 @@ fun findpageMaxCalculation(userOpenPage: List<Int>):List<Int>{
     }
     return userPageMaxValuelist
 }
-fun calculatePage(StringConversion:String):Int{
-    var PlusNum=0
-    var MultiplyNum=1
-    var maxValue:Int
+
+fun calculatePage(StringConversion: String): Int {
+    var PlusNum = 0
+    var MultiplyNum = 1
+    var maxValue: Int
     for (j in 0..StringConversion.length - 1) {
         //문자열로 분리하여 양쪽페이지의 최대 값을 저장 ex) 2 2 7
         val stringNumber = StringConversion.substring(j until j + 1).toInt()
