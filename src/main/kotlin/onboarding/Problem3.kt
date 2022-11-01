4def813633f7319bf2b6d72510ffc00520c0291c
JinJoa <mitejoa@gmail.com>
2022-11-01 19:29:18 +0900

    문제3 - 예외사항 및 테스트 완료

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -2,6 +2,8 @@ package onboarding
 
 //369게임
 fun solution3(number: Int): Int {
+    //number는 1 이상 10,000 이하인 자연수이다.
+    if(number < 1 || number > 10000){return -1}
 
     var clap_cnt :Int = 0 //박수 카운터
     for (i in 1..number){
