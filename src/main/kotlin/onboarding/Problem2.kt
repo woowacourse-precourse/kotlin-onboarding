6340fcf2d49716fef01059a8f6fe24b0e7f3449b
Suheon Song <ssheon0812@naver.com>
2022-11-01 18:18:43 +0900

    refactor: [Problem2] 중복제거시 정규표현식을 활용

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,33 +1,21 @@
 package onboarding
 
+val regex = Regex(pattern ="""([a-z])\1+""");
 
 fun solution2(cryptogram: String): String {
-    return removeConsecutiveDuplicateCharacters(cryptogram)
+    return removeConsecutiveDuplicates(cryptogram)
 }
 
-private fun removeConsecutiveDuplicateCharacters(cryptogram: String): String {
-    var decodedString = ""
-    var isConsecutive = false
+private fun removeConsecutiveDuplicates(cryptogram: String): String {
+    var clearText = cryptogram
 
-    for (i in 0 until cryptogram.length) {
-        if (i == cryptogram.length - 1) {
-            // 현재 검사하고 있는 문자가 마지막 문자이고 그 전의 문자와 연속적으로 반복되지 않고 있다면 해독한 문자열에 추가한다.
-            if (!isConsecutive) {
-                decodedString += cryptogram[i]
-            }
-        } else {
-            if (cryptogram[i] == cryptogram[i + 1]) {
-                isConsecutive = true
-            } else {
-                // 현재 검사하고 있는 문자가 다음 문자와 다르고 그 전의 문자와 연속적으로 반복되지 않고 있다면 해독한 문자열에 추가한다.
-                if (!isConsecutive) {
-                    decodedString += cryptogram[i]
-                }
-                isConsecutive = false
-            }
-        }
-    }
+    do {
+        clearText = clearText.replace(regex, "")
+    } while (shouldRepeats(clearText))
 
-    if (cryptogram.length != decodedString.length) return removeConsecutiveDuplicateCharacters(decodedString)
-    return decodedString
+    return clearText
+}
+
+private fun shouldRepeats(cryptogram: String): Boolean {
+    return regex.containsMatchIn(cryptogram)
 }
