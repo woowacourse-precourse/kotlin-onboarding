207e1f0644c6988250bdc0eea51fa180306a1c65
kitearc <Yeonholee.dev@gmail.com>
2022-11-01 22:55:17 +0900

    Implement Function

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -2,4 +2,50 @@ package onboarding
 
 fun solution5(money: Int): List<Int> {
     TODO("프로그램 구현")
+    var answer = mutableListOf(0,0,0,0,0,0,0,0,0)
+    var _money = money
+
+    if (_money >= 50000) {
+        answer[0] = _money / 50000
+        _money = _money % 50000
+    }
+
+    if (_money >= 10000) {
+        answer[1] = _money / 10000
+        _money = _money % 10000
+    }
+
+    if (_money >= 5000) {
+        answer[2] = _money / 5000
+        _money = _money % 5000
+    }
+
+    if (_money >= 1000) {
+        answer[3] = _money / 1000
+        _money = _money % 1000
+    }
+
+    if (_money >= 500) {
+        answer[4] = _money / 500
+        _money = _money % 500
+    }
+
+    if (_money >= 100) {
+        answer[5] = _money / 100
+        _money = _money % 100
+    }
+
+    if (_money >= 50) {
+        answer[6] = _money / 50
+        _money = _money % 50
+    }
+
+    if (_money >= 10) {
+        answer[7] = _money / 10
+        _money = _money % 10
+    }
+
+    answer[8] = _money
+
+    return answer
 }
--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -2,4 +2,17 @@ package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
+    var result = mutableListOf<String>()
+    for (e in forms) {
+        if (e[0].split("@")[1] != "email.com") continue
+        for (i in 0 until e[0].length) {
+            if (e[0][i] == e[0][i+1]) break
+            else if (e[0][i+1].toInt() - e[0][i].toInt() == 1) break
+            else if (e[0][i+1] == '@') {
+                result.add(e[0])
+                break
+            }
+        }
+    }
+    return result.sorted()
 }
