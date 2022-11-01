89f8648d066008358038e97029f17984bf907dfe
Yuhee Kim <dbgml4509@naver.com>
2022-11-01 19:32:26 +0900

    fix : 예외 사항 수정

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -7,18 +7,10 @@ package onboarding
     3. 2. 에서 확인한 개수를 누적하여 더한다.
     4. 1부터 number까지 1,2. 를 동일하게 수행한다.
  */
-//예외 사항
-/*
-    1. number가 자연수가 아닌 경우
- */
+
 
 fun solution3(number: Int): Int {
     //프로그램 구현
-    return countGame(number)
-}
-
-fun countGame(number: Int): Int{
-
     var count = 0
     //4. 1부터 number까지 1,2. 를 동일하게 수행한다.
     for(i in 1 until number){
