f48dd8a0b0af22abf80961adc97ef407a6a7977e
hayeon <hayeon2401@naver.com>
2022-11-01 19:02:29 +0900

    solution6 구현

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,32 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+
+    var names = forms.map{it[1]}
+    var chopNames = mutableListOf<Set<String>>()
+    var emails = mutableListOf<String>()
+
+    // 이름 2글자씩 분리
+    for(i in names){
+        var set = mutableSetOf<String>()
+        for(j in 0..i.length-2){
+            set.add(i.substring(j..j+1))
+        }
+        chopNames.add(set)
+    }
+
+    // 중복 확인
+    for(n in 0..names.size - 2){
+        for(m in n+1..names.size-2){
+            var union = chopNames[n]+chopNames[m]
+            if(union.size != chopNames[n].size + chopNames[m].size){
+                emails.add(forms[n][0])
+                emails.add(forms[m][0])
+            }
+        }
+    }
+
+    var result = emails.distinct().sorted()
+    return result
+
 }
