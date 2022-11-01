ce4b0e11cb9d9167e6003fd2ff758ade7fa70a11
Suheon Song <ssheon0812@naver.com>
2022-11-01 18:24:45 +0900

    refactor: [Problem3] collection 순환시 for문 대신 forEach사용

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -9,11 +9,12 @@ private fun count369(number: Int): Int {
     var count = 0
     val numberToString = number.toString()
 
-    for (digit in numberToString) {
+    numberToString.forEach { digit ->
         if (digit == '3' || digit == '6' || digit == '9') {
             count++
         }
     }
+
     return count
 }
 
