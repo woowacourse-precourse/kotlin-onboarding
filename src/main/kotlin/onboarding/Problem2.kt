8f834f7e893915cc8e952f854b65e07004186f68
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-01 16:10:27 +0900

    22.11.01 problem2 기능 4번 구현 및 테스트 완료

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,10 @@
 package onboarding
 
+fun main() {
+    val word =  "browoanoommnaon"
+    println(solution2(word))
+}
+
 fun solution2(cryptogram: String): String {
     var cryptogram = cryptogram
 
@@ -8,7 +13,16 @@ fun solution2(cryptogram: String): String {
         while (true) {
             val cryptogramNumList = arrayListOf<Int>()
             for (i in 0..cryptogram.length - 2) if (cryptogram[i] == cryptogram[i + 1]) cryptogramNumList.add(i)
+            if (cryptogramNumList.size != 0) {
+                var adjustStartIndex = 0
+                var adjustEndIndex = -2
 
+                for (i in 0 until cryptogramNumList.size) {
+                    cryptogram = cryptogram.removeRange(cryptogramNumList[i] - adjustStartIndex, cryptogramNumList[i] - adjustEndIndex)
+                    adjustStartIndex += 2
+                    adjustEndIndex += 2
+                }
+            } else break
         }
         return cryptogram
     } else return "wrong cryptogram"
