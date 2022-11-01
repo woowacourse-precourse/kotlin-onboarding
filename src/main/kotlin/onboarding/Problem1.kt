1ddba009c42d360885c476e5e3d94a392e5a2902
cyjadela <adelacho@naver.com>
2022-11-01 18:33:22 +0900

    style: [문제1] code formatting

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -8,25 +8,23 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
         var page: Int = page
 
         while (page != 0) {
-            plusSum = plusSum + (page % 10)
-            mutlipleSum = mutlipleSum * (page % 10)
+            plusSum += (page % 10)
+            mutlipleSum *= (page % 10)
             page /= 10
         }
 
-        if (plusSum < mutlipleSum) {
-            return mutlipleSum
-        }
-        else
-            return plusSum
+        return if (plusSum < mutlipleSum) {
+            mutlipleSum
+        } else
+            plusSum
     }
 
     fun getLeftRightMax(left: Int, right: Int): Int {
-        var max: Int
 
-        if (left < right) {
-            max = right
+        var max: Int = if (left < right) {
+            right
         } else
-            max = left
+            left
 
         return max
     }
@@ -34,18 +32,18 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     var pobiMax = getLeftRightMax(getPageMax(pobi[0]), getPageMax(pobi[1]))
     var crongMax = getLeftRightMax(getPageMax(crong[0]), getPageMax(crong[1]))
 
-    if ((pobi[0] + 1 == pobi[1]) && (crong[0] + 1 == crong[1])) {
+    return if ((pobi[0] + 1 == pobi[1]) && (crong[0] + 1 == crong[1])) {
         if (pobiMax == crongMax) {
-            return 0
+            0
         } else {
             if (pobiMax > crongMax) {
-                return 1
+                1
             } else if (pobiMax < crongMax) {
-                return 2
+                2
             } else
-                return -1
+                -1
         }
     } else
-        return -1
+        -1
 
 }
