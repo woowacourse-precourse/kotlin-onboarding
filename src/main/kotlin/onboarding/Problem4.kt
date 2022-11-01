2d0f53d31eb232504684feb8c5838eb5835b3d4a
lh99j <grand2181@gmail.com>
2022-11-01 13:17:04 +0900

    Solve Problem 4

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var str = ""
+    for(element in word){
+        str += reverseStr(element)
+    }
+
+    return str
+}
+
+fun reverseStr(num : Char) : Char{
+    val temp : Int = num.code
+
+    return when (temp) {
+        in 65..90 -> (155 - temp).toChar()
+        in 97 .. 122 -> (219 - temp).toChar()
+        else -> num
+    }
 }
