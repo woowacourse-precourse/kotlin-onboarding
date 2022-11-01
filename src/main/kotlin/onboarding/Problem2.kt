df58dab5dbc4c220fc9694d4a863d6263217229b
Jihee.Han <42907876+jihee-dev@users.noreply.github.com>
2022-11-01 22:53:09 +0900

    feat: solve problem 2

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,25 @@
 package onboarding
 
+import java.util.*
+
+fun main() {
+    println(solution2("browoanoommnaon"))
+    println(solution2("zyelleyz"))
+}
+
+
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    val charStack = Stack<Char>()
+
+    cryptogram.forEach { char ->
+        if (charStack.isNotEmpty()) {
+            if (char == charStack.peek()) charStack.pop()
+            else charStack.push(char)
+        }
+        else charStack.push(char)
+    }
+
+    var result = ""
+    charStack.forEach { result += it }
+    return result
 }
