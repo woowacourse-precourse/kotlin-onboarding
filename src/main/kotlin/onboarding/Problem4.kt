9cb941e5911b22aa48810dd0fc2517bdfe9480cf
2taezeat <xorudfl772@naver.com>
2022-11-01 13:22:57 +0900

    feat : 0. 알파벳 외의 문자인지 체크하는 함수 작성

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,3 +3,13 @@ package onboarding
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
+
+
+fun isAlphabet(char: Char) : Boolean {
+    val charCode = char.code
+    if ( ('a'.code <= charCode && charCode <= 'z'.code) || (('A'.code <= charCode && charCode <= 'Z'.code)) ) {
+        return true
+    }
+
+    return false
+}
