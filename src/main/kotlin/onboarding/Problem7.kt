f16a652569446f48ac1e932143bf30af9d018407
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-11-01 12:24:19 +0900

    feat: (문제7) 사용자의 친구 찾기 기능 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,17 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val userFriends = findUserFriends(user, friends)
+}
+
+fun findUserFriends(user: String,friends: List<List<String>>): MutableSet<String>{
+    val userFriends = mutableSetOf<String>()
+    friends.forEach{
+        if(it[0] == user) {
+            userFriends.add(it[1])
+        } else if (it[1] == user) {
+            userFriends.add(it[0])
+        }
+    }
+    return userFriends
 }
