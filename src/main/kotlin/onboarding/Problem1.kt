b8d6c6d85a95c7e17e5fff16f1236c6e2e10b593
최재훈 <be_clever@naver.com>
2022-11-01 17:42:45 +0900

    feat: 입력값을 검증하는 기능 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,27 @@
 package onboarding
 
+import kotlin.math.max
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if (!isPageValid(pobi) || !isPageValid(crong)) {
+        return -1
+    }
+}
+
+fun isPageValid(pageNumbers: List<Int>): Boolean {
+    if(pageNumbers[0] + 1 != pageNumbers[1]) {
+        return false
+    }
+
+    if(pageNumbers[0] % 2 != 1 || pageNumbers[1] % 2 != 0) {
+        return false
+    }
+
+    for(page in pageNumbers) {
+        if(page == 1 || page == 4) {
+            return false
+        }
+    }
+
+    return true
 }
