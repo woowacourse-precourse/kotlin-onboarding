05486b80d3b748235dcfcfa8afe7aa5bae7fb469
hayeon <hayeon2401@naver.com>
2022-11-01 19:47:23 +0900

    solution1 수정 (제한사항 예외처리)

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,18 +1,27 @@
 package onboarding
 
+import org.assertj.core.api.Assertions.assertThat
+
 import kotlin.math.*
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+
     val pobiMaxNum = findMaxNum(pobi)
     val crongMaxNum = findMaxNum(crong)
 
+    // 제한사항 예외처리
+    if(pobi.size != 2 || crong.size != 2 || pobi[1]-pobi[0] != 1 || crong[1]-crong[0] != 1) return -1
+
+    // 경우에 따라 결과 값 나누기
     if(pobiMaxNum > crongMaxNum) return 1
     else if(pobiMaxNum < crongMaxNum) return 2
     else if(pobiMaxNum == crongMaxNum) return 0
     else return -1
+
 }
 
 fun findMaxNum(list: List<Int>): Int{
+
     var maxNum = 0
 
     for(i in list){
@@ -28,4 +37,5 @@ fun findMaxNum(list: List<Int>): Int{
     }
 
     return maxNum
+
 }
