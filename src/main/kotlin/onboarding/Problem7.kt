23c466ca1fc217288ff1c4e55e5d6562f137832a
권용민 <briandr@naver.com>
2022-11-01 14:52:41 +0900

    [Problem7] 유저 친구 찾기  - 친구 관계 리스트를 통해 유저의 친구를 찾아 친구 집합을 만든다.

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,15 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val userFriendList = mutableListOf<String>()
+
+    searchUserFriends(user, userFriendList, friends)
 }
+
+private fun searchUserFriends(user: String, userFriendList: MutableList<String>, relation: List<List<String>>) {
+    for (list in relation) {
+        if (list[0] == user) userFriendList.add(list[1])
+        if (list[1] == user) userFriendList.add(list[0])
+    }
+}
+
