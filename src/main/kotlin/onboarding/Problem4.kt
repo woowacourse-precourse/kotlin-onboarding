47d0506375d16465bbcb2bc392b5dfc941d42101
JinJoa <mitejoa@gmail.com>
2022-11-01 21:51:39 +0900

    문제4 - 문자열 거꾸로

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,15 +1,23 @@
 package onboarding
-fun main(){
-    solution4("helloeew")
-}
+
 fun solution4(word: String): String {
     //리턴값
-    val revese_word : String =""
+    var revese_word : String =""
     //a to z & z to a 배열
     val dict = mutableListOf<Char>()
     for (c in 'a'..'z'){ dict.add(c) }
     var revese_dict  = dict.reversed() as MutableList<Char>
 
+    //문자열 거꾸로
+    for(i in 0..word.length-1){
+        for(j in 0..dict.size-1){
+            if (word[i] == dict[j]){
+                revese_word += revese_dict[j]
+            }
+        }
+    }
 
+    
+    println(revese_word)
     return revese_word
 }
