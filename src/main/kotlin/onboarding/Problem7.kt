96f993a0c95ce7eeb553161f066bcf96d9ba5037
lee989898 <@@qwer1352>
2022-11-01 14:36:06 +0900

    [feat](Problem7) 잘못된 값이 들어왔을때 원인을 알려주는 함수 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,31 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    if (isAppropriateInput(user, friends, visitors).isNotEmpty()) {
+        return isAppropriateInput(user, friends, visitors)
+    }
+
+    return listOf()
+}
+
+fun isAppropriateInput(
+    user: String,
+    friends: List<List<String>>,
+    visitors: List<String>
+): List<String> {
+    if (user.length !in 1..30) {
+        return listOf("user는 길이가 1 이상 30 이하인 문자열이어야 합니다.")
+    }
+    if (friends.size !in 1..10000) {
+        return listOf("friends는 길이가 1 이상 10,000 이하인 리스트/배열이어야 합니다.")
+    }
+    if (visitors.size !in 0..10000) {
+        return listOf("visitors는 길이가 0 이상 10,000 이하인 리스트/배열이어야 합니다.")
+    }
+    for (i in friends.indices) {
+        if (friends[i].first().length !in 0..30 || friends[i].last().length !in 0..30) {
+            return listOf("아이디는 길이가 1 이상 30 이하인 문자열이어야 한다.")
+        }
+    }
+    return listOf()
 }
