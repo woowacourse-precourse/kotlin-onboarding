97da6241ecaec3ef904f837b1680d279560da53e
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-01 21:37:39 +0900

    user와 친구인 이름을 찾는 findUserFriends 메소드 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,23 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    /*
+    var userFriendsList // user와 친구인 이름 목록
+    var friendsFriendsList // user의 친구들과 친구인 이름 목록
+     */
+}
+
+/* user와 친구인 이름을 찾는 메소드 */
+fun findUserFriends(user: String, friends: List<List<String>>): ArrayList<String> {
+    var userFriendsList = arrayListOf<String>()
+    for(i in friends.indices){
+        if(friends[i][0] == user){
+            userFriendsList.add(friends[i][1])
+            continue
+        }
+        if(friends[i][1] == user){
+            userFriendsList.add(friends[i][0])
+        }
+    }
+    return userFriendsList
 }
