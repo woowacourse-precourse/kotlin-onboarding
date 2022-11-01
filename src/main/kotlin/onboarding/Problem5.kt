c4b4a004d5172ecdff8f59605357d64f372036b0
최재훈 <be_clever@naver.com>
2022-11-01 19:01:44 +0900

    액면가로 나눈 몫을 리스트에 저장하는 기능

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -2,6 +2,13 @@ package onboarding
 
 val faceValues = arrayOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
 
-fun solution5(money: Int): List<Int> {
+fun solution5(_money: Int): List<Int> {
+    var money = _money
+    val result = mutableListOf<Int>()
 
+    for(face in faceValues) {
+        result.add(money / face)
+    }
+
+    return result
 }
