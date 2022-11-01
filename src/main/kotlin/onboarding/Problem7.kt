7d111e55e38a327b5de58b4480c91a9692f27631
hansh0101 <hansh0101@naver.com>
2022-11-01 15:07:24 +0900

    [REFACTOR] Refactor Problem7

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,9 +5,16 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    val result: List<String>
-    val resultMap = mutableMapOf<String, Int>()
+    var resultMap = mutableMapOf<String, Int>()
 
+    val friendsMap: MutableMap<String, MutableList<String>> = getFriendsRelationshipMap(friends)
+    resultMap = getFriendsOfFriendsPoint(user, resultMap, friendsMap)
+    resultMap = getVisitorsPoint(resultMap, visitors)
+
+    return translateResultMapToResultList(user,resultMap, friendsMap)
+}
+
+private fun getFriendsRelationshipMap(friends: List<List<String>>): MutableMap<String, MutableList<String>> {
     val friendsMap = mutableMapOf<String, MutableList<String>>()
     friends.forEach {
         if (friendsMap.containsKey(it[0])) {
@@ -22,8 +29,16 @@ fun solution7(
             friendsMap[it[1]] = mutableListOf(it[0])
         }
     }
+    return friendsMap
+}
+
+private fun getFriendsOfFriendsPoint(
+    user: String,
+    resultMap: MutableMap<String, Int>,
+    friendsMap: MutableMap<String, MutableList<String>>
+): MutableMap<String, Int> {
     friendsMap[user]?.forEach { friend ->
-        requireNotNull(friendsMap[friend]).filterNot { it == user }.forEach { friendOfFriend ->
+        requireNotNull(friendsMap[friend]).forEach { friendOfFriend ->
             if (resultMap.containsKey(friendOfFriend)) {
                 resultMap[friendOfFriend] = resultMap.getValue(friendOfFriend) + 10
             } else {
@@ -31,18 +46,29 @@ fun solution7(
             }
         }
     }
+    return resultMap
+}
 
-    visitors.filterNot {
-        friendsMap[user]?.contains(it) ?: false
-    }.forEach {
+private fun getVisitorsPoint(resultMap: MutableMap<String, Int>, visitors: List<String>): MutableMap<String, Int> {
+    visitors.forEach {
         if (resultMap[it] == null) {
             resultMap[it] = 1
         } else {
             resultMap[it] = resultMap.getValue(it) + 1
         }
     }
+    return resultMap
+}
 
-    result = resultMap.toList().sortedByDescending { it.second }.map { it.first }.take(5)
-
-    return result
+private fun translateResultMapToResultList(
+    user: String,
+    resultMap: MutableMap<String, Int>,
+    friendsMap: MutableMap<String, MutableList<String>>
+): List<String> {
+    return resultMap.filterNot { it.key == user || friendsMap[user]?.contains(it.key) ?: false }
+        .toList()
+        .sortedBy { it.first }
+        .sortedByDescending { it.second }
+        .map { it.first }
+        .take(5)
 }
