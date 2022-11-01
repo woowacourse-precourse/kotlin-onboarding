61c2772e9184f356ec488a19e07ff7a67ce88d7b
Yuhee Kim <dbgml4509@naver.com>
2022-11-01 23:38:21 +0900

    feat : 최종 결과 return

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -16,6 +16,21 @@ fun solution7(
 ): List<String> {
     //프로그램 구현
 
+//    1. friends에서 user의 친구를 확인한다.
+    val userFriends = checkFriends(user, friends)
+
+
+    var friendsFriendsResult = friendsFriends(user, userFriends, friends)
+    var visitorsResult = checkVisitors(userFriends, visitors)
+    var result = mutableMapOf<String, Int>()
+
+    for(i in 0 until friendsFriendsResult.indices){
+        if(friendsFriendsResult[i][0] == visitorsResult[i][0])
+            result[friendsFriendsResult[i][0]] = (friendsFriendsResult[i][1] + visitorsResult[i][1])
+    }
+
+    return result.filter{ it.value > 0}.toList().sortedWith(comparator).map{it.first}
+
 }
 
 fun checkFriends(user: String, friends: List<List<String>>): List<String>{
@@ -31,7 +46,7 @@ fun checkFriends(user: String, friends: List<List<String>>): List<String>{
 
 fun friendsFriends(user: String, userFriends : List<String>, friends: List<List<String>>): MutableList<String, Int>{
 
-    var scoreList : mutableMapOf<String, Int>()
+    var scoreList = mutableMapOf<String, Int>()
     //2. 1.에서 확인된 친구들의 친구를 friends에서 확인한다.
     //3. 2.에서 확인된 친구의 친구는 10점 부여한다.
     for(i in friends){
@@ -47,7 +62,7 @@ fun friendsFriends(user: String, userFriends : List<String>, friends: List<List<
 
 fun checkVisitors(userFriends: List<String>, visitors: List<String>): MutableList<String, Int>{
 
-    var scoreList : mutableMapOf<String, Int>()
+    var scoreList = mutableMapOf<String, Int>()
 //    4. visitors에서 user의 친구가 아닌 사람들을 확인한다.
 //    5. 4.에서 확인된 사람들은 1점 부여한다.
     for(i in visitors){
