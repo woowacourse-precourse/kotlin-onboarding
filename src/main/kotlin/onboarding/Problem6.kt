1eb16e04502ca28584187dd66eb3581b7de01d6c
lkeonwoo94 <lkeonw0o@naver.com>
2022-11-01 22:36:29 +0900

    solve Prob6

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,8 +1,20 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    var ret = mutableListOf<String>()
+    for (crewIdx in forms.indices)
+        for (i in 0 until crewIdx - 1)
+            if (checkDup(forms[crewIdx][1], forms[i][1])){
+                ret.addAll(listOf(forms[crewIdx][0], forms[i][0]))
+                break
+            }
+    return ret.toSet().toList().sorted()
 }
 
-/* 1. Calculate duplicates when two or more than consecutive elements are the same */
-/* 2. Sort asc*/
+/* 1. Check duplicates when two or more than consecutive elements are the same */
+fun checkDup(str1: String, str2: String): Boolean {
+    for (i in 0 until str1.length - 1)
+        if (str2.contains(str1.substring(i, i + 2)))
+            return true
+    return false
+}
