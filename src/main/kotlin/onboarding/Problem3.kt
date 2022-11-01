09b2a992275e3df5a055b0ca344addbc4b44e9d7
lsakee <lsls4868@gmail.com>
2022-11-01 11:29:36 +0900

    Feat : checkClapCount()메소드 추가 -문자열로 변환하여 3,6,9가 있는지 확인후 clapCount 증가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,18 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    //박수수를 계산하는 함수로 리턴
+    return number
+}
+
+//3,6,9가 들어 있는지 확인하는 함수
+fun checkClapCount(numberChangeString: String): Int {
+    var plusClapCount = 0
+    //각 숫자의 자리별로 3,6,9가 있는지 확인후 존재 하면 카운트 증가
+    for (j in numberChangeString) {
+        if (j == '3' || j == '6' || j == '9') {
+            plusClapCount += 1
+        }
+    }
+    return plusClapCount
 }
