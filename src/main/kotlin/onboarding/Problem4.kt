c2321c15ebaa6ced5ae7a8b3395951bcc073433c
meltnia <72488905+meltnia@users.noreply.github.com>
2022-11-01 21:58:39 +0900

    Add files via upload

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,14 +1,16 @@
 package onboarding
 
 fun solution4(word: String): String {// A=65~90 a=97~122 26문자
-    val changedtext=word.toCharArray()
+    var changedtext=word.toCharArray()
 
 
     for(i: Int in 0 until word.length){
-        changedtext[i]=change(changedtext[i])
+       changedtext[i]=change(changedtext[i])
     }
 
-    return changedtext.toString()
+    var CtS = String(changedtext)
+
+    return CtS
 }
 
 
@@ -16,11 +18,11 @@ fun solution4(word: String): String {// A=65~90 a=97~122 26문자
 fun change(a:Char):Char {
     var i=a.toInt()
     if(i<=90&&i>=65){//대문자일경우
-        i-=65
-        i=25-i
-        i+=65
+            i-=65
+            i=25-i
+            i+=65
 
-        return i.toChar()
+            return i.toChar()
     }
     if(i>=97&&i<=122){//소문자일경우
         i-=97
@@ -31,6 +33,6 @@ fun change(a:Char):Char {
         return i.toChar()
     }
 
-    return a//그 외는 그대로 반환
+    return i.toChar()//그 외는 그대로 반환
     
 }
