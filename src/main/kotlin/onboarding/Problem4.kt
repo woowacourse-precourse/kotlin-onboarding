0b60942cbf364661254848a5ea5be17a4128fcd6
Jaehun-Jo <chojh922@gmail.com>
2022-11-01 18:50:09 +0900

    Problem4-제한사항 예외 처리

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,15 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    checkThrowException(word)
+    return ""
+}
+
+private fun checkThrowException(word: String) {
+    throwStringLengthException(word)
+}
+
+private fun throwStringLengthException(word: String) {
+    if (word.isEmpty() || word.length > 1_000) throw error("word는 길이가 1 이상 1,000 이하인 문자열입니다!")
+
 }
