80e97c32e0e66e2b5c41e347d9e05d4e0cc5599c
s9hn <tnghqn@naver.com>
2022-11-01 21:44:18 +0900

    [REFACTOR(미션7)] 코드 정리 및 리드미 기능목록 수정
    1. 주석 추가
    2. 함수 분리
    3. 변수명 및 함수명 변경
    4. 리드미 기능목록 재수정

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,60 +5,87 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
+
+    return searchUnknownFriends(user, friends, visitors) // 한글정렬, 점수정렬, 5명까지 표기
+        .toList()
+        .sortedBy { it.first }
+        .sortedByDescending { it.second }
+        .map { it.first }.take(5)
+}
+
+fun searchUnknownFriends(
+    user: String,
+    friends: List<List<String>>,
+    visitors: List<String>
+): MutableMap<String, Int> { // user를 알지 못하는 친구, 맵 저장
+    val recommendFriend = mutableMapOf<String, Int>()
+    val myFriend = setMyFriend(user, friends)
+
+    for (i in friends) {
+        // [친구1, 친구2]
+        for (j in 0 until myFriend.size) {
+            val friendOfFriend = (i - myFriend[j])[0] // 친구의 친구이름(= 키값)
+            if ((!myFriend.contains(friendOfFriend)) and i.contains(myFriend[j])) {
+                if (friendOfFriend in recommendFriend) {
+                    setFriendMap(
+                        friendOfFriend,
+                        recommendFriend.getValue(friendOfFriend).plus(10),
+                        recommendFriend,
+                        user
+                    )
+                    continue
+                }
+                setFriendMap(friendOfFriend, 10, recommendFriend, user)
+            }
+        }
+    }
+
+    checkVisitors(user, friends, visitors, recommendFriend)
+    return recommendFriend
+}
+
+fun checkVisitors(
+    user: String,
+    friends: List<List<String>>,
+    visitors: List<String>,
+    recommendFriend: MutableMap<String, Int>
+): MutableMap<String, Int> { // 내 친구가 아닌 방문객 맵 저장
+    val myFriend = setMyFriend(user, friends)
+
+    for (i in visitors) {
+        if (!myFriend.contains(i)) {
+            if (i in recommendFriend) {
+                recommendFriend[i] = recommendFriend.getValue(i).plus(1)
+                continue
+            }
+            recommendFriend[i] = 1
+        }
+    }
+    return recommendFriend
+}
+
+fun setFriendMap(key: String, value: Int, recommendFriend: MutableMap<String, Int>, user: String) { // 친구, 점수 맵 저장
+    recommendFriend[key] = value
+    recommendFriend.remove(user)
+}
+
+fun getMyFriend(user: String, friends: List<List<String>>): MutableList<List<String>> { // 내가 포함된 배열 호출
     val myFriends = mutableListOf<List<String>>()
-    val myFriend = mutableListOf<String>()
-    var resultScore = mutableMapOf<String, Int>()
-
-
-
-    for (i in friends) {   // 내 친구찾기
+    for (i in friends) {
         if (i.contains(user)) {
 
             myFriends.add(i)
         }
     }
+    return myFriends
+}
 
-    for (j in 0 until myFriends.size) { // 내 친구 찾기2
-        if (user in myFriends[j]) {
-
-            myFriend.add((myFriends[j] - user)[0])
-
+fun setMyFriend(user: String, friends: List<List<String>>): MutableList<String> { // 나를 뺀 친구, 리스트 저장
+    val myFriends = mutableListOf<String>()
+    for (j in 0 until getMyFriend(user, friends).size) {
+        if (user in getMyFriend(user, friends)[j]) {
+            myFriends.add((getMyFriend(user, friends)[j] - user)[0])
         }
     }
-    //  println(myFriend)
-
-    for (i in friends) {
-        println("i = $i")
-        for (j in 0 until myFriend.size) {
-            val a = (i - myFriend[j])[0] // 키값
-            if ((!myFriend.contains(a)) and i.contains(myFriend[j])) {
-
-
-                if (a in resultScore) {
-                    resultScore[a] = resultScore.getValue(a).plus(10)
-
-                    continue
-                }
-                resultScore[a] = 10
-                resultScore.remove(user)
-
-            }
-        }
-    }
-
-    for (i in visitors) {
-
-        if (!myFriend.contains(i)) {
-
-            if (i in resultScore) {
-                resultScore[i] = resultScore.getValue(i).plus(1)
-                continue
-            }
-            resultScore[i] = 1
-        }
-
-    }
-
-
-    return resultScore.toList().sortedBy { it.first }.sortedByDescending { it.second }.map { it.first }.take(5)
+    return myFriends
 }
