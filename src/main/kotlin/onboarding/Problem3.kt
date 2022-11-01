cba8eaeed3cc0f2648ed9ec5125f7788b76b5fe9
Jaehun-Jo <chojh922@gmail.com>
2022-11-01 18:36:22 +0900

    Problem3-1의 자리가 3, 6, 9중 하나인 경우를 찾는 메서드 추가

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -12,3 +12,8 @@ private fun checkThrowException(number: Int) {
 private fun throwNumberRangeException(number: Int) {
     if (number > 10000 || number < 1) throw error("number는 1 이상 10,000 이하인 자연수여야 합니다!")
 }
+
+private fun isContainedMultipleOfThree(number: Int): Boolean {
+    if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) return true
+    return false
+}
