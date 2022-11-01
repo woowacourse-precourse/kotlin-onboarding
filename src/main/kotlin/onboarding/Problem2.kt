fefdea7e941335844b52b6bec1e8b99cc95e09a3
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-01 17:29:31 +0900

    removeDuplicateChar 메소드 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    val string: String = cryptogram
+    var duple = arrayListOf<Char>(string.first()) /* 중복되지 않는 문자만 저장하는 arraylist */
+    return duple.joinToString("")
 }
+
+/* 중복문자가 아닌 문자들만 duple에 저장하는 메소드*/
+fun removeDuplicateChar(duple: ArrayList<Char>, c: Char){
+    /* c와 duple의 마지막 값이 같으면 duple에서 제거*/
+    if(duple.last() == c){
+        duple.removeLast()
+        return
+    }
+    /* 다르면 duple에 추가 */
+    duple.add(c)
+}
+
+
