7779134d5f5a68256b9836104c8cfc662b51beb5
lee989898 <@@qwer1352>
2022-11-01 13:46:32 +0900

    [feat](Problem5) 각 화폐 종류로 나눈 몫을 화폐 종류별 개수 리스트/배열에 추가 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -9,6 +9,11 @@ fun solution5(money: Int): List<Int> {
 
     val numberOfCurrencies = MutableList(9) { 0 }
     val typeOfCurrency = arrayOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+
+    numberOfCurrencies[0] = money / typeOfCurrency[0]
+    for (i in 1 until typeOfCurrency.size) {
+        numberOfCurrencies[i] = money % typeOfCurrency[i - 1] / typeOfCurrency[i]
+    }
     return numberOfCurrencies
 }
 
