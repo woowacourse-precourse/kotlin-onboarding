5b5765b45b07d34646458b98e9319395fd94f30a
dev-worthwhile <dev.lboram@gmail.com>
2022-11-01 17:39:32 +0900

    feat: (기능2) friends 목록에서 user 친구와 친구를 맺고있는 사용자에게 10 점 부여

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -15,10 +15,24 @@ fun solution7(
     visitors: List<String>
 ): List<String> {
     val userFriends = mutableListOf<String>()
+    val recommendScore = mutableMapOf<String, Int>() //사용자 점수 목록
 
     for(index in friends.indices){
         if(friends[index][1]==user){ //user 의 친구
             userFriends.add(friends[index][0])
+        }
+    }
+
+    for(index in friends.indices){
+        val friendA=friends[index][0]
+        val friendB=friends[index][1]
+        if(userFriends.contains(friendA) && (friendB != user)){ //friendA 가 user 의 친구인 경우
+            if(recommendScore.containsKey(friendB)){
+                val price = recommendScore[friendB]
+                recommendScore[friendB] = (price?:0) + 10
+                continue
+            }
+            recommendScore[friendB] = 10
         }
     }
 
