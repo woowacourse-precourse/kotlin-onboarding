ce484600f6f11705268ee269f057cb04a4427dbd
leehandsub <gustjq7027@naver.com>
2022-11-01 20:22:10 +0900

    feature/알파벳일 때 변환

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -2,16 +2,23 @@ package onboarding
 
 fun solution4(word: String): String {
     val flog = mutableMapOf<String, String>()
+    var ans = ""
 
     for (i in 0..25) {
         flog[('a' + i).toString()] = ('z' - i).toString()
         flog[('A' + i).toString()] = ('Z' - i).toString()
     }
-    print(flog)
-    return ""
+
+    for (i in word) {
+        ans += flog[i.toString()]
+    }
+    return ans
 }
-fun main(){
-    val word = "I love you"
-    val result = "R olev blf"
+
+fun main() {
+//    val word = "I love you"
+//    val result = "R olev blf"
+    val word = "Iloveyou"
+    val result = "Rolevblf"
     print(solution4(word).equals(result))
 }
