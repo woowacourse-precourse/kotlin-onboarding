aead78a993deeb2491355e1a55021fbfa05cba35
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-01 17:47:30 +0900

    3, 6, 9를 판별하는 is369 메소드 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -3,3 +3,10 @@ package onboarding
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
+
+fun is369(num: Int): Int{
+    if(num % 3 == 0 && num != 0){
+        return 1
+    }
+    return 0
+}
