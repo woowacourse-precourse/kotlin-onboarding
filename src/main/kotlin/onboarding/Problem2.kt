09f757137636e54478c0d31f9a45e1f455adfd77
JinJoa <mitejoa@gmail.com>
2022-11-01 23:50:23 +0900

    문제2 - 수정

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -9,17 +9,24 @@ fun main(){
 fun solution2(cryptogram: String): String {
     //var decryption :String = cryptogram
     var list = mutableListOf<Char>()
+    var list2 = mutableListOf<Char>()
     for (i in 0..cryptogram.length-1){
         list.add(cryptogram[i].toChar())
     }
-    println(list.size)
-    for(i in 1..list.size){
-        if(list[i] == list[i-1]){
-            list.removeAt(i)
-            break
+
+    while(true){                        //무한반복
+        var len = list.size-1
+        for(i in 1..len){
+            //연속하는 중복 숫자 제거
+            if(list[i].toChar() != list[i-1]){
+                list.removeAt(i+1)
+                list.removeAt(i)
+                len -= 2
+                //break
+            }
         }
     }
-    println(list.size)
-    return "---"
+    //println(list)
+    return list.toString()
 
 }
