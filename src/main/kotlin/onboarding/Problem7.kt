986356f888decd60d42cfbb0a971a7109b1c0702
Gloria <ippnsj@gmail.com>
2022-11-01 09:01:18 +0900

    feat(Problem7): 친구 관계 정보 리스트가 올바른지 확인하는 기능 친구 관계 정보 리스트의 길이가 1 이상 10000 이하인지 확인 친구 관계 정보의 각 원소 길이가 2인지 확인

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -3,6 +3,7 @@ package onboarding
 import problem2.AlphabetVerifier
 import problem2.LowerCaseVerifier
 import problem2.StringLengthVerifier
+import problem6.ListSizeVerifier
 
 private fun validateUser(user:String) {
     val userValidator = InputValidator(
@@ -16,12 +17,32 @@ private fun validateUser(user:String) {
     userValidator.validate(user)
 }
 
+private fun validateFriends(friends: List<List<String>>) {
+    val friendsValidator = InputValidator(
+        listOf(
+            ListSizeVerifier(1, 10000)
+        )
+    )
+    friendsValidator.validate(friends)
+
+    val friendValidator = InputValidator(
+        listOf(
+            ListSizeVerifier(2, 2)
+        )
+    )
+    for(friend in friends) {
+        friendValidator.validate(friend)
+    }
+}
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
     validateUser(user)
-    
+
+    validateFriends(friends)
+
     return listOf()
 }
