088a3211e7d1df6e886a32149067b9b7c38e1304
Yuhee Kim <dbgml4509@naver.com>
2022-11-01 21:48:52 +0900

    fix : var 변수 생성하여 값 변경

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -13,14 +13,16 @@ fun solution5(money: Int): List<Int> {
 
 fun getCaches(money: Int): List<Int>{
 
+    var temp = 0
+
     //1. 각 화폐 가치만큼의 수들을 가진 리스트 생성
     val cacheList = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
 
-    val changedMoney = MutableList<Int>(cacheList.size){ 0 }
+    var changedMoney = MutableList<Int>(cacheList.size){ 0 }
     //2. money를 한 자리씩 비교하면서 1.에서 생성한 리스트를 참고해 변환
     for(i in 0 until cacheList.size){
-        changedMoney[i] = money / cacheList[i]
-        money %= cacheList[i]
+        changedMoney[i] = temp / cacheList[i]
+        temp %= cacheList[i]
     }
 
     return changedMoney
