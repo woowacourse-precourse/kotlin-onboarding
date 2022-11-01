2266bb60bd5a5d40ddd0299b3e7c74913b0addd7
leehandsub <gustjq7027@naver.com>
2022-11-01 20:45:28 +0900

    feature/각 지폐 갯수 구하기

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,13 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var temp = money
+    val coins = intArrayOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+
+    for (i in coins) {
+        val value = temp / i
+        if (value != 0) {
+            temp -= value * i
+        }
+    }
 }
