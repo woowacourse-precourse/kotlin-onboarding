605f15ea4a50afea3cde9087d3e8b25561ea8237
noion0511 <noion0511@gmail.com>
2022-11-01 18:52:43 +0900

    [problem 2][feat] 연속하는 중복 문자 제거 기능

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    return deleteDuplicateString(cryptogram)
+}
+
+fun deleteDuplicateString(cryptogram: String): String {
+    var str = cryptogram
+    for (i in 0 until str.length-1) {
+        if (str[i] == str[i+1]) {
+            val del = str[i].toString() + str[i].toString()
+            str = str.replace(del, "")
+        }
+    }
+
+    return str
 }
