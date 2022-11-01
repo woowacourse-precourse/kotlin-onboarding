76db47658ac7497dff53b2400b084487c47b567e
Lee-GS <leegs4831@naver.com>
2022-11-01 20:11:50 +0900

    user와 함께 아는 친구를 구한후 함께 아는 친구수 *10 을 해 함께 아는 친구 수만큼 점수를 설정한후 타임라인에 방문한 친구를 구한후 hashmap에 추가하여 설정

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,40 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val set_visitors = visitors.toMutableList().distinct()
+    val friends =friends.toMutableList()
+    var know_fr = mutableListOf<String>()
+    var with_know_fr = mutableListOf<String>()
+
+    for (i : Int in 0..friends.size-1){ // user와 아는친구
+        if(user in friends[i]){
+            know_fr.add(friends[i][0])
+            know_fr.add(friends[i][1])
+            know_fr.remove(user)
+        }
+    }
+    for (i : Int in 0..know_fr.size-1){ // 함께 아는 친구
+        for(k : Int in 0..friends.size-1){
+            if (know_fr[i] in friends[k]) {
+                with_know_fr.add(friends[k][0])
+                with_know_fr.add(friends[k][1])
+                with_know_fr.remove(know_fr[i])
+            }
+            with_know_fr.remove("mrko")
+        }
+    }
+    var set_list = with_know_fr.distinct()
+    var ans_hashmap = hashMapOf<String,Int>()
+    for (i : Int in 0..set_list.size-1){ // 함께 아는 친구와 점수
+        ans_hashmap.put(set_list[i],with_know_fr.count({it.equals(set_list[i])})*10)
+
+    }
+
+    if(visitors.size>0){ //방문자 해쉬맵에 추가
+        for (i : Int in 0..set_visitors.size-1){
+            ans_hashmap.put(set_visitors[i],visitors.count({it.equals(set_visitors[i])}))
+        }
+    }
+
+    return set_list
 }
