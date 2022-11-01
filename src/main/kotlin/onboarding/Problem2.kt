6b3f07ab3a480810c3373e6f7dcfda2a4db85eef
dev-worthwhile <dev.lboram@gmail.com>
2022-11-01 13:43:24 +0900

    feat: 중복된 문자를 제거한 문자열이 검증 전 문자열과 일치할 때 까지 기능1~2를 반복

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -7,8 +7,22 @@ package onboarding
  * (3) 중복된 문자를 제거한 문자열이 검증 전 문자열과 일치할 때까지 (기능 1~2) 반복
  * **/
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var prevWord = cryptogram //변환 전 단어
+   var resultWord: String  //변환 후 단어
+
+   resultWord = getRedundantWordRemoved(cryptogram)
+
+   for(i in cryptogram.indices){
+       if(prevWord==resultWord){ //변화 없을 때 => 종료
+           break
+       }
+       prevWord = resultWord
+       resultWord = getRedundantWordRemoved(prevWord)
+   }
+
+   return resultWord
 }
+
 private fun getRedundantWordRemoved(cryptogram: String): String{ //중복단어 제거
     val cryptoPiece = cryptogram.toList() //(1) 한 글자씩 분리
     var removedResult = "" //중복된 문자를 제외한 문자열
