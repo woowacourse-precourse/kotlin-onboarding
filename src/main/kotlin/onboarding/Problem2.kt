9c8c4e5580181348d501b0b7a83311e7084c9514
Yujin <jini1514@soongsil.ac.kr>
2022-11-01 21:48:54 +0900

    Add : Problem2.kt

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,22 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var cryptogramWord = cryptogram
+    var overlap = true
+
+    while (overlap && cryptogramWord.isNotEmpty()) {
+        overlap = false
+
+        for (i in 0 until cryptogramWord.length - 1) {
+            if (cryptogramWord[i] == cryptogramWord[i + 1]) {
+                cryptogramWord = cryptogramWord.removeRange(i + 1, i + 2)
+                cryptogramWord = cryptogramWord.removeRange(i, i + 1)
+
+                overlap = true
+                break
+            }
+            overlap = false
+        }
+    }
+    return cryptogramWord
 }
