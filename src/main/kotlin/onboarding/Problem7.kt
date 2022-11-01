36bac551f9e2d064e7c04d25badfbcc09bc3dd6f
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-01 10:01:48 +0900

    7번째 문제: Union-Find 기능 구현하기

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,30 @@
 package onboarding
 
+
+// 사용자
+private lateinit var parentUser: Array<Int>
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
-    visitors: List<String>
+    visitors: List<String>,
 ): List<String> {
-    TODO("프로그램 구현")
+
+
 }
+
+
+private fun union(nodeNumber: Int, nextNodeNumber: Int) {
+    var nodeNumber = find(nodeNumber)
+    var nextNodeNumber = find(nextNodeNumber)
+    if (nodeNumber != nextNodeNumber) {
+        if (nodeNumber < nextNodeNumber) parentUser[nextNodeNumber] = nodeNumber
+        else parentUser[nodeNumber] = nextNodeNumber
+    }
+}
+
+private fun find(nodeNumber: Int): Int {
+    return if (parentUser[nodeNumber] == nodeNumber) nodeNumber
+    else find(parentUser[nodeNumber]).also { parentUser[nodeNumber] = it }
+}
+
