94f526a7522710944a9516f798630d5a02a4b12f
lkeonwoo94 <lkeonw0o@naver.com>
2022-11-01 22:08:11 +0900

    solve Prob5

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,7 +1,12 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    
+    val table = listOf(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1)
+    val ret = mutableListOf<Int>()
+    var money = money
+    for (i in table) {
+        ret.add(money / i)
+        money %= i
+    }
+    return ret
 }
-
-/* Greedy */
