84b451faa19222d51e4e49bca4b8dc9af78e1767
changhwan <chang5hustle@gmail.com>
2022-11-01 14:29:01 +0900

    [Problem 6] (요구사항 6) 닉네임 중복검사 및 결과 도출 함수작성

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,7 +3,14 @@ package onboarding
 import java.util.regex.Pattern
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    var validForms = forms
+    validForms = getValidLengthEmailForms(validForms)
+    validForms = getValidPatternEmailForms(validForms)
+    validForms = getValidEmailDomainForms(validForms)
+    validForms = getValidLengthNickNameForms(validForms)
+    validForms = getValidLanguageNickNameForms(validForms)
+
+    return getDuplicateNicKNameFormEmailList(validForms).sorted()
 }
 
 private fun getValidLengthEmailForms(forms: List<List<String>>): List<List<String>> {
@@ -38,4 +45,17 @@ private fun getValidLanguageNickNameForms(forms: List<List<String>>): List<List<
     val processedForms = forms.toMutableList()
     for (i in forms) if (!Pattern.matches("^[ㄱ-ㅣ가-힣 ]*$", i[1])) processedForms.remove(i)
     return processedForms
+}
+
+private fun getDuplicateNicKNameFormEmailList(forms: List<List<String>>): MutableList<String> {
+    val duplicateNicKNameFormEmailSet = mutableSetOf<String>()
+    for (i in forms.indices) {
+        for (j in 0 until forms[i][1].length - 1) {
+            val checkString = forms[i][1].substring(j, j + 2)
+            for (k in i + 1 until forms.size) {
+                if (checkString in forms[k][1]) duplicateNicKNameFormEmailSet.addAll(listOf(forms[i][0], forms[k][0]))
+            }
+        }
+    }
+    return duplicateNicKNameFormEmailSet.toMutableList()
 }
