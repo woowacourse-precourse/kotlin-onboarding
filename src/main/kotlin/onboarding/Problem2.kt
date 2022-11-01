dc5bf80faa563077ea52b5255316fcb25000b2f3
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2022-11-01 19:31:56 +0900

    docs : 문제2 기능목록 , refactor : 주석 처리 / 함수 간소화

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -8,12 +8,28 @@ fun solution2(cryptogram: String): String {
     var st = Stack<Char>()
     var answer = ""
 
+    comapre_char(cryptogram , st)
+
+
+    for(i in 0 until st.size){
+        answer = st.pop() + answer
+    }
+
+    return answer
+
+
+
+}
+fun comapre_char(cryptogram: String , st : Stack<Char>)  {
+
     for(i in 0 until cryptogram.length) {
 
+        //스택이 비었으면 문자 저장
         if(st.isEmpty()){
             st.push(cryptogram[i])
         }
 
+        //스택의 윗 부분과 다음 저장할 문자 비교
         if (st.isNotEmpty() && i >= 1) {
             if (st.peek() == cryptogram[i]) {
                 st.pop()
@@ -27,14 +43,4 @@ fun solution2(cryptogram: String): String {
 
 
     }
-
-
-    for(i in 0 until st.size){
-        answer = st.pop() + answer
-    }
-
-    return answer
-
-
-
 }
