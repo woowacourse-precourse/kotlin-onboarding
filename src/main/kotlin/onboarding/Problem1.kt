40a77099624fd7f313ba6e5cfaf12de4ba505df2
leeeha <jxlhe46@seoultech.ac.kr>
2022-11-01 18:02:09 +0900

    refactor: Problem1 자릿수가 늘어나도 계산 결과를 얻을 수 있도록 리팩토링

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -2,35 +2,14 @@ package onboarding
 
 import kotlin.math.max
 
-fun getMaxValue(arr: List<Int>): Int {
-    // 왼쪽과 오른쪽 페이지 모두에 대해서 최댓값 계산
-    val ans = mutableListOf(0, 0)
-    for(i in 0..1){
-        val a = arr[i] / 100
-        val b = (arr[i] % 100) / 10
-        val c = (arr[i] % 100) % 10
-
-        if(a != 0){ // 세자리 수
-            ans[i] = max(a * b * c, a + b + c)
-        }else{
-            if(b != 0){ // 두자리 수
-                ans[i] = max(b * c, b + c)
-            }else{ // 한자리 수
-                ans[i] = c
-            }
-        }
-    }
-    return max(ans[0], ans[1])
-}
-
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     // 연속한 두 페이지인지 검사 (입력의 유효성 테스트)
     if(pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1){
         return -1
     }
 
-    val p = getMaxValue(pobi)
-    val c = getMaxValue(crong)
+    val p = compareLeftRight(pobi)
+    val c = compareLeftRight(crong)
     return if(p > c){
         1
     }else if(p == c){
@@ -38,4 +17,27 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     }else{
         2
     }
+}
+
+// 왼쪽과 오른쪽 페이지 번호에 대한 결과 중에서 최댓값 반환
+fun compareLeftRight(arr: List<Int>): Int {
+    val left = getMaxValue(arr[0])
+    val right = getMaxValue(arr[1])
+    return max(left, right)
+}
+
+// 페이지의 각 자릿수를 모두 더하거나 모두 곱한 값 중에 큰 수를 반환
+fun getMaxValue(number: Int): Int{
+    var pageNum = number
+    var plusNum = 0
+    var mulNum = 1
+
+    do{
+        val digit = pageNum % 10 // 일의 자리수부터 계산
+        plusNum += digit
+        mulNum *= digit
+        pageNum /= 10
+    }while(pageNum > 0)
+
+    return max(plusNum, mulNum)
 }
