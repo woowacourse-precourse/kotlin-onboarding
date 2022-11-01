8d0d8c1a57938f75eb2fe4b90568c949b51a05ac
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-01 18:17:51 +0900

    변환된 알파벳을 출력하는 convertAlpha 메소드 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,3 +3,14 @@ package onboarding
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
+
+fun convertAlpha(c: Char): Char{
+    if(c in 'A'..'Z'){
+        return ('Z' - (c - 'A')).toChar()
+    }
+
+    if(c in 'a'..'z'){
+        return ('z' - (c - 'a')).toChar()
+    }
+    return c
+}
