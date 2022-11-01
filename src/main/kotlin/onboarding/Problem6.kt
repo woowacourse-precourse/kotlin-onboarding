dbae42c239f0d5787e6ebce30d2bb74c69f86ad0
윤동현 <donghyunyoon81@gmail.com>
2022-11-01 20:27:35 +0900

    refactor: Create getSeparateNicknameList in solution6 같은 글자가 연속적으로 포함될 수 있는 2글자들을 forms안에 닉네임들에서 추출하여 담습니다.

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,25 +1,21 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    val separateNicknameList = mutableListOf<String>()
+    val separateNicknameList = getSeparateNicknameList(forms)
     val duplicateNicknameList = mutableSetOf<String>()
     val duplicateNicknameEmailList = mutableListOf<String>()
+
+    return duplicateNicknameEmailList.distinct().sorted()
+}
+
+private fun getSeparateNicknameList(forms: List<List<String>>):MutableList<String> {
+    val separateNicknameList = mutableListOf<String>()
     for (i in forms.indices) {
         for(j in forms[i][1].indices) {
             if (j + 1 == forms[i][1].length) break
             separateNicknameList.add("${forms[i][1][j]}${forms[i][1][j + 1]}")
         }
     }
-    for(i in separateNicknameList.indices) {
-        if(separateNicknameList.indexOf(separateNicknameList[i]) !=separateNicknameList.lastIndexOf(separateNicknameList[i])) duplicateNicknameList.add(separateNicknameList[i])
-    }
-    for(i in forms.indices) {
-        for(j in duplicateNicknameList) {
-            if(forms[i][1].indexOf(j)!=-1) {
-                duplicateNicknameEmailList.add(forms[i][0])
-                break
-            }
-        }
-    }
-    return duplicateNicknameEmailList.distinct().sorted()
+    return separateNicknameList
 }
+
