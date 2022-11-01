dd78c904603c17d4a34ae50b7f55710e0fec0ea0
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-11-01 14:18:28 +0900

    style : P6문제 변수, 메서드명 변경

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -5,19 +5,19 @@ fun solution6(forms: List<List<String>>): List<String> {
     var result = mutableSetOf<String>()
 
     forms.forEach { crewInfo ->
-        val nicknameList = createDuplicateNicknames(crewInfo[1])
-        val duplicatedWord = checkDuplication(forms, order++, nicknameList)
+        val splitWords = splitNicknameIntoWords(crewInfo[1])
+        val emailWithDuplicatedWord = checkDuplication(forms, order++, splitWords)
 
-        if (duplicatedWord.isNotEmpty()) {
+        if (emailWithDuplicatedWord.isNotEmpty()) {
             result.add(crewInfo[0])
-            result.add(duplicatedWord)
+            result.add(emailWithDuplicatedWord)
         }
     }
 
     return result.sorted().toList()
 }
 
-fun createDuplicateNicknames(nickname: String) : MutableSet<String> {
+fun splitNicknameIntoWords(nickname: String) : MutableSet<String> {
     val nicknameList = mutableSetOf<String>()
 
     for (j in 2 until nickname.length + 1) {
@@ -35,10 +35,10 @@ fun createDuplicateNicknames(nickname: String) : MutableSet<String> {
 fun checkDuplication(
     forms: List<List<String>>,
     order: Int,
-    nicknameList: MutableSet<String>)
+    splitWords: MutableSet<String>)
 : String {
     for (i in order until forms.size) {
-        nicknameList.forEach { nn ->
+        splitWords.forEach { nn ->
             if (forms[i][1].contains(nn)) {
                 return forms[i][0]
             }
