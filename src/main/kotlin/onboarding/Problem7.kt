e37ee93ede3864c51f94d581e47bdb6a4a2208cc
joh9911 <joh9911@naver.com>
2022-11-01 20:02:36 +0900

    problem7: 유저의 친구인 사람과 그 사람의 위치를 저장하는 함수

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,21 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val user = user
+    val friends = friends
+    val visitors = visitors
+    var friendsOfUser = ArrayList<String>()
+    var indexArray = ArrayList<Int>()
+}
+
+/**
+유저와 친구인 사람의 이름과 전달받은 배열에서의 index를 저장하는 기능
+ */
+fun findFriendsOfUser(user: String, friends: List<List<String>>, friendsOfUser: ArrayList<String>, indexArray: ArrayList<Int>){
+    for (index in friends.indices){
+        if (friends[index].indexOf(user) != -1){
+            friendsOfUser.add(friends[index][1 - friends[index].indexOf(user)])
+            indexArray.add(index)
+        }
+    }
 }
