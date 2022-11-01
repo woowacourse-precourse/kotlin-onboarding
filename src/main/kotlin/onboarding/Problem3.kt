98034602a6dc4dd326530ca4c1754cb4ec2e8124
dpwns1234 <dpwns1234@naver.com>
2022-11-01 21:02:28 +0900

    solve problem3

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,19 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO()
+    var result = 0
+
+    for (i in 1..number) {
+        var variable = i
+        // while 사용해서 각 자리수 체크
+        while (variable > 0) {
+            // 일의 자리 수
+            val one = variable % 10
+            // 3, 6, 9 if문으로 체크
+            if (one == 3 || one == 6 || one == 9) result++
+
+            variable /= 10
+        }
+    }
+    return result
 }
