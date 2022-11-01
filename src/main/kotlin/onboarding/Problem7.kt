520bc64b855099453b8de4a4d52876d1ca6d1321
권용민 <briandr@naver.com>
2022-11-01 18:06:50 +0900

    [Problem7] 유저 친구를 list -> set으로 변경

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -8,35 +8,35 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    val userFriendList = mutableListOf<String>()
+    val userFriendSet = mutableSetOf<String>()
     val recommendedFriends = mutableMapOf<String, Int>()
 
-    searchUserFriends(user, userFriendList, friends)
-    recommendedFriends.putAll(searchFriendsKnowTogether(user, userFriendList, friends))
-    checkVisitor(visitors, userFriendList, recommendedFriends)
+    searchUserFriends(user, userFriendSet, friends)
+    recommendedFriends.putAll(searchFriendsKnowTogether(user, userFriendSet, friends))
+    checkVisitor(visitors, userFriendSet, recommendedFriends)
 
     val resultList = sortRecommendedFriends(recommendedFriends)
     return resultList.filterIndexed { index, _ -> index < 5 }
 }
 
-private fun searchUserFriends(user: String, userFriendList: MutableList<String>, relation: List<List<String>>) {
+private fun searchUserFriends(user: String, userFriendSet: MutableSet<String>, relation: List<List<String>>) {
     for (list in relation) {
-        if (list[0] == user) userFriendList.add(list[1])
-        if (list[1] == user) userFriendList.add(list[0])
+        if (list[0] == user) userFriendSet.add(list[1])
+        if (list[1] == user) userFriendSet.add(list[0])
     }
 }
 
 private fun searchFriendsKnowTogether(
     user: String,
-    userFriendList: MutableList<String>,
+    userFriendSet: MutableSet<String>,
     relation: List<List<String>>
 ): Map<String, Int> {
     val recommendedFriends = mutableMapOf<String, Int>()
     for (list in relation) {
         if (user in list) continue
-        if ((list[0] in userFriendList) and (list[1] in userFriendList)) continue
-        if (list[0] in userFriendList) plusScore(recommendedFriends, list[1], TOGETHER_SCORE)
-        if (list[1] in userFriendList) plusScore(recommendedFriends, list[0], TOGETHER_SCORE)
+        if ((list[0] in userFriendSet) and (list[1] in userFriendSet)) continue
+        if (list[0] in userFriendSet) plusScore(recommendedFriends, list[1], TOGETHER_SCORE)
+        if (list[1] in userFriendSet) plusScore(recommendedFriends, list[0], TOGETHER_SCORE)
     }
     return recommendedFriends
 }
@@ -47,11 +47,11 @@ private fun plusScore(recommendedFriends: MutableMap<String, Int>, name: String,
 
 private fun checkVisitor(
     visitors: List<String>,
-    userFriendList: MutableList<String>,
+    userFriendSet: MutableSet<String>,
     recommendedFriends: MutableMap<String, Int>
 ) {
     visitors.forEach {
-        if (it in userFriendList) return@forEach
+        if (it in userFriendSet) return@forEach
         plusScore(recommendedFriends, it, VISITOR_SCORE)
     }
 }
