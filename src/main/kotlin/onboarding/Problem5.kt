90cae953bb436d304695ca0bcd8833d1eab63759
otter <a33397397@gmail.com>
2022-11-01 22:37:07 +0900

    [UPDATE][problem5] 전체 작성

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,16 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+
+    var tmp_money : Int = money
+
+    val currencyType : List<Int> = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val count : MutableList<Int> = MutableList(9) {0}
+
+    for(i in 0 until 9) {
+        count[i] = tmp_money / currencyType[i]
+        tmp_money -= count[i] * currencyType[i]
+    }
+
+    return count
 }
