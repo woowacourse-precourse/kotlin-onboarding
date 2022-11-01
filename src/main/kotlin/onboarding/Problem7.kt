a06da7a80d2049d974c9b908c0c28452e35cda47
EmilyCh0 <seoyeon3089@naver.com>
2022-11-01 19:32:20 +0900

    refactor(problem7): 코드 정리

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -32,15 +32,10 @@ fun solution7(
         if (!strangersScore.contains(v)) strangersScore[v] = 1
         else strangersScore[v] = strangersScore[v]!!.plus(1)
     }
-    val ans = arrayListOf<String>()
+
     val strangerList = strangersScore.toList()
     val scoreComparator = compareByDescending<Pair<String, Int>> { it.second }
     val scoreAndNameComparator = scoreComparator.thenBy { it.first }
-    val sortedList = strangerList.sortedWith(scoreAndNameComparator)
-    for (s in sortedList) {
-        if (ans.size >= 5) break
-        if (s.second == 0) break
-        ans.add(s.first)
-    }
-    return ans
+
+    return strangerList.sortedWith(scoreAndNameComparator).map { it.first }.take(5)
 }
