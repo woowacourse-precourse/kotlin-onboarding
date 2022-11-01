58a2ade4c2378a9df328701429f30d72cd149725
Jisu Moon <barcode_no_1@naver.com>
2022-11-01 22:41:16 +0900

    Problem5 문제해결 (화폐단위 반환갯수)

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,18 @@
 package onboarding
 
+/* 화폐단위별로 몇 개씩인지 금액 큰 순서대로 반환하는 solution 기능 */
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var money = money
+    val answer: MutableList<Int> = ArrayList()
+    val moneyArr = intArrayOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    var i = 0
+    while (money != 0) {
+        answer.add(money / moneyArr[i])
+        money = money % moneyArr[i]
+        i++
+    }
+    while (moneyArr.size != answer.size)
+        answer.add(0)
+
+    return answer
 }
