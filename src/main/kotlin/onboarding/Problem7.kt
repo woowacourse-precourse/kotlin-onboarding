ea928f384bf3fdb44727e7c784fc56dfa6729471
hyunji1203 <hj27720@gmail.com>
2022-11-01 14:49:07 +0900

    문제7 - 사용자와 함께 아는 친구인 경우 10점 추가

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -14,4 +14,27 @@ fun solution7(user: String, friends: List<List<String>>, visitors: List<String>)
     }
     userFriends.remove(user)
 
+    //사용자와 함께 아는 친구인 경우 10점 추가
+    for (i in friends){
+        for (j in userFriends){
+            //친구 리스트에 있는 친구의 친구 찾기
+            if (i.contains(j)){
+                for (k in (0..1)){
+                    //만약 원래 친구 추천 리스트에 있다면
+                    if (recommend.contains(i[k])){
+                        var score = recommend.get(i[k])
+                        if (score != null) {
+                            recommend.put(i[k],score + 10)
+                        }
+                    }
+                    if (!recommend.contains(i[k])){
+                        recommend.put(i[k], 10)
+                    }
+                }
+            }
+        }
+    }
+    recommend.remove(user)
+
+
 }
