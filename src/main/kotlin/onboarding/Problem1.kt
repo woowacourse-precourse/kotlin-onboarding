71b2764174bf42c0a47fe6a475e2953329e4dc7e
hyungmin <ramkid91@naver.com>
2022-11-01 16:18:52 +0900

    feat: 첫번째 문제 로직 수정 및 여섯번째문제 로직 구성, 테스트 검증 - 첫번째 문제 게임의 예외결과 리턴값이 적용되도록 수정, 각 자리수별 숫자를 분리하는 로직에 오류발견후 수정 - 여섯번째 문제 완전탐색을 통해 모든 경우를 계산하도록 반복문 구성, 삼중 for문은 추후 수정필요

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -3,15 +3,20 @@ package onboarding
 import java.lang.Integer.max
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+
+    if(pobi[1]-pobi[0]!=1 || crong[1]-crong[0]!=1){
+        return -1
+    }
     val resOfPobi = getMax(pobi)
     val resOfCrong = getMax(crong)
     return if (resOfPobi > resOfCrong) {
         1
-    } else if (resOfPobi == resOfPobi) {
+    } else if (resOfPobi == resOfCrong) {
         0
     } else {
         2
     }
+
 }
 
 fun getMax(values: List<Int>): Int {
@@ -24,12 +29,12 @@ fun getMax(values: List<Int>): Int {
 
 fun calculate(number: Int): Int {
     return if (number > 100) {
-        val sum = number / 100 + number / 10 + number % 10
-        val multi = number / 100 * number / 10 - number % 10
+        val sum = (number / 100) + (number%100 / 10) + (number % 10)
+        val multi = (number / 100) * (number%100/ 10) * (number % 10)
         max(sum, multi)
     } else if (number > 10) {
         val sum = number / 10 + number % 10
-        val multi = number / 10 - number % 10
+        val multi = (number / 10) * (number % 10)
         max(sum, multi)
     } else {
         number
--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,29 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val res = mutableListOf<String>()
+    for(i in forms.indices){
+        val nickName = forms[i][1]
+        for(j in 0 until nickName.length-2){
+            val maskString = nickName.substring(j,j+2)
+            var count = 1
+            val temp = mutableListOf<String>()
+            temp += forms[i][0]
+            for(k in i+1 until forms.size){
+                if(forms[k][1].contains(maskString)){
+                    count++
+                    temp += forms[k][0]
+                }
+            }
+            if(count>=2){
+                for(email in temp){
+                    if(!res.contains(email)){
+                        res+= email
+                    }
+                }
+            }
+        }
+
+    }
+    return res.sorted()
 }
