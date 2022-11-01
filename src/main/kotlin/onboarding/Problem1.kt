a484701bb658eee583e2da643df92b66639551b4
s9hn <tnghqn@naver.com>
2022-11-01 18:13:48 +0900

    [REFACTOR(미션1)] 주석 추가
    1. comparator 함수명 수정
    2. 주석 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -3,14 +3,14 @@ package onboarding
 import kotlin.math.max
 
 
-fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+fun solution1(pobi: List<Int>, crong: List<Int>): Int { // 게임 결과값 리턴
 
     return gameResult(pobi, crong)
 }
 
-fun gameResult(pobi: List<Int>, crong: List<Int>): Int {
-    val pobiScore = Comparator(digitPlus(pobi), digitMultiple(pobi))
-    val crongScore = Comparator(digitPlus(crong), digitMultiple(crong))
+fun gameResult(pobi: List<Int>, crong: List<Int>): Int { // 예외사항 체크 및 승자에 따른 점수 리턴
+    val pobiScore = comparator(digitPlus(pobi), digitMultiple(pobi))
+    val crongScore = comparator(digitPlus(crong), digitMultiple(crong))
 
     if ((checkListLength(pobi, crong) == -1) or (checkSerialPage(pobi, crong) == -1)) {
         return -1
@@ -20,7 +20,7 @@ fun gameResult(pobi: List<Int>, crong: List<Int>): Int {
         return 0
     }
 
-    return when (Comparator(pobiScore, crongScore)) {
+    return when (comparator(pobiScore, crongScore)) {
         pobiScore -> 1
         crongScore -> 2
         else -> -1
@@ -29,26 +29,26 @@ fun gameResult(pobi: List<Int>, crong: List<Int>): Int {
 }
 
 
-fun checkListLength(pobi: List<Int>, crong: List<Int>): Int {
+fun checkListLength(pobi: List<Int>, crong: List<Int>): Int { // 리스트 길이 검사
     if ((pobi.size != 2) or (crong.size != 2)) {
         return -1
     }
     return 0
 }
 
-fun checkSerialPage(pobi: List<Int>, crong: List<Int>): Int {
+fun checkSerialPage(pobi: List<Int>, crong: List<Int>): Int { // 연속되는 페이지 여부 검사
     if (((pobi[1] - pobi[0]) != 1) or ((crong[1] - crong[0]) != 1)) {
         return -1
     }
     return 0
 }
 
-fun Comparator(a: Int, b: Int): Int {
+fun comparator(a: Int, b: Int): Int { // 두 수 비교 후, 큰 값 출력
     return max(a, b)
 }
 
 
-fun digitPlus(list: List<Int>): Int { // 각 페이지 자리수 더하기, 큰 수 리턴
+fun digitPlus(list: List<Int>): Int { // 각 페이지 자리수 더하기
     var leftPage = list[0]
     var rightPage = list[1]
     var resultLeftPage = 0
@@ -64,10 +64,10 @@ fun digitPlus(list: List<Int>): Int { // 각 페이지 자리수 더하기, 큰 
         rightPage /= 10
     }
 
-    return Comparator(resultLeftPage, resultRightPage)
+    return comparator(resultLeftPage, resultRightPage)
 }
 
-fun digitMultiple(list: List<Int>): Int {
+fun digitMultiple(list: List<Int>): Int { // 각 페이지 자리수 곱하기
     var leftPage = list[0]
     var rightPage = list[1]
     var resultLeftPage = 1
@@ -83,5 +83,5 @@ fun digitMultiple(list: List<Int>): Int {
         rightPage /= 10
     }
 
-    return Comparator(resultLeftPage, resultRightPage)
+    return comparator(resultLeftPage, resultRightPage)
 }
