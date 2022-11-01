7b15d641ed2fd4e4fc822b675cb3ca8edda845bc
cyjadela <adelacho@naver.com>
2022-11-01 18:23:21 +0900

    feat: [문제7] getUserList 함수 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -7,3 +7,24 @@ fun solution7(
 ): List<String> {
     TODO("프로그램 구현")
 }
+
+fun getUserList(friends: List<List<String>>, visitors: List<String>): List<String> {
+    val userList: MutableList<String> = mutableListOf()
+
+    for (i in friends) {
+        if (!userList.contains(i[0])) {
+            userList.add(i[0])
+        }
+        if (!userList.contains(i[1])) {
+            userList.add(i[1])
+        }
+    }
+
+    for (i in visitors) {
+        if (!userList.contains(i)) {
+            userList.add(i)
+        }
+    }
+
+    return userList
+}
