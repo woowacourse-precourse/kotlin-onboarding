f5ccd0c5b2ac238757321d17c0c8cf6ff1a6f4fd
Lee-GS <leegs4831@naver.com>
2022-11-01 20:11:32 +0900

    중복의 최소조건은 2글자가 연속되는것이므로 이름 2글자씩 잘라 리스트의 길이만큼 비교해보고 일치하는 요소가 있다면 그 요소의 위치에 있는 email을 email_list에 추가 이것을 forms 크기만큼 반복

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,18 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val list = forms
+    var email_list = mutableListOf<String>()
+    for(i : Int in 0..list.size-1){
+        for(j: Int in 0.. list[i][1].length-2){
+            var check = list[i][1].slice(j..j+1)// 최소 2글자 중복이므로 2글자 슬라이스
+            for (k: Int in i+1..list.size-1){
+                if(check in list[k][1]){ // 일치하는 글자가 있을경우 email_list에 추가
+                    email_list.add(list[i][0])
+                    email_list.add(list[k][0])
+                }
+            }
+        }
+    }
+    return email_list.distinct().sorted()
 }
