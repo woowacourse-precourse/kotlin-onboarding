984c6587aaea58a1eaed6346ebec07fe3d995bf6
Jisu Moon <barcode_no_1@naver.com>
2022-11-01 21:15:34 +0900

    중복되는 글자 제거 기능

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -5,8 +5,27 @@ fun solution2(cryptogram: String): String {
     return answer
 }
 
-/* 대,소문자 확인 기능 */
-fun smallAlphabetCheck(string: String): Boolean {
-    for (i in 0 until string.length) if (Character.isUpperCase(string[i])) return false
-    return true
+/* 중복되는 글자 제거 기능 */
+fun deleteDuplicatedChar(cryptogram: String): List<Char> {
+    var i = 0
+    val temp: MutableList<Char> = ArrayList()
+    var preChar = '0'
+    while (i < cryptogram.length) {
+        val cryptoChar = cryptogram[i]
+        if (i == 0) {
+            temp.add(cryptoChar)
+            preChar = cryptoChar
+        } else {
+            if (cryptoChar == preChar) {
+                if (cryptoChar == temp[temp.size - 1]) {
+                    temp.removeAt(temp.size - 1)
+                }
+            } else {
+                temp.add(cryptoChar)
+                preChar = temp[temp.size - 1]
+            }
+        }
+        i++
+    }
+    return temp
 }
