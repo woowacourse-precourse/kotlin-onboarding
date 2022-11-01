4bbd911bf6a3f5ef81db36838da5db68c5a74034
Yuhee Kim <dbgml4509@naver.com>
2022-11-01 21:17:45 +0900

    style : solution4 주석 제거

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,12 +1,5 @@
 package onboarding
 
-//기능 목록
-/*
-    1. 엄마 말씀 word의 알파벳을 청개구리 사전을 참고해 변환
-    2. 알파벳 대문자의 경우 대문자로, 소문자의 경우 소문자로 변환
- */
-//예외 사항
-/*
     1. word가 알파벳 외의 문자인 경우
  */fun solution4(word: String): String {
     //프로그램 구현
