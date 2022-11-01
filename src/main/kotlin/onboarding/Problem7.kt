fb54855ed6fa7cb3660d44488ce4b6f022b450d3
kjyook <106497893+kjyook@users.noreply.github.com>
2022-11-01 20:10:12 +0900

    문제 7 사람들을 기록할 friends 클래스 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,9 +1,16 @@
 package onboarding
 
+class friends{
+    var check = 0       //user와 친구인지 확인해주는 변수
+    var point = 0       //추천 점수를 기록하는 변수
+}
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    var temp = mutableListOf<friends>()
+    var answer = mutableListOf<String>()
+
+    return answer
 }
