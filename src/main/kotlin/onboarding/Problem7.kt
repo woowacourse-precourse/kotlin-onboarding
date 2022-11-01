cf3754cfa96160da6f2da3e0a3cb02d675bc51e5
leeeha <jxlhe46@seoultech.ac.kr>
2022-11-01 17:04:20 +0900

    docs: Problem7 주석으로 설명 추가

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,6 +1,6 @@
 package onboarding
 
-fun findIndex(
+fun findIndex( // Pair의 second를 기준으로 검색
     list: MutableList<Pair<Int, String>>,
     item: String
 ): Int {
@@ -16,7 +16,7 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    // direct 목록 생성
+    // user와 직접적으로 연결된 direct 친구 목록 생성
     val direct = mutableListOf<Pair<Int, String>>() // index, name
     for(i in friends.indices){
         if(friends[i][0] == user){
@@ -30,21 +30,22 @@ fun solution7(
     val indirect = mutableListOf<Pair<Int, String>>() // score, name
     for(i in friends.indices){
         for(j in direct.indices){
-            // direct는 추천에서 제외
+            // direct에 해당하는 친구는 추천에서 제외
             if(i == direct[j].first) continue
 
+            // direct와 연결된 친구는 +10점
             if(direct[j].second == friends[i][0]){
                 val name = friends[i][1] // direct의 친구 이름
                 val idx = findIndex(indirect, name)
-                if(idx == -1){
-                    indirect.add(Pair(10, name))
-                }else{
+                if(idx == -1){ // indirect에 없는 이름인 경우
+                    indirect.add(Pair(10, name)) // 새로 추가
+                }else{ // 이미 한번 추가된 적이 있는 경우
                     val oldPair = indirect[idx]
-                    val newPair = oldPair.copy(first = oldPair.first + 10)
+                    val newPair = oldPair.copy(first = oldPair.first + 10) // 점수 갱신
                     indirect[idx] = newPair
                 }
             }else if(direct[j].second == friends[i][1]){
-                val name = friends[i][0] // direct의 친구 이름
+                val name = friends[i][0]
                 val idx = findIndex(indirect, name)
                 if(idx == -1){
                     indirect.add(Pair(10, name))
@@ -60,29 +61,31 @@ fun solution7(
     // visitors 목록에서 점수 측정
     for(i in visitors.indices){
         val name = visitors[i]
+        // direct에 해당하지 않는 사람들만 점수 측정
         if(findIndex(direct, name) == -1){
             val idx = findIndex(indirect, name)
-            if(idx == -1){
-                indirect.add(Pair(1, name))
-            }else{
+            if(idx == -1){ // indirect에 없는 이름인 경우
+                indirect.add(Pair(1, name)) // 새로 추가
+            }else{ // 이미 추가된 적이 있는 경우
                 val oldPair = indirect[idx]
-                val newPair = oldPair.copy(first = oldPair.first + 1)
+                val newPair = oldPair.copy(first = oldPair.first + 1) // 점수 갱신
                 indirect[idx] = newPair
             }
         }
     }
 
     val sortedList = indirect.sortedWith(
-        compareByDescending<Pair<Int, String>>{ it.first } // 점수를 기준으로 내림차순
-            .thenBy { it.second } // 이름을 기준으로 오름차순
+        compareByDescending<Pair<Int, String>>{ it.first } // 점수 기준으로 내림차순 정렬
+            .thenBy { it.second } // 점수가 같으면, 이름을 기준으로 오름차순 정렬
     )
 
+    // 이름만 따로 저장하여 리턴하기
     val result = mutableListOf<String>()
-    if(sortedList.size < 5){
+    if(sortedList.size < 5){ // 5개 미만인 경우
         for(item in sortedList){
             result.add(item.second) // 전부 추가
         }
-    }else{
+    }else{ // 5개 이상인 경우
         var cnt = 0
         for(item in sortedList){
             if(cnt >= 5)  break // 이미 5개 추가했으면 끝!!
