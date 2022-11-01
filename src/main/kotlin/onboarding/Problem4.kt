2f68b32256751448470e54eebd6c1a42df3f6c01
SeungHyeon Shim <tladhehd@naver.com>
2022-11-01 21:11:12 +0900

    4번 리팩토링

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -10,26 +10,30 @@ package onboarding
 // word 1 .. 1000
 // A 65 Z 90 a 97 z 122
 
+fun alphaChange(alpha: Char): Char { // 변환된 문자를 return
+    var letter = ' '
+    var ascii = 0
+    ascii = alpha.toInt()
+    // 대문자일때
+    if (ascii in 65..90) {
+        letter = (90 - (ascii - 65)).toChar()
+    }
+    // 소문자일때
+    else if(ascii in 97 .. 122){
+        letter = (122 - (ascii - 97)).toChar()
+    }
+    // 스페이스일때 스페이스만 추가함
+    if (ascii == 32) {
+        letter=' '
+    }
+
+    return letter
+}
+
 fun solution4(word: String): String {
     var result = ""
-    var ascii = 0
-    var letter = ' '
     for (i in 0 until word.length) {
-        ascii = word[i].toInt()
-        // 대문자일때
-        if (ascii in 65..90) {
-            letter = (90 - (ascii - 65)).toChar()
-            result+=letter
-        }
-        // 소문자일때
-        else if(ascii in 97 .. 122){
-            letter = (122 - (ascii - 97)).toChar()
-            result+=letter
-        }
-        // 스페이스일때 스페이스만 추가함
-        if (ascii == 32) {
-            result+=' '
-        }
+        result+= alphaChange(word[i])
     }
     return result
 }
