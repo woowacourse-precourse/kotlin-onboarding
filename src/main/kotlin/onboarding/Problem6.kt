153a0f1cee2c527ecf38b95a54ba80907cbf7237
JinJoa <mitejoa@gmail.com>
2022-11-01 23:45:35 +0900

    문제6 - 미완

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,27 @@
 package onboarding
 
+//두글자 연속 같은 닉네임 -> 이메일 목록 리턴
+// 1 <= n <= 10,000
+// 이메일 도메인 :email.com -> 정규식 :
+// 닉넴 한글, 1~19 -> 정규식
+
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    //겹치는 닉넴
+    var name_list = mutableListOf<String>()
+    //반환 리스트
+    var email_list = mutableListOf<String>()
+
+
+    for(i in 0 until forms.size){
+        for(j in 1 until forms[i][1].length){
+            //두글자씩 추출
+            val jm = forms[i][1].substring(j .. j+1)
+            print(jm)
+        }
+
+
+    }
+
+
+    return email_list
 }
