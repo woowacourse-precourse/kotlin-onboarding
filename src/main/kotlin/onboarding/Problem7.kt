3999f8ae5dcb0684c6ab1ac7ac978e64d491aa93
lee989898 <@@qwer1352>
2022-11-01 21:58:24 +0900

    [refactor](Problem7) isAppropriateInput 함수 if문 when으로 변경

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -53,14 +53,10 @@ fun isAppropriateInput(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    if (user.length !in 1..30) {
-        return listOf("user는 길이가 1 이상 30 이하인 문자열이어야 합니다.")
-    }
-    if (friends.size !in 1..10000) {
-        return listOf("friends는 길이가 1 이상 10,000 이하인 리스트/배열이어야 합니다.")
-    }
-    if (visitors.size !in 0..10000) {
-        return listOf("visitors는 길이가 0 이상 10,000 이하인 리스트/배열이어야 합니다.")
+    when {
+        user.length !in 1..30 -> return listOf("user는 길이가 1 이상 30 이하인 문자열이어야 합니다.")
+        friends.size !in 1..10000 -> return listOf("friends는 길이가 1 이상 10,000 이하인 리스트/배열이어야 합니다.")
+        visitors.size !in 0..10000 -> return listOf("visitors는 길이가 0 이상 10,000 이하인 리스트/배열이어야 합니다.")
     }
     for (i in friends.indices) {
         if (friends[i].first().length !in 0..30 || friends[i].last().length !in 0..30) {
