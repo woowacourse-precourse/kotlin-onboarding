74cd679a0c99c168c68fa7c8999491d9614c2088
noion0511 <noion0511@gmail.com>
2022-11-01 23:48:34 +0900

    [problem 6][feat] 닉네임이 중복되면 이메일을 결과 값에 추가하는 기능

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,26 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val result = arrayListOf<String>()
+    val wordsList = getWordsList(forms)
+
+    for(i in forms.indices) {
+        if(isDuplicatedNickname(i, wordsList)) {
+            result.add(forms[i][0])
+        }
+    }
+
+    result.sort()
+    return result
 }
+
+fun isDuplicatedNickname(index: Int, wordsList: Array<HashSet<String>>): Boolean {
+    for(i in wordsList.indices) {
+        if(index != i && wordsList[index].any { wordsList[i].contains(it) }) {
+            return true
+        }
+    }
+    return false
+}
+
+
