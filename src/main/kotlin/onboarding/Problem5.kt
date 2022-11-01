db829483585fa0a819605870756384815296447f
Jihee.Han <42907876+jihee-dev@users.noreply.github.com>
2022-11-01 22:53:23 +0900

    feat: solve problem 5

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,25 @@
 package onboarding
 
+fun main() {
+    solution5(50237).forEach { print("$it ") }
+    println()
+    solution5(15000).forEach { print("$it ") }
+}
+
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val units = intArrayOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val result = intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+    var current = money
+
+    for (index in units.indices) {
+        while (current - units[index] >= 0) {
+            current -= units[index]
+            result[index]++
+
+            if (current == 0) break
+        }
+    }
+
+    return result.toList()
 }
