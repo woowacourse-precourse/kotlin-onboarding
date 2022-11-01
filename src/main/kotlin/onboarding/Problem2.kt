8d99d485d9012e1b6a4c4c96b2c0f5f2b609f1ac
Jisu Moon <barcode_no_1@naver.com>
2022-11-01 21:14:20 +0900

    대,소문자 알파벳 확인 기능

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,12 +1,12 @@
 package onboarding
 
-/* 2번 솔루션 로직 */
 fun solution2(cryptogram: String): String {
-    var answer = "answer"
-    if (cryptogram.length < 1 || cryptogram.length > 1000 || !smallAlphabetCheck(cryptogram)) return "Cryptogram must be 1~1000 lowercase letters" //요구사항 설정된 예외처리
-    val temp = deleteDuplicatedChar(cryptogram)
-    if (cryptoCheck(listToString(temp), cryptogram) || temp.size <= 1) return listToString(temp)
-    answer = solution2(listToString(temp))
+    val answer = "answer"
     return answer
 }
 
+/* 대,소문자 확인 기능 */
+fun smallAlphabetCheck(string: String): Boolean {
+    for (i in 0 until string.length) if (Character.isUpperCase(string[i])) return false
+    return true
+}
