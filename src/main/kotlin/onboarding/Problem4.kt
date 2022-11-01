1021e0197488c27d9239abce1801584db463fc97
lee989898 <@@qwer1352>
2022-11-01 13:09:18 +0900

    [feat](Problem4) 알파벳을 역순으로 담은 리스트 변수 추가

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -6,7 +6,12 @@ fun solution4(word: String): String {
     }
 
     var treeFrogWord = ""
-    return ""
+    val reverseOrderAlphabet = listOf(
+        "Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H",
+        "G", "F", "E", "D", "C", "B", "A"
+    )
+
+    return treeFrogWord
 }
 fun isAppropriateWord(word: String): Boolean {
     if (word.length !in 1..1000) {
