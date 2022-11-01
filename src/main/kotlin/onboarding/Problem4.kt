f5c0cffb17cc4fec5290c99ad9f520bbba661ee3
JinJoa <mitejoa@gmail.com>
2022-11-01 21:33:30 +0900

    문제4 - a to z && z to a 배열 생성

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,15 @@
 package onboarding
-
+fun main(){
+    solution4("helloeew")
+}
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    //리턴값
+    val revese_word : String =""
+    //a to z & z to a 배열
+    val dict = mutableListOf<Char>()
+    for (c in 'a'..'z'){ dict.add(c) }
+    var revese_dict  = dict.reversed() as MutableList<Char>
+
+
+    return revese_word
 }
