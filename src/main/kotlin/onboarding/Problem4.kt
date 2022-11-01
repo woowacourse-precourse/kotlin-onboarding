e2c4f8a476af69c6b815f8a086646c5be73805bf
sjh970603 <sjh970603@naver.com>
2022-11-01 15:15:24 +0900

    problem 4번 완료 소대문자 (A~M , Z~N)의 index위치가 같다는것을 이용함

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -5,29 +5,46 @@ import java.lang.StringBuilder
 fun solution4(word: String): String {
     //TODO("프로그램 구현")
     var str : StringBuilder = StringBuilder(word)
+    val bigarray1: List<String> = listOf("A","B","C","D","E","F","G","H","I","J","K","L","M")
+    val bigarray2: List<String> = listOf("Z","Y","X","W","V","U","T","S","R","Q","P","O","N")
+    val smallarray1: List<String> = listOf("a","b","c","d","e","f","g","h","i","j","k","l","m")
+    val smallarray2: List<String> = listOf("z","y","x","w","v","u","t","s","r","q","p","o","n")
+    var str2 : StringBuilder = StringBuilder()
 
+    var count = 0
+    println(str2)
 
-    for (j in 1..word.length)
+    for(i in 0..str.length-1)
     {
-        if ( (word[j].code >= 65 && word[j].code <= 77) || (word[j].code >= 97 && word[j].code <= 109) )
+        count=0
+        for (j in 0..bigarray1.size-1)
         {
-            for (i in 25 downTo 1 step(2) ) {
-                if (word[j].code ==65+i)
-                {
-                    str[j] = str[j + i]
-                }
+            if (str[i].toString() == bigarray1[j])
+            {
+                str2.append(bigarray2[j])
+                count+=1
+            }
+            else if (str[i].toString() == bigarray2[j])
+            {
+                str2.append(bigarray1[j])
+                count+=1
+            }
+            else if (str[i].toString() == smallarray1[j])
+            {
+                str2.append(smallarray2[j])
+                count+=1
+            }
+            else if (str[i].toString() == smallarray2[j])
+            {
+                str2.append(smallarray1[j])
+                count+=1
             }
         }
-        else if ( (word[j].code >= 78 && word[j].code <= 90 ) || (word[j].code >= 110 || word[j].code <= 122))
+        if (count==0)
         {
-            for (i in 1..25 step(2))
-            {
-                if (word[j].code ==78-i)
-                {
-                    str[j] = str[j - i]
-                }
-            }
+            str2.append(str[i])
         }
     }
- return str
+
+    return str2.toString()
 }
