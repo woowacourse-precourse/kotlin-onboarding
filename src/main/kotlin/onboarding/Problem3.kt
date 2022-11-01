0ea5e48d18b376fd83852a6c89ef6453ab35045d
dltkd1395 <dltkd13956@naver.com>
2022-11-01 16:05:20 +0900

    refactor : [PROBLEM3] const val로 수정

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,10 +1,10 @@
 package onboarding
 
-val START_NUMBER = 1
-val THREE = 3
-val SIX = 6
-val NINE = 9
-val TEN = 10
+const val START_NUMBER = 1
+const val THREE = 3
+const val SIX = 6
+const val NINE = 9
+const val TEN = 10
 
 fun solution3(number: Int): Int {
     return startGame(START_NUMBER, number, 0)
