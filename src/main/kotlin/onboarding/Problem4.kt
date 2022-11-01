0e3539ea139c10c1f9c86f0f4e6b4b1b6dcf018d
최재훈 <be_clever@naver.com>
2022-11-01 18:38:25 +0900

    feat(Problem4): 문자를 대소문자에 따라 나누는 기능 작성

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,21 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+
+}
+
+fun getConvertedWord(character: Char): Char {
+    return if (character.isUpperCase()) {
+        convertUpperCase(character)
+    } else {
+        convertLowerCase(character)
+    }
+}
+
+fun convertUpperCase(character: Char): Char {
+
+}
+
+fun convertLowerCase(character: Char): Char {
+
 }
