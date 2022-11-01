f6ae73f41b0be16e36d7012e453f33fec4532247
권용민 <briandr@naver.com>
2022-11-01 17:27:43 +0900

    [Problem1] 홀수 짝수 검사 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -20,7 +20,7 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 }
 
 private fun isException(pages: List<Int>): Boolean {
-    return (isContinuous(pages) and isInRange(pages))
+    return (isContinuous(pages) and isInRange(pages) and checkOddEven(pages))
 }
 
 private fun isContinuous(pages: List<Int>): Boolean {
@@ -29,6 +29,10 @@ private fun isContinuous(pages: List<Int>): Boolean {
 
 private fun isInRange(pages: List<Int>): Boolean {
     return (pages[0] in 3..398) and (pages[1] in 3..398)
+}
+
+private fun checkOddEven(pages: List<Int>): Boolean {
+    return (pages[0] % 2 != 0) and (pages[1] % 2 == 0)
 }
 
 private fun calculatePageMax(pages: List<Int>): Int {
