4734dbd4ae57f87b9e6f0213d58772af0b264663
Jaehun-Jo <chojh922@gmail.com>
2022-11-01 16:12:44 +0900

    Problem1-제한사항 예외 처리

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -8,5 +8,32 @@ const val DRAW = 0
 const val EXCEPTION = -1
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if (isException(pobi, crong)) return EXCEPTION
+
+    return 0
+}
+
+private fun isException(pobi: List<Int>, crong: List<Int>): Boolean {
+    listOf(pobi, crong).forEach { list ->
+        if (isListSizeException(list)) return true
+        if (isPageRangeException(list)) return true
+        if (isContinuedPageException(list)) return true
+    }
+
+    return false
+}
+
+private fun isListSizeException(list: List<Int>): Boolean {
+    if (list.size != 2) return true
+    return false
+}
+
+private fun isPageRangeException(list: List<Int>): Boolean {
+    if (list[FIRST_PAGE] <= 1 || list[SECOND_PAGE] >= 400) return true
+    return false
+}
+
+private fun isContinuedPageException(list: List<Int>): Boolean {
+    if (list[SECOND_PAGE] - list[FIRST_PAGE] != 1) return true
+    return false
 }
