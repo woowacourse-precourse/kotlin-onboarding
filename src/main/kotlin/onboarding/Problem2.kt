c562fe43589974965a065f4315e91df9d9fe87e8
Lee-GS <leegs4831@naver.com>
2022-11-01 20:10:16 +0900

    글자를 리스트화 한후 i 글자와 i+1의 글자가 같은지 확인하고 같다면 혹시 3글자 이상으로 같진 않은지 확인하고 같다면 삭제후 위의 작업 반복

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,31 @@
 package onboarding
 
+import com.sun.jdi.connect.Connector.IntegerArgument
+
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    val crypto = cryptogram.chunked(1) // 1글자씩 리스트화
+    val list  = crypto.toMutableList()
+    fun execute(list: List<String>) {
+
+    }
+    var i = 0
+    while (i<list.size-1){
+        if (list[i] == list[i+1]){ //  2개연속 같은 글자
+            var k = i+1
+            var cnt = 0
+            while (k<list.size-1) { // 3개연속 같은 글자일경우
+                if (list[k] == list[k + 1]){
+                    cnt++
+                    k++
+                }
+                else{ break }
+            }
+            for (j in 0..cnt+1) { list.removeAt(i) } //삭제
+        }
+        else{ i++ }
+    }
+
+    val ans = list.joinToString("")
+    return ans
+
 }
