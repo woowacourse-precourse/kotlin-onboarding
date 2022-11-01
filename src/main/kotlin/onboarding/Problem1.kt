d8b0f8f121abe640b0ced00b73be2a479777f1e7
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-01 15:12:32 +0900

    전달 받은 리스트의 범위 검사 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,26 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if(!isValid(pobi) || !isValid(crong)){
+        return -1
+    }
+    return 0;
+}
+
+/* pobi와 crong의 범위를 검사하는 메소드 */
+fun isValid(book: List<Int>): Boolean {
+    val left = book[0]
+    val right = book[1]
+
+    if(left < 1 || left > 400 || right < 1 || right > 400){
+        return false
+    }
+    if((right - left) != 1) {
+        return false
+    }
+    if(left % 2 != 1 || right % 2 != 0){
+        return false
+    }
+
+    return true
 }
