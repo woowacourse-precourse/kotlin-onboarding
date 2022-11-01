2af6f71b69c94bf3b08ef834abe35d7f91f5da47
lee989898 <@@qwer1352>
2022-11-01 12:55:37 +0900

    [feat](Problem3) 333,666,999 같은 숫자를 하나씩 나눠서 세는 기능 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -8,7 +8,16 @@ fun solution3(number: Int): Int {
     var countNumberOf369 = 0
     val filterWith369 = (0..number).filter { it % 10 == 3 || it % 10 == 6 || it % 10 == 9 || it / 10 == 3 || it / 10 == 6 || it / 10 == 9 }
 
-    return -1
+    for (element in filterWith369) {
+        var element369 = element
+        while (element369 > 0) {
+            if (element369 % 10 == 3 || element369 % 10 == 6 || element369 % 10 == 9) {
+                countNumberOf369++
+            }
+            element369 /= 10
+        }
+    }
+    return countNumberOf369
 }
 
 fun isAppropriateNumber(number: Int): Boolean {
