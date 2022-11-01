416f42875f945feb5daec38d3e8877d34d007b1d
dlgocks1 <cksgodl1@naver.com>
2022-11-01 11:31:25 +0900

    fix: (Problem6) Collections함수 사용하는 코드로 변경

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -4,7 +4,7 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     val answer = mutableListOf<String>()
 
-    for (i in 0 until forms.size) {
+    for (i in forms.indices) {
         val email = forms[i][0]
         val nickname = forms[i][1]
 
@@ -14,7 +14,7 @@ fun solution6(forms: List<List<String>>): List<String> {
         check(email.length in 11..19) {
             "$email 이메일의 길이를 지켜주세요."
         }
-        check(nickname.length in 1..19 && checkNicknameIsKr(nickname = nickname)) {
+        check(checkNicknameIsKr(nickname = nickname)) {
             "$nickname 이름의 형식을 지켜주세요."
         }
 
@@ -36,15 +36,13 @@ fun solution6(forms: List<List<String>>): List<String> {
     }
 
     // 중복제거 및 정렬
-    return sortEmailByAesc(emailDuplicateCheck(answer))
+    return answer.distinct().sorted()
 }
 
-/** 닉네임이 한글인지 체크한다. */
+/** 닉네임이 한글인지, 1자 이상 20자 미만인지 체크한다. */
 private fun checkNicknameIsKr(nickname: String): Boolean {
-    for (i in nickname) {
-        if (i.code < '가'.code || i.code > '힣'.code) return false
-    }
-    return true
+    val regex = Regex("^[가-힣]{1,20}$")
+    return regex.matches(nickname)
 }
 
 /**
@@ -65,66 +63,12 @@ fun exportStrList(nickname: String): List<String> {
  */
 fun divideEmail(email: String): Pair<String, String> {
     var idx = 0
-    for (i in 0 until email.length) {
-        if (email[i] == '@') break
+    for (i in email) {
+        if (i == '@') break
         idx += 1
     }
     return email.subSequence(0, idx).toString() to email.subSequence(idx + 1, email.length).toString()
 }
 
-/**
- * 기존문자열에서 파라미터로 온 2글자의 문자열을 포함하는지 체크한다.
- * 2글자 이상의 파라미터 문자열 및 기본 문자열만 체크한다.
- */
-fun String.contains(findStr: String): Boolean {
-    if (this.length <= 1 && findStr.length <= 1) {
-        return false
-    }
-    for (i in 0 until this.length - 1) {
-        if ((this[i].toString() + this[i + 1].toString()) == findStr) {
-            return true
-        }
-    }
-    return false
-}
-
 /** 이메일의 도메인이 "email.com"인지 체크하는 함수 */
 private fun String.isDomainEmail(): Boolean = this == "email.com"
-
-/**
- * 이메일 리스트를 이메일에 해당하는 부분의 문자열 기준으로
- * 오름차순으로 정렬하여 반환한다.
- */
-fun sortEmailByAesc(emailList: List<String>): List<String> {
-    val emailList = ArrayList(emailList.map {
-        divideEmail(it).first
-    })
-    // 버블 정렬
-    for (i in 0 until emailList.size) {
-        for (j in i until emailList.size) {
-            if (emailList[i] > emailList[j]) {
-                val temp = emailList[j]
-                emailList[j] = emailList[i]
-                emailList[i] = temp
-            }
-        }
-    }
-    return emailList.map {
-        "$it@email.com"
-    }.toList()
-}
-
-/** 리스트에 중복된 이메일을 제거한다. */
-fun emailDuplicateCheck(emailList: List<String>): List<String> {
-    val pureList = mutableListOf<String>()
-    for (i in 0 until emailList.size) {
-        pureList.add(emailList[i])
-        for (j in i + 1 until emailList.size) {
-            if (pureList.contains(emailList[j])) {
-                pureList.removeAt(pureList.lastIndex)
-                break
-            }
-        }
-    }
-    return pureList
-}
