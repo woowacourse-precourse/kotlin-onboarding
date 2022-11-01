af6a69cf06aeac897d2ea9a53903a63903a7d064
choHarmony <jws0183@naver.com>
2022-11-01 22:03:23 +0900

    [문제 3] number 이하 수를 쪼개 3,6,9가 있을 경우 clap에 1씩 더해줌

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -2,9 +2,20 @@ package onboarding
 
 fun solution3(number: Int): Int {
 
+    var clap = 0
+
     fun chunkNum(n: Int): IntArray {
         return n.toString().chunked(1).map { it.toInt() }.toIntArray()
     }
 
-    return 0
+    for (i in 1..number) {
+        val chunkedNumList = chunkNum(i)
+        for (j in chunkedNumList) {
+            if (j == 3 || j == 6 || j == 9) {
+                clap += 1
+            }
+        }
+    }
+
+    return clap
 }
