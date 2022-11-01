80b26e0db48c91a469d5a873cbd42d88db6a73d6
lsakee <lsls4868@gmail.com>
2022-11-01 11:12:52 +0900

    Feat : overlapRemove()메서드 기능 추가 -중복된 문자열을 replace를통하여 공백으로 바꿔주는 기능

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,30 @@
 package onboarding
 
+
+//기능
+//중복된 문제열을 제거 하는 기능
+//문자열 중복 문자열을 찾아 배열의 저장 하는 기능
+
+
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var inputCrytoGram = cryptogram
+
+    return inputCrytoGram
 }
+
+
+
+// 중복 문자를 삭제해서 리턴해주는 함수
+fun overlapRemove(overlapArray: ArrayList<String>, input: String): String {
+    // 리턴 할 변수
+    var result = input
+
+    // 중복 문자열의 갯수만큼 반복, 공백으로 replace
+    for (i in 0..overlapArray.size - 1) {
+        result = result.replace(overlapArray[i], "")
+    }
+
+    // 중복된 문자열을 제거 후 리턴
+    return result
+}
+
