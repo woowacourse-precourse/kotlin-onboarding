b03b1b4484a4d7a1e14edcbb74148f7d51e9c9f0
lsakee <lsls4868@gmail.com>
2022-11-01 10:50:25 +0900

    Feat : CalculatePage() 메서드 추가 -숫자를 문자열로 변환후 인덱스 분리후 더한 값과 곱한 값을 비교하여 최대 값을 저장한다

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,30 @@
 package onboarding
 
+import java.util.*
+
+//기능 정리
+//각 유저가 펼친 페이지에서 더하거나 곱했을 때 큰값을 찾는 기능
+//숫자를 문자로 전환하여 분리
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    var result: Int = 0
+    return result
+}
+
+fun calculatePage(StringConversion: String): Int {
+    var PlusNum = 0
+    var MultiplyNum = 1
+    var maxValue: Int
+    for (j in 0..StringConversion.length - 1) {
+        //문자열로 분리하여 양쪽페이지의 최대 값을 저장 ex) 2 2 7
+        val stringNumber = StringConversion.substring(j until j + 1).toInt()
+        PlusNum = PlusNum + stringNumber
+        MultiplyNum = MultiplyNum * stringNumber
+    }
+    if (PlusNum > MultiplyNum) {
+        maxValue = PlusNum
+    } else
+        maxValue = MultiplyNum
+
+    return maxValue
 }
