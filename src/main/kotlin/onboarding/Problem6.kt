b1b74be8e7a78c0524ecf528730bc4389df1b852
s9hn <tnghqn@naver.com>
2022-11-01 19:55:15 +0900

    [REFACTOR(미션6)] 코드 정리 및 리드미 기능목록 수정
    1. 주석 추가
    2. 함수 분리
    3. 변수명 및 함수명 변경
    4. 리드미 기능목록 재수정

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,24 +1,30 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    val resultSet = mutableSetOf<String>()
 
-    for (j in forms.indices) { // j = 0
+    return checkSerialName(forms)
+}
 
-        val nickName = forms[j][1] // 제이슨
-        for (k in 0 until nickName.length - 1) { // k = 0
-            val string = nickName.substring(k, k + 2)
+fun checkSerialName(forms: List<List<String>>): List<String> { // 리스트 탐색, 이름 2글자 분리 및 대조, 이메일 리스트 저장
+    val resultSet = mutableSetOf<String>() // 중복제거 Set
 
-            for (l in forms.indices) { // l = 0 , 1부터 4
-
-                if ((nickName != forms[l][1]) and forms[l][1].contains(string)) { // 원피수.contains(원피)
-                    resultSet.add(forms[l][0])
-
-
+    for (i in forms.indices) {
+        // [이메일, 이름]
+        val nickName = forms[i][1]
+        // [이름, 이름, 이름] 각 인덱스의 이름 저장
+        for (j in 0 until nickName.length - 1) {
+            val splitName = nickName.substring(j, j + 2)
+            // 이름 첫글자부터 +2글자씩 파싱
+            for (k in forms.indices) {
+                // [이메일, 이름]
+                if ((nickName != forms[k][1]) and forms[k][1].contains(splitName)) {
+                    // 닉네임!=이름, 파싱된 이름이 포함될 경우
+                    resultSet.add(forms[k][0])
+                    // 해당 이름의 이메일, 리스트에 저장
                 }
             }
         }
     }
-
     return resultSet.sorted()
+    // 오름차순 정렬
 }
