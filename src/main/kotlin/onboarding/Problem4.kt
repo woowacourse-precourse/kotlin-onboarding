2d5207b02bae40ba0566f4f0ed776e58215d0764
lh99j <grand2181@gmail.com>
2022-11-01 12:49:36 +0900

    Refactoring Problem4

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -3,21 +3,18 @@ package onboarding
 fun solution4(word: String): String {
     var str = ""
     for(element in word){
-        str += getAscIICode(element)
+        str += reverseStr(element)
     }
 
     return str
 }
 
-fun getAscIICode(num : Char) : Char{
+fun reverseStr(num : Char) : Char{
     val temp : Int = num.code
 
-    return if(num == ' ')
-        ' '
-    else if(temp in 65..90)
-        (155 - temp).toChar()
-    else if(temp in 97 .. 122)
-        (219 - temp).toChar()
-    else
-        num
+    return when (temp) {
+        in 65..90 -> (155 - temp).toChar()
+        in 97 .. 122 -> (219 - temp).toChar()
+        else -> num
+    }
 }
