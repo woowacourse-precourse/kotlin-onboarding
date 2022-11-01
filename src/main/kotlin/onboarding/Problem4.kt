b2b06aa22b91d98f9796864cee275a7d74df0a17
leehandsub <gustjq7027@naver.com>
2022-11-01 20:19:09 +0900

    feature/map에 미리 바꿀 문자 추가

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    val flog = mutableMapOf<String, String>()
+
+    for (i in 0..25) {
+        flog[('a' + i).toString()] = ('z' - i).toString()
+        flog[('A' + i).toString()] = ('Z' - i).toString()
+    }
+    print(flog)
+    return ""
+}
+fun main(){
+    val word = "I love you"
+    val result = "R olev blf"
+    print(solution4(word).equals(result))
 }
