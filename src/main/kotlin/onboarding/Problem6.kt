3b10f1312d4a1eabe561e11dd8d013cff8a5350b
leehandsub <gustjq7027@naver.com>
2022-11-01 21:57:10 +0900

    feature/각 닉네임마다 부분 닉네임 비교

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,22 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+
+    val ans = mutableSetOf<String>()
+
+    for (i in forms.indices) {
+        val email = forms[i][0]
+        val nick = forms[i][1]
+
+        if (nick.length >= 2) {
+            for (j in (i + 1) until forms.size) {
+                val nowEmail = forms[j][0]
+                val nowNick = forms[j][1]
+
+
+            }
+        }
+    }
+    return ans.toList().sorted()
 }
+
