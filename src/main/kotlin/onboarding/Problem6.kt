735c0d06ac26b3ee8add0c9c12a6af262d8c3377
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-01 23:02:56 +0900

    22.11.01 problem6 기능목록 및 기능 미구현 상태로  제출

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,20 @@
 package onboarding
 
-fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+fun solution6(forms: List<List<String>>) {
+    var result = listOf<String>()
+    val formsSize = forms.size
+    var word = ""
+    var targetWord = ""
+    val targetWordList = arrayListOf<String>()
+
+    for (i in 0 until formsSize){
+        word = forms[i][1]
+        for (j in 0..word.length-2) {
+            targetWord = word.substring(j..j + 1)
+            targetWordList.add(targetWord)
+        }
+        for (j in 0 until targetWordList.size)
+            for (k in i+1 until formsSize)
+                if (forms[k][1].contains(targetWordList[j])) println("${forms[i][1]}")
+    }
 }
