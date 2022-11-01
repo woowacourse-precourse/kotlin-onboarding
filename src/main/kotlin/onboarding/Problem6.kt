242d6fc0c64d0f0582c3d329a9dee75d0fbdd641
Jisu Moon <barcode_no_1@naver.com>
2022-11-01 23:00:36 +0900

    Problem6 문제 해결 (연속된 글자 닉네임 지원자 메일목록 리턴)

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,24 @@
 package onboarding
 
+import java.util.Collections
+import java.util.stream.Collectors
+
+/* 문제해결 - 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 목록을 return */
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val temp: MutableList<String> = ArrayList()
+    for (i in forms.indices) {
+        val s = forms[i][1]
+        for (j in 0 until s.length - 2) {
+            val str = s.substring(j, j + 2)
+            for (k in i + 1 until forms.size) {
+                if (forms[k][1].contains(str)) {
+                    temp.add(forms[i][0])
+                    temp.add(forms[k][0])
+                }
+            }
+        }
+    }
+    val answer = temp.stream().distinct().collect(Collectors.toList())
+    Collections.sort(answer)
+    return answer
 }
