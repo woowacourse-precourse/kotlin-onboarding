668f87b1696d7638d4c4d0cb086478ad2902f309
Jaehun-Jo <chojh922@gmail.com>
2022-11-01 16:01:29 +0900

    Problem1-상수 추가

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,12 @@
 package onboarding
 
+const val FIRST_PAGE = 0
+const val SECOND_PAGE = 1
+const val POBI_WIN = 1
+const val CRONG_WIN = 2
+const val DRAW = 0
+const val EXCEPTION = -1
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
