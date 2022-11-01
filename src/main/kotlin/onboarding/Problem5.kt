a4ed4ea7cf19fb9aa90f0d4a9bb6c9138bb0d8a1
leehandsub <gustjq7027@naver.com>
2022-11-01 20:45:43 +0900

    feature/갯수 list화

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -3,11 +3,14 @@ package onboarding
 fun solution5(money: Int): List<Int> {
     var temp = money
     val coins = intArrayOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val ans = mutableListOf<Int>()
 
     for (i in coins) {
         val value = temp / i
         if (value != 0) {
             temp -= value * i
         }
+        ans.add(value)
     }
+    return ans
 }
