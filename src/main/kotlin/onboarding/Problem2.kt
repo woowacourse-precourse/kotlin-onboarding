cdadc4566fdfa97cf07b5936b168cf18690a42ec
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-01 15:58:14 +0900

    22.11.01 problem2 기능목록 및 기능 1번 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var cryptogram = cryptogram
+
+    if (isCorrect(cryptogram)) {
+
+    } else return "wrong cryptogram"
+}
+
+
+fun isCorrect(cryptogram: String): Boolean {
+    val condition1 = (cryptogram.length >= 1) && (cryptogram.length <= 1000)
+    var condition2 = true
+    for (i in cryptogram.indices) if (cryptogram[i] !in 'a'..'z') {
+        condition2 = false
+        break
+    }
+    return condition1 && condition2
 }
