37facfc97f1625fbdce6520a4202cafa896f44bb
changhwan <chang5hustle@gmail.com>
2022-11-01 15:23:14 +0900

    [Problem 5] (리펙토링 2) 변수 선언시 불필요한 generic제거

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,7 +1,7 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    val moneyUnitList = listOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val moneyUnitList = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
     return calcUnitCount(money, moneyUnitList)
 }
 
