8d55be1fdd8ee3df607a7db15bac3e9474c198a8
Jisu Moon <barcode_no_1@naver.com>
2022-11-01 21:16:10 +0900

    문자가 담긴 list를 String형태로 변환 기능

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -5,27 +5,9 @@ fun solution2(cryptogram: String): String {
     return answer
 }
 
-/* 중복되는 글자 제거 기능 */
-fun deleteDuplicatedChar(cryptogram: String): List<Char> {
-    var i = 0
-    val temp: MutableList<Char> = ArrayList()
-    var preChar = '0'
-    while (i < cryptogram.length) {
-        val cryptoChar = cryptogram[i]
-        if (i == 0) {
-            temp.add(cryptoChar)
-            preChar = cryptoChar
-        } else {
-            if (cryptoChar == preChar) {
-                if (cryptoChar == temp[temp.size - 1]) {
-                    temp.removeAt(temp.size - 1)
-                }
-            } else {
-                temp.add(cryptoChar)
-                preChar = temp[temp.size - 1]
-            }
-        }
-        i++
-    }
-    return temp
+/* 문자가 담긴 list를 String형태로 변환 기능 */
+fun listToString(temp: List<Char>): String {
+    val str = StringBuilder()
+    for (cryptoChar in temp) str.append(cryptoChar)
+    return str.toString()
 }
