9ddf1983b57dc34586aa66ea65674eb77d6f5a8b
JunHyeok0205 <wnsdlfgur@naver.com>
2022-11-01 20:52:44 +0900

    문제 2번 수정2 / 예외처리

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,9 +1,10 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    val cryptoArray = cryptogram.toMutableList()
 
+    val cryptoArray = cryptogram.lowercase().toMutableList()
     var result: String = ""
+
     result = fixArray(cryptoArray)
     println(result)
 
@@ -12,14 +13,16 @@ fun solution2(cryptogram: String): String {
 
 fun fixArray(newcrypto: MutableList<Char>): String {
     var result: String = ""
-    for (i in 1 until newcrypto.size) {
-        if (newcrypto[i-1] == newcrypto[i]) {
-            newcrypto.removeAt(i-1)
-            newcrypto.removeAt(i-1)
-            fixArray(newcrypto)
-            return fixArray(newcrypto)
+    if (newcrypto.size in 1..1000) {
+        for (i in 1 until newcrypto.size) {
+            if (newcrypto[i - 1] == newcrypto[i]) {
+                newcrypto.removeAt(i - 1)
+                newcrypto.removeAt(i - 1)
+                fixArray(newcrypto)
+                return fixArray(newcrypto)
+            }
         }
-    }
+    } else return result
 
     result = newcrypto.joinToString("")
     println(result)
