db4839c50540cf29b8d1bd587868f697a2f1968e
JinJoa <mitejoa@gmail.com>
2022-11-01 19:18:44 +0900

    문제1 - 예외사항 제외 & 테스트완

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -4,6 +4,12 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     var pobi_max :Int = 0
     var crong_max : Int = 0
 
+    //예외 사항 제외
+    if (pobi[0] % 2 != 1 || crong[0] % 2 != 1){return -1} //왼쪽페이지가 홀수가 아닐때
+    if (pobi[0] < 3 || pobi[1] > 398){return -1} //포비가 범위 밖의 페이지를 펼칠 경우
+    if (crong[0] < 3 || crong[1] > 398){return -1} //크롱이 범위 밖의 페이지를 펼칠 경우
+    if (pobi[1] != pobi[0]+1 || crong[1] != crong[0] +1){return -1} //왼쪽페이지랑 오른쪽페이지가 매칭 안될때
+
     for (j in 0..1){ // 제한사항 : 리스트 크기는 2
         var pobi_page = pobi[j].toString() //페이지 리스트 문자열로 치환
         //sum, mul 선언
