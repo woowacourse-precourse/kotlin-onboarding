5c07fb8930083c8afe09455facfe47995d864e6a
idra79haza <idra79haza@github.com>
2022-11-01 23:01:26 +0900

    1st week task

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,88 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    // "리스트에서 각 원소 뽑아내기"
+    val aPobiOrigin = pobi[0]
+    val bPobiOrigin = pobi[1]
+    val aCrongOrigin = crong[0]
+    val bCrongOrigin = crong[1]
+
+    // "예외처리"
+    if(!errorCheck(aPobiOrigin, bPobiOrigin) || !errorCheck(aCrongOrigin, bCrongOrigin)){
+        return -1
+    }
+
+    // "각 원소 더한값, 곱한값 뽑아내기"
+    val sumPobiA = sum(aPobiOrigin)
+    val mulPobiA = mul(aPobiOrigin)
+
+    val sumPobiB = sum(bPobiOrigin)
+    val mulPobiB = mul(bPobiOrigin)
+
+
+    val sumCrongA = sum(aCrongOrigin)
+    val mulCrongA = mul(aCrongOrigin)
+
+    val sumCrongB = sum(bCrongOrigin)
+    val mulCrongB = mul(bCrongOrigin)
+
+    // "덧셈과 곱셈 중 큰거 구하기
+    val bigPobiA = com1(sumPobiA, mulPobiA)
+    val bigPobiB = com1(sumPobiB, mulPobiB)
+    val bigCrongA = com1(sumCrongA, mulCrongA)
+    val bigCrongB = com1(sumCrongB, mulCrongB)
+    
+    // "그 중 가장 큰 거 구하기
+    val bigPobi = com1(bigPobiA, bigPobiB)
+    val bigCrong = com1(bigCrongA, bigCrongB)
+
+    val res = com2(bigPobi, bigCrong)
+
+    return res
 }
+
+fun sum(input: Int): Int {
+    var num = 0
+    var input = input
+    while(input != 0){
+        num += input % 10
+        input /= 10
+    }
+
+    return num
+}
+
+fun mul(input: Int): Int {
+    var num = 1
+    var input = input
+    while(input != 0){
+        num *= input % 10
+        input /= 10
+    }
+
+    return num
+}
+fun errorCheck(a: Int, b: Int): Boolean {
+    if (a < 2 || b < 2 || a > 401 || b > 401) { return false }
+    if ((b-a) != 1 && (b-a) != -1) { return false }
+    return true
+}
+
+fun com1(pageLeft: Int, pageRight: Int): Int{
+    if(pageLeft>pageRight) { return pageLeft }
+    if(pageLeft<pageRight) { return pageRight }
+    return pageLeft
+}
+
+fun com2(pobi: Int, crong: Int): Int{
+    if(pobi>crong) { return 1 }
+    if(pobi<crong) { return 2 }
+    return 0
+}
+
+/*
+fun com(a: Int, b: Int): Int{
+    if(a>b) { return 0 }
+    if(a==b){ return 1 }
+    return -1
+}*/
--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,62 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    // 포문을 써서 중복을 확인하세요.
+    // 1. string을 돌면서 안에 있는 원소 하나씩 뽑기
+    // 2. 뽑은 것을 기존 것과 비교하기
+    // 3. 같다면, 둘을 없애서 새로운 리스트에 append하기
+
+    var result = duplicateErase(cryptogram)
+    while(duplicateCheck(result)){
+        result = duplicateErase(result)
+    }
+    return result
+
+}
+
+fun duplicateErase(cryptogram: String): String{
+    println(cryptogram)
+    var resArray = arrayListOf<String>()
+    var k = 0
+    for (j in cryptogram.indices) {
+        print("j:$j ")
+        print("k:$k ")
+
+        if (k == cryptogram.lastIndex) {
+            resArray.add(cryptogram[k].toString())
+            println(resArray)
+            break
+        }
+        if (cryptogram[k].toString() == cryptogram[k+1].toString()) {
+            var a = cryptogram[k].toString()
+            var b = cryptogram[k+1].toString()
+            println("cryptogram[k].toString(): $a")
+            println("cryptogram[k+1].toString(): $b")
+            if (cryptogram.length > 2) {
+                k += 2
+            }
+            else {
+                return ""
+            }
+            continue
+        }
+        resArray.add(cryptogram[k].toString())
+        k++
+        println(resArray)
+    }
+    var duplicateEraseResult = resArray.toString()
+    println("duplicateEraseResult: $duplicateEraseResult")
+    return resArray.joinToString("").replace("\\s".toRegex(), "")
+}
+
+fun duplicateCheck(cryptogram: String): Boolean{
+    for ((k, j) in cryptogram.withIndex()) {
+        if (k == cryptogram.lastIndex) {
+            break
+        }
+        if (cryptogram[k].toString() == cryptogram[k+1].toString()) {
+            return true
+        }
+    }
+    return false
 }
