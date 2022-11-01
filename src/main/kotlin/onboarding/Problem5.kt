754f762a4881be644353348fb82db4ca7c8f5f46
Seyong <38519706+Syeeze@users.noreply.github.com>
2022-11-01 17:04:59 +0900

    1~5번

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,19 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    //TODO("프로그램 구현")
+    var currentMoney = money
+    var result = mutableListOf<Int>(0,0,0,0,0,0,0,0,0)
+    val change = arrayOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+
+    var j = 0
+    for (i : Int in change) {
+        var value = currentMoney / i
+        if (value != 0) {
+            result[j] = value
+            currentMoney -= value * i
+        }
+        j++
+    }
+    return result.toList()
 }
