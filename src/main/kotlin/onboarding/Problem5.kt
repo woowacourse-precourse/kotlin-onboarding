1a6dcdbf7356a58b5abb63f431a261e84647249f
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-01 19:14:55 +0900

    22.11.01 problem5 기능목록 및 기능 1번 구현

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,15 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val money = money
+
+    if(isCorrect5(money)) {
+
+    }
+    else return listOf(-1)
+}
+
+fun isCorrect5(money: Int): Boolean {
+    val condition1 = money in 1..1000000
+    return condition1
 }
