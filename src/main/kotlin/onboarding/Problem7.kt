5f45822190a9827dd6ab5cae665a232ffb6be6c9
leeeha <jxlhe46@seoultech.ac.kr>
2022-11-01 17:27:27 +0900

    refactor: Problem7 중복되는 부분을 updateScore 함수로 모듈화

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,16 +1,8 @@
 package onboarding
 
-fun findIndex( // Pair의 second를 기준으로 검색
-    list: MutableList<Pair<Int, String>>,
-    item: String
-): Int {
-    for (i in list.indices) {
-        if (list[i].second == item) {
-            return i
-        }
-    }
-    return -1
-}
+const val INDIRECT_FRIEND_SCORE = 10
+const val VISITOR_SCORE = 1
+
 fun solution7(
     user: String,
     friends: List<List<String>>,
@@ -18,7 +10,7 @@ fun solution7(
 ): List<String> {
     // user와 직접적으로 연결된 direct 친구 목록 생성
     val direct = mutableListOf<Pair<Int, String>>() // index, name
-    for(i in friends.indices){
+    for(i in friends.indices){ // 1만 이하
         if(friends[i][0] == user){
             direct.add(Pair(i, friends[i][1]))
         }else if(friends[i][1] == user){
@@ -28,49 +20,25 @@ fun solution7(
 
     // friends 관계를 통해 점수 측정
     val indirect = mutableListOf<Pair<Int, String>>() // score, name
-    for(i in friends.indices){
-        for(j in direct.indices){
+    for(i in friends.indices){ // 1만 이하
+        for(j in direct.indices){ // 1만 이하
             // direct에 해당하는 친구는 추천에서 제외
             if(i == direct[j].first) continue
 
             // direct와 연결된 친구는 +10점
             if(direct[j].second == friends[i][0]){
-                val name = friends[i][1] // direct의 친구 이름
-                val idx = findIndex(indirect, name)
-                if(idx == -1){ // indirect에 없는 이름인 경우
-                    indirect.add(Pair(10, name)) // 새로 추가
-                }else{ // 이미 한번 추가된 적이 있는 경우
-                    val oldPair = indirect[idx]
-                    val newPair = oldPair.copy(first = oldPair.first + 10) // 점수 갱신
-                    indirect[idx] = newPair
-                }
+                updateScore(indirect, friends[i][1], INDIRECT_FRIEND_SCORE)
             }else if(direct[j].second == friends[i][1]){
-                val name = friends[i][0]
-                val idx = findIndex(indirect, name)
-                if(idx == -1){
-                    indirect.add(Pair(10, name))
-                }else{
-                    val oldPair = indirect[idx]
-                    val newPair = oldPair.copy(first = oldPair.first + 10)
-                    indirect[idx] = newPair
-                }
+                updateScore(indirect, friends[i][0], INDIRECT_FRIEND_SCORE)
             }
         }
     }
 
     // visitors 목록에서 점수 측정
-    for(i in visitors.indices){
-        val name = visitors[i]
+    for(i in visitors.indices){ // 1만 이하
         // direct에 해당하지 않는 사람들만 점수 측정
-        if(findIndex(direct, name) == -1){
-            val idx = findIndex(indirect, name)
-            if(idx == -1){ // indirect에 없는 이름인 경우
-                indirect.add(Pair(1, name)) // 새로 추가
-            }else{ // 이미 추가된 적이 있는 경우
-                val oldPair = indirect[idx]
-                val newPair = oldPair.copy(first = oldPair.first + 1) // 점수 갱신
-                indirect[idx] = newPair
-            }
+        if(findIndex(direct, visitors[i]) == -1){
+            updateScore(indirect, visitors[i], VISITOR_SCORE)
         }
     }
 
@@ -89,12 +57,38 @@ fun solution7(
         var cnt = 0
         for(item in sortedList){
             if(cnt >= 5)  break // 이미 5개 추가했으면 끝!!
-            else{
-                result.add(item.second)
-                cnt++
-            }
+            result.add(item.second)
+            cnt++
         }
     }
 
     return result
 }
+
+fun findIndex( // Pair의 second를 기준으로 검색
+    list: MutableList<Pair<Int, String>>,
+    item: String
+): Int {
+    for (i in list.indices) {
+        if (list[i].second == item) {
+            return i
+        }
+    }
+    return -1
+}
+
+fun updateScore(
+    indirect: MutableList<Pair<Int, String>>,
+    name: String,
+    score: Int
+) {
+    val idx = findIndex(indirect, name)
+    if(idx == -1){ // indirect에 없는 이름인 경우
+        indirect.add(Pair(score, name)) // 새로 추가
+    }else{ // 이미 한번 추가된 적이 있는 경우
+        val oldPair = indirect[idx]
+        val newPair = oldPair.copy(first = oldPair.first + score) // 점수 갱신
+        indirect[idx] = newPair
+    }
+}
+
