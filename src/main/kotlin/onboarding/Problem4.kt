734e9ed6a74a62fc7f8fe4f3d0330e4447d528e6
Jisu Moon <barcode_no_1@naver.com>
2022-11-01 22:55:18 +0900

    Problem4 대, 소문자 구별 기능

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,7 @@
 package onboarding
 
-fun solution4(word: String): String {
-    TODO("프로그램 구현")
+/* 소문자, 대문자 구별하는 기능 */
+fun divideCase(c: Char): Int {
+    if (c.code >= 97 && c.code <= 122) return 1 //소문자
+    return if (c.code >= 65 && c.code <= 90) 2 else 0 //대문자
 }
