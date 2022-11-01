d91409d3c96b857e829ec52ef5883476815e7281
Jisu Moon <barcode_no_1@naver.com>
2022-11-01 18:02:03 +0900

    Problem1 솔루션 함수

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+/* 게임결과값 도출함수 */
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1) return -1 //요구사항 예외설정
+
+    val pobiScore = Math.max(sumMax(pobi), multiplyMax(pobi))
+    val crongScore = Math.max(sumMax(crong), multiplyMax(crong))
+
+    if (pobiScore > crongScore) return 1
+    if (pobiScore < crongScore) return 2
+    if (pobiScore == crongScore) return 0
+    return -1
+    //println("포비:$pobiScore 점, 크롱:$crongScore 점. 게임결과(0:무승부 1:포비승 2:크롱승) =>$answer\n")
 }
