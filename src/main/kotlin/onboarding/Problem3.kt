cc0fc1c5e5615385536da375972e8096c684d45d
Jihee.Han <42907876+jihee-dev@users.noreply.github.com>
2022-11-01 22:53:14 +0900

    feat: solve problem 3

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,15 @@
 package onboarding
 
+fun main() {
+    println(solution3(13))
+    println(solution3(33))
+}
+
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var count = 0
+    for (i in 1..number) {
+        val nums = i.toString().toCharArray().map { it.digitToInt() }
+        count += nums.count { it == 3 || it == 6 || it == 9 }
+    }
+    return count
 }
