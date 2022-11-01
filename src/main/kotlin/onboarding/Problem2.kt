67ca3ba6fb51e3d2da00f99d9793989668e6eb16
Jisu Moon <barcode_no_1@naver.com>
2022-11-01 21:11:13 +0900

    Problem2 솔루션 함수

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,12 @@
 package onboarding
 
+/* 2번 솔루션 로직 */
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var answer = "answer"
+    if (cryptogram.length < 1 || cryptogram.length > 1000 || !smallAlphabetCheck(cryptogram)) return "Cryptogram must be 1~1000 lowercase letters" //요구사항 설정된 예외처리
+    val temp = deleteDuplicatedChar(cryptogram)
+    if (cryptoCheck(listToString(temp), cryptogram) || temp.size <= 1) return listToString(temp)
+    answer = solution2(listToString(temp))
+    return answer
 }
+
