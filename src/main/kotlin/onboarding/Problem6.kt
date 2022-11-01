7ec794d3834c0f95fcbbe568203505585adb34b5
sososochar <dlwps12345@naver.com>
2022-11-01 21:05:41 +0900

    problem2 sol

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,25 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    var a = arrayListOf<String>()
+    var c = arrayListOf<String>()
+    var d = arrayListOf<Int>(forms.size - 1,0)
+    var k = 0
+    for (i in forms){
+        for (j in 0..i[1].length - 1){
+            var b = i[1].substring(j, j + 1)
+            if(b in a){
+                if (i == forms.last()) c.add(i[0])
+                else continue
+            }
+            a.add(b)
+        }
+        d[k] = a.size
+        k++
+    }
+    for (i in 0..d.size){
+        if (d[i] != 0) c.add(forms[i][0])
+    }
+    c.sort()
+    return c
 }
