9eafc70ca3974899f2a94846f6a35b6fb1f5ea72
최재훈 <be_clever@naver.com>
2022-11-01 19:32:31 +0900

    feat(Problem6): (키, (이메일 셋))의 맵에 반환된 문자열을 삽입하는 기능 작성

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,13 +1,30 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val history = initHistory(forms)
+}
+
+fun initHistory(forms: List<List<String>>): MutableMap<String, MutableSet<String>> {
+    val history: MutableMap<String, MutableSet<String>> = mutableMapOf()
+
+    for (form in forms) {
+        val email = form[0]
+        val nickname = form[1]
+
+        val splittedResult = splitByTwo(nickname)
+
+        for (result in splittedResult) {
+            history[result]?.add(email)
+        }
+    }
+
+    return history
 }
 
 fun splitByTwo(nickname: String): MutableList<String> {
     val result = mutableListOf<String>()
 
-    for(i in 0 until nickname.length - 1) {
+    for (i in 0 until nickname.length - 1) {
         result.add(nickname.substring(i, i + 1))
     }
 
