906642623ff6fd760236f821661deb7c3d449e09
lkeonwoo94 <lkeonw0o@naver.com>
2022-11-01 22:13:28 +0900

    Prob6 - Add Funtional Requirement List

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,3 +3,6 @@ package onboarding
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
+/* 1. Calculate duplicates when two or more than consecutive elements are the same */
+/* 2. Sort asc*/
