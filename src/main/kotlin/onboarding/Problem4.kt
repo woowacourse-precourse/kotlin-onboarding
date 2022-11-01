4e810c05ba5ad7790b75c95b01e359ab4ed3861a
joy <6238jcm@gmail.com>
2022-11-01 23:18:48 +0900

    미완성 추후 공부 더 필요

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -2,4 +2,22 @@ package onboarding
 
 fun solution4(word: String): String {
     TODO("프로그램 구현")
+    return alphaConverse(word)
+}
+
+fun alphaConverse(word: String):String {
+    var stringList = word.chunked(1).toMutableList()
+    for (i : Int in 0..word.length-1){
+        if (stringList[i] != " "){
+            var char = stringList[i].single()
+            if(char.code in 65..90){
+                var ascii = (90 - (char.code - 65)).toChar()
+                stringList[i] = ascii.toString()
+            }else if(char.code in 97..122){
+                var ascii = (122-(char.code-97)).toChar()
+                stringList[i] = ascii.toString()
+            }
+        }
+    }
+    return stringList.joinToString("")
 }
