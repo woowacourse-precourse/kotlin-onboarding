5c57711527da35392887d31552c111ac670bab9d
noion0511 <noion0511@gmail.com>
2022-11-01 18:44:46 +0900

    [problem 1][feat] 점수의 최댓값을 찾는 계산 기능

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,25 @@
 package onboarding
 
-fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+fun solution1(pobi: List<Int>, crong: List<Int>):Int {
+    return 0
+}
+
+fun findMaxValue(list: List<Int>) : Int {
+    val values = arrayListOf<Int>()
+    var multi = 1
+
+    for (i in list) {
+        val digits = i.toString().map {
+            it.code - '0'.code
+        }
+        digits.forEach { multi *= it }
+        values.add(digits.sum())
+        values.add(multi)
+    }
+
+    if (values.maxOrNull() == null) {
+        return 0
+    }
+
+    return values.maxOrNull()!!
 }
