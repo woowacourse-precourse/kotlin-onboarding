2c204b4849eb6a4701502a1a01f84cf97254712f
윤동현 <donghyunyoon81@gmail.com>
2022-11-01 19:40:37 +0900

    refactor: RemoveConsecutiveStrings in solution2 cryptogram의 연속된 글자를 제거하는 기능을 만들었습니다.

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,24 +1,29 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
+
+    return RemoveConsecutiveStrings(cryptogram)
+}
+
+private fun RemoveConsecutiveStrings(cryptogram: String):String {
     var decode = cryptogram
     var loop = true
-    while (loop){
-        var count =1
-        var minusCount= 1
+    while (loop) {
+        var count = 1
+        var minusCount = 1
         for (i in decode.indices) {
-            if(i == decode.length-1)  loop = false
-            if(decode.toList().distinct().size==1 && decode.length!=1){
-                decode=""
+            if (i == decode.length - 1) loop = false
+            if (decode.toList().distinct().size == 1 && decode.length != 1) {
+                decode = ""
                 loop = false
                 break
             }
             if (i + 1 < decode.length) {
-                if (decode[i] == decode[i+1]) {
+                if (decode[i] == decode[i + 1]) {
                     count++
                     minusCount--
-                    if(decode[i+minusCount]!=decode[i+count+minusCount]) {
-                        decode=decode.removeRange(i+minusCount,i+count+minusCount)
+                    if (decode[i + minusCount] != decode[i + count + minusCount]) {
+                        decode = decode.removeRange(i + minusCount, i + count + minusCount)
                         break
                     }
                 }
@@ -27,3 +32,5 @@ fun solution2(cryptogram: String): String {
     }
     return decode
 }
+
+
