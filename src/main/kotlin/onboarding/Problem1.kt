02b26e6db2ff7864e4e07f8305041c1422b39558
JinJoa <mitejoa@gmail.com>
2022-11-01 19:04:27 +0900

    문제1 - pobi, crong list 변환
    페이지 자리수별로 계산할 수 있게 쪼갬.

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,17 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    var pobi_max :Int = 0
+    var crong_max : Int = 0
+
+    for (j in 0..1){ // 제한사항 : 리스트 크기는 2
+        var pobi_page = pobi[j].toString() //페이지 리스트 문자열로 치환
+
+        //각 자리 수 더하기 & 곱하기
+        for(i in 0..pobi_page.length-1){
+            var n = pobi_page[i]-'0' //각 자리수 다시 숫자로 치환; 아스키코드;
+
+        }
+    }
+
 }
