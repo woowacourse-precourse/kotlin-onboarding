3c6c673404fad36246bbdfa3302113e7ba5ee133
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-01 18:19:21 +0900

    주석 추가 및 Problem4 완성

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,9 +1,14 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var result:String = ""
+    for(c in word){
+        result += convertAlpha(c)
+    }
+    return result
 }
 
+/* 변환된 알파벳을 출력하는 convertAlpha 메소드 구현 */
 fun convertAlpha(c: Char): Char{
     if(c in 'A'..'Z'){
         return ('Z' - (c - 'A')).toChar()
