1c1525134f05031a8d7a557e86bcb7d9f36791c5
JinJoa <mitejoa@gmail.com>
2022-11-01 19:23:00 +0900

    문제3 - 숫자 문자열로 분리

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,12 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    //369게임
+    var clap_cnt :Int = 0 //박수 카운터
+
+    for (i in 1..number){
+        //숫자 문자열로 분리
+        val num_to_str = i.toString()
+        
+    return clap_cnt
 }
