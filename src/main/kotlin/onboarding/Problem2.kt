4fdb14aab5ca96ec1252ad33809db75a9d781ebb
lee989898 <@@qwer1352>
2022-11-01 21:50:29 +0900

    [refactor](Problem2) isAppropriateCryptogram 함수 if문 when으로 변경

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -29,11 +29,8 @@ fun solution2(cryptogram: String): String {
     return if (cryptogramList.isEmpty()) "" else cryptogramList.joinToString("")
 }
 
-fun isAppropriateCryptogram(cryptogram: String): String {
-    if (cryptogram.length !in 1..1000) {
-        return "cryptogram 길이가 1이상 1000 이하인지 확인해주세요."
-    } else if (cryptogram.count { it !in 'a'..'z' } > 0) {
-        return "cryptogram에 알파벳 소문자 이외것이 있는지 확인해주세요."
-    }
-    return ""
+fun isAppropriateCryptogram(cryptogram: String) = when {
+    cryptogram.length !in 1..1000 -> "cryptogram 길이가 1이상 1000 이하인지 확인해주세요."
+    cryptogram.count { it !in 'a'..'z' } > 0 -> "cryptogram에 알파벳 소문자 이외것이 있는지 확인해주세요."
+    else -> ""
 }
