a090d68a8df6b44c6aee4056bf9e076614c23134
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-11-01 23:35:29 +0900

    refactor: (문제4) 코드 리팩토링

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,28 +1,20 @@
 package onboarding
 
-import java.lang.StringBuilder
-
 fun solution4(word: String): String {
-    return "${convertWord(word)}"
+    return convertWord(word)
 }
 
-fun convertWord(word: String): StringBuilder{
-
-    val newWord = StringBuilder(word)
-
-    val azList: List<Char> =
-        listOf('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-    val zaList: List<Char> =
-        listOf('z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a',
-        'Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K','J','I','H','G','F','E','D','C','B','A')
-
-    for(i in word.indices){
-        val index = azList.indexOf(word[i])
-        if(index == -1){
-            continue
-        }
-        newWord[i] = zaList[index]
+fun convertWord(word : String) : String {
+    var newWord = ""
+    word.forEach {
+        newWord += convertChar(it)
     }
-    return  newWord
+    return newWord
 }
+
+fun convertChar(char: Char) : Char = when(char) {
+    in 'A'..'Z' -> ('A'.code + 'Z'.code - char.code).toChar()
+    in 'a'..'z' -> ('a'.code + 'z'.code - char.code).toChar()
+    else -> char
+}
+
