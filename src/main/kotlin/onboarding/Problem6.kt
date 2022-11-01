6f1d85d9329f34a25f6d6c44a265a9f4842b0ea8
lh99j <grand2181@gmail.com>
2022-11-01 13:17:31 +0900

    Solve Problem 6

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,25 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    var answer : MutableList<String> = mutableListOf<String>()
+
+    for(i in 0 until forms.size - 1){
+        for(j in i + 1 until forms.size){
+            if(checkNickName(forms[i][1], forms[j][1])) {
+                answer.add(forms[i][0])
+                answer.add(forms[j][0])
+            }
+        }
+    }
+
+    return answer.distinct().sorted()
+
+}
+fun checkNickName(user1 : String, user2 : String) : Boolean{
+    for(i in 0 until user1.length - 1){
+        if(user2.contains(user1.substring(i, i + 2)))
+            return true
+    }
+
+    return false
 }
