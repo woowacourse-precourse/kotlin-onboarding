f8faab4ddf646dba0a20956991fb3b95044b3ff8
choHarmony <jws0183@naver.com>
2022-11-01 22:37:02 +0900

    [문제 4] word 속 문자가 대문자일 경우 암호화

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,18 @@
 package onboarding
 
+import java.lang.StringBuilder
+
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    val upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val lower = "abcdefghijklmnopqrstuvwxyz"
+    var code = StringBuilder() // 동적으로 문자열을 조절할 수 있는 빈 객체
+
+    for (i in word) {
+        if (i in upper) {
+            val i_upper_index = upper.indexOf(i)
+            code.append(upper[25-i_upper_index])
+        }
+    }
+
+    return code.toString()
 }
