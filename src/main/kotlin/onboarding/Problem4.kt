ce3e1d744cdfa978a3ae8809f58965c144bae6f9
Jisu Moon <barcode_no_1@naver.com>
2022-11-01 22:56:07 +0900

    Problem4 문제 해결

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,24 @@
 package onboarding
 
+/* 문제 해결 */
+fun solution4(word: String): String {
+    var answer = ""
+    val ch = word.toCharArray()
+    for (i in ch.indices) {
+        val a = divideCase(ch[i])
+        if (a == 1) {
+            answer = answer + (219 - ch[i].code).toChar()
+            continue
+        }
+        if (a == 2) {
+            answer = answer + (155 - ch[i].code).toChar()
+            continue
+        }
+        answer += ch[i]
+    }
+    return answer
+}
+
 /* 소문자, 대문자 구별하는 기능 */
 fun divideCase(c: Char): Int {
     if (c.code >= 97 && c.code <= 122) return 1 //소문자
