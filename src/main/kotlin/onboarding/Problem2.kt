f964a638c6cc31bf9a99eeed2bd91750150121c6
2taezeat <xorudfl772@naver.com>
2022-11-01 11:28:59 +0900

    feat : 0. 연속하는 중복 문자의 인덱스를 찾는 함수

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -3,3 +3,18 @@ package onboarding
 fun solution2(cryptogram: String): String {
     TODO("프로그램 구현")
 }
+
+
+fun findDuplicateIndex(inputString : String) : ArrayList<Int> {
+    var duplicatedCharIndex = arrayListOf<Int>()
+
+    for ( i in 0 until inputString.lastIndex) {
+        println(i)
+        if (inputString[i] == inputString[i+1])  {
+            duplicatedCharIndex.add(i)
+        }
+    }
+
+    return duplicatedCharIndex
+
+}
