0041b1cc3f3911d0759f5043f7536ac7f86866c3
2taezeat <xorudfl772@naver.com>
2022-11-01 12:48:13 +0900

    feat : 0. number 값까지 손뼉을 쳐야 할 갯수를 배열에 1부터 계산하여 리턴하는 함수 작성

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -3,3 +3,16 @@ package onboarding
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
+
+
+fun calculate369(number : Int) : ArrayList<Int> {
+    val resultArray = arrayListOf<Int>(0)
+    for (i in 1 ..number) {
+        var count = 0
+        for (n in i.toString()){
+            if (n == '3' || n == '6' || n == '9') count += 1
+        }
+        resultArray.add(resultArray[i-1] + count)
+    }
+    return resultArray
+}
