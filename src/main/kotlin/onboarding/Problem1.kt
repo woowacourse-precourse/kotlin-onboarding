228a2135f019df734363174feb36e3a700ef2bed
joh9911 <joh9911@naver.com>
2022-11-01 19:26:58 +0900

    problem1: 예외처리 함수

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -3,3 +3,21 @@ package onboarding
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
+
+/**
+ * 예외 체크를 해주는 함수이다. 예외에 해당되면 false를 리턴한다
+ */
+fun exceptionCheck(pobi: List<Int>, crong: List<Int>): Boolean{ //
+    if (pobi[0] % 2 == 0 || crong[0] % 2 == 0){ // 왼쪽 페이지 번호가 짝수일 때
+        return false
+    }
+    else if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1){ // 왼쪽 오른쪽 페이지 번호가 1이 넘게 차이가 날 때
+        return false
+    }
+    else if (pobi[0] >= pobi[1] || crong[0] >= crong[1]){ // 왼쪽 페이지 번호가 오른쪽 번호보다 크거나 같을 때
+        return false
+    }
+    else return !(pobi.size > 2 || crong.size > 2) // 베열 길이가 2보다 클 때
+    // 이 각각을 제외하고 true를 리턴한다.
+}
+
