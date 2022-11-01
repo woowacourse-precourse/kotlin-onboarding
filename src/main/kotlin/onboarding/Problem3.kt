77902ef87e2735d2a5e88af04af5ca5173177427
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-01 22:00:13 +0900

    3번 문제 테스트: 오류 수정

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -2,16 +2,19 @@ package onboarding
 
 fun solution3(number: Int): Int {
 
-    var clapCount: Int = 0
+    var clapCount = 0
 
-    for (i in 1..number) {
-        var currentNumber: Int = i
+    for(i in 1..number) {
+        var currentNumber = i
+        while(currentNumber != 0) {
         val meetCriteriaClap = currentNumber % 10 == 3 || currentNumber % 10 == 6 || currentNumber % 10 == 9
 
-        while (currentNumber != 0) {
-            if (meetCriteriaClap) clapCount++
+            if(meetCriteriaClap) {
+                clapCount++
+            }
             currentNumber /= 10
         }
     }
+
     return clapCount
 }
