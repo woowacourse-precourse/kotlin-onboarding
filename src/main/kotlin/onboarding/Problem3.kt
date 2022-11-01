d85e32b3ac7fb664dc3a84e13db342e05bcd89e8
meltnia <72488905+meltnia@users.noreply.github.com>
2022-11-01 23:06:30 +0900

    Add files via upload

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -11,19 +11,18 @@ fun solution3(number: Int): Int {
         scan=0
         var a=(i%10000)/1000//천의 자리수 추출
         if(a!=0 && a%3==0)//3 6 9 확인
-            scan=1
+            scan+=1
         a=(i%1000)/100//백의 자리수 추출
-        if(a!=0 && a%3==0 && scan!=1)
-            scan=1
+        if(a!=0 && a%3==0)
+            scan+=1
         a=(i%100)/10//십의 자리수
-        if(a!=0 && a%3==0 && scan!=1)
-            scan=1
+        if(a!=0 && a%3==0)
+            scan+=1
         a=i%10//일의 자리수
-        if(a!=0 && a%3==0 && scan!=1)
-            scan=1
+        if(a!=0 && a%3==0)
+            scan+=1
         
-        if(scan==1)//법칙에 적합하면 check증가
-            check++
+        check+=scan
     }
 
     return check
