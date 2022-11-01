67e1a976bff50ef7e14b83cb353a1f96c3a7fed4
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-01 17:48:58 +0900

    필요한 박수의 숫자를 리턴하는 clap 메소드 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -4,6 +4,19 @@ fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
 
+fun clap(num: Int): Int {
+    var num = num
+    var ans: Int = 0
+    var temp: Int = 0
+
+    while(num > 0){
+        temp = num % 10
+        ans += is369(temp)
+        num /= 10
+    }
+    return ans
+}
+
 fun is369(num: Int): Int{
     if(num % 3 == 0 && num != 0){
         return 1
