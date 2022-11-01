20dd14d1335037618d9e8e887bfc35ba8d1dce5f
윤동현 <donghyunyoon81@gmail.com>
2022-11-01 21:03:59 +0900

    refactor: Change getDuplicateNicknameList to findDuplicatesInList 기능을 더 범용성있게 사용 할 수 있도록 기능 이림과 변수이름을 수정했습니다.

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -2,30 +2,30 @@ package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
     val separateNicknameList = getSeparateNicknameList(forms)
-    val duplicateNicknameList = getDuplicateNicknameList(separateNicknameList)
+    val duplicateNicknameList = findDuplicatesInList(separateNicknameList)
     val duplicateNicknameEmailList = mutableListOf<String>()
-
     return duplicateNicknameEmailList.distinct().sorted()
 }
 
 private fun getSeparateNicknameList(forms: List<List<String>>):MutableList<String> {
-    val separateNicknameList = mutableListOf<String>()
+    val list = mutableListOf<String>()
     for (i in forms.indices) {
         for(j in forms[i][1].indices) {
             if (j + 1 == forms[i][1].length) break
-            separateNicknameList.add("${forms[i][1][j]}${forms[i][1][j + 1]}")
+            list.add("${forms[i][1][j]}${forms[i][1][j + 1]}")
         }
     }
-    return separateNicknameList
+    return list
 }
-private fun getDuplicateNicknameList(
-    separateNicknameList:MutableList<String>
+private fun findDuplicatesInList (
+    list:List<String>
 ):MutableSet<String> {
-    val duplicateNicknameList = mutableSetOf<String>()
-    for(i in separateNicknameList.indices) {
-        if(separateNicknameList.indexOf(separateNicknameList[i]) !=separateNicknameList.lastIndexOf(separateNicknameList[i]))
-            duplicateNicknameList.add(separateNicknameList[i])
+    var returnList:MutableSet<String> = mutableSetOf<String>()
+    for(i in list.indices) {
+        if(list.indexOf(list[i]) !=list.lastIndexOf(list[i])) returnList.add(list[i])
     }
-    return duplicateNicknameList
+    return returnList
 }
+
+
 
