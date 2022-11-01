dba0b52403fd56e7626114b844cb83d7ea8de4d8
s9hn <tnghqn@naver.com>
2022-11-01 19:29:03 +0900

    [REFACTOR(미션5)] 코드 정리 및 리드미 기능목록 수정
    1. 주석 추가
    2. 리드미 기능목록 재수정

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -5,7 +5,7 @@ fun solution5(money: Int): List<Int> {
     return splitUnit(money)
 }
 
-fun splitUnit(money: Int): MutableList<Int> {
+fun splitUnit(money: Int): MutableList<Int> { // 화폐 단위로 나누기 반복, 리스트 저장 후 리턴
     var myMoney = money
     val wallet: MutableList<Int> = mutableListOf()
     val currency = mutableListOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
