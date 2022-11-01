622446c24fe68b6c4a448dc6e1ef0bffa76e1312
ghwlsdl <hojinlee93@naver.com>
2022-11-01 17:00:15 +0900

    Problem 3, 4, 5, 6, 7 Sol

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,20 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+
+    return exchangeCoin(money)
+}
+
+fun exchangeCoin(money: Int): List<Int> {
+    var money: Int = money
+    var wallet = mutableListOf<Int>(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    val coin: List<Int> = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+
+    for (i in coin.indices) {
+        if (money >= coin[i]) {
+            wallet[i] = money / coin[i]
+            money %= coin[i]
+        }
+    }
+    return wallet
 }
