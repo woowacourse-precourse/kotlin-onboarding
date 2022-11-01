c005438fcdc1e2e099b71910107248c736f64b01
noion0511 <noion0511@gmail.com>
2022-11-01 18:46:29 +0900

    [problem 1][feat] 게임이 성사되지 않는 예외사항 분류 기능

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,6 +1,10 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>):Int {
+    if(pobi[1]-pobi[0] != 1 || crong[1]-crong[0] != 1){
+        return -1
+    }
+
     val pobiMax = findMaxValue(pobi)
     val crongMax = findMaxValue(crong)
 
