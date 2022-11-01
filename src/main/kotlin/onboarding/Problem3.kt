963a28bd6e5c7f5da31a9d012198bae3589ae471
Yujin <jini1514@soongsil.ac.kr>
2022-11-01 21:18:04 +0900

    Add : Problem3.kt

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,9 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var clapCount: Int = 0
+    for(i in 1..number){
+        clapCount += i.toString().toList().count{ it in listOf('3', '6', '9')}
+    }
+    return clapCount
 }
