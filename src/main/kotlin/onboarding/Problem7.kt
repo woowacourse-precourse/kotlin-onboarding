adec9f23503b39de5492476746c8b9e7a092017d
s9hn <tnghqn@naver.com>
2022-11-01 16:55:45 +0900

    [FEAT(미션7)] 내 친구찾기 구현 && 기능 목록 체크

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,26 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val myFriends = mutableListOf<List<String>>()
+    val myFriend = mutableListOf<String>()
+    var resultScore = mutableMapOf<String, Int>()
+
+
+
+    for (i in friends) {   // 내 친구찾기
+        if (i.contains(user)) {
+
+            myFriends.add(i)
+        }
+    }
+
+    for (j in 0 until myFriends.size) { // 내 친구 찾기2
+        if (user in myFriends[j]) {
+
+            myFriend.add((myFriends[j] - user)[0])
+
+        }
+    }
+    //  println(myFriend)
+
 }
