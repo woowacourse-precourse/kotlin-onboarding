9a57d74c041c99c97ef3f0b85f1fdd796b0caf28
Jihyo-Android <wlgy33@naver.com>
2022-11-01 15:41:37 +0900

    feat: make new multiplyAll method
    plusAll method multiply parameter's each digit

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -17,3 +17,15 @@ fun plusAll(n: Int): Int {
 
     return result
 }
+
+fun multiplyAll(n: Int): Int {
+    var num: Int = n
+    var result: Int = 0
+
+    while (num != 0) {
+        result *= num % 10
+        num /= 10
+    }
+
+    return result
+}
