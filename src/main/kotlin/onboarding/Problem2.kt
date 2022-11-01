6ef68d76541055081dd485f4e8ab0660df83dae4
Jisu Moon <barcode_no_1@naver.com>
2022-11-01 21:20:37 +0900

    중복문자가 처리된 문자열의 cryptogram 확인 기능

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,13 +1,11 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    val answer = "answer"
+    var answer = "answer"
     return answer
 }
 
-/* 문자가 담긴 list를 String형태로 변환 기능 */
-fun listToString(temp: List<Char>): String {
-    val str = StringBuilder()
-    for (cryptoChar in temp) str.append(cryptoChar)
-    return str.toString()
+/* 중복문자가 처리된 문자열의 cryptogram 확인 기능 */
+fun cryptoCheck(duplCleanedStr: String, cryptogram: String): Boolean {
+    return duplCleanedStr == cryptogram
 }
