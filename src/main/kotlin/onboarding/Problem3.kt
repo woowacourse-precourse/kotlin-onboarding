c653091b0b955c31c736bc751ceab570cfdcec16
lh99j <grand2181@gmail.com>
2022-11-01 12:10:16 +0900

    Refactoring Problem 3

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,37 +1,24 @@
 package onboarding
 
-import kotlin.math.sign
-
 fun solution3(number: Int): Int {
     var answer : Int = 0
 
     for(i in 1 .. number){
-        val temp : Int= i
-        answer += isMatchNum(temp)
+        answer += isMatchNum(i)
     }
 
-    var a = 'A'
-    var b = 'Z'
-
-    println(a.code)
-    println(b.code)
     return answer
 }
-
 fun isMatchNum(num : Int) : Int{
     var count : Int = 0
     var tempNum = num
-    var array : ArrayList<Int> = ArrayList<Int>()
 
     while(tempNum != 0){
-        array.add(tempNum % 10)
+        val number : Int = tempNum % 10
+        if(number == 3 || number == 6 || number == 9)
+            count++
 
         tempNum /= 10
-    }
-
-    for(i in 0 until array.size){
-        if(array[i] == 3 || array[i] == 6 || array[i] == 9)
-            count++
     }
 
     return count
