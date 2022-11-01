ab7790cfed6426783faf46cf86bdcddcb7c0c2ee
sososochar <dlwps12345@naver.com>
2022-11-01 21:05:33 +0900

    problem2 sol

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,15 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var a = money
+    var arr = arrayListOf<Int>(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    var arr1 = arrayListOf<Int>(0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+    for (i in 0..8){
+        while (a >= arr[i]){
+            a -= arr[i]
+            arr1[i]++
+        }
+    }
+    return arr1
 }
