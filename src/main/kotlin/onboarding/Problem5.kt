588ea3fd4786cbab593f6f1d3f65fdaa405656ea
sjh970603 <sjh970603@naver.com>
2022-11-01 15:25:20 +0900

    problem 5번
    if문 활용해서 result값에 개수 저장

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,55 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    //TODO("프로그램 구현")
+
+    val result = mutableListOf<Int>(0,0,0,0,0,0,0,0,0)
+    var money1 = money
+
+
+    if (money1 >= 50000)
+    {
+        result[0] = money1/50000
+        money1 = money1%50000
+    }
+
+    if (money1 >= 10000)
+    {
+        result[1] = money1/10000
+        money1 = money1%10000
+    }
+    if (money1 >= 5000)
+    {
+        result[2] = money1/5000
+        money1 = money1%5000
+    }
+    if (money1 >= 1000)
+    {
+        result[3] = money1/1000
+        money1 = money1%1000
+    }
+    if (money1 >= 500)
+    {
+        result[4] = money1/500
+        money1 = money1%500
+    }
+    if (money1 >= 100)
+    {
+        result[5] = money1/100
+        money1 = money1%100
+    }
+    if (money1 >= 50)
+    {
+        result[6] = 1/50
+        money1 = money1%50
+    }
+    if (money1 >= 10)
+    {
+        result[7] = money1/10
+        money1 = money1%10
+        result[8] = money1
+    }
+
+    return result
+
 }
