80e1bdabae2ad23feb8bfc895ca852bd91397140
kjyook <106497893+kjyook@users.noreply.github.com>
2022-11-01 20:39:23 +0900

    문제 7번 친구들을 기록해둘 temp에 중복없이 친구들을 입력함

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,16 +1,36 @@
 package onboarding
 
-class friends{
+class friend(str : String){
     var check = 0       //user와 친구인지 확인해주는 변수
     var point = 0       //추천 점수를 기록하는 변수
+    val name = str
 }
 fun solution7(
     user: String,
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    var temp = mutableListOf<friends>()
+
+    var temp = mutableListOf<friend>()
     var answer = mutableListOf<String>()
 
+    for(p in friends){
+        for(i in 0 .. 1){
+            var idx = 0
+            var count = 0
+
+            for(t in temp){
+                if(t.name != p[i]){
+                    count++
+                }
+            }
+
+            if( count == temp.size ){
+                temp.add(friend(p[i]))
+                idx++
+            }
+
+        }
+    }
     return answer
 }
