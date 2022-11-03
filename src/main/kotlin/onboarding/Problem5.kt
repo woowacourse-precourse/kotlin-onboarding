024da1645fb57cfe3fb8962841691ed3f16dcf37
CHOSEONGROK <csr8367@hanmail.net>
2022-11-03 17:04:16 +0900

    Style: 확장 함수 mapWithFold() 정의

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,17 +1,21 @@
 package onboarding
 
-import java.util.*
-
 fun solution5(money: Int): List<Int> {
-    val answer = LinkedList<Int>()
-    val units = intArrayOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
-    var n = money
+    return listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+        .mapWithFold(money) { balance, unit ->
+            balance % unit to balance / unit
+        }
+}
 
-    for (unit in units) {
-        answer.add(n / unit)
+private inline fun <I, T, R> Iterable<T>.mapWithFold(initial: I, transform: (I, T) -> Pair<I, R>): List<R> {
+    var accumulator = initial
+    val destination = mutableListOf<R>()
 
-        n %= unit
+    for (item in this) {
+        val (acc2, item2) = transform(accumulator, item)
+        accumulator = acc2
+        destination.add(item2)
     }
 
-    return answer
+    return destination
 }
