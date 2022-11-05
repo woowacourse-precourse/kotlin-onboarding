a23a3340119492b93f93cb3941b3079d2b938d05
hoje <83208807+zini9188@users.noreply.github.com>
2022-11-06 01:47:20 +0900

    style: 변수명 변경

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,13 +1,13 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    val won = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    val moneys = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
     val result: ArrayList<Int> = arrayListOf()
     var temp = money
 
-    for (i in won) {
-        result.add(temp / i)
-        temp %= i
+    for (won in moneys) {
+        result.add(temp / won)
+        temp %= won
     }
 
     return result
