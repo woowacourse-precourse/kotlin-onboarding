1545b71bd75edc6ff1a8af1383a3ee5993dffb5f
윤동현 <donghyunyoon81@gmail.com>
2022-11-24 08:48:01 +0900

    feat<Problem1> : 숫자의 자리 값끼리 더하는 addDigits 기능 구현
    문자열로 변경하고 각 문자들을 다시 숫자로 변경해서 모두 더함
    각 자릿수를 더한 결과값을 반환 받기 위해서 구현 했습니다.

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,18 @@
 package onboarding
-
+//### 기능 목록 작성
+//
+//- 각 페이지 번호 더하기 (페이지)
+//- 각 페이지 번호 곱하기 (페이지)
+//- 값 비교하여 최대값 구하기 (더하기,곱하기)
+//- 점수를 비교해 결과 출력
+//- 예외 : 시작면이나 마지막면은 나오지 않는다
+//- 예외 : 책 페이지는 1 부터 400 까지이다.
+const val minPage = 1
+const val maxPage = 400
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     TODO("프로그램 구현")
 }
+
+fun addDigits(page: Int) = page.toString().toCharArray().sumOf { it.digitToInt() }
+
+
