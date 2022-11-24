ed7a545bcd1632d52f33ec000e971eb7274edfce
윤동현 <donghyunyoon81@gmail.com>
2022-11-24 23:00:27 +0900

    feat<Problem2AnotherCase> : 암호문의 연속된 글자가 중복인지 확인하는 기능

---
+++ src/main/kotlin/onboarding/Problem2AnotherCase.kt
@@ -0,0 +1,7 @@
+package onboarding
+
+fun solution2AnotherCase(cryptogram: String): String {
+    return ""
+}
+
+private fun isDuplicate(cryptogram: String, index: Int) = cryptogram[index] == cryptogram[index + 1]
