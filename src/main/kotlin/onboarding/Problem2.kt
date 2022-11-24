0541c92e6c25c47ba362dd64f6d4edb56b4d7280
윤동현 <donghyunyoon81@gmail.com>
2022-11-24 21:53:49 +0900

    feat<solution2> : 암호문을 해결한 값을 반환하는 solution2 기능 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -5,8 +5,8 @@ import net.bytebuddy.implementation.bytecode.collection.ArrayLength
 //- 문자 해독
 //- 문자 재해독
 //- 현 글자가 다음 글자랑 중복인지
-fun solution2(속: String): String {
-    TODO("프로그램 구현")
+fun solution2(cryptogram: String): String {
+    return repeatDecode(cryptogram)
 }
 
 private fun repeatDecode(cryptogram: String): String {
