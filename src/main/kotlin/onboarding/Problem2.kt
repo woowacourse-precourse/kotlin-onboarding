d2fe04591574dd67741ef4a7e8db193dee733959
윤동현 <donghyunyoon81@gmail.com>
2022-11-24 21:45:18 +0900

    feat<Problem2> : 연속된 문자를 제거하는 decode를 반복하는 repeatDecode 기능 구현
    연속된 문자를 제거중에 decode에서 반환받은 Pair에서 false를 받을시에 반복 종료
    연속된 문자를 제거하고 나머지 값들중에 다시 연속된 문자가 있을시 제거하기 위해 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -9,13 +9,24 @@ fun solution2(속: String): String {
     TODO("프로그램 구현")
 }
 
+private fun repeatDecode(cryptogram: String): String {
+    var loop = true
+    var decode = Pair(cryptogram, false)
+    while (loop) {
+        decode = decode(decode.first)
+        loop = decode.second
+    }
+    return decode.first
+}
+
 private fun decode(cryptogram: String): Pair<String, Boolean> {
     for (index in cryptogram.indices) {
-        if (hasDuplicate(index,cryptogram.length)) return Pair(cryptogram, false)
+        if (hasDuplicate(index, cryptogram.length)) return Pair(cryptogram, false)
         if (isDuplicate(cryptogram, index)) return Pair(cryptogram.removeRange(index, index + 2), true)
     }
     return Pair(cryptogram, false)
 }
+
 private fun isDuplicate(cryptogram: String, index: Int) = cryptogram[index] == cryptogram[index + 1]
 
 private fun hasDuplicate(index: Int, length: Int) = index != length - 1
