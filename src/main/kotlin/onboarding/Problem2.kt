d6d84606b53ddf916cd1973e789ee24ee98a769b
윤동현 <donghyunyoon81@gmail.com>
2022-11-24 17:00:58 +0900

    feat<Problem2> : 현 글자가 다음 글자랑 중복인지를 반환하는 isDuplicate 기능 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,6 +1,9 @@
 package onboarding
 //- 문자 해독
 //- 문자 재해독
+//- 현 글자가 다음 글자랑 중복인지
 fun solution2(cryptogram: String): String {
     TODO("프로그램 구현")
 }
+
+private fun isDuplicate(cryptogram: String, index: Int) = cryptogram[index] == cryptogram[index + 1]
