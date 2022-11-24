cdc41e281d8e3d8407239db08c2cd6cdff588a7c
윤동현 <donghyunyoon81@gmail.com>
2022-11-24 21:40:44 +0900

    feat<Problem2> : 암호문을 해독하는 decode 기능 구현
    for문을 사용하여 암호문의 모든 문자를 비교해서
    연속된 중복된 값이 있을경우 연속된 중복 문자를 제거합니다.

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -9,6 +9,13 @@ fun solution2(속: String): String {
     TODO("프로그램 구현")
 }
 
+private fun decode(cryptogram: String): Pair<String, Boolean> {
+    for (index in cryptogram.indices) {
+        if (hasDuplicate(index,cryptogram.length)) return Pair(cryptogram, false)
+        if (isDuplicate(cryptogram, index)) return Pair(cryptogram.removeRange(index, index + 2), true)
+    }
+    return Pair(cryptogram, false)
+}
 private fun isDuplicate(cryptogram: String, index: Int) = cryptogram[index] == cryptogram[index + 1]
 
 private fun hasDuplicate(index: Int, length: Int) = index != length - 1
