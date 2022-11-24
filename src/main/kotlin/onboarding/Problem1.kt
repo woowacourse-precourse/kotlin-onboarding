37e994477597daf14ff206a65df93ab39f840039
윤동현 <donghyunyoon81@gmail.com>
2022-11-24 12:33:30 +0900

    refactor<Problem1> : 하드코딩한 부분을 상수로 선언한 변수로 모두 수정

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -8,12 +8,16 @@ package onboarding
 //- 점수를 비교해 결과 출력
 //- 예외 : 시작면이나 마지막면은 나오지 않는다
 //- 예외 : 책 페이지는 1 부터 400 까지이다.
-const val minPage = 1
-const val maxPage = 400
+const val MIN_PAGE = 1
+const val MAX_PAGE = 400
+const val POBI_WIN_NUMBER = 1
+const val CRONG_WIN_NUMBER = 2
+const val DRAW_NUMBER = 0
+const val EXCEPTION_NUMBER = -1
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    require(isPagesRange(pobi) || isPagesRange(crong)) { return -1 }
-    require(isPages(pobi) && isPages(crong)) { return -1 }
+    require(isPagesRange(pobi) || isPagesRange(crong)) { return EXCEPTION_NUMBER }
+    require(isPages(pobi) && isPages(crong)) { return EXCEPTION_NUMBER }
     return result(getScore(pobi), getScore(crong))
 }
 
@@ -31,7 +35,7 @@ fun getScore(pages: List<Int>): Int {
 }
 
 fun isPagesRange(pages: List<Int>): Boolean {
-    return pages.minOf { page -> page > minPage } || pages.maxOf { page -> page < maxPage }
+    return pages.minOf { page -> page > MIN_PAGE } || pages.maxOf { page -> page < MAX_PAGE }
 }
 
 fun isPages(pages: List<Int>): Boolean {
@@ -40,8 +44,8 @@ fun isPages(pages: List<Int>): Boolean {
 
 fun result(pobiScore: Int, crongScore: Int): Int {
     return when {
-        pobiScore > crongScore -> return 1
-        pobiScore < crongScore -> return 2
-        else -> 0
+        pobiScore > crongScore -> return POBI_WIN_NUMBER
+        pobiScore < crongScore -> return CRONG_WIN_NUMBER
+        else -> DRAW_NUMBER
     }
 }
