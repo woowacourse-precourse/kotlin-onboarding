89db4442a1370bb259051ad0ea65fc76c728a763
윤동현 <donghyunyoon81@gmail.com>
2022-11-24 23:05:12 +0900

    feat<Problem2AnotherCase> : 암호문에 연속된 중복 문자를 제거하는 기능
    중복된 글자가 없을시 Pair의 first에 받은 암호문 그대로 다시 반환한다.
    중복된 문자가 있을시 count는 증가시키고 minusCount는 감소 시킨다.
    암호문에서 중복제거를 할시 1이고 1이 아닐 경우에 ex(ee,aaa,aaaa)  암호문 공백 loop false 반환
    중복이 끝날시에 연속적으로 중복된 문자 모두 제거

--- src/main/kotlin/onboarding/Problem2AnotherCase.kt
+++ src/main/kotlin/onboarding/Problem2AnotherCase.kt
@@ -4,6 +4,22 @@ fun solution2AnotherCase(cryptogram: String): String {
     return ""
 }
 
+private fun decode(cryptogram: String): Pair<String, Boolean> {
+    var count = 1
+    var minusCount = 1
+    for (index in cryptogram.indices) {
+        if (!hasDuplicate(index,cryptogram.length)) return Pair(cryptogram, false)
+        if (isDuplicate(cryptogram, index)) {
+            count++
+            minusCount--
+            if (cryptogram.toList().distinct().size == 1 && cryptogram.length != 1) return Pair("", false)
+            if (cryptogram[index +minusCount ] != cryptogram[index + count +minusCount]) {
+                return Pair(cryptogram.removeRange(index + minusCount, index + count + minusCount), true)
+            }
+        }
+    }
+    return Pair(cryptogram, false)
+}
 private fun isDuplicate(cryptogram: String, index: Int) = cryptogram[index] == cryptogram[index + 1]
 
 private fun hasDuplicate(index: Int, length: Int) = index != length - 1
