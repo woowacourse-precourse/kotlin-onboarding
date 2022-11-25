705b89dd7281b9a81e37a42c9c1a87d4cc78e6ef
윤동현 <donghyunyoon81@gmail.com>
2022-11-25 17:32:01 +0900

    feat<Problem3> : 상수 계산할 3,6,9  초기화 숫자 0 for문 시작 부분 1 선언

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,6 +1,13 @@
 package onboarding
 
 //- 3의 개수가 있을때마다 숫자가 올라가는 기능
+const val THREE = '3'
+const val SIX = '6'
+const val NINE = '9'
+const val INITIAL_NUMBER = 0
+const val LOOP_START_NUMBER = 1
 fun solution3(number: Int): Int {
     TODO("프로그램 구현")
 }
+
+
