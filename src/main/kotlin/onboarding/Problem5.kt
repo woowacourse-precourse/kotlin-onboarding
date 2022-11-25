ba76ef5fb4e48d92066cd04f82b295d48d52a8a8
ChunBaee <mljung10@naver.com>
2022-11-25 15:31:43 +0900

    refactor: Sol5. 현재 금액으로 바꾼 수, 나머지를 반환받는 부분 List에서 Pair로 변경
    바꾼 수, 나머지값 총 2개의 Int형만 반환받으면 되므로 List보다는 Pair가 더 효율적일 것 같다는 생각이 들어 변경했다.

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -10,8 +10,8 @@ fun loopExchange(money : Int, resultList : MutableList<Int> ) : List<Int> {
     var mMoney = money
     for(i in priceList) {
         val mList = checkHowMuch(mMoney, i)
-        resultList.add(mList[0]) //위의 mList의 0번째에는 몫의 값, 즉 몇 번 바꿀 수 있는지가 들어간다. 그러므로 결과값을 리턴해줄 List에 추가한다.
-        mMoney = mList[1] // mList의 1번째에는 나머지 값, 즉 남은 금액이 들어가므로, 이를 활용해 다시 한번 루프를 돌리면 남은 금액에 그 다음번 필터가 몇 번 돌아갈지가 구해진다.
+        resultList.add(mList.first) //위의 mList의 0번째에는 몫의 값, 즉 몇 번 바꿀 수 있는지가 들어간다. 그러므로 결과값을 리턴해줄 List에 추가한다.
+        mMoney = mList.second // mList의 1번째에는 나머지 값, 즉 남은 금액이 들어가므로, 이를 활용해 다시 한번 루프를 돌리면 남은 금액에 그 다음번 필터가 몇 번 돌아갈지가 구해진다.
     }
     return resultList
 }
@@ -19,7 +19,7 @@ fun loopExchange(money : Int, resultList : MutableList<Int> ) : List<Int> {
 /**
  * 현재 금액을 몇 번 바꿀 수 있는지, 바꾼 후 금액은 얼마인지 계산해주는 함수
  */
-private fun checkHowMuch(price : Int, priceList : Int) : List<Int> {
-    return listOf(price / priceList, price % priceList) // 몫과 나머지 모두를 반환해줘야 하므로 List<Int> 형 반환을 선택했다.
+private fun checkHowMuch(price : Int, priceList : Int) : Pair<Int, Int> {
+    return Pair(price / priceList, price % priceList) // 몫과 나머지 모두를 반환해줘야 하므로 Pair<Int, Int> 형 반환을 선택했다.
 }
 
