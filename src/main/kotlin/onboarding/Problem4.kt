4b290613839cbc5662372c718864757627353477
윤동현 <donghyunyoon81@gmail.com>
2022-11-26 21:09:56 +0900

    feat<Problem4> : 각 알파벳을 반대 순서로 호출하는 getReverseWord 기능 구현
    word를 매개변수로 받아 map을 사용해서 각 알파벳을 호출한다.
    reverseWord에 소문자 대문자 그 외에 값을 구분해서 반대 순서의 알파벳을 반환해서 추가한다.
    reverseWord를 반환한다.

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -6,16 +6,28 @@ const val LOWER_START_ALPHABET = 'a'
 const val LOWER_END_ALPHABET = 'z'
 const val UPPER_START_ALPHABET = 'A'
 const val UPPER_END_ALPHABET = 'Z'
+const val INITIAL_STRING = ""
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
 
+private fun alphabetRange(start: Char, end: Char) = (start..end).toList()
 
+private fun getReverseWord(word: String): String {
+    var reverseWord = INITIAL_STRING
+    word.map { alphabet ->
+        reverseWord += if (alphabetRange(LOWER_START_ALPHABET, LOWER_END_ALPHABET).contains(alphabet)) {
+            reverseLowerAlphabets()[alphabet]
+        } else if (alphabetRange(UPPER_START_ALPHABET, UPPER_END_ALPHABET).contains(alphabet)) {
+            reverseLowerAlphabets()[alphabet]
+        } else alphabet
+    }
+    return reverseWord
+}
 
-private fun alphabetRange(start:Char,end:Char) = (start..end).toList()
 private fun reverseLowerAlphabets(): Map<Char, Char> {
     val lowerAlphabetsMap = mutableMapOf<Char, Char>()
-    val lowerAlphabets = alphabetRange(LOWER_START_ALPHABET,LOWER_END_ALPHABET)
+    val lowerAlphabets = alphabetRange(LOWER_START_ALPHABET, LOWER_END_ALPHABET)
     for (i in lowerAlphabets.indices) {
         lowerAlphabetsMap[lowerAlphabets[i]] = lowerAlphabets.reversed()[i]
     }
@@ -25,7 +37,7 @@ private fun reverseLowerAlphabets(): Map<Char, Char> {
 
 private fun reverseUpperAlphabets(): Map<Char, Char> {
     val upperAlphabetsMap = mutableMapOf<Char, Char>()
-    val upperAlphabets = alphabetRange(UPPER_START_ALPHABET,UPPER_END_ALPHABET)
+    val upperAlphabets = alphabetRange(UPPER_START_ALPHABET, UPPER_END_ALPHABET)
     for (upperAlphabet in upperAlphabets.indices) {
         upperAlphabetsMap[upperAlphabets[upperAlphabet]] = upperAlphabets.reversed()[upperAlphabet]
     }
@@ -33,6 +45,6 @@ private fun reverseUpperAlphabets(): Map<Char, Char> {
 }
 
 private fun isAlphabet(alphabet: Char): Boolean {
-    val allAlphabets = alphabetRange(UPPER_START_ALPHABET,LOWER_END_ALPHABET)
+    val allAlphabets = alphabetRange(UPPER_START_ALPHABET, LOWER_END_ALPHABET)
     return allAlphabets.contains(alphabet)
 }
