444561cd32918f27bb2112bb85d97d7d4abcc911
윤동현 <donghyunyoon81@gmail.com>
2022-11-26 23:41:34 +0900

    feat<Problem5> : 지폐 단위를 enum Class의 상수로 선언

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -4,3 +4,14 @@ fun solution5(money: Int): List<Int> {
     TODO("프로그램 구현")
 }
 
+enum class BillUnits(val unit : Int) {
+    FIFTY_THOUSAND(50000),
+    TEN_THOUSAND(10000),
+    FIVE_THOUSAND(5000),
+    ONE_THOUSAND(1000),
+    FIVE_HUNDRED(500),
+    ONE_HUNDRED(100),
+    FIFTY(50),
+    TEN(10),
+    ONE(1)
+}
