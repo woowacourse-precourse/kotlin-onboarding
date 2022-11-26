2ee847e67e9981acbc49475b3205abd341068c62
윤동현 <donghyunyoon81@gmail.com>
2022-11-26 20:09:35 +0900

    feat<Problem4> :  알파벳 상수 선언
    소문자 시작과 'a' 소문자 끝 'z'
    대문자 시작 'A' 대문자 끝 'Z' 선언

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -2,6 +2,13 @@ package onboarding
 
 //- 알파벳을 반대로 출력 하는 기능
 
+const val LOWER_START_ALPHABET = 'a'
+const val LOWER_END_ALPHABET = 'z'
+const val UPPER_START_ALPHABET = 'A'
+const val UPPER_END_ALPHABET = 'Z'
 fun solution4(word: String): String {
     TODO("프로그램 구현")
 }
+
+
+
