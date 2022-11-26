0de0afbd994a128d471e13579db0a3a7a702be7d
윤동현 <donghyunyoon81@gmail.com>
2022-11-26 21:46:35 +0900

    comment<Problem4> : 기능 구현에 참고할 주석 삭제

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,12 +1,11 @@
 package onboarding
 
-//- 알파벳을 반대로 출력 하는 기능
-
 const val LOWER_START_ALPHABET = 'a'
 const val LOWER_END_ALPHABET = 'z'
 const val UPPER_START_ALPHABET = 'A'
 const val UPPER_END_ALPHABET = 'Z'
 const val INITIAL_STRING = ""
+
 fun solution4(word: String): String = getReverseWord(word)
 
 private fun alphabetRange(start: Char, end: Char) = (start..end).toList()
