833e11bb8c7a76ee92793b66a5dbe3d3e61f543a
윤동현 <donghyunyoon81@gmail.com>
2022-11-27 18:49:45 +0900

    feat<Problem6> : 기능에 사용할 상수 선언
    NICKNAME의 자릿수 1
    EMAIL의 자릿수 0
    다음 자릿수 1

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -3,6 +3,11 @@ package onboarding
 //- 연속된 글자 추출 기능
 //- 연속된 글자가 있는지 확인하는 기능
 //- 이메일을 추출 하는 기능
+
+const val NICKNAME_INDEX = 1
+const val EMAIL_INDEX = 0
+const val NEXT_NUMBER = 1
 fun solution6(forms: List<List<String>>): List<String> {
     TODO("프로그램 구현")
 }
+
