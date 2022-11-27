a9afb806e2d634be786d1231711f6e7062a70d94
윤동현 <donghyunyoon81@gmail.com>
2022-11-27 22:09:52 +0900

    comment<Problem7> : 구현할 기능 목록 주석으로 작성
    기능 구현할 목록을 보면서 구현하기 위해서

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -1,5 +1,11 @@
 package onboarding
 
+//- 사용자가 함께 아는 친구 목록 반환 기능
+//- 사용자의 타임라인에 방문한 사람들과 그 횟수 구하는 기능
+//- user 길이 제한 기능
+//- friends 길이 제한 기능
+//- visitors 길이 제한 기능
+//- 사용자 아이디 알파벳 소문자 제한 기능
 fun solution7(
     user: String,
     friends: List<List<String>>,
