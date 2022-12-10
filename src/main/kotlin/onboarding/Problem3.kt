396a769a8c1a861759a6ce9b2b303a7dbeaf77b2
ChunBaee <mljung10@naver.com>
2022-12-10 17:48:06 +0900

    refactor: Sol3. 확장함수 사용하여 전반적인 메서드 길이 축약 및 쓸데없는 변수 삭제 리팩토링

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -7,12 +7,10 @@ fun solution3(number: Int): Int {
 /**
  * 원하는 숫자 (3, 6, 9)를 포함하는 숫자 갯수 반환하는 함수
  */
-private fun checkContain(num : Int, standard : Int) : Int {
+private fun checkContain(num: Int, standard: Int): Int {
     var count = 0
-    for(i in 1..num) {
-        if(i.toString().contains(standard.toString())) { //3이 포함된 숫자일 경우 동작
-            count += checkHowMuch(i.toString(), standard) //3이 하나만 포함되지 않았을 수도 있으므로 몇 개의 3이 있는지를 판단 후 추가한다. ex) 33, 303, 313 ...
-        }
+    for (loop in 1..num) {
+        count = count.checkItHasTrigger(loop, standard)
     }
     return count
 }
@@ -20,10 +18,21 @@ private fun checkContain(num : Int, standard : Int) : Int {
 /**
  * 파라미터로 받아오는 수 안에 원하는 수 (3,6,9) 가 몇 개 포함되어 있는지 반환하는 함수
  */
-private fun checkHowMuch(param : String, num : Int) : Int{
-    return param.split("").count {it.contains(num.toString())} //숫자 각 자리별 비교를 위해 split() 사용하여 분할 후, 각 아이템을 원하는 숫자 (3,6,9)와 비교했다. ex) 33 -> 3, 3
+private fun checkHowMuch(param: String, num: Int): Int {
+    return param.split("")
+        .count { it.contains(num.toString()) } //숫자 각 자리별 비교를 위해 split() 사용하여 분할 후, 각 아이템을 원하는 숫자 (3,6,9)와 비교했다. ex) 33 -> 3, 3
 }
 
-private fun calculateTotal(number : Int) : Int {
-    return checkContain(number, 3) + checkContain(number, 6) + checkContain(number, 9)
+private fun calculateTotal(number: Int): Int {
+    return standardList().sumOf { checkContain(number, it) }
 }
+
+private fun Int.checkItHasTrigger(loop: Int, standard: Int): Int {
+    return if (loop.toString().contains(standard.toString())) {
+        this + checkHowMuch(loop.toString(), standard)
+    } else {
+        this
+    }
+}
+
+private fun standardList() = listOf(3, 6, 9)
