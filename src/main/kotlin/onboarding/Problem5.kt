e86bea544e2d79b7a6f559726aa70eebab13b907
ChunBaee <mljung10@naver.com>
2022-12-12 12:50:23 +0900

    refactor: Sol5. 배열을 돌면서 잔돈 체크해주던 함수 확장함수로 변경 및 변수명 재조정

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -3,17 +3,17 @@ package onboarding
 private val priceList = mutableListOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
 fun solution5(money: Int): List<Int> {
     val resultList = mutableListOf<Int>()
-    return loopExchange(money, resultList)
+    return resultList.loopExchange(money)
 }
 
-fun loopExchange(money : Int, resultList : MutableList<Int> ) : List<Int> {
+fun MutableList<Int>.loopExchange(money : Int) : List<Int> {
     var mMoney = money
-    for(i in priceList) {
-        val mList = checkHowMuch(mMoney, i)
-        resultList.add(mList.first) //위의 mList의 0번째에는 몫의 값, 즉 몇 번 바꿀 수 있는지가 들어간다. 그러므로 결과값을 리턴해줄 List에 추가한다.
-        mMoney = mList.second // mList의 1번째에는 나머지 값, 즉 남은 금액이 들어가므로, 이를 활용해 다시 한번 루프를 돌리면 남은 금액에 그 다음번 필터가 몇 번 돌아갈지가 구해진다.
+    for(price in priceList) {
+        val result = checkHowMuch(mMoney, price)
+        this.add(result.first) //위의 mList의 0번째에는 몫의 값, 즉 몇 번 바꿀 수 있는지가 들어간다. 그러므로 결과값을 리턴해줄 List에 추가한다.
+        mMoney = result.second // mList의 1번째에는 나머지 값, 즉 남은 금액이 들어가므로, 이를 활용해 다시 한번 루프를 돌리면 남은 금액에 그 다음번 필터가 몇 번 돌아갈지가 구해진다.
     }
-    return resultList
+    return this
 }
 
 /**
