aca149f147be8d7f7ca80395c8fd655b8db9439f
ChunBaee <mljung10@naver.com>
2022-12-12 12:43:33 +0900

    refactor: Sol4. 확장함수를 사용하여 코드 길이 축약 및 의미없던 변수 명 수정

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -7,9 +7,11 @@ const val CAPITAL_END = 90
 const val SMALL_START = 97
 const val SMALL_END = 122
 fun solution4(word: String): String {
-    val mList = word.split("").filter { it != "" } // 문자를 각 문자들을 체크하기 용이하도록 "" 기준, 즉 한 글자마다 문자열 배열로 변환한다. 이렇게 하면 맨 앞과 뒤에 공백을 가지는 빈 배열칸이 생성되므로, filter를 통해 없애준다.
+    val mList = word.convertToList()
     return checkItsLetter(mList)
 }
+
+private fun String.convertToList() : List<String> = split("").filter { it != "" }
 
 /**
  * String 을 아스키 코드를 사용하기 위해 Char 형으로 변환시켜주는 함수
@@ -18,11 +20,11 @@ private fun convertToChar(word: String): Char {
     return word.single() //.single() 은 String 문자를 Char 형태로 바꿔준다고 한다.
 }
 
-private fun returnWordToAdd(code : Int) : Char? {
-    return when(code) {
-        EMPTY_CODE -> convertEmptyLetter(code)
-        in CAPITAL_START..CAPITAL_END -> convertPrimeLetter(code)
-        in SMALL_START..SMALL_END -> convertSmallLetter(code)
+private fun Int.returnWordToAdd() : Char? {
+    return when(this) {
+        EMPTY_CODE -> convertEmptyLetter(this)
+        in CAPITAL_START..CAPITAL_END -> convertPrimeLetter(this)
+        in SMALL_START..SMALL_END -> convertSmallLetter(this)
         else -> null
     }
 }
@@ -32,8 +34,8 @@ private fun returnWordToAdd(code : Int) : Char? {
  */
 private fun checkItsLetter(list: List<String>) : String{
     val concat = mutableListOf<Char>() // 변환 후 새롭게 변경되는 문자들이 담겨질 배열
-    for (i in list) {
-        returnWordToAdd(convertToChar(i).code)?.let { concat.add(it) }
+    for (letter in list) {
+        convertToChar(letter).code.returnWordToAdd()?.let { concat.add(it) }
     }
     return String(concat.toCharArray()) // char 문자열을 String으로 취합하기 위해 사용했다. joinToString 의 경우는 공백과 , 부분이 같이 붙어서 기존에 위치해있는 공백과 구분이 어렵다고 판단되어 사용하지 않았다.
 }
